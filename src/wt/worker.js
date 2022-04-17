e7694ec21afeb0b8ee89c3c1d176e4b3de14697d
Maksim Shylau <shilaums@gmail.com>
2022-04-17 19:40:38 +0200

    feat: create basic wt structure

---
+++ src/wt/worker.js
@@ -0,0 +1,6 @@
+// n should be received from main thread
+export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+
+export const sendResult = () => {
+    // This function sends result of nthFibonacci computations to main thread
+};
