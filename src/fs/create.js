0b8ddbc423a7f09610e22b5be2bfdb19bd1aeb47
Anastasia Polivoda <polivoda.a@zovplita.by>
2022-05-30 17:01:57 +0300

    feat: done create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,19 @@
+import fs from 'fs';
+import path from 'path';
+const freshPath = path.resolve(path.dirname(''), 'src', 'fs', 'files', 'fresh.txt');
+
 export const create = async () => {
-    // Write your code here 
+    fs.access(freshPath, function (error) {
+        if (error) {
+            fs.writeFile(freshPath, "I am fresh and young", () => {});
+        } else {
+            try {
+                throw new Error('FS operation failed');
+            } catch (e) {
+                console.log(e.message);
+            }
+        }
+    });
 };
+
+create();
