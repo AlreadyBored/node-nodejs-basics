418584e48ca129ecb6c3ebeb92fc30cf4fb11647
vhoreho <vpubuntu@gmail.com>
2022-05-30 21:57:25 +0300

    feat: add created file features

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,17 @@
+import fs from 'fs';
+import * as url from 'url';
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 export const create = async () => {
-    // Write your code here 
+    fs.readdir(`${__dirname}/files`, (error, data) => {
+        if (data.includes('fresh.txt')) throw new Error('FS operation failed');
+        else {
+            fs.writeFile(`${__dirname}/files/fresh.txt`, 'I am fresh and young', (err) => {
+                console.log(err ? err.message : 'File created');
+            })
+        }
+    })
 };
+
+create();
