a718669c91095f4e92accdd06826dfe5f8347555
vhoreho <vpubuntu@gmail.com>
2022-05-31 11:02:09 +0300

    feat: add copy files features

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,19 @@
+import {copyFile, mkdir, readdir} from 'node:fs/promises'
+import url from 'url';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 export const copy = async () => {
-    // Write your code here 
+    try {
+        await mkdir(`${__dirname}/files_copy`);
+        const arrayOfFilesInDir = await readdir(`${__dirname}/files`).then(data => data);
+        await arrayOfFilesInDir.forEach(item => {
+            copyFile(`${__dirname}/files/${item}`, `${__dirname}/files_copy/${item}`)
+        })
+        console.log('Files copied');
+    }
+    catch {
+        throw new Error('FS operation failed');
+    }
 };
+
+copy();
