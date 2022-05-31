5e50cd892970d2542c7539c29d828d8b634f2c67
Imamov Erzhigit <erjigit17@gmail.com>
2022-06-01 00:12:23 +0600

    implement environment parseser

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,43 @@
 export const parseEnv = () => {
-    // Write your code here 
+  import { accessSync, readFileSync } from 'fs';
+  import { fileURLToPath } from 'url'
+  import { dirname, join } from 'path'
+
+  const __filename = fileURLToPath(import.meta.url)
+  const __dirname = dirname(__filename)
+  const file = join(__dirname, '.env')
+
+  const isFileExists = exists(file)
+  if (!isFileExists) throw new Error('FS operation failed')
+
+
+  function main() {
+    try {
+      const data = readFileSync(file);
+      console.log(parseVariables(data))
+    } catch {
+      console.error(`The file ${file} could not be read`);
+    }
+  }
+  main()
+
+
+  function parseVariables(data) {
+    const array = data.toString().split('\n').filter(el => el !== '')
+    const result = array.map(el => {
+      const variable = el.split('=')
+      if (variable.length !== 2) throw new Error(`${el} is not valid environment variable`)
+      return `RSS_${variable[0]}=${variable[1]}`
+    })
+    return result.join('; ')
+  }
+
+  function exists(path) {
+    try {
+      accessSync(path)
+      return true
+    } catch {
+      return false
+    }
+  }
 };
