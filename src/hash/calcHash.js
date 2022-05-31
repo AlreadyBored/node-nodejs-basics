be23b330cfb05e2f80d94c864774f432140574fd
bababuuui <bubinatar@gmail.com>
2022-05-31 21:47:11 +0200

    feat: implement hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,17 @@
+import * as fs  from 'fs';
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+import * as crypto from "crypto"
+
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const PATH_TO_FILE =  __dirname + "/files/fileToCalculateHashFor.txt";
+    const buffer = await fs.promises.readFile(PATH_TO_FILE)
+    const hash = crypto.createHash('sha256');
+    const hex= hash.update(buffer).digest("hex");
+    console.log(hex);
 };
+
+await calculateHash();
