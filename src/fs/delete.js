282fa8a9f67b1d9ce0cb727c1d8193fe31c88fc0
Anastasia Polivoda <polivoda.a@zovplita.by>
2022-05-31 10:42:56 +0300

    feat: done fs/delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,23 @@
+import fs from 'fs';
+import fsPromises from 'fs/promises';
+import path from 'path';
+
+const filePath = path.resolve(path.dirname(''), 'src', 'fs', 'files', 'fileToRemove.txt');
+
 export const remove = async () => {
-    // Write your code here 
+    fs.access(filePath, async function (error) {
+        if (error) throwError();
+        else {
+            await fsPromises.unlink(filePath);
+        }
+    });
 };
+function throwError() {
+    try {
+        throw new Error('FS operation failed');
+    } catch (e) {
+        console.log(e.message);
+    }
+}
+
+remove();
