30191787bcb1455c77d232c3813f99e973126f90
Anastasia Polivoda <polivoda.a@zovplita.by>
2022-06-01 00:34:12 +0300

    feat: done cli part

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,8 @@
 export const parseArgs = () => {
-    // Write your code here 
+    console.log(process.argv
+    .filter(arg => arg.match(/--\w*/))
+    .map(value => value = `${value.slice(2)} is ${process.argv[process.argv.indexOf(value) + 1]}`)
+    .join(', '));
 };
+
+parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,14 @@
 export const parseEnv = () => {
-    // Write your code here 
+    let answer = [];
+    /*console.log(process.env
+        .filter(env => env.match(/RSS_\w/))
+        //.map(value => value = `${value.slice(2)} is ${process.argv[process.argv.indexOf(value) + 1]}`)
+       /* .join(', '));*/
+       
+    for (let env in process.env){
+        if (env.match(/RSS_\w*/)) answer.push(`${env}=${process.env[env]}`);
+    }
+    console.log(answer.join('; '));
 };
+
+parseEnv();
