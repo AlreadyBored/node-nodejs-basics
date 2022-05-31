ab8499ca4cfbc6dcf9b28e7506bd269ee5fbba33
Yauheni Udavihin <e.udavihin@itransition.com>
2022-06-01 00:20:23 +0300

    Implement read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,26 @@
+import {readFile} from "node:fs/promises";
+import process from 'node:process';
+import path from 'node:path';
+
 export const read = async () => {
-    // Write your code here 
+    const currentDir = process.cwd();
+    const subDir = '/src/fs/files'
+    const fileName = 'fileToRead.txt';
+    const absolutePath = path.join(currentDir, subDir, fileName);
+
+    const STATUS_SUCCESS = 0;
+
+    const readResult = await readFile(absolutePath, {
+        encoding: "UTF8",
+    }).catch(err => err);
+
+    if (readResult instanceof Error) {
+        throw new Error('FS operation failed');
+    }
+
+    console.log(readResult);
+
+    return STATUS_SUCCESS;
 };
+
+read();
