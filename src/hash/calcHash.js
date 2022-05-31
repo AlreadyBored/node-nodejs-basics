917ccba65bf6b500c283a445c25a1915f306f6c7
Imamov Erzhigit <erjigit17@gmail.com>
2022-06-01 01:32:14 +0600

    implement SHA256 hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,37 @@
 export const calculateHash = async () => {
-    // Write your code here 
+  import { access, readFile } from 'fs/promises';
+  import { fileURLToPath } from 'url';
+  import { dirname, join } from 'path';
+  import  crypto  from 'crypto'
+
+
+  const __filename = fileURLToPath(import.meta.url)
+  const __dirname = dirname(__filename)
+  const file = join(__dirname, 'files', 'fileToCalculateHashFor.txt')
+
+  const isFileExists = await exists(file)
+  if (!isFileExists) throw new Error('FS operation failed')
+
+
+  async function main() {
+    try {
+      const data = await readFile(file );
+      const hashSum = crypto.createHash('sha256')
+      hashSum.update(data)
+      const hex = hashSum.digest('hex')
+      console.log(hex)
+    } catch {
+      console.error(`The file ${file} could not be removed`);
+    }
+  }
+  await main()
+
+  async function exists(path) {
+    try {
+      await access(path)
+      return true
+    } catch {
+      return false
+    }
+  }
 };
