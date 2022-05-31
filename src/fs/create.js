2a5953b8ca842bec94276eef71f7b51dde784be6
Imamov Erzhigit <erjigit17@gmail.com>
2022-05-31 22:41:36 +0600

    implement create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,30 @@
 export const create = async () => {
-    // Write your code here 
+  import { writeFile, access } from 'fs/promises';
+  import { fileURLToPath } from 'url'
+  import { dirname, join } from 'path'
+
+  const CONTENT = 'I am fresh and young'
+
+  const __filename = fileURLToPath(import.meta.url)
+  const __dirname = dirname(__filename)
+  const PATH = join(__dirname, 'fresh.txt')
+
+  const isFileExists = await exists(PATH)
+  if (isFileExists) throw new Error('FS operation failed')
+
+  try {
+    await writeFile(PATH, CONTENT)
+  } catch (e) {
+    console.error(e)
+  }
+
+  async function exists(path) {
+    try {
+      await access(path)
+      return true
+    } catch {
+      return false
+    }
+  }
+
 };
