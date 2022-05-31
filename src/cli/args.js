e058ddd0927d63697c817bcc9f2009270daedf15
bababuuui <bubinatar@gmail.com>
2022-05-31 21:16:35 +0200

    feat: implement cli module

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,10 @@
+
+//implement function that parses command line arguments (given in format --propName value --prop2Name value2,
+// you don't need to validate it) and prints them to the console in the format propName is value, prop2Name is value2
 export const parseArgs = () => {
-    // Write your code here 
+    for (let i=2 ; i< process.argv.length;i=i+2){
+        console.log(`${process.argv[i]} is ${process.argv[i+1]}`)
+    }
 };
+
+parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,14 @@
+//env.js - implement function that parses environment variables with prefix RSS_ and prints them to the console in the format RSS_name1=value1; RSS_name2=value2
 export const parseEnv = () => {
-    // Write your code here 
+
+    let result ="";
+    for (const key in process.env) {
+        if(key.startsWith("RSS_")){
+           const value = process.env[`${key}`];
+           result += `${key}=${value};`;
+        }
+    }
+    console.log(result);
 };
+
+parseEnv();
