2f00b5771e2ee43663c2e6091df9dbc3b097ad1c
Anastasia Polivoda <polivoda.a@zovplita.by>
2022-05-31 10:38:03 +0300

    feat: done fs/rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,28 @@
+import fs from 'fs';
+import fsPromises from 'fs/promises';
+import path from 'path';
+
+const oldPath = path.resolve(path.dirname(''), 'src', 'fs', 'files', 'wrongFilename.txt');
+const newPath = path.resolve(path.dirname(''), 'src', 'fs', 'files', 'properFilename.md');
+
 export const rename = async () => {
-    // Write your code here 
+    fs.access(oldPath, function (error) {
+        if (error) throwError();
+        else {
+            fs.access(newPath, async function (error) {
+                if (error) {
+                    await fsPromises.rename(oldPath, newPath);
+                } else throwError();
+            });
+        }
+    });
 };
+function throwError() {
+    try {
+        throw new Error('FS operation failed');
+    } catch (e) {
+        console.log(e.message);
+    }
+}
+
+rename();
