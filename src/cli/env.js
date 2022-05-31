586747ca08635f7e40c4e646e6a5df8c86c6e178
Anastasia Polivoda <polivoda.a@zovplita.by>
2022-06-01 00:37:08 +0300

    fix: cli/env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,10 +1,6 @@
 export const parseEnv = () => {
     let answer = [];
-    /*console.log(process.env
-        .filter(env => env.match(/RSS_\w/))
-        //.map(value => value = `${value.slice(2)} is ${process.argv[process.argv.indexOf(value) + 1]}`)
-       /* .join(', '));*/
-       
+           
     for (let env in process.env){
         if (env.match(/RSS_\w*/)) answer.push(`${env}=${process.env[env]}`);
     }
