eb0d1d53d3cd6f1a9a984d7388fd7e579669380b
Peter Vereshagin <peter@vereshagin.org>
2022-05-31 03:28:48 +0000

    feat: :white_check_mark: creation

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,27 @@
+'use strict'
+
+const fsPromises = require( 'fs').promises
+const path = require('path')
+
+const freshFName = path.resolve(__dirname, './files/fresh.txt')
+
 export const create = async () => {
+
     // Write your code here 
+    try{
+
+const fHandle = await fsPromises.open(freshFName, `ax`)
+await fHandle.close()
+
+// console.log(fHandle)
+
+    } catch(e){
+        console.log(e)
+        if(e.code === `EEXIST`){
+            throw new Error(`FS operation failed`)
+        }
+
+    }
 };
+
+// module.exports={create}
