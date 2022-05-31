a0bb040bbd3b36a9eb23109092d8e19d2a5b448a
rubiaqute <rubiaqute@gmail.com>
2022-05-31 23:05:03 +0300

    feat: add list implementation

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,19 @@
+import * as fs from "fs";
+import path from "path";
+import * as url from "url";
+
 export const list = async () => {
-    // Write your code here 
+  const _dirname = path.dirname(url.fileURLToPath(import.meta.url));
+  const dirPath = path.resolve(_dirname, "files");
+  const errorMessage = "FS operation failed";
+
+  fs.access(dirPath, (err) => {
+    if (err) throw new Error(errorMessage);
+    fs.readdir(dirPath, { withFileTypes: true }, (error, files) => {
+      const fileNames = files.map((file) => file.name);
+      console.log(fileNames);
+    });
+  });
 };
+
+list();
