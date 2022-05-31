533b550347fc8a8a0a55913a330aa1e903f35c71
rubiaqute <rubiaqute@gmail.com>
2022-05-31 23:49:47 +0300

    feat: add env implementation

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,14 @@
+// Для проверки
+process.env.RSS_name1 = "value1";
+process.env.RSS_name2 = "value2";
 export const parseEnv = () => {
-    // Write your code here 
+  const result = [];
+  Object.entries(process.env).forEach(([key, value]) => {
+    if (/^RSS_*/.test(key)) {
+      const item = `${key}=${value}`;
+      result.push(item);
+    }
+  });
+  console.log(result.join("; "));
 };
+parseEnv();
