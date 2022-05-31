05df1375963209219a36bc544d384beed48cf162
Yauheni Udavihin <e.udavihin@itransition.com>
2022-05-31 22:19:55 +0300

    Implement copy and create functions

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,33 @@
+import {cp, access} from "node:fs/promises";
+import process from 'node:process';
+
 export const copy = async () => {
-    // Write your code here 
+  const currentDir = process.cwd();
+  const subDir = '/src/fs'
+  const src = '/files';
+  const dest = '/files_copy';
+  const absoluteSrcPath = currentDir + subDir + src;
+  const absoluteDestPath = currentDir + subDir + dest;
+
+  const STATUS_SUCCESS = 0;
+
+  const accessResult = await access(absoluteSrcPath).catch(err => err);
+
+  if (accessResult instanceof Error) {
+    throw new Error('FS operation failed');
+  }
+
+  const cpResult = await cp(absoluteSrcPath, absoluteDestPath, {
+    errorOnExist: true,
+    force: false,
+    recursive: true,
+  }).catch(err => err);
+
+  if (cpResult instanceof Error) {
+    throw new Error('FS operation failed');
+  }
+
+  return STATUS_SUCCESS;
 };
+
+copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,26 @@
+import {writeFile, access} from "node:fs/promises";
+import process from 'node:process';
+
 export const create = async () => {
-    // Write your code here 
+  const filename = 'fresh.txt';
+  const content = 'I am fresh and young';
+  const absoluteFilePath = process.cwd() + '/src/fs/files/' + filename;
+  const STATUS_SUCCESS = 0;
+  const STATUS_FAILURE = 1;
+
+  const accessResult = await access(absoluteFilePath).catch(err => err);
+
+  if (!accessResult) {
+    throw new Error('FS operation failed');
+  }
+
+  try {
+    await writeFile(absoluteFilePath, content);
+  } catch (e) {
+    return STATUS_FAILURE;
+  }
+
+  return STATUS_SUCCESS;
 };
+
+create();
