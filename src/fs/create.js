b4eec1c5d6ea1d5eb18ae99838c90aa52b09ffd1
rubiaqute <rubiaqute@gmail.com>
2022-05-31 22:47:59 +0300

    feat: add rename implementation

--- src/fs/create.js
+++ src/fs/create.js
@@ -5,13 +5,14 @@ import * as url from "url";
 export const create = async () => {
   const _dirname = path.dirname(url.fileURLToPath(import.meta.url));
   const text = "I am fresh and young";
+  const errorMessage = "FS operation failed";
 
   fs.writeFile(
     path.resolve(_dirname, "files", "fresh.txt"),
     text,
     { flag: "wx" },
     (error) => {
-      if (error) throw new Error("FS operation failed");
+      if (error) throw new Error(errorMessage);
     }
   );
 };
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,19 @@
+import * as fs from "fs";
+import path from "path";
+import * as url from "url";
+
 export const rename = async () => {
-    // Write your code here 
+  const _dirname = path.dirname(url.fileURLToPath(import.meta.url));
+  const oldFilePath = path.resolve(_dirname, "files", "wrongFilename.txt");
+  const newFilePath = path.resolve(_dirname, "files", "properFilename.md");
+  const errorMessage = "FS operation failed";
+
+  fs.access(oldFilePath, (err) => {
+    if (err) throw new Error(errorMessage);
+    fs.access(newFilePath, (errNew) => {
+      if (!errNew) throw new Error(errorMessage);
+      fs.rename(oldFilePath, newFilePath, () => {});
+    });
+  });
 };
+rename();
