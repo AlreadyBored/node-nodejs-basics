21663359d05d0792410e022b207482718240c1ac
Anastasia Polivoda <polivoda.a@zovplita.by>
2022-05-31 11:56:11 +0300

    feat: done fs/list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,28 @@
+import fs from 'fs';
+import fsPromises from 'fs/promises';
+import path from 'path';
+
+const folder = path.resolve(path.dirname(''), 'src', 'fs', 'files');
+
 export const list = async () => {
-    // Write your code here 
+    fs.access(folder, function (error) {
+        if (error) throwError();
+        else {
+            fsPromises.readdir(folder).
+            then(files => {
+                files.forEach(file => {
+                    console.log(file);
+                });
+            })
+        }
+    });
 };
+function throwError() {
+    try {
+        throw new Error('FS operation failed');
+    } catch (e) {
+        console.log(e.message);
+    }
+}
+
+list();
