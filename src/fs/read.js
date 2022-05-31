811ab7df786dbc98d46ffc31fa54c38f38671eb4
bababuuui <bubinatar@gmail.com>
2022-05-31 20:53:55 +0200

    feat: implement fs read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,19 @@
+import * as fs  from 'fs';
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+
 export const read = async () => {
-    // Write your code here 
+
+    const PATH_TO_FILE =  __dirname + "/files/fileToRead.txt";
+
+    try {
+        console.log(await fs.promises.readFile(PATH_TO_FILE,"utf8"));
+    } catch {
+        throw new Error("FS operation failed");
+    }
 };
+
+await read();
