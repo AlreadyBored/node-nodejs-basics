b0f9da3503727e0ff429eb27fd3636a8e37f7c1f
rubiaqute <rubiaqute@gmail.com>
2022-05-31 22:54:01 +0300

    feat: add delete implementation

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,15 @@
+import * as fs from "fs";
+import path from "path";
+import * as url from "url";
+
 export const remove = async () => {
-    // Write your code here 
+  const _dirname = path.dirname(url.fileURLToPath(import.meta.url));
+  const filePath = path.resolve(_dirname, "files", "fileToRemove.txt");
+  const errorMessage = "FS operation failed";
+
+  fs.access(filePath, (error) => {
+    if (error) throw new Error(errorMessage);
+    fs.unlink(filePath, () => {});
+  });
 };
+remove();
