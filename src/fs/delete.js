9e48c123f032849023f135765d96e90ce31ae35c
bababuuui <bubinatar@gmail.com>
2022-05-31 15:48:21 +0200

    fix: change checking of file  to promises in delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -7,13 +7,12 @@ const __dirname = dirname(__filename)
 
 export const remove = async () => {
     const PATH_TO_FILE =  __dirname + "/files/fileToRemove.txt";
+    fs.promises.access(PATH_TO_FILE).then(()=> {
+        fs.promises.rm(PATH_TO_FILE).catch(e=> {throw e});
 
-    if (!fs.existsSync(PATH_TO_FILE)){
+    }).catch(()=>{
         throw new Error("FS operation failed")
-    }
-    else{
-        await fs.rmSync(PATH_TO_FILE);
-    }
+    })
 };
 
 await remove();
