b51b8fee01342e91d64008a5b4f5a786eeea8bed
rubiaqute <rubiaqute@gmail.com>
2022-05-31 21:34:38 +0300

    feat: add copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,38 @@
+import * as fs from "fs";
+import path from "path";
+import * as url from "url";
+
 export const copy = async () => {
-    // Write your code here 
+  const _dirname = path.dirname(url.fileURLToPath(import.meta.url));
+  const errorMessage = "FS operation failed";
+  const filesPath = path.resolve(_dirname, "files");
+  const filesCopyPath = path.resolve(_dirname, "files_copy");
+
+  fs.access(filesPath, function (error) {
+    if (error) {
+      throw new Error(errorMessage);
+    } else {
+      fs.access(filesCopyPath, async (error) => {
+        if (error) {
+          await fs.promises.mkdir(filesCopyPath);
+          copyFiles();
+        } else {
+          throw new Error(errorMessage);
+        }
+      });
+    }
+  });
+
+  const copyFiles = async () => {
+    fs.readdir(filesPath, { withFileTypes: true }, async (err, files) => {
+      if (err) throw new Error(errorMessage);
+      files.forEach(async (file) => {
+        await fs.promises.copyFile(
+          path.resolve(filesPath, file.name),
+          path.resolve(filesCopyPath, file.name)
+        );
+      });
+    });
+  };
 };
+copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,9 +1,18 @@
 import * as fs from "fs";
+import path from "path";
+import * as url from "url";
 
 export const create = async () => {
+  const _dirname = path.dirname(url.fileURLToPath(import.meta.url));
   const text = "I am fresh and young";
-  fs.writeFile("src/fs/files/fresh.txt", text, { flag: "wx" }, (error) => {
-    if (error) throw new Error("FS operation failed");
-  });
+
+  fs.writeFile(
+    path.resolve(_dirname, "files", "fresh.txt"),
+    text,
+    { flag: "wx" },
+    (error) => {
+      if (error) throw new Error("FS operation failed");
+    }
+  );
 };
 create();
