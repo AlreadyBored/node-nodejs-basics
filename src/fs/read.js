b915a0be51c01dafe59868530549fe658ff65e3e
vhoreho <vpubuntu@gmail.com>
2022-05-31 13:51:52 +0300

    feat: add read file features

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,14 @@
+import {readFile} from 'node:fs/promises'
+import url from 'url';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 export const read = async () => {
-    // Write your code here 
+    try {
+        await readFile(`${__dirname}/files/fileToRead.txt`, {encoding: 'utf8'}).then(data => console.log(data));
+    }
+    catch {
+        throw new Error('FS operation failed');
+    }
 };
+
+read()
