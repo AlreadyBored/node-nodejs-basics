bcdd7e348c4ca654f1d9880abbd857ab1d5bfb35
Imamov Erzhigit <erjigit17@gmail.com>
2022-05-31 23:27:26 +0600

    implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,40 @@
 export const list = async () => {
-    // Write your code here 
+  import { access, readdir } from 'fs/promises';
+  import { fileURLToPath } from 'url'
+  import { dirname, join } from 'path'
+
+  const __filename = fileURLToPath(import.meta.url)
+  const __dirname = dirname(__filename)
+  const path = join(__dirname, 'files')
+
+  const isFolderExists = await exists(path)
+  if (!isFolderExists) throw new Error('FS operation failed')
+
+
+  async function main() {
+    const files = await getFileNamesFromFolder(path)
+    if (files === -1) throw new Error(`The folder ${path} could not be readed.`)
+
+    console.clear()
+    process.stdout.write(files.join('\n'))
+  }
+  await main()
+
+
+  async function getFileNamesFromFolder(path) {
+    try {
+      return await readdir(path)
+    } catch {
+      return -1
+    }
+  }
+
+  async function exists(path) {
+    try {
+      await access(path)
+      return true
+    } catch {
+      return false
+    }
+  }
 };
