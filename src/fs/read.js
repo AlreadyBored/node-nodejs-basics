8997738d5a089eb06bb31c02c15cdaba3ca2437e
rubiaqute <rubiaqute@gmail.com>
2022-05-31 23:11:17 +0300

    feat: add read implementation

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,17 @@
+import * as fs from "fs";
+import path from "path";
+import * as url from "url";
+
 export const read = async () => {
-    // Write your code here 
+  const _dirname = path.dirname(url.fileURLToPath(import.meta.url));
+  const filePath = path.resolve(_dirname, "files", "fileToRead.txt");
+  const errorMessage = "FS operation failed";
+
+  fs.access(filePath, (error) => {
+    if (error) throw new Error(errorMessage);
+    fs.readFile(filePath, "utf8", (err, data) => {
+      console.log(data);
+    });
+  });
 };
+read();
