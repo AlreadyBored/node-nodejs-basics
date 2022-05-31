99c9d642bf381b5568cee49a1e63baba79d61b46
Imamov Erzhigit <erjigit17@gmail.com>
2022-05-31 23:27:38 +0600

    implement read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,32 @@
 export const read = async () => {
-    // Write your code here 
+  import { access, readFile } from 'fs/promises';
+  import { fileURLToPath } from 'url'
+  import { dirname, join } from 'path'
+
+  const __filename = fileURLToPath(import.meta.url)
+  const __dirname = dirname(__filename)
+  const file = join(__dirname, 'fileToRead.txt')
+
+  const isFileExists = await exists(file)
+  if (!isFileExists) throw new Error('FS operation failed')
+
+
+  async function main() {
+    try {
+      const content = await readFile(file, "utf8" );
+      console.log(content)
+    } catch {
+      console.error(`The file ${file} could not be read`);
+    }
+  }
+  await main()
+
+  async function exists(path) {
+    try {
+      await access(path)
+      return true
+    } catch {
+      return false
+    }
+  }
 };
