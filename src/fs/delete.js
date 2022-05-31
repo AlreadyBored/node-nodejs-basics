1cfff9490361ef81c86e89689dbfc5b005c0a4c0
Yauheni Udavihin <e.udavihin@itransition.com>
2022-05-31 22:56:41 +0300

    Implement delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,22 @@
+import {rm} from "node:fs/promises";
+import process from 'node:process';
+import path from 'node:path';
+
 export const remove = async () => {
-    // Write your code here 
+    const currentDir = process.cwd();
+    const subDir = '/src/fs/files'
+    const fileName = 'fileToRemove.txt';
+    const absolutePath = path.join(currentDir, subDir, fileName);
+
+    const STATUS_SUCCESS = 0;
+
+    const rmResult = await rm(absolutePath).catch(err => err);
+
+    if (rmResult instanceof Error) {
+        throw new Error('FS operation failed');
+    }
+
+    return STATUS_SUCCESS;
 };
+
+remove();
