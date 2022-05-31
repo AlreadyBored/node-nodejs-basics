7e1bbd44420f2c6a12bf33826854931e09022ac1
Yauheni Udavihin <e.udavihin@itransition.com>
2022-05-31 23:10:01 +0300

    Implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,23 @@
+import {readdir} from "node:fs/promises";
+import process from 'node:process';
+import path from 'node:path';
+
 export const list = async () => {
-    // Write your code here 
+    const currentDir = process.cwd();
+    const subDir = '/src/fs/files'
+    const absolutePath = path.join(currentDir, subDir);
+
+    const STATUS_SUCCESS = 0;
+
+    const listResult = await readdir(absolutePath).catch(err => err);
+
+    if (listResult instanceof Error) {
+        throw new Error('FS operation failed');
+    }
+
+    console.log(listResult.join('\n'));
+
+    return STATUS_SUCCESS;
 };
+
+list();
