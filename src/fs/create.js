e6d6b5728fd0bc9fa8c4b90472886207694c9be7
Ayan <a.nurinvest@gmail.com>
2022-06-01 02:58:20 +0600

    FS solution_ayan

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,15 @@
+import fsp from 'fs/promises';
 export const create = async () => {
-    // Write your code here 
+    let path = './files/fresh.txt';
+    let content = 'I am fresh and young'
+    fsp.readFile(path)
+        .catch(()=> fsp.writeFile(path, content))
+        .then((data)=>{
+            if(data){
+                 throw new Error('FS operation failed')
+            }
+        })
+        .catch(e=>console.log(e))
+ // Write your code here
 };
+create()
