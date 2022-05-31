f8aedbc16b21a812bfe6084e8f5271714c3ffeae
Yauheni Udavihin <e.udavihin@itransition.com>
2022-06-01 01:59:43 +0300

    Implemented cli task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,18 @@
+import process from "node:process"
+
 export const parseArgs = () => {
-    // Write your code here 
+    const nameRegExp = /^--/;
+    const argv = process.argv.slice(2);
+    const args = [];
+
+    while (argv.length > 0) {
+        let argName = argv.shift();
+        if (nameRegExp.test(argName)) {
+            args.push(argName.slice(2) + ' is ' + argv.shift());
+        }
+    }
+
+    console.log(args.join(', '));
 };
+
+parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,17 @@
+import process from "node:process"
+
 export const parseEnv = () => {
-    // Write your code here 
+    const regExp = /^RSS_/;
+    const envVars = process.env;
+    const vars = [];
+
+    for(let varName in envVars) {
+        if (regExp.test(varName) && envVars.hasOwnProperty(varName)) {
+            vars.push(varName + '=' + envVars[varName] + ';');
+        }
+    }
+
+    console.log(vars.join(' '));
 };
+
+parseEnv()
