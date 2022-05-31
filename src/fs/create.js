cd9ccca1e8efb883998640e576e8699670a715b3
vhoreho <vpubuntu@gmail.com>
2022-05-31 13:41:21 +0300

    feat: add rename file features

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,17 +1,17 @@
-import fs from 'fs';
-import * as url from 'url';
-const __filename = url.fileURLToPath(import.meta.url);
+import {readdir,writeFile} from 'node:fs/promises';
+import url from 'url';
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 export const create = async () => {
-    fs.readdir(`${__dirname}/files`, (error, data) => {
-        if (data.includes('fresh.txt')) throw new Error('FS operation failed');
-        else {
-            fs.writeFile(`${__dirname}/files/fresh.txt`, 'I am fresh and young', (err) => {
-                console.log(err ? err.message : 'File created');
-            })
-        }
-    })
+    try {
+        await readdir(`${__dirname}/files`).then(data => {
+           if (data.includes('fresh.txt')) throw new Error('FS operation failed');
+        })
+        await writeFile(`${__dirname}/files/fresh.txt`, 'I am fresh and young');
+    }
+    catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 create();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,14 @@
-export const rename = async () => {
-    // Write your code here 
+import {rename} from 'node:fs/promises';
+import url from 'url';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+export const renameFile = async () => {
+    try {
+        await rename(`${__dirname}/files/wrongFilename.txt`, `${__dirname}/files/properFilename.md`);
+    }
+    catch {
+        throw new Error('FS operation failed');
+    }
 };
+
+renameFile()
