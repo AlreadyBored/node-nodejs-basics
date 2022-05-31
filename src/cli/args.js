2c9d838cd4b5fa470ab6f4e204bb13a523c1b443
rubiaqute <rubiaqute@gmail.com>
2022-06-01 00:00:18 +0300

    feat: add args implementation

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,11 @@
 export const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const result = [];
+  args.forEach((arg, index) => {
+    if (/^--*/.test(arg) && args[index + 1]) {
+      result.push(`${arg.slice(2)} is ${args[index + 1]}`);
+    }
+  });
+  console.log(result.join(", "));
 };
+parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,6 +1,7 @@
 // Для проверки
 process.env.RSS_name1 = "value1";
 process.env.RSS_name2 = "value2";
+
 export const parseEnv = () => {
   const result = [];
   Object.entries(process.env).forEach(([key, value]) => {
