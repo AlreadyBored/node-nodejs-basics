c084cd92b3d39d9694b239462c6e0077201019b3
Litvinov Iurii <litvinov.yuriy1996@gmail.com>
2022-06-01 18:22:37 +0400

    feat: Copy task implemented

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,45 @@
+import { stat, constants, mkdir, readdir } from 'fs';
+import { copyFile } from 'fs/promises';
+import { fsException, folderName } from '../constants.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const copyFolderName = `${__dirname}/${folderName}-copy`;
+const rootFolderName = `${__dirname}/${folderName}`;
+
 export const copy = async () => {
-    // Write your code here 
+    stat(rootFolderName, function(err, stats) {
+        if (!stats) throw new Error(fsException);
+        stat(copyFolderName, copyFiles);
+    });
 };
+
+function copyFiles(err, stats) {
+    if (stats) throw new Error(fsException);
+
+    readdir(rootFolderName, (readdirErr, files) => {
+        if (readdirErr) throw new Error(readdirErr);
+
+        mkdir(copyFolderName, async mkdirErr => {
+            if (mkdirErr) throw new Error(mkdirErr);
+            const promises = [];
+            for (let file of files) {
+                promises.push(copyFile(
+                    `${rootFolderName}/${file}`,
+                    `${copyFolderName}/${file}`,
+                    constants.COPYFILE_EXCL
+                ));
+            }
+
+            try {
+                await Promise.all(promises);
+                console.log('All ready!'); 
+            } catch (error) {
+                if (error) throw new Error(error);
+            }
+        });
+    });
+}
+
+copy();
