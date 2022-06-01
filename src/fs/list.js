f790851b0c77fd27667ba610479a5c8dee4f028f
Siarhei Khonski <armonius@mail.ru>
2022-06-01 22:27:59 +0300

    feat: creat the function printing the list of the files

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,18 @@
-export const list = async () => {
-    // Write your code here 
+import { __dirname } from "./constants.js";
+import { readdir } from "fs/promises";
+import path from "path";
+import { stderr, stdout } from "process";
+
+export const list = async (pathToDir = path.join(__dirname, "files")) => {
+  try {
+    const filesList = await readdir(pathToDir);
+    stdout.write(`\n The directory ${pathToDir} contains: \n`)
+    filesList.forEach(fileName => stdout.write(`\t ${fileName} \n`))   
+  }
+  catch(err) {
+    stderr.write(`\n ERROR>>> FS operation failed. \n ${err.message} \n`)
+  } 
 };
+
+//list();
+//list("wrongDirectory")
