db653aa37cbb0f220a797b27ff4f4e1497b1a017
Roman Andreev <andevrom@gmail.com>
2022-06-01 10:07:55 +0300

    feat: read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,21 @@
+import {existsSync} from 'fs';
+import {readFile} from 'fs/promises';
+import {fileURLToPath} from 'url';
+import {join} from 'path';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
 export const read = async () => {
-    // Write your code here 
+    const pathFile = join(__dirname, 'files', 'fileToRead.txt');
+
+    if (!existsSync(pathFile)) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        const content = await readFile(pathFile, 'utf-8');
+        console.log(content);
+    } catch(err) {
+        throw err;
+    }
 };
