d35e9faaf80d77fa8d1130197e764de13b75daef
Litvinov Iurii <litvinov.yuriy1996@gmail.com>
2022-06-01 18:57:03 +0400

    feat: Read function implemented

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,16 @@
+import { readFile } from 'fs';
+import { fsException, folderName } from '../constants.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const rootFolderName = `${__dirname}/${folderName}`;
+
 export const read = async () => {
-    // Write your code here 
+    readFile(`${rootFolderName}/fileToRead.txt`, 'utf8', function(err, content) {
+        if (err) throw (err?.code === 'ENOENT' ? new Error(fsException) : err);
+        console.log(content);
+    });
 };
+
+read();
