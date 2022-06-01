2b08e6a493b3259065d64999ce0ae8f37510eded
bababuuui <bubinatar@gmail.com>
2022-06-01 20:01:00 +0200

    feat: implement zip task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,24 @@
+//compress.js - implement function that compresses file fileToCompress.txt to archive.gz using zlib and Streams API
+import * as fs  from 'fs';
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+import {promisify} from "util";
+import { pipeline } from "stream";
+import { createGzip } from "zlib";
+
+
 export const compress = async () => {
-    // Write your code here 
+    const pipe = promisify(pipeline);
+    const PATH_TO_FILE =  __dirname + "/files/fileToCompress.txt";
+    const PATH_TO_RESULT_FILE = __dirname + "/files/archive.gz";
+    const gzip = createGzip();
+    const source = fs.createReadStream(PATH_TO_FILE);
+    const destination = fs.createWriteStream(PATH_TO_RESULT_FILE);
+    await pipe(source, gzip, destination);
+
 };
+
+await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,25 @@
+
+//compress.js - implement function that compresses file fileToCompress.txt to archive.gz using zlib and Streams API
+import * as fs  from 'fs';
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+import {promisify} from "util";
+import { pipeline } from "stream";
+import { createGunzip } from "zlib";
+
+
 export const decompress = async () => {
-    // Write your code here 
+    const PATH_TO_RESULT_FILE =  __dirname + "/files/fileToCompress.txt";
+    const PATH_TO_FILE = __dirname + "/files/archive.gz";
+    const unzip = createGunzip();
+    const source = fs.createReadStream(PATH_TO_FILE);
+    const destination = fs.createWriteStream(PATH_TO_RESULT_FILE);
+    source.pipe(unzip).pipe(destination);
 };
+
+await decompress();
+
+
