bae30c53e761c890558c5c708ab366e54f9964e1
Artsiom Filkevich <artsiom_filkevich@senla.eu>
2022-06-01 16:14:38 +0300

    feat: implement read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,22 @@
+import { getFileDirName } from '../utils/utils.js'
+import { readFile } from 'fs/promises'
+import { join } from 'path'
+
 export const read = async () => {
-    // Write your code here 
-};
+    try {
+        const [__filename, __dirname] = await getFileDirName(import.meta.url)
+        const filePath = join(__dirname, 'files', 'fileToRead.txt')
+        try {
+            const read = await readFile(filePath, 'utf-8')
+            console.log(read)
+        }
+        catch(err) {
+            throw new Error('FS operation failed')
+        }
+    }
+    catch(err) {
+        console.error(err)
+    }
+}
+
+read()
