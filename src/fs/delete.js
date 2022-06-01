f6894a6b254faacd4e6f64a78ef1c64472c2262d
chaplygindenys <chaplygindenys@gmail.com>
2022-06-01 18:28:57 +0300

    delete +

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,17 @@
+import { rm } from "node:fs/promises";
+
 export const remove = async () => {
-    // Write your code here 
+  const path = "./files/fileToRemove.txt";
+  const errMess = "FS operation failed";
+  try {
+    rm(path).catch((e) => {
+      if (e.code === "ENOENT") {
+        throw new Error(errMess);
+      }
+    });
+  } catch (err) {
+    const error = err;
+    console.error(error.message);
+  }
 };
+remove();
