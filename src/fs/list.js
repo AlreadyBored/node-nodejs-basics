6a028df56e83c3d076a77c1da77a37eb2101ef09
effgenij <effgenij@gmail.com>
2022-06-02 01:21:15 +0300

    Change name of constant

--- src/fs/list.js
+++ src/fs/list.js
@@ -5,10 +5,10 @@ import { fileURLToPath } from "url";
 export const list = async () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
-  const filePath = path.join(__dirname, "files/");
+  const folderPath = path.join(__dirname, "files/");
 
   try {
-    const fileNames = await readdir(filePath);
+    const fileNames = await readdir(folderPath);
     console.log(fileNames);
   } catch {
     throw new Error("FS operation failed");
