a593057a2ca4b27d593518c19c6997468448a08e
Artsiom Filkevich <artsiom_filkevich@senla.eu>
2022-06-01 15:42:32 +0300

    feat: implement rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,22 @@
+import { getFileDirName } from '../utils/utils.js'
+import fs from 'fs/promises'
+import { join } from 'path'
+
 export const rename = async () => {
-    // Write your code here 
-};
+    try {
+        const [__filename, __dirname] = await getFileDirName(import.meta.url)
+        const oldFilePath = join(__dirname, 'files', 'wrongFilename.txt')
+        const newFilePath = join(__dirname, 'files', 'properFilename.txt')
+        try {
+            await fs.rename(oldFilePath, newFilePath)
+        }
+        catch(err) {
+            throw new Error('FS operation failed')
+        }
+    }
+    catch(err) {
+        console.error(err)
+    }
+}
+
+rename()
