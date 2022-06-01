ad77f4c71b50dd9709027439d392bdb16d92d94d
sodapng <sodapng@ya.ru>
2022-06-01 21:53:48 +0400

    feat: rename.js v2

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,23 @@
+import { existsSync } from 'node:fs'
+import { rename as renameFile } from 'node:fs/promises'
+import { resolve } from 'path'
+import { cwd } from 'process'
+
 export const rename = async () => {
-    // Write your code here 
-};
+  try {
+    const fileToPath = resolve(cwd(), 'src/fs/files', 'wrongFilename.txt')
+    const fileFromPath = resolve(cwd(), 'src/fs/files', 'properFilename.md')
+
+    if (
+      existsSync(fileFromPath) ||
+      (existsSync(fileToPath) && existsSync(fileFromPath))
+    )
+      throw new Error('The file exists')
+
+    await renameFile(fileToPath, fileFromPath)
+  } catch (error) {
+    console.error('FS operation failed')
+  }
+}
+
+rename()
