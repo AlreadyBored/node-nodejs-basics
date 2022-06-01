2988db6c219e0eb65bb775ee55990f175eab8be0
sodapng <sodapng@ya.ru>
2022-06-01 21:53:18 +0400

    feat: create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,16 @@
+import { writeFile } from 'node:fs/promises'
+import { resolve } from 'path'
+import { cwd } from 'process'
+
 export const create = async () => {
-    // Write your code here 
-};
+  try {
+    const filePath = resolve(cwd(), 'src/fs/files', 'fresh.txt')
+    await writeFile(filePath, 'I am fresh and young', {
+      flag: 'wx',
+    })
+  } catch (error) {
+    console.error('FS operation failed')
+  }
+}
+
+create()
