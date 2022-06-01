1607e0568a9ab22874cea34af7e7c6b57f10b741
Imamov Erzhigit <erjigit17@gmail.com>
2022-06-01 14:36:13 +0600

    finish gzip solves

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,41 @@
 export const compress = async () => {
-    // Write your code here 
+  import { pipeline } from "stream";
+  import { createReadStream, createWriteStream } from "fs";
+  import { fileURLToPath } from "url";
+  import { dirname, join } from "path";
+  import { access } from "fs/promises";
+  import zlib  from 'zlib';
+
+  const __filename = fileURLToPath(import.meta.url)
+  const __dirname = dirname(__filename)
+  const path = join(__dirname, 'files', 'fileToCompress.txt')
+  const pathOutput = join(__dirname, 'files', 'archive.gz')
+
+  const isFileExists = await exists(path)
+  if (!isFileExists) throw new Error('File not exists')
+
+  const gzip = zlib.createGzip()
+  const source = createReadStream(path)
+  const destination = createWriteStream(pathOutput)
+
+  console.clear()
+  pipeline(
+    source, gzip, destination, (err) => {
+      if (err) {
+        console.error('An error occurred:', err);
+        process.exitCode = 1;
+      }
+    }
+  )
+
+
+
+  async function exists(path) {
+    try {
+      await access(path)
+      return true
+    } catch {
+      return false
+    }
+  }
 };
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,40 @@
 export const decompress = async () => {
-    // Write your code here 
+  import { pipeline } from "stream";
+  import { createReadStream, createWriteStream } from "fs";
+  import { fileURLToPath } from "url";
+  import { dirname, join } from "path";
+  import { access } from "fs/promises";
+  import zlib  from 'zlib';
+
+  const __filename = fileURLToPath(import.meta.url)
+  const __dirname = dirname(__filename)
+  const pathArchive = join(__dirname, 'files', 'archive.gz')
+  const pathOutput = join(__dirname, 'files', 'fileToCompress.txt')
+
+  const isFileExists = await exists(pathArchive)
+  if (!isFileExists) throw new Error('File not exists')
+
+  const source = createReadStream(pathArchive)
+  const destination = createWriteStream(pathOutput)
+  const unzip = zlib.createGunzip()
+
+  console.clear()
+  pipeline(
+    source,unzip, destination, (err) => {
+      if (err) {
+        console.error('An error occurred:', err);
+        process.exitCode = 1;
+      }
+    }
+  )
+
+  async function exists(path) {
+    try {
+      await access(path)
+      return true
+    } catch {
+      return false
+    }
+  }
+
 };
