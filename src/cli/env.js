e2f6713956a7bc4e49c4262f5d09c77c2294588d
Peter Vereshagin <peter@vereshagin.org>
2022-06-01 10:39:59 +0000

    feat: :white_check_mark: implementation

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,14 @@
+'use strict'
+
 export const parseEnv = () => {
+    let output = ``
+
+    const varNames = Object.keys(process.env).filter(key => key.match(/^RSS_/))
+
+    console.log(varNames.map(key => {
+        const value = process.env[key]
+        return `${key}=${value}`
+    }).join(`;`))
+
     // Write your code here 
 };
