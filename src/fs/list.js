64970c734201f4639c7504edf67fdd766d048a13
effgenij <effgenij@gmail.com>
2022-06-02 00:47:38 +0300

    Solution for list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,18 @@
+import { readdir } from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
 export const list = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, "files/");
+
+  try {
+    const fileNames = await readdir(filePath);
+    console.log(fileNames);
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
+
+list();
