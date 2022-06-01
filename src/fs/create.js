76a38680d8ba33f836cd436b3f37f0d714135654
effgenij <effgenij@gmail.com>
2022-06-02 00:26:54 +0300

    Solution for create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,18 @@
+import { writeFile } from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
 export const create = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, "files/fresh.txt");
+  const content = "I am fresh and young";
+
+  try {
+    await writeFile(filePath, content, { flag: "wx" });
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
+
+create();
