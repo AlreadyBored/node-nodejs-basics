52cd1d053da965bb2316ed292907c9ad376c8c3b
Peter Vereshagin <peter@vereshagin.org>
2022-06-01 08:53:47 +0000

    feat: :white_check_mark: implementation

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,30 @@
-export const list = async () => {
+'use strict'
+
+const { opendir } = require('fs/promises')
+const path = require('path')
+
+const dName = path.resolve(__dirname, `files`)
+const errorText = `FS operation failed`
+
+const list = async () => {
     // Write your code here 
+    const output = []
+
+    let dirHandle
+    try {
+        dirHandle = await opendir(dName)
+    } catch (e) {
+        if (e.code === `ENOENT`) {
+            throw new Error(errorText)
+        }
+    }
+
+    let dirEntry
+    while (dirEntry = await dirHandle.read()) {
+        output.push(dirEntry.name)
+    }
+
+    console.log(output)
 };
+
+module.exports = { list }
