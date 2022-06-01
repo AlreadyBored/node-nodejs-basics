c57df67299349ef0f57e15678f9b9e82dc77e8bc
Roman Andreev <andevrom@gmail.com>
2022-06-01 09:49:47 +0300

    feat: delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,20 @@
+import {existsSync} from 'fs';
+import {rm} from 'fs/promises';
+import {fileURLToPath} from 'url';
+import {join} from 'path';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
 export const remove = async () => {
-    // Write your code here 
+    const pathFile = join(__dirname, 'files', 'fileToRemove.txt');
+
+    if (!existsSync(pathFile)) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        await rm(pathFile);
+    } catch(err) {
+        throw err;
+    }
 };
