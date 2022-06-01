8e320af343f86074dcdd213f0034e7b76c840cbf
chaplygindenys <chaplygindenys@gmail.com>
2022-06-01 19:15:02 +0300

    fs is  done

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,18 +1,17 @@
-import { readdir } from "node:fs/promises";
+import * as fsPromise from "fs/promises";
 
 export const rename = async () => {
+  const from = "./files/wrongFilename.txt";
+  const to = "./files/properFilename.md";
   const errMess = `FS operation failed`;
-  let files = [];
+
   try {
     try {
-      files = await readdir("./files");
-      for (const file of files) {
-        console.log(file);
-      }
-    } catch (e) {
-      if (e) {
-        throw new Error(errMess);
-      }
+      await fsPromise.rename(from, to);
+      const stats = await fsPromise.stat(to);
+      console.log(`stats: ${JSON.stringify(stats)}`);
+    } catch (error) {
+      throw new Error(errMess);
     }
   } catch (err) {
     const e = err;
