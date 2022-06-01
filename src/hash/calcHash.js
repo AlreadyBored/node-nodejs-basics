c37be46e66c01f17338cd039b0da9bf4296d421b
Yauheni Udavihin <e.udavihin@itransition.com>
2022-06-01 20:35:32 +0300

    Implement hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,24 @@
+import {fileURLToPath} from 'node:url';
+import {readFile} from 'node:fs/promises';
+import {createHash} from 'node:crypto';
+import path from 'node:path';
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const subFolder = 'files';
+    const fileName = 'fileToCalculateHashFor.txt';
+    const absolutePath = path.join(__dirname, subFolder, fileName);
+
+    readFile(absolutePath, {
+        encoding: "UTF8",
+    }).then((content) => {
+            const hash = createHash('sha256')
+                .update(content)
+                .digest('hex');
+
+            console.log(hash);
+        }
+    ).catch(err => err);
 };
+
+calculateHash();
