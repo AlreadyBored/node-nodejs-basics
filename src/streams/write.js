0566ecf57645d6f7282bf605fe810c7c93d53d34
sodapng <sodapng@ya.ru>
2022-06-02 02:18:58 +0400

    feat: write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,12 @@
+import { createWriteStream } from 'node:fs'
+import { resolve } from 'node:path'
+import { cwd, stdin } from 'node:process'
+import { pipeline } from 'node:stream/promises'
+
 export const write = async () => {
-    // Write your code here 
-};
+  const fileToPath = resolve(cwd(), 'src/streams/files', 'fileToWrite.txt')
+  const ws = createWriteStream(fileToPath)
+  await pipeline(stdin, ws)
+}
+
+write()
