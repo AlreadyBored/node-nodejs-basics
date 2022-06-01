7dc4a2945cecd02772cc746a66949f2cf8884a69
Peter Vereshagin <peter@vereshagin.org>
2022-06-01 09:27:24 +0000

    feat:

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,25 @@
+'use strict'
+
+const path = require('path')
+const { readFile } = require('fs/promises')
+
+const fName = path.resolve(__dirname, `files`, `fileToRead.txt`)
+
 export const read = async () => {
-    // Write your code here 
+  let output = ''
+
+  try {
+    output = await readFile(fName)
+  } catch (e) {
+    if (e.code === `ENOENT`) {
+      throw new Error(errorText)
+    } else {
+      throw e
+    }
+  }
+
+  console.log(output)
+
+  // Write your code here 
+
 };
