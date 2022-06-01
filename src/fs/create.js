721000f7f61c73222b6a196caa372b5773aa42a3
Roman Andreev <andevrom@gmail.com>
2022-06-01 06:23:46 +0300

    feat: create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,19 @@
+import {appendFile, existsSync} from 'fs';
+import {fileURLToPath} from 'url';
+import {join} from 'path';
+
+const text = 'I am fresh and young';
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+const path = join(__dirname, 'files', 'fresh.txt');
+
 export const create = async () => {
-    // Write your code here 
+    if (existsSync(path)) {
+        throw new Error('FS operation failed');
+    }
+
+    await appendFile(path, text, 'utf-8', (err) => {
+        if (err) {
+            throw err;
+        }
+    });
 };
