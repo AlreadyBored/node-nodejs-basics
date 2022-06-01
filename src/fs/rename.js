793fdfe1c00e27c7c1d8eec21b385022f6727452
Litvinov Iurii <litvinov.yuriy1996@gmail.com>
2022-06-02 00:24:17 +0400

    feat: [fs] rename function implemented

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,15 @@
+import * as fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { fsException, folderName } from '../constants.js';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const rootFolderName = `${__dirname}/${folderName}`;
+
 export const rename = async () => {
-    // Write your code here 
+    fs.rename(`${rootFolderName}/wrongFilename.txt`, `${rootFolderName}/properFilename.md`, (err) => {
+        if (err) throw (err?.code === 'ENOENT' ? new Error(fsException) : err);
+    });
 };
+
+rename();
