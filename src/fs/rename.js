491cb3d1df8505f8ce6287353aa4e372153e15be
Peter Vereshagin <peter@vereshagin.org>
2022-06-01 06:07:47 +0000

    feat: :white_check_mark: implementation

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,23 @@
+const fsPromises = require('fs/promises')
+const path = require('path')
+
+const errorText = `FS operation failed`
+const [src, dest] = [`wrongFilename.txt`, `properFilename.md`].map(dName => path.resolve(__dirname, `files`, dName))
+
 export const rename = async () => {
-    // Write your code here 
+  // Write your code here 
+  let srcStat, destStat
+  try {
+    srcStat = await fsPromises.stat(src)
+  } catch (e) {
+    throw new Error(errorText)
+  }
+
+  try {
+    destStat = await fsPromises.stat(dest)
+  } catch (e) { }
+  if (destStat) throw new Error(errorText)
+
+  await fsPromises.rename(src, dest)
+
 };
