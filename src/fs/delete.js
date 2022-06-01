4f6ca471d18d5d73d5d6022fdbf90ab220ec0442
Artsiom Filkevich <artsiom_filkevich@senla.eu>
2022-06-01 15:22:51 +0300

    feat: implement delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,21 @@
+import { getFileDirName } from '../utils/utils.js'
+import { rm } from 'fs/promises'
+import { join } from 'path'
+
 export const remove = async () => {
-    // Write your code here 
-};
+    try {
+        const [__filename, __dirname] = await getFileDirName(import.meta.url)
+        const fileToRemovePath = join(__dirname, 'files', 'fileToRemove.txt')
+        try { 
+            await rm(fileToRemovePath) 
+        }
+        catch(err) {
+            throw new Error('FS operation failed')
+        }
+    }
+    catch(err) {
+        console.error(err)
+    }
+}
+
+remove()
