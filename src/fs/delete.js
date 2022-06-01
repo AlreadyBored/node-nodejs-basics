b6d326d98192fb5936d233a1ede7cccd91e4abee
Litvinov Iurii <litvinov.yuriy1996@gmail.com>
2022-06-01 18:47:18 +0400

    feat:  Delete function implemented

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,14 @@
+import { unlink } from 'node:fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { folderName, fsException } from '../constants.js';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export const remove = async () => {
-    // Write your code here 
+    unlink(`${__dirname}/${folderName}/fileToRemove.txt`, err => {
+        if (err) throw (err?.code === 'ENOENT' ? new Error(fsException) : err);
+    });
 };
+
+remove();
