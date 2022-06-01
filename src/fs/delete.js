d7804ea46b129762c1064f7b2e5ee831872a9321
effgenij <effgenij@gmail.com>
2022-06-02 00:54:08 +0300

    Solution for delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,18 @@
+import { rm } from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
 export const remove = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, "files/fileToRemove.txt");
+
+  try {
+    await rm(filePath);
+    console.log("File removed successfully!");
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
+
+remove();
