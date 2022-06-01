654e9329741c03ef578b5ee78f559425cbd91c2b
Litvinov Iurii <litvinov.yuriy1996@gmail.com>
2022-06-01 18:52:07 +0400

    feat: List function implemented

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,16 @@
+import { readdir } from 'fs';
+import { fsException, folderName } from '../constants.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const rootFolderName = `${__dirname}/${folderName}`;
+
 export const list = async () => {
-    // Write your code here 
+    readdir(rootFolderName, (err, files) => {
+        if (err) throw (err?.code === 'ENOENT' ? new Error(fsException) : err);
+        console.log(files);
+    });
 };
+
+list();
