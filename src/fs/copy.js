0352dcd50078f08e10afd9739a69f62c9f0c7cbb
sodapng <sodapng@ya.ru>
2022-06-01 21:53:31 +0400

    feat: copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,19 @@
+import { cp } from 'node:fs/promises'
+import { resolve } from 'path'
+import { cwd } from 'process'
+
 export const copy = async () => {
-    // Write your code here 
-};
+  try {
+    const folderToPath = resolve(cwd(), 'src/fs', 'files')
+    const folderFromPath = resolve(cwd(), 'src/fs', 'files_copy')
+    await cp(folderToPath, folderFromPath, {
+      errorOnExist: true,
+      recursive: true,
+      force: false,
+    })
+  } catch (error) {
+    console.error('FS operation failed')
+  }
+}
+
+copy()
