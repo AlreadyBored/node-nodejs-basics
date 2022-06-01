190849f786823c4863a76301a36bb5991f031f8c
Peter Vereshagin <peter@vereshagin.org>
2022-06-01 17:16:29 +0000

    feat: :rocket: reader

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,7 @@
+'use strict'
+
 export const read = async () => {
+    // process.stdout.write('This file should be read using Streams API')
+    // process.stdout.end()
     // Write your code here 
 };
