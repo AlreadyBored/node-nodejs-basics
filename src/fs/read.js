2ab30273c4abca88f1501634456bac3f10dd3e81
sodapng <sodapng@ya.ru>
2022-06-01 22:18:13 +0400

    feat: read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,18 @@
+import { createReadStream } from 'node:fs'
+import { resolve } from 'node:path'
+import { cwd, stdout } from 'node:process'
+import { pipeline } from 'node:stream/promises'
+
 export const read = async () => {
-    // Write your code here 
-};
+  try {
+    const fileToPath = resolve(cwd(), 'src/fs/files', 'fileToRead.txt')
+    const rs = createReadStream(fileToPath, {
+      encoding: 'utf8',
+    })
+    await pipeline(rs, stdout)
+  } catch (error) {
+    console.error('FS operation failed')
+  }
+}
+
+read()
