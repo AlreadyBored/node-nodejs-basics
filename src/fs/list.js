eff97999ce0417cc2079d3279f09c6a905f7b190
Artsiom Filkevich <artsiom_filkevich@senla.eu>
2022-06-01 16:01:57 +0300

    feat: implement list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,22 @@
+import { getFileDirName } from '../utils/utils.js'
+import { readdir } from 'fs/promises'
+import { join } from 'path'
+
 export const list = async () => {
-    // Write your code here 
-};
+    try {
+        const [__filename, __dirname] = await getFileDirName(import.meta.url)
+        const filesDirPath = join(__dirname, 'files')
+        try {
+            const filesFromDir = await readdir(filesDirPath)
+            console.log(filesFromDir)
+        }
+        catch(err) {
+            throw new Error('FS operation failed')
+        }
+    }
+    catch(err) {
+        console.error(err)
+    }
+}
+
+list()
