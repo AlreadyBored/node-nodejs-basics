3ecdc7c310f80c394caacc4c88104a297a7550f4
Peter Vereshagin <peter@vereshagin.org>
2022-06-01 05:13:03 +0000

    feat: :white_check_mark: implementation

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,28 @@
+'use strict'
+
+const fsPromises = require('fs/promises')
+const path = require('path')
+
+const errorText = `FS operation failed`
+const [src, dest] = [`files`, `files_copy`].map(dName => path.resolve(__dirname, dName))
+
 export const copy = async () => {
-    // Write your code here 
+  let srcStat, destStat
+  try {
+    srcStat = await fsPromises.stat(src)
+  } catch (e) {
+    throw new Error(errorText)
+  }
+  if (!srcStat.isDirectory()) {
+    throw new Error(errorText)
+  }
+
+  try {
+    destStat = await fsPromises.stat(dest)
+  } catch (e) { }
+  if (destStat) throw new Error(errorText)
+
+  await fsPromises.cp(src, dest, { recursive: true })
+
+  // Write your code here 
 };
