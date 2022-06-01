ed1f36c3ee2bed0934e0e2f6291c0e80fce84a14
Artsiom Filkevich <artsiom_filkevich@senla.eu>
2022-06-01 14:58:33 +0300

    fix: utils; reject in getFileDirName

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,3 @@
-import { rejects } from 'assert';
 import { constants, access } from 'fs';
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
@@ -6,13 +5,13 @@ const { F_OK } = constants
 
 export const customExist = path => new Promise (resolve => access(path, F_OK, err => resolve(!err)))
 
-export const getFileDirName = metaUrl => new Promise (resolve => {
+export const getFileDirName = metaUrl => new Promise ((resolve, reject) => {
   try {
     const __filename = fileURLToPath(metaUrl)
     const __dirname = dirname(__filename)
     resolve([__filename, __dirname])
   }
   catch (err) {
-    rejects(err)
+    reject(err)
   }
 })
