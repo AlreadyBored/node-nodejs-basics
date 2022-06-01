d08be833feef6a4d210242b0e09723d29b9ee433
sodapng <sodapng@ya.ru>
2022-06-02 02:06:23 +0400

    feat: calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,21 @@
+import { createReadStream } from 'node:fs'
+import { resolve } from 'node:path'
+import { cwd, stdout } from 'node:process'
+import { pipeline } from 'node:stream/promises'
+
+const { createHash } = await import('node:crypto')
+
 export const calculateHash = async () => {
-    // Write your code here 
-};
+  const hash = createHash('sha256')
+
+  const fileToPath = resolve(
+    cwd(),
+    'src/hash/files',
+    'fileToCalculateHashFor.txt'
+  )
+
+  const rs = createReadStream(fileToPath)
+  await pipeline(rs, hash.setEncoding('hex'), stdout)
+}
+
+calculateHash()
