4cbfc860dcff5a3aecb56761619935ef5ad1992f
sodapng <sodapng@ya.ru>
2022-06-02 02:14:15 +0400

    feat: read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,12 @@
+import { createReadStream } from 'node:fs'
+import { resolve } from 'node:path'
+import { cwd, stdout } from 'node:process'
+import { pipeline } from 'node:stream/promises'
+
 export const read = async () => {
-    // Write your code here 
-};
+  const fileToPath = resolve(cwd(), 'src/streams/files', 'fileToRead.txt')
+  const rs = createReadStream(fileToPath, { encoding: 'utf8' })
+  await pipeline(rs, stdout)
+}
+
+read()
