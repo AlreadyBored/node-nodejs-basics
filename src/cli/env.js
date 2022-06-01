a1a0fdfdb07103fc99c547878a03965c73fa43c6
Litvinov Iurii <litvinov.yuriy1996@gmail.com>
2022-06-02 01:08:37 +0400

    feat: [cli] env function implemented

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,9 @@
 export const parseEnv = () => {
-    // Write your code here 
+    const message = Object.keys(process.env)
+        .filter(key => key.includes('RSS_'))
+        .map(key => `${key}=${process.env[key]}`)
+        .join('; ');
+    console.log(message);
 };
+
+parseEnv();
