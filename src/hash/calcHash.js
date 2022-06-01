442fa37cff8bb0aafa30df9a75c3d6b54d1cee6b
Anastasia Polivoda <polivoda.a@zovplita.by>
2022-06-01 23:00:49 +0300

    feat: done hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,19 @@
+import crypto from 'crypto';
+import fs from 'fs';
+import path from 'path';
+
+const toRead = path.resolve(path.dirname(''), 'src', 'hash', 'files', 'fileToCalculateHashFor.txt');
+
 export const calculateHash = async () => {
-    // Write your code here 
+    await fs.promises.readFile(toRead)
+        .then(fileBuffer => {
+            const hashSum = crypto.createHash('sha256');
+            hashSum.update(fileBuffer);
+
+            const hex = hashSum.digest('hex');
+
+            console.log(hex);
+        })
 };
+
+calculateHash()
