39b53411bf5ea2480fdb39a31b62c0e5af4bdb6d
sodapng <sodapng@ya.ru>
2022-06-01 22:02:54 +0400

    feat: delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,14 @@
+import { rm } from 'node:fs/promises'
+import { resolve } from 'node:path'
+import { cwd } from 'node:process'
+
 export const remove = async () => {
-    // Write your code here 
-};
+  try {
+    const fileToPath = resolve(cwd(), 'src/fs/files', 'fileToRemove.txt')
+    await rm(fileToPath)
+  } catch (error) {
+    console.error('FS operation failed')
+  }
+}
+
+remove()
