27deaf26ed957d563cd958481da78d02aa075089
sodapng <sodapng@ya.ru>
2022-06-02 02:33:42 +0400

    feat: transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,18 @@
+import { stdin, stdout } from 'node:process'
+import { Transform } from 'node:stream'
+import { pipeline } from 'node:stream/promises'
+
 export const transform = async () => {
-    // Write your code here 
-};
+  const reverseTransform = new Transform({
+    transform(chunk, _, callback) {
+      callback(
+        null,
+        chunk.toString().split('').reverse().join('').trim() + '\n'
+      )
+    },
+  })
+
+  await pipeline(stdin, reverseTransform, stdout)
+}
+
+transform()
