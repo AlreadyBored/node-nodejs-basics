f11d6bb2999c21a78c6fb6a0f4294b31f70da22e
sodapng <sodapng@ya.ru>
2022-06-02 20:23:27 +0400

    refactor: args.js replaced reduce

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,23 +1,21 @@
 import { argv } from 'node:process'
 
 export const parseArgs = () => {
-  argv
-    .slice(2)
-    .reduce((acc, key, i, arr, value = arr[i + 1]) => {
-      return !(i % 2) && value ? acc.push(`${key} is ${value}`) && acc : acc
-    }, [])
-    .forEach((item) => {
-      console.log(item)
-    })
+  // argv
+  //   .slice(2)
+  //   .reduce((acc, key, i, arr, value = arr[i + 1]) => {
+  //     return !(i % 2) ? acc.push(`${key} is ${value}`) && acc : acc
+  //   }, [])
+  //   .forEach((item) => {
+  //     console.log(item)
+  //   })
 
-  //   A better solution :D
-  //   const valueArgv = argv.slice(2)
+  // A better solution :D
+  const argValues = argv.slice(2)
 
-  //   for (let i = 0; i < valueArgv.length; i += 2) {
-  //     if (valueArgv[i + 1]) {
-  //       console.log(`${valueArgv[i]} is ${valueArgv[i + 1]}`)
-  //     }
-  //   }
+  for (let i = 0; i < argValues.length; i += 2) {
+    console.log(`${argValues[i]} is ${argValues[i + 1]}`)
+  }
 }
 
 parseArgs()
