9b9fef608670fb8b86e3e47af65fe75af6daf252
vitalysoroko95 <isephar@gmail.com>
2022-06-02 16:37:12 +0300

    feat: add create solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,20 @@
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import * as fs from "fs";
+import * as path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const create = async () => {
-    // Write your code here 
+  fs.writeFile(
+    path.join(__dirname, "files", "fresh.txt"),
+    "I am fresh and young",
+    (err) => {
+      if (err) throw err;
+      console.log("Файл был создан");
+    }
+  );
 };
+
+create();
