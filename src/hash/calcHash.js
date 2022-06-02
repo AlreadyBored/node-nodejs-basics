09235bf2117be655c7974c2d43dfa65f2e22fc07
Siarhei Khonski <armonius@mail.ru>
2022-06-02 13:05:50 +0300

    feat: create the function calculating the hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,22 @@
+import { stderr, stdout } from "process";
+import { createHash } from "crypto";
+import { open } from "fs/promises";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+
 export const calculateHash = async () => {
-    // Write your code here 
+  try {
+    stdout.write(`\t\t\t-----CALCULATING SHA256 HASH-----\n`);
+    const filehandle = await open(__filename, "r");
+    const data = await filehandle.readFile();
+    await filehandle.close();
+    const hash = createHash("sha256");
+    hash.update(data);
+    stdout.write(`The SHA256 HASH is ${hash.digest("hex")}. \n`);
+  } 
+  catch(err) {
+    stderr.write(`ERROR>>> ${err.message} \n`);
+  };  
 };
+
+calculateHash();
