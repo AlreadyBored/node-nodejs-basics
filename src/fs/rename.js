8556044de2e18aac2f8cb2e939476abe239a57f0
«Alexander-Zhibul» <alexanderzhibul@mail.ru>
2022-06-03 00:25:46 +0300

    init(nodejs-basics): add function in the rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,21 @@
+import fs from "fs";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(__dirname, "files", "wrongFilename.txt");
+const copyFilePath = path.join(__dirname, "files", "properFilename.md");
+
 export const rename = async () => {
-    // Write your code here 
+  // Write your code here
+
+  fs.rename(filePath, copyFilePath, (error) => {
+    if (error) {
+      throw new Error("FS operation failed");
+    }
+  });
 };
+
+rename();
