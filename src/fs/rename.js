2dfd04471be6b2e05c1d4d7f257d6787fb1cbf81
raimovasad <81373435+asadbek2021@users.noreply.github.com>
2022-06-02 20:44:05 +0500

    feat(rename): add rename functionality

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,27 @@
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { promisify } from 'util';
+import { join, dirname } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const renameFile = promisify(fs.rename);
+
 export const rename = async () => {
-    // Write your code here 
+   
+    try {
+        if(
+            fs.existsSync(join(__dirname,'files','wrongFilename.txt')) &&
+            !fs.existsSync(join(__dirname,'files', 'properFilename.md'))
+        ){
+           await renameFile(join(__dirname,'files','wrongFilename.txt'), join(__dirname, 'files', 'properFilename.md'))
+        }
+        else {
+            throw new Error('FS operation failed: Files are already exist')
+        }
+    }
+    catch(err) {
+        console.error(err.message);
+    }
+    
 };
+rename()
