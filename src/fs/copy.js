026bc0df0259a633103712967f9ed9542dd33369
Anton Barouka <52159126+blinkoliver@users.noreply.github.com>
2022-06-02 22:58:25 +0300

    init: fork and first exercises

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,24 @@
+import { existsSync, readdirSync, copyFile } from "fs";
+import { mkdir } from "fs/promises";
+
 export const copy = async () => {
-    // Write your code here 
+  if (existsSync("./files")) {
+    if (!existsSync("./files_copy")) {
+      await mkdir("files_copy", true);
+      const files = readdirSync("./files");
+      files.forEach((file) => {
+        copyFile(`./files/${file}`, `./files_copy/${file}`, (err) => {
+          if (err) {
+            throw new Error("failed to copy file");
+          }
+        });
+      });
+    } else {
+      throw new Error("folder files_copy already exist");
+    }
+  } else {
+    throw new Error("folder files not exist");
+  }
 };
+
+copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,12 @@
-export const create = async () => {
-    // Write your code here 
+import { appendFile, open } from "fs/promises";
+
+const create = async () => {
+  try {
+    await open("./fresh.txt", "wx");
+    const content = "I am fresh and young";
+    await appendFile("fresh.txt", content);
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
+create();
