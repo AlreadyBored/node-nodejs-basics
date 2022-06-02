3085be2168f30df3dafd4cbc6c8deb357e7866ea
sodapng <sodapng@ya.ru>
2022-06-02 15:32:42 +0400

    feat: main.js & worker.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,38 @@
+import { cpus } from 'node:os'
+import { resolve } from 'node:path'
+import { cwd } from 'node:process'
+import { Worker } from 'node:worker_threads'
+
 export const performCalculations = async () => {
-    // Write your code here
-};
+  const workers = []
+  const numOfCpus = cpus().length
+  const filename = resolve(cwd(), 'src/wt', 'worker.js')
+
+  for (let i = 0; i < numOfCpus; i++) {
+    const workerData = {
+      n: 10 + i,
+    }
+
+    workers.push(
+      new Promise((resolve, reject) => {
+        const worker = new Worker(filename, {
+          workerData,
+        })
+
+        worker.on('message', resolve)
+        worker.on('error', reject)
+      })
+    )
+  }
+
+  const result = await Promise.allSettled(workers)
+
+  return result.map(({ status, value }) => {
+    return {
+      status: status === 'fulfilled' ? 'resolved' : 'error',
+      data: value ? value : null,
+    }
+  })
+}
+
+console.log(await performCalculations())
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,29 +1,12 @@
-import { resolve } from 'node:path'
-import { cwd } from 'node:process'
-import {
-  isMainThread,
-  parentPort,
-  Worker,
-  workerData,
-} from 'node:worker_threads'
+import { parentPort, workerData } from 'node:worker_threads'
 
 export const nthFibonacci = (n) =>
   n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2)
 
-export const sendResult = (props) => {
-  const result = nthFibonacci(props.n)
+export const sendResult = () => {
+  const { n } = workerData
+  const result = nthFibonacci(n)
   parentPort.postMessage(result)
 }
 
-if (isMainThread) {
-  const filename = resolve(cwd(), 'src/wt', 'worker.js')
-  const worker = new Worker(filename, {
-    workerData: { n: 6 },
-  })
-
-  worker.on('message', (msg) => {
-    console.log(msg)
-  })
-} else {
-  sendResult(workerData)
-}
+sendResult()
