8c244360ecaa518e7d58356a5fd8374802a9dc9c
Yauheni Udavihin <e.udavihin@itransition.com>
2022-06-02 13:33:09 +0300

    Implement read stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,26 @@
+import {open} from 'node:fs/promises';
+import {fileURLToPath} from 'node:url';
+import path from 'node:path';
+import process from 'node:process';
+
 export const read = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const subDir = 'files'
+    const fileName = 'fileToRead.txt';
+    const absolutePath = path.join(__dirname, subDir, fileName);
+
+    const fd = await open(absolutePath).catch(err => err);
+
+    if (fd instanceof Error) {
+        throw new Error('FS operation failed');
+    }
+
+    const stream = fd.createReadStream({encoding: 'utf8', autoClose: false});
+    const outStream = process.stdout;
+
+    stream
+        .on('end', () => {stream.close()})
+        .pipe(outStream);
 };
+
+read();
