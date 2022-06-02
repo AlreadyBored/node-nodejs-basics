2c13fdc76cb73f1a04f271a45f3026c9669e1ec1
Vladyslav <makarellajs@gmail.com>
2022-06-02 08:21:24 +0300

    streams: done

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,9 @@
+import { createReadStream } from 'fs';
+
 export const read = async () => {
-    // Write your code here 
+  const stream = createReadStream('src/streams/files/fileToRead.txt', 'utf8');
+
+  stream.on('data', (text) => process.stdout.write(text));
 };
+
+read();
