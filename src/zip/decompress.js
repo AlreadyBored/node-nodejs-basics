75a7a8ed42f802411de4517adce16e6a55f38f5e
Vladyslav <makarellajs@gmail.com>
2022-06-02 09:01:31 +0300

    zip: done

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,12 @@
+import { createUnzip } from 'zlib';
+import { createWriteStream, createReadStream } from 'fs';
+
 export const decompress = async () => {
-    // Write your code here 
+  const zip = createUnzip();
+  const fileToRead = createReadStream('src/zip/files/archive.gz');
+  const fileToWrite = createWriteStream('src/zip/files/fileToCompress.txt');
+
+  fileToRead.pipe(zip).pipe(fileToWrite);
 };
+
+decompress();
