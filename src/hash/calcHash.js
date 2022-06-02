b46c706ec968905ac2b4fb7841f10e1dfd86437d
Alexey Krukovich <Shaht3r@gmail.com>
2022-06-02 21:08:39 +0300

    faet: add tusk for calculate hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,23 @@
+import crypto from 'crypto';
+import fs from "fs";
+import * as url from 'url';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 export const calculateHash = async () => {
-    // Write your code here 
+  const inputPath = `${__dirname}files/fileToCalculateHashFor.txt`;
+  const receiveData = fs.readFileSync(inputPath, 'utf8');
+  const hash = crypto.createHash('sha256');
+  let hexCode;
+
+  hash.on('readable', () => {
+    const data = hash.read();
+    if (data) {
+      hexCode = data.toString('hex');
+    }
+  });
+
+  hash.write(receiveData);
+  hash.end();
+  console.log(hexCode);
+  return hexCode;
 };
