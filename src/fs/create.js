b5071be13b8b2cf341349ada182d31e22ee4407f
Yuliya Khvan <yuliya.khv@gmail.com>
2022-06-02 22:01:54 +0600

    feat: implement create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,22 @@
+import * as fs from 'fs/promises';
+  
 export const create = async () => {
-    // Write your code here 
+  const path = './src/fs/files/fresh.txt';
+  const filePath = './src/fs/files';
+  const content = 'I am fresh and young';
+  const fileName = 'fresh.txt';
+  try {
+    const files = await fs.readdir(filePath);
+    const exist = files.find((file) => file === fileName);
+    if (exist) {
+      throw new Error('FS operation failed');
+    } else {
+      await fs.appendFile(path, content);
+      console.log('File fresh.txt created');
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
+
+create();
