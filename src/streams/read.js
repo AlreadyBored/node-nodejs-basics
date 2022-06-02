192e845141945b414c9e870e14211f060a88da7b
rubiaqute <rubiaqute@gmail.com>
2022-06-02 23:38:03 +0300

    feat: implement read stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,13 @@
+import * as fs from "fs";
+import path from "path";
+import * as url from "url";
+import { createReadStream } from "fs";
+
 export const read = async () => {
-    // Write your code here 
+  const _dirname = path.dirname(url.fileURLToPath(import.meta.url));
+  const filePath = path.resolve(_dirname, "files", "fileToRead.txt");
+  const readable = createReadStream(filePath);
+
+  readable.pipe(process.stdout);
 };
+read();
