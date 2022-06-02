b183b9f95f2367a4190dc076ea1f53879cb9c416
Maksim Kirylka <maksim_kirylka@epam.com>
2022-06-02 21:00:11 +0300

    Implement fs/create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,28 @@
+import { promises } from 'fs';
+import * as url from 'url';
+import path from 'path';
+
+const checkIfExists = async (file) => {
+    try {
+        await promises.stat(file);
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+
 export const create = async () => {
-    // Write your code here 
+    const dirname = url.fileURLToPath(new URL('.', import.meta.url));
+    const filename = path.resolve(dirname, 'files/fresh.txt');
+    try {
+        const hasFile = await checkIfExists(filename);
+        if (hasFile) {
+            throw new Error('FS operation failed');
+        }
+        await promises.writeFile(filename, 'I am fresh and young');
+    } catch (error) {
+        console.error(error);
+    }
 };
+
+await create();
