a3667115f5ac14c11b3bf51e4d939895e70a2c1c
Roman Andreev <andevrom@gmail.com>
2022-06-02 17:45:51 +0300

    feat: write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,13 @@
+import {createWriteStream} from 'fs';
+import {fileURLToPath} from 'url';
+import {join} from 'path';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
 export const write = async () => {
-    // Write your code here 
+    const pathFile = join(__dirname, 'files', 'fileToWrite.txt');
+
+    const writeStream = createWriteStream(pathFile);
+
+    process.stdin.pipe(writeStream);
 };
