45c544ce461deb2c141d71835ef7bdf77946169c
vitalysoroko95 <isephar@gmail.com>
2022-06-02 17:00:23 +0300

    feat: add copy solution

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,29 @@
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import * as fs from "fs";
+import * as path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const copy = async () => {
-    // Write your code here 
+  await fs.promises.rm(path.resolve(__dirname, "files-copy"), {
+    recursive: true,
+    force: true,
+  });
+  await fs.promises.mkdir(path.resolve(__dirname, "files-copy"), {
+    recursive: true,
+  });
+  await fs.readdir(path.resolve(__dirname, "files"), (err, files) => {
+    files.forEach((item) => {
+      fs.copyFile(
+        path.resolve(__dirname, "files", item),
+        path.resolve(__dirname, "files-copy", item),
+        (err) => {
+          if (err) throw err;
+        }
+      );
+    });
+  });
 };
+copy();
