8095e747910c5fb7894e32a665025d2e3c12fb4e
chaplygindenys <chaplygindenys@gmail.com>
2022-06-02 22:05:30 +0300

    Transform is done

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,25 @@
+import { pipeline } from "stream/promises";
+import { Transform } from "stream";
+import { StringDecoder } from "string_decoder";
+import { stdout, stdin } from "process";
+
+class Reverce extends Transform {
+  constructor(options) {
+    super(options);
+    this._decoder = new StringDecoder("utf-8");
+  }
+  _transform(chunk, encoding, callback) {
+    chunk = this._decoder.write(chunk);
+    let newStr = "";
+    for (let i = chunk.length - 1; i >= 0; i--) {
+      newStr += chunk[i];
+    }
+    chunk = newStr;
+    callback(null, chunk);
+  }
+}
+
 export const transform = async () => {
-    // Write your code here 
+  await pipeline(stdin, new Reverce(), stdout);
 };
+transform().catch(console.error);
