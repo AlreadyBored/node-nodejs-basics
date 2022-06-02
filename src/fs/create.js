4310298718f3484c0bae2472dc65e6d008d89cfb
Alexey Pirozerskiy <pirozerskiy.a@wb.ru>
2022-06-02 10:48:35 +0300

    feat:make create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,20 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const create = async () => {
-    // Write your code here 
+    const filePath= path.join(__dirname, 'files', 'fresh.txt');
+    if (fs.existsSync(filePath)) {
+        console.error('FS operation failed');
+    }
+    fs.writeFile(filePath, 'I am fresh and young', (error) => {
+        if (error) {
+            throw new Error();
+        }
+    });
 };
+
+create();
