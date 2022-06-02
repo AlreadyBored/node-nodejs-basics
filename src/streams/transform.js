aba31ae78d596397e1e14414566f8160b1f8c0de
rubiaqute <rubiaqute@gmail.com>
2022-06-03 00:00:01 +0300

    feat: add write and transform implementation

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,18 @@
+import { Transform, pipeline } from "stream";
+
 export const transform = async () => {
-    // Write your code here 
+  class myTransform extends Transform {
+    constructor(options = {}) {
+      super(options);
+    }
+    _transform(chunk, enc, cb) {
+      const newChunk = chunk.toString().split("").reverse().join("");
+      this.push(newChunk);
+      cb();
+    }
+  }
+
+  const transformStream = new myTransform();
+  pipeline(process.stdin, transformStream, process.stdout, (error) => {});
 };
+transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,12 @@
+import path from "path";
+import * as url from "url";
+import { createWriteStream } from "fs";
+
 export const write = async () => {
-    // Write your code here 
+  const _dirname = path.dirname(url.fileURLToPath(import.meta.url));
+  const filePath = path.resolve(_dirname, "files", "fileToWrite.txt");
+  const writable = createWriteStream(filePath);
+
+  process.stdin.pipe(writable);
 };
+write();
