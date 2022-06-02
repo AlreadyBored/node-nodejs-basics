bd5a6334a282c52864d80d59b26f1e50511bd908
chaplygindenys <chaplygindenys@gmail.com>
2022-06-02 09:04:13 +0300

    env done

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,13 @@
+import { env } from "process";
 export const parseEnv = () => {
-    // Write your code here 
+  try {
+    const envMassive = Object.entries(env);
+    const onlyRssMassive = envMassive.filter(([key]) => key.includes("RSS_"));
+    console.log(onlyRssMassive);
+  } catch (error) {
+    if (error) {
+      console.error(error);
+    }
+  }
 };
+parseEnv();
