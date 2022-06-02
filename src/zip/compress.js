d1a1daf32b05791afc523b5231d15e22ace3222f
Yauheni Udavihin <e.udavihin@itransition.com>
2022-06-02 20:41:56 +0300

    Implement compress and decompress functions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,30 @@
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+import {open} from 'node:fs/promises';
+import fs from 'node:fs';
+import zlib from 'node:zlib';
+
 export const compress = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const subDir = 'files'
+    const fileName = 'fileToCompress.txt';
+    const compressedFileName = 'archive.gz';
+    const absoluteReadPath = path.join(__dirname, subDir, fileName);
+    const absoluteWriteFileName = path.join(__dirname, subDir, compressedFileName);
+
+    const fd = await open(absoluteReadPath).catch(err => err);
+
+    if (fd instanceof Error) {
+        throw new Error('FS operation failed');
+    }
+
+    const readStream = fd.createReadStream();
+    const gZip = zlib.createGzip();
+    const writeStream = fs.createWriteStream(absoluteWriteFileName);
+
+    readStream
+        .pipe(gZip)
+        .pipe(writeStream);
 };
+
+compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,30 @@
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+import {open} from 'node:fs/promises';
+import fs from 'node:fs';
+import zlib from 'node:zlib';
+
 export const decompress = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const subDir = 'files'
+    const fileName = 'fileToCompress.txt';
+    const compressedFileName = 'archive.gz';
+    const absoluteOriginalPath = path.join(__dirname, subDir, fileName);
+    const absoluteCompressedFileName = path.join(__dirname, subDir, compressedFileName);
+
+    const fd = await open(absoluteCompressedFileName).catch(err => err);
+
+    if (fd instanceof Error) {
+        throw new Error('FS operation failed');
+    }
+
+    const readStream = fd.createReadStream();
+    const gunZip = zlib.createGunzip();
+    const writeStream = fs.createWriteStream(absoluteOriginalPath);
+
+    readStream
+        .pipe(gunZip)
+        .pipe(writeStream);
 };
+
+decompress();
