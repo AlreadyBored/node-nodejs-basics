5bc8297c2bd20c72b70106feb6e2b7601b91878b
Oleksii Korovushkin <alexkrv07@gmail.com>
2022-06-02 20:16:26 +0300

    feat: implement args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,13 @@
 export const parseArgs = () => {
-    // Write your code here 
+    const startProp = '--';
+    const args = process.argv.slice(2);
+    for (let i = 0; i < args.length; i++) {
+        if (args[i].slice(0,2) === startProp) {
+            console.log(`${args[i].slice(2)} is ${args[i + 1]}`);
+            i++;
+        }
+    }
 };
+
+
+parseArgs();
