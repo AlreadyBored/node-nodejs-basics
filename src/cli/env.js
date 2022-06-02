700f73037c6817719d327464c8929356bc612786
sodapng <sodapng@ya.ru>
2022-06-02 20:13:28 +0400

    fix: env.js correct conclusion

--- src/cli/env.js
+++ src/cli/env.js
@@ -2,8 +2,8 @@ import { env } from 'process'
 
 export const parseEnv = () => {
   for (const key in env) {
-    if (key.includes('RSS_')) {
-      console.log(env[key])
+    if (key.startsWith('RSS_')) {
+      console.log(`${key}=${env[key]};`)
     }
   }
 }
