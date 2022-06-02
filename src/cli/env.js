bba850afc972f9e8305bf19ba144fc95fe9580ec
Oleksii Korovushkin <alexkrv07@gmail.com>
2022-06-02 20:42:29 +0300

    feat: solve env js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,11 @@
 export const parseEnv = () => {
-    // Write your code here 
+    const prefixEnvArg = 'RSS_';
+    const envArgs = Object.keys(process.env);
+    envArgs.forEach(envArg => {
+        if (envArg.slice(0,4) === prefixEnvArg) {
+            console.log(`${envArg}=${process.env[envArg]}`);
+        }
+    });
 };
+
+parseEnv();
