c3437ad9daf1914235853dffcdf461a6ec82da77
rubiaqute <rubiaqute@gmail.com>
2022-06-02 22:34:45 +0300

    feat: implement hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,26 @@
+import path from "path";
+import * as url from "url";
+import * as crypto from "crypto";
+import { promises as fs, createReadStream } from "fs";
+
 export const calculateHash = async () => {
-    // Write your code here 
+  const _dirname = path.dirname(url.fileURLToPath(import.meta.url));
+  const filePath = path.resolve(
+    _dirname,
+    "files",
+    "fileToCalculateHashFor.txt"
+  );
+  const hash = crypto.createHash("sha256");
+  const input = createReadStream(filePath);
+  const result = await new Promise((resolve) => {
+    input.on("readable", () => {
+      const data = input.read();
+      if (data) hash.update(data);
+      else {
+        resolve(hash.digest("hex"));
+      }
+    });
+  });
+  return result;
 };
+console.log(await calculateHash());
