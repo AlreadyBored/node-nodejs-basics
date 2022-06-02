c87e772b9114cac166ff1ba2ebd4ddc7d003987f
bababuuui <bubinatar@gmail.com>
2022-06-02 23:05:04 +0200

    feat: implement child process task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,15 @@
+import * as fs  from 'fs';
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+import {fork} from "child_process";
+import * as path  from "path"
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = fork(path.join(__dirname, 'files', 'script.js'), args , {stdio: ['pipe', 'pipe', 'pipe', 'ipc']});
+    child.stdout.on('data', (data) => { console.log(`stdout: ${data}`); })
 };
+
+await spawnChildProcess(["Asda","!23","xzcz"]);
