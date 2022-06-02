a2981e946b09965684cc3ebec0ce0d9152f82f8b
«Alexander-Zhibul» <alexanderzhibul@mail.ru>
2022-06-02 21:49:34 +0300

    init(nodejs-basics): add function in the create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,26 @@
+import { fileURLToPath } from "url";
+import path from "path";
+import fs from "fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const create = async () => {
-    // Write your code here 
+  // Write your code here
+
+  const filePath = path.join(__dirname, "files", "fresh.txt");
+
+  fs.access(filePath, fs.constants.W_OK, (error) => {
+    if (error) {
+      fs.writeFile(filePath, "I am fresh and young", (error) => {
+        if (error) {
+          console.log("Sorry ...");
+        }
+      });
+    } else {
+      throw new Error("FS operation failed");
+    }
+  });
 };
+
+create();
