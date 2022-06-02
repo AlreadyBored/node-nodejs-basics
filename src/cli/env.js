320c32f29578187905fca3aab61dabf24348c1e2
Siarhei Khonski <armonius@mail.ru>
2022-06-02 10:33:16 +0300

    feat: create the function that screens the properties of the environment

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,15 @@
+import { env, stderr, stdout } from "process";
+
 export const parseEnv = () => {
-    // Write your code here 
+  try {
+    stdout.write(`\t\t\t -----THE LIST OF THE ENVIROMENT'S PROPERTIES----- \n`);
+      for (let property in env) {
+       stdout.write(`RSS_${property}=${env[property]} \n`);
+    };  
+  }
+  catch(err) {
+     stderr.write(`ERROR>>> It is incredably, but something gets wrong... \n ${err.message} \n`);
+  };
 };
+
+//parseEnv();
