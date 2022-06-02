6d77aeb7e82bb818f99d21320735425c7a63da22
zhenya_adamchyk <e.adamchyk@softteco.com>
2022-06-02 19:16:07 +0300

    wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,23 @@
+import * as workerJS from 'worker_threads'
+import * as os from 'os'
+
 export const performCalculations = async () => {
-    // Write your code here
+    const cpudLength = os.cpus().length
+    const arr = []
+
+    for (let i = 1; i <= cpudLength; i++) {
+        arr.push(
+            new Promise((resolve, rej) => {
+                new workerJS.Worker('./src/wt/worker.js', {
+                    workerData: 10 + i
+                }).on('message', msg => {
+                    resolve(msg)
+                })
+            })
+        )
+    }
+    const res = await Promise.all(arr)
+    return res
 };
+
+console.log(await performCalculations())
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,12 @@
-// n should be received from main thread
+import * as workerJS from 'worker_threads'
+
 export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 export const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    workerJS.parentPort.postMessage({
+        status: nthFibonacci(workerJS.workerData) ? 'resolved' : 'error',
+        data: nthFibonacci(workerJS.workerData) ? nthFibonacci(workerJS.workerData) : null
+    })
 };
+
+sendResult()
