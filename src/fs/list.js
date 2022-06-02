282a1fbeb9960b86b114ede878e4955b9f3a02ed
bababuuui <bubinatar@gmail.com>
2022-06-02 20:06:41 +0200

    feat: implement fs list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,21 @@
+import * as fs  from 'fs';
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+
 export const list = async () => {
-    // Write your code here 
+    const PATH_TO_FOLDER =  __dirname + "/files/";
+
+    if (!fs.existsSync(PATH_TO_FOLDER)){
+        throw new Error("FS operation failed")
+    }
+    else{
+        console.log( await fs.promises.readdir(PATH_TO_FOLDER));
+    }
 };
+
+await list();
+
+
