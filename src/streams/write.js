47932010df24472bdcf447d6a21d60a81efa6793
Yauheni Udavihin <e.udavihin@itransition.com>
2022-06-02 17:02:14 +0300

    Implement write stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,29 @@
+import {open} from 'node:fs/promises';
+import {fileURLToPath} from 'node:url';
+import path from 'node:path';
+import process from 'node:process';
+
 export const write = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const subDir = 'files'
+    const fileName = 'fileToWrite.txt';
+    const absolutePath = path.join(__dirname, subDir, fileName);
+
+    const fd = await open(absolutePath, 'r+').catch(err => err);
+
+    if (fd instanceof Error) {
+        throw new Error('FS operation failed');
+    }
+
+    const writeStream = fd.createWriteStream({encoding: 'utf8'});
+    const inStream = process.stdin;
+
+    inStream
+        .on('end', () => {
+            inStream.close();
+            writeStream.close();
+        })
+        .pipe(writeStream);
 };
+
+write();
