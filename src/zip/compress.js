cd6fcbd803e6eb1c05b8fc2c536b4c85b4810af2
sodapng <sodapng@ya.ru>
2022-06-02 12:30:40 +0400

    feat: compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,16 @@
+import { createReadStream, createWriteStream } from 'node:fs'
+import { resolve } from 'node:path'
+import { cwd } from 'node:process'
+import { pipeline } from 'node:stream/promises'
+import { createGzip } from 'node:zlib'
+
 export const compress = async () => {
-    // Write your code here 
-};
+  const fileToPath = resolve(cwd(), 'src/zip/files', 'fileToCompress.txt')
+  const fileFromPath = resolve(cwd(), 'src/zip/files', 'archive.gz')
+  const gzip = createGzip()
+  const rs = createReadStream(fileToPath)
+  const ws = createWriteStream(fileFromPath)
+  await pipeline(rs, gzip, ws)
+}
+
+compress()
