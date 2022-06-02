182c7a3fd40de783c537c6ba0fa3143f015d41ad
shiroe66 <shiroeqky@gmail.com>
2022-06-02 22:51:41 +0500

    feat: create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,16 @@
+import { writeFile } from "fs/promises"
+import { fileURLToPath } from "url"
+import path, { dirname } from "path"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 export const create = async () => {
-    // Write your code here 
-};
+  const file = path.join(__dirname, "files", "fresh.txt")
+
+  writeFile(file, "I am fresh and young", {
+    flag: "wx",
+  }).catch(() => {
+    throw new Error()
+  })
+}
