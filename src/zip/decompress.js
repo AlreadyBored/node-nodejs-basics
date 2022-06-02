f323976aea173627d63ec830c88861637e67de2b
sodapng <sodapng@ya.ru>
2022-06-02 12:30:52 +0400

    feat: decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,16 @@
+import { createReadStream, createWriteStream } from 'node:fs'
+import { resolve } from 'node:path'
+import { cwd } from 'node:process'
+import { pipeline } from 'node:stream/promises'
+import { createUnzip } from 'node:zlib'
+
 export const decompress = async () => {
-    // Write your code here 
-};
+  const fileToPath = resolve(cwd(), 'src/zip/files', 'archive.gz')
+  const fileFromPath = resolve(cwd(), 'src/zip/files', 'fileToCompress.txt')
+  const unzip = createUnzip()
+  const rs = createReadStream(fileToPath)
+  const ws = createWriteStream(fileFromPath)
+  await pipeline(rs, unzip, ws)
+}
+
+decompress()
