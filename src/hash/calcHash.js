40124f640cbcb840459198b69494a8583b8c387d
Siarhei Khonski <armonius@mail.ru>
2022-06-02 13:07:57 +0300

    feat: fix the hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -9,10 +9,10 @@ export const calculateHash = async () => {
     stdout.write(`\t\t\t-----CALCULATING SHA256 HASH-----\n`);
     const filehandle = await open(__filename, "r");
     const data = await filehandle.readFile();
-    await filehandle.close();
     const hash = createHash("sha256");
     hash.update(data);
     stdout.write(`The SHA256 HASH is ${hash.digest("hex")}. \n`);
+    await filehandle.close();
   } 
   catch(err) {
     stderr.write(`ERROR>>> ${err.message} \n`);
