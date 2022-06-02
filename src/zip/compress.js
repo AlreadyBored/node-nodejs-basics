1758da75a73c8a149fe7a67d0e77d3ce68fe3db6
Alexey Krukovich <Shaht3r@gmail.com>
2022-06-02 20:27:09 +0300

    feat: add tusk for zip and unzip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,25 @@
+import { createGzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import { promisify } from 'util';
+import { pipeline } from 'stream';
+import * as url from 'url';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 export const compress = async () => {
-    // Write your code here 
+  const inputPath = `${__dirname}/files/fileToCompress.txt`;
+  const outputPath = `${__dirname}/files/archive.gz`;
+
+  const do_gzip = async (input, output) => {
+    const pipe = promisify(pipeline);
+    const gzip = createGzip();
+    const source = createReadStream(input);
+    const destination = createWriteStream(output);
+    await pipe(source, gzip, destination);
+  }
+
+  do_gzip(inputPath, outputPath)
+    .catch((err) => {
+      console.error('An error occurred:', err);
+      process.exitCode = 1;
+    });
 };
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,25 @@
+import { createGunzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import { promisify } from "util";
+import { pipeline } from 'stream';
+import * as url from 'url';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 export const decompress = async () => {
-    // Write your code here 
+  const inputPath = `${__dirname}/files/archive.gz`;
+  const outputPath = `${__dirname}/files/fileToCompress1.txt`;
+
+  const do_unzip = async (input, output) => {
+    const pipe = promisify(pipeline);
+    const gzip = createGunzip();
+    const source = createReadStream(input);
+    const destination = createWriteStream(output, { encoding: "utf8" });
+    await pipe(source, gzip, destination);
+  }
+
+  do_unzip(inputPath, outputPath)
+    .catch((err) => {
+      console.error('An error occurred:', err);
+      process.exitCode = 1;
+    });
 };
