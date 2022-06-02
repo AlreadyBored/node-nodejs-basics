dafedcb334e7409def4bffdf4599031543f2692c
Artiom Filkevich <artiom.filkevich@gmail.com>
2022-06-02 18:26:16 +0300

    feat: implement env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,13 @@
+import { env } from 'process'
+
 export const parseEnv = () => {
-    // Write your code here 
-};
+    const accumulator = []
+    for (const property in env) {
+        if (property.startsWith('RSS_')) {
+            accumulator.push(`${property}=${env[property]}`)
+        }
+    }
+    console.log(accumulator.join('; '))
+}
+
+parseEnv()
