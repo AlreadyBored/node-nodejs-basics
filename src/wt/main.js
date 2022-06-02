1edcbd1839fe5bdfb7415b27dbf2aefd03b03e05
Alexey Krukovich <Shaht3r@gmail.com>
2022-06-03 00:30:43 +0300

    feat: add tusk for worker

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,31 @@
+import os from 'os';
+import child_process from 'child_process';
+import * as url from 'url';
+import fs from 'fs';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 export const performCalculations = async () => {
-    // Write your code here
+  const cpus = os.cpus();
+  const workerPath = `${__dirname}worker.js`;
+  const outPutPath = `${__dirname}temp.txt`;
+  const prepareData = [];
+
+  cpus.forEach((_, index) => {
+    const child = child_process.fork(workerPath, [String(10 + index)]);
+
+    child.on('message', (msg) => {
+      fs.promises.appendFile(outPutPath, msg, (err) => {
+        if (err) {
+          throw new Error('FS operation failed');
+        }
+      })
+        .then(() => prepareData.push(JSON.parse(msg)))
+        .then(() => {
+          if (prepareData.length === cpus.length) {
+            console.log(prepareData);
+            return prepareData;
+          }
+        });
+    });
+  });
 };
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,12 @@
 // n should be received from main thread
 export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
-export const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+export const sendResult = (args) => {
+  const prepareData = {
+    'status': Number.isInteger(Number(...args)) ? 'resolved' : 'error',
+    'data': Number.isInteger(Number(...args)) ? nthFibonacci(Number(...args)) : null,
+  }
+  process.send(JSON.stringify(prepareData));
 };
+
+sendResult(process.argv.slice(2));
