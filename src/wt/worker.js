599b0b80be2a21962831064a6f9dea789626d0a9
sodapng <sodapng@ya.ru>
2022-06-02 13:34:39 +0400

    feat: worker.js

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,29 @@
-// n should be received from main thread
-export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { resolve } from 'node:path'
+import { cwd } from 'node:process'
+import {
+  isMainThread,
+  parentPort,
+  Worker,
+  workerData,
+} from 'node:worker_threads'
 
-export const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
-};
+export const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2)
+
+export const sendResult = (props) => {
+  const result = nthFibonacci(props.n)
+  parentPort.postMessage(result)
+}
+
+if (isMainThread) {
+  const filename = resolve(cwd(), 'src/wt', 'worker.js')
+  const worker = new Worker(filename, {
+    workerData: { n: 6 },
+  })
+
+  worker.on('message', (msg) => {
+    console.log(msg)
+  })
+} else {
+  sendResult(workerData)
+}
