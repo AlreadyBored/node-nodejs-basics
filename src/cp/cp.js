7988f95cb6c248469950fa3b8ddf4f72bc4022bc
Ilay Morozov <morozoff1994@yandex.ru>
2022-06-03 22:34:06 +0300

    added worker and cp tasks

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,20 @@
+import url from 'url';
+import path from 'path';
+import child_process from 'child_process';
+
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    const pathToFile = path.join(__dirname, 'files', 'script.js');
+    
+    const childProcess = child_process.spawn(`node ${pathToFile}`, args, {shell: true})
+
+    process.stdin.pipe(childProcess.stdin);
+    childProcess.stdout.pipe(process.stdout)
+
+    childProcess.on('close', (code) => {
+        console.log(`child process was closed with ${code}`);
+    });
 };
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,34 @@
+import url from 'url';
+import path from 'path';
+import os from 'os';
+import { Worker } from 'worker_threads';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const performCalculations = async () => {
-    // Write your code here
+    try {
+        const valueCPUS = os.cpus().length;
+        const arr = [];
+
+        for (let i = 0; i < valueCPUS; i++) {
+            const workerPromise = new Promise((resolve) => {
+                const worker = new Worker(path.join(__dirname, 'worker.js'), {
+                    workerData: 10 + i
+                });
+                worker.on('message', (data) => {
+                    resolve({status: 'resolved', data});
+                })
+                worker.on('error', () => {
+                    resolve({status: 'error', data: null});
+                })
+            })
+            arr.push(workerPromise);
+        }
+        const result = await Promise.all(arr);
+        console.log(result);
+        return result;
+    } catch (error) {
+        console.log(error);
+    }
 };
