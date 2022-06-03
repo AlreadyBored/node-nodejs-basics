ccfa39a942d8dcf31cd60e1a62d8dbd8547a2933
ezzikone <47362010+ezzikone@users.noreply.github.com>
2022-06-03 18:12:03 +0400

    feat: hash updated

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,19 @@
+import crypto from 'crypto'
+import fs from 'fs'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
 export const calculateHash = async () => {
-    // Write your code here 
-};
+    const hash = crypto.createHash('sha256')
+    try {
+        const data = fs.readFileSync(
+            dirname(fileURLToPath(import.meta.url)) +
+                '\\files\\fileToCalculateHashFor.txt'
+        )
+        const ret = hash.update(data).digest('hex')
+        process.stdout.write(ret + '\n')
+        return ret
+    } catch (error) {
+        process.stderr.write('Hash error')
+    }
+}
