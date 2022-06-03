4161691ddd55e38d5eb13915e273a8ba06dad8ac
ezzikone <47362010+ezzikone@users.noreply.github.com>
2022-06-03 18:12:41 +0400

    feat: zlib updated

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,15 @@
+import zlib from 'zlib'
+import fs from 'fs'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
 export const compress = async () => {
-    // Write your code here 
-};
+    const __filename = fileURLToPath(import.meta.url)
+    const __dirname = dirname(__filename)
+
+    const zip = zlib.createGzip()
+    const read = fs.createReadStream(__dirname + '\\files\\fileToCompress.txt')
+    const write = fs.createWriteStream(__dirname + '\\files\\archive.gz')
+
+    read.pipe(zip).pipe(write)
+}
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,17 @@
+import zlib from 'zlib'
+import fs from 'fs'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
 export const decompress = async () => {
-    // Write your code here 
-};
+    const __filename = fileURLToPath(import.meta.url)
+    const __dirname = dirname(__filename)
+
+    const zip = zlib.createUnzip()
+    const read = fs.createReadStream(__dirname + '\\files\\archive.gz')
+    const write = fs.createWriteStream(
+        __dirname + '\\files\\fileToCompress.txt'
+    )
+
+    read.pipe(zip).pipe(write)
+}
