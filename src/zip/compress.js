e20d609821e076b557a7a052d028e277a0250c6d
chaplygindenys <chaplygindenys@gmail.com>
2022-06-03 15:19:19 +0300

    ZIP IS DONE

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,64 @@
+import { gzip, constants } from "zlib";
+import { WritableStream } from "node:stream/web";
+import { readFileSync } from "fs";
+import { appendFile } from "fs/promises";
+import { pipeline } from "stream/promises";
+import { createReadStream, createWriteStream } from "fs";
+
 export const compress = async () => {
-    // Write your code here 
+  // FIRST WAY
+
+  //   await pipeline(
+  //     createReadStream("./files/fileToCompress.txt"),
+  //     createGzip(),
+  //     createWriteStream("./files/archive.gz")
+  //   );
+
+  // SECOND WAY
+  try {
+    let zip = {};
+    let str = "";
+    try {
+      str = readFileSync("./files/fileToCompress.txt");
+    } catch (err) {
+      if (err) {
+        console.error(`readS`, err);
+        throw err;
+      }
+    }
+
+    try {
+      const stream = new WritableStream({
+        write(chunk) {
+          gzip(
+            chunk,
+            { finishFlush: constants.Z_SYNC_FLUSH },
+            (err, buffer) => {
+              if (err) {
+                console.error("An error occurred:", err);
+                process.exitCode = 1;
+              }
+              console.log(buffer);
+              appendFile("./files/archive.gz", buffer, {
+                flag: "w",
+              });
+            }
+          );
+        },
+      });
+      await stream.getWriter().write(str);
+    } catch (err) {
+      if (err) {
+        console.error(`stream`, err);
+        throw err;
+      }
+    }
+  } catch (error) {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(" succeeded.");
+    }
+  }
 };
+compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,51 @@
+import { unzip, constants } from "zlib";
+import { WritableStream } from "node:stream/web";
+import { readFileSync } from "fs";
+import { appendFile } from "fs/promises";
+
 export const decompress = async () => {
-    // Write your code here 
+  try {
+    let zip = {};
+    try {
+      zip = readFileSync("./files/archive.gz");
+    } catch (err) {
+      if (err) {
+        console.error(`readS`, err);
+        throw err;
+      }
+    }
+
+    try {
+      const stream = new WritableStream({
+        write(chunk) {
+          unzip(
+            chunk,
+            { finishFlush: constants.Z_SYNC_FLUSH },
+            (err, buffer) => {
+              if (err) {
+                console.error("An error occurred:", err);
+                process.exitCode = 1;
+              }
+              appendFile("./files/fileToCompress.txt", buffer.toString(), {
+                flag: "w",
+              });
+            }
+          );
+        },
+      });
+      await stream.getWriter().write(zip);
+    } catch (err) {
+      if (err) {
+        console.error(`stream`, err);
+        throw err;
+      }
+    }
+  } catch (error) {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(" succeeded.");
+    }
+  }
 };
+decompress();
