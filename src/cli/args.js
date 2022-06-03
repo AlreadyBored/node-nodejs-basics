5b17421288d445eb4f2d77764840a39210c45e86
effgenij <effgenij@gmail.com>
2022-06-03 23:25:42 +0300

    Solution for args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,15 @@
 export const parseArgs = () => {
-    // Write your code here 
+    const [_exec, _script, ...argv] = process.argv;
+
+    const substrings = [];
+    for(let i = 0; i < argv.length; i += 2) {
+        const name = argv[i];
+        const value = argv[i + 1];
+
+        substrings.push(`${name.slice(2)} is ${value}`);
+    }
+    const result = substrings.join(', ');
+    console.log(result);
 };
+
+parseArgs();
