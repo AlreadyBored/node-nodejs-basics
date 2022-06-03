d8c5572533fad40b805c3811c04319a7491812a3
effgenij <effgenij@gmail.com>
2022-06-03 22:38:29 +0300

    solution for env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,18 @@
 export const parseEnv = () => {
-    // Write your code here 
+  const enviroment = process.env;
+
+  let substrings = [];
+  Object.keys(enviroment).forEach((key) => {
+    if (key.startsWith("RSS_")) {
+      substrings.push(`${key}=${enviroment[key]}`);
+    }
+  });
+  if(substrings.length === 0){
+      console.log("No variables start with RSS_ in the environment");
+      return;
+  };
+  const result = substrings.join("; ");
+  console.log(result);
 };
+
+parseEnv();
