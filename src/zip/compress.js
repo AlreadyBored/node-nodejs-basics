aad92b7b5a20ffe9af263674db5b0ad6e6c6961e
Roman Andreev <andevrom@gmail.com>
2022-06-03 16:40:38 +0300

    feat: decompress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -3,7 +3,7 @@ import {pipeline} from 'stream'
 import {join} from 'path';
 import {fileURLToPath} from 'url';
 import {promisify} from 'util';
-import {createGzip} from 'zlib'
+import {createGzip} from 'zlib';
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
@@ -11,7 +11,7 @@ const pipe = promisify(pipeline);
 
 export const compress = async () => {
     const source = createReadStream(join(__dirname, 'files', 'fileToCompress.txt'));
-    const dest = createWriteStream(join(__dirname, 'files', 'archive.txt.gz'));
+    const dest = createWriteStream(join(__dirname, 'files', 'archive.gz'));
     const gzip = createGzip();
 
     await pipe(source, gzip, dest);
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,18 @@
+import {createReadStream, createWriteStream} from 'fs';
+import {pipeline} from 'stream'
+import {join} from 'path';
+import {fileURLToPath} from 'url';
+import {promisify} from 'util';
+import {createUnzip} from 'zlib';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+const pipe = promisify(pipeline);
+
 export const decompress = async () => {
-    // Write your code here 
+    const source = createReadStream(join(__dirname, 'files', 'archive.gz'));
+    const dest = createWriteStream(join(__dirname, 'files', 'fileToCompress.txt'));
+    const gzip = createUnzip();
+
+    await pipe(source, gzip, dest);
 };
