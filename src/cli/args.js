c8be3e44112b30e3302c2b2ca3a7d01146da65ae
«Alexander-Zhibul» <alexanderzhibul@mail.ru>
2022-06-03 14:16:46 +0300

    init(nodejs-basics): add function in the args.js and env.js, finish block cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,15 @@
+import process from "process";
+
 export const parseArgs = () => {
-    // Write your code here 
+  // Write your code here
+
+  const argument = [];
+
+  for (let i = 2; i < process.argv.length; i += 2) {
+    argument.push(`${process.argv[i]} is ${process.argv[i + 1]}`);
+  }
+
+  console.log(argument.join(", "));
 };
+
+parseArgs();
