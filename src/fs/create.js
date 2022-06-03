0e9ca8c5eeb26872a1018c9ab712cef4f8f80efd
syrymumbetov <umbetov.syrym@gmail.com>
2022-06-03 23:49:33 +0600

    finished 1st task create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,8 @@
 export const create = async () => {
-    // Write your code here 
+  let fs = require('fs');
+
+  fs.writeFile('./files/fresh.txt', 'I am fresh and young', function (err) {
+    if (err) throw err;
+    console.log('FS operation failed');
+  });
 };
