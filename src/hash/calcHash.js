3322ae8398b86f46bc17bc9dd2f34b9529225f34
«Alexander-Zhibul» <alexanderzhibul@mail.ru>
2022-06-03 16:05:37 +0300

    init(nodejs-basics): add function in the calcHash.js, finish block hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,19 @@
+import { fileURLToPath } from "url";
+import path from "path";
+import crypto from "crypto";
+import fs from "fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const calculateHash = async () => {
-    // Write your code here 
+  // Write your code here
+
+  const filePath = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+  const fileBuffer = await fs.promises.readFile(filePath);
+  const hash = crypto.createHash("sha256").update(fileBuffer).digest("hex");
+
+  console.log(hash);
 };
+
+calculateHash();
