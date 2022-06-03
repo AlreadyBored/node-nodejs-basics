8c1cdf1a4811d2ff5d9b85121acd46bd43983545
Yauheni Udavihin <e.udavihin@itransition.com>
2022-06-03 13:15:13 +0300

    Implement worker threads functions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,21 @@
+import {fileURLToPath} from 'node:url';
+import path from 'node:path';
+import os from 'node:os';
+import {Worker} from 'node:worker_threads';
+
 export const performCalculations = async () => {
-    // Write your code here
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const workerPath = path.join(__dirname, 'worker.js');
+
+    const CPUcount = os.cpus().length;
+
+    const workerPromises = [...new Array(CPUcount)].map((value, index) => new Promise((resolve, reject) => {
+        const worker = new Worker(workerPath, {workerData: 10 + index + 1});
+        worker.on('message', resolve);
+        worker.on('error', reject);
+    }));
+
+    return Promise.all(workerPromises);
 };
+
+console.log(await performCalculations());
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,22 @@
+import {parentPort, workerData} from 'node:worker_threads';
+
 // n should be received from main thread
 export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 export const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    // This function sends result of nthFibonacci computations to main thread?
+    try {
+        const value = nthFibonacci(workerData);
+        parentPort.postMessage({
+            status: 'resolved',
+            data: value,
+        });
+    } catch (error) {
+        parentPort.postMessage({
+            status: 'error',
+            data: null,
+        });
+    }
 };
+
+sendResult();
