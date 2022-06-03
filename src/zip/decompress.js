a176c536988cf456da0ea5b035e597f626e3e17f
Artsiom Filkevich <artsiom_filkevich@senla.eu>
2022-06-03 17:18:42 +0300

    feat: implement decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,27 @@
+import { getFileDirName } from '../utils/utils.js'
+import { join } from 'path'
+import { createReadStream, createWriteStream } from 'fs'
+import { promisify } from 'util'
+import { pipeline } from 'stream'
+import { createUnzip } from 'zlib'
+
 export const decompress = async () => {
-    // Write your code here 
-};
+    try {
+        const { __dirname } = await getFileDirName(import.meta.url)
+        const pipe = promisify(pipeline)
+    
+        const sourcePath = join(__dirname, 'files', 'archive.gz')
+        const destinationPath = join(__dirname, 'files', 'fileToCompress.txt')
+    
+        const source = createReadStream(sourcePath)
+        const destination = createWriteStream(destinationPath)
+        const unzip = createUnzip()
+    
+        await pipe(source, unzip, destination)
+    }
+    catch(err) {
+        console.error(err)
+    }
+}
+
+decompress()
