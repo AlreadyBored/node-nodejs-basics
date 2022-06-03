1ed3729bdbfa27195fc9f9663f480fca6a1ba2d0
Yauheni Udavihin <e.udavihin@itransition.com>
2022-06-03 16:40:47 +0300

    Implement child process function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,16 @@
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+import {spawn} from 'node:child_process';
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const cpScript = 'script.js';
+    const cpSubFolder = 'files';
+    const cpAbsolutePath = path.join(__dirname, cpSubFolder, cpScript);
+
+    spawn(process.execPath, [cpAbsolutePath, ...args], {
+        stdio: 'inherit'
+    });
 };
+
+spawnChildProcess(process.argv);
