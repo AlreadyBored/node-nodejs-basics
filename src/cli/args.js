bc6968fb9a3ad05877c5136e3afba754124b03e8
sodapng <sodapng@ya.ru>
2022-06-03 15:10:36 +0400

    fix: output format

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,21 +1,19 @@
 import { argv } from 'node:process'
 
 export const parseArgs = () => {
-  // argv
-  //   .slice(2)
-  //   .reduce((acc, key, i, arr, value = arr[i + 1]) => {
-  //     return !(i % 2) ? acc.push(`${key} is ${value}`) && acc : acc
-  //   }, [])
-  //   .forEach((item) => {
-  //     console.log(item)
-  //   })
+  return argv
+    .slice(2)
+    .reduce((acc, key, i, arr, value = arr[i + 1]) => {
+      return !(i % 2) ? acc.push(`${key} is ${value}`) && acc : acc
+    }, [])
+    .join(', ')
 
   // A better solution :D
-  const argValues = argv.slice(2)
+  // const argValues = argv.slice(2)
 
-  for (let i = 0; i < argValues.length; i += 2) {
-    console.log(`${argValues[i]} is ${argValues[i + 1]}`)
-  }
+  // for (let i = 0; i < argValues.length; i += 2) {
+  //   console.log(`${argValues[i]} is ${argValues[i + 1]}`)
+  // }
 }
 
-parseArgs()
+console.log(parseArgs())
