50afe45e35f96029db91b806445dc2db5eece101
sodapng <sodapng@ya.ru>
2022-06-03 15:21:18 +0400

    refactor: "reduce" instead of "for"; fix: output format

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,11 +1,17 @@
 import { env } from 'process'
 
 export const parseEnv = () => {
-  for (const key in env) {
-    if (key.startsWith('RSS_')) {
-      console.log(`${key}=${env[key]};`)
-    }
-  }
+  // for (const key in env) {
+  //   if (key.startsWith('RSS_')) {
+  //     console.log(`${key}=${env[key]};`)
+  //   }
+  // }
+
+  return Object.entries(env)
+    .reduce((acc, [key, value]) => {
+      return key.startsWith('RSS_') ? acc.push(`${key}=${value}`) && acc : acc
+    }, [])
+    .join('; ')
 }
 
-parseEnv()
+console.log(parseEnv())
