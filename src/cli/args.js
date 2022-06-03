3cc6da78b5ca043b128bb187846ad35df1459983
raimovasad <81373435+asadbek2021@users.noreply.github.com>
2022-06-03 07:46:34 +0500

    feat(args,env): add cli functionalities

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,10 @@
 export const parseArgs = () => {
-    // Write your code here 
+  const list = process.argv.slice( 2, process.argv.length );
+  list.forEach( (item,index) => {
+      let next = process.argv[index+3];
+    if(item.includes('--')){
+        console.log(`${item.replace('--','')} is ${next}`);
+    }
+  });
 };
+parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,14 @@
+process.env.RSS_School='Uzbekistan';
+
 export const parseEnv = () => {
-    // Write your code here 
+    const list  = Object.keys(process.env);
+    const result = list.map( variable => {
+        if(variable.includes('RSS_')){
+            return {[variable]: process.env[variable]};
+        }
+    }).filter(item=> item!=null);
+    result.forEach( item => console.log(`${Object.keys(item)[0]}=${Object.values(item)[0]};`))
+
 };
+
+parseEnv()
