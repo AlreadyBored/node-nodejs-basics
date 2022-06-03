120fc44de01dd53e3d06d21f916f412f05f8336f
Yuliya Khvan <yuliya.khv@gmail.com>
2022-06-03 23:31:43 +0600

    feat: implement copying of files folder

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,39 @@
+import fs from "fs/promises";
+
 export const copy = async () => {
-    // Write your code here 
+  const filesFolder = "./src/fs/files";
+  const filesCopyFolder = "./src/fs/files_copy";
+  let files = [];
+  try {
+    files = await fs.readdir(filesFolder);
+    if (files) {
+      try {
+        const filesCopy = await fs.readdir(filesCopyFolder);
+        if (filesCopy) {
+            throw new Error("FS operation failed");
+        }
+      } catch (error) {
+        if (error.code === "ENOENT") {
+          await fs.mkdir(filesCopyFolder);
+          for (const file of files) {
+            await fs.copyFile(
+              `${filesFolder}/${file}`,
+              `${filesCopyFolder}/${file}`
+            );
+          }
+          console.log('Files copied to new folder');
+        } else {
+            console.log(error);
+        }
+      }
+    }
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    } else {
+        console.log(error);
+    }
+  }
 };
+
+copy();
