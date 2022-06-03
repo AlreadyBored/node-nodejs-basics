0b4a798f3161c48a133af36f742af5a4e652a122
Maria-Latysheva <mlatysheva@gmail.com>
2022-06-03 21:07:31 +0200

    feat: add rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,33 @@
+import * as fs from 'fs/promises';
+import path from 'path';
+
+// rename.js - implement function that renames file wrongFilename.txt to properFilename 
+// with extension .md (if there's no file wrongFilename.txt or properFilename.md already exists 
+// Error with message FS operation failed must be thrown)
+
+const __dirname = path.resolve(path.dirname(''));
+
+export const folderExists = async (path) => {
+  try {
+    await fs.access(path);
+    return true;
+  } catch (err) {
+    return false;
+  }  
+}
+
 export const rename = async () => {
-    // Write your code here 
+  const fileToRename = 'wrongFilename.txt';
+  const renamedFile = 'properFilename.md';
+  const fileToRenamePath = path.join(__dirname, `files/${fileToRename}`);
+  const renamedFilePath = path.join(__dirname, `files/${renamedFile}`);
+  const fileExists = await folderExists(renamedFilePath);
+  console.log(fileExists);
+  if (!fileExists) {
+    fs.rename(fileToRenamePath, renamedFilePath);
+  } else {
+    throw new Error ('FS operation failed');
+  }
 };
+
+rename();
