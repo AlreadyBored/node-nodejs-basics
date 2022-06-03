6924e077062cea3016f804f76dd69ab2150e5e87
Artsiom Filkevich <artsiom_filkevich@senla.eu>
2022-06-03 13:51:54 +0300

    feat: implement write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,18 @@
+import { getFileDirName } from '../utils/utils.js'
+import { createWriteStream } from 'fs'
+import { join } from 'path'
+
 export const write = async () => {
-    // Write your code here 
-};
+    try {
+        const { __dirname } = await getFileDirName(import.meta.url)
+        const fileToWrite = join(__dirname, 'files', 'fileToWrite.txt')
+        const stream = createWriteStream(fileToWrite)
+
+        process.stdin.pipe(stream)
+    }
+    catch(err) {
+        console.error(err)
+    }
+}
+
+write()
