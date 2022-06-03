4cb4340ed807d401915f97c109a960ae152a85e1
trun_0ff <freeze2991@gmail.com>
2022-06-04 02:37:48 +0300

    env and args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,16 @@
 export const parseArgs = () => {
-    // Write your code here 
+    let args = process.argv.slice(2),
+        responseArr = [];
+
+    args.map((item, index) => {
+        if(item.startsWith('--')){
+            let nextElement = args[index+1];
+            let value = typeof nextElement === 'undefined' || nextElement.startsWith('--') ? 'empty' : args[index+1];
+            responseArr.push(item.substring(2) + ' value is ' + value);
+        }
+    });
+
+    console.log(responseArr.join(', '));
 };
+
+parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,10 @@
+const PREFIX = 'RSS_';
+
 export const parseEnv = () => {
-    // Write your code here 
+    Object.entries(process.env).map(item => {
+        let [key, value] = item;
+        if(key.startsWith(PREFIX)) console.log(value);
+    });
 };
+
+parseEnv();
