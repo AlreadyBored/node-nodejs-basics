9f30a993e9f9e9088710e249a89a8bb6bde56bb1
Roman Andreev <andevrom@gmail.com>
2022-06-03 16:30:44 +0300

    feat: compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,18 @@
+import {createReadStream, createWriteStream} from 'fs';
+import {pipeline} from 'stream'
+import {join} from 'path';
+import {fileURLToPath} from 'url';
+import {promisify} from 'util';
+import {createGzip} from 'zlib'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+const pipe = promisify(pipeline);
+
 export const compress = async () => {
-    // Write your code here 
+    const source = createReadStream(join(__dirname, 'files', 'fileToCompress.txt'));
+    const dest = createWriteStream(join(__dirname, 'files', 'archive.txt.gz'));
+    const gzip = createGzip();
+
+    await pipe(source, gzip, dest);
 };
