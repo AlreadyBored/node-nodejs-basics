d057f597582fa7d1e8083e756161f5079d1e1c30
ezzikone <47362010+ezzikone@users.noreply.github.com>
2022-06-03 18:13:01 +0400

    feat: worker threads updated

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,34 @@
+import { Worker } from 'worker_threads'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+import os from 'os'
+
 export const performCalculations = async () => {
-    // Write your code here
-};
+    const path = dirname(fileURLToPath(import.meta.url)) + '/worker.js'
+    const cpuCount = os.cpus().length
+    const workersCount = new Array(cpuCount)
+    let workersInst = []
+    for (let i = 0; i < workersCount.length; i++) {
+        workersInst.push(new Worker(path, { workerData: i + 10 }))
+    }
+
+    const workersPromise = workersInst.map((work) => {
+        return new Promise((res) => {
+            work.on('message', (e) => {
+                res({
+                    status: 'resolved',
+                    data: e,
+                })
+            })
+            work.on('error', (e) => {
+                res({
+                    status: 'error',
+                })
+            })
+        })
+    })
+
+    const res = await Promise.all(workersPromise)
+
+    return res
+}
