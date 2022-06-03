88f02e4f1873ddd4b84857dcea00cc12107466ad
mrfz <zlobin.f@gmail.com>
2022-06-03 21:24:34 +0300

    docs: write comments to create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -3,13 +3,13 @@ const fileFullPath = "./src/fs/files/fresh.txt";
 
 export const create = async () => {
     try {
+        //trying to open file< if we can - throwing exception that file existgit 
         await open(fileFullPath,"r");
-
         console.log("FS operation failed, file ", fileFullPath, " exists");
         throw new Error ("FS operation failed");
         
     } catch (err) {
-        // throw new Error("FS operation error")
+        // catching that file doesn't exist and creting new file
         if (err.code === "ENOENT") {
             let newFile = await open(fileFullPath,'w');
             await newFile.write("I am fresh and young");
