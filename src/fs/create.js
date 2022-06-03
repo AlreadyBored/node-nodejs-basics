0a372d3beb4ed79d2b5e0309a30b359eff367c1a
Maria-Latysheva <mlatysheva@gmail.com>
2022-06-03 15:43:31 +0200

    feat: add create file function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,15 @@
+import { writeFile } from "fs/promises";
+import path from 'path';
+
+const __dirname = path.resolve(path.dirname(''));
+
 export const create = async () => {
-    // Write your code here 
+  const resolvedPath = path.resolve(__dirname, 'files/fresh.txt');
+  try {
+    await writeFile(resolvedPath, 'I am fresh and yound', { flag: 'wx' });
+  } catch (err) {
+    console.log('FS operation failed');
+  }
 };
+
+create();
