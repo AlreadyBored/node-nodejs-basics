230b7c9be5fe69438ba81d4cbafaf18f1b92a375
rubiaqute <rubiaqute@gmail.com>
2022-06-04 00:44:14 +0300

    feat: implement child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,15 @@
+import * as childProcess from "child_process";
+import path from "path";
+import * as url from "url";
+import { stdout, stdin } from "process";
+import { pipeline } from "stream";
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  const _dirname = path.dirname(url.fileURLToPath(import.meta.url));
+  const filePath = path.resolve(_dirname, "files", "script.js");
+  const child = childProcess.fork(filePath, args, {
+    stdio: ["pipe", "pipe", "pipe", "ipc"],
+  });
+  pipeline(stdin, child.stdin, child.stdout, stdout, () => {});
 };
+spawnChildProcess([78]);
