92f7ebb5580b87cfec0c0a4c1936398c533cd885
Vitali Savelyev <vsavelyev@elinext.com>
2022-06-03 09:58:36 +0300

    copy create rename

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,35 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+
 export const create = async () => {
-    // Write your code here 
+  fs.readFile(
+      path.join(__dirname, 'files', 'fresh.txt'),
+      (err, data) => {
+        try {
+          if (err) {
+            fs.writeFile(
+                path.join(__dirname, 'files', 'fresh.txt'),
+                'I am fresh and young',
+                (err) => {
+                  if (err) {
+                    throw err;
+                  }
+                  console.log('File created');
+                }
+            )
+          } else {
+            throw new Error('FS operation failed');
+          }
+        } catch (err) {
+          process.stderr.write(err.message);
+          process.exit(1);
+        }
+      }
+  )
 };
+create();
+
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,44 @@
-export const rename = async () => {
-    // Write your code here 
+import * as fs from 'fs';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+
+export const rename = async (file, rename) => {
+  fs.readFile(
+      file,
+      (err, data) => {
+        try {
+          if (err) {
+            throw new Error(`FS operation failed. File not found`);
+          } else {
+            fs.readFile(
+                rename,
+                (err, data) => {
+                  try {
+                    if (err) {
+                      fs.rename(file, rename, (err) => {
+                        if(err){
+                          throw err;
+                        }
+                        console.log('File renamed')
+                      })
+                    } else {
+                      throw new Error(`FS operation failed. File exists`);
+                    }
+                  }  catch (err) {
+                    process.stderr.write(err.message);
+                    process.exit(1);
+                  }
+                }
+            )
+          }
+        } catch (err) {
+          process.stderr.write(err.message);
+          process.exit(1);
+        }
+      }
+  )
 };
+rename(path.join(__dirname, 'files', 'wrongFilename.txt'), path.join(__dirname, 'files', 'properFilename.md'))
