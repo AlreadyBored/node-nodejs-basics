65adfc9287524a99fa2ecfd621796cb6434cc52b
Aljaetc <daasvsvami@gmail.com>
2022-06-03 19:10:09 +0300

    hot fix

--- src/fs/create.js
+++ src/fs/create.js
@@ -2,12 +2,10 @@ export const create = async () => {
   // Write your code here
   try {
     const fs = await import("fs/promises");
-    let file = await fs.writeFile("./files/file.txt", "I am fresh and young", {
+    await fs.writeFile("./files/file.txt", "I am fresh and young", {
       flag: "wx",
     });
   } catch (e) {
     throw new Error("FS operation failed");
   }
 };
-
-create();
