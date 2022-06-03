d148a44930842ca30d364067b471347256bd8188
ezzikone <47362010+ezzikone@users.noreply.github.com>
2022-06-03 18:13:25 +0400

    feat: child processes updated

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,8 @@
+import child_process from 'child_process'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+    const path = dirname(fileURLToPath(import.meta.url)) + '\\files\\script.js'
+    const child = child_process.fork(path, [...args])
+}
