c793f4d824cb6f83705ce44abd76dcd8709a52bc
«Alexander-Zhibul» <alexanderzhibul@mail.ru>
2022-06-03 09:48:13 +0300

    init(nodejs-basics): add function in the read.js and finish block fs

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,22 @@
+import fs from "fs";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(__dirname, "files", "fileToRead.txt");
+
 export const read = async () => {
-    // Write your code here 
+  // Write your code here
+
+  fs.readFile(filePath, (error, data) => {
+    if (error) {
+      throw new Error("FS operation failed");
+    } else {
+      console.log(String(data));
+    }
+  });
 };
+
+read();
