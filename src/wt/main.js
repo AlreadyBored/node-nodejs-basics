fdea60494a52c092c86848ee57f009e92a420c3a
rubiaqute <rubiaqute@gmail.com>
2022-06-03 23:38:46 +0300

    feat: implement worker and main threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,26 @@
+import { Worker } from "worker_threads";
+import path from "path";
+import * as url from "url";
+import * as os from "os";
+
 export const performCalculations = async () => {
-    // Write your code here
+  const _dirname = path.dirname(url.fileURLToPath(import.meta.url));
+  const filePath = path.resolve(_dirname, "worker.js");
+  const numberOfThreads = os.cpus().length;
+  const workers = [];
+  for (let i = 0; i < numberOfThreads; i++) {
+    const worker = new Worker(filePath, { workerData: 10 + i });
+    workers.push(
+      new Promise((resolve) => {
+        worker.on("message", (msg) =>
+          resolve({ status: "resolved", data: msg })
+        );
+        worker.on("error", () => resolve({ status: "error", data: null }));
+      })
+    );
+  }
+  const info = await Promise.allSettled(workers);
+
+  return info.map((el) => el.value);
 };
+console.log(await performCalculations());
