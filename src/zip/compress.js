4b6c7c46379b130683b388ae234c347b184badf3
Artsiom Filkevich <artsiom_filkevich@senla.eu>
2022-06-03 17:18:26 +0300

    feat: implement compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,28 @@
+import { getFileDirName } from '../utils/utils.js'
+import { join } from 'path'
+import { createReadStream, createWriteStream } from 'fs'
+import { promisify } from 'util'
+import { pipeline } from 'stream'
+import { createGzip } from 'zlib'
+
 export const compress = async () => {
-    // Write your code here 
-};
+    try {
+        const { __dirname } = await getFileDirName(import.meta.url)
+        const pipe = promisify(pipeline)
+    
+        const sourcePath = join(__dirname, 'files', 'fileToCompress.txt')
+        const destinationPath = join(__dirname, 'files', 'archive.gz')
+    
+        const source = createReadStream(sourcePath, 'utf-8')
+        const destination = createWriteStream(destinationPath)
+        const gzip = createGzip()
+    
+        await pipe(source, gzip, destination)
+    }
+    catch(err) {
+        console.error(err)
+    }
+
+}
+
+compress()
