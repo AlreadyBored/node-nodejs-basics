adbf9b8a52399b70ca2e007f1541649e1feb34fc
mrfz <zlobin.f@gmail.com>
2022-06-03 20:43:54 +0300

    feat: coplete create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,21 @@
+import { access, open, writeFile} from "node:fs/promises";
+const fileFullPath = "./src/fs/files/fresh.txt";
+
 export const create = async () => {
-    // Write your code here 
+    try {
+        await open(fileFullPath,"r");
+
+        console.log("FS operation failed, file ", fileFullPath, " exists");
+        throw new Error ("FS operation failed");
+        
+    } catch (err) {
+        // throw new Error("FS operation error")
+        if (err.code === "ENOENT") {
+            let newFile = await open(fileFullPath,'w');
+            await newFile.write("I am fresh and young");
+            newFile.close();
+        }
+    }
 };
+
+create();
