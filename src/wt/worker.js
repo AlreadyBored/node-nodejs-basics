08f9623579fa587ecc46d713acb0b57f28b67751
Siarhei Khonski <armonius@mail.ru>
2022-06-03 13:48:26 +0300

    feat: create the function using worker_threads

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,14 @@
-// n should be received from main thread
+import { parentPort, workerData } from "worker_threads";
+import { Buffer } from "buffer";
+
 export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 export const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  //throw new Error("Something gets wrong...");
+  parentPort.postMessage(nthFibonacci(workerData.num));
 };
+
+sendResult();
+
+
+
