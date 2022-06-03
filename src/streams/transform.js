6803ef3dd5aacbb372c3615598dfed022a61945c
Artsiom Filkevich <artsiom_filkevich@senla.eu>
2022-06-03 13:51:38 +0300

    feat: implement transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,18 @@
+import { Transform } from 'stream'
+
 export const transform = async () => {
-    // Write your code here 
-};
+    try {
+        const reverse = new Transform({
+            transform: (chunk, encoding, callback) => {
+                callback(null, chunk.toString().split('').reverse().join(''))
+            }
+        })
+
+        process.stdin.pipe(reverse).pipe(process.stdout)
+    }
+    catch(err) {
+        console.error(err)
+    }
+}
+
+transform()
