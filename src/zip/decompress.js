8ab303ce75b7926491bf0fc0be98a0e5197e85e7
Litvinov Iurii <litvinov.yuriy1996@gmail.com>
2022-06-03 15:29:50 +0400

    feat: [zip] Decompress function implemented

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,25 @@
+import { createUnzip } from 'zlib';
+import { pipeline } from 'stream';
+import { createReadStream, createWriteStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { folderName } from '../constants.js';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const pathToArchiveFile = path.join(__dirname, folderName, 'archive.gz');
+const pathToDecompressedFile = path.join(__dirname, folderName, 'fileToCompress.txt');
+
 export const decompress = async () => {
-    // Write your code here 
+    const unzip = createUnzip();
+    const source = createReadStream(pathToArchiveFile);
+    const decompressedFile = createWriteStream(pathToDecompressedFile);
+
+    pipeline(
+        source, 
+        unzip, 
+        decompressedFile, 
+        err => err ? console.error(err) : null
+    );
 };
+
+decompress();
