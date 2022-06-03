88d4e75f74a0d5ab243d966137aaadb9d327022f
Artsiom Filkevich <artsiom_filkevich@senla.eu>
2022-06-03 13:51:13 +0300

    feat: implement read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,18 @@
+import { getFileDirName } from '../utils/utils.js'
+import { createReadStream } from 'fs'
+import { join } from 'path'
+
 export const read = async () => {
-    // Write your code here 
-};
+    try {
+        const { __dirname } = await getFileDirName(import.meta.url)
+        const fileToRead = join(__dirname, 'files', 'fileToRead.txt')
+        const stream = createReadStream(fileToRead, 'utf-8')
+
+        stream.pipe(process.stdout)
+    }
+    catch(err) {
+        console.error(err)
+    }
+}
+
+read()
