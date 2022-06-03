e88e0fa4543642c09af4c7c6945bea623fe8637d
bababuuui <bubinatar@gmail.com>
2022-06-03 15:34:08 +0200

    feat: implement worker js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,50 @@
+import { Worker, workerData, parentPort, isMainThread } from "worker_threads";
+import * as os from "os";
+import {fileURLToPath} from "url";
+import {dirname} from "path";
+import path from "path";
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+
 export const performCalculations = async () => {
-    // Write your code here
+    console.log(" I am main");
+    const cpuCoresCount = os.cpus().length;
+    const promises = [];
+    console.log(`CPU cores count : ${cpuCoresCount}`);
+    for (let i =1; i<=cpuCoresCount; i++){
+        const promise = new Promise((resolve, reject) => {
+            try{
+                const worker = new Worker(path.join(__dirname,  'worker.js'), {workerData: 10+i}).on("message", message=>{
+                    console.log( " received message from worker: " + message);
+                    resolve(message);
+                })
+            }
+            catch (e) {
+                reject(e);
+            }
+        })
+        promises.push(promise);
+    }
+
+    const data=  await Promise.allSettled(promises);
+
+    // transforming data to required format
+    const result = [];
+    for (const item of data) {
+        let status="";
+        let data = null;
+        if (item.status == "fulfilled") {
+            status = "resolved";
+            data = item.value;
+        }
+        else {
+            status = "error";
+        }
+        result.push({status,data})
+
+    }
+   return result;
 };
+
+console.log(await performCalculations());
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,16 +1,10 @@
-//worker.js - extend given function to work with data received from main thread
-// and implement function which sends result of the computation to the main thread
-
 import { workerData, parentPort, isMainThread } from "worker_threads";
-
-
 // n should be received from main thread
 export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 export const sendResult = () => {
     console.log(workerData);
     parentPort.postMessage(nthFibonacci(workerData))
-    // This function sends result of nthFibonacci computations to main thread
 };
 
 sendResult();
