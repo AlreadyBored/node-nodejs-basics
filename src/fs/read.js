dfb292adb6c404e13ddc03e085c41d4d6f4e3d71
Ayan <a.nurinvest@gmail.com>
2022-06-04 09:29:38 +0600

    task#1_fs_readandlisttask

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,12 @@
+import fsp from 'fs/promises';
 export const read = async () => {
-    // Write your code here 
+    try {
+        const data = await fsp.readFile('files/fileToRead.txst', 'utf-8')
+        console.log(data);
+    }
+    catch(e) {
+        console.log(e);
+        throw new Error('FS operation failed');
+    }
 };
+read()
