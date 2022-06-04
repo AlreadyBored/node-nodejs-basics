511c027a5a3970cfa64839db5b85fceceeea81d4
Oleksii Korovushkin <alexkrv07@gmail.com>
2022-06-04 12:15:07 +0300

    feat: implement streams transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,14 @@
+import { Transform } from 'stream';
+
 export const transform = async () => {
-    // Write your code here 
+    const transformStream = new Transform();
+
+    transformStream._transform = (chunk, encoding, callback) => {
+
+        const reverseChunk = chunk.toString().trim().split('').reverse().join('');
+        transformStream.push(reverseChunk);
+    }
+
+    process.stdin.pipe(transformStream).pipe(process.stdout);
 };
+transform();
