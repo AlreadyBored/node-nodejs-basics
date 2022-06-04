a8c9e4f96c439e85e299fd1ddc0a7020b185eb4d
Olena Honcharuk <gasmin07@gmail.com>
2022-06-04 16:05:57 +0300

    feat: add solution to create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,16 @@
+import fs from 'fs/promises';
+
+
 export const create = async () => {
     // Write your code here 
+    try {
+        const content = "I am fresh and young";
+        await fs.writeFile('files/fresh.txt', content, 'utf8', { flag: 'wx' });
+    } catch (error) {
+        console.log('FS operation failed');
+        throw new Error('FS operation failed');
+    }
+
 };
+
+create();
