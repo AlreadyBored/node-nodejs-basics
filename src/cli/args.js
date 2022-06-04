5ca0ad4d1857a10dc4d27cce7d37330efeb81eb7
Vadzim Shut <vadzim_shut@epam.com>
2022-06-05 02:15:01 +0300

    feat: implement argv function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,14 @@
+import { argv } from 'process'
+
 export const parseArgs = () => {
-    // Write your code here 
-};
+  const obj = argv.slice(2).reduce((acc, value, idx, arr) => {
+    if (idx % 2 === 0) acc[value] = arr[idx + 1]
+    return acc
+  }, {})
+
+  Object.entries(obj).forEach(([key, value]) =>
+    console.log(`${key} is ${value},`),
+  )
+}
+
+parseArgs()
