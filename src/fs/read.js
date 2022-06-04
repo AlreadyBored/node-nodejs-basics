c75d9a9b5a69280a96c27b9379c68a27b35a0abb
Aljaetc <daasvsvami@gmail.com>
2022-06-04 17:08:07 +0300

    fs_read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,12 @@
 export const read = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    const fs = await import("fs/promises");
+    const con = await fs.readFile("files/fileToRead.txt", "utf8");
+    console.log(con);
+  } catch (e) {
+    throw new Error("FS operation failed");
+  }
 };
+
+read();
