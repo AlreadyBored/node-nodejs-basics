ea59c573216a7f8020ebadb46b6c97e481300547
Aljaetc <daasvsvami@gmail.com>
2022-06-04 17:18:40 +0300

    fs-rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,5 @@
 export const rename = async () => {
-    // Write your code here 
+  // Write your code here
+  const fs = await import("fs/promises");
+  await fs.rename("files/wrongFilename.txt", "files/proppeFilename.js");
 };
