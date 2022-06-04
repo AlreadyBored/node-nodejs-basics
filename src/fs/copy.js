6b0ff054cf50dff258e19d79fb2663a5d2a48bf8
Olena Honcharuk <gasmin07@gmail.com>
2022-06-05 00:51:21 +0300

    fix: throw error on copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -10,18 +10,24 @@ export const copy = async () => {
     let source = path.resolve(__dirname, 'files');
     let destination = path.resolve(__dirname, 'files_copy');
     let files = await fs.readdir(source);
+
+    try {
+        await fs.mkdir(destination, { recursive: false });
+
+    } catch (err) {
+        if (err) {
+            console.log(err);
+            throw new Error("FS operation failed");
+        } else {
+            console.log("folder created");
+        }
+    }
+
     for (let file of files) {
         let filePath = path.resolve(source, file);
         let fileStat = await fs.stat(filePath);
 
-        await fs.mkdir(destination, { recursive: true }, function (err) {
-            if (err && err.code === "EEXIST") {
-                console.log("Directory already exists");
-                throw new Error("FS operation failed");
-            } else {
-                console.log("folder created");
-            }
-        });
+
 
         if (fileStat.isFile()) {
             await copyFile(filePath, path.resolve(destination, file));
@@ -34,51 +40,6 @@ export const copy = async () => {
         }
     }
 }
-
-
-
-
-// fs.mkdir(destination, { recursive: true }, function (err) {
-//     if (err && err.code === "EEXIST") {
-//         console.log("Directory already exists");
-//         throw new Error('FS operation failed');
-//     } else {
-//         console.log("folder created");
-//     }
-// });
-
-// await fs.readdir(source, { withFileTypes: true }, (err, files) => {
-//     if (err) throw err;
-
-//     files.forEach((file) => {
-//         if (file.isFile()) {
-//             const filePath = path.resolve(__dirname, source, file.name);
-//             const filePathNew = path.resolve(__dirname, destination, file.name);
-
-
-//             copyFile(filePath, filePathNew, (err) => {
-//                 if (err) throw err;
-//                 console.log(`${file.name} was copied to destination`);
-//             });
-
-//         }
-//     });
-// });
-
-
-
-// fs.access(destination, constants.R_OK | constants.W_OK, (err) => {
-//     if (err) {
-//         copyFolder();
-//     } else {
-//         fs.rm(destination, { recursive: true }, (err) => {
-//             if (err) {
-//                 throw err;
-//             }
-//             copyFolder();
-//         });
-//     }
-// });
 
 
 copy();
