28a963cef1bb8c9674b4055531e332334703b157
effgenij <effgenij@gmail.com>
2022-06-05 00:16:33 +0300

    solution for write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,19 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
 export const write = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, "files/fileToWrite.txt");
+
+  try {
+    console.log("Начинайте вводить текст. Для выхода используйте 'Ctrl+C'");
+    const writeStream = fs.createWriteStream(filePath);
+    process.stdin.pipe(writeStream);
+  } catch (err) {
+    console.error(err);
+  }
 };
+
+write();
