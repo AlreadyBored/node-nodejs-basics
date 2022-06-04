ed95517a758fd073ee888ff09250dd8ef038c5f8
Boris Krasko <boris.krasko@live.com>
2022-06-04 23:19:47 +0200

    feat: done delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,16 @@
-export const remove = async () => {
-    // Write your code here 
+import fs from "fs";
+
+const remove = async () => {
+  fs.stat('src/fs/files/fileToRemove.txt', function (err, stats) {
+    if (err) {
+      throw err('FS operation failed!');
+    } else {
+      fs.unlink('src/fs/files/fileToRemove.txt', function (err) {
+        if (err) throw err;
+        console.log('Done!');
+      });
+    }
+  });
 };
+
+remove();
