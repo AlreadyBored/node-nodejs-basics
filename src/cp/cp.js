75e6a0b430d6435c633aa81b40137e47de7669df
Artiom Filkevich <artiom.filkevich@gmail.com>
2022-06-05 01:56:36 +0300

    feat: implement cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,12 @@
+import { getFileDirName } from '../utils/utils.js'
+import { join } from 'path'
+import { fork } from 'child_process'
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+    const { __dirname } = await getFileDirName(import.meta.url)
+    const scriptPath = join(__dirname, 'files', 'script.js')
+    fork(scriptPath, args)
+    process.stdin.on('data', (data) => process.stdout.write(`Recieved from child process: ${data.toString()}`));
+}
+
+spawnChildProcess(process.argv)
