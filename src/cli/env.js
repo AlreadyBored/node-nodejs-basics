f3c263dd2164b8af1903fdf2b5e8320b07b37892
Maria-Latysheva <mlatysheva@gmail.com>
2022-06-04 16:12:59 +0200

    feat: add env variables

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,15 @@
+import { env } from 'node:process';
+
 export const parseEnv = () => {
-    // Write your code here 
+    const arrayOfVariables = [];
+    for (let variable in env) {
+      if (variable.startsWith('RSS_')) {
+        arrayOfVariables.push(`${variable}=${env[variable]}`);
+      }
+    }
+    const result = arrayOfVariables.join('; ').toString();
+    console.log(result);
+    return result;
 };
+
+parseEnv();
