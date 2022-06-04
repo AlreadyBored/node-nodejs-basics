dd713255b07e1deeddc33efbd7e0d54dceb8fbbb
Boris Krasko <boris.krasko@live.com>
2022-06-04 22:58:50 +0200

    feat: done rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,22 @@
-export const rename = async () => {
-    // Write your code here 
+import fs from "fs";
+
+const rename = async () => {
+  fs.stat('src/fs/files/properFilename.md', function (err, stats) {
+    if (err) {
+      fs.stat('src/fs/files/wrongFilename.txt', function (err, stats) {
+        if (err) {
+          throw err('FS operation failed!');
+        } else {
+          fs.rename('src/fs/files/wrongFilename.txt', 'src/fs/files/properFilename.md', function (err) {
+            if (err) throw err;
+            console.log('Done!');
+          });
+        }
+      });
+    } else {
+      throw err('FS operation failed!');
+    }
+  })
 };
+
+rename();
