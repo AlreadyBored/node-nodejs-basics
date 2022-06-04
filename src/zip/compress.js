0a7bda12a9e6667f2f22957cc4ebeae48518c594
MINSK\Nikita_Pytalev <nikita_pytalev@epam.com>
2022-06-05 01:27:32 +0300

    implement compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,19 @@
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import zlib from 'zlib';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const fileToCompressPath = path.join(__dirname, 'files', 'fileToCompress.txt');
+const archivePath = path.join(__dirname, 'files', 'archive.gz');
+
 export const compress = async () => {
-    // Write your code here 
+    var zip = zlib.createGzip();
+    
+    var read = fs.createReadStream(fileToCompressPath);
+    var write = fs.createWriteStream(archivePath);
+
+    read.pipe(zip).pipe(write);	
 };
