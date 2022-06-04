185acefa04825d21af8c1b7ec97be89da3f42648
Olena Honcharuk <gasmin07@gmail.com>
2022-06-05 00:08:32 +0300

    feat: add solution to delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,18 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { __dirname } from './create.js';
+
+
 export const remove = async () => {
     // Write your code here 
+    try {
+        await fs.rm(path.join(__dirname, "files/fileToRemove.txt"), { force: false });
+        console.log('Файл удален');
+    } catch (err) {
+        console.log(err);
+        console.log('FS operation failed');
+        throw new Error('FS operation failed');
+    }
+
 };
+remove();
