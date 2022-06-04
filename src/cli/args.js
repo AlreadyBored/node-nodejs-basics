2f1c4b64c9e807051afa89b76660da7e40e238b5
Egorius1979 <Egoriyvelikolepny1979@gmail.com>
2022-06-04 19:03:36 +0200

    fix: change substring search

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,6 +1,6 @@
 export const parseArgs = () => {
   const resultVarObj = process.argv.slice(2).reduce((acc, it, idx, arr) => {
-    if (it.includes('--')) {
+    if (it.startsWith('--')) {
       const key = it.slice(2);
       console.log(`${key} is ${arr[idx + 1]}`);
       return { ...acc, [key]: arr[idx + 1] };
@@ -10,4 +10,4 @@ export const parseArgs = () => {
   // console.log(resultVarObj);
 };
 
-// parseArgs();
+parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,10 +1,10 @@
 export const parseEnv = () => {
   const rssVarArray = Object.keys(process.env).filter((it) =>
-    it.includes('RSS_')
+    it.startsWith('RSS_')
   );
   rssVarArray.forEach((it) => {
     console.log(`${it}=${process.env[it]}`);
   });
 };
 
-// parseEnv();
+parseEnv();
