3a65afaac02a92e30b86dd802c3e1248f31d33e4
karpinskaya <kivvvimail@gmail.com>
2022-06-04 10:45:45 +0300

    feat: implement write function logic

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,11 @@
+import fs from 'fs';
+
 export const write = async () => {
-    // Write your code here 
+    const path = './files/fileToWrite.txt';
+    let stream = new fs.WriteStream(path);
+
+    process.stdin.on('data', data => {
+        stream.write(data);
+        process.exit();
+    });
 };
