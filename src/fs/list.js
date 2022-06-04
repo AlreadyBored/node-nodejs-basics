3998d4586b91724fd6c90d4ee82204f13de7f1f7
mrfz <zlobin.f@gmail.com>
2022-06-04 20:25:38 +0300

    feat: add list files function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,10 @@
+import {dirname} from "node:path";
+import { fileURLToPath } from "node:url";
+import { readdir } from "node:fs/promises";
+
+const folderPath = dirname(fileURLToPath(import.meta.url)) + "/files/";
+
 export const list = async () => {
-    // Write your code here 
+  let filesArray = await readdir(folderPath, {withFileTypes: false});
+  console.log(filesArray);
 };
