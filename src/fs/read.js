ca8841e94a310f11280ef20ae0967cdb9a30ddd3
Evgeny Vasilko <shadezp@gmail.com>
2022-06-04 15:09:29 +0300

    feat: added read task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,21 @@
+import { readFile } from 'fs/promises';
+
 export const read = async () => {
     // Write your code here 
+
+    const PATH = './src/fs/files/fileToRead.txt';
+	const ERROR_MSG = 'FS operation failed';
+
+    try {
+        try {
+            const data = await readFile(PATH, {encoding: 'utf-8'});
+            console.log(data)
+        } catch {
+            throw new Error(ERROR_MSG);
+        }
+    } catch(err) {
+        console.log(err);
+    }
 };
+    
+read();
