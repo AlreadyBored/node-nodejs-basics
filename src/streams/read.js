497880f848bdc235ee5bd1c7c3f99719724838e5
Oleksii Korovushkin <alexkrv07@gmail.com>
2022-06-04 11:36:11 +0300

    feat: implement streams read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,24 @@
+import * as fs from 'fs';
+import  * as path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+export const __filename = fileURLToPath(import.meta.url);
+export const __dirname = dirname(__filename);
+
 export const read = async () => {
-    // Write your code here 
+    const fileName = 'fileToRead.txt';
+    const pathToReadFile = path.join(__dirname, '/files', fileName);
+
+    fs.access(pathToReadFile, fs.constants.F_OK, (err) => {
+        if (err) {
+            console.log(`file "${pathToReadFile}" does not exist`);
+            throw new Error('FS operation failed');
+        }
+    });
+    const readableStream = fs.createReadStream(pathToReadFile, 'utf-8');
+    readableStream.on('data', chunk => process.stdout.write(chunk));
+    // Write your code here
 };
+
+read();
