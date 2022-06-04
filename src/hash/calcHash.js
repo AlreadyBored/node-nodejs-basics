5727d52913be923eccedef77d6e85739de7f45da
Evgeny Vasilko <shadezp@gmail.com>
2022-06-04 18:01:40 +0300

    feat: add calcHash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,15 @@
+import { readFile } from 'fs/promises'
+import { createHash } from 'crypto'
+
 export const calculateHash = async () => {
     // Write your code here 
+    const PATH = './src/hash/files/fileToCalculateHashFor.txt'
+
+    const file = await readFile(PATH);
+    const hash = createHash('sha256').update(file).digest('hex');
+
+    console.log(hash)
+    return hash;
 };
+
+calculateHash();
