53f97c33ddc14f90f8134b86358c2e56d093fe44
Vitali Savelyev <vsavelyev@elinext.com>
2022-06-04 09:19:09 +0300

    compress decompress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,37 @@
-export const compress = async () => {
-    // Write your code here 
+import { createGzip } from 'zlib'
+import { pipeline } from 'stream';
+import * as path from 'path';
+import * as fs from 'fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const compress = async (path, pathZlib) => {
+  return new Promise((resolve, reject) => {
+    const gzip = createGzip()
+    const readable = fs.createReadStream(path);
+    readable.on("error", (err) => {
+      reject(err)
+    });
+    const writable = fs.createWriteStream(pathZlib)
+    pipeline(
+        readable,
+        gzip,
+        writable,
+        (err) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve('File compress')
+          }
+        })
+  })
 };
+
+compress(path.join(__dirname, 'files', 'fileToCompress.txt'), path.join(__dirname, 'files', 'archive.gz'))
+    .then(res => console.log(res))
+    .catch(err => {
+      process.stderr.write('FS operation failed. File not found');
+      process.exit(1);
+    })
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,37 @@
-export const decompress = async () => {
-    // Write your code here 
+import { createUnzip } from 'zlib'
+import { pipeline } from 'stream';
+import * as path from 'path';
+import * as fs from 'fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const decompress = async (pathGzip, path) => {
+  return new Promise((resolve, reject) => {
+    const unzip = createUnzip();
+    const readable = fs.createReadStream(pathGzip);
+    readable.on("error", (err) => {
+      reject(err)
+    });
+    const writable = fs.createWriteStream(path)
+    pipeline(
+        readable,
+        unzip,
+        writable,
+        (err) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve('File decompress')
+          }
+        })
+  })
 };
+
+decompress(path.join(__dirname, 'files', 'archive.gz'), path.join(__dirname, 'files', 'fileToCompress.txt'))
+    .then(res => console.log(res))
+    .catch(err => {
+      process.stderr.write(err.message);
+      process.exit(1);
+    })
