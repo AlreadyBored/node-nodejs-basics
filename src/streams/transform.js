961869dff759aee914365cee59fe5eae405f52a7
effgenij <effgenij@gmail.com>
2022-06-05 01:26:18 +0300

    solution for transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,23 @@
+import { Transform } from "stream";
+
 export const transform = async () => {
-    // Write your code here 
+  try {
+    const transformer = new Transform({
+      transform(chunk, _encoding, _callback) {
+        const reversedChunk = chunk
+          .toString()
+          .split("")
+          .reverse()
+          .slice(1)
+          .join("");
+        console.log("Развернутый ввод:", reversedChunk);
+      },
+    });
+    console.log('Введите любой текст. Когда захотите выйти нажмите "Ctrl+C"');
+    process.stdin.pipe(transformer).pipe(process.stdout);
+  } catch (err) {
+    console.error(err);
+  }
 };
+
+transform();
