4a6cc31aa5fd796c0b8e7594e3d0d1787ea44c51
Oleksii Korovushkin <alexkrv07@gmail.com>
2022-06-04 11:45:07 +0300

    feat: implement streams write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,18 @@
+import * as fs from 'fs';
+import  * as path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+export const __filename = fileURLToPath(import.meta.url);
+export const __dirname = dirname(__filename);
+
 export const write = async () => {
-    // Write your code here 
+    const fileName = 'fileToWrite.txt';
+    const pathToWriteFile = path.join(__dirname, '/files', fileName);
+
+    const writableStream = fs.createWriteStream(pathToWriteFile, 'utf-8');
+    process.stdin.on('data', chunk => writableStream.write(chunk));
+    // Write your code here
 };
+
+write();
