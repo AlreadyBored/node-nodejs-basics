c61139f42f230fa01b83a025e49fed5f721847f7
Olena Honcharuk <gasmin07@gmail.com>
2022-06-04 16:50:14 +0300

    fix: bug with path to file in create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,11 +1,15 @@
 import fs from 'fs/promises';
-
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 export const create = async () => {
     // Write your code here 
     try {
         const content = "I am fresh and young";
-        await fs.writeFile('files/fresh.txt', content, 'utf8', { flag: 'wx' });
+        const __dirname = dirname(fileURLToPath(import.meta.url));
+        const filePath = path.join(__dirname, "files/fresh.txt");
+        await fs.writeFile(filePath, content, 'utf8', { flag: 'wx' });
     } catch (error) {
         console.log('FS operation failed');
         throw new Error('FS operation failed');
