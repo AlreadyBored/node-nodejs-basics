b01bb0e0632e6fce52a2d97acde3c933711788d3
MINSK\Nikita_Pytalev <nikita_pytalev@epam.com>
2022-06-05 00:53:21 +0300

    implement list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,17 @@
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filesPath = path.join(__dirname, 'files');
+
 export const list = async () => {
-    // Write your code here 
+    fs.readdir(filesPath, (err, files) => {
+        if (err){
+            throw Error('FS operation failed');
+        }
+
+        console.log(files);
+    });
 };
