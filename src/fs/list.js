9682c4cb5cbc94cff7e59e3f011adb3d8d37fff2
Vadzim Shut <vadzim_shut@epam.com>
2022-06-05 00:41:43 +0300

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,20 @@
+import { readdir } from 'fs/promises'
+import { FileError, checkPathForExistence } from './utils.js'
+import { FS_PATH } from './constants.js'
+
 export const list = async () => {
-    // Write your code here 
-};
+  const { verdict, error } = await checkPathForExistence(FS_PATH)
+  if (!verdict) {
+    throw new FileError(error)
+  }
+
+  try {
+    const fileList = await readdir(FS_PATH)
+    fileList.forEach((fileName) => console.log(fileName))
+  } catch (err) {
+    throw new FileError('Error with reading direcory.')
+  }
+}
+
+
+list()
