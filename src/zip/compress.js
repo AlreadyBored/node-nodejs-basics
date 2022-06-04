9d98401d6c1cfb6411dd97bc076c02b4613c7f88
Anastasia Polivoda <polivoda.a@zovplita.by>
2022-06-04 14:58:15 +0300

    fix: zip/compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,17 +1,16 @@
 import fs from 'fs';
 import path from 'path';
 import zlib from 'zlib';
-import { pipeline } from 'stream';
 
 const toRead = path.resolve(path.dirname(''), 'src', 'zip', 'files', 'fileToCompress.txt');
 const toWrite = path.resolve(path.dirname(''), 'src', 'zip', 'files', 'archive.gz');
 
 export const compress = async () => {
-    const input = fs.ReadStream(toRead, 'utf8');
-    const output = fs.WriteStream(toWrite, 'utf8');
-    const gzip = zlib.Gzip();
+    const input = fs.createReadStream(toRead);
+    const output = fs.createWriteStream(toWrite);
+    const gzip = zlib.createGzip();
 
-    pipeline(input, gzip, output, ()=>{});
+    input.pipe(gzip).pipe(output);
 };
 
 compress();
