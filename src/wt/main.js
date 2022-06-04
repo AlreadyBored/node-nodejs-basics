bcc8607aed6e4b49bb4247da92fe6fd0b784ac6a
Peter Vereshagin <peter@vereshagin.org>
2022-06-04 13:49:31 +0000

    fix: :white_check_mark: main imports/consts

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,16 @@
 'use strict'
 
+import { Worker } from 'worker_threads';
+import os from 'os'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const cores = os.cpus().length
+
+const workerDatas = [...Array(cores)].map((core, i) => i + 10)
+const errorMained = { status: `error`, data: null }
+const fName = path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'worker.js')
+
 export const performCalculations = async () => {
   const workerPromises = workerDatas.map(workerData => {
     return new Promise((resolve, reject) => {
