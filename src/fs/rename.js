b6df8d29f52b1f15837b96e36b6bb0370af7b28e
mrfz <zlobin.f@gmail.com>
2022-06-04 21:04:16 +0300

    feat: add rename wrong file

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,17 @@
+import {copyFile, rm} from 'fs/promises';
+import { constants } from 'fs';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const fileToRenamePath = dirname(fileURLToPath(import.meta.url)) + '/files/wrongFilename.txt';
+const renamedFilePath = dirname(fileURLToPath(import.meta.url)) + '/files/properFilename.md';
+
 export const rename = async () => {
-    // Write your code here 
+    try {
+        await copyFile (fileToRenamePath, renamedFilePath, constants.COPYFILE_EXCL);
+        await rm(fileToRenamePath);
+    } catch (err) {
+        console.log("Got some error: " + err);
+        throw new Error("FS operation failed");
+    }
 };
