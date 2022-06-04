7247cb50b2393c08cfa9180d48ad39e04014c3ca
shiroe66 <shiroeqky@gmail.com>
2022-06-04 16:17:04 +0500

    feat: createHash on txt file

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,18 @@
+import { createHash } from "crypto"
+import { readFile } from "fs/promises"
+import { fileURLToPath } from "url"
+import path, { dirname } from "path"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 export const calculateHash = async () => {
-    // Write your code here 
-};
+  const file = path.join(__dirname, "files", "fileToCalculateHashFor.txt")
+
+  readFile(file, "utf-8").then((data) => {
+    const hash = createHash("sha256").update(data).digest("hex")
+    console.log(hash)
+  })
+}
+
+calculateHash()
