29d2d1857dbf117dad6adca9d374584ffbc0936c
«Alexander-Zhibul» <alexanderzhibul@mail.ru>
2022-06-04 20:28:51 +0300

    init(nodejs-basics): add function in the read.js, transform.js, write.js and finish block streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,27 @@
+import { fileURLToPath } from "url";
+import path from "path";
+import fs from "fs";
+import process from "process";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const read = async () => {
-    // Write your code here 
+  // Write your code here
+
+  const filePath = path.resolve(__dirname, "files", "fileToRead.txt");
+  const stream = fs.createReadStream(filePath, { encoding: "utf-8" });
+
+  stream
+    .on("data", (chunk) => {
+      process.stdout.write(chunk);
+    })
+    .on("error", (error) => {
+      process.stdout.write(error);
+    })
+    .on("end", () => {
+      stream.destroy();
+    });
 };
+
+read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,18 @@
+import { fileURLToPath } from "url";
+import path from "path";
+import fs from "fs";
+import process from "process";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const write = async () => {
-    // Write your code here 
+  // Write your code hereclear
+
+  const filePath = path.resolve(__dirname, "files", "fileToWrite.txt");
+  const stream = fs.createWriteStream(filePath, { encoding: "utf-8" });
+
+  process.stdin.pipe(stream);
 };
+
+write();
