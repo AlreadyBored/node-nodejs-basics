d6aa64cc2048f29f6549d845d31d1eb3db0d8d39
Evgeny Vasilko <shadezp@gmail.com>
2022-06-04 15:04:26 +0300

    feat: added list task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,21 @@
+import { readdir } from 'fs/promises';
+
 export const list = async () => {
     // Write your code here 
+
+    const PATH = './src/fs/files';
+	const ERROR_MSG = 'FS operation failed';
+    
+    try {
+        try {
+            const flieList = await readdir(PATH);
+            console.log(flieList);
+        } catch {
+            throw new Error(ERROR_MSG);
+        }
+    } catch(err) {
+        console.log(err);
+    };
 };
+
+list();
