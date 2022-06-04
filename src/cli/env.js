25df55947ba01fbef8e02ea67c763e6328ca8376
Vadzim Shut <vadzim_shut@epam.com>
2022-06-05 01:30:53 +0300

    feat: implement env function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,19 @@
+import { env } from 'process'
+
+const setEnvVariables = () => {
+  env.RSS_name1 = 'value1'
+  env.RSS_name2 = 'value2'
+}
+
 export const parseEnv = () => {
-    // Write your code here 
-};
+  setEnvVariables()
+
+  const regexp = /^RSS_/
+  Object.keys(env).forEach((key) => {
+    if (regexp.test(key)) {
+      console.log(`${key}=${env[key]}`)
+    }
+  })
+}
+
+parseEnv()
