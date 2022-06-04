cb28fd1b3963fa3b57f101326e99e4a2871d093e
Oleksii Korovushkin <alexkrv07@gmail.com>
2022-06-04 16:23:40 +0300

    feat: implement calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,23 @@
+import * as fs from 'fs';
+import  * as path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { createHash } from 'crypto';
+
+export const __filename = fileURLToPath(import.meta.url);
+export const __dirname = dirname(__filename);
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const fileName = 'fileToCalculateHashFor.txt';
+    const pathToFileName = path.join(__dirname, '/files', fileName);
+
+    const readableStream = fs.createReadStream(pathToFileName);
+    readableStream.on('data', (chunk) => {
+        const data = chunk.toString();
+        const hash = createHash('sha256').update(data).digest('hex');
+        // return hash;
+        console.log(hash);
+    });
 };
+
+calculateHash();
