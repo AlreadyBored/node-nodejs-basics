da20bf8801fdc9f3d9f4253504e5f31151173730
Boris Krasko <boris.krasko@live.com>
2022-06-04 23:50:23 +0200

    feat: done read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,19 @@
-export const read = async () => {
-    // Write your code here 
+import fs from "fs";
+
+const read = async () => {
+  fs.stat('src/fs/files/fileToRead.txt', function (err, stats) {
+    if (err) {
+      throw err('FS operation failed!');
+    } else {
+      fs.readFile('src/fs/files/fileToRead.txt', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log(data);
+      });
+    }
+  })
 };
+
+read();
