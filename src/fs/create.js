9ad7af3b2fd22d50bb4d4e23246ed6af9914d456
MINSK\Nikita_Pytalev <nikita_pytalev@epam.com>
2022-06-05 00:13:19 +0300

    implement create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,15 @@
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const freshFilePath = path.join(__dirname, 'files', 'fresh.txt');
+
 export const create = async () => {
-    // Write your code here 
+    fs.writeFile(freshFilePath, "I am fresh and young", { flag: 'wx' }, function(err) {
+        if(err) {
+            throw Error('FS operation failed')
+        }
+    }); 
 };
