dd85bf30e9965322a004e1a1aab3353caadbd44f
«Alexander-Zhibul» <alexanderzhibul@mail.ru>
2022-06-04 22:44:53 +0300

    init(nodejs-basics): add function in the compress.js, decompress.js and finish zip block

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,28 @@
+import { fileURLToPath } from "url";
+import path from "path";
+import { createGzip } from "zlib";
+import { pipeline } from "stream";
+import { createWriteStream, createReadStream } from "fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const compress = async () => {
-    // Write your code here 
+  // Write your code here
+
+  const filePath = path.join(__dirname, "files", "fileToCompress.txt");
+  const filePathGz = path.join(__dirname, "files", "archive.gz");
+
+  const gzip = createGzip();
+  const source = createReadStream(filePath);
+  const destination = createWriteStream(filePathGz);
+
+  pipeline(source, gzip, destination, (error) => {
+    if (error) {
+      console.error("An error occurred:", error);
+      process.exitCode = 1;
+    }
+  });
 };
+
+compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,28 @@
+import { fileURLToPath } from "url";
+import path from "path";
+import { createUnzip } from "zlib";
+import { pipeline } from "stream";
+import { createWriteStream, createReadStream } from "fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const decompress = async () => {
-    // Write your code here 
+  // Write your code here
+
+  const filePathGz = path.join(__dirname, "files", "archive.gz");
+  const filePath = path.join(__dirname, "files", "fileToCompress.txt");
+
+  const gzip = createUnzip();
+  const source = createReadStream(filePathGz);
+  const destination = createWriteStream(filePath);
+
+  pipeline(source, gzip, destination, (error) => {
+    if (error) {
+      console.error("An error occurred:", error);
+      process.exitCode = 1;
+    }
+  });
 };
+
+decompress();
