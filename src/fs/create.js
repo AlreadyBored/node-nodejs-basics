5b1b72962b2e4468b4fd9c3aa0090d1056dbb8f1
Boris Krasko <boris.krasko@live.com>
2022-06-04 22:29:24 +0200

    feat: done create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,16 @@
-export const create = async () => {
-    // Write your code here 
+import fs from "fs";
+
+const create = async () => {
+  fs.stat('src/fs/files/fresh.txt', function(err, stats) {
+    if (err) {
+      fs.appendFile('src/fs/files/fresh.txt', 'I am fresh and young', function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+      });
+    } else {
+      throw err('FS operation failed!');
+    }
+  });
 };
+
+create();
