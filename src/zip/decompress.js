201449ec15d4a34d270559f477889f47af6bc6a8
effgenij <effgenij@gmail.com>
2022-06-05 01:51:00 +0300

    solution for decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,23 @@
+import { createUnzip } from "zlib";
+import { createReadStream, createWriteStream } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+
 export const decompress = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const sourcefilePath = path.join(__dirname, "files/archive.gz");
+    const destinationfilePath = path.join(__dirname, "files/fileToCompress.txt");
+    const unzip = createUnzip();
+    const source = createReadStream(sourcefilePath);
+    const destination = createWriteStream(destinationfilePath);
+  
+    try {
+      source.pipe(unzip).pipe(destination);
+    } catch (err) {
+      console.error(err);
+    } 
 };
+
+decompress();
