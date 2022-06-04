4fabf33a8ac4d1fb5c8ccf229158cb31d4d8454a
Oleksii Korovushkin <alexkrv07@gmail.com>
2022-06-04 10:30:58 +0300

    feat: implement read.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,7 +1,34 @@
+import * as fs from 'fs';
+import  * as path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+export const __filename = fileURLToPath(import.meta.url);
+export const __dirname = dirname(__filename);
+
 export const copy = async () => {
+    const pathToSourceDir = path.join(__dirname, '/files');
+
+    fs.access(pathToSourceDir, fs.constants.F_OK, (err) => {
+        if (err) {
+            console.log(`The directory "${pathToSourceDir}" does not exists`);
+            throw new Error('FS operation failed');
+        }
+    });
+
+    const pathToDestDir = path.join(__dirname, '/files_copy');
+
+    fs.access(pathToDestDir, fs.constants.F_OK, (err) => {
+        if (!err) {
+            console.log(`The directory "${pathToDestDir}" already exists`);
+            throw new Error('FS operation failed');
+        }
+    });
 
 
 
 };
 
-// copy();
+
+
+copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -3,27 +3,25 @@ import  * as path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-export const checkIsFileExist = (fileName) => {
-    fs.access(fileName, fs.constants.F_OK, (err) => {
-        if (!err) {
-            console.log('file exists');
-            throw new Error('FS operation failed');
-        }
-    });
-}
+export const __filename = fileURLToPath(import.meta.url);
+export const __dirname = dirname(__filename);
 
 export const create = async () => {
     const fileName = 'fresh.txt';
     const content = 'I am fresh and young';
     const pathToNewFile = path.join(__dirname, '/files', fileName);
-    checkIsFileExist(pathToNewFile);
+
+    fs.access(pathToNewFile, fs.constants.F_OK, (err) => {
+        if (!err) {
+            console.log('file exists');
+            throw new Error('FS operation failed');
+        }
+    });
+
     fs.writeFile(pathToNewFile, content, (err) => {
         if (err) throw err;
         console.log(`The file: ${fileName} has been saved!`);
-      });
+    });
 };
 
 create();
