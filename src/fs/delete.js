3dcb44f0dd63faf35940beca89cd76e48f4b439f
Evgeny Vasilko <shadezp@gmail.com>
2022-06-04 14:54:26 +0300

    feat: added delete task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,21 @@
+import { rm } from 'fs/promises';
+
 export const remove = async () => {
     // Write your code here 
+
+    const PATH = './src/fs/files/fileToRemove.txt';
+	const ERROR_MSG = 'FS operation failed';
+
+    try {
+        try {
+            await rm(PATH);
+        } catch {
+            throw new Error(ERROR_MSG);
+        }
+    } catch(err) {
+        console.log(err);
+    }
+
 };
+
+remove();
