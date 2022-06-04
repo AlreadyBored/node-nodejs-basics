fb22d7248124f4a73cdb5b54a7c6b1ad67b23da7
MINSK\Nikita_Pytalev <nikita_pytalev@epam.com>
2022-06-04 23:24:08 +0300

    implement read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,11 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const read = async () => {
-    // Write your code here 
+    const stream = fs.createReadStream(`${__dirname}/files/fileToRead.txt`);
+    stream.pipe(process.stdout);
 };
