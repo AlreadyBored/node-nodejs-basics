9403ce4853f3559111b2c8be14efd197115c1f29
Evgeny Vasilko <shadezp@gmail.com>
2022-06-04 12:53:48 +0300

    feat: added fs:create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,30 @@
+import { access, appendFile } from 'fs/promises';
+ 
 export const create = async () => {
     // Write your code here 
+
+  const PATH = './src/fs/files/fresh.txt';
+	const DATA = 'I am fresh and young';
+	const ERROR_MSG = 'FS operation failed'
+
+	try {
+		let isExistFile = false;
+
+		try {
+  		await access(PATH);
+  		isExistFile = true;
+		} catch {
+  		isExistFile = false;
+		};
+
+		if (isExistFile) {
+			throw new Error(ERROR_MSG);
+		} else {
+			appendFile(PATH, DATA);
+		}
+	} catch (err) {
+		console.log(err);
+	}
 };
+
+create();
