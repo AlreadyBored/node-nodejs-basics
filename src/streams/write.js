f5a3ff1ffd0780a73e475a5c258245a4b335ac01
shiroe66 <shiroeqky@gmail.com>
2022-06-05 00:13:12 +0500

    feat: createWriteStream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,17 @@
+import { createWriteStream } from "fs"
+import { fileURLToPath } from "url"
+import path, { dirname } from "path"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 export const write = async () => {
-    // Write your code here 
-};
+  const file = path.join(__dirname, "files", "fileToWrite.txt")
+
+  const ws = createWriteStream(file)
+  process.stdin.pipe(ws)
+
+  process.stdin.resume()
+}
+
+write()
