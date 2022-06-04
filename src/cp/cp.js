d0061ee49770d4e0cccc88a4bb7374da08b52dea
sodapng <sodapng@ya.ru>
2022-06-04 14:42:47 +0400

    feat: cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,13 @@
+import { fork } from 'node:child_process'
+import { resolve } from 'node:path'
+import { argv, cwd, stdin, stdout } from 'node:process'
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+  const modulePath = resolve(cwd(), 'src/cp/files', 'script.js')
+  const child = fork(modulePath, args, { silent: true })
+
+  stdin.pipe(child.stdin)
+  child.stdout.pipe(stdout)
+}
+
+spawnChildProcess(argv.slice(2))
