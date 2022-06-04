29baf3794347e2e0289d017abe65372131247029
MINSK\Nikita_Pytalev <nikita_pytalev@epam.com>
2022-06-04 22:20:24 +0300

    implement args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,7 @@
 export const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+
+    for (let i = 0; i < args.length; i += 2) {
+        console.log(`${args[i].substring(2)} is ${args[i + 1]}`);
+    }
 };
