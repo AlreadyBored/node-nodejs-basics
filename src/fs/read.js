6541c8e20318552d9564313c07dbd94687648a79
MINSK\Nikita_Pytalev <nikita_pytalev@epam.com>
2022-06-05 00:56:42 +0300

    implement read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,17 @@
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const fileToReadPath = path.join(__dirname, 'files', 'fileToRead.txt');
+
 export const read = async () => {
-    // Write your code here 
+    fs.readFile(fileToReadPath, 'utf8', function(err, data) {
+        if (err) {
+            throw Error('FS operation failed');
+        }
+
+        console.log(data)
+      });
 };
