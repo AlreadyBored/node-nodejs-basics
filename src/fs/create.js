668a5b3a4e060d63993051744614ff7a59736f4a
Sergey Denisenko <sergey.denisenko@five9.com>
2022-06-05 00:00:34 +0300

    resolve: create.js, copy.js; without testing

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,16 @@
 export const create = async () => {
-    // Write your code here 
+    // Write your code here
+    const fs = require('fs');
+    const content = "I am fresh and young"
+    const path = "files/fresh.txt"
+
+    if (fs.existsSync(path)){
+        throw Error("FS operation failed");
+    }
+
+    fs.writeFile(path, content, err=>{
+        if(err){
+            console.error(err);
+        }
+    })
 };
