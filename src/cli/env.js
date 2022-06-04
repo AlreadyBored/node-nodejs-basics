fa401bc0e6fca33a150e360727e7de3d20c8baa4
Evgeny Vasilko <shadezp@gmail.com>
2022-06-04 16:46:26 +0300

    feat: add env task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,18 @@
+import { stdout } from 'process'
+ 
 export const parseEnv = () => {
     // Write your code here 
+    const PREFIX = 'RSS_'
+    const env  = process.env;
+    let str = '';
+
+    Object.keys(env).forEach((key) => {
+        if(key.startsWith(PREFIX)) {
+            str += `${key}=${env[key]}; `
+        }
+    })
+
+    stdout.write(str);
 };
+
+parseEnv();
