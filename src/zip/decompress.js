3528932653aa2141ad3c484eabd76feeff0e8469
MINSK\Nikita_Pytalev <nikita_pytalev@epam.com>
2022-06-05 01:27:42 +0300

    implement decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,19 @@
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import zlib from 'zlib';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const fileToCompressPath = path.join(__dirname, 'files', 'fileToCompress.txt');
+const archivePath = path.join(__dirname, 'files', 'archive.gz');
+
 export const decompress = async () => {
-    // Write your code here 
+    var unzip = zlib.createUnzip();
+
+    var read = fs.createReadStream(archivePath);
+    var write = fs.createWriteStream(fileToCompressPath);
+
+    read.pipe(unzip).pipe(write);	
 };
