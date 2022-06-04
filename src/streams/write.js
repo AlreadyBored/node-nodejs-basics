62468e9b7736595a9b6dc802c75ebae5f5d4f699
Anastasia Polivoda <polivoda.a@zovplita.by>
2022-06-04 13:15:03 +0300

    fix: streams/write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,24 +1,13 @@
 import fs from 'fs';
 import path from 'path';
-import * as readline from 'readline';
-import { stdin as input, stdout as output } from 'process';
+import { pipeline } from 'stream';
 
 const toWrite = path.resolve(path.dirname(''), 'src', 'streams', 'files', 'fileToWrite.txt');
 
 export const write = async () => {
     const stream = fs.WriteStream(toWrite, 'utf8');
-    const rl = readline.createInterface({ input, output });
-
-    rl.setPrompt('input> ');
-    rl.prompt();
-    rl.on('line', (line) => {
-        stream.write(line + '\n');
-        rl.prompt()
-    });
-
-    process.on('exit', (code) => {
-        console.log(`${code === 0 ? '\n' : ''}Great! See you later :)`);
-    });
+    const read = process.stdin;
+    pipeline(read, stream, ()=>{});
 };
 
 write();
