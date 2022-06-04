97c9042ee524ddae34178122e3149d78d28153c0
nukulele <zonough@gmail.com>
2022-06-04 14:14:38 +0400

    create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,21 @@
+import { access, constants, writeFile } from "fs";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const create = async () => {
-    // Write your code here 
+  const file = "fresh.txt";
+  const message = "I am fresh and young";
+  access(__dirname + "/files/" + file, constants.F_OK, (error) => {
+    if (error) {
+      writeFile(__dirname + "/files/" + file, message, "utf8", (err) => {
+        if (err) throw err;
+      });
+    } else {
+      throw new Error("FS operation failed");
+    }
+  });
 };
+
+create();
