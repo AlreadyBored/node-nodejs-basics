75ceaf5538aedc15264502b38da6e936f5675a52
shiroe66 <shiroeqky@gmail.com>
2022-06-05 00:32:36 +0500

    feat: transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,21 @@
+import { Transform, pipeline } from "stream"
+
 export const transform = async () => {
-    // Write your code here 
-};
+  const readable = process.stdin
+  const writable = process.stdout
+
+  const transform = new Transform({
+    transform(chunk, _, cb) {
+      const chunkStringify = chunk.toString().trim()
+      const reverse = chunkStringify.split("").reverse().join("")
+
+      cb(null, reverse + "\n")
+    },
+  })
+
+  pipeline(readable, transform, writable, (err) => {
+    console.error(err)
+  })
+}
+
+transform()
