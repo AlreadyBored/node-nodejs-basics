9c12e911ba8a0921af05c1c78da7bf0f110a353a
effgenij <effgenij@gmail.com>
2022-06-05 00:09:30 +0300

    solution for read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,18 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
 export const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, "files/fileToRead.txt");
+
+  try {
+    const readStream = fs.createReadStream(filePath);
+    readStream.pipe(process.stdout);
+  } catch (err) {
+    console.error(err);
+  }
 };
+
+read();
