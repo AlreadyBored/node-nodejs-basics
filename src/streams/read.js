3697c6e9a9372e7830c87be9f9083db8f8775642
shiroe66 <shiroeqky@gmail.com>
2022-06-05 00:13:25 +0500

    feat: createReadStream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,18 @@
+import { createReadStream } from "fs"
+import { fileURLToPath } from "url"
+import path, { dirname } from "path"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 export const read = async () => {
-    // Write your code here 
-};
+  const file = path.join(__dirname, "files", "fileToRead.txt")
+
+  const rs = createReadStream(file)
+  rs.on("data", (chunk) => {
+    process.stdout.setEncoding("utf-8")
+    process.stdout.write(chunk)
+  })
+}
+
+read()
