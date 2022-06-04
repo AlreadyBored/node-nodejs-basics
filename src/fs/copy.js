2ccc25967bc8801439db1ab046841ef9712b4c82
Evgeny Vasilko <shadezp@gmail.com>
2022-06-04 13:27:07 +0300

    feat: copy task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,38 @@
+import { access, copyFile, cp } from 'fs/promises';
+
 export const copy = async () => {
-    // Write your code here 
+  // Write your code here 
+
+  const PATH = './src/fs/files';
+  const COPY_PATH = './src/fs/files-copy';
+	const ERROR_MSG = 'FS operation failed';
+
+  try {
+		let isExistFolder;
+
+		try {
+  		await access(PATH);
+  		isExistFolder = true;
+		} catch {
+  		isExistFolder = false;
+		};
+
+		if (!isExistFolder) {
+			throw new Error(ERROR_MSG);
+		} 
+
+		try {
+			await cp(PATH, COPY_PATH, {
+				errorOnExist: true,
+				recursive: true,
+				force: false,
+			});
+		} catch {
+			throw new Error(ERROR_MSG);
+		}
+	} catch (err) {
+		console.log(err);
+	}
 };
+
+copy();
