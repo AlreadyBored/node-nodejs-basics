23b5368eda3d5303027738aee752e8b5379e5708
Anastasia Polivoda <polivoda.a@zovplita.by>
2022-06-04 11:03:14 +0300

    feat: done streams/write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,20 @@
+import fs from 'fs';
+import path from 'path';
+
+const toWrite = path.resolve(path.dirname(''), 'src', 'streams', 'files', 'fileToWrite.txt');
+
 export const write = async () => {
-    // Write your code here 
+    const stream = fs.WriteStream(toWrite, 'utf8');
+
+    process.stdin.on('readable', data => {
+        while ((data = process.stdin.read()) !== null) {
+            stream.write(data);
+        }
+    });
+
+    process.on('exit', (code) => {
+        console.log('Great! See you later :)');
+    });
 };
+
+write();
