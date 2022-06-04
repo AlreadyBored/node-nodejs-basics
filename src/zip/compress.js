73060d08e8471baeccba1bf538f6a1996a1b55ff
Anastasia Polivoda <polivoda.a@zovplita.by>
2022-06-04 13:28:56 +0300

    feat: done zip/compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,17 @@
+import fs from 'fs';
+import path from 'path';
+import zlib from 'zlib';
+import { pipeline } from 'stream';
+
+const toRead = path.resolve(path.dirname(''), 'src', 'zip', 'files', 'fileToCompress.txt');
+const toWrite = path.resolve(path.dirname(''), 'src', 'zip', 'files', 'archive.gz');
+
 export const compress = async () => {
-    // Write your code here 
+    const input = fs.ReadStream(toRead, 'utf8');
+    const output = fs.WriteStream(toWrite, 'utf8');
+    const gzip = zlib.Gzip();
+
+    pipeline(input, gzip, output, ()=>{});
 };
+
+compress();
