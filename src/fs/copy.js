1d9615eef516ba01818a9c524ebf2229e649417d
MINSK\Nikita_Pytalev <nikita_pytalev@epam.com>
2022-06-05 00:38:39 +0300

    implement copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,26 @@
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const src = path.join(__dirname, 'files');
+const dest = path.join(__dirname, 'files_copy');
+
 export const copy = async () => {
-    // Write your code here 
-};
+    fs.mkdir(dest, err => {
+        if (err){
+          throw Error('FS operation failed');
+        }
+    })
+
+    fs.readdir(src, (err, files) => {
+        files.forEach(file => {
+            fs.copyFile(path.join(src, file), path.join(dest, file), err => {
+                if (err){
+                    throw Error('FS operation failed');
+                  }
+            });
+        });
+    });
+}
