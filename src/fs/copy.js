a7cb82925ce247b11bc1406c3d074806b357204b
Maksim Pevnev <drummen11@gmail.com>
2022-06-04 09:11:11 +0400

    develop: add fs solutions

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,39 @@
+import { access, constants, readdir, mkdir, copyFile } from 'fs';
+
+const SRC_DIR_NAME = `files`;
+const DEST_DIR_NAME = `files_copy`;
+const FS_OPERATION_ERROR = 'FS operation failed';
+
 export const copy = async () => {
-    // Write your code here 
+    // this solution can be simplified if use "cp" function. But for nodejs v16 this function has experimental status.
+
+    access(SRC_DIR_NAME, constants.F_OK, (err) => {
+        if (err !== null) {
+            throw new Error(FS_OPERATION_ERROR);
+        }
+
+        access(DEST_DIR_NAME, constants.F_OK, (err) => {
+            if (err !== null) {
+                mkdir(DEST_DIR_NAME, (err) => {
+                    if (err) {
+                        throw err;
+                    }
+                });
+            }
+
+            readdir(SRC_DIR_NAME, (error, files) => {
+                files.forEach(file => {
+                    copyFile(`${SRC_DIR_NAME}/${file}`, `${DEST_DIR_NAME}/${file}`, (err) => {
+                        if (err) {
+                            throw err;
+                        }
+
+                        console.log(`${file} was copied successfully`);
+                    });
+                });
+            });
+        });
+    });
 };
+
+copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,23 @@
+import { access, constants, writeFile } from 'fs';
+
+const FILE_NAME = 'fresh.txt';
+const FILE_CONTENT = 'I am fresh and young';
+const FS_OPERATION_ERROR = 'FS operation failed';
+
 export const create = async () => {
-    // Write your code here 
+    access(FILE_NAME, constants.F_OK, (err) => {
+        if (err === null) {
+            throw new Error(FS_OPERATION_ERROR);
+        }
+
+        writeFile(FILE_NAME, FILE_CONTENT, (error) => {
+            if (error) {
+                console.error(error)
+            }
+
+            console.log(`${file} was created successfully`);
+        });
+    });
 };
+
+create();
