c270b619fbf71252a06f04a51ed69c79ba65294e
MINSK\Nikita_Pytalev <nikita_pytalev@epam.com>
2022-06-05 00:49:00 +0300

    implement delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,15 @@
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const fileToRemovePath = path.join(__dirname, 'files', 'fileToRemove.txt');
+
 export const remove = async () => {
-    // Write your code here 
+    fs.unlink(fileToRemovePath, err => {
+        if (err) {
+            throw Error('FS operation failed')
+        }
+    });
 };
