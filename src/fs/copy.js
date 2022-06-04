45d873be97496be6c2a47966f1fb325b7aa5dc0f
mrfz <zlobin.f@gmail.com>
2022-06-04 20:12:58 +0300

    feat: add copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,21 @@
+import {copyFile, opendir, mkdir, readdir} from 'node:fs/promises';
+import {dirname} from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const folderOriginPath = dirname(fileURLToPath(import.meta.url)) + "/files/"
+const folderDestPath = dirname(fileURLToPath(import.meta.url)) + "/files_copy/"
+
 export const copy = async () => {
     // Write your code here 
+    try {
+        let newFolder = await mkdir(folderDestPath);
+        await opendir (folderOriginPath);
+        let nestedFiles = await readdir(folderOriginPath, {withTypeFiles: true});
+        nestedFiles.forEach ((file) => {
+            copyFile(folderOriginPath + file, folderDestPath + file)
+            // console.log(file);
+        })
+    } catch (err) {
+        console.log ('something went wrong, error: \n', err);
+    }
 };
