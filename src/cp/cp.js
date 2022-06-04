a83ac7b5022005e63394d0289c0cce0bbf013d9f
Oleksii Korovushkin <alexkrv07@gmail.com>
2022-06-04 18:58:42 +0300

    feat: implement cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,18 @@
+import { spawn } from 'child_process';
+import  * as path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+export const __filename = fileURLToPath(import.meta.url);
+export const __dirname = dirname(__filename);
+
+const argsFromParent = process.argv.slice(2);
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    const fileNameChildProcess = 'script.js';
+    const pathToFileNameChildProcess = path.join(__dirname, '/files', fileNameChildProcess);
+    const child = spawn(process.execPath, [pathToFileNameChildProcess, ...args]);
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
 };
+
+spawnChildProcess(argsFromParent);
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,6 @@
+import { Worker } from 'worker_threads';
+const worker = new Worker('./worker.js');
+
 export const performCalculations = async () => {
     // Write your code here
 };
