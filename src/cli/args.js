ab44c204b456fb9c606b25a04c0e648915ed059e
Your Name <you@example.com>
2022-06-04 21:41:57 +0300

    'try'

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,15 @@
-export const parseArgs = () => {
-    // Write your code here 
+import * as readline from 'readline';
+import { stdin as input, stdout as output } from 'process';
+
+export const parseArgs =  () => {
+   const rl = readline.createInterface({ input, output });
+
+   rl.question(' ', (value) => {
+      rl.question(' ', (value2) => {
+         console.log(`propName is ${value}, prop2Name is ${value2}`);
+         rl.close();
+      });
+   });
 };
+// rl.close();
+parseArgs()
