dbda4edd0b71e35697adba31d0130fb5d0a637fe
MINSK\Nikita_Pytalev <nikita_pytalev@epam.com>
2022-06-05 22:02:14 +0300

    implement cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,15 @@
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { fork } from 'child_process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const scriptPath = path.join(__dirname, 'files', 'script.js');
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = fork(scriptPath, args, {silent: true});
+
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
 };
