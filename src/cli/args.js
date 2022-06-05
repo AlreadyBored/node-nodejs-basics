5e89e4528da357e0d176a9bff44e011cc279fac5
webdev <dezzzh93@gmail.com>
2022-06-05 17:52:21 +0800

    feat: completed the task of the subtask "Command line interface" (src/cli)

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,18 @@
 export const parseArgs = () => {
-    // Write your code here 
+    let result='';
+    process.argv.forEach((value,index)=>{
+        if(index > 1){
+            console.log(index)
+            if(index % 2 == 0){
+                result+=value.substring(2)+" is ";
+            }else {
+                result+=value+",";
+            }
+        }
+
+    });
+    console.log(result.slice(0,result.length-1));
 };
+parseArgs();
+
+
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,12 @@
-export const parseEnv = () => {
-    // Write your code here 
+ const parseEnv = () => {
+     const arr = process.env;
+     let result;
+     Object.keys(arr).forEach(function(key) {
+         if(key.indexOf('RSS_') + 1) {
+             result+=key+"="+this[key]+"; ";
+         }
+     }, arr);
+     console.log(result)
+
 };
+ export default parseEnv();
