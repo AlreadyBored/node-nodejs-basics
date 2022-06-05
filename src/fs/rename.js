d8d817fb1d99c00c401c99c5f06b63cb46514ade
Maria White <redstone.mari@gmail.com>
2022-06-05 11:43:22 +0300

    feat: add rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,28 @@
+import * as fs from 'fs';
+
 export const rename = async () => {
-    // Write your code here 
+    try {
+        fs.access('./src/fs/files/properFilename.md', (err) => {
+            if (!err) {
+                throw new Error('FS operation failed');
+            } else {
+                fs.access('./src/fs/files/wrongFilename.txt', (err) => {
+                    if (err) {
+                        throw new Error('FS operation failed');
+                    } else {
+                        fs.rename('./src/fs/files/wrongFilename.txt', './src/fs/files/properFilename.md', 
+                        (err) => {
+                            if (err) {
+                                throw err;
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    } catch (e) {
+        console.log(e);
+    }
 };
+
+await rename();
