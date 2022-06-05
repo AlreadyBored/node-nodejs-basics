da885bbc5b121b4ee78f07d1849fc3bad75b66de
MINSK\Nikita_Pytalev <nikita_pytalev@epam.com>
2022-06-05 23:55:09 +0300

    implement create.js with async promises

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,15 +1,15 @@
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import fs from 'fs';
+import fs from 'fs/promises';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const freshFilePath = path.join(__dirname, 'files', 'fresh.txt');
 
 export const create = async () => {
-    fs.writeFile(freshFilePath, "I am fresh and young", { flag: 'wx' }, function(err) {
-        if (err) {
-            throw Error('FS operation failed')
-        }
-    }); 
+    try {
+        await fs.writeFile(freshFilePath, "I am fresh and young", { flag: 'wx' })
+    } catch {
+        throw Error('FS operation failed')
+    }
 };
