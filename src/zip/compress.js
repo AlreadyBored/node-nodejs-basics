844161122d16602b88f48de029331972de2e860a
pavello91 <pavlentianno27@gmail.com>
2022-06-05 21:33:41 +0300

    third commit

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,20 @@
+import {createGzip} from 'zlib'
+import {pipeline } from 'stream'
+import {
+  createReadStream,
+  createWriteStream
+} from 'fs'
 export const compress = async () => {
-    // Write your code here 
+    const gzip = createGzip();
+    const source = createReadStream('files/fileToCompress.txt');
+    const destination = createWriteStream('files/archive.gz');
+
+    pipeline(source, gzip, destination, (err) => {
+        if (err) {
+          console.error('An error occurred:', err);
+          process.exitCode = 1;
+        }
+      });
 };
+
+compress()
