b6561ec83119064b46b68d5cc76d5addf42b9691
mrfz <zlobin.f@gmail.com>
2022-06-05 18:14:24 +0300

    refactor: move filter and map dot function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,7 @@
 export const parseEnv = () => {
     let outputString = ""
-    Object.entries(process.env).filter(([key,value]) => key.substring(0,4) === 'RSS_').map(([key,value]) => outputString = outputString.concat(`${key}=${value}; `));
+    Object.entries(process.env)
+        .filter(([key,value]) => key.substring(0,4) === 'RSS_')
+        .map(([key,value]) => outputString = outputString.concat(`${key}=${value}; `));
     outputString !== ""  ?  console.log(outputString.slice(0,outputString.length-2)) : null;
 };
