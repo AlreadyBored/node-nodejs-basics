0b7de6b7c8861471530b83328ee14ee9db821566
Roman Andreev <andevrom@gmail.com>
2022-06-05 12:54:31 +0300

    feat: args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,19 @@
 export const parseArgs = () => {
-    // Write your code here 
+    const argsWithValues = process.argv.reduce((acc, curr, idx) => {
+        if (curr.startsWith('--')) {
+            const arg = [curr.slice(2)];
+            const nextArg = process.argv[idx + 1];
+
+            if (!nextArg.startsWith('--')) {
+                arg.push(nextArg);
+            }
+
+            return [...acc, arg.join(' is ')];
+        }
+        return acc;
+    }, []);
+
+    console.log(argsWithValues.join(', '));
 };
+
+parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,13 +1,11 @@
-import {env} from 'process';
-
 export const parseEnv = () => {
 	const variables = [];
 
-    for (const variable in env) {
+    for (const variable in process.env) {
 		if (variable.startsWith('RSS_')) {
 			variables.push(`${variable}=${process.env[variable]}`);
 		}
     }
 
-	console.log(variables.join('; '));;
+	console.log(variables.join('; '));
 };
