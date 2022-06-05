a66cc18d498748516d9125418f90aec892d2c102
Anastasia Polivoda <polivoda.a@zovplita.by>
2022-06-05 16:50:47 +0300

    feat: done workwer treads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,24 @@
+import { Worker } from 'worker_threads';
+import * as os from 'os';
+import path, { resolve } from 'path';
+
 export const performCalculations = async () => {
-    // Write your code here
+    let answer = [];
+    os.cpus().forEach((v, i) => {
+        let worker = new Worker(path.resolve(path.dirname(''), 'src', 'wt', 'worker.js'), {workerData: 10 + i});
+        answer.push(
+            new Promise(resolve => {
+                worker.on('message', obj => {
+                    resolve(answer[i] = obj);
+                });
+                worker.on('error', () => {
+                    resolve(answer[i] = {status: 'error', data: null});
+                });
+            })
+        ) 
+    })
+    
+    console.log(await Promise.all(answer));
+
 };
+performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,11 @@
+import { workerData, parentPort } from 'worker_threads';
+
+const answer = {}
+
 // n should be received from main thread
 export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 export const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    parentPort.postMessage({status: 'resolved', data: nthFibonacci(workerData)});
 };
+sendResult();
