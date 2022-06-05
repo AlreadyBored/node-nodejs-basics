c8b81a6af0bbf58121940e26a935efba949882c2
Muhammed Abdrahman <muhammed03amin.com>
2022-06-06 01:23:40 +0600

    feat: add solution for env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,11 @@
 export const parseEnv = () => {
-    // Write your code here 
+  let arr = [];
+  for (const [key, value] of Object.entries(process.env)) {
+    if (key.includes('RSS_')) {
+      arr.push(`${key}=${value}`);
+    };
+  }
+  console.log(arr.join('; '));
 };
+
+parseEnv();
