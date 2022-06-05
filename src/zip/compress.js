22e340d0aacffa61f45b658e51702deb978f917c
DavydVeremchuk <davyd.v@code-care.pro>
2022-06-05 21:41:18 +0300

    feat: add zip solutions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,20 @@
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import { createGzip } from "node:zlib";
+import { pipeline } from "node:stream/promises";
+import { createReadStream, createWriteStream } from "node:fs";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const compress = async () => {
-    // Write your code here 
+  // Write your code here
+  const gzip = createGzip();
+  const source = createReadStream(
+    join(__dirname, "files", "fileToCompress.txt")
+  );
+  const destination = createWriteStream(join(__dirname, "files", "archive.gz"));
+
+  await pipeline(source, gzip, destination);
 };
+
+await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,20 @@
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import { createUnzip } from "node:zlib";
+import { pipeline } from "node:stream/promises";
+import { createReadStream, createWriteStream } from "node:fs";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const decompress = async () => {
-    // Write your code here 
+  // Write your code here
+  const unzip = createUnzip();
+  const source = createReadStream(join(__dirname, "files", "archive.gz"));
+  const destination = createWriteStream(
+    join(__dirname, "files", "fileToCompress.txt")
+  );
+
+  await pipeline(source, unzip, destination);
 };
+
+await decompress();
