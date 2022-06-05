8345af0b4ff405f08b1d2629e1b4a536cef0f4b8
AntonFartovii <anton@rza.by>
2022-06-05 18:06:56 +0300

    feat: update rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,25 @@
+const path = require('path')
+const fs = require('fs')
+
 export const rename = async () => {
-    // Write your code here 
+    // Write your code here
+    // sorry на скорую руку код
+    fs.access(
+        path.join(__dirname, 'files', 'properFilename.md'),
+        function(error){
+            if (error) {
+                fs.rename(
+                    path.join(__dirname, 'files', 'wrongFilename.txt'),
+                    path.join(__dirname, 'files', 'properFilename.md'),
+                    (err) => {
+                        if (err) {
+                            console.log ( 'FS operation failed must be thrown' )
+                        }
+                    }
+                )
+            }
+            else {
+                console.log('FS operation failed must be thrown');
+            }
+        });
 };
