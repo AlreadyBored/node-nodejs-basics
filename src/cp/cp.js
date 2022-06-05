8f22ba5065f31733c07cd38e1ec71483962dde3c
aleksbuk <booker.ua@gmail.com>
2022-06-05 09:11:39 +0300

    feat: funcion for child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,8 @@
+import { fork } from 'child_process';
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = fork('./files/script.js', args);
+    return child
 };
+
+spawnChildProcess(['1', '2', '3']);
