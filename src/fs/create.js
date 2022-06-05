fe67588604dc832e9e34961546a46c491367a363
IrinaAfinogenova <52318667+IrinaAfinogenova@users.noreply.github.com>
2022-06-05 22:25:59 +0300

    create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,18 @@
+import {writeFile} from 'fs/promises';
+import {checkFileExists} from './utils.js';
+
+const CONTENT = 'I am fresh and young';
+
 export const create = async () => {
-    // Write your code here 
+    const isAlreadyExist = await checkFileExists('src/fs/files/fresh.txt');
+
+    if (isAlreadyExist) {
+        throw new Error('FS operation failed');
+    }
+
+    await writeFile('src/fs/files/fresh.txt', CONTENT);
 };
+
+create()
+    .then(() => console.log('file was created'))
+    .catch((err) => console.log('Please, delete fresh.txt'));
