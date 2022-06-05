f2a18d5333e0f34d48a796b61d6b695a7e534b88
shiroe66 <shiroeqky@gmail.com>
2022-06-05 22:47:30 +0500

    fix: slice on log

--- src/cli/args.js
+++ src/cli/args.js
@@ -2,6 +2,12 @@ export const parseArgs = () => {
   const args = process.argv.slice(2)
 
   for (let i = 0; i < args.length; i += 2) {
-    console.log(`${args[i].slice(2)} is ${args[i + 1]}`)
+    if (args[i].startsWith("--")) {
+      console.log(`${args[i].slice(2)} is ${args[i + 1]}`)
+    } else {
+      console.log(`${args[i]} is ${args[i + 1]}`)
+    }
   }
 }
+
+parseArgs()
