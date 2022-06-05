5d5caf5454ea42d74c8c84d0bb544a702c8eb052
DavydVeremchuk <davyd.v@code-care.pro>
2022-06-05 12:42:08 +0300

    feat: add hash solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,17 @@
+import { createHash } from "node:crypto";
+import { createReadStream } from "node:fs";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash("sha256");
+  const file = join(__dirname, "files", "fileToCalculateHashFor.txt");
+  const input = createReadStream(file);
+
+  input.pipe(hash).setEncoding("hex").pipe(process.stdout);
 };
+
+await calculateHash();
