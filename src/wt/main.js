1fba0b3982a7495cd6f13b88f5bed405ee260fe6
shiroe66 <shiroeqky@gmail.com>
2022-06-05 20:21:14 +0500

    fix: worker_threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -2,13 +2,21 @@ import { Worker } from "worker_threads"
 import { cpus } from "os"
 
 export const performCalculations = async () => {
-  return new Promise((resolve, reject) => {
-    const worker = new Worker("./src/wt/worker.js", {
-      workerData: cpus().length,
-    })
+  const result = []
 
-    worker.on("message", (msg) => console.log(msg))
-  })
+  for (let i = 0; i < cpus().length; i++) {
+    result.push(
+      new Promise((resolve, reject) => {
+        const worker = new Worker("./src/wt/worker", {
+          workerData: i + 10,
+        })
+        worker.on("message", (data) => resolve({ status: "resolved", data }))
+        worker.on("error", () => resolve({ status: "error", data: null }))
+      })
+    )
+  }
+
+  Promise.all(result).then((value) => console.log(value))
 }
 
 performCalculations()
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -4,20 +4,7 @@ export const nthFibonacci = (n) =>
   n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2)
 
 export const sendResult = () => {
-  const result = []
-
-  for (let i = 0; i < workerData; i++) {
-    const data = nthFibonacci(i + 10)
-    const status = "resolved"
-
-    result.push({ status, data })
-  }
-
-  if (result.length) {
-    parentPort.postMessage(result)
-  } else {
-    parentPort.postMessage({ status: "error", data: null })
-  }
+  parentPort.postMessage(nthFibonacci(workerData))
 }
 
 sendResult()
