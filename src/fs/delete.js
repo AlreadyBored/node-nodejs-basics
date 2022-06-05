341cf55b3f105f2b3dbf33d06a34e795a8d43a29
mrfz <zlobin.f@gmail.com>
2022-06-05 07:05:22 +0300

    feat: add delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,18 @@
+import { rm } from "fs/promises"
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const fileToDeletePath = dirname(fileURLToPath(import.meta.url)) + '/files/fileToRemove.txt';
+
 export const remove = async () => {
-    // Write your code here 
+    try {
+        await rm(fileToDeletePath);
+    } catch (err) {
+        if (err.code === "ENOENT") {
+         throw new Error('FS operation failed');
+        }
+        else {
+            console.log('Some error ocured, error: ' + err)
+        }
+    }
 };
