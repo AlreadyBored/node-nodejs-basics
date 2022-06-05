21d9ae8c6142749e6777d22b463429cef6518b14
Vladimir <98771978+VladIn4e@users.noreply.github.com>
2022-06-06 00:09:57 +0400

    Update create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,20 @@
 export const create = async () => {
-    // Write your code here 
+    //Импорт
+    const fs = require("fs");
+    const path = require("path");
+    
+    const dir = path.resolve(__dirname); //Директория
+    fs.access(((dir+"/files/fresh.txt")), function(error){ //Проверяем файл на существование
+        if (error) { //Если файл не существует
+            fs.writeFile((dir+"/files/fresh.txt"), "I am fresh and young", function(error){ //
+                if(error) { //Если произошла ошибка
+                    console.log("FS operation failed");
+                } else { //Иначе
+                    console.log("Файл создан");
+                }
+            });
+        } else { //Иначе
+            console.log("FS operation failed");
+        }
+    });
 };
