15ca4ade4d7cd94720d18afb4248cce55b3a08e6
antoshastark <antonkozel761@gmail.com>
2022-06-05 21:39:15 +0300

    Finalised Stream part, but does not work

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,12 +1,14 @@
 import * as fs from 'fs'
-import * as stream from 'stream'
 
 const fileLoc = "./src/streams/files/fileToRead.txt"
 
+// Не работает когда пытаюсь исключительно стримами пишет. This file should be read using Streams API. Почему нельзя через пайп кинуть что-то process.stdp?
 export const read = async () => {
     const readable = fs.createReadStream(fileLoc)
-    readable.on('data', (chunk) => {
-        return process.stdout.write(chunk)
+    readable.on("data", (dataChunk) => {
+        process.stdout.write(dataChunk, () => {
+            console.log("Успешно перенесено")
+        })
     })
 };
 read()
