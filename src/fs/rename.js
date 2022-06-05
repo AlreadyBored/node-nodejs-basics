f9fe3ec34c632971c5456d4597303cdcad61a199
IrinaAfinogenova <52318667+IrinaAfinogenova@users.noreply.github.com>
2022-06-05 22:29:22 +0300

    rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,16 @@
+import fs, {access} from 'fs/promises';
+import {checkFileExists} from './utils.js';
+
 export const rename = async () => {
-    // Write your code here 
+    const isOldFileExist = await checkFileExists('src/fs/files/wrongFilename.txt');
+    const isNewFileAlrearyExist = await checkFileExists('src/fs/files/properFilename.md');
+    
+    if (!isOldFileExist || isNewFileAlrearyExist) {
+        throw new Error('FS operation failed');
+    }
+
+    await fs.rename('src/fs/files/wrongFilename.txt', 'src/fs/files/properFilename.md');
 };
+
+rename()
+    .catch(() => console.log('properFilename.md already exists or wrongFilename.txt has already been deleted'));
