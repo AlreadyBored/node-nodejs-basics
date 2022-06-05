3013429f3dbead453ec4444aa2dcea49da4115d2
AntonFartovii <anton@rza.by>
2022-06-05 17:28:48 +0300

    feat: updata delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,13 @@
-export const remove = async () => {
-    // Write your code here 
+const path = require('path')
+const fs = require('fs')
+
+export const remove = async (filename) => {
+    // Write your code here
+    fs.unlink(
+        path.join(__dirname, 'files', filename),
+        (err) => {
+            if (err) {
+                console.log ( 'FS operation failed' )
+            }
+        })
 };
