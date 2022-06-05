1dc94384fa2d9877927e1fed60ff0e89b10e53f2
Evgeniy <85104881+EVG777-prog@users.noreply.github.com>
2022-06-05 23:39:02 +0300

    feat: streams

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,15 @@
-export const write = async () => {
-    // Write your code here 
+import * as fs from 'fs';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+import { stdin, stdout } from 'process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const write = async (file) => {
+  const filePath = path.join(__dirname, 'files', file);
+  const writeStream = fs.createWriteStream(filePath);
+  stdin.pipe(writeStream);
 };
+
+write('fileToWrite.txt');
