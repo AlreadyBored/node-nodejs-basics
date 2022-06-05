4182d7599b2c6a52c82753655efee8c5df1663e1
Maksim Pevnev <drummen11@gmail.com>
2022-06-05 23:31:10 +0400

    develop: worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,39 @@
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+
+const START_NUMBER_OF_CALCULATE = 10;
+
 export const performCalculations = async () => {
-    // Write your code here
+    const cpuCores = cpus();
+
+    const resultArray = await Promise.all(
+        cpuCores.map(async (cpuCore, index) => {
+            const promisedWorkerResult = new Promise((resolve, reject) => {
+                const worker = new Worker('./worker.js', {
+                    workerData: START_NUMBER_OF_CALCULATE + index,
+                });
+                worker.on('message', resolve);
+                worker.on('error', reject);
+            });
+
+            return promisedWorkerResult.then(
+                (calculatedResult) => {
+                    return {
+                        status: 'resolved',
+                        data: calculatedResult,
+                    };
+                },
+                () => {
+                    return {
+                        status: 'error',
+                        data: null,
+                    };
+                },
+            );
+        }),
+    );
+
+    console.log(resultArray);
 };
+
+performCalculations();
