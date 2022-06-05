bb8f6901911616a4a8193a263824f7f255e70beb
AntonFartovii <anton@rza.by>
2022-06-05 17:43:47 +0300

    feat: update read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,16 @@
+const path = require('path')
+const fs = require('fs')
+
 export const read = async () => {
-    // Write your code here 
+    // Write your code here
+    fs.readFile(
+        path.join(__dirname, 'files', 'fileToRead.txt'),
+        function (err, data) {
+            if (err) {
+                console.log ( 'FS operation failed must be thrown' )
+            } else {
+                console.log (data.toString())
+            }
+        }
+    )
 };
