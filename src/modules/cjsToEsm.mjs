878508ee5d598fae9aad4a78f6e3e790a024afc3
Yuliya Khvan <yuliya.khv@gmail.com>
2022-06-06 02:34:11 +0600

    feat: implement refactor

---
+++ src/modules/cjsToEsm.mjs
@@ -0,0 +1,29 @@
+import path from 'path';
+import {fileURLToPath} from 'url';
+import { release, version } from 'os';
+import { createServer as createServerHttp } from 'http';
+import './files/c.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const random = Math.random();
+
+let unknownObject;
+
+if (random > 0.5) {
+    unknownObject = import ('./files/a.json', { assert: { type: 'json' } });
+} else {
+    unknownObject = import ('./files/b.json', { assert: { type: 'json' } });
+}
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${path.sep}"`);
+
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
+
+export const createMyServer = createServerHttp((_, res) => {
+    res.end('Request accepted');
+});
