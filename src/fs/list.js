3c20d510553a26d6d64ea2c75fd2bf686e3dcefb
MINSK\Nikita_Pytalev <nikita_pytalev@epam.com>
2022-06-05 23:51:43 +0300

    implement list.js with async promises

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,17 +1,16 @@
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import fs from 'fs';
+import fs from 'fs/promises';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const filesPath = path.join(__dirname, 'files');
 
 export const list = async () => {
-    fs.readdir(filesPath, (err, files) => {
-        if (err) {
-            throw Error('FS operation failed');
-        }
-
+    try {
+        const files = await fs.readdir(filesPath);
         console.log(files);
-    });
+    } catch {
+        throw Error('FS operation failed');
+    }
 };
