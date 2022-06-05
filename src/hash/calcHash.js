b44b4ac36c79e44155221a2ecc771d72f3f9c4cf
Alex <x3m.f@mail.ru>
2022-06-06 00:29:49 +0400

    feat: add hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,20 @@
+import path, { dirname } from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import crypto from 'crypto'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+    fs.readFile(filePath, (err, data) => {
+        if(err) throw err;
+        const hashSum = crypto.createHash('sha256');
+        hashSum.update(data);
+        const hex = hashSum.digest('hex');
+        console.log(hex);
+    });
 };
+
+calculateHash();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,10 +1,5 @@
-import fs, { createReadStream, createWriteStream } from 'fs';
+import fs from 'fs';
 import { createUnzip } from 'zlib';
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 export const decompress = async () => {
     const unzip = createUnzip();
