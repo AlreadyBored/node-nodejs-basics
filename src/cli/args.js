f87bce53a0bdebdaef944ba7244952df079e41f4
IliaPavl <i.pavl@list.ru>
2022-06-05 12:43:18 +0300

    make cli / npm run testCli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,14 @@
 export const parseArgs = () => {
     // Write your code here 
+    const data = process.argv;
+
+    let localData;
+    for (let index in data) {
+        if (index > 1) {
+            (data[index].startsWith('--')) ?
+                localData = data[index] :
+                console.log(localData + ' is ' + data[index]);
+        }
+    }
 };
+
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,9 @@
 export const parseEnv = () => {
     // Write your code here 
+    const data = process.env;
+    for (let name in data) {
+        if (name.startsWith('RSS_'))
+            console.log(name + '=' + data[name])
+    }
 };
+
