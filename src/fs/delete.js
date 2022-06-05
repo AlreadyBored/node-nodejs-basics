89ada5318ec513b629692ff4abdf98754782744c
Muhammed Abdrahman <muhammed03amin.com>
2022-06-06 00:54:03 +0600

    feat: add solution for delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,15 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from 'fs';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export const remove = async () => {
-    // Write your code here 
+    const delFile = path.join(__dirname, '/files/fileToRemove.txt');
+    if (!fs.existsSync(delFile)) {
+      throw new Error('FS operation failed');
+    }
+    fs.unlinkSync(delFile);
 };
+
+remove();
