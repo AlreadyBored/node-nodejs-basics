061ec2dff5a35d3ef2c85c190b3c98bde9f058f4
Maksim Pevnev <drummen11@gmail.com>
2022-06-05 19:57:33 +0400

    develop: add stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,18 @@
+import {  access, constants, createReadStream } from 'fs';
+
+const FILE_TO_READ = `./files/fileToRead.txt`;
+const FS_OPERATION_ERROR = 'FS operation failed';
+
 export const read = async () => {
-    // Write your code here 
+    access(FILE_TO_READ, constants.F_OK, (err) => {
+        if (err !== null) {
+            throw new Error(FS_OPERATION_ERROR);
+        }
+    });
+
+    const stream = createReadStream(FILE_TO_READ);
+
+    stream.pipe(process.stdout);
 };
+
+read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,22 @@
+import {  access, constants, createWriteStream } from 'fs';
+
+const FILE_TO_WRITE = `./files/fileToWrite.txt`;
+const FS_OPERATION_ERROR = 'FS operation failed';
+
 export const write = async () => {
-    // Write your code here 
+    access(FILE_TO_WRITE, constants.F_OK, (err) => {
+        if (err !== null) {
+            throw new Error(FS_OPERATION_ERROR);
+        }
+    });
+
+    const stream = createWriteStream(FILE_TO_WRITE);
+
+    process.stdin.pipe(stream);
+
+    process.stdin.on('end', function() {
+        process.stdout.write('Stream ended.');
+    });
 };
+
+write();
