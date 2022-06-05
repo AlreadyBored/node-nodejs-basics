bfcf7a8fc6bc1d62fcbb84e64e954a8d98e1eb3d
MINSK\Nikita_Pytalev <nikita_pytalev@epam.com>
2022-06-05 23:28:46 +0300

    implement wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,39 @@
+import { cpus } from 'os';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { Worker } from 'worker_threads';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const workerPath = path.join(__dirname, 'worker.js');
+
 export const performCalculations = async () => {
-    // Write your code here
+    const cpuCount = cpus().length;
+    const nums = [];
+    for (let i = 0; i < cpuCount; i++) {
+        try {
+            const num = await runWorker(10 + i);
+
+            nums.push({
+                status: 'resolved',
+                data: num
+            });
+        } catch {
+            nums.push({
+                status: 'error',
+                data: null
+            });
+        }
+    }
+
+    return nums;
 };
+
+const runWorker = workerData => {
+    return new Promise((resolve, reject) => {
+        const worker = new Worker(workerPath, { workerData });
+        worker.on('message', resolve);
+        worker.on('error', reject);
+    });
+}
