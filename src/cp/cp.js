411af049ee2c2328321e387446b0639f1b3caafc
Mikita Masliyevich <m.masliyevich@godeltech.com>
2022-06-05 15:21:42 +0200

    feat: add task1

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,14 @@
+import process from "process";
+import path from "path";
+import { fork } from "child_process";
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  try {
+    const __dirname = path.resolve();
+    const filePath = path.join(__dirname, "/files", "script.js");
+    let x = args.slice(2);
+    fork(filePath, x);
+  } catch (error) {
+    throw error;
+  }
 };
+spawnChildProcess(process.argv);
