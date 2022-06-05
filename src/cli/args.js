87e6c059d9a5e7dbebc8f2043e3708c54334ccac
Henadzi Arashkevich <oreshkevich85@mail.ru>
2022-06-05 19:25:42 +0300

    feat: add cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,32 @@
-export const parseArgs = () => {
-    // Write your code here 
+const parseArgs = () => {
+  const fs = require('fs');
+  const path = require('path');
+  const { stdin, stdout } = require('process');
+
+  stdout.write(
+    'Write something in the format --propName value --prop2Name value2'
+  );
+
+  stdin.addListener('data', (data) => {
+    let example = data.toString().split('--');
+
+    let value1 = example[1];
+    let value2 = example[2];
+
+    if (data.toString().trim() == 'exit') {
+      console.log('Bye!');
+      process.exit();
+    }
+
+    console.log(`you wrote`);
+    console.log(`${value1} ${value2}`);
+  });
+
+  process.addListener('SIGINT', function () {
+    console.log('Bye!');
+    process.exit();
+  });
 };
+
+parseArgs();
+
