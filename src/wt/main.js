d7bb8e34be16aae5d45e264ae0a5d0411b77b406
Vladyslav <makarellajs@gmail.com>
2022-06-05 13:37:55 +0300

    wt: done

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,37 @@
+import { cpus } from 'os';
+import { Worker } from 'worker_threads';
+
 export const performCalculations = async () => {
-    // Write your code here
+  const cpuData = cpus();
+  const promises = [];
+
+  for (let i = 0; i < cpuData.length; i++) {
+    const promise = new Promise((resolve, reject) => {
+      const worker = new Worker('./src/wt/worker.js', {
+        workerData: 10 + i,
+      });
+
+      worker.on('message', (msg) => {
+        resolve({
+          status: 'resolved',
+          data: msg,
+        });
+      });
+
+      worker.on('error', () => {
+        reject({
+          status: 'error',
+          data: null,
+        });
+      });
+    });
+
+    promises.push(promise);
+  }
+
+  const result = await Promise.all(promises);
+
+  return result;
 };
+
+console.log(await performCalculations());
