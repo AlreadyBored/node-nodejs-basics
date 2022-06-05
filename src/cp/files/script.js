8f280e22e57c9b7d97d06cabb134e2f562bb7930
Henadzi Arashkevich <oreshkevich85@mail.ru>
2022-06-05 20:24:15 +0300

    feat: add cp

--- src/cp/files/script.js
+++ src/cp/files/script.js
@@ -1,12 +1,16 @@
-const args = process.argv.slice(2);
+const sayHi = (args) => {
+  console.log(`Total number of arguments is ${args.length}`);
+  console.log(`Arguments: ${JSON.stringify(args)}`);
 
-console.log(`Total number of arguments is ${args.length}`);
-console.log(`Arguments: ${JSON.stringify(args)}`);
-
-const echoInput = (chunk) => {
+  const echoInput = (chunk) => {
     const chunkStringified = chunk.toString();
     if (chunkStringified.includes('CLOSE')) process.exit(0);
-    process.stdout.write(`Received from master process: ${chunk.toString()}\n`)
+    process.stdout.write(`Received from master process: ${chunk.toString()}\n`);
+  };
+
+  process.stdin.on('data', echoInput);
+};
+module.exports = {
+  sayHi,
 };
 
-process.stdin.on('data', echoInput);
