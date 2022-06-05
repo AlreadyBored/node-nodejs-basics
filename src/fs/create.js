2511c871131edc8da9d78026d1a1d318f2966e54
Muhammed Abdrahman <muhammed03amin.com>
2022-06-06 00:46:59 +0600

    feat: add solution for create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,18 @@
+import path from 'path';
+import { fileURLToPath } from "url";
+import fs from 'fs';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export const create = async () => {
-    // Write your code here 
+  const file = path.join(__dirname, '/files/fresh.txt')
+  if (fs.existsSync(file)) {
+    throw new Error('FS operation failed')
+  }
+
+  fs.writeFileSync(file, 'I am fresh and young')
+
+  fs.writeFileSync(path.join(__dirname, 'jai.txt'), 'Zhai zat qoi')
 };
+
+create()
