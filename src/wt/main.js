51e5f70abdcbc053486ece329b07121bf62e30e9
Oleksii <alexv77ua@gmail.com>
2022-06-05 13:58:23 +0300

    complete work threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,21 @@
+import { Worker, workerData } from "node:worker_threads";
+import os from "os";
+
 export const performCalculations = async () => {
-    // Write your code here
+  const threadsNum = os.cpus();
+  let fibData = 10;
+  const promiceArray = [];
+
+  for (let index = 1; index <= threadsNum.length; index++) {
+    promiceArray.push(
+      new Promise((resolve, reject) => {
+        new Worker("./wt/worker.js", { workerData: fibData++ })
+          .on("message", (data) => resolve({ status: "resolved", data }))
+          .on("error", () => resolve({ status: "error", data: null }));
+      })
+    );
+  }
+  return await Promise.all(promiceArray).then((val) => console.log(val));
 };
+
+await performCalculations();
