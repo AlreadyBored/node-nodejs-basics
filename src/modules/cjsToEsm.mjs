4adcbe9b042e14a1af5f3ac7d36dc0a93f1cd3c8
sir-kitter <sire.kitter@gmail.com>
2022-06-05 18:14:04 +0300

    feat: modules

---
+++ src/modules/cjsToEsm.mjs
@@ -0,0 +1,24 @@
+import path from 'path'
+import { release, version } from 'os'
+import * as http from 'http'
+import './files/c.js'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const random = Math.random();
+
+export let unknownObject = await import(random > 0.5 ? './files/a.json' : './files/b.json', { assert: { type: 'json' } }) 
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${path.sep}"`);
+
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
+
+export const createMyServer = http.createServer((_, res) => {
+    res.end('Request accepted');
+})
+
