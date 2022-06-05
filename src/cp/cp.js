839a5399a672ac774e3cbf025552a1d4d014e5b9
webdev <dezzzh93@gmail.com>
2022-06-06 00:03:08 +0800

    feat: completed the task of the subtask "Child Processes (src/cp)"

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,9 @@
-export const spawnChildProcess = async (args) => {
-    // Write your code here
+import { exec } from "child_process";
+
+const spawnChildProcess = async (args) => {
+    exec(`node "./files/script.js" ${args}`, (err, stdout) => {
+        stdout.write( stdout );
+    })
 };
+
+export default  spawnChildProcess();
