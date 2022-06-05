98b106ae2b0784e6a4e609bcabf9c0d932fc2890
Yuliya Khvan <yuliya.khv@gmail.com>
2022-06-06 00:46:44 +0600

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,19 @@
+import fs from "fs/promises";
+
 export const list = async () => {
-    // Write your code here 
+    const filesFolder = "./src/fs/files";
+    try {
+        const files = await fs.readdir(filesFolder);
+        for (const file of files) {
+            console.log(file);
+        }
+    } catch (error) {
+        if (error.code === "ENOENT") {
+            throw new Error("FS operation failed");
+          } else {
+              console.log(error);
+          }
+    }
 };
+
+list();
