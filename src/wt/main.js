e19336c2346ca06377886a44d4ab85bf3333dae0
Aleksey Shulyak <a.shulyak@itransition.com>
2022-06-06 00:45:23 +0400

    feature: nodejs basics wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,41 @@
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+import path from "path";
+import {fileURLToPath} from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const performCalculations = async () => {
-    // Write your code here
+    const pathToWorker = path.resolve(__dirname, 'worker.js');
+    const numCPUs = cpus().length;
+    const countStart = 10;
+    const countEnd = 10 + numCPUs;
+    const listPromises = [];
+
+    for (let i = countStart; i < countEnd; i++) {
+        const worker = new Worker(pathToWorker);
+        const prm = new Promise(((resolve, reject) => {
+            worker.once('message', resolve);
+            worker.on('error', reject);
+            worker.postMessage(i + 1);
+        }));
+
+        listPromises.push(prm);
+    }
+
+    const result = await Promise.allSettled(listPromises);
+
+    const data = result.map(({ status, value }) => {
+        return {
+            status: status === 'rejected' ? 'error' : 'resolved',
+            data: value || null,
+        }
+    });
+
+    console.log(data);
+
+    return data;
 };
+
+performCalculations();
