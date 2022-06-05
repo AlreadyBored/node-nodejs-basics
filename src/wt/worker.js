1a616f89225a031baa7c505a431a2360c1e0c621
Vadzim Shut <vadzim_shut@epam.com>
2022-06-05 22:48:37 +0300

    feat: implement wt task

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,16 @@
+import { parentPort, workerData } from 'worker_threads'
+
 // n should be received from main thread
-export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+export const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2)
 
 export const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
-};
+  // This function sends result of nthFibonacci computations to main thread
+  const data = nthFibonacci(workerData)
+  parentPort.postMessage({
+    status: data ? 'resolved' : 'error',
+    data: data ?? null,
+  })
+}
+
+sendResult()
