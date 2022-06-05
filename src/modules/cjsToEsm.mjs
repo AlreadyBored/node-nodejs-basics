9667c4baa6cd6ae63d335ae512f64cec2ac2a7de
DavydVeremchuk <davyd.v@code-care.pro>
2022-06-05 21:22:47 +0300

    fix: make json imports dynamic in modules task

--- src/modules/cjsToEsm.mjs
+++ src/modules/cjsToEsm.mjs
@@ -2,8 +2,6 @@ import * as path from "node:path";
 import { release, version } from "node:os";
 import { createServer as createServerHttp } from "node:http";
 import { fileURLToPath } from "node:url";
-import aJSON from "./files/a.json" assert { type: "json" };
-import bJSON from "./files/b.json" assert { type: "json" };
 import "./files/c.js";
 
 const random = Math.random();
@@ -13,10 +11,15 @@ const __dirname = path.dirname(__filename);
 let unknownObject;
 
 if (random > 0.5) {
-  unknownObject = aJSON;
+  unknownObject = await import("./files/a.json", {
+    assert: { type: "json" },
+  }).then((data) => data.default);
 } else {
-  unknownObject = bJSON;
+  unknownObject = await import("./files/b.json", {
+    assert: { type: "json" },
+  }).then((data) => data.default);
 }
+console.log(unknownObject);
 
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
