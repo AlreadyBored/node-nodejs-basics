37b16c1fa6c59a1a735769d04efa42d30dc234cc
Vladimir <98771978+VladIn4e@users.noreply.github.com>
2022-06-06 00:19:04 +0400

    Update rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,29 @@
+//Импорт
+const fs = require("fs");
+const fsp = require("fs/promises");
+const path = require("path");
+
 export const rename = async () => {
-    // Write your code here 
+    //Указываем пути
+    const dir = path.resolve(__dirname);
+    const dir1 = (dir+"/wrongFilename.txt");
+    const dir2 = (dir+"/properFilename.md");
+
+    fs.access(((dir1)), function(error){ // Проверяем существует ли файл
+        if (error) { //Если нет то выводис ошибку
+            console.log("FS operation failed");
+        } else { // Иначе
+            fs.access(((dir2)), function(error){ //Проверяем существуетли второй файл
+                if (error) { //Если нет то
+                    try { 
+                        fsp.rename(dir1, dir2) //Переименновываем
+                    } catch (ex) {
+                        console.log("FS operation failed"); //В случае ошибки выводим сообщение об ошибке
+                    }
+                } else { //Иначе
+                    console.log("FS operation failed");
+                }
+            });
+        }
+    });
 };
