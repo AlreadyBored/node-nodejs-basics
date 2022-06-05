3001f3747a8a5d55a59af6c5d4cc2c945e49b8ae
Muhammed Abdrahman <muhammed03amin.com>
2022-06-06 02:42:49 +0600

    feat: add solution for worker.js

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,13 @@
+import {workerData, parentPort} from 'worker_threads';
+
 // n should be received from main thread
 export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 export const sendResult = () => {
+  parentPort.postMessage(nthFibonacci(workerData))
     // This function sends result of nthFibonacci computations to main thread
 };
+
+parentPort.on('message', () => {
+  sendResult()
+})
