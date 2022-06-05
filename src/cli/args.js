3a63859eaabfcabc983dc6c1e208f46db8ff92b1
NatalliaK <studina-natali@yandex.by>
2022-06-05 18:51:48 +0300

    feat: complete cli blocs tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,18 @@
+import { argv } from 'node:process';
+
 export const parseArgs = () => {
-    // Write your code here 
+    const args = [...argv].slice(2);
+
+    let printedStr = '';
+    args.forEach((arg, i) => {
+        printedStr = !!arg.startsWith('--') ? `${arg.slice(2)} is ` : printedStr + arg;
+
+        if (i % 2 !== 0) {
+            console.log(printedStr);
+            printedStr = '';
+        }
+    })
+
 };
+
+parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,13 @@
+import { env } from 'node:process';
+
 export const parseEnv = () => {
-    // Write your code here 
+    // 2 test env
+    env.RSS_test1 = 1;
+    env.RSS_test2 = 'hello';
+
+    Object.entries(env)
+        .filter(([key, value]) => key.startsWith('RSS_'))
+        .forEach(([key, value]) => console.log(`${key}=${value}`));
 };
+
+parseEnv();
