aafa94b8dde9e444add1071a0f7cee5bc7e37595
Yuliya Khvan <yuliya.khv@gmail.com>
2022-06-06 00:41:20 +0600

    feat: implement delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,20 @@
+import fs from "fs/promises";
+
 export const remove = async () => {
-    // Write your code here 
+    const filesFolder = "./src/fs/files";
+    const removeFilename = 'fileToRemove.txt';
+    try {
+        const files = await fs.readdir(filesFolder);
+        const deleteFiles = files.find((file) => file === removeFilename);
+        if (!deleteFiles) {
+            throw new Error('FS operation failed');
+        }
+        if (deleteFiles) {
+            await fs.unlink(`${filesFolder}/${removeFilename}`);
+        }
+    } catch (error) {
+        console.log(error);
+    } 
 };
+
+remove();
