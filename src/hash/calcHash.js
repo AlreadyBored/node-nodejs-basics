a598a891a1e878b6347c7d61cfabe46f3c944475
mrfz <zlobin.f@gmail.com>
2022-06-05 12:52:33 +0300

    feat: add hash calculation feature

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,18 @@
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { readFile } from 'node:fs/promises';
+import { createHash } from 'node:crypto';
+
+const pathToHashCalc = dirname(fileURLToPath(import.meta.url)) + '/files/fileToCalculateHashFor.txt'
+
 export const calculateHash = async () => {
-    // Write your code here 
+    
+    try {
+        const fileToHashCalc = await readFile(pathToHashCalc);
+        const hash = createHash('sha256');
+        console.log(hash.update(fileToHashCalc).digest('hex'));   
+    } catch (err) {
+        console.log("Catch some error: ") + err;
+    }
+    
 };
