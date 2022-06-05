16b0e6219afa3863b3ce8a13681ac0a0f15d4b88
PAVEL KOROTEEV <pavelkoroteev@yandex-team.ru>
2022-06-05 18:02:43 +0300

    cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,25 @@
+const KEY_REGEXP = /--(\w+)/;
+const SEPARATOR = ', ';
+
 export const parseArgs = () => {
-    // Write your code here 
+    // args.js - implement function that parses command line arguments
+    // (given in format --propName value --prop2Name value2, you don't need to validate it)
+    // and prints them to the console in the format propName is value, prop2Name is value2
+    const output = process.argv.slice(2).reduce((previous, arg) => {
+        const result = KEY_REGEXP.exec(arg);
+
+        if (result) {
+            const [, key] = result;
+
+            if (previous) {
+                return `${previous}${SEPARATOR}${key}`;
+            }
+            return key;
+        }
+        return `${previous} is ${arg}`;
+    }, '');
+
+    console.log(output);
 };
+
+parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,14 @@
 export const parseEnv = () => {
-    // Write your code here 
+    // env.js - implement function that parses environment variables with prefix RSS_
+    // and prints them to the console in the format RSS_name1=value1; RSS_name2=value2
+
+    const rssEntries = Object.entries(process.env).filter(([key]) => {
+        return key.startsWith('RSS_');
+    });
+
+    console.log(rssEntries.map(([key, value]) => {
+        return `${key}=${value}`;
+    }).join('; '));
 };
+
+parseEnv();
