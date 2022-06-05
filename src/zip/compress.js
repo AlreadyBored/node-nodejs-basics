cd49515661f898f5a18f3e0cdc03c743a41cf69d
IrinaAfinogenova <52318667+IrinaAfinogenova@users.noreply.github.com>
2022-06-05 22:33:35 +0300

    compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,12 @@
+import fs from 'fs';
+import zlib from 'zlib';
+
 export const compress = async () => {
-    // Write your code here 
+    const input = fs.createReadStream('src/zip/files/fileToCompress.txt', 'utf-8');
+    const output = fs.createWriteStream('src/zip/files/archive.gz');
+    const gzip = zlib.createGzip();
+
+    input.pipe(gzip).pipe(output);
 };
+
+compress();
