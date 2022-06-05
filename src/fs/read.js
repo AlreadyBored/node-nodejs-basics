75d2ecbbd24836600f4364714539a6b877d70b77
mrfz <zlobin.f@gmail.com>
2022-06-05 07:20:06 +0300

    feat: add read file func

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,19 @@
+import { readFile } from 'fs/promises';
+import { dirname } from 'path'
+import { fileURLToPath } from 'url';
+
+const fileToReadPath = dirname(fileURLToPath(import.meta.url)) + '/files/fileToRead.txt'
+
 export const read = async () => {
-    // Write your code here 
+    try {
+        const outputString = await readFile(fileToReadPath, 'utf8');
+        console.log(outputString)
+    } catch (err) {
+        if (err.code === "ENOENT") {
+             throw new Error('FS operation failed');
+        }
+        else {
+            console.log("What hAPPEND:" + err);
+        }
+    }
 };
