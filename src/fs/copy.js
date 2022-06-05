15a90df7d6de12b11cdbc517b2e987611ba1777d
IrinaAfinogenova <52318667+IrinaAfinogenova@users.noreply.github.com>
2022-06-05 22:25:23 +0300

    copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,18 @@
+import {mkdir, readdir, copyFile} from 'fs/promises';
+
 export const copy = async () => {
-    // Write your code here 
-};
+    try {
+        const files = await readdir('src/fs/files');
+        await mkdir('src/fs/files_copy');
+
+        for (const file of files) {
+            await copyFile(`src/fs/files/${file}`, `src/fs/files_copy/${file}`);
+        }
+    } catch {
+        throw new Error('FS operation failed')
+    }
+}
+
+copy()
+    .then(() => console.log('success'))
+    .catch(() => console.log('Please, check what files forder exist or delete files_copy forder'));
