d65503617b07584406c65e50a81bc58f0488fd18
MINSK\Nikita_Pytalev <nikita_pytalev@epam.com>
2022-06-05 23:53:26 +0300

    implement delete.js with async promises

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,15 +1,15 @@
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import fs from 'fs';
+import fs from 'fs/promises';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const fileToRemovePath = path.join(__dirname, 'files', 'fileToRemove.txt');
 
 export const remove = async () => {
-    fs.unlink(fileToRemovePath, err => {
-        if (err) {
-            throw Error('FS operation failed')
-        }
-    });
+    try {
+        await fs.unlink(fileToRemovePath);
+    } catch {
+        throw Error('FS operation failed');
+    }
 };
