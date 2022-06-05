4fad1a44a690d8082021ab11ca1318bbd22cd865
mrfz <zlobin.f@gmail.com>
2022-06-05 20:35:09 +0300

    feat: add args parse func

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,8 @@
 export const parseArgs = () => {
-    // Write your code here 
+    let argsArray = process.argv;
+    let outputString = "";
+    for (let i = 2; i < argsArray.length; i++) {
+        outputString = outputString.concat(`${argsArray[i].slice(2)} is ${argsArray[++i]} `);
+    }
+    console.log(outputString.slice(0,-1));
 };
