e5ecb8f865cbbac07a0bdbea09420907f66f9c38
Aleksey Shulyak <a.shulyak@itransition.com>
2022-06-06 00:46:14 +0400

    feature: nodejs basics hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,20 @@
+import crypto from "crypto";
+import path from "path";
+import fs from "fs/promises";
+import {fileURLToPath} from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const filePath = path.resolve(__dirname, 'files', 'fileToCalculateHashFor.txt');
+    const data = await fs.readFile(filePath);
+    const hash = crypto.createHash('sha256').update(data).digest('hex');
+
+    console.log("\x1b[32m", hash, '\x1b[0m');
+
+    return hash;
+
 };
+
+calculateHash();
