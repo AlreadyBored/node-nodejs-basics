9423169578abeea52f0a206ded83792374723f26
Vadzim Shut <vadzim_shut@epam.com>
2022-06-05 19:13:11 +0300

    feat: implement read function about writeStream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,25 @@
+import { open } from 'fs/promises'
+import { stdin } from 'process'
+
+const FILE_TO_WRITE = './files/fileToWrite.txt'
+
 export const write = async () => {
-    // Write your code here 
-};
+  let fd
+
+  const writableStream = async (data) => {
+    try {
+      fd = await open(FILE_TO_WRITE, 'a')
+      fd.createWriteStream().write(data)
+    } catch (error) {
+      throw new Error('Error with open file.')
+    } finally {
+      await fd?.close()
+    }
+  }
+
+  stdin.on('data', async (chunk) => {
+    await writableStream(chunk)
+  })
+}
+
+write()
