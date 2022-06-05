6258052851ec4403605a82c88ff18b7e791da722
Maria White <redstone.mari@gmail.com>
2022-06-05 12:47:11 +0300

    feat: add list funcstion

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,24 @@
+import * as fs from 'fs';
+
 export const list = async () => {
-    // Write your code here 
+    try {
+        fs.access('./src/fs/files', (err) => {
+            if (err) {
+                throw new Error('FS operation failed');
+            } else {
+                fs.readdir('./src/fs/files/', (err, files) => {
+                    if (err) {
+                        throw err;
+                    }
+                    files.forEach(file => {
+                      console.log(file);
+                    });
+                  });
+            }
+        });        
+    } catch (e) {
+        console.log(e);
+    }
 };
+
+await list();
