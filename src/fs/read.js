9605304c81c48a9697569ba47feb15f2357c5621
Yuliya Khvan <yuliya.khv@gmail.com>
2022-06-06 00:53:15 +0600

    feat: implement read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,25 @@
+import fs from "fs/promises";
+
 export const read = async () => {
-    // Write your code here 
+    const filesFolder = "./src/fs/files";
+    const readFilename = 'fileToRead.txt';
+    try {
+        const files = await fs.readdir(filesFolder);
+        const readFile = files.find((file) => file === readFilename);
+        if (!readFile) {
+            throw new Error("FS operation failed");
+        }
+        if (readFile) {
+            const content = await fs.readFile(`${filesFolder}/${readFilename}`, 'utf8');
+            console.log(content);
+        }
+    } catch (error) {
+        if (error.code === "ENOENT") {
+            throw new Error("FS operation failed");
+          } else {
+              console.log(error);
+          }
+    } 
 };
+
+read();
