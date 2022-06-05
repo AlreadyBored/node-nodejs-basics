8ce066a0cd778dbc064ce778d67a397a4c0418d6
Aleksey Shulyak <a.shulyak@itransition.com>
2022-06-06 00:44:50 +0400

    feature: nodejs basics zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,25 @@
+import * as zlib from 'zlib';
+import * as stream from 'stream';
+import path from "path";
+import * as fs from "fs";
+import {fileURLToPath} from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const compress = async () => {
-    // Write your code here 
+    const fileFrom = path.resolve(__dirname, 'files', 'fileToCompress.txt');
+    const fileTo = path.resolve(__dirname, 'files', 'archive.gz');
+
+    const gzip = zlib.createGzip();
+    const source = fs.createReadStream(fileFrom);
+    const destination = fs.createWriteStream(fileTo);
+
+    stream.pipeline(source, gzip, destination, (err) => {
+        if (err) {
+            console.error(err);
+        }
+    });
 };
+
+compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,23 @@
+import * as zlib from 'zlib';
+import path from "path";
+import * as fs from "fs";
+import {fileURLToPath} from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const decompress = async () => {
-    // Write your code here 
+    const filePath = path.resolve(__dirname, 'files', 'fileToCompress.txt');
+    const archive = path.resolve(__dirname, 'files', 'archive.gz');
+    const buffer = await fs.promises.readFile(archive);
+
+    zlib.unzip(buffer, async (err, buffer) => {
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
+        await fs.promises.appendFile(filePath, buffer.toString());
+    });
 };
+
+decompress();
