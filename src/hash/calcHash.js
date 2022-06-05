34427721b5a96f6457f963297bef4ca40437861a
NatalliaK <studina-natali@yandex.by>
2022-06-05 20:23:09 +0300

    feat: complete hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,19 @@
+import { createHash } from 'node:crypto';
+import { fileURLToPath } from 'node:url';
+import { readFile } from 'node:fs/promises';
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const filePath = fileURLToPath(new URL('./files/fileToCalculateHashFor.txt', import.meta.url));
+
+    const data = await readFile(filePath, 'utf-8');
+
+    const hash = await createHash('sha256')
+        .update(data)
+        .digest('hex');
+
+    console.log(hash);
+
+    return hash;
 };
+
+calculateHash();
