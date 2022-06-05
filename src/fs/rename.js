205373e556003669eaae9050cc5f17257b077f44
Yuliya Khvan <yuliya.khv@gmail.com>
2022-06-06 00:36:03 +0600

    feat: implement rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,22 @@
+import fs from "fs/promises";
+
 export const rename = async () => {
-    // Write your code here 
+  const filesFolder = "./src/fs/files";
+  const wrongFilename = 'wrongFilename.txt';
+  const properFilename = 'properFilename.md';
+  try {
+    const files = await fs.readdir(filesFolder);
+    const wrongFiles = files.find((file) => file === wrongFilename);
+    const properFiles = files.find((file) => file === properFilename);
+    if (!wrongFiles || properFiles) {
+      throw new Error('FS operation failed');
+    }
+    if (wrongFiles && !properFiles) {
+      await fs.copyFile(`${filesFolder}/${wrongFilename}`, `${filesFolder}/${properFilename}`);
+    }
+  } catch (error) {
+    console.log(error);
+  } 
 };
+
+rename();
