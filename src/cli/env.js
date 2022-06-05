775050ebb0a3a5c585df24b4d50a38b2a5fe7b4b
mrfz <zlobin.f@gmail.com>
2022-06-05 18:06:10 +0300

    feat: add parse environment vars

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,5 @@
 export const parseEnv = () => {
-    // Write your code here 
+    let outputString = ""
+    Object.entries(process.env).filter(([key,value]) => key.substring(0,4) === 'RSS_').map(([key,value]) => outputString = outputString.concat(`${key}=${value}; `));
+    outputString !== ""  ?  console.log(outputString.slice(0,outputString.length-2)) : null;
 };
