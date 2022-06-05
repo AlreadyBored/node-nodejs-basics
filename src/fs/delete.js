4a6a97507c223b8a83e6e971e97f29d2feb514f9
AntonFartovii <anton@rza.by>
2022-06-05 17:31:22 +0300

    fix: filename delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,10 +1,10 @@
 const path = require('path')
 const fs = require('fs')
 
-export const remove = async (filename) => {
+export const remove = async () => {
     // Write your code here
     fs.unlink(
-        path.join(__dirname, 'files', filename),
+        path.join(__dirname, 'files', 'fileToRemove.txt'),
         (err) => {
             if (err) {
                 console.log ( 'FS operation failed' )
