dc6982d285efc82566182f0a86a30d69386cf926
IrinaAfinogenova <52318667+IrinaAfinogenova@users.noreply.github.com>
2022-06-05 22:26:26 +0300

    remove

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,16 @@
+import {rm} from 'fs/promises';
+import {checkFileExists} from './utils.js';
+
 export const remove = async () => {
-    // Write your code here 
+    const isFileExist = await checkFileExists('src/fs/files/fileToRemove.txt');
+
+    if (!isFileExist) {
+        throw new Error('FS operation failed');
+    }
+
+    await rm('src/fs/files/fileToRemove.txt');
 };
+
+remove()
+    .then(() => console.log('The file was deleted successfully'))
+    .catch(() => console.log('The file has already been deleted'));
