abb75c52d213e822ae5f931fa1983de49804c6b7
Muhammed Abdrahman <muhammed03amin.com>
2022-06-06 02:10:17 +0600

    feat: add solution for write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,14 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from 'fs';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export const write = async () => {
-    // Write your code here 
+  const file = path.join(__dirname, '/files/fileToRead.txt');
+  const writeStream = fs.createWriteStream(file, 'utf8');
+  process.stdin.pipe(writeStream)
+  process.stdin.resume()
 };
+
+write()
