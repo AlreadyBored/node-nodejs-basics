20a37e51c32625f078bb4cb68474a77b7c07b085
Olena Honcharuk <gasmin07@gmail.com>
2022-06-05 14:31:15 +0300

    feat: add solution for streams write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,44 @@
+import fs from 'fs';
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import readline from 'readline';
+const stdout = process.stdout;
+const stdin = process.stdin;
+const rl = readline.createInterface(process.stdin, process.stdout);
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
 export const write = async () => {
-    // Write your code here 
-};
+    const filePath = path.join(__dirname, 'files/fileToWrite.txt');
+    const input = rl.question('Enter text to write to file: ', (text) => {
+        fs.writeFile(filePath, text, (err) => {
+            if (err) {
+                console.log('Error writing to file', err.message);
+            } else {
+                console.log('Successfully wrote to file');
+            }
+        });
+    });
+    rl.on("line", function (line) {
+        if (line === "exit") {
+            rl.close();
+            stdout.write(`Good luck,\nexit with code: ${code}`);
+            process.exit(0);
+        }
+    }).on("close", function () {
+        process.exit(0);
+    });
+    stdin.on("data", (data) => {
+        fs.appendFile(filePath, data, (err) => {
+            if (err) throw err;
+        });
+    });
+    process.on("exit", (code) => {
+        rl.close();
+        stdout.write(`Good luck,\nexit with code: ${code}`);
+        process.exit();
+
+    });
+}
+write();
