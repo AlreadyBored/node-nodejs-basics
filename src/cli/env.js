8ec0b695e2df2e2c104d4e41e50ffb615c6c1030
Olena Honcharuk <gasmin07@gmail.com>
2022-06-05 23:41:47 +0300

    feat: add solution for cli.env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,14 @@
+import { env } from 'process';
+
 export const parseEnv = () => {
     // Write your code here 
+    const result = Object.keys(env).filter(key => key.startsWith('RSS_')).map(key => {
+        console.log(key + ' = ' + env[key]);
+        return {
+            key: key,
+            value: env[key]
+        };
+    });
 };
+
+parseEnv();
