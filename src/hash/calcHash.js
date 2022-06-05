5b2905abbdbf5ea19d9eca75c54e9eb82f192ee5
PAVEL KOROTEEV <pavelkoroteev@yandex-team.ru>
2022-06-05 18:29:35 +0300

    hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,16 @@
+import fs from 'fs/promises';
+import { createHash } from 'crypto';
+
 export const calculateHash = async () => {
-    // Write your code here 
+    // You should implement several functions in dedicated files
+    // calcHash.js - implement function that calculates SHA256 hash for file fileToCalculateHashFor.txt and return it as hex
+    const path = (p) => new URL(p, import.meta.url);
+
+    const file = await fs.readFile(path('./files/fileToCalculateHashFor.txt'), 'utf-8');
+
+    return createHash('SHA256').update(file).digest('hex');
 };
+
+calculateHash().then(hash => {
+    console.log(hash);
+});
