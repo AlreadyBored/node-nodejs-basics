34be8122b0b53a5c6813e8e8c80c6b8bf88d4c2a
webdev <dezzzh93@gmail.com>
2022-06-06 00:06:02 +0800

    feat: completed only worker.js in subtask "Worker Threads (src/wt)"

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -3,7 +3,8 @@ import { parentPort, workerData } from "worker_threads";
 // n should be received from main thread
 export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
-export const sendResult = () => {
+const sendResult = () => {
     // This function sends result of nthFibonacci computations to main thread
     parentPort.postMessage(nthFibonacci(workerData.num))
 };
+export default  sendResult();
