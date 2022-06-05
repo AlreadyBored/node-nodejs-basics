a43fad240ee844ee53869f952492b02573c4ece6
IrinaAfinogenova <52318667+IrinaAfinogenova@users.noreply.github.com>
2022-06-05 22:30:36 +0300

    calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,14 @@
-export const calculateHash = async () => {
-    // Write your code here 
+import crypto from 'crypto';
+import fs from 'fs/promises';
+
+export const calculateHash = async () => {  
+    const fileBuffer = await fs.readFile('src/hash/files/fileToCalculateHashFor.txt');
+    const hashSum = crypto.createHash('sha256');
+    hashSum.update(fileBuffer);
+    
+    const hex = hashSum.digest('hex');
+    
+    console.log(hex);
 };
+
+calculateHash();
