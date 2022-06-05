e6b88ae9c8c66b8d8bc369200447af04b26869a0
effgenij <effgenij@gmail.com>
2022-06-05 15:23:01 +0300

    solution for main.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,38 @@
+import { cpus } from "os";
+import { Worker, workerData } from "worker_threads";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, "/worker.js");
+
+const workerPromise = (number) =>
+  new Promise((resolve, reject) => {
+    const worker = new Worker(filePath, { workerData: number });
+    worker.on("message", (msg) => {
+      resolve({
+        status: "resolved",
+        data: msg,
+      });
+    });
+    worker.on("error", (_error) => {
+      reject({
+        status: "reject",
+        data: null,
+      });
+    });
+  });
+
 export const performCalculations = async () => {
-    // Write your code here
+  const workers = [];
+
+  for (let index = 0; index < cpus().length; index++) {
+    workers.push(workerPromise(index + 11));
+  }
+
+  const result = await Promise.all(workers);
+  console.log(result);
 };
+
+performCalculations();
