f172c9995fc300fb3325529d789f1de888806154
IrinaAfinogenova <52318667+IrinaAfinogenova@users.noreply.github.com>
2022-06-05 22:23:18 +0300

    parseArgs

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,11 @@
 export const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+
+    args.forEach((key, index) => {
+        if (key.includes('--')) {
+            console.log(`${key.slice(2)} is ${args[index + 1]}`)
+        }
+    });
 };
+
+parseArgs();
