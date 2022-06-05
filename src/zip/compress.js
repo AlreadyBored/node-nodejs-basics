3563ebb5659d130e80ecb43936c11d3742975ab8
webdev <dezzzh93@gmail.com>
2022-06-06 00:00:41 +0800

    feat: completed the task of the subtask "Zlib (src/zip)"

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,12 @@
-export const compress = async () => {
-    // Write your code here 
+import { createReadStream, createWriteStream } from "fs";
+import { createGzip } from "zlib";
+
+ const compress = async () => {
+    const gzip = createGzip();
+    const input = createReadStream('./files/fileToCompress.txt');
+    const output = createWriteStream('./files/compress.gz');
+
+    input.pipe(gzip).pipe(output);
 };
+
+export default compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,11 @@
-export const decompress = async () => {
-    // Write your code here 
+import { createReadStream, createWriteStream } from "fs";
+import { createUnzip } from "zlib";
+
+ const decompress = async () => {
+     const gzip = createUnzip();
+     const input = createReadStream('./files/compress.gz');
+     const output = createWriteStream('./files/decompress.txt');
+
+     input.pipe(gzip).pipe(output);
 };
+export default decompress();
