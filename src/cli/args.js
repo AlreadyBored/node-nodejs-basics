08d8cfe9413b3fd5271e6a4bbfa0affa6d639927
Muhammed Abdrahman <muhammed03amin.com>
2022-06-06 01:29:48 +0600

    feat: add solution for argv.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,10 @@
 export const parseArgs = () => {
-    // Write your code here 
+  let result = [];
+  const args = process.argv.slice(2);
+  for (let i = 0; i < args.length; i=i+2) {
+    result.push(args[i] + ' is ' + args[i+1])
+  }
+  console.log(result.join(', '))
 };
+
+parseArgs()
