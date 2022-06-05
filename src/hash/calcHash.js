522ab9ce7cb303ff59b2da177c4d864ddd54b608
Muhammed Abdrahman <muhammed03amin.com>
2022-06-06 01:49:14 +0600

    feat: add solution for calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,19 @@
+import path from 'path';
+import {fileURLToPath} from "url";
+import fs from 'fs';
+import crypto from 'crypto';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const file = path.join(__dirname, '/files/fileToCalculateHashFor.txt');
+    if (!fs.existsSync(file)) {
+      throw new Error('FS operation failed');
+    }
+
+    fs.readFile(file, 'utf8', (err, data) => {
+      console.log(crypto.createHash('SHA256').update(data).digest('hex'));
+    })
 };
+
+calculateHash()
