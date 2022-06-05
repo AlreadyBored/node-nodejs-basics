3c050c370e7c7f0182e55c4e6ef204b564528e3a
Maria White <redstone.mari@gmail.com>
2022-06-05 14:07:56 +0300

    feat: add anv function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,11 @@
 export const parseEnv = () => {
-    // Write your code here 
+    const result = [];
+    Object.keys(process.env).forEach(element => {
+        if (element.startsWith('RSS_')) {
+            result.push(element + '=' + process.env[element]);
+        }
+    });
+    console.log(result.join('; '));
 };
+
+await parseEnv();
