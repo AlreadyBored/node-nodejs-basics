e16bc9eb7f867bfa59f8ea1537721a2ec0230015
Aljaetc <daasvsvami@gmail.com>
2022-06-05 18:42:08 +0300

    fs_list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,10 @@
 export const list = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    const fs = await import("fs/promises");
+    const Filesnames = await fs.readdir("files");
+    console.log("fsh: ", Filesnames);
+  } catch (e) {
+    throw new Error("FS operation failed");
+  }
 };
