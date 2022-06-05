e399ad8053c2e4c2910c08ecbbda56242e2112bb
AntonFartovii <anton@rza.by>
2022-06-05 17:02:55 +0300

    feat: update create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,12 @@
+const path = require('path')
+const fs = require('fs')
+
 export const create = async () => {
-    // Write your code here 
-};
+    // Write your code here
+    fs.writeFile ( path.join ( __dirname, 'files', 'fresh.txt' ),
+        'I am fresh and young', (err) => {
+        if (err) {
+            console.log ( 'FS operation failed' )
+        }
+    } )
+}
