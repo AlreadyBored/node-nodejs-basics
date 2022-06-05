88b56282b703e49db01703fab55ab444c816c09c
Ivan <subscription3333@outlook.com>
2022-06-05 19:39:07 +0400

    Implemented file create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,38 @@
+/***
+ * Имплементация пользовательского класса Error
+ */
+class AppUserError extends Error {
+    constructor(_message, _code) {
+        super(_message); //конструктор предка
+        this.name = "AppUserError"; //переопределение имени типа
+        this.Code = _code || 0; //поле с кодом ошибки
+    }
+}
+
+import { open } from 'fs/promises';
+//const PATH_FILE = 'c:\\Learn\\node-nodejs-basics\\src\\fs\\files\\my-test.txt'; //путь к файлу
+const PATH_FILE = '.\\src\\fs\\files\\my-test.txt'; //путь к файлу
+const OPENING_MODE = 'w+'; //режим в котором будем открывать файл, при данном режиме при отсутствии файла он создается
+
+/**
+ * Функция создает файл в соответствии с переданным параметром
+ */
 export const create = async () => {
-    // Write your code here 
+    // Write your code here
+    try {
+        await open(PATH_FILE, OPENING_MODE); //создать файл
+        console.log(`Debugging information: `);
+            console.log(`     file created.Path - ${PATH_FILE}`);
+    } catch (error) {
+            console.log(error);
+                throw new AppUserError('Failed to create a file !', 1);       
+    }
 };
+/***************************СЕКЦИЯ RUN ДЛЯ ПРОВЕРКИ****************** */
+create();
+
+
+
+
+
+
