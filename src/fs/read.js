e96c7afc42d06855f986d47125bf74727ede330f
MINSK\Nikita_Pytalev <nikita_pytalev@epam.com>
2022-06-05 23:46:16 +0300

    implement read.js with promises

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,17 +1,16 @@
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import fs from 'fs';
+import fs from 'fs/promises';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const fileToReadPath = path.join(__dirname, 'files', 'fileToRead.txt');
 
 export const read = async () => {
-    fs.readFile(fileToReadPath, 'utf8', function(err, data) {
-        if (err) {
-            throw Error('FS operation failed');
-        }
-
+    try {
+        const data = await fs.readFile(fileToReadPath, { encoding: 'utf8'});
         console.log(data)
-      });
+    } catch (e) {
+        throw Error('FS operation failed');
+    } 
 };
