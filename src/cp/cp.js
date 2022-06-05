079232f3addeed5b60ff91f114923765c453c108
Egorius1979 <Egoriyvelikolepny1979@gmail.com>
2022-06-05 15:54:21 +0200

    feat: implement "src/cp" tasks

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,18 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { spawn } from 'child_process';
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  const filename = fileURLToPath(import.meta.url);
+  const childPath = join(dirname(filename), 'files/script.js');
+
+  const child = spawn('node', [childPath, ...args.slice(2)]);
+
+  child.stdout.on('data', (data) => {
+    console.log('From child:', data.toString());
+  });
+
+  process.stdin.pipe(child.stdin);
 };
+
+spawnChildProcess(process.argv);
