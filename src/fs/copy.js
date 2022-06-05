b125138c257e11b97171fc0bb194eac50404d98d
Muhammed Abdrahman <muhammed03amin.com>
2022-06-06 00:35:51 +0600

    feat: add solution of copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,18 @@
+import path from 'path';
+import {fileURLToPath} from 'url';
+import fs from 'fs'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export const copy = async () => {
-    // Write your code here 
+  const srcFolder = path.join(__dirname, '/files')
+  const targetFolder = path.join(__dirname, '/files_copy');
+
+  fs.mkdirSync(targetFolder)
+  let files = fs.readdirSync(srcFolder)
+  for (let file of files) {
+    fs.copyFileSync(path.join(srcFolder, file), path.join(targetFolder, file))
+  }
 };
+
+copy();
