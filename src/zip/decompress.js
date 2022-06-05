69ad057a91678b88a9e7aa9ae1782a65b548d5b8
Muhammed Abdrahman <muhammed03amin.com>
2022-06-06 02:40:23 +0600

    feat: add solution for decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,21 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { createUnzip } from 'zlib';
+import fs from 'fs';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export const decompress = async () => {
-    // Write your code here 
+  const readFile = path.join(__dirname, '/files/archive.gz');
+  const writeFile = path.join(__dirname, '/files/fileToCompress.txt');
+
+  const readStream = fs.createReadStream(readFile);
+  const writeStream = fs.createWriteStream(writeFile);
+
+  const unzip = createUnzip();
+
+  readStream.pipe(unzip).pipe(writeStream);
+
 };
+
+decompress();
