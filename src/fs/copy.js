92d3eb666c13170d4de83f2dbee63bf8350cce0c
Aljaetc <daasvsvami@gmail.com>
2022-06-05 21:07:46 +0300

    fs_copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -2,17 +2,31 @@ export const copy = async () => {
   // Write your code here
   try {
     const fs = await import("fs/promises");
-    // await fs.cp("files", "filesCopy");
-    await fs.mkdir("files-copy");
-    const fileHandle = await fs.open("files", "r");
-    // const fhs = await fileHandle.stat();
 
-    // console.log("fh", fhs.isDirectory());
-    console.log("fh: ", fileHandle);
-    fileHandle.close();
+    const SOURCE_DIRECTORY = "files";
+    const DEST_DIRECTORY = "files_copy";
+
+    async function copier(names, path) {
+      const currentDestDirectory =
+        DEST_DIRECTORY + path.slice(SOURCE_DIRECTORY.length);
+      await fs.mkdir(currentDestDirectory);
+      names.forEach(async (name) => {
+        const ItemPath = path + "/" + name;
+        const itemStat = await fs.stat(ItemPath);
+        if (itemStat.isDirectory()) {
+          copier(await fs.readdir(ItemPath), ItemPath);
+        } else if (itemStat.isFile()) {
+          await fs.writeFile(
+            currentDestDirectory + name,
+            await fs.readFile(ItemPath)
+          );
+        }
+      });
+    }
+
+    const Filenames = await fs.readdir(SOURCE_DIRECTORY);
+    copier(Filenames, SOURCE_DIRECTORY);
   } catch (e) {
     console.log("mYeRror: ", e.message);
   }
 };
-
-copy();
