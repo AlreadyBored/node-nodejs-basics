7602a0d02ec336105c535836387d68d5564ccba9
IrinaAfinogenova <52318667+IrinaAfinogenova@users.noreply.github.com>
2022-06-05 22:31:23 +0300

    modules refactor

--- src/modules/cjsToEsm.cjs
+++ src/modules/cjsToEsm.cjs
@@ -1,31 +1,26 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path from 'path';
+import {release, version} from 'os';
+import {fileURLToPath} from 'url';
+import http from 'http';
+import './files/c.js';
+
+import a from './files/a.json' assert {type: 'json'};
+import b from './files/b.json' assert {type: 'json'};
 
 const random = Math.random();
-
-let unknownObject;
-
-if (random > 0.5) {
-    unknownObject = require('./files/a.json');
-} else {
-    unknownObject = require('./files/b.json');
-}
 
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
 console.log(`Path segment separator is "${path.sep}"`);
 
+const __filename = fileURLToPath(import.meta.url); 
+const __dirname = path.dirname(__filename);
+
 console.log(`Path to current file is ${__filename}`);
 console.log(`Path to current directory is ${__dirname}`);
 
-const createMyServer = createServerHttp((_, res) => {
+export const createMyServer = http.createServer((_, res) => {
     res.end('Request accepted');
 });
 
-module.exports = {
-    unknownObject,
-    createMyServer,
-};
-
+export const unknownObject = random > 0.5 ? a : b;
