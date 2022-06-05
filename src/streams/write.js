3de40c195a2f29e0d9ea63c2885dec4c0c37a3e5
IrinaAfinogenova <52318667+IrinaAfinogenova@users.noreply.github.com>
2022-06-05 22:32:56 +0300

    write stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,18 @@
+import fs from 'fs';
+import readline from 'readline';
+
 export const write = async () => {
-    // Write your code here 
+    const writeStream = fs.createWriteStream('src/streams/files/fileToWrite.txt')
+
+    var rl = readline.createInterface({
+        input: process.stdin,
+        output: writeStream,
+        terminal: false
+    });
+  
+    rl.on('line', (result) => {
+        writeStream.write(result);
+    });
 };
+
+write();
