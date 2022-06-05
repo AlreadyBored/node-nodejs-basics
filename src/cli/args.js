6edf741e8d530a474eaf2b6e8dcbd091908305a0
DavydVeremchuk <davyd.v@code-care.pro>
2022-06-05 12:40:40 +0300

    feat: add cli solutions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,12 @@
 export const parseArgs = () => {
-    // Write your code here 
+  // Write your code here
+  let result = "";
+  process.argv.forEach((item, index, array) => {
+    if (item.match(/^(?:--).+/g)) {
+      result += `${item} is ${array[index + 1]}; `;
+    }
+  });
+  console.log(result.slice(0, result.length - 2));
 };
+
+parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,11 @@
 export const parseEnv = () => {
-    // Write your code here 
+  // Write your code here
+  const envVars = process.env;
+  for (const varName in envVars) {
+    if (varName.match(/^(?:RSS_).+/g)) {
+      console.log(varName + "=" + envVars[varName]);
+    }
+  }
 };
+
+parseEnv();
