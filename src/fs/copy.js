69c4d843df59b42b0dc20c751d37760d64c33c57
pavello91 <pavlentianno27@gmail.com>
2022-06-05 21:30:53 +0300

    second commit

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,26 @@
+import { promises as fs } from 'fs'
 export const copy = async () => {
-    // Write your code here 
+   
+       let files= await fs.access("files");
+        if(files!=undefined){
+          console.log('Error:files folder not created')  
+          return
+        }
+        
+        try {
+            let files_copy = await fs.access("files_copy")
+                if(files_copy==undefined){
+                     console.log('Error:files_copy folder already created')
+                     return
+                  }
+        } catch {
+             fs.mkdir('files_copy')
+        } 
+
+
+
+       
+
 };
+
+copy()
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,52 +1,13 @@
 import { promises as fs } from 'fs'
-import { existsSync } from 'node:fs';
 import { constants } from 'fs';
 
-export const create = async() => {
-    // if (fs.access('files/fresh.txt')) {
-    //     console.log('sa')
-    // } else console.log('aa')
-
+export const create = async () => {
     try {
         await fs.access('files/fresh.txt', constants.F_OK);
-        console.log('file exists');
+        console.log('Error:FS operation failed');
     } catch {
         await fs.writeFile("files/fresh.txt", "I am fresh and young");
-    }
-
-
-
-    // fs.access('files/fresh.txt', (err) => {
-    //     if (!err) {
-    //         console.error('myfile already exists');
-    //         return;
-    //     }
-
-    // if (existsSync('files/fresh.txt')) {
-    //     console.log('Путь существует.');
-    // } else
-    //     fs.writeFile("files/fresh.txt", "I am fresh and young");
-    //console.log(await fs.access("files/fresh.txt"))
-    // try {
-    //     if (fs.access("files/fresh.txt")) {
-    //         fs.writeFile("files/fresh.txt", "I am fresh and young");
-    //     }
-
-    // } catch (err) {
-    //     console.error(err)
-    // }
-}
-
+    } 
+};
 
 create()
-
-
-// var fs = require("fs");
-
-// fs.unlink("filename.txt", function(err){
-//     if (err) {
-//         console.log(err);
-//     } else {
-//         console.log("Файл удалён");
-//     }
-// })
