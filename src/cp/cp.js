411b8f9ea073518f59b885d52e1759b8219ac284
Aleksey Shulyak <a.shulyak@itransition.com>
2022-06-06 00:46:06 +0400

    feature: nodejs basics cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,21 @@
+import { fork } from 'child_process';
+import path from "path";
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    const filePath = path.resolve(process.argv[1], '../files/script.js');
+
+    const child = fork(filePath, args, {
+        // uid: process.geteuid(),
+        // gid: process.getgid(),
+        // cwd: path.resolve(process.argv[1], '../files'),
+        stdio: [ process.stdin, process.stdout, process.stderr, 'ipc'],
+    });
+
+    child.on("close", function (code) {
+        console.log("child process exited with code " + code);
+    });
+
 };
+
+
+spawnChildProcess(process.argv);
