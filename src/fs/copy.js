626c5feb52f7cf894f5ecaa35a478ff642739f76
Muhammed Abdrahman <muhammed03amin.com>
2022-06-06 00:46:46 +0600

    feat: add err condition

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,12 +1,19 @@
 import path from 'path';
 import {fileURLToPath} from 'url';
-import fs from 'fs'
+import fs from 'fs';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export const copy = async () => {
   const srcFolder = path.join(__dirname, '/files')
   const targetFolder = path.join(__dirname, '/files_copy');
+
+  if (
+    !fs.existsSync(srcFolder)
+    || fs.existsSync(targetFolder)
+  ) {
+    throw new Error('FS operation failed')
+  }
 
   fs.mkdirSync(targetFolder)
   let files = fs.readdirSync(srcFolder)
