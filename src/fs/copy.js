2ab6d18c9ad902a404bd2c3f1a740610ecd4b6b2
Maria White <redstone.mari@gmail.com>
2022-06-05 15:22:57 +0300

    feat: add copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,32 @@
+import * as fs from 'fs';
+
 export const copy = async () => {
-    // Write your code here 
+    try {
+        fs.access('./src/fs/files', (err) => {
+            if (err) {
+                throw new Error('FS operation failed');
+            } else {
+                fs.access('./src/fs/files_copy', (err) => {
+                    if (!err) {
+                        throw new Error('FS operation failed');
+                    } else {
+                        fs.mkdir('./src/fs/files_copy', (err) => {
+                            if (err) {
+                                throw err;
+                            }
+                        });
+                        fs.cp('src/fs/files', 'src/fs/files_copy', {recursive: true}, (err) => {
+                            if (err) {
+                                throw err;
+                            }
+                        });
+                    }
+                });
+            }
+        });        
+    } catch (e) {
+        console.log(e);
+    }
 };
+
+await copy();
