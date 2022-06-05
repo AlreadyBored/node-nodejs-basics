ca82b124642ee82d457f078ae2d39489f9c809ba
PAVEL KOROTEEV <pavelkoroteev@yandex-team.ru>
2022-06-05 21:08:14 +0300

    implement archiver

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,32 @@
+import fs from 'fs';
+import { pipeline, Transform } from 'stream';
+import zlib from 'zlib';
+
 export const compress = async () => {
-    // Write your code here 
+    // compress.js - implement function that compresses file fileToCompress.txt 
+    // to archive.gz using zlib and Streams API
+
+    const readable = await fs.createReadStream(new URL('./files/fileToCompress.txt', import.meta.url));
+    const writeable = await fs.createWriteStream(new URL('./files/archive.gz', import.meta.url));
+
+    pipeline(
+        readable,
+        new Transform({
+            transform(chunk, encoding, callback) {
+                zlib.deflate(chunk, (e, buffer) => {
+                    callback(e, buffer);
+                })
+            }
+        }),
+        writeable,
+        (e) => {
+            if (e) {
+                console.error('pipeline error', e);
+            } else {
+                console.log('pipeline successfull');
+            }
+        }
+    )
 };
+
+compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,32 @@
+import fs from 'fs';
+import { pipeline, Transform } from 'stream';
+import zlib from 'zlib';
+
 export const decompress = async () => {
-    // Write your code here 
+    // decompress.js - implement function that decompresses archive.gz
+    // back to the fileToCompress.txt with same content as before compression using zlib and Streams API
+
+    const readable = await fs.createReadStream(new URL('./files/archive.gz', import.meta.url));
+    const writeable = await fs.createWriteStream(new URL('./files/fileToCompressTest.txt', import.meta.url));
+
+    pipeline(
+        readable,
+        new Transform({
+            transform(chunk, encoding, callback) {
+                zlib.inflate(chunk, (e, buffer) => {
+                    callback(e, buffer);
+                })
+            }
+        }),
+        writeable,
+        (e) => {
+            if (e) {
+                console.error('pipeline error', e);
+            } else {
+                console.log('pipeline successfull');
+            }
+        }
+    )
 };
+
+decompress();
