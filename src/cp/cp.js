0989c11ccc43b7e857bc115375ce287b382bc60a
shiroe66 <shiroeqky@gmail.com>
2022-06-05 22:10:43 +0500

    feat: fork method

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,21 @@
+import { fork } from "child_process"
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+  const child = fork("./src/cp/files/script.js", args, { silent: true })
+
+  process.stdin.pipe(child.stdin)
+
+  child.stdout.on("data", (data) => {
+    console.log(data.toString())
+  })
+
+  child.stderr.on("data", (data) => {
+    console.log(data.toString())
+  })
+
+  child.on("close", (code) => {
+    console.log(code)
+  })
+}
+
+spawnChildProcess(process.argv.slice(2))
