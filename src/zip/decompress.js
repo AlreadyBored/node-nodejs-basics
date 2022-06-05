aede371883e29b851e9e7557aef88ff1cee013a1
NatalliaK <studina-natali@yandex.by>
2022-06-05 22:37:51 +0300

    feat: complete zip tasks

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,24 @@
+import { fileURLToPath } from 'node:url';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream';
+import { createUnzip } from 'node:zlib';
+
 export const decompress = async () => {
-    // Write your code here 
+    const fileInputPath = await fileURLToPath(new URL('./files/archive.gz', import.meta.url));
+    const fileOutputPath = await fileURLToPath(new URL('./files/fileToCompress.txt', import.meta.url));
+
+    const readableStream = await createReadStream(fileInputPath);
+    const writableStream = await createWriteStream(fileOutputPath);
+    const zip = createUnzip();
+
+    pipeline(readableStream, zip, writableStream,
+        (err) => {
+            if (err) {
+                console.error('Pipeline failed.', err);
+            } else {
+                console.log('Pipeline succeeded.');
+            }
+        });
 };
+
+decompress();
