82c358e7a9ce800e0803cafc89f1ba08861bc585
Maksim Pevnev <drummen11@gmail.com>
2022-06-05 17:07:19 +0400

    develop: add hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,24 @@
+import { createHash } from 'crypto';
+import { access, constants, readFile } from 'fs';
+
+const ENCODING_TYPE = 'utf8';
+const FILE_TO_READ = `./files/fileToCalculateHashFor.txt`;
+const FS_OPERATION_ERROR = 'FS operation failed';
+
 export const calculateHash = async () => {
-    // Write your code here 
+    access(FILE_TO_READ, constants.F_OK, (err) => {
+        if (err !== null) {
+            throw new Error(FS_OPERATION_ERROR);
+        }
+
+        readFile(FILE_TO_READ, ENCODING_TYPE, (error, data) => {
+            if (error) {
+                console.error(error)
+            }
+
+            console.log(createHash('sha256').update(data).digest('hex'));
+        })
+    });
 };
+
+calculateHash();
