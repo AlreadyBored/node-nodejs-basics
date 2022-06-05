3f6cba2ed9f82423b04579f0303bcb85a9f8a13f
Evgeniy <85104881+EVG777-prog@users.noreply.github.com>
2022-06-05 22:05:17 +0300

    feat: zip and hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,22 @@
-export const calculateHash = async () => {
-    // Write your code here 
+import crypto from 'crypto';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const calculateHash = async (file) => {
+  const filePath = path.join(__dirname, 'files', file);
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) throw err;
+    const hash = crypto.createHash('sha256');
+    hash.update(data);
+    const hex = hash.digest('hex');
+    console.log(hex);
+  });
+  
 };
+
+calculateHash('fileToCalculateHashFor.txt');
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,27 @@
-export const decompress = async () => {
-    // Write your code here 
+import fs from 'fs';
+import path from 'path';
+import zlib from 'zlib';
+import { pipeline } from 'stream';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const decompress = async (fileArchived, file) => {
+  const filePath = path.join(__dirname, 'files', file);
+  const fileArchivedPath = path.join(__dirname, 'files', fileArchived);
+
+  const gzip = zlib.createGunzip();
+
+  const source = fs.createReadStream(fileArchivedPath);
+  const destination = fs.createWriteStream(filePath);
+
+  pipeline(source, gzip, destination, (err) => {
+    if (err) {
+      console.error('An error occurred:', err);
+      process.exitCode = 1;
+    }
+  });
 };
+
+decompress('archive.gz', 'fileToCompress.txt');
