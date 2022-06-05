79a4d64b66771ee03e7f8a51f1e17563152ee74f
SlizerS <zerelaw@bk.ru>
2022-06-05 22:27:40 +0300

    feat: wt tasks done

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,25 @@
+import {Worker} from 'worker_threads';
+import os from "os";
+
+function runService(workerData) {
+    return new Promise((resolve, reject) => {
+      const worker = new Worker('./src/wt/worker.js', { workerData });
+      worker.on('message', resolve);
+      worker.on('error', reject);
+      worker.on('exit', (code) => {
+        if (code !== 0)
+          reject(new Error(`Worker stopped with exit code ${code}`));
+      })
+    })
+  }
+
 export const performCalculations = async () => {
-    // Write your code here
+    const arr = [];
+    for (let i = 0, counter = 10; i <= os.cpus().length; i++,counter++) {
+      const result = await runService(counter);
+      arr.push(result);
+    }
+    console.log(arr)
+    return arr;
 };
+performCalculations();
