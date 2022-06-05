2e0b02557b00b03ec0ff9a3ddefed07d5ccbc1d4
NatalliaK <studina-natali@yandex.by>
2022-06-05 22:15:23 +0300

    feat: complete streams tasks

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,16 @@
+import { stdin, stdout } from 'node:process';
+import { Transform } from 'node:stream';
+
 export const transform = async () => {
-    // Write your code here 
+    const readableStream = stdin;
+    const writableStream = stdout;
+    const transformStream = new Transform({
+        transform(chunk, encoding, callback) {
+            callback(null, chunk.toString().split('').reverse().join(''));
+        }
+    });
+
+    readableStream.pipe(transformStream).pipe(writableStream);
 };
+
+transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,15 @@
+import { createWriteStream } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import { stdin } from 'node:process';
+
 export const write = async () => {
-    // Write your code here 
+    const filePath = await fileURLToPath(new URL('./files/fileToWrite.txt', import.meta.url));
+    const readableStream = stdin;
+    const writableStream = await createWriteStream(filePath, 'utf-8');
+
+    readableStream.on('data', function(chunk) {
+        writableStream.write(chunk);
+    });
 };
+
+write();
