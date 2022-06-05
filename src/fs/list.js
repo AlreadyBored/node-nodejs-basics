f6cd79c90f2ddee6784761931b5ad40f8abf6365
IrinaAfinogenova <52318667+IrinaAfinogenova@users.noreply.github.com>
2022-06-05 22:28:26 +0300

    list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,9 @@
+import {mkdir, readdir, copyFile} from 'fs/promises';
+import {checkFileExists} from './utils.js';
+
 export const list = async () => {
-    // Write your code here 
+    const files = await readdir('src/fs/files');
+    console.log(files);
 };
+
+list().catch(() => console.log('files does not exist or it is not directory'));
