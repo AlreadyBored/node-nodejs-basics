001306a8e453962ae60d0d3b65c9510501499491
ivanlasov <lyashov@nwtn-group.com>
2022-06-06 21:19:40 +0300

    fet: add write function (stream)

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,9 @@
+import path from 'path';
+import fs from 'fs';
+
 export const write = async () => {
-    // Write your code here 
+    const output =  fs.createWriteStream(path.join(path.resolve(), 'files', 'fileToWrite.txt'), 'utf-8');
+    let text = '';
+    process.stdin.on('data', (data) => output.write(data.toString()));
+
 };
