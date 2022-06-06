cf3c3798674d24e40faaf7d846c5c83f082fc820
ivanlasov <lyashov@nwtn-group.com>
2022-06-06 09:58:59 +0300

    feat:add function list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,16 @@
+import { readdir } from 'fs/promises';
+import path from 'path';
+
 export const list = async () => {
-    // Write your code here 
+    const dirname = path.resolve();
+
+    const dirPath = path.join(dirname, 'files');
+
+    try{
+        const filesName = await readdir(dirPath);
+        console.log(filesName);
+    }catch {
+        console.error('FS operation failed');
+    }
+
 };
