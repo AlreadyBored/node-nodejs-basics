6d8191b6452ed3bf7a4ff0ab62120f688f916ba8
SemenSavlook <semensavluk@gmail.com>
2022-06-06 15:29:37 +0600

    fix: funcs

--- src/cli/args.js
+++ src/cli/args.js
@@ -4,7 +4,7 @@ export const parseArgs = () => {
     console.error('Expected argument(-s)!');
     process.exit(1);
   }
-
+  
   const args = process.argv.slice(2).map((el) => el = el.replace('--',''));
   console.log(`${args[0]} is ${args[1]}, ${args[2]} is ${args[3]}`);
 
