2902febda4893bfb20bbc86f4f9581b50c7f56b5
ivanlasov <lyashov@nwtn-group.com>
2022-06-06 10:05:12 +0300

    feat:add function read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,18 @@
+import { readFile } from 'fs/promises';
+import path from 'path';
+
 export const read = async () => {
-    // Write your code here 
+    const dirname = path.resolve();
+
+    const filePath = path.join(dirname, 'files', 'fileToRead.txt');
+
+    try {
+        const fileData = await readFile(filePath);
+        console.log(fileData.toString());
+    }catch {
+        console.error('FS operation failed');
+    }
+
 };
+
+read()
