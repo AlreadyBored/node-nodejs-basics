b1709a19ce3a83a2110052dfc86315520660d7b1
Maksim Shylau <shilaums@gmail.com>
2022-11-06 20:58:53 +0100

    feat: update templates

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -4,3 +4,5 @@ const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2
 const sendResult = () => {
     // This function sends result of nthFibonacci computations to main thread
 };
+
+sendResult();
