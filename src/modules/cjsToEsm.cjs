0699f2e40c29e84c81005c27e8f14e0870b86e32
Maksim Shylau <shilaums@gmail.com>
2022-11-06 20:58:53 +0100

    feat: update templates

--- src/modules/cjsToEsm.cjs
+++ src/modules/cjsToEsm.cjs
@@ -20,13 +20,18 @@ console.log(`Path segment separator is "${path.sep}"`);
 console.log(`Path to current file is ${__filename}`);
 console.log(`Path to current directory is ${__dirname}`);
 
-const createMyServer = createServerHttp((_, res) => {
+const myServer = createServerHttp((_, res) => {
     res.end('Request accepted');
 });
 
+const PORT = 3000;
+
 console.log(unknownObject);
 
-createMyServer();
+myServer.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+    console.log('To terminate it, use Ctrl+C combination');
+});
 
 module.exports = {
     unknownObject,
