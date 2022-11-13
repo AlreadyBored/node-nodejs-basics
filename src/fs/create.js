eccd2a5109183d86ffd8eda1992a0f518028f0b9
Alex Kochnev <madjok87@icloud.com>
2022-11-13 18:29:00 +0300

    feat: add create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 const create = async () => {
-    // Write your code here 
+    fs.writeFile(
+        path.join(__dirname, 'files', 'fresh.txt'),
+        'I am fresh and young',
+        (err) => {
+            if (err) throw err;
+            console.log('FS operation failed');
+        }
+    );
 };
 
 await create();
