2bf429755cd3809132fdf40c8cbdd589b8a7a2a7
Khalid <shkhalid95@mail.ru>
2022-11-21 07:30:14 +0300

    feat: implement "Command line interface(src/cli)"

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
 const parseArgs = () => {
-    // Write your code here 
+  const myArgs = process.argv.slice(2);
+  myArgs.forEach((arg, ind) => {
+    if (arg.startsWith("--")) {
+      console.log(`${arg.slice(2)} is ${myArgs[ind + 1]}`);
+    }
+  });
 };
-
+// node cli/args.js --agr1 1 --arg2 2 --arg3 'some'
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+  for (let [key, value] of Object.entries(process.env)) {
+    if (key.startsWith("RSS_")) {
+      console.log(value);
+    }
+  }
 };
-
+// RSS_FOO=111 RSS_BAR='BAR' node env.js to set up env variables
 parseEnv();
