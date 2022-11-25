6b2fba105ee1c837e9791cf0bd4038cd1df62941
Khalid <shkhalid95@mail.ru>
2022-11-25 15:57:20 +0300

    feat: implement zlib

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,30 @@
+import { createReadStream, createWriteStream } from "fs";
+import { createGzip } from "zlib";
+import { promisify } from "util";
+import { pipeline } from "stream";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const folder = path.join(__dirname, "files");
+
+const pipelineAsync = promisify(pipeline);
+
+const doZip = async (input, output) => {
+  const source = createReadStream(input);
+  const gzip = createGzip();
+  const destination = createWriteStream(output);
+  await pipelineAsync(source, gzip, destination);
+};
+
 const compress = async () => {
-    // Write your code here 
+  const source = path.join(folder, "fileToCompress.txt");
+  const destination = path.join(folder, "archive.gz");
+  await doZip(source, destination).catch((err) => {
+    console.error("An error occurred: ", err);
+    process.exitCode = 1;
+  });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,30 @@
+import { createReadStream, createWriteStream } from "fs";
+import { createGunzip } from "zlib";
+import { promisify } from "util";
+import { pipeline } from "stream";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const folder = path.join(__dirname, "files");
+
+const pipelineAsync = promisify(pipeline);
+
+const doUnZip = async (input, output) => {
+  const source = createReadStream(input);
+  const unzip = createGunzip();
+  const destination = createWriteStream(output);
+  await pipelineAsync(source, unzip, destination);
+};
+
 const decompress = async () => {
-    // Write your code here 
+  const source = path.join(folder, "archive.gz");
+  const destination = path.join(folder, "fileToCompress.txt");
+  await doUnZip(source, destination).catch((err) => {
+    console.error("An error occurred: ", err);
+    process.exitCode = 1;
+  });
 };
 
 await decompress();
