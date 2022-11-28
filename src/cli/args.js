330d903856668aca8d21552bd1a95447089130bf
Andrew Gerasimchik <gerasimchick@tut.by>
2022-11-28 23:34:53 +0200

    feat: add solution cli module

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
-};
+    let result = process.argv
+        .splice(2)
+        .reduce((acc, elem, i, array) => {
+            if (elem.startsWith('--')) {
+                return [...acc, `${elem.slice(2)} is ${array[++i]}`]
+            }
+            return acc
+        }, [])
+        .join(', ')
+    console.log(result)
+}
 
-parseArgs();
+parseArgs()
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
-};
+    const result = Object.entries(process.env)
+        .filter(([key, value]) => key.startsWith('RSS_'))
+        .map((elem) => elem.join('='))
+        .join('; ')
+    console.log(result)
+}
 
-parseEnv();
+parseEnv()
