1ee75ef78cefb9e6e552873b83ab821981173874
Anna Surahatava <anika3160@gmail.com>
2022-11-28 17:14:06 +0100

    feat: add create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,28 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const create = async () => {
     // Write your code here 
+    fs.readFile(
+        path.join(__dirname, 'files', 'fresh.txt'),
+        'utf-8',
+        (err, data) => {
+            if (err) {
+                fs.writeFile(
+                    path.join(__dirname, 'files', 'fresh.txt'),
+                    'I am fresh and young',
+                    (err) => {
+                        if (err) throw new Error('FS operation failed')
+                        console.log('File created')
+                    }
+                )
+            } else throw new Error('FS operation failed')
+        }
+    )
 };
 
 await create();
