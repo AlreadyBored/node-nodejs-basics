567e51c9f48f04c064b0463b32784750ac8c013a
Andrew Gerasimchik <gerasimchick@tut.by>
2022-11-29 00:00:25 +0200

    feat: add solution hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { createReadStream } from 'node:fs'
+import { stdout } from 'node:process'
 
-await calculateHash();
+const { createHash } = await import('node:crypto')
+
+const calculateHash = async () => {
+    const fileToHASH = join(
+        dirname(fileURLToPath(import.meta.url)),
+        'files',
+        'fileToCalculateHashFor.txt'
+    )
+
+    const hash = createHash('sha256')
+    const input = createReadStream(fileToHASH)
+    input.pipe(hash).setEncoding('hex').pipe(stdout)
+}
+
+await calculateHash()
