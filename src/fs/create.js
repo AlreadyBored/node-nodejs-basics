4007e7cf540086ae5d76d960700759aba8571bb6
Anna Surahatava <anika3160@gmail.com>
2022-11-28 23:33:02 +0100

    ref: add promisify

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { promisify } from 'util';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -9,21 +10,17 @@ const create = async () => {
     // Write your code here 
     const file = `${__dirname}/files/fresh.txt`;
 
-    fs.access(file, fs.constants.F_OK, (err) => {
-        if (err) {
-            // console.log('File does not exist')
-            fs.writeFile(
-                path.join(__dirname, 'files', 'fresh.txt'),
-                'I am fresh and young',
-                (err) => {
-                    if (err) throw err
-                    // console.log('File created')
-                }
-            )
-        } else {
-            throw new Error('FS operation failed')
-        }
-      });
+    const prAccess = promisify(fs.access);
+    const prWriteFile = promisify(fs.writeFile);
+
+    try {
+        await prAccess(file, fs.constants.F_OK);
+    }
+    catch (err) {
+        await prWriteFile(path.join(__dirname, 'files', 'fresh.txt'), 'I am fresh and young');
+        return; 
+    }
+    throw new Error('FS operation failed'); 
 };
 
 await create();
