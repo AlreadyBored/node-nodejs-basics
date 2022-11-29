5a9f75360911a29b3d9464ed0462d36473831589
Andrew Gerasimchik <gerasimchick@tut.by>
2022-11-29 22:41:31 +0200

    feat: create zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import { createGzip } from 'node:zlib'
+import { fileURLToPath } from 'node:url'
+import { dirname, join } from 'node:path'
+import { createReadStream, createWriteStream } from 'node:fs'
 const compress = async () => {
-    // Write your code here 
-};
+    const sourceFile = join(
+        dirname(fileURLToPath(import.meta.url)),
+        'files',
+        'fileToCompress.txt'
+    )
+    const zipFile = join(
+        dirname(fileURLToPath(import.meta.url)),
+        'files',
+        'archive.gz'
+    )
+    const sourceStream = createReadStream(sourceFile)
+    const zipStream = createWriteStream(zipFile)
+    sourceStream.pipe(createGzip()).pipe(zipStream)
+}
 
-await compress();
+await compress()
