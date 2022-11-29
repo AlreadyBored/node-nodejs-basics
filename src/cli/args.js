a455f1fe40cba1515a09c1101d49932a7019ff4e
Anna Surahatava <anika3160@gmail.com>
2022-11-29 14:29:03 +0100

    feat: add parse arguments function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
     // Write your code here 
+    const result = process.argv.reduce((acc, cur, index) => {
+        if (cur.indexOf('--') === 0 && process.argv?.[index+1]) {
+            return `${acc} ${cur} is ${process.argv[index+1]},`;
+        }
+        return acc
+    },
+    '')
+    console.log(result.slice(1, -1))
 };
 
 parseArgs();
