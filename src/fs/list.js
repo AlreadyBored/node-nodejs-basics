c4cd0d21396147cfa5cb94721bd63a62122b8b56
Anna Surahatava <anika3160@gmail.com>
2022-11-29 13:14:19 +0100

    feat: add list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { promisify } from 'util';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const list = async () => {
     // Write your code here 
+    const pathToDir = `${__dirname}/files/`;
+    const prReaddir = promisify(fs.readdir);
+
+    try {
+        const files = await prReaddir(pathToDir, {encoding: 'utf-8'});
+        console.log(files);
+    }
+    catch(err) {
+        throw new Error('FS operation failed')
+    }
 };
 
 await list();
