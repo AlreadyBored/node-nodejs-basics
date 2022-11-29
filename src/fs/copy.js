3d8d5e6f15f0f4986f7886f0a7269736ed8e57e3
Anna Surahatava <anika3160@gmail.com>
2022-11-29 12:46:20 +0100

    ref: delete comments

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,9 +1,3 @@
-/*
-copy.js - implement function that copies folder files files with all its content into folder files_copy
- at the same level (if files folder doesn't exists or files_copy has already been created Error 
- with message FS operation failed must be thrown)
-*/
-
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
