9dc750a40656aac50445275178996d1efbdb8499
Anna Surahatava <anika3160@gmail.com>
2022-11-29 12:59:36 +0100

    feat: add rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { promisify } from 'util';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const rename = async () => {
     // Write your code here 
+    const originPath = `${__dirname}/files/wrongFilename.txt`;
+    const newPath = `${__dirname}/files/properFilename.md`;
+    const prRename = promisify(fs.rename);
+    
+    try {
+        await prRename(originPath, newPath)
+    }
+    catch (err) {
+        throw new Error('FS operation failed')
+    }
 };
 
 await rename();
