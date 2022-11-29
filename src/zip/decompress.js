e94dde22621acde47fbc6a4e315e3208482c5d03
Andrew Gerasimchik <gerasimchick@tut.by>
2022-11-30 01:01:43 +0200

    feat: add decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
-const decompress = async () => {
-    // Write your code here 
-};
+import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { createReadStream, createWriteStream } from 'node:fs'
+import { createGunzip } from 'node:zlib'
 
-await decompress();
+const decompress = async () => {
+    const decompressedFile = join(
+        dirname(fileURLToPath(import.meta.url)),
+        'files',
+        'fileToCompress.txt'
+    )
+    const compressedFile = join(
+        dirname(fileURLToPath(import.meta.url)),
+        'files',
+        'archive.gz'
+    )
+    const compressedStream = createReadStream(compressedFile)
+    const decompressedStream = createWriteStream(decompressedFile)
+    compressedStream.pipe(createGunzip()).pipe(decompressedStream)
+}
+
+await decompress()
