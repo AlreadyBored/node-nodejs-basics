8bc652a4a5b8b1b1a2bb9ee36d69c0d214019231
Bobur Abdullayev <changeitgo3@gmail.com>
2022-11-29 20:56:16 +0500

    feat: modules operation

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,11 +1,13 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path from 'path';
+import { release, version } from 'os';
+import { createServer as createServerHttp } from 'http';
+import './files/c.js';
+import { createRequire } from 'module';
 
 const random = Math.random();
 
 let unknownObject;
+const require = createRequire(import.meta.url);
 
 if (random > 0.5) {
     unknownObject = require('./files/a.json');
@@ -17,8 +19,8 @@ console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
 console.log(`Path segment separator is "${path.sep}"`);
 
-console.log(`Path to current file is ${__filename}`);
-console.log(`Path to current directory is ${__dirname}`);
+console.log(`Path to current file is ${new URL(import.meta.url)}`);
+console.log(`Path to current directory is ${new URL("../",import.meta.url)}`);
 
 const myServer = createServerHttp((_, res) => {
     res.end('Request accepted');
@@ -33,7 +35,7 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
+export default {
     unknownObject,
     myServer,
 };
