1b10fe49480a874b7cb232456352e1395f564413
Anna Surahatava <anika3160@gmail.com>
2022-11-29 15:44:17 +0100

    feat: add hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import { createHash } from 'node:crypto'
+import path from 'node:path';
+import fs from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import { promisify } from 'node:util';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const calculateHash = async () => {
     // Write your code here 
+    const prReadFile = promisify(fs.readFile);
+    try {
+        const data = await prReadFile(`${__dirname}/files/fileToCalculateHashFor.txt`, { encoding: 'utf-8'})
+        console.log(createHash('sha256').update(data).digest('hex'))
+    }
+    catch (err) {
+        throw err
+    }
 };
 
 await calculateHash();
