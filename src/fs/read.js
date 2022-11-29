c1a1e36c77bcf851293f28f1b3b7d97713107680
Anna Surahatava <anika3160@gmail.com>
2022-11-29 13:21:55 +0100

    feat: add read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { promisify } from 'util';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
     // Write your code here 
+    const pathToReadFile = `${__dirname}/files/fileToRead.txt`;
+    const prReadFile = promisify(fs.readFile);
+
+    try {
+        const data = await prReadFile(pathToReadFile, 'utf-8');
+        console.log(data);
+    }
+    catch(err) {
+        throw new Error('FS operation failed')
+    }
 };
 
 await read();
