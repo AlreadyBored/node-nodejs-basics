1e5586bc65514317c0ea31612efd74ac768b3aa8
Bobur Abdullayev <changeitgo3@gmail.com>
2022-11-29 21:10:50 +0500

    feat: hash operation

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,11 +1,13 @@
 const parseEnv = () => {
     // Write your code here
     const argument = process.argv
+    const consol = []
     for (let i = 0; i < argument.length; i++) {
         if(i > 1){
-            console.log(`${argument[i]} is ${argument[i += 1]}`);
-        }        
+            consol.push(`${argument[i]} is ${argument[i += 1]}`);
+        }
     }
+    console.log(consol.join(", "));
 };
 
 parseEnv();
--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,14 @@
+import {createHash} from "node:crypto"
+import {readFile} from "node:fs";
+import {fileURLToPath} from "node:url"
+
 const calculateHash = async () => {
     // Write your code here 
+    const pathToFile = fileURLToPath(new URL("./files/fileToCalculateHashFor.txt", import.meta.url))
+    await readFile(pathToFile, "utf-8", (err, data) => {
+        if(err) throw new Error("FS operation failed")
+        else console.log(createHash('sha256').update(data).digest('hex'));
+    })
 };
 
 await calculateHash();
