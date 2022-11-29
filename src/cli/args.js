40e443895150ea62378caf4f4610107125f141c4
Bobur Abdullayev <changeitgo3@gmail.com>
2022-11-29 20:33:22 +0500

    feat: cli operations

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+    // Write your code here
+    const variables = process.env;
+    for(let key in variables){
+        let keyVar = key
+        keyVar = "RSS__" + keyVar
+        console.log(keyVar + `=${variables[key]}`);
+    }
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+    // Write your code here
+    const argument = process.argv
+    for (let i = 0; i < argument.length; i++) {
+        if(i > 1){
+            console.log(`${argument[i]} is ${argument[i += 1]}`);
+        }        
+    }
 };
 
 parseEnv();
