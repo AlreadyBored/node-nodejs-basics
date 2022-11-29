15496cd78a85d9f0be5e719e43e3dc95326ea28c
Anna Surahatava <anika3160@gmail.com>
2022-11-29 13:09:13 +0100

    feat: add delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,21 @@
+import { unlink } from 'node:fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { promisify } from 'util';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const remove = async () => {
     // Write your code here 
+    const prUnlink = promisify(unlink);
+    const pathToDeleteFile = `${__dirname}/files/fileToRemove.txt`;
+    try {
+        await prUnlink(pathToDeleteFile)
+    }
+    catch (err) {
+        throw new Error('FS operation failed'); 
+    }
 };
 
 await remove();
