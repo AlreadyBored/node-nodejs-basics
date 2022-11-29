817f33c8b7b1dceefaa150f6d2d1f28345d3b918
Andrew Gerasimchik <gerasimchick@tut.by>
2022-11-29 20:37:38 +0200

    feat: create stream read and write

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
-const read = async () => {
-    // Write your code here 
-};
+import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { createReadStream } from 'node:fs'
+import { stdout } from 'node:process'
 
-await read();
+const read = async () => {
+    const fileToRead = join(
+        dirname(fileURLToPath(import.meta.url)),
+        'files',
+        'fileToRead.txt'
+    )
+    let readStream = createReadStream(fileToRead)
+    readStream.on('open', () => readStream.pipe(stdout))
+}
+
+await read()
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
-const write = async () => {
-    // Write your code here 
-};
+import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { stdin } from 'node:process'
+import { createWriteStream } from 'node:fs'
 
-await write();
+const write = async () => {
+    const fileToWrite = join(
+        dirname(fileURLToPath(import.meta.url)),
+        'files',
+        'fileToWrite.txt'
+    )
+    const streamToWrite = createWriteStream(fileToWrite)
+    stdin.pipe(streamToWrite)
+}
+
+await write()
