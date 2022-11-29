16f3992e8cc5dc0b8722e1ca823187bf6123aff7
Anna Surahatava <anika3160@gmail.com>
2022-11-29 14:14:40 +0100

    feat: add parseEnv function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
     // Write your code here 
+    const arrayOfEntries = Object.entries(process.env);
+    const result = arrayOfEntries.reduce((acc, cur) => {
+        if (cur[0].indexOf('RSS_') === 0) {
+            return `${acc} ${cur[0]}=${cur[1]};`;
+        }
+        return acc
+    },
+    '')
+    console.log(result.slice(1, -1))
 };
 
 parseEnv();
