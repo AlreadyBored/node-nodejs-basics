f07d1c639ce5d72b076b90b62a1407bd63786efa
sesychev <sergeysychou@gmail.com>
2022-11-30 09:35:10 +0300

    init: start tasks

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,31 @@
+const fs = require("fs");
+const path = require('path');
+
 const create = async () => {
+    let pathname = path.join(__dirname, "/fresh.txt"); // используйте path.join()
+    const stream = fs.createWriteStream("FILE.TXT");
     // Write your code here 
 };
 
 await create();
+
+/*
+const fs = require("fs");
+const path = require("path");
+const process = require("process");
+
+const readPath = path.join(__dirname, "/text.txt");
+
+let readable = fs.createReadStream(readPath, {
+  encoding: "utf-8",
+});
+
+readable.pipe(process.stdout); //вывод текста в консоль
+
+const wtitePath = path.join(__dirname, "/temp.txt");
+
+let writable = fs.createWriteStream(wtitePath, {
+  encoding: "utf-8",
+});
+
+readable.pipe(writable); // запись в файл из потока/файла
