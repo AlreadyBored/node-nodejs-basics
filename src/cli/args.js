f5f7843b669162730752ca6f9b25c042f3f7d89e
aNNiPAk <annipak@yandex.ru>
2022-12-01 00:12:34 +0300

    feat: task args.js solved

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+    const argv = process.argv.slice(2)
+
+    const cliArguments = argv.reduce(
+        (acc, val, index, arr) => {
+            if (val.startsWith('--')) {
+                acc.push(`${val.slice(2)} is ${arr[index + 1]}`)
+            }
+
+            return acc
+        }, [])
+
+    console.log(cliArguments.join(', '))
 };
 
 parseArgs();
