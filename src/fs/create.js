62d0c4d8bb09cd3d6b1fe82dc486f657c4c35ce4
Анастасия Рябчикова <a.ryabchikova@postgrespro.ru>
2022-12-01 01:07:12 +0300

    src/fs/create.js: create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,26 @@
-const create = async () => {
-    // Write your code here 
+import { appendFile, stat } from "node:fs/promises";
+
+const PATH = './src/fs/files/fresh.txt';
+
+const checkFileExist = async (path) => {
+  try {
+    const data = await stat(path);
+    return Boolean(data);
+  } catch (error) {
+    return false;
+  }
 };
 
-await create();
+const create = async () => {
+  try {
+    const isFileExist = await checkFileExist(PATH);
+    if (isFileExist) {
+      throw new Error('FS operation failed');
+    }
+    await appendFile(PATH, 'I am fresh and young');
+  } catch (error) {
+    console.error(error)
+  }
+};
+
+await create(); 
