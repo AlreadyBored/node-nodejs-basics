f2100b106ee362395d4354ef5211ae98e2d91337
staciekot <stacie.kot@gmail.com>
2022-11-30 01:00:30 +0100

    feat: implement function to calculate SHA256 hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,15 @@
+import { readFile } from "node:fs/promises";
+const { createHash } = await import("node:crypto");
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = new URL(
+    "./files/fileToCalculateHashFor.txt",
+    import.meta.url
+  );
+
+  const content = await readFile(filePath, { encoding: "utf8" });
+
+  console.log(createHash("sha256").update(content).digest("hex"));
 };
 
 await calculateHash();
