bc2cec2219e6f2b01267e7454e86d3224f84364d
aNNiPAk <annipak@yandex.ru>
2022-11-30 23:59:04 +0300

    feat: task env.js solved

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+    const variablesStartWithRSS = Object.entries(process.env).reduce(
+        (variables, [key, value]) => {
+            if (key.startsWith('RSS_')) {
+                variables.push(`${key}=${value}`)
+            }
+            return variables
+        }, [])
+    console.log(variablesStartWithRSS.join('; '))
 };
 
 parseEnv();
