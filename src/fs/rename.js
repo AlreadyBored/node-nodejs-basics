bc6142764fcc82cffc4f672fd86ff82c0328712a
aNNiPAk <annipak@yandex.ru>
2022-11-30 23:17:52 +0300

    feat: task rename.js solved

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,19 @@
+import { copyFile, constants, rm } from 'fs/promises'
+import { getCombinedPath } from '../pathHelper.js';
+
 const rename = async () => {
-    // Write your code here 
+    const oldFileName = 'wrongFilename.txt'
+    const newFileName = 'properFilename.md'
+
+    const oldPath = getCombinedPath(import.meta.url, 'files', oldFileName)
+    const newPath = getCombinedPath(import.meta.url, 'files', newFileName)
+
+    try {
+        await copyFile(oldPath, newPath, constants.COPYFILE_EXCL);
+        await rm(oldPath)
+    } catch {
+        throw Error('FS operation failed')
+    }
 };
 
 await rename();
