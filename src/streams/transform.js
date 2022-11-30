7d4d5d922a4446db744ecec2824e3f51bafb06a9
Anna Surahatava <anika3160@gmail.com>
2022-11-30 16:00:56 +0100

    feat: add transform stream

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,21 @@
+import process from 'node:process';
+import fs from 'node:fs';
+
+import { Transform } from 'stream';
+
+const reverseTr = new Transform({
+  transform(chunk, encoding, callback) {
+    this.push(chunk.toString().trim().split('').reverse().join(''));
+    callback();
+  }
+});
+
+
 const transform = async () => {
     // Write your code here 
+    process.stdin
+        .pipe(reverseTr)
+        .pipe(process.stdout)
 };
 
 await transform();
