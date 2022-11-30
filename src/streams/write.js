172604f938c841a1a3a17e48264fbcc10aaf2523
Anna Surahatava <anika3160@gmail.com>
2022-11-30 13:58:53 +0100

    feat: add write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,22 @@
+import path from 'node:path';
+import fs from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import { stdin } from 'node:process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const write = async () => {
     // Write your code here 
+    const writeStream = fs.createWriteStream(`${__dirname}/files/fileToWrite.txt`);
+
+    stdin.on('data', (data) => {
+        writeStream.write(data.toString().slice(0,-1));
+        process.exit();
+    })
+    stdin.on('error', (err) => {
+        throw (err);
+    })
 };
 
 await write();
