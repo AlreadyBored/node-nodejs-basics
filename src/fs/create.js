613e2e30c7f0c3f3e6c83607aca32d9dbfdcd286
lelek337 <ioan.af@mail.ru>
2022-11-30 13:09:30 +0300

    feate: added functionality to files create.js and delete.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,27 @@
+// const fs = require('fs');
+// const path = require('path');
+import fs from 'fs/promises';
+import { constants } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+    fs.access(path.join(__dirname, 'fresh.txt'), constants.F_OK).then(() => {
+        console.error('Error: FS operation failed');
+    }).catch(() => {
+        // console.error('file does not exists');
+        fs.writeFile(
+            path.join(__dirname, 'fresh.txt'),
+            'I am fresh and young',
+            (err) => {
+                if (err) throw err;
+                console.log('file create');
+            }
+        )
+    });
 };
 
 await create();
--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+    fs.unlink(
+        path.join(__dirname, 'fresh.txt'),
+        (err) => {
+            if (err) throw console.error('Error: FS operation failed');
+            console.log('file delete');
+        }
+    )
 };
 
 await remove();
