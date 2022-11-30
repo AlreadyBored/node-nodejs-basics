4424368d59157b6dc157f06557b69245d3aa366b
Vyacheslav <Smolensky-VO@rshbext.ru>
2022-11-30 23:12:41 +0300

    feat(fs): add create func

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import {access, appendFile} from "fs";
+import {fileURLToPath} from "url";
+import {createCorrectPath} from "../helper.js";
+
+
 const create = async () => {
-    // Write your code here 
+    const _filename = fileURLToPath(import.meta.url)
+    const filePath = createCorrectPath(_filename, 'files', 'fresh.txt');
+    const errorMessage = 'FS operation failed'
+
+    try {
+        await access(filePath, err => {
+            if (!err) {
+                throw Error(errorMessage)
+            }
+        });
+        await appendFile(filePath, 'I am fresh and young', (error) => {
+            if (error) throw Error(errorMessage)
+        });
+    } catch (error) {}
 };
 
 await create();
