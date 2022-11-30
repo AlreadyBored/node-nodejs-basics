8ec5f71ddd919f47394a381fdcb45937f5f25950
Наконечный Антон Викторович <anakonechny@alfabank.ru>
2022-11-30 11:41:58 +0300

    feat: implement cli functions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
+const getArgs = (args) =>
+    args.slice(2).reduce((acc, value, index, array) => {
+        if (value.slice(0, 2) !== "--") {
+            return acc;
+        }
+
+        return { ...acc, [value.slice(2)]: array[index + 1] };
+    }, {});
+
 const parseArgs = () => {
-    // Write your code here 
+    const args = Object.entries(getArgs(process.argv))
+        .map(([key, value]) => `${key} is ${value}`)
+        .join(", ");
+
+    console.log(args);
 };
 
 parseArgs();
