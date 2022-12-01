d077cfb88f161c5df9786ffb12d99bfb4a3d7bf3
Viktar Zanimonets <vzanimonets@gmail.com>
2022-12-01 19:40:00 +0300

    feat: implementation of function for env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+    const PREFIX = 'RSS_'
+    const output = []
+    for (const [key, value] of Object.entries(process.env)) {
+        if (!key.indexOf(PREFIX)) output.push(`${key}=${value}`)
+    }
+    console.log(output.join(';'))
 };
 
 parseEnv();
