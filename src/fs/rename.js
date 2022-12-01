eb586e634753a639f02579d146a0d74465afea6b
Анастасия Рябчикова <a.ryabchikova@postgrespro.ru>
2022-12-01 19:46:17 +0300

    src/fs/rename.js: create

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,19 @@
+import fsPromises from 'node:fs/promises';
+import { checkFileExist } from './utils.js';
+
+const PATH = './src/fs/files';
+const WRONG_PATH = PATH + '/wrongFilename.txt';
+const PROPER_PATH = PATH + '/properFilename.md';
+
 const rename = async () => {
-    // Write your code here 
+  const isWrongFileExist = await checkFileExist(WRONG_PATH);
+  const isProperFileExist = await checkFileExist(PROPER_PATH);
+
+  if (!isWrongFileExist || isProperFileExist) {
+    throw new Error('FS operation failed');
+  }
+
+  fsPromises.rename(WRONG_PATH, PROPER_PATH);
 };
 
 await rename();
