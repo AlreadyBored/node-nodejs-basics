d323bc6a0e5234d3585c4d40c084ee083db625f7
Viktar Zanimonets <vzanimonets@gmail.com>
2022-12-01 15:56:28 +0300

    feat: implementation of function for rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,40 @@
+import * as fs from 'node:fs/promises'
+import {fileURLToPath} from 'url'
+import path from 'path'
+
+const FILES_FOLDER = 'files'
+const W_FILE = 'wrongFilename.txt'
+const P_FILE = 'properFilename.md'
+const ERROR_MESSAGE = 'FS operation failed'
+
+
+const isExist = (path) => {
+    return fs.stat(path).then(fs => {
+        return fs.isFile()
+    }).catch(() => {
+        return false
+    })
+}
+
 const rename = async () => {
-    // Write your code here 
+    const filename = fileURLToPath(import.meta.url)
+    const __dirname = path.dirname(filename);
+    const wFile = path.join(__dirname, path.sep, FILES_FOLDER, path.sep, W_FILE)
+    const pFile = path.join(__dirname, path.sep, FILES_FOLDER, path.sep, P_FILE)
+
+    try {
+        const isExistW = await isExist(wFile)
+        const isExistP = await isExist(pFile)
+        if (isExistW && !isExistP) {
+            fs.rename(wFile, pFile, (err) => {
+                if (err) throw ERROR_MESSAGE
+            })
+        } else {
+            throw ERROR_MESSAGE
+        }
+    } catch (e) {
+        throw new Error(ERROR_MESSAGE)
+    }
 };
 
 await rename();
