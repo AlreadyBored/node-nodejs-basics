f61da0e40a8750aa633bf401f79fcc064acd92d8
Vyacheslav <Smolensky-VO@rshbext.ru>
2022-12-01 22:04:17 +0300

    feat(fs): add copy func

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,43 @@
+import {access, readdir, copyFile, mkdir} from 'fs';
+import {join} from 'path';
+import {fileURLToPath} from 'url';
+import {createCorrectPath} from "../helper.js";
+
 const copy = async () => {
-    // Write your code here 
+    const _filename = fileURLToPath(import.meta.url)
+    const filesFolder = createCorrectPath(_filename, 'files');
+    const targetFilesCopyFolder = createCorrectPath(_filename, 'files_copy');
+    const errorMessage = 'FS operation failed';
+    try {
+        await access(filesFolder, (error) => {
+            if (error) {
+                throw Error(errorMessage)
+            }
+        })
+        await access(targetFilesCopyFolder, (error) => {
+            if (!error) {
+                throw Error(errorMessage)
+            }
+        })
+        await readdir(filesFolder, (error, files) => {
+            if (error) {
+                console.log(errorMessage)
+            }
+            mkdir(targetFilesCopyFolder, (error) => {
+                if (error) {
+                    console.log(errorMessage)
+                }
+            })
+
+            files.forEach(file => {
+                copyFile(join(filesFolder, file), join(targetFilesCopyFolder, file), (error) => {
+                    if (error) {
+                        console.log(error)
+                    }
+                })
+            })
+        });
+    } catch (error) {}
 };
 
 copy();
