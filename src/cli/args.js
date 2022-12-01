21142aad4c2ce1d634eccc7059d2621e80d42ce2
Viktar Zanimonets <vzanimonets@gmail.com>
2022-12-01 20:40:46 +0300

    feat: implementation of function for args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+    const inputArgs = process.argv.slice(2)
+    const output = inputArgs.reduce((acc, value, i, arr) => {
+        const argVal = arr[i + 1]
+        if (value.startsWith('--')) {
+            const argument = value.slice(2)
+            acc.push(`${argument} is ${argVal}`)
+        }
+        return acc
+    }, [])
+    console.log(output.join())
 };
 
 parseArgs();
