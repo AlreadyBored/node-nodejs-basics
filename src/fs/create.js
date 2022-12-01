7626be5cd398fa4a36405d34b7f349f7e5a0a7ac
Viktar Zanimonets <vzanimonets@gmail.com>
2022-12-01 12:19:31 +0300

    feat: implement function for create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
-const create = async () => {
-    // Write your code here 
-};
+import {writeFile} from 'node:fs/promises'
+import {fileURLToPath} from 'url'
+import path from 'path'
 
-await create();
+const FILE_CONTENT = 'I am fresh and young'
+const ERROR_MESSAGE = 'FS operation failed'
+const FILE_NAME = 'fresh.txt'
+const FILE_FOLDER = 'files'
+
+const filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(filename);
+const filepath = path.join(__dirname, path.sep, FILE_FOLDER, FILE_NAME)
+
+const create = async () => {
+    try {
+        await writeFile(filepath, FILE_CONTENT, {flag: 'wx'})
+    } catch (e) {
+        throw new Error(ERROR_MESSAGE)
+    }
+}
+
+await create()
+
