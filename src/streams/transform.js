c42e6a471ab7df0dbf86d4bc9133e6016ec82616
aNNiPAk <annipak@yandex.ru>
2022-12-02 00:01:16 +0300

    feat: task streams/transform.js solved

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,27 @@
+import { pipeline, Transform } from 'stream'
+import { EOL } from 'os'
+
 const transform = async () => {
-    // Write your code here 
+    const bytesOfEOL = Buffer.from(EOL)
+    const lengthBytesOfEOL = bytesOfEOL.length
+
+    class ReverseStream extends Transform {
+        _transform(chunk, encoding, callback) {
+            const reverseChunk = chunk.reverse()
+            const clearReverseChunk = reverseChunk.slice(lengthBytesOfEOL)
+            const reverseChunkWithEOL = Buffer.concat([clearReverseChunk, bytesOfEOL])
+
+            try {
+                callback(null, reverseChunkWithEOL)
+            } catch (err) {
+                callback(err)
+            }
+        }
+    }
+
+    pipeline(process.stdin, new ReverseStream(), process.stdout, (err) => {
+        err && console.error(err);
+    })
 };
 
 await transform();
