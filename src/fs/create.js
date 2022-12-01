6fe96b33af1164f75be14f259a5306df1ce29318
Roman Panchenko <roman.p@aquimarketing.com>
2022-12-01 22:13:37 +0200

    feat: create file function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import { writeFile, access, constants } from 'node:fs/promises';
+const FILE_PATH = './files/fresh.txt';
+
 const create = async () => {
-    // Write your code here 
+    const string = 'I am fresh and young';
+
+    let isFileExist;
+
+    try {
+        await access(FILE_PATH, constants.R_OK);
+        isFileExist = true;
+    } catch {
+        isFileExist = false;
+    }
+
+    if (!isFileExist) {
+        return writeFile(FILE_PATH, string);
+    } else {
+        throw new Error('FS operation failed');
+    }
 };
 
 await create();
