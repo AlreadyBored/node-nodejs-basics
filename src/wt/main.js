5e244e8f320a1eb0894c9f723dc23cd46e7fb67d
Anna Surahatava <anika3160@gmail.com>
2022-12-01 14:56:25 +0100

    feat: add worker function

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,57 @@
+import { Worker, BroadcastChannel } from 'node:worker_threads';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+import { cpus } from 'node:os';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const performCalculations = async () => {
     // Write your code here
+    const numCPUs = cpus().length;
+    const dataFromWorkers = [];
+    const bc = new BroadcastChannel('chanel1');
+    let counter = 0;
+
+    const printResult = () => {
+        dataFromWorkers.sort((a, b) => {
+            if (a.id<b.id) return -1;
+            if (a.id>b.id) return 1;
+            return 0;
+        });
+        const result = dataFromWorkers.map(obj => ({
+            status: obj.status,
+            data: obj.data,
+        }));
+        console.log(result)
+        bc.close();
+    }
+
+    for (let n = 0; n < numCPUs; n++) {
+        const worker = new Worker(`${__dirname}/worker.js`, {
+            workerData: n + 10
+        });
+        worker.on('error', (err) => {
+            dataFromWorkers.push({
+                status: 'error',
+                data: null,
+                id: n + 10,
+            })
+            ++counter;
+            if (counter === numCPUs) printResult();
+        });
+    }
+
+    bc.onmessage = (event) => {
+        dataFromWorkers.push({
+            status: event.data.result ? 'resolved' : 'error',
+            data: event.data.result || null,
+            id: event.data.id,
+        })
+        ++counter;
+        if (counter === numCPUs) printResult();
+    };
+    
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,10 @@
+import { workerData, BroadcastChannel } from 'node:worker_threads';
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    const bc = new BroadcastChannel('chanel1');
+    bc.postMessage({id: workerData, result: nthFibonacci(workerData)});
 };
 
 sendResult();
