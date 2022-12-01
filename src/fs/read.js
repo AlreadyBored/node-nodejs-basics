77ec7e255e94340a756fb1b75de2abc9f87c1e6c
Vyacheslav <Smolensky-VO@rshbext.ru>
2022-12-02 00:22:33 +0300

    feat(fs): add read func

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import {access, readFile} from "fs";
+import {fileURLToPath} from "url";
+import {createCorrectPath} from "../helper.js";
+
 const read = async () => {
-    // Write your code here 
+    const _filename = fileURLToPath(import.meta.url)
+    const filePath = createCorrectPath(_filename, 'files', 'fileToRead.txt');
+    const errorMessage = 'FS operation failed'
+    try {
+        await access(filePath, (error) => {
+            if (error) throw Error(errorMessage);
+        })
+        await readFile(filePath, 'utf8', (error, data) => {
+            if (error) throw Error(errorMessage);
+            process.stdout.write(data);
+        })
+    } catch (error) {}
 };
 
 await read();
