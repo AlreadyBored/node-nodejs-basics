bd46fad8a450cc81674ced0cc3b159aa0eb105d8
Анастасия Рябчикова <a.ryabchikova@postgrespro.ru>
2022-12-01 20:24:45 +0300

    src/fs/read.js: create

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,14 @@
+import { readFile } from 'node:fs/promises';
+
+const PATH = './src/fs/files/fileToRead.txt';
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const contents = await readFile(PATH, { encoding: 'utf8' });
+    console.log(contents);
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await read();
