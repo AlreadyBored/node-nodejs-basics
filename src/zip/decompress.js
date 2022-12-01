9e6edffc6a24d9be8f207fee9b977225b216b51e
aNNiPAk <annipak@yandex.ru>
2022-12-02 00:23:06 +0300

    feat: task zip/decompress.js solved

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import { getCombinedPath } from '../pathHelper.js'
+import { createReadStream, createWriteStream } from 'fs'
+import { pipeline } from 'stream'
+import { createGunzip } from 'zlib'
+
 const decompress = async () => {
-    // Write your code here 
+    const pathToArchive = getCombinedPath(import.meta.url, 'files', 'archive.gz')
+    const pathToFile = getCombinedPath(import.meta.url, 'files', 'fileToCompress.txt')
+
+    const readableStream = createReadStream(pathToArchive)
+    const writableStream = createWriteStream(pathToFile)
+
+    pipeline(readableStream, createGunzip(), writableStream, err => {
+        err && console.error(err);
+    })
 };
 
 await decompress();
