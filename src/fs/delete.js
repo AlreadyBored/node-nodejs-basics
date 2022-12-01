9efc95ac98276b9909907fd7e0e2f1a9cf908695
Vyacheslav <Smolensky-VO@rshbext.ru>
2022-12-01 23:19:08 +0300

    feat(fs): add remove func

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import {access, rm} from "fs";
+import {createCorrectPath} from "../helper.js";
+import {fileURLToPath} from "url";
+
 const remove = async () => {
-    // Write your code here 
+    const _filename = fileURLToPath(import.meta.url)
+    const filePath = createCorrectPath(_filename, 'files', 'fileToRemove.txt');
+    const errorMessage = 'FS operation failed';
+
+    try {
+        await access(filePath, (error) => {
+            if (error) throw Error(errorMessage);
+        })
+        rm(filePath, (error) => {
+            if (error) throw Error(errorMessage);
+        })
+    } catch (error) {}
 };
 
 await remove();
