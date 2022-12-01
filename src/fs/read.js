d4585bfe3cd2f03bb0297edfc8103e199fa24ead
Viktar Zanimonets <vzanimonets@gmail.com>
2022-12-01 18:13:01 +0300

    feat: implementation of function for read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,22 @@
+import {readFile} from 'node:fs/promises'
+import {fileURLToPath} from 'url'
+import path from 'path'
+
+const ERROR_MESSAGE = 'FS operation failed'
+const FILE_NAME = 'fileToRead.txt'
+const FILE_FOLDER = 'files'
+
+const filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(filename);
+const filepath = path.join(__dirname, path.sep, FILE_FOLDER, FILE_NAME)
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const data = await readFile(filepath, {flag: 'r+'})
+        console.log(data.toString())
+    } catch (e) {
+        throw new Error(ERROR_MESSAGE)
+    }
 };
 
 await read();
