321017e1f91390db0a61a42a1cc76bcc13302b6a
Aibek <ai.bek@mail.ru>
2022-12-01 20:30:07 +0600

    feat: done create task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,16 @@
+import * as fsp from 'fs/promises'
+import * as fs from 'fs'
+
+
 const create = async () => {
-    // Write your code here 
+  try {
+    if (fs.existsSync("files/fresh.txt")) {
+      throw new Error("FS operation failed");
+    }
+    await fsp.writeFile("files/fresh.txt", "I am fresh and young");
+  } catch(e){
+    console.error(e)
+  }
 };
 
 await create();
