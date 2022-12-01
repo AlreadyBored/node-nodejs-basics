a8d00027e1fe74450b02c96a3998e9c41fb05b9a
Bobur Abdullayev <changeitgo3@gmail.com>
2022-12-01 19:31:16 +0500

    feat: stream operation succes

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,6 +1,24 @@
+import { createGzip } from "node:zlib";
+import { pipeline } from "node:stream";
+import { fileURLToPath } from "node:url";
+import { createReadStream, createWriteStream } from "node:fs";
+
 const compress = async () => {
-    // Write your code here
-    
+  // Write your code here
+  const pathToFile = fileURLToPath(
+    new URL("./files/fileToCompress.txt", import.meta.url)
+  );
+  const pathToZip = fileURLToPath(
+    new URL("./files/archive.txt.gz", import.meta.url)
+  );
+  const gzip = createGzip();
+  const src = createReadStream(pathToFile);
+  const zipFile = createWriteStream(pathToZip);
+  pipeline(src, gzip, zipFile, (err) => {
+    if (err) {
+        throw new Error("Operation is failed")
+    }
+  });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import { createUnzip } from "node:zlib";
+import { pipeline } from "node:stream";
+import { fileURLToPath } from "node:url";
+import { createReadStream, createWriteStream } from "node:fs";
+
 const decompress = async () => {
-    // Write your code here 
+  // Write your code here
+  const pathToFile = fileURLToPath(
+    new URL("./files/fileToCompress.txt", import.meta.url)
+  );
+  const pathToZip = fileURLToPath(
+    new URL("./files/archive.txt.gz", import.meta.url)
+  );
+  const unZip = createUnzip();
+  const src = createReadStream(pathToZip);
+  const zipFile = createWriteStream(pathToFile);
+  pipeline(src, unZip, zipFile, (err) => {
+    if (err) {
+      throw new Error("Operation is failed");
+    }
+  });
 };
 
 await decompress();
