6fb2aa566717b361d28e5571281896a507b8fc77
Vyacheslav <Smolensky-VO@rshbext.ru>
2022-12-02 00:10:30 +0300

    feat(fs): add list func

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import {access, readdir} from 'fs';
+import {fileURLToPath} from "url";
+import {createCorrectPath} from "../helper.js";
+
 const list = async () => {
-    // Write your code here 
+    const _filename = fileURLToPath(import.meta.url)
+    const filesDir = createCorrectPath(_filename, 'files');
+    const errorMessage = 'FS operation failed';
+
+    try {
+        await access(filesDir, (error) => {
+            if (error) throw Error(errorMessage)
+        })
+        await readdir(filesDir, (error, files) => {
+            if (error) throw Error(errorMessage)
+            console.log(files);
+        })
+    } catch (error) {}
 };
 
 await list();
