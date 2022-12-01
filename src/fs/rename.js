6f075f425c1d6daa34675b42c95322421879200c
Vyacheslav <Smolensky-VO@rshbext.ru>
2022-12-01 22:58:05 +0300

    feat(fs): add rename func

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,24 @@
+import {access, rename as fsRename} from 'fs'
+import {fileURLToPath} from "url";
+import {createCorrectPath} from "../helper.js";
+
 const rename = async () => {
-    // Write your code here 
+    const _filename = fileURLToPath(import.meta.url)
+    const filePath = createCorrectPath(_filename, 'files', 'wrongFilename.txt');
+    const propNameFile = createCorrectPath(_filename, 'files', 'properFilename.md');
+    const errorMessage = 'FS operation failed'
+
+    try {
+        await access(filePath, (error) => {
+            if (error) throw Error(errorMessage)
+        })
+        await access(propNameFile, (error) => {
+            if (!error) throw Error(errorMessage)
+        })
+        await fsRename(filePath, propNameFile, (error) => {
+            if (error) throw Error(errorMessage)
+        })
+    } catch (error) {}
 };
 
 await rename();
