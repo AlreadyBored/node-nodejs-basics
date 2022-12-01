7e960dcd55f6919ef7ba57fcf4eea58ec7d4d5b4
Анастасия Рябчикова <a.ryabchikova@postgrespro.ru>
2022-12-01 19:19:49 +0300

    src/fs/copy.js: create

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,23 @@
+import { cp } from 'node:fs/promises';
+import { checkFileExist } from './utils.js';
+
+const PATH = './src/fs';
+
 const copy = async () => {
-    // Write your code here 
+  const isFiLesCopyExist = await checkFileExist(`${PATH}/files_copy`);
+  const isFiLesExist = await checkFileExist(`${PATH}/files`);
+  if (isFiLesCopyExist || !isFiLesExist) {
+    throw new Error('FS operation failed');
+  }
+  cp(
+    `${PATH}/files`,
+    `${PATH}/files_copy`,
+    {
+      recursive: true,
+      errorOnExist: true,
+      force: false,
+    }
+  )
 };
 
 copy();
