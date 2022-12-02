8a4144e5db4397733c0f26c5714e798aa22122f6
ShotAlex <shot.aleks@gmail.com>
2022-12-02 23:11:29 +0300

    feat: add create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import fs  from "fs";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
 const create = async () => {
-    // Write your code here 
+    const pathFiles = path.dirname(fileURLToPath(import.meta.url)) + '/files/';
+
+    const fileName = pathFiles + 'fresh.txt';
+    const fileContent = 'I am fresh and young';
+
+    if (fs.existsSync(fileName)) {
+        console.log('FS operation failed');
+        throw 'FS operation failed';
+    }
+
+    fs.writeFile(fileName, fileContent, function(error){
+        if(error) throw 'FS operation failed';
+    });
 };
 
 await create();
