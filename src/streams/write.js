10cc0ad149cab3dfe87a1677c7abe47d45ea57f6
Viktar Zanimonets <vzanimonets@gmail.com>
2022-12-02 13:34:43 +0300

    feat: implementation of write function in stream/write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import {createWriteStream} from 'node:fs'
+import {fileURLToPath} from 'url'
+import path, {dirname} from 'path'
+
+const FOLDER_NAME = 'files'
+const FILE_NAME = 'fileToWrite.txt'
+const filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(filename);
+const src = path.join(__dirname, path.sep, FOLDER_NAME, path.sep, FILE_NAME)
+
+
 const write = async () => {
-    // Write your code here 
+    const writeStream = createWriteStream(src)
+    process.stdin.on('data', data => writeStream.write(data.toString()))
 };
 
 await write();
