682bcef61d41db7634888a65d509c0f8de76a679
Vyacheslav <Smolensky-VO@rshbext.ru>
2022-12-03 00:12:56 +0300

    refactor(module): refactor cjToEsm file

---
+++ src/modules/esm.mjs
@@ -0,0 +1,46 @@
+import {sep, dirname} from 'path'
+import {fileURLToPath} from 'url';
+import {release, version} from 'os';
+import {createServer as createServerHttp} from 'http';
+import aJson from './files/a.json' assert {type: 'json'};
+import bJson from './files/a.json' assert {type: 'json'};
+import './files/c.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const random = Math.random();
+
+let unknownObject;
+
+if (random > 0.5) {
+    unknownObject = aJson;
+} else {
+    unknownObject = bJson;
+}
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${sep}"`);
+
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
+
+const myServer = createServerHttp((_, res) => {
+    res.end('Request accepted');
+});
+
+const PORT = 3000;
+
+console.log(unknownObject);
+
+myServer.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+    console.log('To terminate it, use Ctrl+C combination');
+});
+
+export {
+    unknownObject,
+    myServer,
+};
+
