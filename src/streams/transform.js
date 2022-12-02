23994089064130dcf3f6fe86a5d2ca626dbd8034
Viktar Zanimonets <vzanimonets@gmail.com>
2022-12-02 16:57:48 +0300

    feat: implementation of write function in stream/transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,23 @@
+import {pipeline, Transform} from 'stream'
+import {EOL} from 'os'
+
 const transform = async () => {
-    // Write your code here 
+    const revert = new Transform({
+        transform(chunk, encoding, callback) {
+            const chunkContent = chunk.toString().replace(EOL, '')
+            const reversedChunk = chunkContent.split('').reverse().join('')
+            //
+            callback(null, reversedChunk + EOL)
+        }
+    })
+    pipeline(
+        process.stdin,
+        revert,
+        process.stdout,
+        (err) => {
+            throw err
+        }
+    )
 };
 
 await transform();
