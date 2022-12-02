4b548648ec47316ece9fd0837c101d037b599f4f
yuranius <yuranius.webber@gmail.com>
2022-12-02 21:16:53 +0300

    delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,13 @@
+import { rm } from 'fs/promises'
+
 const remove = async () => {
-    // Write your code here 
+    const deleteFile = 'src/fs/files/fileToRemove.txt';
+
+    try {
+        await rm(deleteFile)
+    } catch (err) {
+        throw new Error ('FS operation failed')
+    }
 };
 
 await remove();
