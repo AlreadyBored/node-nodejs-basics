fb947210f87c57b3b4ff3d83cd0eda7568bbfb28
Viktar Zanimonets <vzanimonets@gmail.com>
2022-12-02 12:18:06 +0300

    feat: implementation of hash function in calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,27 @@
+import {createReadStream} from 'node:fs'
+import path, {dirname} from 'path'
+import {fileURLToPath} from "url"
+
+const {createHash} = await import('node:crypto')
+
+const FILE_NAME = 'fileToCalculateHashFor.txt'
+const FILE_FOLDER = 'files'
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const filepath = path.join(__dirname, path.sep, FILE_FOLDER, FILE_NAME)
+
+    const hash = createHash('sha256')
+    const input = createReadStream(filepath)
+    input.on('readable', () => {
+        const data = input.read();
+        if (data)
+            hash.update(data);
+        else {
+            console.log(`${hash.digest('hex')} `);
+        }
+    });
 };
 
 await calculateHash();
