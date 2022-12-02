767130facdb904d188a008bead4ee7f477fb1229
yuranius <yuranius.webber@gmail.com>
2022-12-02 20:40:47 +0300

    copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,27 @@
+import fs  from 'fs';
+import { copyFile, mkdir } from 'fs/promises';
+
 const copy = async () => {
-    // Write your code here 
+    const newDir = 'src/fs/files_copy';
+    const dir = 'src/fs/files';
+
+    fs.readdir( dir, function ( err, files) {
+        if (err || !files.length) {
+            throw new Error ('FS operation failed');
+        } else {
+            ensureDirSync(newDir, files);
+        }
+    })
+
+    async function ensureDirSync (folder, files) {
+        try {
+            await mkdir(folder);
+            await Promise.all(files.map( file  => copyFile (dir + '/' + file, newDir + '/' + file)));
+            console.log('Success!');
+        } catch (err) {
+            throw new Error ('FS operation failed');
+        }
+    }   
 };
 
 copy();
