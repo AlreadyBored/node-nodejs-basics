0c607d37b896e73880ccecf7774989b35530a73a
Bobur Abdullayev <changeitgo3@gmail.com>
2022-12-02 11:25:10 +0500

    feat: child_procces succes

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,10 @@
+import {fork} from "node:child_process"
+import {fileURLToPath} from "node:url"
+
 const spawnChildProcess = async (args) => {
     // Write your code here
-    
+    const pathToFile = fileURLToPath(new URL("./files/script.js", import.meta.url))
+    fork(pathToFile, args)
 };
 
-spawnChildProcess();
+spawnChildProcess(process.argv);
