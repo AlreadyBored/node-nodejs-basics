90d1c386103936d057fbaf0a3dcebcd1ffdb6211
yuranius <yuranius.webber@gmail.com>
2022-12-02 21:50:36 +0300

    env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+    let arrRss = []
+
+    for (const [key, value] of Object.entries(process.env)) {
+        if (key.match(/RSS_\S*/ig)){
+            arrRss.push(`${key}=${value}`);
+        }
+    }
+
+    console.log(arrRss.join ('; '));
 };
 
 parseEnv();
