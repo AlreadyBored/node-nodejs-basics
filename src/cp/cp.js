59ed80b9a403802b4fbfbf175198b119b7763723
Anna Surahatava <anika3160@gmail.com>
2022-12-02 14:03:44 +0100

    feat: add child process function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,16 @@
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+import { spawn } from 'node:child_process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    // Write your code here 
+   const childProcess = spawn('node', [`${__dirname}/files/script.js`, args]);
+
+   childProcess.stdout.pipe(process.stdout);
+   process.stdin.pipe(childProcess.stdin);
 };
 
 spawnChildProcess();
