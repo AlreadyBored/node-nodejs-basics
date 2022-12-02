50c336f648d9c038ccda3328d28f5753f43d6634
Vyacheslav <Smolensky-VO@rshbext.ru>
2022-12-03 00:58:35 +0300

    feat(hash): add calcHash func

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,13 @@
+import {createHash} from 'crypto';
+import {fileURLToPath} from "url";
+import {createCorrectPath} from "../helper.js";
+
 const calculateHash = async () => {
-    // Write your code here 
+    const _filename = fileURLToPath(import.meta.url)
+    const file = createCorrectPath(_filename, 'files', 'fileToCalculateHashFor.txt');
+    const hash = createHash('sha256')
+    const hashedFile = hash.update(file).digest("hex");
+    console.log(hashedFile)
 };
 
 await calculateHash();
