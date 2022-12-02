dac3027187e48bb0ef7a0a7aed40c19301f56e56
Viktar Zanimonets <vzanimonets@gmail.com>
2022-12-02 13:24:27 +0300

    feat: implementation of read function in stream/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import {createReadStream} from 'node:fs'
+import {fileURLToPath} from 'url'
+import path, {dirname} from 'path'
+
+const FOLDER_NAME = 'files'
+const FILE_NAME = 'fileToRead.txt'
+const CODE_PAGE = 'utf8'
+const filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(filename);
+const src = path.join(__dirname, path.sep, FOLDER_NAME, path.sep, FILE_NAME)
+
 const read = async () => {
-    // Write your code here 
+    const readStream = createReadStream(src, CODE_PAGE)
+    readStream.on('data', data => process.stdout.write(data))
 };
 
 await read();
