689bd00703c627d3c4cbcedbc94d22a8e01b8edb
yuranius <yuranius.webber@gmail.com>
2022-12-02 18:43:53 +0300

    create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import fs  from 'fs';
+
+const DIR = 'src/fs/files/fresh.txt'
+
 const create = async () => {
     // Write your code here 
+    fs.readFile(DIR, (err) => {
+        if (err) {
+                fs.open(DIR, 'w', (err) => {
+                if (err) throw console.error(err);
+                    fs.appendFile(DIR, 'I am fresh and young', (err) => {
+                    if(err) throw err;
+                })
+            })
+        } else {
+        throw new Error ('FS operation failed');
+        }
+    })
 };
 
 await create();
