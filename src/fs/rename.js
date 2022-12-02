8ccccdcb2f80876ca68777c0b8b89b11c1b4fa1e
yuranius <yuranius.webber@gmail.com>
2022-12-02 21:09:36 +0300

    rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,14 @@
+import { rename as renaming } from 'fs/promises'
+
 const rename = async () => {
-    // Write your code here 
+    const oldName = 'src/fs/files/wrongFilename.txt';
+    const newName = 'src/fs/files/properFilename.md';
+
+    try {
+        await renaming( oldName, newName )
+    } catch (err) {
+        throw new Error ('FS operation failed')
+    }
 };
 
 await rename();
