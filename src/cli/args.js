ffba1450f91d4bdc31729fdd46342daedf846880
Anna Surahatava <anika3160@gmail.com>
2022-12-02 14:24:11 +0100

    fix: change print format

--- src/cli/args.js
+++ src/cli/args.js
@@ -2,7 +2,7 @@ const parseArgs = () => {
     // Write your code here 
     const result = process.argv.reduce((acc, cur, index) => {
         if (cur.indexOf('--') === 0 && process.argv?.[index+1]) {
-            return `${acc} ${cur} is ${process.argv[index+1]},`;
+            return `${acc} ${cur.slice(2)} is ${process.argv[index+1]},`;
         }
         return acc
     },
