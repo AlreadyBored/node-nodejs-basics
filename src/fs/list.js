85772c331e08a0868643bd9104cd121a6fd6ba5c
yuranius <yuranius.webber@gmail.com>
2022-12-02 21:21:46 +0300

    list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,15 @@
+import fs  from 'fs';
+
 const list = async () => {
-    // Write your code here 
+    const dir = 'src/fs/files';
+
+    fs.readdir( dir, function ( err, files) {
+        if (err || !files.length) {
+            throw new Error ('FS operation failed');
+        } else {
+            console.log(files);
+        }
+    }) 
 };
 
 await list();
