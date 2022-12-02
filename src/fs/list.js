04497f4099bd789e1de0002c3e9f2abe9354e97e
Roman Panchenko <roman.p@aquimarketing.com>
2022-12-02 20:19:42 +0200

    feat: read files names inside directory

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,13 @@
+import { readdir } from 'node:fs/promises';
+const DIRECTORY_PATH = './files';
+
 const list = async () => {
-    // Write your code here 
+    try {
+        const files = await readdir(DIRECTORY_PATH);
+        files.forEach((file) => console.log(file));
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await list();
