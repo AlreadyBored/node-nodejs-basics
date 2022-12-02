128333acb09dbc587f68eb43a25a38ae1f0d88f2
Anna Surahatava <anika3160@gmail.com>
2022-12-02 15:44:23 +0100

    feat: add a restriction on input values

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -4,13 +4,22 @@ import { spawn } from 'node:child_process';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
+/*
+cp.js - implement function spawnChildProcess
+that receives array of arguments args and creates child process
+from file script.js, passing these args to it.
+This function should create IPC-channel between
+stdin and stdout of master process and child process
+*/
 const spawnChildProcess = async (args) => {
     // Write your code here 
-   const childProcess = spawn('node', [`${__dirname}/files/script.js`, args]);
-
-   childProcess.stdout.pipe(process.stdout);
-   process.stdin.pipe(childProcess.stdin);
+    if (args?.length && typeof args !== 'string') {
+        const childProcess = spawn('node', [`${__dirname}/files/script.js`, ...args]);
+        childProcess.stdout.pipe(process.stdout);
+        process.stdin.pipe(childProcess.stdin);
+    } else {
+        throw new Error('Function spawnChildProcess expects to receive an array')
+    }
 };
 
-spawnChildProcess();
+spawnChildProcess([1,3,7]);
