28529f9719fc6db9c264511398924bf576edfd16
PalinaZinouyeva <bozkovapa@gmail.com>
2022-12-03 20:46:51 +0400

    cli-tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
+const { argv, stdout } = process;
+
 const parseArgs = () => {
-    // Write your code here 
+    const data = argv.slice(2);
+
+    const result = data.map((argument, index) => {
+        const value = index % 2 === 0 || index === 0 ? data[index + 1] : null;
+
+        return value ? `${argument} is ${value}` : null;
+    }).filter(value => !!value).join(',');
+
+    stdout.write(result + '\n');
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
+const { env, stdout } = process;
+
 const parseEnv = () => {
-    // Write your code here 
+    const envKeys = Object.keys(env);
+
+    const markedKeys = envKeys.filter(value => value.startsWith('RSS_')).map(key => {
+        return `${key}=${env[key]}`;
+    }).join(';');
+
+    stdout.write(markedKeys + '\n');
+    process.exit();
 };
 
 parseEnv();
