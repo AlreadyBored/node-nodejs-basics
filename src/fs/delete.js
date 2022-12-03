2c3b880e3de6ab39b8b5d0eb04468832a99568ca
ShotAlex <shot.aleks@gmail.com>
2022-12-03 20:42:41 +0300

    feat: add delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import fs  from "fs";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
 const remove = async () => {
-    // Write your code here 
-};
+    const currentPath = path.dirname(fileURLToPath(import.meta.url));
+    const deleteFilePath = currentPath + '/files/fileToRemove.txt';
+
+    if (!fs.existsSync(deleteFilePath)) {
+        throw 'FS operation failed';
+    } else {
+        fs.unlinkSync(deleteFilePath);
+    }
+}
 
 await remove();
