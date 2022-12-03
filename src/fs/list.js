bf419c3c0ae6ad468ca3d96ddc713d5d753ff129
ShotAlex <shot.aleks@gmail.com>
2022-12-03 20:49:13 +0300

    feat: add list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import fs  from "fs";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const list = async () => {
-    // Write your code here 
+    const currentPath = path.dirname(fileURLToPath(import.meta.url));
+    const folderPath = currentPath + '/files';
+
+    if (!fs.existsSync(folderPath)) {
+        console.log(1);
+        throw 'FS operation failed';
+    }
+
+    fs.readdir(folderPath, (err, files) => {
+        const arrFileNames = [];
+        files.forEach(file => arrFileNames.push(file));
+        console.log(arrFileNames);
+    });
 };
 
 await list();
