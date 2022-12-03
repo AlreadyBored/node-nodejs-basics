eb747a56ea971582dc05c0c091e9b33c3cdaeda3
Roman Panchenko <roman.p@aquimarketing.com>
2022-12-03 22:21:16 +0200

    feat: zip/unzip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
-const compress = async () => {
-    // Write your code here 
+import { createReadStream, createWriteStream } from 'node:fs';
+import zlib from 'node:zlib';
+import { isTargetAccessible } from "../fs/helpers/helperFunctions.js";
+
+const compress = async (fileToCompress, zipFileName) => {
+    const isFileExist = isTargetAccessible(fileToCompress);
+
+    if (isFileExist) {
+        const readStream = createReadStream(fileToCompress);
+        const writeStream = createWriteStream(zipFileName);
+        const gzip = zlib.createGzip();
+
+        readStream.pipe(gzip).pipe(writeStream);
+    } else {
+        throw new Error(`Target file doesn't exist`);
+    }
 };
 
-await compress();
+await compress('./files/fileToCompress.txt','./files/archive.gz');
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,26 @@
-const decompress = async () => {
-    // Write your code here 
+import { createReadStream, createWriteStream } from 'node:fs';
+import zlib from 'node:zlib';
+import { isTargetAccessible } from "../fs/helpers/helperFunctions.js";
+
+const decompress = async (fileToDecompress, unzipFileName) => {
+    const isArchiveExist = isTargetAccessible(fileToDecompress);
+    const isUnzipFileExist = isTargetAccessible(fileToDecompress);
+
+    const gunzip = zlib.createGunzip();
+
+    if (isArchiveExist && !isUnzipFileExist) {
+        const readStream = createReadStream(fileToDecompress);
+        const writeStream = createWriteStream(unzipFileName);
+
+        readStream.pipe(gunzip).pipe(writeStream);
+    } else if (isArchiveExist && isUnzipFileExist) {
+        const readStream = createReadStream(fileToDecompress);
+        const writeStream = createWriteStream(`${unzipFileName}_copy_${Date.now()}`);
+
+        readStream.pipe(gunzip).pipe(writeStream);
+    } else {
+        throw new Error('CAN NOT DECOMPRESS');
+    }
 };
 
-await decompress();
+await decompress('./files/archive.gz', './files/fileToCompress.txt');
