05eb23d7bc95d284b44d8d49b83ccd83e674f18c
yuranius <yuranius.webber@gmail.com>
2022-12-03 21:41:28 +0300

    worker-threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,29 @@
+import { cpus } from 'os';
+import { Worker } from 'worker_threads'
+
 const performCalculations = async () => {
-    // Write your code here
+
+    const workerFile = './src/wt/worker.js'
+
+    const cpu = cpus();
+
+    let number = 10;
+    
+    const arrResult = await Promise.allSettled( cpu.map ( (cpu, index) => {
+        return new Promise ((res, rej) => {
+            const worker = new Worker (workerFile, {
+                workerData: number + index
+            });
+
+            worker.on('message', msg => res(msg))
+            worker.on('error', msg => rej(msg))
+        })
+    }))
+
+    console.log(arrResult.map( ({ status, value }) => ({
+        status: status === 'fulfilled' ? 'resolved' : 'error',
+        data: status === 'fulfilled' ? value : null
+    })));
 };
 
 await performCalculations();
