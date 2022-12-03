d8715a3e39d77723ee6c4074c8c7dd3bbff7cc5f
yuranius <yuranius.webber@gmail.com>
2022-12-03 19:07:55 +0300

    zlib:compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import fs from 'fs'
+import { createGzip } from 'zlib'
+import { pipeline } from 'stream'
+
 const compress = async () => {
-    // Write your code here 
+    const src = 'src/zip/files/fileToCompress.txt';
+    const dest = 'src/zip/files/archive.gz ';
+
+    const gzip = createGzip();
+    const source = fs.createReadStream(src);
+    const destination = fs.createWriteStream(dest);
+
+    pipeline(source, gzip, destination, (err) => {
+        if (err) {
+            console.error('An error occurred:', err);
+            process.exitCode = 1;
+        }
+    });
 };
 
 await compress();
