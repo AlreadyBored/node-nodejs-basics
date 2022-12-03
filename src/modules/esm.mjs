2ff3abfcfdc0c6406363cdf0ec4929e517a1bfa4
Fiodar Savutsin <federikos@bk.ru>
2022-12-03 22:10:03 +0200

    fix: fix paths

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,4 +1,5 @@
-import path from "path";
+import { sep, dirname } from "path";
+import { fileURLToPath } from "url";
 import { release, version } from "os";
 import { createServer as createServerHttp } from "http";
 import "./files/c.js";
@@ -15,10 +16,12 @@ if (random > 0.5) {
 
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
-console.log(`Path segment separator is "${path.sep}"`);
+console.log(`Path segment separator is "${sep}"`);
 
-console.log(`Path to current file is ${import.meta.url}`);
-console.log(`Path to current directory is ${path.dirname(import.meta.url)}`);
+const filename = fileURLToPath(import.meta.url);
+
+console.log(`Path to current file is ${filename}`);
+console.log(`Path to current directory is ${dirname(filename)}`);
 
 const myServer = createServerHttp((_, res) => {
   res.end("Request accepted");
