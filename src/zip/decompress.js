25a953f23e24ffba90181d468f243849ce5016de
yuranius <yuranius.webber@gmail.com>
2022-12-03 19:23:54 +0300

    zlib:decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+import fs from 'fs'
+import { createUnzip } from 'zlib'
+import { pipeline } from 'stream'
+
 const decompress = async () => {
-    // Write your code here 
+    const dest = 'src/zip/files/fileToCompress.txt';
+    const src = 'src/zip/files/archive.gz ';
+
+    const unzip = createUnzip();
+    const source = fs.createReadStream(src);
+    const destination = fs.createWriteStream(dest);
+
+    pipeline(source, unzip, destination, (err) => {
+        if (err) {
+            console.error('An error occurred:', err);
+            process.exitCode = 1;
+        }
+    });
+
 };
 
 await decompress();
