1323649d8279db8c4cb5041686de04bb6265f3ee
vkordovich <vadim_kordovich@epam.com>
2022-12-03 23:33:40 +0300

    cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,22 @@
 const parseArgs = () => {
-    // Write your code here 
+    // Write your code here
+    const stringArgs = getArgs(process.argv);
+    console.log(stringArgs)
 };
+
+const getArgs = (args) => {
+    const resultArgs = [];
+    const [,, ...rest] = args;
+    rest.forEach((value, index, arr) => {
+        if(value.charAt(0) === '-' && value.charAt(1) === '-') {
+            if(arr[index + 1].charAt(0) !== '-') {
+                resultArgs.push(
+                    `${value.slice(2)} is ${arr[index + 1]}`
+                )
+            }
+        }
+    })
+    return resultArgs.join(', ');
+}
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,16 @@
 const parseEnv = () => {
-    // Write your code here 
+    // Write your code here
+    const stringEnv = getRSSEnv(process.env);
+    console.log(stringEnv)
 };
+
+const getRSSEnv = (env) => {
+    const resultEnv = [];
+    Object.keys(env)
+        .filter(key => key.match(/RSS/gi))
+        .forEach(key => resultEnv.push(`${key}=${env[key]}`));
+
+    return resultEnv.join('; ');
+}
 
 parseEnv();
