859cf1782a98ded08093c4d470e3137f9812dc3a
Fiodar Savutsin <federikos@bk.ru>
2022-12-03 17:35:12 +0200

    feat: implement subtask Hash (src/hash)

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import { createReadStream } from "node:fs";
+import { createHash } from "node:crypto";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = new URL(
+    "./files/fileToCalculateHashFor.txt",
+    import.meta.url
+  );
+  const hash = createHash("sha256");
+  const input = createReadStream(filePath);
+  input.on("readable", () => {
+    const data = input.read();
+
+    if (data) {
+      hash.update(data);
+    } else {
+      console.log(hash.digest("hex"));
+    }
+  });
 };
 
 await calculateHash();
