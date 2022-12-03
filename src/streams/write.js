567375a1ce2359dbeb012418899d0f1d3c30b8b4
yuranius <yuranius.webber@gmail.com>
2022-12-03 11:47:36 +0300

    streams:write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import fs from 'fs';
+
 const write = async () => {
-    // Write your code here 
+    const dir = 'src/streams/files/fileToWrite.txt';
+
+    const writeStream = fs.createWriteStream(dir); 
+    
+    process.stdin.on('data', (chunk) => {
+        writeStream.write(chunk.toString());
+    })
+    
 };
 
 await write();
