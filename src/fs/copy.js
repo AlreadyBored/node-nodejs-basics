b2d6f09e30e00cf0d419de3ebb01484f89fe51ed
ShotAlex <shot.aleks@gmail.com>
2022-12-03 20:11:01 +0300

    feat: add copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,26 @@
+import fs  from "fs";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
 const copy = async () => {
-    // Write your code here 
+    const currentPath = path.dirname(fileURLToPath(import.meta.url));
+
+    const fromFolderPath = currentPath + '/files';
+    const toFolderPath = currentPath + '/files_copy';
+
+    if (fs.existsSync(toFolderPath)) {
+        throw 'FS operation failed';
+    }
+
+    fs.mkdirSync(toFolderPath);
+    fs.readdirSync(fromFolderPath).forEach(element => {
+        if (fs.lstatSync(path.join(fromFolderPath, element)).isFile()) {
+            fs.copyFileSync(path.join(fromFolderPath, element), path.join(toFolderPath, element));
+        } else {
+            copyFolderSync(path.join(fromFolderPath, element), path.join(toFolderPath, element));
+        }
+    });
 };
 
 copy();
