d4c73cec8e5900abfc270125c4eeba6e2fd99a45
Evgen Orlovski <orloffski@gmail.com>
2022-12-04 02:36:06 +0300

    feat: implement create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,28 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { access, appendFile, constants } from 'node:fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __newFilePath = path.dirname(__filename) + '/files/fresh.txt';
+const __newFileContent = 'I am fresh and young';
+const __errorMessage = 'FS operation failed';
+
 const create = async () => {
-    // Write your code here 
+    try {
+        const ifFileExists = 
+            await access(__newFilePath, constants.R_OK)
+                .then(value => true)
+                .catch(err => false)
+
+        if(ifFileExists){
+            throw Error(__errorMessage);
+        }else{
+            await appendFile(__newFilePath, __newFileContent);
+        }
+    }catch (e) {
+        console.log(e);
+    }
+
 };
 
 await create();
