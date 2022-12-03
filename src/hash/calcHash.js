21c050c64327329b04baea90a917038a72573bd8
yuranius <yuranius.webber@gmail.com>
2022-12-03 10:59:31 +0300

    calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import { createHash } from 'crypto';
+import { readFile } from 'fs/promises';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    let filename = __dirname + '/files/fileToCalculateHashFor.txt';
+
+    try {
+        const content = await readFile(filename);
+        const hash = createHash('sha256').update(content).digest('hex');
+
+        console.log(hash);
+    } catch (err) {
+        throw err;
+    }
+
 };
 
 await calculateHash();
