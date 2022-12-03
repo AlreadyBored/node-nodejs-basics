fc0d5315f18be72831d3e8e6fd100802015db1b4
Eugene Marinovich <eugene.m@razortheory.com>
2022-12-03 17:57:40 +0300

    feat: add cli logic

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+  const needData = process.argv.slice(2);
+  const result = [];
+  needData.forEach((item, index, array) => {
+    if (index % 2 == 0) {
+      result.push(`${item.slice(2)} is ${array[index + 1]}`);
+    }
+  });
+  console.log(result.join(", "));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,16 @@
 const parseEnv = () => {
-    // Write your code here 
+  /* For test function 
+  process.env.RSS_cource = "node.js";
+  process.env.RSS_year = "2022"; */
+
+  const rssValues = Object.entries(process.env).reduce((acc, [key, value]) => {
+    if (key.startsWith("RSS_")) {
+      acc.push(`${key}=${value}`);
+    }
+
+    return acc;
+  }, []);
+  console.log(rssValues.join("; "));
 };
 
 parseEnv();
