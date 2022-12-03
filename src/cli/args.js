350e98b846f316463c55e89514569e09ddbdf30f
yuranius <yuranius.webber@gmail.com>
2022-12-03 09:59:06 +0300

    args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+    let arrArgs = []
+
+    process.argv.slice(2).forEach ( (arg, index, arr) => {
+        if (arg.match(/--\S*/ig)) {
+            arrArgs.push(arg + ' is ' + arr[index + 1]);
+        }
+    })
+
+    console.log(arrArgs.join (', '));
 };
 
 parseArgs();
