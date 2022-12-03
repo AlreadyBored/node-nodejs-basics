4ddfbdae2d340de05495b99ee51595cfbe9831af
Vyacheslav <Smolensky-VO@rshbext.ru>
2022-12-04 01:27:22 +0300

    feat(streams): add write func

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import {createWriteStream} from 'fs';
+import {fileURLToPath} from "url";
+import {createCorrectPath} from "../helper.js";
+
 const write = async () => {
-    // Write your code here 
+    const _filename = fileURLToPath(import.meta.url)
+    const file = createCorrectPath(_filename, 'files', 'fileToWrite.txt');
+
+    try {
+        const writeStream = createWriteStream(file);
+        process.stdin.on('data', (data) => {
+            writeStream.write(data);
+        })
+    } catch (error) {}
 };
 
 await write();
