810584d0e471900de0a6c5017e7e0e77b953456f
Fiodar Savutsin <federikos@bk.ru>
2022-12-03 15:25:39 +0200

    feat: implement subtask Command line interface(src/cli)

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+  const [, , ...args] = process.argv;
+
+  for (let i = 0; i < args.length - 1; i += 2) {
+    const name = args[i].slice(2);
+    const value = args[i + 1];
+    console.log(`${name} is ${value}`);
+  }
 };
 
 parseArgs();
