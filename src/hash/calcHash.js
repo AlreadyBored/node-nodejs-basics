dcab6fd87cf3bd2def2d7f12fd9ad42158e98173
ShotAlex <shot.aleks@gmail.com>
2022-12-03 21:35:39 +0300

    feat: add calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import fs  from "fs";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { createHash } from 'node:crypto'
+
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = 'SHA256';
+
+    const filePath = path.dirname(fileURLToPath(import.meta.url)) + '/files/fileToCalculateHashFor.txt';
+
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        const hashedData = createHash('sha256').update(data).digest('hex');
+        console.log(hashedData);
+      });
 };
 
 await calculateHash();
