2d98d6ecc1db5876b2a583ef7ce08a5fa82b4ec4
ShotAlex <shot.aleks@gmail.com>
2022-12-03 21:23:13 +0300

    feat: add args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
 const parseArgs = () => {
-    // Write your code here 
+    const logArgs = process.argv.slice(2)
+
+    const arr = []
+    for (let i = 0; i <= logArgs.length; i += 2) {
+        const isTrueName = /^--/.test(logArgs[ i ])
+
+        if (isTrueName) {
+            const str = logArgs[ i ].replace('--', '') + ' is ' + logArgs[ i + 1 ];
+            arr.push(str)
+        }
+    }
+
+    console.log(arr.join(', '));
 };
 
 parseArgs();
