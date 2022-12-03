cb2589c5a4dff79ac8992449db67f7ecfac5b24f
ShotAlex <shot.aleks@gmail.com>
2022-12-03 20:37:45 +0300

    feat: add rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,24 @@
+import fs  from "fs";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
 const rename = async () => {
-    // Write your code here 
+    const currentPath = path.dirname(fileURLToPath(import.meta.url));
+
+    const wrongFilenamePath = currentPath + '/files/wrongFilename.txt';
+    const properFilenamePath = currentPath + '/files/properFilename.md';
+
+    const isHaveFile = !fs.existsSync(wrongFilenamePath) || fs.existsSync(properFilenamePath);
+    if (isHaveFile) {
+        throw 'FS operation failed';
+    }
+
+    fs.rename(wrongFilenamePath, properFilenamePath, (err) => {
+        if (err) {
+            throw 'FS operation failed';
+        }
+    });
 };
 
 await rename();
