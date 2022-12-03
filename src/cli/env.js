ab6ae07edd96f0d31e079d9822878022a9459df9
ShotAlex <shot.aleks@gmail.com>
2022-12-03 21:09:59 +0300

    feat: add env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+    const prefix = 'RSS_';
+
+    const obj = Object
+        .entries(process.env)
+        .filter(el => el[ 0 ].includes(prefix));
+
+    const formatData = obj.map(el => `${el[ 0 ]}=${el[ 1 ]}`);
+
+    console.log(formatData.join('; '));
 };
 
 parseEnv();
