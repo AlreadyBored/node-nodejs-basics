02917af0d8ea7f333ba07fc0261e0402742b0d50
Vyacheslav <Smolensky-VO@rshbext.ru>
2022-12-03 22:06:09 +0300

    feat(streams): add read func

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import {createReadStream} from 'fs';
+import {createCorrectPath} from "../helper.js";
+import {fileURLToPath} from "url";
+
 const read = async () => {
-    // Write your code here 
+    const _filename = fileURLToPath(import.meta.url)
+    const file = createCorrectPath(_filename, 'files', 'fileToRead.txt');
+
+    try {
+        const readStream = createReadStream(file);
+        readStream.on('data', (chunk) => {
+            process.stdout.write(chunk.toString())
+        })
+    } catch (error) {}
 };
 
 await read();
