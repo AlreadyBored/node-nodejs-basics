562365331280f95162215cba69033776e97c9b81
Nikita Pytalev <Nikita_Pytalev@epam.com>
2022-12-04 01:40:05 +0100

    feat: implement create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,13 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { dirname } from '../utils.js';
+
 export const create = async () => {
-    // Write your code here 
+    try{
+        await fs.writeFile(path.join(dirname, 'files', 'fresh.txt'), 'I am fresh and young', {flag: "wx"});
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
+
+await create();
