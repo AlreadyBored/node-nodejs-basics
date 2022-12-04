c28e54ab2562d591bae41e85cfdad01431a4e60b
Vladimir Zinovik <zinovikv@gmail.com>
2022-12-05 01:30:40 +0300

    feat: implement create.js function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,9 +1,21 @@
 import fs from 'fs';
+
 const create = async () => {
-  fs.appendFile('./files/fresh.txt', 'I am fresh and young', function (err) {
-    if (err) throw err;
-    console.log('Saved!');
+
+  const path = './files/fresh.txt';
+
+  const cause = new Error('FS operation failed');
+
+  fs.access(path, function (error) {
+    if (error) {
+      fs.appendFile('./files/fresh.txt', 'I am fresh and young', function (err) {
+        if (err) throw err;
+      });
+    } else {
+      console.log(cause);
+    }
   });
+
 };
 
 await create();
