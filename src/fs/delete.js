42c927c550bb49c2d74be73235d63c6e7c290eda
Evgen Orlovski <orloffski@gmail.com>
2022-12-04 05:28:17 +0300

    feat: implement delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,26 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { access, constants, rm as removeFile } from 'node:fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __fileToDelete = path.dirname(__filename) + '/files/fileToRemove.txt';
+const __errorMessage = 'FS operation failed';
+
 const remove = async () => {
-    // Write your code here 
+    try {
+        const ifFileExist = 
+            await access(__fileToDelete, constants.R_OK)
+                .then(value => true)
+                .catch(err => false);
+
+        if(!ifFileExist){
+            throw Error(__errorMessage);
+        }else{
+            removeFile(__fileToDelete);
+        }
+    }catch (e) {
+        console.log(e);
+    }
 };
 
 await remove();
