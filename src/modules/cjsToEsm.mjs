b527f882b7940ddebeb9fc6c4d1d73cbb697a516
PalinaZinouyeva <bozkovapa@gmail.com>
2022-12-04 15:27:57 +0400

    cjs task

--- src/modules/cjsToEsm.mjs
+++ src/modules/cjsToEsm.mjs
@@ -1,72 +1,36 @@
 import path from 'path';
-import { fileURLToPath } from 'url'
-import { dirname } from 'path'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import { release, version } from 'os';
 import { createServer } from 'http';
-//
-// const __filename = fileURLToPath(import.meta.url)
-// const __dirname = dirname(__filename)
-//
-// const random = Math.random();
-// const pathString = random > 0.5 ? './src/modules/files/a.json' : './src/modules/files/b.json';
-// const unknownObject = new URL(pathString, import.meta.url);
-//
-// console.log(`Release ${release()}`);
-// console.log(`Version ${version()}`);
-// console.log(`Path segment separator is "${path.sep}"`);
-//
-// console.log(`Path to current file is ${__filename}`);
-// console.log(`Path to current directory is ${__dirname}`);
-//
-// const myServer = createServer((_, res) => {
-//     res.end('Request accepted');
-// });
-//
-// const PORT = 3000;
-//
-// console.log(unknownObject);
-//
-// myServer.listen(PORT, () => {
-//     console.log(`Server is listening on port ${PORT}`);
-//     console.log('To terminate it, use Ctrl+C combination');
-// });
-//
-// module.exports = {
-//     unknownObject,
-//     myServer,
-// };
 
-(function(exports, require, module, __filename, __dirname) {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-    // const __filename = fileURLToPath(import.meta.url)
-    // const __dirname = dirname(__filename)
-    const random = Math.random();
-    const pathString = random > 0.5 ? './src/modules/files/a.json' : './src/modules/files/b.json';
-    const unknownObject = new URL(pathString, import.meta.url);
+const random = Math.random();
+const pathString = random > 0.5 ? './files/a.json' : './files/b.json';
 
-    console.log(`Release ${release()}`);
-    console.log(`Version ${version()}`);
-    console.log(`Path segment separator is "${path.sep}"`);
+const unknownObject = await import(pathString, { assert: { type: 'json' } });
 
-    console.log(`Path to current file is ${__filename}`);
-    console.log(`Path to current directory is ${__dirname}`);
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${path.sep}"`);
 
-    const myServer = createServer((_, res) => {
-        res.end('Request accepted');
-    });
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
 
-    const PORT = 3000;
+const myServer = createServer((_, res) => {
+    res.end('Request accepted');
+});
 
-    console.log(unknownObject);
+const PORT = 3000;
 
-    myServer.listen(PORT, () => {
-        console.log(`Server is listening on port ${PORT}`);
-        console.log('To terminate it, use Ctrl+C combination');
-    });
+console.log(unknownObject);
 
-    exports = {
-        unknownObject,
-        myServer,
-    }
-})();
+myServer.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+    console.log('To terminate it, use Ctrl+C combination');
+});
 
+export const unknownObjectData = unknownObject;
+export const myServerData = myServer;
