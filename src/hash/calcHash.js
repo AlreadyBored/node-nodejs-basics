ea543ca9359be57ba91b0dcec4490e29b6133988
vkordovich <vadim_kordovich@epam.com>
2022-12-04 17:35:43 +0300

    hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import { createHash } from 'node:crypto'
+import {fileURLToPath} from "url";
+import path from "path";
+import fs from "fs/promises";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirName = path.dirname(__filename);
+const filesPath = path.join(__dirName, "files");
+const [fileName] = await fs.readdir(filesPath);
+const fileToCalcPath = path.join(__dirName, "files", fileName);
+
+console.log()
 const calculateHash = async () => {
-    // Write your code here 
+    // Write your code here
+    const hex = sha256(fileToCalcPath)
+    console.log(hex)
 };
+
+const sha256 = (content) => {
+    return createHash('sha3-256').update(content).digest('hex')
+}
 
 await calculateHash();
