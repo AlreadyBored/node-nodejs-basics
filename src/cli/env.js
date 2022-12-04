b4fc349e025432106434ea783debffb18f641380
Tomas_Zagorskis <tomzagos@gmail.com>
2022-12-04 21:26:03 +0200

    feat: print env variables

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
+process.env.RSS_name1 = 'value1';
+process.env.RSS_name2 = 'value2';
+
 const parseEnv = () => {
-    // Write your code here 
+	const variables = [];
+
+	for (let env in process.env) {
+		if (env.match(/RSS_\w*/)) {
+			variables.push(`${env}=${process.env[env]}`);
+		}
+	}
+	console.log(variables.join('; '));
 };
 
 parseEnv();
