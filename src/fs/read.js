ae40f38da8ccd1ec78ae567e55af31e424e4d006
propatch <propatchme@gmail.com>
2022-12-04 22:53:07 +0300

    feat:function that prints content of the fileToRead.txt into console

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,23 @@
+import fs from "node:fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  // Write your code here
+  const errMsg = "FS operation failed";
+
+  try {
+    const readSourceFile = await fs.readFile(
+      __dirname + "/files/fileToRead.txt",
+      "utf8"
+    );
+    console.log(readSourceFile);
+  } catch (err) {
+    throw new Error(errMsg);
+  }
 };
 
 await read();
