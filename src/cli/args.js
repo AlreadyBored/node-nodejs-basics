3dea3337be61ecd13952dbe73681bdc48a047acf
Tomas_Zagorskis <tomzagos@gmail.com>
2022-12-04 21:34:59 +0200

    feat: print cli args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
+// execute in terminal: node src/cli/args --propName 123 --prop2Name 'Hello'
+
 const parseArgs = () => {
-    // Write your code here 
+	const result = process.argv
+		.filter(arg => arg.match(/--\w*/))
+		.map(
+			value =>
+				(value = `${value.slice(2)} is ${
+					process.argv[process.argv.indexOf(value) + 1]
+				}`)
+		)
+		.join(', ');
+	console.log(result);
 };
 
 parseArgs();
