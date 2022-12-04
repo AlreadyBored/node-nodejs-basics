a01f1dfb9155cd1682b470e7ecaa4bba6ee93971
potyoma <artyomnovosyolov254@gmail.com>
2022-12-04 19:03:55 +0400

    finished with child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,11 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+import { fork } from "child_process"
 
-spawnChildProcess();
+const spawnChildProcess = async args => {
+  // Write your code here
+  const script = "./src/cp/files/script.js"
+  const child = fork(script, args, { stdio: "pipe" })
+  process.stdin.pipe(child.stdin)
+  child.stdout.pipe(process.stdout)
+}
+
+spawnChildProcess()
