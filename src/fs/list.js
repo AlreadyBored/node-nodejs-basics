36e84744c75adba3193ee0cee27cf3c2e01bf773
potyoma <artyomnovosyolov254@gmail.com>
2022-12-04 13:35:34 +0400

    implemented list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,14 @@
-const list = async () => {
-    // Write your code here 
-};
+import fs from "fs/promises"
+import { exists, applyToAllFiles } from "./utils.js"
 
-await list();
+const list = async () => {
+  // Write your code here
+  const path = "src/fs/files"
+
+  if (!(await exists(path))) throw Error("FS operation failed")
+
+  const dir = await fs.opendir(path)
+  await applyToAllFiles(dir, entry => console.log(entry.name))
+}
+
+await list()
