cc84cfa083fab24c234838bfd2ad0964fbbca85c
propatch <propatchme@gmail.com>
2022-12-04 22:37:31 +0300

    feat:implement function that deletes file fileToRemove.txt

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import fs from "node:fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+  // Write your code here
+  const errMsg = "FS operation failed";
+  try {
+    await fs.rm(__dirname + "/files/fileToRemove.txt");
+  } catch (err) {
+    throw new Error(errMsg);
+  }
 };
 
 await remove();
