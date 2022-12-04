c6e58916d1fd37d0218720e314ecd8533ea2f385
propatch <propatchme@gmail.com>
2022-12-04 23:58:27 +0300

    feat: implement function that parses

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+  // Write your code here
+  const findRSS = Object.entries(process.env).reduce((acc, [key, val]) => {
+    if (key.startsWith("RSS_")) acc.push(`${key}=${val}`);
+    return acc;
+  }, []);
+  console.log(findRSS.join("; "));
 };
 
 parseEnv();
