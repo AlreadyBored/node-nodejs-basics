d1b1dae9c5f972f9dcd2d7f9f2a3d2985b800f35
vkordovich <vadim_kordovich@epam.com>
2022-12-04 22:03:20 +0300

    streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,27 @@
+import fs from 'fs';
+import {fileURLToPath} from "url";
+import path from "path";
+import { Readable } from "stream";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirName = path.dirname(__filename);
+const filesPath = path.join(__dirName, "files");
+const [fileToRead] = await fs.promises.readdir(filesPath);
+const fileToReadPath = path.join(__dirName, "files", fileToRead);
 const read = async () => {
-    // Write your code here 
+    // Write your code here
+
+    /*FS*/
+    // const readStreamFS = fs.createReadStream(fileToReadPath, 'utf-8');
+    // readStreamFS.pipe(process.stdout)
+
+    /*Stream*/
+    const readStream = new Readable({
+        encoding: 'utf-8'
+    })
+    readStream._read = ( ) => { }
+    readStream.push(await fs.promises.readFile(fileToReadPath));
+    readStream.pipe(process.stdout);
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,29 @@
+import {Writable} from "stream";
+import fs from "fs";
+import {fileURLToPath} from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirName = path.dirname(__filename);
+const filesPath = path.join(__dirName, "files");
+const [,fileToWrite] = await fs.promises.readdir(filesPath);
+const fileToWritePath = path.join(__dirName, "files", fileToWrite);
 const write = async () => {
-    // Write your code here 
+    // Write your code here
+
+    /*FS*/
+    // const writableStreamFS = fs.createWriteStream(fileToWritePath);
+    // process.stdin.pipe(writableStreamFS);
+
+    /*Stream*/
+    const writableStream = new Writable({
+        encoding: 'utf-8'
+    })
+    writableStream._write = async (chunk) => {
+       await fs.promises.writeFile(fileToWritePath, chunk.toString());
+        process.exit();
+    }
+    process.stdin.pipe(writableStream);
 };
 
 await write();
