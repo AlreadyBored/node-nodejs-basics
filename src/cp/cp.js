43729c88cc5fa9f5b4070973993c43258d7682be
Eugene Marinovich <eugene.m@razortheory.com>
2022-12-04 18:46:14 +0300

    feat: add cp logic

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,13 @@
+import path from "path";
+import { fork } from "child_process";
+
+import { getPath } from "../helpers/getPath.js";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const { __dirname } = getPath(import.meta.url);
+  const fullPath = path.join(__dirname, "/files", "script.js");
+
+  fork(fullPath, args);
 };
 
 spawnChildProcess();
