334dceb6c50b2aeb96fcb770a3f55c52d5e3793e
Nikita Pytalev <Nikita_Pytalev@epam.com>
2022-12-04 12:08:13 +0100

    feat: implement env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,9 @@
 export const parseEnv = () => {
-    // Write your code here 
+    for(const [key, value] of Object.entries(process.env)) {
+        if(key.startsWith('RSS_')){
+            process.stdout.write(`${key}=${value}; `);
+        }
+    }
 };
+
+parseEnv();
