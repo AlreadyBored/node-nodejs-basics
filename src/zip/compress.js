21af47393ba56a57efa3d9775db38d46c81de35c
Nikita Pytalev <Nikita_Pytalev@epam.com>
2022-12-04 13:53:22 +0100

    feat: implement compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+import zlib from 'zlib';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+const fileToCompressPath = path.join(dirname, 'files', 'fileToCompress.txt');
+const archivePath = path.join(dirname, 'files', 'archive.gz');
+
 export const compress = async () => {
-    // Write your code here 
+    var zip = zlib.createGzip();
+    
+    var read = fs.createReadStream(fileToCompressPath);
+    var write = fs.createWriteStream(archivePath);
+
+    read.pipe(zip).pipe(write);	
 };
+
+await compress();
