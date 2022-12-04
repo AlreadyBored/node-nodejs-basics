3690036e1f442b41cfbecd1c8097a86666ed4edc
ToyBoy <lary911@Ya.ru>
2022-12-04 05:31:56 +0300

    feat: add solving to hash folder
    Took 52 seconds

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
-const calculateHash = async () => {
-    // Write your code here 
+import fs from "fs/promises";
+import crypto from "crypto";
+import url from "url";
+import path from "path";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const pathToFile = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+
+const calculateHash = async (filePath) => {
+  try {
+    const fileBuffer = await fs.readFile(filePath);
+    const hashSum = await crypto.createHash("sha256");
+    hashSum.update(fileBuffer);
+    const hex = hashSum.digest("hex");
+    console.log(hex);
+  } catch (err) {
+    throw new Error("Calculate hash has failed");
+  }
 };
 
-await calculateHash();
+await calculateHash(pathToFile);
