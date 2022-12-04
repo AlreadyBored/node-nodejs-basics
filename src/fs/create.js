2be7aa94a3a346ad56e33a4760ac64d305d04be2
Jevgenijs Kozlovs <Yevgeny>
2022-12-04 16:45:17 +0200

    feat: implement fs create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import fs from 'node:fs/promises';
+
+const fileData = 'I am fresh and young';
+const errorText = 'FS operation failed';
+const filePath = process.cwd() + '/src/fs/files/fresh.txt';
+
 const create = async () => {
-    // Write your code here 
+    let fileExists = false;
+    try {
+        const fileStat = await fs.stat(filePath);
+        if (fileStat) fileExists = true;;
+    } catch (e) { }
+
+    if (fileExists) throw Error(errorText);
+
+
+    await fs.writeFile(filePath, fileData);
 };
 
 await create();
