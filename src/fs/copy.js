00ee652c468d5c204e931101de26387f1c9e01dc
Nikita Pytalev <Nikita_Pytalev@epam.com>
2022-12-04 02:04:30 +0100

    feat: implement copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,18 @@
+import fs from 'fs/promises';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
 export const copy = async () => {
-    // Write your code here 
+    const srcDir = path.join(dirname, 'files');
+    const destDir = path.join(dirname, 'files-copy');
+    try{
+        await fs.cp(srcDir, destDir, { recursive: true, force: false, errorOnExist: true })
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
+
+await copy();
