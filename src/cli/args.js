b2675126304dd6b62d6befb203d34c66475e9038
potyoma <artyomnovosyolov254@gmail.com>
2022-12-04 14:09:53 +0400

    implemented parseArgs

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
-};
+  // Write your code here
+  const args = process.argv.slice(2)
+  const parsedArgs = []
 
-parseArgs();
+  args.forEach((arg, i) => {
+    if (i % 2 === 0) parsedArgs.push(`${arg.replace("--", "")} is`)
+    else parsedArgs.push(`${parsedArgs.pop()} ${arg}`)
+  })
+
+  console.log(parsedArgs.join(", "))
+}
+
+parseArgs()
