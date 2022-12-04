1083d1676e4a3f1a1542dbd0b4d52f25eb0fd984
Eugene Marinovich <eugene.m@razortheory.com>
2022-12-04 18:00:36 +0300

    feat: add zip logic

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+import zlib from "zlib";
+import fs from "fs";
+import path from "path";
+
+import { getPath } from "../helpers/getPath.js";
+
 const compress = async () => {
-    // Write your code here 
+  const { __dirname } = getPath(import.meta.url);
+  const pathRead = path.join(__dirname, "/files", "fileToCompress.txt");
+  const pathWrite = path.join(__dirname, "/files", "archive.gz");
+
+  const streamRead = fs.createReadStream(pathRead);
+  const streamWrite = fs.createWriteStream(pathWrite);
+  const compress = zlib.createGzip();
+
+  streamRead.pipe(compress).pipe(streamWrite);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+import zlib from "zlib";
+import fs from "fs";
+import path from "path";
+
+import { getPath } from "../helpers/getPath.js";
+
 const decompress = async () => {
-    // Write your code here 
+  const { __dirname } = getPath(import.meta.url);
+  const pathWrite = path.join(__dirname, "/files", "fileToCompress.txt");
+  const pathRead = path.join(__dirname, "/files", "archive.gz");
+
+  const streamRead = fs.createReadStream(pathRead);
+  const streamWrite = fs.createWriteStream(pathWrite);
+  const decompress = zlib.createGunzip();
+
+  streamRead.pipe(decompress).pipe(streamWrite);
 };
 
 await decompress();
