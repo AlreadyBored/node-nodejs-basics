a472ea8ba75205ec49c6d15d01d9b606fda60cf5
potyoma <artyomnovosyolov254@gmail.com>
2022-12-04 13:20:35 +0400

    implemeted rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,16 @@
-const rename = async () => {
-    // Write your code here 
-};
+import exists from "./exists.js"
+import fs from "fs/promises"
 
-await rename();
+const rename = async () => {
+  // Write your code here
+  const basePath = "src/fs/files"
+  const wrongFilePath = `${basePath}/wrongFilename.txt`
+  const properFilePath = `${basePath}/properFilename.md`
+
+  if (!(await exists(wrongFilePath)) || (await exists(properFilePath)))
+    throw Error("FS operation failed")
+
+  await fs.rename(wrongFilePath, properFilePath)
+}
+
+await rename()
