0a66e6bab396146ff219c25d026eee8cf0fc8ed4
Evgen Orlovski <orloffski@gmail.com>
2022-12-04 22:38:33 +0300

    feat: implement write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import { createWriteStream } from 'node:fs'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __fileToWrite = path.dirname(__filename) + '/files/fileToWrite.txt';
+
 const write = async () => {
-    // Write your code here 
+    const writeFile = createWriteStream(__fileToWrite);
+
+    process.stdin.pipe(writeFile);
 };
 
 await write();
