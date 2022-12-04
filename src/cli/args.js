fb8c45c954744b1c37cfc8ec900b7037c556e6c4
Evgen Orlovski <orloffski@gmail.com>
2022-12-04 07:26:55 +0300

    feat: implement parseArgs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
+import { argv } from 'node:process';
+
 const parseArgs = () => {
-    // Write your code here 
+    const argsArray = [];
+
+    argv.forEach((val, index) => {
+        if(index > 1 && index % 2 == 0)
+        argsArray.push(`${val} is ${argv[index+1]}`)
+    });
+
+    console.log(argsArray.join(', '));
 };
 
 parseArgs();
