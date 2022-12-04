65c6368368dfa6a62725cf86aa089ce2cade9b1e
ShotAlex <shot.aleks@gmail.com>
2022-12-04 22:34:39 +0300

    feat: add cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,21 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { fork } from 'child_process'
+
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const currentPath = path.dirname(fileURLToPath(import.meta.url));
+    const filePath = currentPath + '/files/script.js';
+
+    args.splice(0, 2);
+    const child = fork(filePath, args, { silent: true });
+
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
+
+    child.stdout.on('data', data => {
+        process.stdout.write(data);
+    })
 };
 
-spawnChildProcess();
+spawnChildProcess(process.argv);
