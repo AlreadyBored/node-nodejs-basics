1b758d87655ef875f9c3538960df5403a3bf6e3c
Наконечный Антон Викторович <anakonechny@alfabank.ru>
2022-12-04 18:10:19 +0300

    feat: implement wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,40 @@
+import { cpus } from "os";
+import { Worker } from "worker_threads";
+import { join } from "path";
+
+import { __dirname } from "./../fs/utils/fs.js";
+
+const INITIAL_NUM = 10;
+const numCPUs = cpus().length;
+const path = join(__dirname, "wt", "worker.js");
+const statuses = {
+    fulfilled: "resolved",
+    rejected: "error",
+};
+
+const compute = (data) =>
+    new Promise((res, rej) => {
+        const worker = new Worker(path, {
+            workerData: data,
+        });
+
+        worker.on("message", res);
+
+        worker.on("error", rej);
+    });
+
 const performCalculations = async () => {
-    // Write your code here
+    const workers = [];
+    for (let workerNum = 0; workerNum < numCPUs; workerNum++) {
+        workers.push(compute(INITIAL_NUM + workerNum));
+    }
+
+    (await Promise.allSettled(workers)).forEach(({ status, value }) =>
+        console.log({
+            status: statuses[status],
+            data: value ?? null,
+        })
+    );
 };
 
 await performCalculations();
