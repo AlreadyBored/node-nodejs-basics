36f82b8e3914f89ba39b5b45213cfaea1346a60e
potyoma <artyomnovosyolov254@gmail.com>
2022-12-04 13:45:24 +0400

    implemented parseEnv

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,8 @@
 const parseEnv = () => {
-    // Write your code here 
-};
+  // Write your code here
+  const filteredVars = Object.entries(process.env).filter(([key]) => key.startsWith("RSS_"))
+  const parsedVars = filteredVars.map(([key, value]) => `${key}=${value}`)
+  console.log(parsedVars.join("; "))
+}
 
-parseEnv();
+parseEnv()
