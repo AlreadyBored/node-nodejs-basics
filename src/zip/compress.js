0c0301279593d8c033c25a26428fb740e87c5d99
Dimitry-prog <kfifa3@gmail.com>
2022-12-04 17:48:48 +0300

    feat: add zip-tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+import {createReadStream, createWriteStream} from 'node:fs'
+import {createGzip}  from 'node:zlib';
+import { pipeline } from 'node:stream';
+import { promisify } from 'node:util';
+
+
 const compress = async () => {
-    // Write your code here 
+    const readPath = './src/zip/files/fileToCompress.txt'
+    const writePath = './src/zip/files/archive.gz'
+    const read = createReadStream(readPath);
+    const write = createWriteStream(writePath);
+    const gzip = createGzip();
+    const pipe = promisify(pipeline);
+
+    await pipe(read, gzip, write);
+
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import {createReadStream, createWriteStream} from 'node:fs'
+import {createUnzip }  from 'node:zlib';
+import { pipeline } from 'node:stream';
+import { promisify } from 'node:util';
+
+
 const decompress = async () => {
-    // Write your code here 
+    const writePath = './src/zip/files/fileToCompress.txt'
+    const readPath = './src/zip/files/archive.gz'
+    const read = createReadStream(readPath);
+    const write = createWriteStream(writePath);
+    const unzip = createUnzip();
+    const pipe = promisify(pipeline);
+
+    await pipe(read, unzip, write);
 };
 
 await decompress();
