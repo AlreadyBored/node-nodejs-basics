670f411533a2a5940a5fae4a8ac75751157f2deb
Dimitry-prog <kfifa3@gmail.com>
2022-12-04 18:47:28 +0300

    feat: add wt-tasks

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,41 @@
+import { Worker } from 'node:worker_threads';
+import {cpus} from 'node:os';
+
 const performCalculations = async () => {
-    // Write your code here
+    const path = './src/wt/worker.js';
+    const cpuCores = cpus();
+    const res = [];
+    const startStreams = 10;
+    const promise = [];
+
+    cpuCores.forEach((elem, index) => {
+        promise.push(new Promise((resolve, reject) => {
+                const worker = new Worker(path, {
+                    workerData: index + startStreams,
+                });
+                worker.on('message', resolve);
+                worker.on('error', reject);
+                worker.on('exit', (code) => {
+                    if (code !== 0)
+                        reject(new Error(`Worker stopped with exit code ${code}`));
+                });
+            }).then(result => {
+            res[index] = {
+                    status: 'resolved',
+                    data: result
+                }
+            }).catch(() => {
+            res[index] = {
+                    status: 'error',
+                    data: null
+                }
+            })
+        )
+    })
+
+    await Promise.all(promise).then(()=>{
+        console.log(res)
+    });
 };
 
 await performCalculations();
