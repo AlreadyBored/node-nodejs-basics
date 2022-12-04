f41c6ec0a832f363da36394f30732073236fac9e
Nikita Pytalev <Nikita_Pytalev@epam.com>
2022-12-04 11:13:51 +0100

    feat: implement list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,20 @@
+import fs from 'fs/promises';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
 export const list = async () => {
-    // Write your code here 
+    const srcDir = path.join(dirname, 'files');
+
+    try {
+        const filenames = await fs.readdir(srcDir);
+        filenames.forEach(filename => console.log(filename));
+    } catch {
+        throw new Error('FS operation failed');
+    }
+
 };
+
+await list();
