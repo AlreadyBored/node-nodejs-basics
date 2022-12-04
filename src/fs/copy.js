4959617fda18a93a0defea772241f709ee45ad01
propatch <propatchme@gmail.com>
2022-12-04 21:14:40 +0300

    feat:implement function that renames file wrongFilename.txt to properFilename

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -23,4 +23,4 @@ const copy = async () => {
   }
 };
 
-copy();
+await copy();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
+import fs from "node:fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const rename = async () => {
-    // Write your code here 
+  // Write your code here
+  const errMsg = "FS operation failed";
+  try {
+    await fs.rename(
+      path.join(__dirname, "/files", "wrongFilename.txt"),
+      path.join(__dirname, "/files", "properFilename.md")
+    );
+  } catch (err) {
+    throw new Error(errMsg);
+  }
 };
 
 await rename();
