a1f5a0081bba3d3b9900fd13efecd5a8de858b9c
Evgen Orlovski <orloffski@gmail.com>
2022-12-04 22:34:01 +0300

    feat: implement read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import { createReadStream } from 'node:fs'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __fileToRead = path.dirname(__filename) + '/files/fileToRead.txt';
+
 const read = async () => {
-    // Write your code here 
+    const readFile = createReadStream(__fileToRead);
+
+    readFile.pipe(process.stdout);
 };
 
 await read();
