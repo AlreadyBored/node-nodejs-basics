cd462ed3583225a6b87ec0758b2e4331ef826744
Nikita Pytalev <Nikita_Pytalev@epam.com>
2022-12-04 17:35:08 +0100

    feat: implement cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,16 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { fork } from 'child_process';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+const scriptPath = path.join(dirname, 'files', 'script.js');
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = fork(scriptPath, args, {silent: true});
+
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
 };
+
+await spawnChildProcess(process.argv.slice(2));
