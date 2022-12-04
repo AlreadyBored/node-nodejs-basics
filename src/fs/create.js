e8ee6368923d13876513e04e3f511cc2d9bd890e
Ash Karapetyan <ashot.karapetyan.y@gmail.com>
2022-12-04 22:24:41 +0300

    Implemented create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,15 @@
+import { writeFile } from 'fs'
+import { resolve } from 'path'
+
 const create = async () => {
-    // Write your code here 
+    const absolutePath = resolve('files', 'fresh.txt')
+    const data = 'I am fresh and young'
+    await writeFile(absolutePath, data, {flag: 'wx'}, (err) => {
+        if (err && err.code === 'EEXIST') {
+            throw new Error('FS operation failed');
+        }
+        console.log('File created Successfully!');
+    });
 };
 
 await create();
