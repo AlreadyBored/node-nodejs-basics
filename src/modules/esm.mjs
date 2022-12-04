2f754ded762356c5f5545decd2f3130dffa66116
potyoma <artyomnovosyolov254@gmail.com>
2022-12-04 14:29:49 +0400

    moved cjs to mjs

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,17 +1,21 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path from 'path';
+import { release, version } from 'os';
+import { createServer as createServerHttp } from 'http';
+import './files/c.js';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const random = Math.random();
 
-let unknownObject;
-
-if (random > 0.5) {
-    unknownObject = require('./files/a.json');
-} else {
-    unknownObject = require('./files/b.json');
-}
+const unknownObject = await import(
+    random > 0.5 ? './files/a.json' : './files/b.json', 
+    {
+        assert: {
+            type: "json"
+        }
+    });
 
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
@@ -33,7 +37,7 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
+export {
     unknownObject,
     myServer,
 };
