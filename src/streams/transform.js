11b05fb0d9b03164a3913d2074790f44eaa385a7
ShotAlex <shot.aleks@gmail.com>
2022-12-04 18:20:02 +0300

    feat: add stream/transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,27 @@
+import { Transform } from 'stream';
+
+
 const transform = async () => {
-    // Write your code here 
+    const myTransform = new Transform({
+        writableObjectMode: true,
+        transform(chunk, encoding, callback) {
+            const data = chunk
+                .toString(16)
+                .split('')
+                .reverse()
+                .join('');
+
+            callback(null, data);
+        }
+    });
+
+    myTransform.setEncoding('utf8');
+    myTransform.on('data', (chunk) => process.stdout.write(chunk));
+
+    process.stdin.setEncoding('utf8');
+    process.stdin.on("data", data => {
+        myTransform.write(data);
+    });
 };
 
 await transform();
