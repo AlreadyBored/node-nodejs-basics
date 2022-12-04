ae2861e8625c03d5d926506f4b69d1533c56fc56
Vyacheslav <Smolensky-VO@rshbext.ru>
2022-12-05 00:17:35 +0300

    feat(wt): add performCalculations and sendResult func

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,37 @@
+import {Worker} from 'worker_threads';
+import {cpus} from 'os';
+
+
 const performCalculations = async () => {
-    // Write your code here
+    try {
+        const cpusArr = cpus();
+        const workersPromises = cpusArr.map((cpu, index) => {
+            return new Promise((resolve, reject) => {
+                const worker = new Worker('./worker.js', {
+                    workerData: 10 + index
+                })
+                worker.on('message', (message) => {
+                    if (message) {
+                        resolve({
+                            status: 'resolved',
+                            data: message
+                        })
+                    } else {
+                        reject(
+                            {
+                                status: 'error',
+                                data: null
+                            }
+                        )
+                    }
+                })
+            })
+        })
+        const allSettledArr = await Promise.allSettled(workersPromises);
+        const workerPromisesResultArr = allSettledArr.map(item => item.value);
+        console.log(workerPromisesResultArr);
+
+    } catch (error) {}
 };
 
 await performCalculations();
