4aeaf03d2b5ebac211c1e3bf989276faeb1743de
Nikita Pytalev <Nikita_Pytalev@epam.com>
2022-12-04 13:50:16 +0100

    feat: implement transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,21 @@
+import { Transform, pipeline } from 'stream';
+
 export const transform = async () => {
-    // Write your code here 
+    const transform = new Transform({
+        transform (chunk, enc, cb) {
+            const reversedChunk = chunk.toString().trim().split('').reverse().join('');
+            this.push(reversedChunk + '\n');
+            cb();
+        }
+    });
+    pipeline(
+        process.stdin,
+        transform,
+        process.stdout,
+        err => {
+            console.log(err);
+        }
+    );
 };
+
+await transform();
