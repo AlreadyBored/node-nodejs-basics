df9462ad720eef8e98b9323fe7d3c1d1607da653
Roman Panchenko <roman.p@aquimarketing.com>
2022-12-04 14:54:39 +0200

    feat: es6 modules fixes

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,16 +1,27 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path from 'node:path';
+import { release, version } from 'node:os';
+import { createServer as createServerHttp } from 'http';
+await import('./files/c.js');
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+// console.log(c);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const random = Math.random();
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+    unknownObject = await import('./files/a.json', {
+        assert: { type: "json" },
+    });
 } else {
-    unknownObject = require('./files/b.json');
+    unknownObject = await import('./files/b.json', {
+        assert: { type: "json" },
+    });
 }
 
 console.log(`Release ${release()}`);
@@ -26,14 +37,14 @@ const myServer = createServerHttp((_, res) => {
 
 const PORT = 3000;
 
-console.log(unknownObject);
+console.log(unknownObject.default);
 
 myServer.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
+export {
     unknownObject,
     myServer,
 };
