0a7d808b2fe226d586ab1afaaa8f55ebe8b2bc51
Vladimir Burmistrov <thereddevil18@gmail.com>
2022-12-04 20:25:57 +0300

    chenge files with cli task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+    let args;
+    const processArgs = process.argv;
+    for (let arg of processArgs) {
+        if (arg.substring(0, 2) == '--') {
+            const nameValue = arg.split("=");
+            args = args ? `${args}, ${nameValue[0]} is ${nameValue[1]}` : `${nameValue[0]} is ${nameValue[1]}`;
+        }
+    }
+    console.log(process.argv);
+    console.log(args);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
 const parseEnv = () => {
-    // Write your code here 
-};
+    
+    let envVariables;
+    
+    const processVariables = process.env;
+    for (const [key, value] of Object.entries(processVariables)) {
+        if (!envVariables) {
+            envVariables = `RSS_${key}=${value}`
+        }
+        envVariables = `${envVariables}; RSS_${key}=${value}`;
+      }
+      console.log(envVariables);
+};  
 
 parseEnv();
