f66df362381f2f0b569025a7dfa4378888b79d8c
PalinaZinouyeva <bozkovapa@gmail.com>
2022-12-04 16:32:14 +0400

    hach

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import { fileURLToPath } from "url";
+import fs from "fs/promises";
+import crypto from "crypto";
+import path, {dirname} from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+    const content = await fs.readFile(path.resolve(__dirname, 'files/fileToCalculateHashFor.txt'));
+
+    const hash = crypto.createHash('SHA256').update(content);
+
+    const hex = hash.digest('hex');
+
+    console.log(hex);
 };
 
 await calculateHash();
