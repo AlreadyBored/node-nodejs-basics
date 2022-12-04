e00b634172ef971b0b102a3f37b5f78988d4ac5f
Nikita Pytalev <Nikita_Pytalev@epam.com>
2022-12-04 12:28:51 +0100

    feat: implement args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,16 @@
 export const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    let isFirstRun = true;
+
+    for (let i = 0; i < args.length; i += 2) {
+        if (isFirstRun) {
+            isFirstRun = false;
+        } else {
+            process.stdout.write(', ');
+        }
+
+        process.stdout.write(`${args[i].substring(2)} is ${args[i + 1]}`);
+    }
 };
+
+parseArgs();
