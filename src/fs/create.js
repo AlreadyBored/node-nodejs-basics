c351f80b6161117a8b5800ce76f23d653d03839f
Your Name <you@example.com>
2022-12-05 02:17:07 +0300

    feat: add fs create method

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,26 @@
-const create = async () => {
-    // Write your code here 
+import { existsSync } from "node:fs";
+import { writeFile } from "node:fs/promises";
+import { join, resolve } from "node:path";
+
+const DIR_NAME = "files";
+const FILE_NAME = "fresh.txt";
+const FILE_CONTENT = "I am fresh and young";
+
+const create = async (
+  dirName = DIR_NAME,
+  fileName = FILE_NAME,
+  data = FILE_CONTENT
+) => {
+  const [_, currentPath] = process.argv;
+  const path = resolve(currentPath, "../", dirName, fileName);
+
+  if (existsSync(path)) throw new Error("FS operation failed");
+
+  writeFile(path, data);
 };
 
-await create();
+try {
+  await create();
+} catch (e) {
+  console.log(e);
+}
