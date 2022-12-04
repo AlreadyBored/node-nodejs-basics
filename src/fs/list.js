5dc9797851e113a7884b662acba277eef0ff4f03
Evgen Orlovski <orloffski@gmail.com>
2022-12-04 05:32:39 +0300

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,28 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { access, constants, readdir } from 'node:fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __folderToList = path.dirname(__filename) + '/files';
+const __errorMessage = 'FS operation failed';
+
 const list = async () => {
-    // Write your code here 
+    try {
+        const ifFolderExist = 
+            await access(__folderToList, constants.R_OK)
+                .then(value => true)
+                .catch(err => false);
+
+        if(!__folderToList){
+            throw Error(__errorMessage);
+        }else{
+            await readdir(__folderToList).then(files => {
+                console.log(files);
+            })
+        }
+    }catch (e) {
+        console.log(e);
+    }
 };
 
 await list();
