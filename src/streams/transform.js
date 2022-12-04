6d2e71136166ee863c418e2dcc089861d0ef381f
Vyacheslav <Smolensky-VO@rshbext.ru>
2022-12-04 17:08:23 +0300

    feat(streams): add transform func

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+import {Transform} from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+    try {
+        const createRevertStringTransformStream = () => {
+            return new Transform({
+                transform(chunk) {
+                    return chunk.toString().split('').reverse().join('');
+                }
+            })
+        }
+
+        const transformStream = createRevertStringTransformStream();
+        process.stdin.pipe(transformStream).pipe(process.stdout)
+    } catch (error) {}
 };
 
 await transform();
