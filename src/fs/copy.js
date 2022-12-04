ebab0cd7aafe89c26bafaeba741a578938eac8ad
potyoma <artyomnovosyolov254@gmail.com>
2022-12-04 13:20:16 +0400

    implemented copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,25 @@
-const copy = async () => {
-    // Write your code here 
-};
+import fs from "fs/promises"
+import exists from "./exists.js"
 
-copy();
+const copy = async () => {
+  // Write your code here
+  const basePath = "src/fs"
+  const copyPath = `${basePath}/files_copy`
+  const originalPath = `${basePath}/files`
+
+  if (!(await exists(originalPath)) || (await exists(copyPath)))
+    throw Error("FS operation failed")
+
+  await fs.mkdir(copyPath)
+  const dir = await fs.opendir(originalPath)
+  await copyFiles(originalPath, copyPath, dir)
+}
+
+const copyFiles = async (from, to, dir) => {
+  const entry = await dir.read()
+  if (!entry) return
+  await fs.copyFile(`${from}/${entry.name}`, `${to}/${entry.name}`)
+  return await copyFiles(from, to, dir)
+}
+
+copy()
