47562da8eb6eb7bea97e6fe1a280ee09d2432a58
Nikita Pytalev <Nikita_Pytalev@epam.com>
2022-12-04 02:30:04 +0100

    feat: implement delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,19 @@
+import fs from 'fs/promises';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
 export const remove = async () => {
-    // Write your code here 
+    const toRemove = path.join(dirname, 'files', 'fileToRemove.txt');
+
+    try {
+        await fs.unlink(toRemove);
+    } catch {
+        throw new Error('FS operation failed');
+    }
+
 };
+
+await remove();
