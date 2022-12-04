5b295c0421d1957ca0b4226571d31b99baffb31a
Nikita Pytalev <Nikita_Pytalev@epam.com>
2022-12-04 11:17:03 +0100

    feat: implement read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,17 @@
+import fs from 'fs/promises';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+export const filename = fileURLToPath(import.meta.url);
+export const dirname = path.dirname(filename);
+
 export const read = async () => {
-    // Write your code here 
+    try{
+        const data = await fs.readFile(path.join(dirname, 'files', 'fileToRead.txt'),  { encoding: 'utf8'});
+        console.log(data);
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
+
+await read();
