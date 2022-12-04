377bba7011163ae8c9dc720f0d1e19f473ce0fc4
Nikita Pytalev <Nikita_Pytalev@epam.com>
2022-12-04 13:44:07 +0100

    feat: implement write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,13 @@
+import fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+export const filename = fileURLToPath(import.meta.url);
+export const dirname = path.dirname(filename);
+
 export const write = async () => {
-    // Write your code here 
+    const stream = fs.createWriteStream(path.join(dirname, 'files', 'fileToWrite.txt'));
+    process.stdin.pipe(stream);
 };
+
+await write();
