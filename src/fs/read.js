7c0a813eaa546543cb0b74374add519bd380cda6
potyoma <artyomnovosyolov254@gmail.com>
2022-12-04 13:35:44 +0400

    implemented read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,14 @@
-const read = async () => {
-    // Write your code here 
-};
+import { exists } from "./utils.js"
+import fs from "fs/promises"
 
-await read();
+const read = async () => {
+  // Write your code here
+  const fileToRead = "src/fs/files/fileToRead.txt"
+
+  if (!(await exists(fileToRead))) throw Error("FS operation failed")
+
+  const file = await fs.readFile(fileToRead, "utf8")
+  console.log(file)
+}
+
+await read()
