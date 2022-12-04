68845c2362c0d2a71bead1f418edb066bc2b75a7
potyoma <artyomnovosyolov254@gmail.com>
2022-12-04 14:53:44 +0400

    implemented write function using streams

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,11 @@
-const write = async () => {
-    // Write your code here 
-};
+import fs from "fs"
 
-await write();
+const write = async () => {
+  // Write your code here
+  const path = "src/streams/files/fileToWrite.txt"
+  const fileStream = fs.createWriteStream(path)
+  const dataStream = process.stdin
+  dataStream.on("data", chunk => fileStream.write(chunk))
+}
+
+await write()
