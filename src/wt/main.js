a3b387d834ac5ccdf38e4972a13e5601cd8d6198
ToyBoy <lary911@Ya.ru>
2022-12-04 05:35:24 +0300

    feat: add solving to wt folder
    Took 54 seconds

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,39 @@
+import { Worker } from "worker_threads";
+import path from "path";
+import url from "url";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const pathToWorker = path.join(__dirname, "worker.js");
+const cpus = process.env.NUMBER_OF_PROCESSORS;
 const performCalculations = async () => {
-    // Write your code here
+  try {
+    let num = 10;
+    const arrOfPromisesWorkers = [];
+
+    for (let i = 0; i < cpus; i++) {
+      arrOfPromisesWorkers.push(
+        new Promise((resolve) => {
+          new Worker(pathToWorker, { workerData: num + i })
+            .on("message", (res) => {
+              console.log(res);
+              resolve([{ status: "resolve", data: res }]);
+            })
+            .on("error", (err) => {
+              resolve([{ status: "error", data: null }]);
+            });
+        })
+      );
+    }
+
+    const result = await Promise.all(arrOfPromisesWorkers);
+
+    console.log(result);
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 await performCalculations();
