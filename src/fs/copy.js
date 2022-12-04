7dded77587b24e5222ee8e574f51e219fc1435bd
Ash Karapetyan <ashot.karapetyan.y@gmail.com>
2022-12-05 02:37:15 +0300

    Implemented copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,12 @@
+import { cp } from 'fs'
+
 const copy = async () => {
-    // Write your code here 
-};
+    cp("./files", './files_copy', { 'recursive': true, 'force': false, 'errorOnExist': true}, (err)=>{
+        if (err && err.info.code === 'EEXIST') {
+            throw new Error('FS operation failed');
+        }
+        console.log('Folder copied Successfully!');
+    })
+}
 
 copy();
