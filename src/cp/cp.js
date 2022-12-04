06b54d384e2ba4a1a8c2371a0cb46c7becdbec8f
ToyBoy <lary911@Ya.ru>
2022-12-04 05:36:35 +0300

    feat: add solving to cp folder
    Took 1 minute

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,16 @@
+import url from "url";
+import path from "path";
+import child_process from "child_process";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const pathToFile = path.join(__dirname, "files", "script.js");
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = await child_process.fork(pathToFile, args);
+
+  child.on("exit", () => console.log("Child process terminated"));
 };
 
-spawnChildProcess();
+await spawnChildProcess([123, true, "false"]);
