fb55f461e1af4c4a83e64dc5d1d6df6a378ae419
Vyacheslav <Smolensky-VO@rshbext.ru>
2022-12-04 20:46:11 +0300

    feat(zip): add compress func

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import {createGzip} from 'zlib';
+import {createReadStream, createWriteStream} from 'fs';
+import {pipeline} from 'stream';
+import {fileURLToPath} from "url";
+import {createCorrectPath} from "../helper.js";
+
 const compress = async () => {
-    // Write your code here 
+    const _filename = fileURLToPath(import.meta.url)
+
+    try {
+        const rs = createReadStream(createCorrectPath(_filename, 'files', 'fileToCompress.txt'));
+        const ws = createWriteStream(createCorrectPath(_filename, 'files', 'archive.gz'));
+        const gzip = createGzip();
+        pipeline(rs, gzip, ws, (error) => {
+            if (error) console.log('pipeline failed')
+        });
+    } catch (error) {}
 };
 
 await compress();
