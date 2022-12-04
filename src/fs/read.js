28bc62824fc3712ca914f84dc48daa39aab90b02
Evgen Orlovski <orloffski@gmail.com>
2022-12-04 05:41:06 +0300

    feat: implement read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,27 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { access, constants, readFile } from 'node:fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __fileToRead = path.dirname(__filename) + '/files/fileToRead.txt';
+const __errorMessage = 'FS operation failed';
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const ifFileExist = 
+            await access(__fileToRead, constants.R_OK)
+                .then(value => true)
+                .catch(err => false);
+
+        if(!ifFileExist){
+            throw Error(__errorMessage);
+        }else{
+            const contents = await readFile(__fileToRead, { encoding: 'utf8' });
+            console.log(contents);
+        }
+    }catch (e) {
+        console.log(e);
+    }
 };
 
 await read();
