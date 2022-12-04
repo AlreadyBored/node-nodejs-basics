39f257d2b5d8ed65518aee36eddc51871534c5b3
potyoma <artyomnovosyolov254@gmail.com>
2022-12-04 15:12:52 +0400

    implemented transform stream and function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,30 @@
-const transform = async () => {
-    // Write your code here 
-};
+import { Transform } from "stream"
 
-await transform();
+class TextToEmojis extends Transform {
+  constructor(emoji) {
+    super()
+    this.emoji = emoji
+  }
+
+  _transform(chunk, encoding, callback) {
+    const transformChunk = [...chunk].map(_ => this.emoji).join("")
+    this.push(transformChunk)
+    callback()
+  }
+
+  _flush(callback) {
+    callback()
+  }
+}
+
+const transform = async () => {
+  // Write your code here
+  const inStream = process.stdin
+  const outStream = process.stdout
+  const emoji = "🎄"
+  const transformStream = new TextToEmojis(emoji)
+
+  inStream.pipe(transformStream).pipe(outStream)
+}
+
+await transform()
