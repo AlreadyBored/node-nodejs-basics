70fb100b0a8d7de22ee95369de17b5996772ad94
Nikita Pytalev <Nikita_Pytalev@epam.com>
2022-12-04 02:05:37 +0100

    fix: remove utils

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,6 +1,9 @@
 import fs from 'fs/promises';
 import path from 'path';
-import { dirname } from '../utils.js';
+import {fileURLToPath} from 'url';
+
+export const filename = fileURLToPath(import.meta.url);
+export const dirname = path.dirname(filename);
 
 export const create = async () => {
     try{
--- src/utils.js
+++
@@ -1,5 +0,0 @@
-import path from 'path';
-import {fileURLToPath} from 'url';
-
-export const filename = fileURLToPath(import.meta.url);
-export const dirname = path.dirname(filename);
