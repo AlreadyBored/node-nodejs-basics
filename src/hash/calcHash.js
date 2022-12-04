7b1132271218ed10406efd6a587391b0658c431c
potyoma <artyomnovosyolov254@gmail.com>
2022-12-04 14:40:19 +0400

    hash function implementation

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+import path from "path"
+import { fileURLToPath } from "url"
+import fs from "fs/promises"
+import crypto from "crypto"
 
-await calculateHash();
+const calculateHash = async () => {
+  // Write your code here
+  const dir = path.dirname(fileURLToPath(import.meta.url))
+  const filePath = path.join(dir, "files", "fileToCalculateHashFor.txt")
+  const buffer = await fs.readFile(filePath)
+  const hashBuffer = await crypto.subtle.digest("SHA-256", buffer)
+  const asHex = [...new Uint8Array(hashBuffer)]
+    .map(h => h.toString(16).padStart(2, "0"))
+    .join("")
+  console.log(asHex)
+}
+
+await calculateHash()
