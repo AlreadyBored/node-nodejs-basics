bf9117903d384382e6800e016961a4c5f1a79cbe
ShotAlex <shot.aleks@gmail.com>
2022-12-04 13:18:31 +0300

    feat: add streams/read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import fs from "fs";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
 const read = async () => {
-    // Write your code here 
+    const currentPath = path.dirname(fileURLToPath(import.meta.url));
+    const filePath = currentPath + '/files/fileToRead.txt';
+
+    const readable = fs.createReadStream(filePath);
+
+    readable.on('readable', () => {
+        let chunk;
+        while (null !== (chunk = readable.read())) {
+            process.stdout.write(chunk);
+        }
+    });
 };
 
 await read();
