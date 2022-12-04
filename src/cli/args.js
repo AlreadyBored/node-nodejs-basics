f8ade904a2074155e545bf44f1c13d2e6ece3ae7
propatch <propatchme@gmail.com>
2022-12-05 00:24:51 +0300

    feat: implenitation arg.slice

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+  // Write your code here
+  const recieveAgrs = process.argv.slice(2);
+  console.log(recieveAgrs);
+  recieveAgrs.forEach((arg) => {
+    if (arg.startsWith("--")) {
+      const transformArg = arg.slice(2);
+    }
+  });
 };
 
 parseArgs();
