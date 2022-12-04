f18284bd4181c4300c091036566f3e2697341042
vadimdudko <v.dudko93@gmail.com>
2022-12-04 11:08:15 +0300

    init: start task env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+    // Write your code here
+
+    Object.keys(process.env).map((str) => {
+        if (str.slice(0, 4).includes('RSS_')) {
+            console.log(str)
+        }
+    })
 };
 
 parseEnv();
