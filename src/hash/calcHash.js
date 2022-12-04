5ef69ec949fcbf2279b0549176f51b27c8557048
Evgen Orlovski <orloffski@gmail.com>
2022-12-04 21:32:19 +0300

    feat: implement calculateHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { readFile } from 'node:fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __fileToHash = path.dirname(__filename) + '/files/fileToCalculateHashFor.txt';
+const { createHash } = await import('node:crypto');
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256');
+
+    const contents = await readFile(__fileToHash, { encoding: 'utf8' });
+    hash.update(contents);
+    console.log(hash.digest('hex'));
 };
 
 await calculateHash();
