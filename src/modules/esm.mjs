5a57a2fc2a85267f6bdf6020d3ae80b4fd3cd66f
Evgen Orlovski <orloffski@gmail.com>
2022-12-04 08:11:49 +0300

    refactor: rename file cjsToEsm.cjs to esm.mjs feat: implement const calculateHash function

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,24 +1,26 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path from 'path';
+import { release, version } from 'os';
+import { createServer as createServerHttp } from 'http';
+import './files/c.js';
+import jsonA from './files/a.json' assert { type: 'json' };
+import jsonB from './files/b.json' assert { type: 'json' };
 
 const random = Math.random();
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+    unknownObject = jsonA;
 } else {
-    unknownObject = require('./files/b.json');
+    unknownObject = jsonB;
 }
 
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
 console.log(`Path segment separator is "${path.sep}"`);
 
-console.log(`Path to current file is ${__filename}`);
-console.log(`Path to current directory is ${__dirname}`);
+console.log(`Path to current file is ${global.__filename}`);
+console.log(`Path to current directory is ${global.__dirname}`);
 
 const myServer = createServerHttp((_, res) => {
     res.end('Request accepted');
@@ -33,7 +35,7 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
+export {
     unknownObject,
     myServer,
 };
