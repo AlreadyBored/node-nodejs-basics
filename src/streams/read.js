c969aab7b453a88d210e86dae80392a8605d161d
Nikita Pytalev <Nikita_Pytalev@epam.com>
2022-12-04 13:34:01 +0100

    feat: implement read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,14 @@
+import { open } from 'fs/promises';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+export const filename = fileURLToPath(import.meta.url);
+export const dirname = path.dirname(filename);
+
 export const read = async () => {
-    // Write your code here 
+    const fd = await open(path.join(dirname, 'files', 'fileToRead.txt'));
+    const stream = fd.createReadStream();
+    stream.pipe(process.stdout);
 };
+
+await read();
