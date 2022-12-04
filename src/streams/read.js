ea8c2ec4aa4e32a14532b99895dc896cf3388399
potyoma <artyomnovosyolov254@gmail.com>
2022-12-04 14:50:10 +0400

    implemented read function using stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,10 @@
-const read = async () => {
-    // Write your code here 
-};
+import fs from "fs"
 
-await read();
+const read = async () => {
+  // Write your code here
+  const path = "src/streams/files/fileToRead.txt"
+  const readable = fs.createReadStream(path)
+  readable.on("data", chunk => process.stdout.write(chunk))
+}
+
+await read()
