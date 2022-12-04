83515316958cfe78d109c00dc3001fe996a632eb
Tomas Zagorskis <tomzagos@gmail.com>
2022-12-04 15:59:22 +0200

    feat: create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import { existsSync, writeFile } from 'fs';
+
+const path = './files/fresh.txt';
+
 const create = async () => {
-    // Write your code here 
+	try {
+		if (existsSync(path)) {
+			throw new Error('FS operation failed');
+		}
+		writeFile(path, 'I am fresh and young', err => {
+			if (err) throw new Error('FS operation failed');
+		});
+	} catch (err) {
+		console.error(err);
+	}
 };
 
 await create();
