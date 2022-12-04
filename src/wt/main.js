9580ba60ea531a8a38082ff1547d56ae28c97e61
PalinaZinouyeva <bozkovapa@gmail.com>
2022-12-04 23:49:42 +0400

    workers

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,47 @@
+import { Worker } from 'worker_threads';
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+import { cpus } from "os";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+function createWorker(data) {
+    return new Promise((res, rej) => {
+        const worker = new Worker(resolve(__dirname, './worker.js'));
+
+        worker.postMessage(data);
+        worker.once('message', response => {
+            res(response);
+            worker.terminate();
+        });
+        worker.once('error', () => {
+            rej();
+            worker.terminate();
+        });
+        worker.once('exit', (code) => {
+            if (code !== 0) {
+                rej(new Error(`Worker stopped with exit code ${code}`));
+            }
+        });
+
+        return worker;
+    })
+}
+
 const performCalculations = async () => {
-    // Write your code here
+    const data = cpus().map((_, index) => createWorker(10 + index));
+
+    await Promise.allSettled(data)
+        .then(data => console.log(mapData(data)));
 };
+
+const mapData = (data) => {
+    return data.map(item => {
+        const succeed = item.status === 'fulfilled';
+
+        return succeed ? { status: 'resolved', data: item.value } : { status: 'error', data: null };
+    })
+}
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,13 @@
-// n should be received from main thread
+import { parentPort } from "worker_threads";
+
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
-const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+const sendResult = async() => {
+    parentPort.on('message', data => {
+        const processedData = nthFibonacci(data);
+
+        parentPort.postMessage(processedData);
+    })
 };
 
-sendResult();
+await sendResult();
