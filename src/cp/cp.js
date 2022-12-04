539cabfdd1c17b69a57a3c3c786fa38b000d0bc7
yuranius <yuranius.webber@gmail.com>
2022-12-04 18:19:47 +0300

    child_process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,15 @@
+import { fork } from 'child_process'
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+
+    const childProcessFile = './src/cp/files/script.js';
+
+    const child = fork(childProcessFile, args.split(' '));
+
+    child.on('close', data => {
+        console.log('Child process closed with exit code: ', data);
+    })
+
 };
 
-spawnChildProcess();
+spawnChildProcess('arg0 arg1 arg2');
