9d706b979dae21655d10b4d471508258a0b1b706
Evgen Orlovski <orloffski@gmail.com>
2022-12-04 05:13:21 +0300

    feat: implement create function add: copyRecursive function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,42 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { access, constants, mkdir, lstat, readdir, copyFile } from 'node:fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __folderFromPath = path.dirname(__filename) + '/files';
+const __folderToPath = path.dirname(__filename) + '/files_copy';
+const __errorMessage = 'FS operation failed';
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        const ifFolderExists = 
+            await access(__folderToPath, constants.R_OK)
+                .then(value => true)
+                .catch(err => false)
+
+        if(ifFolderExists){
+            throw Error(__errorMessage);
+        }else{
+            await copyRecursive(__folderFromPath, __folderToPath);
+        }
+    }catch (e) {
+        console.log(e);
+    }
 };
+
+const copyRecursive = async(from, to) => {
+    await mkdir(to);
+    
+    const files = await readdir(from);
+
+    files.forEach(async(elem) => {
+        const stats = await lstat(path.join(from, elem));
+        if (stats.isFile()) {
+            copyFile(path.join(from, elem), path.join(to, elem));
+        } else {
+            copyRecursive(path.join(from, elem), path.join(to, elem));
+        }
+    })
+}
 
 copy();
