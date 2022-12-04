3d4933cf43250368262ffb6324473021442c11b6
PalinaZinouyeva <bozkovapa@gmail.com>
2022-12-04 20:30:58 +0400

    zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
-const compress = async () => {
-    // Write your code here 
+import fs from 'fs';
+import { dirname, resolve } from 'path';
+import { createGzip } from 'zlib';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compress = () => {
+    const input = fs.createReadStream(resolve(__dirname, 'files/fileToCompress.txt'), 'utf-8');
+    const output = fs.createWriteStream(resolve(__dirname, 'files/archive.txt.gz'));
+    const gzip = createGzip();
+
+    input.pipe(gzip).pipe(output);
 };
 
-await compress();
+compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,17 @@
-const decompress = async () => {
-    // Write your code here 
+import fs from 'fs';
+import { dirname, resolve } from 'path';
+import { createGunzip } from 'zlib';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const decompress = () => {
+    const input = fs.createReadStream(resolve(__dirname, 'files/archive.txt.gz'));
+    const output = fs.createWriteStream(resolve(__dirname, 'files/fileToDecompress.txt'), 'utf-8');
+    const gunzip = createGunzip();
+
+    input.pipe(gunzip).pipe(output);
 };
 
-await decompress();
+decompress();
