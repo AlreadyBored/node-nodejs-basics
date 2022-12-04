e37d4afdbc11a32d5386bf0ed67300e994235fce
Vyacheslav <Smolensky-VO@rshbext.ru>
2022-12-04 20:45:17 +0300

    feat(zip): add decompress func

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
+import {createUnzip} from 'zlib';
+import {createReadStream, createWriteStream} from 'fs';
+import {pipeline} from 'stream';
+import {fileURLToPath} from "url";
+import {createCorrectPath} from "../helper.js";
+
 const decompress = async () => {
-    // Write your code here 
+    const _filename = fileURLToPath(import.meta.url)
+
+    try {
+        const rs = createReadStream(createCorrectPath(_filename, 'files', 'archive.gz'));
+        const ws = createWriteStream(createCorrectPath(_filename, 'files', 'fileToCompress.txt'));
+        const unzip = createUnzip();
+        pipeline(rs, unzip, ws, (error) => {
+            if (error) console.log(error);
+        });
+    } catch (error) {}
 };
 
 await decompress();
