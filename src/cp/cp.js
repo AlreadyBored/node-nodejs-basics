a60f646773273cb6dc3d5b8315d91347ca48221b
Dimitry-prog <kfifa3@gmail.com>
2022-12-04 19:06:09 +0300

    feat: add cp-tasks

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,21 @@
+import { spawn } from 'node:child_process';
+import {stdin, stdout} from 'node:process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const path = './src/cp/files/script.js'
+    const node = spawn('ls', [path, ...args]);
+
+    node.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
+    stdin.pipe(node.stdin)
+    node.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+    stdout.pipe(node.stdout)
+    node.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
 };
 
-spawnChildProcess();
+spawnChildProcess(['tewte', '47457']);
