3393c9355a4a918e9efc0b33bb571671c7c0adf7
ShotAlex <shot.aleks@gmail.com>
2022-12-04 13:46:12 +0300

    feat: add stream/write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
-const write = async () => {
-    // Write your code here 
+import fs from "fs";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const testString = 'This file should be write using Streams API';
+
+const write = async (str = testString) => {
+    const currentPath = path.dirname(fileURLToPath(import.meta.url));
+    const filePath = currentPath + '/files/fileToWrite.txt';
+
+    const writeStream = fs.createWriteStream(filePath);
+
+    process.stdin.on("data", data => {
+        writeStream.write(data, 'utf8');
+    })
 };
 
 await write();
