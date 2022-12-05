0631562fc92443372211250f955aba128afca3f2
vkordovich <vadim_kordovich@epam.com>
2022-12-05 17:59:21 +0300

    wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,28 @@
-const performCalculations = async () => {
-    // Write your code here
-};
+import {Worker} from "worker_threads"
+import os from "os";
 
-await performCalculations();
+const performCalculations = async (data) => {
+    // Write your code here
+    return new Promise((resolve, reject) => {
+        const worker = new Worker('./src/wt/worker.js', {
+            workerData: data
+        })
+
+        worker.on('message', (msg) => {
+            resolve({
+                status: 'resolved',
+                data: msg
+            })
+        })
+
+        worker.on('error', () => {
+            reject({
+                status: 'error',
+                data: null
+            })
+        })
+    })
+};
+const cpuCoreValues = os.cpus().map((core, index) => performCalculations(10+index));
+const result = await Promise.all(cpuCoreValues);
+console.log(result)
