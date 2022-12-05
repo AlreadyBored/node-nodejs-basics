aa228b60c4721e2c988c87fd4eeaf593ba1e1061
Anton Gadiyatov <gadiyatov.an@gmail.com>
2022-12-05 14:49:33 +0300

    feat: added cli/env implementation

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envVars = Object.entries(process.env);
+
+  envVars.forEach(([key, value]) => {
+    if (key.startsWith("RSS")) {
+      console.log(`${key}=${value}`);
+    }
+  });
 };
 
 parseEnv();
