be019206a43fd80324de774e81c9b28e70d72d64
Anton Gadiyatov <gadiyatov.an@gmail.com>
2022-12-05 14:53:59 +0300

    feat: added streams/write implementation

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import { createWriteStream } from "node:fs";
+import { makePath } from "../utils/makePath.js";
+
+const path = makePath(import.meta.url, "/files/fileToWrite.txt");
+
 const write = async () => {
-    // Write your code here 
+  const writableStream = createWriteStream(path, "utf-8");
+
+  process.stdin.on("data", (data) => {
+    writableStream.write(data);
+  });
 };
 
 await write();
