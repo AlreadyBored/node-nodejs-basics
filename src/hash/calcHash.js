a8550a91a3047a0b010307e830f09c2649bc60dc
Evgeny-Chabrovsky <chabor111@gmail.com>
2022-12-05 21:58:41 +0300

    feat: add hash section

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { readFile } from 'node:fs/promises';
+import { createHash } from 'node:crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const fileBuffer = await readFile('./files/fileToCalculateHashFor.txt');
+    console.log('fileBuffer', fileBuffer);
+    const hashSum = createHash('sha256');
+    hashSum.update(fileBuffer);
+    const hex = hashSum.digest('hex');
+    console.log(hex);
+  } catch (err) {
+    console.error(err.message);
+  }
 };
 
 await calculateHash();
