7353ea509b59d91d51afe2eee8eaf54026df94b6
Tomas_Zagorskis <tomzagos@gmail.com>
2022-12-05 19:43:32 +0200

    feat: add hash calc functionality

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,12 @@
+import crypto from 'crypto';
+import fs from 'fs';
+
 const calculateHash = async () => {
-    // Write your code here 
+	fs.readFile('./files/fileToCalculateHashFor.txt', (err, data) => {
+		if (err) throw new Error('FS operation failed');
+		const hexVal = crypto.createHash('sha256').update(data).digest('hex');
+		console.log(hexVal);
+	});
 };
 
 await calculateHash();
