2c27768a10b292728b18f57253c9a7e07d424432
dmitriysemenko <dm123semenko@gmail.com>
2022-12-05 21:42:43 +0300

    feat: add create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import fs from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = __dirname + "/files/fresh.txt";
+const content = "I am fresh and young";
+const errorMessage = "FS operation failed";
+
 const create = async () => {
-    // Write your code here 
+    if (fs.existsSync(filePath)) {
+        throw new Error(errorMessage);
+    } else {
+        fs.writeFile(filePath, content, (error) => {
+            if (error) {
+                throw new Error(errorMessage);
+            }
+        });
+    }
 };
 
 await create();
