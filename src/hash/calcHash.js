1e3e74f627b5ea9d482e6430e14a38cba1c93ced
Ash Karapetyan <ashot.karapetyan.y@gmail.com>
2022-12-05 17:59:39 +0300

    Implemented hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import { readFile } from 'node:fs';
+import { resolve } from 'node:path';
+import { createHash } from 'node:crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const absolutePath = await resolve('files', 'fileToCalculateHashFor.txt');
+
+    await readFile(absolutePath, (err, data) => {
+        if (err) {
+            throw new Error('FS operation failed');
+        };
+        const hash = createHash('sha256');
+        const hex = hash.update(data).digest('hex');
+        console.log(hex);
+    });
 };
 
 await calculateHash();
