f8e556f71f3d309f80c9f6d61de1ce66724980c0
Anton Gadiyatov <gadiyatov.an@gmail.com>
2022-12-05 14:59:48 +0300

    feat: added wt/main and wt/worker implementations

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,29 @@
+import { Worker } from "node:worker_threads";
+import { cpus } from "node:os";
+import { makePath } from "../utils/makePath.js";
+
+const path = makePath(import.meta.url, "worker.js");
+
 const performCalculations = async () => {
-    // Write your code here
+  const cp = cpus();
+  const workersData = await Promise.allSettled(
+    cp.map((item, index) => {
+      return new Promise((resolve, reject) => {
+        const worker = new Worker(path, { workerData: 10 + index });
+        worker.on("message", (message) => resolve(message));
+        worker.on("error", (message) => reject(message));
+      });
+    })
+  );
+
+  const results = workersData.map(({ status, value }) => {
+    return {
+      status: status === "fulfilled" ? "resolved" : "error",
+      data: status === "fulfilled" ? value : null,
+    };
+  });
+
+  console.log(results);
 };
 
 await performCalculations();
