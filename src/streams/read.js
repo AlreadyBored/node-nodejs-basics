84853b52420746f890cb2933a5faec05259307ae
dmitriysemenko <dm123semenko@gmail.com>
2022-12-06 01:30:18 +0300

    feat: add streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,24 @@
+import fs from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { stdout } from "process";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const file = __dirname + "/files/fileToRead.txt";
+const errorMessage = "FS operation failed";
+
 const read = async () => {
-    // Write your code here 
+    console.log(file);
+    let fileData = [];
+    const stream = fs.ReadStream(file, "utf-8");
+    stream.on("data", (chunk) => {
+        fileData.push(chunk.toString());
+    });
+    stream.on("error", () => {
+        throw new Error(errorMessage);
+    });
+    stream.on("end", () => stdout.write(fileData.join("")));
 };
 
 await read();
