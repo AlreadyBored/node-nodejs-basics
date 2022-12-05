d761e3e117abe65862162d112836c069c519db80
Anton Gadiyatov <gadiyatov.an@gmail.com>
2022-12-05 14:53:13 +0300

    feat: added streams/read implementation

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import { createReadStream } from "node:fs";
+import { makePath } from "../utils/makePath.js";
+
+const path = makePath(import.meta.url, "/files/fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+  const readableStream = createReadStream(path, "utf-8");
+
+  readableStream.on("data", (data) => {
+    process.stdout.write(data + "\n");
+  });
 };
 
 await read();
