5530ccc2ad49d312dd44ec593710339269d2dc86
Tomas_Zagorskis <tomzagos@gmail.com>
2022-12-05 21:41:42 +0200

    feat: worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,23 @@
+import { Worker } from 'worker_threads';
+import * as os from 'os';
+
 const performCalculations = async () => {
-    // Write your code here
+	let answer = [];
+	os.cpus().forEach((_, i) => {
+		let worker = new Worker('./worker.js', { workerData: 10 + i });
+		answer.push(
+			new Promise(resolve => {
+				worker.on('message', obj => {
+					resolve((answer[i] = obj));
+				});
+				worker.on('error', () => {
+					resolve((answer[i] = { status: 'error', data: null }));
+				});
+			})
+		);
+	});
+
+	console.log(await Promise.all(answer));
 };
 
 await performCalculations();
