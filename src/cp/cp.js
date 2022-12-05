7a3a901da83309c16ffefb16483da62540356116
Павел Трипольский <paveltripolskij@MacBook-Air-Pavel.local>
2022-12-05 17:47:08 +0300

    feat: cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,14 @@
+import * as fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+import * as cp from 'child_process'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+	cp.fork(`${__dirname}/files/script.js`, args)
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['arg one', 'arg two']);
