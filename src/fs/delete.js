38960805cfaae4bc3ca6d961a99fda1a33f708ab
Bahdan Bivenis <biven4i.en@gmail.com>
2022-12-05 22:45:28 +0300

    feat: solve fs/delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import fsPromises from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const src = path.join(__dirname, "files", "fileToRemove.txt");
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    await fsPromises.rm(src);
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await remove();
