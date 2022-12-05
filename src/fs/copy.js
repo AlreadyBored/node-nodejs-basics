ede0e8d85ac3bf9f3117bf865cbd96be00bf4fce
dmitriysemenko <dm123semenko@gmail.com>
2022-12-05 22:35:46 +0300

    feat: add copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,23 @@
+import fs from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const copyFrom = __dirname + "/files/";
+const copyTo = __dirname + "/files_copy/";
+const errorMessage = "FS operation failed";
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        fs.cpSync(copyFrom, copyTo, {
+            recursive: true,
+            force: false,
+            errorOnExist: true,
+        });
+    } catch (error) {
+        throw new Error(errorMessage);
+    }
 };
 
 await copy();
