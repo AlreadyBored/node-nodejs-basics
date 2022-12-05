06bbf4f80e4d3671e98a5af2569f33e7f3a243c7
Павел Трипольский <paveltripolskij@MacBook-Air-Pavel.local>
2022-12-05 16:03:28 +0300

    feat: zip/unzip pipes

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import * as fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+import * as zlib from 'zlib'
+
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const compress = async () => {
-    // Write your code here 
+	let readStream = fs.createReadStream(`${__dirname}/files/fileToCompress.txt`)
+	let writeStream = fs.createWriteStream(`${__dirname}/files/archive.gz`)
+	let gZip = zlib.createGzip()
+	readStream.pipe(gZip).pipe(writeStream)
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,17 @@
+import * as fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+import * as zlib from 'zlib'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const decompress = async () => {
-    // Write your code here 
+	let readStream = fs.createReadStream(`${__dirname}/files/archive.gz`)
+	let writeStream = fs.createWriteStream(`${__dirname}/files/fileToCompress.txt`)
+	const unzip = zlib.createGunzip();
+
+	readStream.pipe(unzip).pipe(writeStream)
 };
 
 await decompress();
