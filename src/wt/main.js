fe771a1d53226657f26cb05a3c5c25fe6c96a516
Павел Трипольский <paveltripolskij@MacBook-Air-Pavel.local>
2022-12-05 21:52:25 +0300

    feat: worker thread

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,33 @@
+import * as os  from 'os'
+import { Worker } from 'worker_threads'
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const performCalculations = async () => {
-    // Write your code here
+	let cp = os.cpus()
+	let num = 10
+
+
+	const workers = cp.map(() => {
+		return new Promise((resolve, reject) => {
+			const worker = new Worker(`${__dirname}/worker.js`, {
+				workerData: num++
+			})
+
+			worker.on('message', (msg) => resolve(msg))
+			worker.on('error', msg => reject(msg))
+		})
+
+	})
+
+	let result = await Promise.all(workers)
+	console.log(result)
+	return result
+
+	// return Promise.all(workers).then(data => console.log(data))
 };
 
 await performCalculations();
