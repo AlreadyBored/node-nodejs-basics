23d9b831b6899baeb345ba62c69c38264cd3ebd4
Evgen Orlovski <orloffski@gmail.com>
2022-12-05 22:22:29 +0300

    fix: implement __dirname, __filename constants

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,9 +1,13 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { release, version } from 'os';
 import { createServer as createServerHttp } from 'http';
 import './files/c.js';
 import jsonA from './files/a.json' assert { type: 'json' };
 import jsonB from './files/b.json' assert { type: 'json' };
+
+const __filename = path.basename(fileURLToPath(import.meta.url));
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const random = Math.random();
 
@@ -19,8 +23,8 @@ console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
 console.log(`Path segment separator is "${path.sep}"`);
 
-console.log(`Path to current file is ${global.__filename}`);
-console.log(`Path to current directory is ${global.__dirname}`);
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
 
 const myServer = createServerHttp((_, res) => {
     res.end('Request accepted');
