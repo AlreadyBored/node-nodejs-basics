d1c9f86aff718944278b215f1e39f77e90402034
Tomas_Zagorskis <tomzagos@gmail.com>
2022-12-05 21:48:39 +0200

    feat: child processes

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,16 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { spawn } from 'child_process';
+
+const spawnChildProcess = async args => {
+	const child = spawn('node', ['./files/script.js', ...args]);
+
+	process.stdin.on('data', data => {
+		child.stdin.write(data);
+	});
+
+	child.stdout.on('data', data => {
+		console.log(data.toString());
+	});
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['--arg1', '--arg2']);
