a375942fd5c157caec8cf5013cb6065381652576
Andrew Gerasimchik <gerasimchick@tut.by>
2022-12-05 03:26:23 +0200

    feat: add cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,30 @@
+import { spawn } from 'node:child_process'
+import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
 const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+    const childScript = join(
+        dirname(fileURLToPath(import.meta.url)),
+        'files',
+        'script.js'
+    )
+    console.log('Spawn child process. To close child type "CLOSE"')
+    const child = spawn('node', [childScript, ...args])
 
-spawnChildProcess();
+    process.stdin.on('data', (data) => {
+        console.log('This string send to child process ', data.toString())
+        child.stdin.write(data)
+    })
+
+    child.stdout.on('data', (data) => {
+        console.log(
+            'This string received from child process: ',
+            data.toString()
+        )
+    })
+
+    child.on('close', (code) => {
+        console.log('Child process closed with code ', code)
+    })
+}
+
+spawnChildProcess(['--RSS-Arg', '--RSS-Data', '--RSS-Option'])
