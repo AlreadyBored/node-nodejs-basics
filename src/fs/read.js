105a7a24e8d1c7c4d1d0211d17c2f928a8694ea7
Bahdan Bivenis <biven4i.en@gmail.com>
2022-12-05 22:45:47 +0300

    feat: solve fs/read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import fsPromises from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const src = path.join(__dirname, "files", "fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const content = await fsPromises.readFile(src, { encoding: "utf-8" });
+    console.log(content);
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await read();
