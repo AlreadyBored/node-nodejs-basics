260cb24024510e04e93a2ee080fbc1e98db79ab7
Andrey <aalisenak@gmail.com>
2022-12-06 00:33:44 +0300

    done:remove tast

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,21 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const dirName = path.dirname(fileURLToPath(import.meta.url));
+const removeFile = path.join(dirName, 'files', 'fileToRemove.txt');
+const errorMsg = 'FS operation failed';
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    await fs.rm(removeFile, (err) => {
+      if (err) throw new Error();
+    });
+  } catch (err) {
+    if (err.code === 'EEXIST') {
+      console.error(Error(errorMsg));
+    }
+  }
 };
 
 await remove();
