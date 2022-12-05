6fd161890e84f0d6fe94ea028fea8f38770f8e3f
propatch <34714410+propatch@users.noreply.github.com>
2022-12-05 16:17:42 +0300

    feat: implement function that calculates SHA256 hash for file

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,26 @@
+import fs from "node:fs/promises";
+import path from "path";
+
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    const { createHash } = await import("node:crypto");
+    const readSourceFile = await fs.readFile(
+      __dirname + "/files/fileToCalculateHashFor.txt",
+      "utf8"
+    );
+    console.log(readSourceFile);
+    const hash = createHash("sha256").update(readSourceFile).digest("hex");
+
+    console.log(hash);
+  } catch (err) {
+    throw err;
+  }
 };
 
 await calculateHash();
