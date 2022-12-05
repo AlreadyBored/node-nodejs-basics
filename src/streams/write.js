2ec295020236e44dcfa29ff1e8c0af42d27454b7
Evgen Orlovski <orloffski@gmail.com>
2022-12-05 22:43:44 +0300

    fix: change write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,14 +1,29 @@
 import { createWriteStream } from 'node:fs'
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { access, constants } from 'node:fs/promises';
 
 const __filename = fileURLToPath(import.meta.url);
 const __fileToWrite = path.dirname(__filename) + '/files/fileToWrite.txt';
+const __errorMessage = 'STREAMS operation failed: file not found';
 
 const write = async () => {
-    const writeFile = createWriteStream(__fileToWrite);
+    try{
+        const ifFileToWriteExists = 
+            await access(__fileToWrite, constants.R_OK)
+                .then(value => true)
+                .catch(err => false)
 
-    process.stdin.pipe(writeFile);
+        if(!ifFileToWriteExists){
+            throw Error(__errorMessage);
+        }
+
+        const writeFile = createWriteStream(__fileToWrite);
+
+        process.stdin.pipe(writeFile);
+    }catch(e){
+        console.error(e.message);
+    }
 };
 
 await write();
