ba562628e6dd5f3cac8bd93d4294cd923222b6be
Evgeny-Chabrovsky <chabor111@gmail.com>
2022-12-05 21:54:35 +0300

    feat: add command line interface section

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+  const argsObjects = [];
+
+  process.argv.slice(2).forEach((item, index) => {
+    if (index % 2 === 0) {
+      argsObjects.push(
+        `${[...item.slice(2)].join('')} is ${process.argv.slice(2)[index + 1]}`
+      );
+    }
+  });
+
+  console.log(argsObjects.join(', '));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+  const rssVariables = [];
+
+  for (let variable of Object.entries(process.env)) {
+    if (variable[0].includes('RSS_')) {
+      rssVariables.push(`${variable[0]}=${variable[1]}`);
+    }
+  }
+  console.log(rssVariables.join('; '));
 };
 
 parseEnv();
