936096d92b75650d0555d791466979b1984b741f
Paul Grin <pashkagrin@gmail.com>
2022-12-06 01:59:23 +0300

    zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,34 @@
+import zlib from "zlib";
+import fs from "fs";
+import fsPromises from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+
 const compress = async () => {
-    // Write your code here 
+  const compressFilePath = path.resolve(
+    __dirname,
+    "./files",
+    "fileToCompress.txt"
+  );
+  await fsPromises.access(
+    compressFilePath,
+    fs.constants.F_OK,
+    (error, data) => {
+      if (error) {
+        console.log("File does not exist");
+      }
+    }
+  );
+
+  const inp = fs.createReadStream(compressFilePath);
+  const out = fs.createWriteStream(
+    path.resolve(__dirname, "./files", "archive.gz")
+  );
+  const zip = zlib.createGzip();
+
+  inp.pipe(zip).pipe(out);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,31 @@
+import zlib from "zlib";
+import fs from "fs";
+import fsPromises from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+
 const decompress = async () => {
-    // Write your code here 
+  const decompressFilePath = path.resolve(__dirname, "./files", "archive.gz");
+
+  await fsPromises.access(
+    decompressFilePath,
+    fs.constants.F_OK,
+    (error, data) => {
+      if (error) {
+        console.log("File does not exist");
+      }
+    }
+  );
+
+  const inp = fs.createReadStream(decompressFilePath);
+  const out = fs.createWriteStream(
+    path.resolve(__dirname, "./files", "textToCompress.txt")
+  );
+  const unzip = zlib.createUnzip();
+
+  inp.pipe(unzip).pipe(out);
 };
 
 await decompress();
