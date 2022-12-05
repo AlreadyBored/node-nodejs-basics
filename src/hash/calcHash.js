d7f4bb8859443dd43bb579274cbff3645b30b80d
Anton Gadiyatov <gadiyatov.an@gmail.com>
2022-12-05 14:52:20 +0300

    feat: added hash/caclHash implementation

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,14 @@
+import { createHash } from "node:crypto";
+import { readFileSync } from "node:fs";
+import { makePath } from "../utils/makePath.js";
+
+const path = makePath(import.meta.url, "/files/fileToCalculateHashFor.txt");
+
 const calculateHash = async () => {
-    // Write your code here 
+  const buff = readFileSync(path);
+  const hash = createHash("sha256").update(buff).digest("hex");
+
+  console.log(hash);
 };
 
 await calculateHash();
