474ce6db840e359964e1f430cb1749f1f5853aad
dmitriysemenko <dm123semenko@gmail.com>
2022-12-06 01:00:17 +0300

    feat: add cjsToEsm.mjs

--- src/modules/cjsToEsm.mjs
+++ src/modules/cjsToEsm.mjs
@@ -1,16 +1,20 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path from "path";
+import { release, version } from "os";
+import { createServer as createServerHttp } from "http";
+import { fileURLToPath } from "url";
+import * as jsonA from "./files/a.json" assert { type: "json" };
+import * as jsonB from "./files/b.json" assert { type: "json" };
+import * as c from "./files/c.js";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const random = Math.random();
-
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+    unknownObject = jsonA;
 } else {
-    unknownObject = require('./files/b.json');
+    unknownObject = jsonB;
 }
 
 console.log(`Release ${release()}`);
@@ -21,7 +25,7 @@ console.log(`Path to current file is ${__filename}`);
 console.log(`Path to current directory is ${__dirname}`);
 
 const myServer = createServerHttp((_, res) => {
-    res.end('Request accepted');
+    res.end("Request accepted");
 });
 
 const PORT = 3000;
@@ -30,11 +34,10 @@ console.log(unknownObject);
 
 myServer.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
-    console.log('To terminate it, use Ctrl+C combination');
+    console.log("To terminate it, use Ctrl+C combination");
 });
 
 module.exports = {
     unknownObject,
     myServer,
 };
-
