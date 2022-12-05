ade5c440676e7ba069d737405c16ce094143c9b6
Anton Gadiyatov <gadiyatov.an@gmail.com>
2022-12-05 14:48:40 +0300

    feat: added fs/read implementation

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import { readFile } from "node:fs/promises";
+import { ERR_DATA } from "../const/error.js";
+import { makePath } from "../utils/makePath.js";
+
+const path = makePath(import.meta.url, "/files/fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const data = await readFile(path, { encoding: "utf-8" });
+    console.log(data);
+  } catch (err) {
+    throw new Error(ERR_DATA);
+  }
 };
 
 await read();
