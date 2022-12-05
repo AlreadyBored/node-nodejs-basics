387a0ca1379fec5498bdbef84e546ffd1c1baba6
Bahdan Bivenis <biven4i.en@gmail.com>
2022-12-05 22:49:32 +0300

    feat: solve streams/write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import { createWriteStream } from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const src = path.join(__dirname, "files", "fileToWrite.txt");
+
 const write = async () => {
-    // Write your code here 
+  const output = createWriteStream(src);
+  process.stdin.on("data", (chunk) => {
+    output.write(chunk);
+  });
 };
 
 await write();
