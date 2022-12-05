f03005938ebb9d6ca11d9e2d32aa64f56ce08a3a
dmitriysemenko <dm123semenko@gmail.com>
2022-12-05 23:36:32 +0300

    feat: add read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import fs from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const file = __dirname + "/files/fileToRead.txt";
+const errorMessage = "FS operation failed";
+
 const read = async () => {
-    // Write your code here 
+    fs.readFile(file, "utf-8", (error, data) => {
+        if (error) {
+            throw new Error(errorMessage);
+        } else {
+            console.log(data.toString());
+        }
+    });
 };
 
 await read();
