c87f11052ec60a83e472c4a9184580e84574d14b
propatch <propatchme@gmail.com>
2022-12-05 21:08:52 +0300

    feat: implement function that reads data from process.stdin

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,21 @@
+import { pipeline, Transform } from "stream";
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const transform = async () => {
-    // Write your code here 
+  // Write your code here
+  const transformText = new Transform({
+    transform(chunk, encoding, callback) {
+      callback(null, chunk.toString().split("").reverse().join("") + "\n");
+    },
+  });
+  pipeline(process.stdin, transformText, process.stdout, (err) =>
+    console.error(err)
+  );
 };
 
 await transform();
