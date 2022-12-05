0b770a16e2c5e5865af7ac39984e7b4b90295d3d
Anton Gadiyatov <gadiyatov.an@gmail.com>
2022-12-05 14:45:24 +0300

    feat: added fs/delete implementation

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,15 @@
+import { rm } from "node:fs/promises";
+import { ERR_DATA } from "../const/error.js";
+import { makePath } from "../utils/makePath.js";
+
+const path = makePath(import.meta.url, "files/fileToRemove.txt");
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    await rm(path);
+  } catch (err) {
+    throw new Error(ERR_DATA);
+  }
 };
 
 await remove();
