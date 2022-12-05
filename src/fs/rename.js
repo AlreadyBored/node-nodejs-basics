d1e6e83887127b7899b8672fbff7a2365ec5db29
Oops14 <ups14q@gmail.com>
2022-12-05 22:19:49 +0200

    solved rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,17 @@
+import fs from 'fs';
+
 const rename = async () => {
-    // Write your code here 
+    fs.access('./src/fs/files/properFilename.md', (err) => {
+		if (err) {
+            fs.rename('./src/fs/files/wrongFilename.txt', './src/fs/files/properFilename.md', function(err) {
+                if (err) throw err;
+                console.log('Rename complete!');
+            });
+
+		} else {
+			console.log(new Error('FS operation failed'));
+		}
+	});
 };
 
 await rename();
