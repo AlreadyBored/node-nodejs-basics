5f160ff15eb6bdf3b536cab4190608029f03e58f
Anton Gadiyatov <gadiyatov.an@gmail.com>
2022-12-05 14:40:33 +0300

    feat: added fs/create implementation

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import { writeFile } from "node:fs/promises";
+import { ERR_DATA } from "../const/error.js";
+import { makePath } from "../utils/makePath.js";
+
+const DATA = "I am fresh and young";
+
+const path = makePath(import.meta.url, "/files/fresh.txt");
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await writeFile(path, DATA, { flag: "wx" });
+  } catch (err) {
+    throw new Error(ERR_DATA);
+  }
 };
 
 await create();
