dad4c3ce6d0dd38f951cb2ae279d9df048231a37
Evgen Orlovski <orloffski@gmail.com>
2022-12-05 21:59:48 +0300

    fix: change decompress function

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -4,18 +4,44 @@ import { createGunzip } from 'node:zlib';
 import  { pipeline } from 'node:stream';
 import { promisify } from 'node:util';
 import { createReadStream, createWriteStream } from 'node:fs';
+import { access, constants} from 'node:fs/promises';
 
 const __filename = fileURLToPath(import.meta.url);
 const __fileAfterDecompress = path.dirname(__filename) + '/files/fileToCompress.txt';
 const __fileToDecompress = path.dirname(__filename) + '/files/archive.gz';
+const __errorMessage = 'ZIP operation failed';
+const __errorFileNotExists = ': file archive.gz not exists';
+const __errorFileExists = ': file fileToCompress.txt exists';
 
 const decompress = async () => {
-    const gunzip = createGunzip();
-    const pipe = promisify(pipeline);
-    const source = createReadStream(__fileToDecompress);
-    const destination = createWriteStream(__fileAfterDecompress);
+    try{
+        const ifFileToDecompressExists = 
+            await access(__fileToDecompress, constants.R_OK)
+                .then(value => true)
+                .catch(err => false);
 
-    await pipe(source, gunzip, destination);
+        const ifFileAfterDecompressExists = 
+                await access(__fileAfterDecompress, constants.R_OK)
+                    .then(value => true)
+                    .catch(err => false);
+
+        if(!ifFileToDecompressExists){
+            throw Error(__errorMessage + __errorFileNotExists);
+        }
+            
+        if(ifFileAfterDecompressExists){
+            throw Error(__errorMessage + __errorFileExists);
+        }
+
+        const gunzip = createGunzip();
+        const pipe = promisify(pipeline);
+        const source = createReadStream(__fileToDecompress);
+        const destination = createWriteStream(__fileAfterDecompress);
+
+        await pipe(source, gunzip, destination);
+    }catch(e){
+        console.error(e.message);
+    }
 };
 
 await decompress();
