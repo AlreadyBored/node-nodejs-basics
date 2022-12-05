c4e123ba1b122eb6218e812c6b8edfd3bef08bb1
Bahdan Bivenis <biven4i.en@gmail.com>
2022-12-05 22:49:44 +0300

    feat: solve streams/transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,19 @@
+import { Transform } from "node:stream";
+
+class TransformReverse extends Transform {
+  _transform(chunk, encoding, callback) {
+    try {
+      const resultSring = chunk.toString("utf8").split("").reverse().join("") + '\n';
+      callback(null, resultSring);
+    } catch (err) {
+      callback(err);
+    }
+  }
+}
+
 const transform = async () => {
-    // Write your code here 
+  const transformReverse = new TransformReverse();
+  process.stdin.pipe(transformReverse).pipe(process.stdout);
 };
 
 await transform();
