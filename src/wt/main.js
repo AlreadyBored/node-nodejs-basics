13cd41db8fd26df150a75986735b84b2ac36bd49
aNNiPAk <annipak@yandex.ru>
2022-12-05 22:45:39 +0300

    feat: task 'Worker Threads' solved

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,27 @@
+import { cpus } from 'os'
+import { Worker } from 'worker_threads'
+import { getCombinedPath } from '../pathHelper.js'
+
 const performCalculations = async () => {
-    // Write your code here
+    const pathToFile = getCombinedPath(import.meta.url, 'worker.js')
+    let startNumber = 10
+
+    const workersResults = await Promise.allSettled(cpus().map(() => {
+        return new Promise((resolve, reject) => {
+            const worker = new Worker(pathToFile, {
+                workerData: startNumber++
+            })
+
+            worker.on('message', resolve)
+            worker.on('error', reject)
+        })
+    }))
+
+    const results = workersResults.map(({ status, value }) => ({
+        status: status === 'fulfilled' ? 'resolved' : 'error',
+        data: status === 'fulfilled' ? value : null
+    }))
+    console.log(results)
 };
 
 await performCalculations();
