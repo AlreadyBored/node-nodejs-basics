705c02679461d3dd43cf6a8c017650e29d6b12bb
propatch <34714410+propatch@users.noreply.github.com>
2022-12-05 11:08:23 +0300

    implement function that parses command line args and print

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,12 +1,15 @@
 const parseArgs = () => {
   // Write your code here
   const recieveAgrs = process.argv.slice(2);
-  console.log(recieveAgrs);
-  recieveAgrs.forEach((arg) => {
+  const acc = [];
+  const newArgs = recieveAgrs.forEach((arg, i, arr) => {
     if (arg.startsWith("--")) {
-      const transformArg = arg.slice(2);
+      const transformArg = arg.slice(2) + " is " + arr[i + 1];
+      acc.push(transformArg);
     }
+    return acc;
   });
+  console.log(acc.join(", "));
 };
 
 parseArgs();
