836e9dbc1f32f99e5f46016be5b92acbcebae760
Aliaksandr Okrul <aokrul@mts.by>
2022-12-05 13:32:09 +0300

    fix: changed filename

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,7 +1,7 @@
 import { writeFile } from 'node:fs/promises';
 
 const create = async () => {
-    await writeFile("1.txt", "I am fresh and young", {flag: "wx"});
+    await writeFile("fresh.txt", "I am fresh and young.", {flag: "wx"});
 };
 
 await create();
