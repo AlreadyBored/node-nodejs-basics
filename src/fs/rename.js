40d27a858e4c04b6b095c49ebec9ba5287028b62
Anton Gadiyatov <gadiyatov.an@gmail.com>
2022-12-05 14:44:44 +0300

    feat: added fs/rename implementation

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,16 @@
+import fsPromises from "node:fs/promises";
+import { ERR_DATA } from "../const/error.js";
+import { makePath } from "../utils/makePath.js";
+
+const path = makePath(import.meta.url, "files/wrongFilename.txt");
+const renamePath = makePath(import.meta.url, "files/properFilename.md");
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    await fsPromises.rename(path, renamePath);
+  } catch (err) {
+    throw new Error(ERR_DATA);
+  }
 };
 
 await rename();
