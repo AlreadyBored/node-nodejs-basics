70c9e39388fef4d0c08a47dc93f959aa31ec3740
Paul Grin <pashkagrin@gmail.com>
2022-12-06 01:29:30 +0300

    cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,18 @@
+import { spawn } from "child_process";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const childPath = path.resolve(__dirname, "./files", "script.js");
+  const child = spawn("node", [childPath, ...args]);
+
+  child.stdout.on("data", (data) => {
+    console.log(data.toString());
+  });
+
+  child.stdin.write("Child Process started");
 };
 
-spawnChildProcess();
+spawnChildProcess(process.argv.slice(2));
