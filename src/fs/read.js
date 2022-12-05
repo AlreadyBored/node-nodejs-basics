61063d6b8f524afdac0af295852f6a5b130c77ca
Your Name <you@example.com>
2022-12-05 13:54:18 +0300

    feat: add fs read method

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,23 @@
+import { existsSync } from "node:fs";
+import { readFile } from "node:fs/promises";
+import { resolve } from "node:path";
+
+import { DIR_NAME, FS_ERROR_TEXT } from "./constants.js";
+
+const FILE_NAME = "fileToRead.txt";
+
 const read = async () => {
-    // Write your code here 
+  const [_, currentPath] = process.argv;
+  const path = resolve(currentPath, "../", DIR_NAME, FILE_NAME);
+
+  if (!existsSync(path)) throw new Error(FS_ERROR_TEXT);
+
+  const contents = await readFile(path, { encoding: "utf8" });
+  console.log(contents);
 };
 
-await read();
+try {
+  await read();
+} catch (err) {
+  console.error(err);
+}
