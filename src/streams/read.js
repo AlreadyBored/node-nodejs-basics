b7015a0dd3ad2baa5d24afcf7230c3aab3506c68
Oops14 <ups14q@gmail.com>
2022-12-05 23:37:50 +0200

    solved streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,11 @@
+import fs from 'fs';
+
 const read = async () => {
-    // Write your code here 
+    let readStream = fs.createReadStream('./src/streams/files/fileToRead.txt', 'utf8');
+
+    readStream.on('data', (str) => {
+        process.stdout.write(str);
+    });
 };
 
 await read();
