14d90c19a986ea20379977e187929d8cc921fb00
Your Name <you@example.com>
2022-12-05 14:16:52 +0300

    feat: add cli parseEnv method

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+  const result = Object.keys(process.env).reduce((acc, key) => {
+    if (key.includes("RSS_")) {
+      acc += `${key}=${process.env[key]}; `;
+    }
+    return acc;
+  }, "");
+
+  console.log(result.trim());
 };
 
 parseEnv();
