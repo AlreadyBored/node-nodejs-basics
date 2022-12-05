45aca9ad840d5bdaa5cbebb2df58b94e6e2b885c
PalinaZinouyeva <bozkovapa@gmail.com>
2022-12-05 19:12:27 +0400

    cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,16 @@
+import { fork } from 'child_process';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { stdin, stdout} from 'process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childProcess = fork(resolve(__dirname, 'files/script.js'), args, { stdio: 'pipe' });
+
+    stdin.pipe(childProcess.stdin);
+    childProcess.stdout.pipe(stdout);
 };
 
-spawnChildProcess();
+await spawnChildProcess(['qwe', 'wer', 'ert', 'CLOSE']);
