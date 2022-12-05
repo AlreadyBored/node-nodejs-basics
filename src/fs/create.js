7c635383f23e138a70c8c5518dbfa5711965ebf4
Bahdan Bivenis <biven4i.en@gmail.com>
2022-12-05 22:43:46 +0300

    feat: solve fs/create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import { access, appendFile } from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const src = path.join(__dirname, "files", "fresh.txt");
 const create = async () => {
-    // Write your code here 
+  try {
+    await access(src);
+    throw new Error("FS operation failed");
+  } catch (err) {
+    if (err.message === "FS operation failed") {
+      throw err;
+    }
+    appendFile(src, "I am fresh and young");
+  }
 };
 
 await create();
