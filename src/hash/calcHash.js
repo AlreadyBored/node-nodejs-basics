4e21af9b6865f1696b563e35293645c884103b98
Evgen Orlovski <orloffski@gmail.com>
2022-12-05 22:26:15 +0300

    fix: change calculateHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,17 +1,32 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { readFile } from 'node:fs/promises';
+import { access, constants} from 'node:fs/promises';
 
 const __filename = fileURLToPath(import.meta.url);
 const __fileToHash = path.dirname(__filename) + '/files/fileToCalculateHashFor.txt';
 const { createHash } = await import('node:crypto');
+const __errorMessage = 'HASH operation failed: file not found';
 
 const calculateHash = async () => {
-    const hash = createHash('sha256');
+    try{
+        const ifFileToHashExists = 
+            await access(__fileToHash, constants.R_OK)
+                .then(value => true)
+                .catch(err => false);
 
-    const contents = await readFile(__fileToHash, { encoding: 'utf8' });
-    hash.update(contents);
-    console.log(hash.digest('hex'));
+        if(!ifFileToHashExists){
+            throw Error(__errorMessage);
+        }
+
+        const hash = createHash('sha256');
+
+        const contents = await readFile(__fileToHash, { encoding: 'utf8' });
+        hash.update(contents);
+        console.log(hash.digest('hex'));
+    }catch(e){
+        console.error(e.message);
+    }
 };
 
 await calculateHash();
