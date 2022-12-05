e561691f2e3bf6057463146d1f1e95faaf10a945
Andrey <aalisenak@gmail.com>
2022-12-06 00:51:30 +0300

    done: read task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,22 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const dirName = path.dirname(fileURLToPath(import.meta.url));
+const pathFile = path.join(dirName, 'files', 'fileToRead.txt');
+const errorMsg = 'FS operation failed';
+
 const read = async () => {
-    // Write your code here 
+  try {
+    await fs.readFile(pathFile, 'utf-8', (err, data) => {
+      console.log(data);
+      if (err) throw new Error();
+    });
+  } catch (err) {
+    if (err.code === 'EEXIST') {
+      console.error(Error(errorMsg));
+    }
+  }
 };
 
 await read();
