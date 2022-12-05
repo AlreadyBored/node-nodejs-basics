107dbcadd514aa364791aca2212400caf45d4bce
Evgeny-Chabrovsky <chabor111@gmail.com>
2022-12-05 22:00:53 +0300

    feat: add zlib section

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGzip } from 'node:zlib';
+import { rm } from 'node:fs/promises';
+
 const compress = async () => {
-    // Write your code here 
+  const pathFileToCompress = './files/fileToCompress.txt';
+  const gzip = createGzip();
+
+  const readStream = createReadStream(pathFileToCompress);
+  const writeStream = createWriteStream('files/archive.gz');
+
+  readStream
+    .pipe(gzip)
+    .pipe(writeStream)
+    .on('finish', async () => {
+      try {
+        await rm(pathFileToCompress);
+      } catch (err) {
+        console.log(err);
+      }
+    });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGunzip } from 'node:zlib';
+import { rm } from 'node:fs/promises';
+
 const decompress = async () => {
-    // Write your code here 
+  const pathFileToDecompress = './files/archive.gz';
+
+  const gunzip = createGunzip();
+
+  const readStream = createReadStream(pathFileToDecompress);
+  const writeStream = createWriteStream('files/fileToCompress.txt');
+
+  readStream
+    .pipe(gunzip)
+    .pipe(writeStream)
+    .on('finish', async () => {
+      try {
+        await rm(pathFileToDecompress);
+      } catch (err) {
+        console.log(err);
+      }
+    });
 };
 
 await decompress();
