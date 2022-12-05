995e99bd2fe4564057c8665712219d041bb79233
Yuliya Karimova <y.o.karimova@gmail.com>
2022-12-06 01:28:29 +0400

    feat: wt, cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,13 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { fork } from 'child_process';
+import { resolve } from 'path';
+
+const scriptPath = resolve('src', 'cp', 'files', 'script.js');
+const args = process.argv.slice(2);
+
+const spawnChildProcess = async () => {
+    fork(scriptPath, args, {
+        stdio: [process.stdin, process.stdout, "ipc"],
+    });
 };
 
 spawnChildProcess();
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,24 @@
+import { resolve } from 'path';
+import { Worker } from 'worker_threads';
+import os from 'os';
+
+const workerPath = resolve('src', 'wt', 'worker.js');
+
 const performCalculations = async () => {
-    // Write your code here
+    const promiseArray = [];
+    
+    for (let index = 0; index < os.cpus().length; index++) {
+        const workerPromise = new Promise((res, rej) => {
+            const worker = new Worker(workerPath, { workerData: 10 + index } );
+
+            worker.on('message', (data) => res({ status: "resolved", data }));
+            worker.on('error', (err) => rej({ status: "error", data: null }));
+        })
+        promiseArray.push(workerPromise);      
+    }
+
+    const result = await Promise.all(promiseArray)
+    console.log(result)
 };
 
 await performCalculations();
