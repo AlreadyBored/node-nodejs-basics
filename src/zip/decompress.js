f2008d81a124d74076c1bd88c578587949420da3
Anton Gadiyatov <gadiyatov.an@gmail.com>
2022-12-05 14:58:16 +0300

    feat: added zip/decompress implementation

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,23 @@
+import { createGunzip } from "node:zlib";
+import { pipeline } from "node:stream";
+import { createReadStream, createWriteStream } from "node:fs";
+import { promisify } from "node:util";
+import { makePath } from "../utils/makePath.js";
+
+const path = makePath(import.meta.url, "/files/archive.gz");
+const pathToDecompressed = makePath(
+  import.meta.url,
+  "/files/fileToCompress.txt"
+);
+
 const decompress = async () => {
-    // Write your code here 
+  const unzip = createGunzip();
+  const source = createReadStream(path);
+  const destination = createWriteStream(pathToDecompressed);
+
+  const pipe = promisify(pipeline);
+
+  await pipe(source, unzip, destination);
 };
 
 await decompress();
