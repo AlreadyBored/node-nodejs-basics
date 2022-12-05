b81f36c3a505293f188d6a13dcc1b8476e03a14d
Oops14 <ups14q@gmail.com>
2022-12-05 23:35:48 +0200

    solved delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import fs from 'fs/promises';
+import { constants } from 'fs';
+
 const remove = async () => {
-    // Write your code here 
+    fs.access('./src/fs/files/fileToRemove.txt', constants.F_OK).then(() => {
+
+        fs.unlink('./src/fs/files/fileToRemove.txt',function(err){
+            if(err) return console.log(err);
+            console.log('file deleted successfully');
+       }); 
+
+    }).catch(() => {
+        console.log(new Error('FS operation failed'));
+    });
 };
 
 await remove();
