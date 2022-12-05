3210a8f291126f22f52e1b8afc01ec3938d26879
Likvidas <bobento@yandex.ru>
2022-12-05 10:28:25 +0300

    init: started nodejs basics tasks

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,25 @@
-const create = async () => {
-    // Write your code here 
-};
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
+import { stat, writeFile } from 'fs/promises'
 
-await create();
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const fileExists = (path) => stat(path).then(() => true).catch(() => false)
+
+const create = async () => {
+  const targetPath = join(__dirname, 'files/fresh.txt')
+  const TEXT = 'I am fresh and young'
+  const ERROR_MESSAGE = 'FS operation failed'
+
+  try {
+    const isFileExists = await fileExists(targetPath)
+    if (isFileExists) {
+      throw new Error(ERROR_MESSAGE)
+    }
+    await writeFile(targetPath, TEXT)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+await create()
