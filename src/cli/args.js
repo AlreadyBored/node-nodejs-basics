72faad05c72eb6ba8d3fb4c4c142f3bfdd2c43c5
htretsiakova <halina.tretsiakova@ERSONLINE1.onmicrosoft.com>
2022-12-05 21:31:12 +0300

    cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+    const argv = process.argv
+    let result = '';
+    for (let i=0, max=argv.length; i<max; i++) {
+        if (argv[i].startsWith('--')) {
+            result += `${argv[i].substr(2)} ${argv[i+1]},`
+        }
+    }
+    console.log(result.substring(0, result.length - 1));
 };
 
 parseArgs();
