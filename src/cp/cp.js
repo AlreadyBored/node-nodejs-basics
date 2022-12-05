273290d81cd52c844b998c451d3dec06442474e3
Evgen Orlovski <orloffski@gmail.com>
2022-12-05 21:45:11 +0300

    fix: change spawnChildProcess function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,12 @@
+import { fork } from 'node:child_process';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __childFile = path.dirname(__filename) + '/files/script.js';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    fork(__childFile, args);
 };
 
 spawnChildProcess();
