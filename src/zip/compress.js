5b47a1f4c1558c6a565c4086a33d770a9edcb20f
Fiodar Savutsin <federikos@bk.ru>
2022-12-05 14:08:38 +0200

    feat: implement subtask Zlib (src/zip)

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,14 @@
+import { createGzip } from "node:zlib";
+import { pipeline } from "node:stream";
+import { createReadStream, createWriteStream } from "node:fs";
+
 const compress = async () => {
-    // Write your code here 
+    const fileToCompressPath = new URL("./files/fileToCompress.txt", import.meta.url);
+    const archivePath = new URL("./files/archive.gz", import.meta.url);
+    const readStream = createReadStream(fileToCompressPath);
+    const writeStream = createWriteStream(archivePath);
+    const gzip = createGzip();
+    pipeline(readStream, gzip, writeStream, err => console.error(err));
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,14 @@
+import { createGunzip } from "node:zlib";
+import { pipeline } from "node:stream";
+import { createReadStream, createWriteStream } from "node:fs";
+
 const decompress = async () => {
-    // Write your code here 
+    const fileToCompressPath = new URL("./files/fileToCompress.txt", import.meta.url);
+    const archivePath = new URL("./files/archive.gz", import.meta.url);
+    const readStream = createReadStream(archivePath);
+    const writeStream = createWriteStream(fileToCompressPath);
+    const gunzip = createGunzip();
+    pipeline(readStream, gunzip, writeStream, err => console.error(err));
 };
 
 await decompress();
