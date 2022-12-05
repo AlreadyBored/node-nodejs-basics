284c45dcd11fa0dffef0d3abe339488956950dfd
Andrey <aalisenak@gmail.com>
2022-12-05 22:56:11 +0300

    done: create task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,16 @@
+import { writeFile } from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+const pathFile = path.join(dirname, 'files', 'fresh.txt');
+const text = 'FS operation failed';
+const error = 'FS operation failed';
+
 const create = async () => {
-    // Write your code here 
+  writeFile(pathFile, text, { flag: 'wx' }, (err) => {
+    if (err) throw new Error(error);
+  });
 };
 
 await create();
