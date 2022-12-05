a50a5b8a5421275593ac8545db653160a402905a
Evgen Orlovski <orloffski@gmail.com>
2022-12-05 22:40:31 +0300

    fix: change read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,14 +1,29 @@
 import { createReadStream } from 'node:fs'
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { access, constants } from 'node:fs/promises';
 
 const __filename = fileURLToPath(import.meta.url);
 const __fileToRead = path.dirname(__filename) + '/files/fileToRead.txt';
+const __errorMessage = 'STREAMS operation failed: file not found';
 
 const read = async () => {
-    const readFile = createReadStream(__fileToRead);
+    try{
+        const ifFileToReadExists = 
+            await access(__fileToRead, constants.R_OK)
+                .then(value => true)
+                .catch(err => false)
 
-    readFile.pipe(process.stdout);
+        if(!ifFileToReadExists){
+            throw Error(__errorMessage);
+        }
+
+        const readFile = createReadStream(__fileToRead);
+
+        readFile.pipe(process.stdout);
+    }catch(e){
+        console.error(e.message);
+    }
 };
 
 await read();
