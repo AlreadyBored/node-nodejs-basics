2dbd7f759e7d4e3020d8caaba8a31cce030d9388
Anton Gadiyatov <gadiyatov.an@gmail.com>
2022-12-05 14:46:23 +0300

    feat: added fs/list implementation

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import { readdir } from "node:fs/promises";
+import { ERR_DATA } from "../const/error.js";
+import { makePath } from "../utils/makePath.js";
+
+const path = makePath(import.meta.url, "/files");
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const files = await readdir(path);
+    files.forEach((file) => {
+      console.log(file);
+    });
+  } catch (err) {
+    throw new Error(ERR_DATA);
+  }
 };
 
 await list();
