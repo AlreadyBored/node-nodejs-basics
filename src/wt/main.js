072e587b754a45bd64200e279c8582bc7edf39b3
Orlovski Evgen <orloffski@gmail.com>
2022-12-05 11:52:54 +0300

    feat: implement performCalculations function in main.js add: createWorker function in main.js feat: implement sendResult function in main.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,42 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { Worker } from 'node:worker_threads';
+
+const __filename = fileURLToPath(import.meta.url);
+const __workerFile = path.dirname(__filename) + '/worker.js';
+
+const CORE_NUMBERS = 8;
+
+const createWorker = (coreNumber) => {
+	return new Promise((resolve, reject) => {
+		const worker = new Worker(__workerFile, {
+			workerData: coreNumber
+		})
+
+		worker.on('message', (message) => {
+			resolve({status:'resolved', data:message});
+		})
+
+		worker.on('error', (error) => {
+			reject({status:'error', data:null});
+		})
+	})
+}
+
 const performCalculations = async () => {
-    // Write your code here
+    const workersArray = [];
+
+	for(let i = 1; i <= CORE_NUMBERS; i++){
+		workersArray.push(createWorker(i + 9));
+	}
+
+	await Promise.all(workersArray)
+		.then(result => {
+			console.log(result);
+		})
+		.catch(error => {
+			console.error(error);
+		});
 };
 
 await performCalculations();
