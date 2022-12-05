e4596b522ccc2dd31282f640eedeaca3bd6cd369
Your Name <you@example.com>
2022-12-05 22:45:57 +0300

    feat: add streams read method

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import { stdout } from "node:process";
+import { resolve } from "node:path";
+import { createReadStream } from "node:fs";
+import { fileURLToPath } from "url";
+
+const DIR_NAME = "files";
+const FILE_NAME = "fileToRead.txt";
+
 const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const path = resolve(__filename, "../", DIR_NAME, FILE_NAME);
+  const stream = createReadStream(path);
+
+  stream.pipe(stdout);
 };
 
 await read();
