79a13b9425d748d5a2eb055cd0ef9c0ee1fccdc9
aNNiPAk <annipak@yandex.ru>
2022-12-05 23:36:28 +0300

    feat: task 'Child Processes' solved

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,15 @@
+import { spawn } from 'child_process'
+import { getCombinedPath } from '../pathHelper.js'
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    if (args === undefined) args = []
+
+    const pathToFile = getCombinedPath(import.meta.url, 'files', 'script.js')
+
+    const subProcess = spawn('node', [pathToFile, ...args])
+
+    process.stdin.pipe(subProcess.stdin)
+    subProcess.stdout.pipe(process.stdout)
 };
 
 spawnChildProcess();
