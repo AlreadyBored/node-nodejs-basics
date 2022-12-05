f4b0714e94691cf763d1902b1b7d5c7b4a6a9e17
Anton Gadiyatov <gadiyatov.an@gmail.com>
2022-12-05 14:49:54 +0300

    feat: added cli/args implementation

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+  const envArgs = process.argv;
+
+  envArgs.forEach((arg, index) => {
+    if (arg.startsWith("--")) {
+      console.log(`${arg} is ${envArgs[index + 1]}`);
+    }
+  });
 };
 
 parseArgs();
