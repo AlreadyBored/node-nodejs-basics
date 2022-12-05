bdd99800d976ebcfacaba32543d99def3d701260
propatch <34714410+propatch@users.noreply.github.com>
2022-12-05 16:43:45 +0300

    feat: implement function that writes process.stdin

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import { createWriteStream } from "node:fs";
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+  // Write your code here
+  const writeStr = createWriteStream(
+    __dirname + "/files/fileToWrite.txt",
+    "utf8"
+  );
+  process.stdin.pipe(writeStr);
+  console.log(" Write to stdin...\n >");
 };
 
 await write();
