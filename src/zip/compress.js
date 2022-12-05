2d50263f8762057a89689fd622274fedd3783bd6
Anton Gadiyatov <gadiyatov.an@gmail.com>
2022-12-05 14:58:04 +0300

    feat: added zip/compress implementation

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import { createGzip } from "node:zlib";
+import { pipeline } from "node:stream";
+import { createReadStream, createWriteStream } from "node:fs";
+import { promisify } from "node:util";
+import { makePath } from "../utils/makePath.js";
+
+const path = makePath(import.meta.url, "/files/fileToCompress.txt");
+const pathToCompressed = makePath(import.meta.url, "/files/archive.gz");
+
 const compress = async () => {
-    // Write your code here 
+  const gzip = createGzip();
+  const source = createReadStream(path);
+  const destination = createWriteStream(pathToCompressed);
+
+  const pipe = promisify(pipeline);
+
+  await pipe(source, gzip, destination);
 };
 
 await compress();
