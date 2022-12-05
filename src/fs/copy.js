50929090824e5b39ffcfd3dda0a3f4e92c215754
Ash Karapetyan <ashot.karapetyan.y@gmail.com>
2022-12-05 03:05:51 +0300

    Implemented copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,12 +1,20 @@
-import { cp } from 'fs'
-
+import { cp, access, constants } from 'fs';
 const copy = async () => {
-    cp("./files", './files_copy', { 'recursive': true, 'force': false, 'errorOnExist': true}, (err)=>{
+    const source = './files';
+    const destination = './files_copy';
+
+    await access(source, constants.F_OK, (err) => {
+        if(err && err.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        };
+    });
+
+    await cp(source, destination, { 'recursive': true, 'force': false, 'errorOnExist': true }, (err)=>{
         if (err && err.info.code === 'EEXIST') {
             throw new Error('FS operation failed');
-        }
+        };
         console.log('Folder copied Successfully!');
-    })
-}
+    });
+};
 
 copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,13 +1,13 @@
-import { writeFile } from 'fs'
-import { resolve } from 'path'
+import { writeFile } from 'fs';
+import { resolve } from 'path';
 
 const create = async () => {
-    const absolutePath = resolve('files', 'fresh.txt')
-    const data = 'I am fresh and young'
+    const absolutePath = resolve('files', 'fresh.txt');
+    const data = 'I am fresh and young';
     await writeFile(absolutePath, data, {flag: 'wx'}, (err) => {
         if (err && err.code === 'EEXIST') {
             throw new Error('FS operation failed');
-        }
+        };
         console.log('File created Successfully!');
     });
 };
