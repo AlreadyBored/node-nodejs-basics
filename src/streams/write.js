957e02cdaab61c4894c0f7df6960f911e3da1328
Your Name <you@example.com>
2022-12-05 22:53:38 +0300

    feat: add streams write method

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import { stdin } from "node:process";
+import { resolve } from "node:path";
+import { createWriteStream } from "node:fs";
+import { fileURLToPath } from "url";
+
+const DIR_NAME = "files";
+const FILE_NAME = "fileToWrite.txt";
+
 const write = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const path = resolve(__filename, "../", DIR_NAME, FILE_NAME);
+  const stream = createWriteStream(path);
+
+  stdin.pipe(stream);
 };
 
 await write();
