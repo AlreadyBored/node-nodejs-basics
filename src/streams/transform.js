8be47d0a8e3f76a4cf864b0d52d9d534588c6032
Your Name <you@example.com>
2022-12-06 00:32:01 +0300

    fix: add async pipeline to transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,10 +1,12 @@
 import { stdout, stdin } from "node:process";
+import { pipeline } from "node:stream/promises";
 import { Transform } from "node:stream";
 
 class TextTransform extends Transform {
   _transform(chunk, encoding, callback) {
     try {
-      const resultString = chunk.toString("utf8").split("").reverse().join("");
+      const resultString =
+        chunk.toString("utf8").split("").reverse().join("") + "\n";
 
       callback(null, resultString);
     } catch (err) {
@@ -16,7 +18,7 @@ class TextTransform extends Transform {
 const transform = async () => {
   const stream = new TextTransform();
 
-  stdin.pipe(stream).pipe(stdout);
+  await pipeline(stdin, stream, stdout);
 };
 
 await transform();
