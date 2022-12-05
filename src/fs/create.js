7512e0df49a69f742493e47d4887da8366437c34
Svetlana Leshukova <s.leshukova@uds.systems>
2022-12-05 23:46:40 +0200

    feat: add first solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+const pathToDir = path.join(dirname, 'files');
+
+
 const create = async () => {
-    // Write your code here 
-};
+  fs.writeFile(path.resolve(pathToDir,'fresh.txt'), 'I am fresh and young inside of the files folder', function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+  });
+}
+
 
 await create();
