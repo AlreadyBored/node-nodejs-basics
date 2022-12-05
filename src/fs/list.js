f920e591e8c0854c1641db47feaede828bd3ebb0
Andrey <aalisenak@gmail.com>
2022-12-06 00:46:21 +0300

    done: list task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,22 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const dirName = path.dirname(fileURLToPath(import.meta.url));
+const pathFile = path.join(dirName, 'files');
+const errorMsg = 'FS operation failed';
+
 const list = async () => {
-    // Write your code here 
+  try {
+    await fs.readdir(pathFile, (err, files) => {
+      console.dir(files);
+      if (err) throw new Error();
+    });
+  } catch (err) {
+    if (err.code === 'EEXIST') {
+      console.error(Error(errorMsg));
+    }
+  }
 };
 
 await list();
