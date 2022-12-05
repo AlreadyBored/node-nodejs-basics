df3a6dffbad357c8064948f1e3da63808dd4034d
Bahdan Bivenis <biven4i.en@gmail.com>
2022-12-05 22:46:52 +0300

    feat: solve cli/args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
 const parseArgs = () => {
-    // Write your code here 
+  const arrOfArg = process.argv;
+  for (let i = 0; i < arrOfArg.length; i++) {
+    if (arrOfArg[i].includes("--")) {
+      console.log(`${arrOfArg[i].slice(2)} is ${arrOfArg[i + 1]}`);
+    }
+  }
 };
 
 parseArgs();
