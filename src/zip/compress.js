763d115ea8811c821a2cb9dc27526e57d3704d6b
PereverzaV <vika.mandarinka98@gmail.com>
2022-12-06 00:50:01 +0400

    zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+//10
+
+import fs from 'fs';
+import zlib from 'zlib';
+import stream from 'stream';
+
 const compress = async () => {
-    // Write your code here 
+ 
+  const gzip = zlib.createGzip();
+  const source = fs.createReadStream('./zip/files/fileToCompress.txt');
+  const destination = fs.createWriteStream('./zip/files/archive.txt.gz');
+
+  stream.pipeline(source, gzip, destination, (err) => {
+    if (err)  console.log('An error occurred');
+});
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+//5
+
+import util from 'util';
+import fs from 'fs';
+import zlib from 'zlib';
+import stream from 'stream';
+
 const decompress = async () => {
-    // Write your code here 
+  const pipe = util.promisify(stream.pipeline);
+
+  async function do_gzip(input, output) {
+    const gzip = zlib.createGzip();
+    const source = fs.createReadStream.toString('./zip/files/archive.txt.gz');
+    const destination = fs.createWriteStream('./zip/files/filetoCompress.txt');
+    await pipe(source, gzip, destination);
+  }
+
+  do_gzip('./zip/files/fileToCompress.txt', './zip/files/archive.txt.gz')
+ 
 };
 
 await decompress();
