4a4e4719303f0e7cf7936523ee8b28fccb3066d7
Ash Karapetyan <ashot.karapetyan.y@gmail.com>
2022-12-05 04:04:02 +0300

    Implemented read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,14 @@
+import { readFile } from 'node:fs';
+import { resolve } from 'path';
+
 const read = async () => {
-    // Write your code here 
+    const absolutePath = await resolve('files', 'fileToRead.txt')
+    await readFile(absolutePath, 'utf8', (err, data) => {
+        if(err){
+            throw new Error('FS operation failed');
+        };
+        console.log(data);
+    });
 };
 
 await read();
