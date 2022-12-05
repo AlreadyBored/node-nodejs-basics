5cc5a7e56c5c6879a7c1fce9d7b85b04c66cd029
Павел Трипольский <paveltripolskij@MacBook-Air-Pavel.local>
2022-12-05 14:32:42 +0300

    feat: implement hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import * as fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+import {createHash} from 'crypto'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const calculateHash = async () => {
-    // Write your code here 
+	let file = await fs.promises.readFile(`${__dirname}/files/fileToCalculateHashFor.txt`)
+		.catch((e) => {
+			console.log(e)
+		})
+	let hash = createHash('sha256').update(`${__dirname}/files/fileToCalculateHashFor.txt`).update(file).digest('hex')
+	console.log(hash)
 };
 
 await calculateHash();
