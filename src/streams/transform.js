e77ad9967416714f575fc502f10a16a33dc0e39d
propatch <propatchme@gmail.com>
2022-12-05 21:15:32 +0300

    refact: delete empty string

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,11 +1,5 @@
 import { pipeline, Transform } from "stream";
 
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const transform = async () => {
   // Write your code here
   const transformText = new Transform({
