9dfd4a1b6f968c6cfe63291df62979d3bea63230
Bahdan Bivenis <biven4i.en@gmail.com>
2022-12-05 22:46:26 +0300

    feat: solve cli/env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+  const arr = Object.entries(process.env);
+  for (let item of arr) {
+    if (item[0].includes("RSS")) {
+      console.log(`${item[0]}=${item[1]}`);
+    }
+  }
 };
 
 parseEnv();
