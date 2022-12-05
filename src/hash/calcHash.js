13109afd233f4f1d5d79bb071ff5470aa5f0150f
Your Name <you@example.com>
2022-12-05 22:24:14 +0300

    feat: add hash calculateHash method

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import { createReadStream } from "node:fs";
+import { stdout } from "node:process";
+import { createHash } from "node:crypto";
+import { resolve } from "node:path";
+import { fileURLToPath } from "url";
+
+const DIR_NAME = "files";
+const FILE_NAME = "fileToCalculateHashFor.txt";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const path = resolve(__filename, "../", DIR_NAME, FILE_NAME);
+  const hash = createHash("sha256");
+  const input = createReadStream(path);
+
+  input.pipe(hash).setEncoding("hex").pipe(stdout);
 };
 
 await calculateHash();
