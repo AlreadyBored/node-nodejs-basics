e5ed9758039f6885312fc8d58e9ed68e672004e4
Fiodar Savutsin <federikos@bk.ru>
2022-12-05 20:27:20 +0200

    feat: implement subtask Worker Threads (src/wt)

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,34 @@
+import { cpus } from "node:os";
+import { Worker } from "node:worker_threads";
+
 const performCalculations = async () => {
-    // Write your code here
+    const workerFilePath = new URL("./worker.js", import.meta.url);
+    const numberOfCPUs = cpus().length;
+    const rangeFrom10 = [...Array(numberOfCPUs).keys()].map(next => next + 10);
+
+    const promises = rangeFrom10.map(n => {
+        return new Promise((resolve, reject) => {
+            const worker = new Worker(workerFilePath);
+            worker.postMessage(n);
+    
+            worker.on("message", (data) => {
+                resolve({
+                    status: "resolved",
+                    data
+                });
+            })
+
+            worker.on("error", (err) => {
+                reject({
+                    status: "error",
+                    data: null
+                });
+            })
+        });
+    });
+
+    Promise.allSettled(promises)
+        .then((results) => console.dir(results.map(next => next.value)));
 };
 
 await performCalculations();
