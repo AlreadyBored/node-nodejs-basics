0ed05a6c964ed69c9f12ae708662f6464b6b2e75
dmitriysemenko <dm123semenko@gmail.com>
2022-12-06 01:15:42 +0300

    feat: add calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import fs from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { createHash } from "crypto";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const path = __dirname + "/files/fileToCalculateHashFor.txt";
+const errorMessage = "FS operation failed";
+
 const calculateHash = async () => {
-    // Write your code here 
+    fs.readFile(path, (error, data) => {
+        if (error) {
+            throw new Error(errorMessage);
+        } else {
+            const hash = createHash("sha256");
+            hash.update(data);
+            const hex = hash.digest("hex");
+            console.log(hex);
+        }
+    });
 };
 
 await calculateHash();
