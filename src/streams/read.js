0fc4d3e8a108e996327157ce6fc7fd9287233a28
Ash Karapetyan <ashot.karapetyan.y@gmail.com>
2022-12-05 18:18:11 +0300

    Implemented read stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import { createReadStream } from 'node:fs';
+import { resolve } from 'path';
+import { stdout } from 'node:process';
+
 const read = async () => {
-    // Write your code here 
+
+    const absolutePath = await resolve('files', 'fileToRead.txt');
+    const readableStream = createReadStream(absolutePath);
+
+    readableStream.on('error', (error) => {
+        stdout.write(`error: ${error.message}`);
+    })
+
+    readableStream.on('data', (chunk) => {
+        stdout.write(chunk);
+    })
 };
 
 await read();
