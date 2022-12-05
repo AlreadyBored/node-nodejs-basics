bf901ad36d0c0fa5615e1e153e9c14eca3b33abd
Ash Karapetyan <ashot.karapetyan.y@gmail.com>
2022-12-05 23:33:49 +0300

    Impemented compress function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,16 @@
+import { createGzip } from 'node:zlib';
+import { createWriteStream, createReadStream } from 'node:fs';
+import { resolve } from 'path';
+
 const compress = async () => {
-    // Write your code here 
+    const absoluteFilePath = await resolve('files', 'fileToCompress.txt');
+    const stream = createReadStream(absoluteFilePath);
+    stream
+        .pipe(createGzip())
+        .pipe(createWriteStream(`${absoluteFilePath}.gz`))
+        .on("finish", () =>
+                console.log(`Successfully compressed the file at ${absoluteFilePath}`)
+        );
 };
 
 await compress();
