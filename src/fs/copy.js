ba4992422c0b3ceb43283989e602eed54ed4edb4
Bahdan Bivenis <biven4i.en@gmail.com>
2022-12-05 22:44:41 +0300

    feat: solve fs/copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,27 @@
+import fsPromises from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const srcTo = path.join(__dirname, "files_copy");
+const src = path.join(__dirname, "files");
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    await fsPromises.access(srcTo);
+    throw new Error("FS operation failed");
+  } catch (err) {
+    if (err.message === "FS operation failed") {
+      throw err;
+    }
+  }
+
+  try {
+    await fsPromises.cp(src, srcTo, { recursive: true });
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
-await copy();
+copy();
