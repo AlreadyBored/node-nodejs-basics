36e41d2fd4b3f93fdcb354a870c0d6fce733562a
Evgen Orlovski <orloffski@gmail.com>
2022-12-05 21:57:29 +0300

    fix: change compress function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -4,18 +4,44 @@ import { createGzip } from 'node:zlib';
 import  { pipeline } from 'node:stream';
 import { promisify } from 'node:util';
 import { createReadStream, createWriteStream } from 'node:fs';
+import { access, constants} from 'node:fs/promises';
 
 const __filename = fileURLToPath(import.meta.url);
 const __fileToCompress = path.dirname(__filename) + '/files/fileToCompress.txt';
 const __fileAfterCompress = path.dirname(__filename) + '/files/archive.gz';
+const __errorMessage = 'ZIP operation failed';
+const __errorFileNotExists = ': file fileToCompress.txt not exists';
+const __errorFileExists = ': file archive.gz exists';
 
 const compress = async () => {
-    const pipe = promisify(pipeline);
-    const gzip = createGzip();
-    const source = createReadStream(__fileToCompress);
-    const destination = createWriteStream(__fileAfterCompress);
+    try{
+        const ifFileToCompressExists = 
+            await access(__fileToCompress, constants.R_OK)
+                .then(value => true)
+                .catch(err => false);
 
-    await pipe(source, gzip, destination);
+        const ifFileAfterCompressExists = 
+                await access(__fileAfterCompress, constants.R_OK)
+                    .then(value => true)
+                    .catch(err => false);
+
+        if(!ifFileToCompressExists){
+            throw Error(__errorMessage + __errorFileNotExists);
+        }
+
+        if(ifFileAfterCompressExists){
+            throw Error(__errorMessage + __errorFileExists);
+        }
+
+        const pipe = promisify(pipeline);
+        const gzip = createGzip();
+        const source = createReadStream(__fileToCompress);
+        const destination = createWriteStream(__fileAfterCompress);
+
+        await pipe(source, gzip, destination);
+    }catch(e){
+        console.error(e.message);
+    }
 };
 
 await compress();
