e98fb2ed84b7ba9812fb3d3b6fde1994639a2ece
Bahdan Bivenis <biven4i.en@gmail.com>
2022-12-05 22:48:51 +0300

    feat: solve hash/calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { createReadStream } from "node:fs";
+import path from "node:path";
+import { stdout } from "node:process";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const src = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+
 const calculateHash = async () => {
-    // Write your code here 
+  const { createHash } = await import("node:crypto");
+  const hash = createHash("sha256");
+  const readableStream = createReadStream(src);
+  readableStream.pipe(hash).setEncoding('hex').pipe(stdout);
 };
 
 await calculateHash();
