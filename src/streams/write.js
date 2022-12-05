5c739d82110264cfb09c545fd68ee5a96e09cba0
Oops14 <ups14q@gmail.com>
2022-12-05 22:17:46 +0200

    solved write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import fs from 'fs';
+
 const write = async () => {
-    // Write your code here 
+    let writeStream = fs.createWriteStream('./src/streams/files/fileToWrite.txt');
+    process.stdin.pipe(writeStream);
+
+    setTimeout(() => {
+        console.log('Manually close the file stream.');
+    
+        writeStream.end();
+    }, 5000);
 };
 
 await write();
