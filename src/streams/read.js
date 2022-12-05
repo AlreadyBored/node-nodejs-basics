5d09ccf61afd996d706842d6a1f21e7516ee3b89
Bahdan Bivenis <biven4i.en@gmail.com>
2022-12-05 22:49:21 +0300

    feat: solve streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import { createReadStream } from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const src = path.join(__dirname, "files", "fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+  const input = createReadStream(src);
+  input.on("data", (chunk) => {
+    process.stdout.write(chunk);
+  });
 };
 
 await read();
