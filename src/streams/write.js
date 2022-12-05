8d36f277026186512423b72889918ba91a56681d
Tomas_Zagorskis <tomzagos@gmail.com>
2022-12-05 20:52:08 +0200

    feat: add write stream to file

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,12 @@
+import fs from 'fs';
+import { pipeline } from 'stream';
+
 const write = async () => {
-    // Write your code here 
+	const stream = fs.createWriteStream('./files/fileToWrite.txt', 'utf8');
+	const read = process.stdin;
+	pipeline(read, stream, err => {
+		if (err) throw new Error('Operation failed');
+	});
 };
 
 await write();
