f9b9d3f336c80a044839ff280b15940577829db2
Tomas_Zagorskis <tomzagos@gmail.com>
2022-12-05 21:05:43 +0200

    feat: transform stream

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+import { Transform, pipeline } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+	const input = process.stdin;
+	const output = process.stdout;
+	const reverseStr = new Transform({
+		transform(data, _enc, cb) {
+			this.push(`${data.toString().trim().split('').reverse().join('')}\n`);
+			cb();
+		},
+	});
+	pipeline(input, reverseStr, output, err => {
+		if (err) throw new Error('Operation failed');
+	});
 };
 
 await transform();
