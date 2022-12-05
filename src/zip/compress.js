5a26106a86ffb594853f1b62262a054dfa9109f1
Tomas_Zagorskis <tomzagos@gmail.com>
2022-12-05 21:22:26 +0200

    feat: implement zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import zlib from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+	try {
+		if (!fs.existsSync('./files/fileToCompress.txt'))
+			throw new Error('Operation failed');
+		const input = fs.createReadStream('./files/fileToCompress.txt');
+		const output = fs.createWriteStream('./files/archive.gz');
+		const gzip = zlib.createGzip();
+
+		input.pipe(gzip).pipe(output);
+	} catch (e) {
+		console.error(e);
+	}
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import zlib from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+	try {
+		if (!fs.existsSync('./files/archive.gz'))
+			throw new Error('Operation failed');
+		const input = fs.createReadStream('./files/archive.gz');
+		const output = fs.createWriteStream('./files/fileToCompress.txt');
+		const unzip = zlib.createUnzip();
+
+		input.pipe(unzip).pipe(output);
+	} catch (e) {
+		console.error(e);
+	}
 };
 
 await decompress();
