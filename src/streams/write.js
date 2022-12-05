7b9061024f69b822094d55e8dec7ccf947dee594
Ash Karapetyan <ashot.karapetyan.y@gmail.com>
2022-12-05 21:08:23 +0300

    Implemented write stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,12 @@
+import { createWriteStream } from 'node:fs';
+import { resolve } from 'path';
+import { stdin} from 'node:process';
+
 const write = async () => {
-    // Write your code here 
+    const absolutePath = await resolve('files', 'fileToWrite.txt');
+    const writableStream = createWriteStream(absolutePath);
+
+    stdin.resume().pipe(writableStream);
 };
 
 await write();
