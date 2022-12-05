8b5272ea27a782493f82d23d7a524836c3cb0065
Anton Gadiyatov <gadiyatov.an@gmail.com>
2022-12-05 14:43:01 +0300

    feat: added fs/copy implementation

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,20 @@
+import { readdir, copyFile, mkdir } from "node:fs/promises";
+import { ERR_DATA } from "../const/error.js";
+import { makePath } from "../utils/makePath.js";
+
+const path = makePath(import.meta.url, "/files");
+const copyPath = makePath(import.meta.url, "/files_copy");
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    await mkdir(copyPath);
+    const files = await readdir(path);
+    files.forEach(async (file) => {
+      await copyFile(`${path}/${file}`, `${copyPath}/${file}`);
+    });
+  } catch (err) {
+    throw new Error(ERR_DATA);
+  }
 };
 
 copy();
