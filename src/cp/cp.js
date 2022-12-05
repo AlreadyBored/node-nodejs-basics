ddadf9e88d05f3c12315fd05eee6dcd6e973d775
vkordovich <vadim_kordovich@epam.com>
2022-12-06 00:04:47 +0300

    cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,15 @@
+import {spawn} from "child_process"
 const spawnChildProcess = async (args) => {
     // Write your code here
+    const childProcess = spawn('node',['src/cp/files/script.js', ...args]);
+    childProcess.stdin.write('Test Data')
+    childProcess.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
+
+    childProcess.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
 };
 
-spawnChildProcess();
+await spawnChildProcess(['first', 'second']);
