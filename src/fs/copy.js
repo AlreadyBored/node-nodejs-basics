3d18fce2c26a404803d4b6c0956b880171b0740d
Andrey <aalisenak@gmail.com>
2022-12-06 00:00:38 +0300

    done:copy task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,32 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+const pathFileCopy = path.join(dirname, 'files_copy');
+const pathFiles = path.join(dirname, 'files');
+const error_msg = 'FS operation failed';
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    await fs.mkdir(pathFileCopy, (err) => {
+      if (err) throw new Error(error_msg);
+    });
+
+    await fs.readdir(pathFiles, (err, files) => {
+      if (err) throw new Error(error_msg);
+      files.forEach((file) => {
+        fs.copyFile(
+          path.join(pathFiles, file),
+          path.join(dist, file)
+        );
+      });
+    });
+  } catch (err) {
+    if (err.code === 'EEXIST') {
+      console.error(Error(error_msg));
+    }
+  }
 };
 
-await copy();
+copy();
