be1b116a90755c644d44281246f5d31493e76abb
Roman Panchenko <roman.p@aquimarketing.com>
2022-12-05 15:00:07 +0200

    feat: cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,21 @@
+import { spawn, fork } from 'node:child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childProcess = spawn('node', ['./files/script.js', 'test'], {
+        stdio: [null, null, null, 'ipc'],
+    });
+
+    process.stdin.on('data', (data) => {
+        childProcess.stdin.write(data);
+    });
+
+    childProcess.on('message', (m) => {
+        process.stdout.write(`Received from child process: ${m}`);
+    });
+
+    childProcess.stdout.on('data', (data) => {
+        process.stdout.write(data);
+    });
 };
 
 spawnChildProcess();
