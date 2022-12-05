0a41c83a94bb6f94049651f97133304a67674eab
propatch <34714410+propatch@users.noreply.github.com>
2022-12-05 16:28:54 +0300

    feat:  Readable Stream and prints it's content

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import { createReadStream } from "node:fs";
+import fs from "node:fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  // Write your code here
+  const readStr = createReadStream(__dirname + "/files/fileToRead.txt", "utf8");
+  readStr.pipe(process.stdout);
 };
 
 await read();
