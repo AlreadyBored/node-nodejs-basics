ad1a0d6b0b506f49cbfc7f16102824e921d60121
Your Name <you@example.com>
2022-12-05 23:59:35 +0300

    feat: add zip compress method

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,30 @@
+import { createGzip } from "node:zlib";
+import { pipeline } from "node:stream";
+import { resolve } from "node:path";
+import { createReadStream, createWriteStream } from "node:fs";
+import { fileURLToPath } from "url";
+
+const DIR_NAME = "files";
+const FILE_NAME = "fileToCompress.txt";
+const ZIP_NAME = "archive.gz";
+
 const compress = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const filePath = resolve(__filename, "../", DIR_NAME, FILE_NAME);
+  const gzPath = resolve(__filename, "../", DIR_NAME, ZIP_NAME);
+
+  pipeline(
+    createReadStream(filePath),
+    createGzip(),
+    createWriteStream(gzPath),
+    (err) => {
+      if (err) {
+        console.error("Pipeline failed.", err);
+      } else {
+        console.log("Pipeline succeeded.");
+      }
+    }
+  );
 };
 
 await compress();
