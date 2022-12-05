ee6f46338b4803b33b9e81634b8d37a2dfb92f57
Evgeny-Chabrovsky <chabor111@gmail.com>
2022-12-05 21:57:43 +0300

    feat: add modules section

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,16 +1,26 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import * as path from 'path';
+import {fileURLToPath} from 'url';
+import { release, version } from 'os';
+import { createServer as createServerHttp} from 'http'
+import './files/c.js'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const random = Math.random();
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+   const obg = await import('./files/a.json',{
+     assert: { type: 'json' }
+   });
+    unknownObject = JSON.parse(JSON.stringify(obg));
 } else {
-    unknownObject = require('./files/b.json');
+   const obj =  await import('./files/b.json', {
+     assert: { type: 'json' }
+   })
+    unknownObject = JSON.parse(JSON.stringify(obj))
 }
 
 console.log(`Release ${release()}`);
@@ -33,7 +43,7 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
+export {
     unknownObject,
     myServer,
 };
