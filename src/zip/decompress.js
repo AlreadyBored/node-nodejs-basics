1c2f56f584a0f075470314efbf53a6bf6967d399
Alexgagrun <aleksandr.gargun@gmail.com>
2022-12-06 11:27:10 +0300

    zip

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,17 @@
+import { createGunzip } from 'zlib'
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream';
 const decompress = async () => {
     // Write your code here 
+    const input = createReadStream('./src/zip/files/archive.gz')
+    const output = createWriteStream('./src/zip/files/fileToCompres.txt')
+    const unzip = createGunzip()
+    pipeline(input, unzip, output, (err) => {
+        if (err) {
+            console.error(err)
+            process.exitCode = 1
+        }
+    })
 };
 
 await decompress();
