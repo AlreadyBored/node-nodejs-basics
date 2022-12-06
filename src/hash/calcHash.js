77e60d90a44b05de8b3ce468eb6b84b9d68fe33e
Alexgagrun <aleksandr.gargun@gmail.com>
2022-12-06 12:15:25 +0300

    hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,10 @@
+import { createHash } from 'crypto'
+
 const calculateHash = async () => {
     // Write your code here 
+    const hash = createHash('sha256')
+    const hex = hash.update('./src/hash/files/fileToCalculateHashFor.txt').digest('hex')
+    console.log(hex)
 };
 
 await calculateHash();
