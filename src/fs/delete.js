48dc1d2b2a7ab27095486eaaadbfe8a7594a529a
Aibek <ai.bek@mail.ru>
2022-12-23 22:41:09 +0600

    feat: done delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,24 @@
+import * as fsp from "fs/promises";
+import * as fs from 'fs'
+import * as path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const files = path.join(__dirname, "files");
+
+const errorMessage = 'FS operation failed';
+const getFiled = () => { throw new Error(errorMessage) }
+
 const remove = async () => {
     // Write your code here 
+    try {
+      const removeFile = path.join(files, 'fileToRemove.txt');
+      if(!fs.existsSync(removeFile)) getFiled();
+      await fsp.rm(removeFile)
+    } catch (error) {
+      console.error(error);
+    }
 };
 
 await remove();
