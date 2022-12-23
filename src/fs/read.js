8ebbab2e0b9af8d5504ab6cd8b216cc6ced55f62
Aibek <ai.bek@mail.ru>
2022-12-23 22:54:10 +0600

    feat: done read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,25 @@
+import * as fsp from "fs/promises";
+import * as fs from 'fs'
+import * as path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const files = path.join(__dirname, "files");
+
+const errorMessage = 'FS operation failed';
+const getFiled = () => { throw new Error(errorMessage) }
+
 const read = async () => {
-    // Write your code here 
+    // Write your code here
+    const readFile = path.join(files, 'fileToRead.txt');
+    try {
+      if(!fs.existsSync(readFile)) getFiled();
+      const content = await fsp.readFile(readFile, {encoding: 'utf-8'});
+      console.log(content);
+    } catch (error) {
+      console.error(error);
+    }
 };
 
 await read();
