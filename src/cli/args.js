7bd871710dedc7a0e888d56483f622354e9ebebc
Aibek <ai.bek@mail.ru>
2022-12-26 23:37:13 +0600

    feat: done args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
     // Write your code here 
+    const myArgs = process.argv.slice(2);
+    let result = ''
+    myArgs.forEach((element, i) => {
+      result += i % 2 !== 0 ? `${element},` : `${element.slice(2)} is ` 
+    });
+    console.log(result)
 };
 
 parseArgs();
