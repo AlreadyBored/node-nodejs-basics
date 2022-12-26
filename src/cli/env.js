e6b236e3c698a0365193d95a72f3cdd072e0ea32
Aibek <ai.bek@mail.ru>
2022-12-26 23:28:22 +0600

    feat: don env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
+process.env.RSS_name1 = 'bar';
+process.env.RSS_name2 = 'foo';
 const parseEnv = () => {
     // Write your code here 
+    const envKeys = Object.keys(process.env)
+    const RSSKeys = envKeys.filter(key => key.startsWith('RSS_'))
+    console.log(envKeys)
+    RSSKeys.forEach(key => console.log(`${key}=${process.env[key]}`)) 
 };
 
 parseEnv();
