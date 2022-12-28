84117f76ba780284afe00870020a92cd0f2af952
JMSBRN <zakhavai@gmail.com>
2022-12-28 15:52:11 +0300

    fix: upd_task_1_create_file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
-const create = async () => {
-    // Write your code here 
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename)
+const path = `${_dirname}/fresh.txt`;
+const create = () => {
+ if ( fs.existsSync(path)) {
+        throw new Error('FS operation failed')
+    } else {
+        fs.appendFile(path, 'I am fresh and young', (err) => {
+          if(err) throw err;    
+          console.log(`file created in folder  ${_dirname}`); 
+        })
+    }
 };
 
-await create();
+create();
