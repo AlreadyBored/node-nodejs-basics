2bf17d9a53d1fc6310966e6344a38989b7d9be8c
Aibek <ai.bek@mail.ru>
2023-01-07 18:44:23 +0600

    feat: calcHash done

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import { createHash } from 'node:crypto'
+import { fileURLToPath } from "url";
+import * as path from "path";
+import * as fsp from "fs/promises";
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const file = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
     // Write your code here 
+    const fileBuffer = await fsp.readFile(file);
+    const hex = createHash('SHA256').update(fileBuffer).digest('hex');
+    console.log(hex);
 };
 
 await calculateHash();
