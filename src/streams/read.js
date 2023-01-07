3d699d9b7cecb8e4a992b6ade3248fba6256402c
Aibek <ai.bek@mail.ru>
2023-01-07 19:16:54 +0600

    feat: done read stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import * as fs from 'fs'
+import * as path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const fileToRead = path.join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
     // Write your code here 
+    let data = '';
+    const readStream = fs.createReadStream(fileToRead, 'utf-8');
+    readStream.on('data', chunk => data += chunk);
+    readStream.on('end', () => process.stdout.write(data));
 };
 
 await read();
