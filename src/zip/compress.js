c1bb04bc108c960ed8fc12fc7a56207c15e58def
Aibek <ai.bek@mail.ru>
2023-01-09 00:33:15 +0600

    feat: done zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,23 @@
+import * as fs from 'fs';
+import * as zlib from 'zlib';
+import * as path from "path";
+import { fileURLToPath } from "url";
+import {pipeline} from 'stream';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const fileToPress = path.join(__dirname, 'files', 'fileToCompress.txt');
+const pressedFile = path.join(__dirname, 'files', 'archive.gz');
+
+
 const compress = async () => {
     // Write your code here 
+    const readStream = fs.createReadStream(fileToPress, 'utf-8');
+    const writeStream = fs.createWriteStream(pressedFile);
+    const gzip = zlib.createGzip();
+    pipeline(readStream, gzip, writeStream, (err) => {
+      console.error(err);
+    });
 };
 
 await compress();
