d2662df3c2a45d707371ce3b8a6da47f8e6f313b
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-04-05 14:31:51 +0500

    done copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,23 @@
+import fs from 'fs/promises';
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    // create 'files_copy' folder
+    await fs.mkdir('./files_copy');
+    // get filenames from inside 'files' folder
+    const fileNames = await fs.readdir('./files');
+    fileNames.forEach(async (fileName) => {
+      // copy files from 'files' folder to 'files_copy' folder
+      await fs.copyFile(`./files/${fileName}`, `./files_copy/${fileName}`);
+    });
+  } catch (err) {
+    // catch exist error
+    if (err.code === 'EEXIST' || err.code === 'ENOENT') {
+      throw Error('FS operation failed');
+    } else {
+      throw Error(err);
+    }
+  }
 };
 
 await copy();
