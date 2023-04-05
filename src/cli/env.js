8cdcf3af7edda1008f3e34a7a5f6cbf6e6364eac
Taneros <fricentaurus@GMAIL.COM>
2023-04-05 22:18:58 +0300

    feat: solve env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
-const parseEnv = () => {
-    // Write your code here 
+const parseEnv=() => {
+
+    for(let [key, val] of Object.entries(process.env)) {
+        if(key.includes('RSS')) {
+            console.log(`${key}=${val}`)
+        }
+    }
 };
 
 parseEnv();
