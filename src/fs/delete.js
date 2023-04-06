649b143d03a0f0f2c774366f0e0a8c0869ad7645
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-04-06 15:56:22 +0500

    done delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import fs from 'fs/promises';
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    // remove './files/fileToRemove.txt' file
+    await fs.unlink('./files/fileToRemove.txt');
+    console.log("'./files/fileToRemove.txt' file is removed.");
+  } catch (err) {
+    // catch exist error
+    if (err.code === 'ENOENT') {
+      throw Error('FS operation failed');
+    } else {
+      throw err;
+    }
+  }
 };
 
 await remove();
