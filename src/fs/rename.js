542984ea352f26db29806702f804744cf3306568
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-04-06 16:56:41 +0500

    done rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,19 @@
+import fs from 'fs/promises';
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    //  check './files/properFilename.md' file exists
+    await fs.writeFile('./files/properFilename.md', '', { flag: 'wx' });
+
+    //  rename wrongFilename to properFilename
+    await fs.rename('./files/wrongFilename.txt', './files/properFilename.md');
+  } catch (err) {
+    if (err.code === 'ENOENT' || err.code === 'EEXIST') {
+      throw Error('FS operation failed');
+    } else {
+      throw err;
+    }
+  }
 };
 
 await rename();
