d7e0835444e03b58c9a1d4079b669411a0e663ab
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-04-06 14:38:08 +0500

    update create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -6,12 +6,15 @@ const create = async () => {
     await fs.writeFile('./files/fresh.txt', 'I am freash and young', {
       flag: 'wx', // fails if the path ('./files/fresh.txt') exists
     });
+    console.log(
+      "new './files/fresh.txt' file created with the content 'I am freash and young'."
+    );
   } catch (err) {
     // catch exist error
     if (err.code === 'EEXIST') {
       throw Error('FS operation failed');
     } else {
-      throw Error(err);
+      throw err;
     }
   }
 };
