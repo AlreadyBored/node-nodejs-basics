c47551aa7add2907e85c3bbc9af817143eb4eb7c
rfathullin <rfathullin@luxms.com>
2023-04-06 19:03:48 +0300

    feat: finish read using stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,25 @@
-const read = async () => {
-    // Write your code here 
+import fs, {link} from 'fs'
+import readline from 'readline'
+
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const scriptPath=fileURLToPath(import.meta.url)
+
+const  dirName=path.dirname(scriptPath)
+
+const pathToReadFile=path.join(dirName, 'files', 'fileToRead.txt')
+
+const read=async () => {
+    const lineReader=readline.createInterface({
+        input: fs.createReadStream(pathToReadFile),
+        output: process.stdout,
+        terminal: true
+    })
+
+    lineReader.on('line',(line) => {
+        console.log(line)
+    })
 };
 
 await read();
