82149f3029f0be66bffdd676cc9a0379170fdf1c
rfathullin <rfathullin@luxms.com>
2023-04-07 14:27:57 +0300

    feat: write solution

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,19 @@
-const transform = async () => {
-    // Write your code here 
-};
+import readline from 'readline'
 
+process.stdin.setEncoding('utf8');
+// process.stdin.resume()
+
+const transform=async () => {
+
+    console.log('Enter text\n')
+
+    process.stdin.on('data', (data) => {
+        data = data.split('').reverse().join('')
+        
+        process.stdout.write(data,(err) => {
+            if(err) throw new Error
+        })
+    })
+
+}
 await transform();
