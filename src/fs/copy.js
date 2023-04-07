f60ee1ad55fad184b835d0c7573fc858d68fe823
unknown <andrejvanov42@gmail.com>
2023-04-07 09:59:25 +0300

    init

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,16 @@
+import * as fs from "fs/promises";
 const copy = async () => {
-    // Write your code here 
+  const folderPath = "src/fs/files";
+  const destFolderPath = "src/fs/files_copy";
+  try {
+    await fs.cp(folderPath, destFolderPath, {
+      recursive: true,
+      errorOnExist: true,
+      force: false,
+    });
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
 
 await copy();
