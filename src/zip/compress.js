6d6514bb1c332f6ede14510012bdce1fcff56cab
Kilomaster3 <Aliaksei.Hrom-ext@fidor.com>
2023-04-07 12:50:49 +0500

    Add zip logic

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,23 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGzip } from 'node:zlib';
+import { rm } from 'node:fs/promises';
+
 const compress = async () => {
-    // Write your code here 
+  const gzip = createGzip();
+
+  const readStream = createReadStream('./files/fileToCompress.txt', 'utf8');
+  const writeStream = createWriteStream('./files/archive.gz');
+
+  readStream
+    .pipe(gzip)
+    .pipe(writeStream)
+    .on('finish', async () => {
+      try {
+        await rm('./files/fileToCompress.txt');
+      } catch(e) {
+        console.log(e);
+      }
+    });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,23 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGunzip } from 'node:zlib';
+import { rm } from 'node:fs/promises';
+
 const decompress = async () => {
-    // Write your code here 
+  const gUnZip = createGunzip();
+
+  const readStream = createReadStream('./files/archive.gz');
+  const writeStream = createWriteStream('files/fileToCompress.txt');
+
+  readStream
+    .pipe(gUnZip)
+    .pipe(writeStream)
+    .on('finish', async () => {
+      try {
+        await rm('./files/archive.gz');
+      } catch(e) {
+        console.log(e);
+      }
+    });
 };
 
 await decompress();
