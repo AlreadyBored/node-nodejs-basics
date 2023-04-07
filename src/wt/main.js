66443ac0b7e6bf488771e5f0ad3deb3e46ee6c78
Kilomaster3 <Aliaksei.Hrom-ext@fidor.com>
2023-04-07 14:20:58 +0500

    Add Worker Threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,27 @@
+import * as path from 'path';
+import { Worker } from 'node:worker_threads';
+import { fileURLToPath } from 'url';
+import { cpus } from 'node:os';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const workerPath = `${__dirname}/worker.js`;
+
+const createWorker = (workerData) => {
+  return new Promise((resolve, reject) => {
+    const worker = new Worker (workerPath, workerData);
+    worker.on('message', (data) => { resolve(data) });
+    worker.on('error', (err) => { reject(err) });
+    worker.postMessage(workerData);
+  });
+};
+
+
 const performCalculations = async () => {
-    // Write your code here
+  const promises = cpus().map((cpu, i) => { return createWorker({ num: 10 + i })});
+
+  await Promise.allSettled(promises).then((results) => {
+    results.forEach((result) => { console.log(`For: ${result.value.num} Fibonacci Number is: ${result.value.fib}`)});
+  });
 };
 
 await performCalculations();
