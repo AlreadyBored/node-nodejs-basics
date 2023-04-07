244d073894c1a0bfa322f899f9916b95e496bd72
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-04-07 12:17:21 +0500

    done env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+    // get environment variables
+    const env = process.env;
+
+    // print environment variables with their values
+    for (const variable in env) {
+        console.log(`RSS_${variable}=${env[variable]}`);
+    }
 };
 
 parseEnv();
