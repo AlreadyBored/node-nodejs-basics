2447ddca0dc4810db86019919b42c06e624fd7bf
rfathullin <rfathullin@luxms.com>
2023-04-07 14:34:32 +0300

    feat: finish transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,18 +1,17 @@
 import readline from 'readline'
 
 process.stdin.setEncoding('utf8');
-// process.stdin.resume()
+process.stdin.resume()
 
 const transform=async () => {
 
-    console.log('Enter text\n')
+    const readLine=readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    })
 
-    process.stdin.on('data', (data) => {
-        data = data.split('').reverse().join('')
-        
-        process.stdout.write(data,(err) => {
-            if(err) throw new Error
-        })
+    readLine.on('line',(line) => {
+        console.log(line.split('').reverse().join(''))
     })
 
 }
