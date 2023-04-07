2575b0d50f2a6704e6549bac1dddb855f21fdd19
Kilomaster3 <Aliaksei.Hrom-ext@fidor.com>
2023-04-07 11:16:50 +0500

    Create hash calculation

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { readFile } from 'node:fs/promises';
+import { createHash } from 'node:crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const input = await readFile('./files/fileToCalculateHashFor.txt');
+    console.log('input', input);
+    const calculateHash = createHash('sha256');
+    calculateHash.update(input);
+    const hex = calculateHash.digest('hex');
+    console.log('hex', hex);
+  } catch (err) {
+    console.error(err.message);
+  }
 };
 
 await calculateHash();
