7fc1876bf8ed86cea33a1ceb9cdfc87be2e9f97a
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-04-07 12:05:18 +0500

    done args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+    // get argument variables
+    const argv = process.argv.slice(2);
+
+    // print argument variables and their values
+    for (let i = 0; i < argv.length; i += 2) {
+        console.log(`${argv[i].slice(2)} is ${argv[i + 1]}`);
+    }
 };
 
 parseArgs();
