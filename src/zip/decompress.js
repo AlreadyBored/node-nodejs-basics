68f63f0213f7e0f3699cffe35f28bedf77dd53bc
rfathullin <rfathullin@luxms.com>
2023-04-07 17:31:02 +0300

    feat: finish Zlib

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,29 @@
-const decompress = async () => {
-    // Write your code here 
+import fs from 'fs'
+import path from 'path';
+import {fileURLToPath} from 'url';
+import zlib from 'zlib';
+
+const scriptPath=fileURLToPath(import.meta.url)
+
+const dirName=path.dirname(scriptPath)
+
+const pathToOriginFile=path.join(dirName,'files','archive.gz')
+
+const pathToOutuptFile = path.join(dirName, 'files', 'fileToCompress.txt')
+
+const decompress=async () => {
+
+        const unZip=zlib.createUnzip()
+    
+        const readFile=fs.createReadStream(pathToOriginFile)
+
+        const writeFile=fs.createWriteStream(pathToOutuptFile)
+    
+        readFile.pipe(unZip).pipe(writeFile)
+
+
+    
+
 };
 
 await decompress();
