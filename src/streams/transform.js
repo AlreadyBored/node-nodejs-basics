ed50942d0491f44e9eb03d23e053b045ec8e6d80
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-04-07 16:28:41 +0500

    done transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,8 @@
 const transform = async () => {
-    // Write your code here 
+    // handle chunks from stdin and write them to stdout
+    process.stdin.on('data', (chunk) =>
+        process.stdout.write(`received chunk: ${chunk}`)
+    );
 };
 
 await transform();
