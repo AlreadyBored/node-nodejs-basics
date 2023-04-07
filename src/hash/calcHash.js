eb488d50e6fd9a5b5833c9a245f395b33df7e65c
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-04-07 15:10:13 +0500

    done calHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import fs from 'fs/promises';
+import { createHmac } from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+    try {
+        // get data from fileToCalculateHashFor.txt
+        const data = await fs.readFile('./files/fileToCalculateHashFor.txt');
+
+        // set secret key
+        const secret = '123';
+
+        // generate hashed data
+        const hash = createHmac('sha256', secret).update(data).digest('hex');
+
+        // print hashed data
+        console.log(hash);
+    } catch (err) {
+        throw err;
+    }
 };
 
 await calculateHash();
