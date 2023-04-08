e98f0815ff01fd8aefe9f753af7cbfec0ab9c8f6
Taneros <fricentaurus@GMAIL.COM>
2023-04-08 22:51:56 +0300

    feat: start solve

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,6 @@
 const spawnChildProcess = async (args) => {
     // Write your code here
+    console.log(`cp.js - line: 3 ->> `, )
 };
 
 // Put your arguments in function call to test this functionality
