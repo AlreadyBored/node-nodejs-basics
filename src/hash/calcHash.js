fd704b96723326ba97959cb5366cb65c9f1919a6
Liubov Krivtsova <avostvirk@gmail.com>
2023-04-08 20:08:43 +0300

    Hash tasks

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,13 @@
+import * as crypto from "crypto";
+import fs from "fs";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const pathToData = './src/hash/files/fileToCalculateHashFor.txt';
+  fs.readFile(pathToData, {encoding: 'utf8'}, (err, data) => {
+    if (err) throw new Error('FS operation failed');
+    const result = crypto.createHash('SHA256').update(data).digest('hex');
+    console.log(result)
+  });
 };
 
 await calculateHash();
