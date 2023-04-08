169493028eb3bee8a3c9201930142ba3b4d98d5c
Liubov Krivtsova <avostvirk@gmail.com>
2023-04-08 20:09:52 +0300

    Zlib tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import zlib from "zlib";
+import fs from "fs";
+import stream from "stream";
+
 const compress = async () => {
-    // Write your code here 
+  const gzip = zlib.createGzip();
+  const source = fs.createReadStream('./src/zip/files/fileToCompress.txt');
+  const destination = fs.createWriteStream('./src/zip/files/archive.gz');
+
+  stream.pipeline(source, gzip, destination, (err) => {
+    if (err) {
+      console.error('An error occurred:', err);
+      process.exitCode = 1;
+    }
+  });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,13 @@
+import zlib from "zlib";
+import fs from "fs";
+
 const decompress = async () => {
-    // Write your code here 
+  const unzip = zlib.createUnzip();
+
+  const source = fs.createReadStream('./src/zip/files/archive.gz');
+  const destination = fs.createWriteStream('./src/zip/files/fileToCompress.txt');
+
+  source.pipe(unzip).pipe(destination);
 };
 
 await decompress();
