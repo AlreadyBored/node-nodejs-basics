e5c515292fc1ba1b0e867c307755d1f020209678
WilhelmYakunin <v.jakunin@inbox.ru>
2023-04-09 19:36:05 +0300

    feat: implement fs/read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,27 @@
+import { promises, existsSync } from 'fs';
+import path from 'path';
+import m from './messages.js';
+
+const __dirname = path.resolve();
+const readeePath = path.join(__dirname, 'files');
+
 const read = async () => {
-    // Write your code here 
+    if (!existsSync(readeePath)) throw new Error(m.ERR);
+
+    try {
+        const dir = await promises.readdir(readeePath, { withFileTypes: true });
+
+        for (const file of dir) {
+            if (file.isFile()) {
+                const filePath = path.join(readeePath, file.name);
+                const data = await promises.readFile(filePath, 'utf-8');
+                console.log(data);
+            }
+        }
+
+    } catch (err) {
+        console.log(`${err}`);
+    }
 };
 
 await read();
