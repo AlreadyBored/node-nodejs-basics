df0039920298b2fb1c118f383d4e8e0f312a47e3
Bekhzod Shukhratov <bekhzod2204@gmail.com>
2023-04-09 16:33:15 +0500

    finish src/cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
+
 const parseArgs = () => {
-    // Write your code here 
+    // Write your code here
+    //console.log(process.argv.slice(2));
+    const args = process.argv.slice(2); //to remove path to the Node.js executable and the path to the script file
+    for (let i = 0; i < args.length; i += 2) {
+    const propName = args[i].slice(2); //to remove -- before propName
+    const value = args[i + 1];
+    console.log(`${propName} is ${value}`);
+  }
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
     // Write your code here 
+    //console.log(process.env);
+    for (const [key, value] of Object.entries(process.env)) {
+        if (key.startsWith('RSS_')) {
+          console.log(`${key}=${value}`);
+        }
+      }
 };
 
 parseEnv();
