ec871c2172c82543a55929decdd0321ad138e833
WilhelmYakunin <v.jakunin@inbox.ru>
2023-04-09 19:26:46 +0300

    feat: implement fs/list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,25 @@
+import { promises, existsSync } from 'fs';
+import path from 'path';
+import m from './messages.js';
+
+const __dirname = path.resolve();
+const readeePath = path.join(__dirname, 'files');
+
 const list = async () => {
-    // Write your code here 
+    if (!existsSync(readeePath)) throw new Error(m.ERR);
+
+    try {
+        const dir = await promises.readdir(readeePath, { withFileTypes: true });
+
+        for (const file of dir) {
+            if (file.isFile()) {
+                console.log(file.name);
+            }
+        }
+
+    } catch (err) {
+        console.log(`${err}`);
+    }
 };
 
 await list();
