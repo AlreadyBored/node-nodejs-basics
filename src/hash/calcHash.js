0e517cbbb7dc695e7c2ebadceffb4635f6e4b439
Taneros <taneros@duck.com>
2023-04-09 20:42:26 +0300

    Squashed commit of the following:
    commit 49c891ac5c005a81c33f4b74ef7ef73a69bf1c95
    Author: rfathullin <rfathullin@luxms.com>
    Date:   Thu Apr 6 17:48:49 2023 +0300
    
        feat: finish create file hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
-const calculateHash = async () => {
-    // Write your code here 
+import fs from 'fs'
+import path from 'path'
+import {fileURLToPath} from 'url';
+import crypto from 'crypto'
+
+const scriptPath=fileURLToPath(import.meta.url)
+
+const dirName=path.dirname(scriptPath)
+
+const pathToFileGetHash=path.join(dirName,'files','fileToCalculateHashFor.txt')
+
+const calculateHash=async () => {
+    try {
+
+        const readFileToBuffer=fs.readFileSync(pathToFileGetHash)
+        const hashSum=crypto.createHash('sha256')
+        hashSum.update(readFileToBuffer)
+
+        const hex=hashSum.digest('hex')
+        
+        console.log('sha256 hash in hex: ', hex)
+
+    } catch(error) {
+        throw new Error('Create HASH operation failed!')
+    }
 };
 
 await calculateHash();
