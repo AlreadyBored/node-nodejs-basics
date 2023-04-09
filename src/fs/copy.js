b604469b785cb2f5ba3572aaa016d98dae4da7b9
WilhelmYakunin <v.jakunin@inbox.ru>
2023-04-09 19:15:14 +0300

    feat: implement fs/copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,31 @@
+import { promises, existsSync } from 'fs';
+import path from 'path';
+import m from './messages.js';
+
+const __dirname = path.resolve();
+const currentFolderPath = path.join(__dirname, 'files');
+const copiedFolderPath = path.join(__dirname, 'files_copy');
+
 const copy = async () => {
-    // Write your code here 
+    if (!existsSync(currentFolderPath) || existsSync(copiedFolderPath)) throw new Error(m.ERR);
+
+    try {
+        const dir = await promises.readdir(currentFolderPath, { withFileTypes: true });
+	    await promises.mkdir(copiedFolderPath, { recursive: true });
+
+        for (const file of dir) {
+            if (file.isFile()) {
+                const copeeFilePath = path.join(currentFolderPath, file.name);
+                const newFilePath = path.join(copiedFolderPath, file.name);
+                
+                const data = await promises.readFile(copeeFilePath);
+                await promises.writeFile(newFilePath, data)
+            }
+        }
+
+    } catch (err) {
+        console.log(`${err}`);
+    }
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -6,8 +6,9 @@ const __dirname = path.resolve();
 const newFilePath = path.join(__dirname, 'files', 'fresh.txt');
 
 const create = async () => {
+    if (existsSync(newFilePath)) throw new Error(m.ERR);
+    
     try {		
-	    if (existsSync(newFilePath)) throw new Error(m.ERR);
         await promises.writeFile(newFilePath, m.NEW_FILE_CONTENT);   
 	} catch (err) {
 		console.log(`${err}`);
