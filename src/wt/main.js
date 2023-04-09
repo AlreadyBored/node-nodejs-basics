a33e048e66bcf40c97212e4903b30df0b6157535
Taneros <taneros@duck.com>
2023-04-09 20:58:52 +0300

    refactor:

--- src/wt/main.js
+++ src/wt/main.js
@@ -3,12 +3,13 @@ import os from 'os'
 
 const performCalculations=async () => {
 
-    console.log("Number of available CPUs: ",os.cpus().length, '\n');
+    console.log("Number of available CPUs: ", os.cpus().length, '\n');
 
     const fibDataArr = await Promise.all(os.cpus().map((cpu,idx) =>
 
         new Promise((resolve, reject) => {
-            const worker=new Worker("./src/wt/worker.js",{workerData: idx+10})
+            const worker=new Worker("./src/wt/worker.js", {workerData: idx+10})
+
             worker.on('message',(message) => resolve({status: 'resolved', data: message}))
             worker.on('error',(() => resolve({status: 'error', data: null})))
             worker.on('exit', (code) => resolve({status: `exited with code: ${code}`, data: null}))
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -3,7 +3,7 @@ import {workerData, parentPort} from 'worker_threads'
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
-const sendResult = () => {
+const sendResult=() => {
     const fibNum=nthFibonacci(workerData)
     
     parentPort.postMessage(fibNum)
