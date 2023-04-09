432a4d731f60385428a2245d325da86a2a3cf929
Taneros <taneros@duck.com>
2023-04-09 20:31:29 +0300

    refactor: env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,7 +1,7 @@
 const parseEnv=() => {
 
     for(let [key, val] of Object.entries(process.env)) {
-        if(key.includes('RSS')) {
+        if(key.includes('RSS_')) {
             console.log(`${key}=${val}`)
         }
     }
