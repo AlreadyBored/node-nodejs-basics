a21a1c5a973a397f9e4bc4dd8a5cc913d6b75a28
Bekhzod Shukhratov <bekhzod2204@gmail.com>
2023-04-09 17:54:06 +0500

    finish src/hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import {createHash} from "crypto";
+import fs from "fs";
 const calculateHash = async () => {
     // Write your code here 
+    fs.readFile("./files/fileToCalculateHashFor.txt", {encoding: 'utf8'}, (err, data)=>{
+        if(err){
+            console.error(err);
+            return
+        }
+        if (data) {
+            console.log(createHash("sha256").update(data).digest('hex'));
+            //console.log(data);
+        }
+    })
+    
 };
 
 await calculateHash();
