2d67bacb8048f0480410fd6bf12d8f8513de7a7c
WilhelmYakunin <v.jakunin@inbox.ru>
2023-04-09 19:23:31 +0300

    feat: implement fs/delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import { promises, existsSync } from 'fs';
+import path from 'path';
+import m from './messages.js';
+
+const __dirname = path.resolve();
+const removeePath = path.join(__dirname, 'files', 'fileToRemove.txt');
+
 const remove = async () => {
-    // Write your code here 
+    if (!existsSync(removeePath)) throw new Error(m.ERR);
+
+    try {
+        await promises.rm(removeePath);
+    } catch (err) {
+        console.log(`${err}`);
+    }
 };
 
 await remove();
