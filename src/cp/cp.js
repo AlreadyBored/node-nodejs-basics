5f7fadbafc76cccebef788d019e01ca34d2a5fd7
Taneros <taneros@duck.com>
2023-04-09 19:15:54 +0300

    feat: finish child process streams

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,7 +1,28 @@
+
+import path from 'path';
+import {fileURLToPath} from 'url';
+import {spawn} from 'child_process';
+
+const scriptPath=fileURLToPath(import.meta.url)
+const dirName=path.dirname(scriptPath)
+
+const pathToScriptFile=path.join(dirName,'files','script.js')
+
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
-    console.log(`cp.js - line: 3 ->> `, )
+
+    const child=spawn('node',[pathToScriptFile,...args],
+        {
+            stdio: [null, null, null,'ipc'],
+            encoding: 'utf-8'
+        }
+    )
+
+    
+    child.stdout.pipe(process.stdout)
+    process.stdin.pipe(child.stdin)
+
+
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( [333, 444]);
