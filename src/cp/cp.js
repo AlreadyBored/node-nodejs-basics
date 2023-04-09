e14d02cdc5775c47788a550f99c86ac2468567ad
Liubov Krivtsova <avostvirk@gmail.com>
2023-04-09 09:46:54 +0300

    Child Processes tasks

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,17 @@
+import child_process from "child_process";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const forkProcess = child_process.fork('./src/cp/files/script.js', args);
+
+  forkProcess.on('message', msg => {
+    process.stdout.write(msg);
+  });
+
+  forkProcess.on('data', data => {
+    forkProcess.send(data);
+  });
+  return null;
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ['someArgument1', 'someArgument2']);
