d6bdc2a942965ea4817651490a8d9882e00c95fd
WilhelmYakunin <v.jakunin@inbox.ru>
2023-04-09 19:16:32 +0300

    feat: implement fs/raname

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,19 @@
+import { promises, existsSync } from 'fs';
+import path from 'path';
+import m from './messages.js';
+
+const __dirname = path.resolve();
+const renameePath = path.join(__dirname, 'files', 'wrongFilename.txt');
+const renamePath = path.join(__dirname, 'files', 'properFilename.md');
+
 const rename = async () => {
-    // Write your code here 
+    if (!existsSync(renameePath) || existsSync(renamePath)) throw new Error(m.ERR);
+    
+    try {
+        await promises.rename(renameePath, renamePath);
+    } catch (err) {
+        console.log(`${err}`);
+    }
 };
 
 await rename();
