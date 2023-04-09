d46c2c308ed7cb6f9ab619033fb9e1e2f129f9d1
Taneros <taneros@duck.com>
2023-04-09 20:26:56 +0300

    Squashed commit of the following:
    commit d7b8c28bcb3950062a9d8b8bc279fe42dda37ecb
    Author: Taneros <taneros@duck.com>
    Date:   Sun Apr 9 20:23:34 2023 +0300
    
        refactor: fs
    
    commit ed1ffa47bfa79a65bdb2becedb64097640f22e0f
    Author: Taneros <fricentaurus@GMAIL.COM>
    Date:   Wed Apr 5 21:53:16 2023 +0300
    
        feat: add .gitignore
    
    commit 0ce45949e8c70d6b13629e0cdfea60c2a7062f3c
    Author: rfathullin <rfathullin@luxms.com>
    Date:   Wed Apr 5 19:18:22 2023 +0300
    
        feat: finish read file
    
    commit b54316e88cc9b7bdbcc849108de30b222763d580
    Author: rfathullin <rfathullin@luxms.com>
    Date:   Wed Apr 5 19:10:52 2023 +0300
    
        feat: finish create list
    
    commit 6cf2579d98da638648a30ac591511efdfb47e53b
    Author: rfathullin <rfathullin@luxms.com>
    Date:   Wed Apr 5 19:07:21 2023 +0300
    
        feat: finish delete
    
    commit 0028c0499baa6d21b9de8f8cee689a1d7a62251a
    Author: rfathullin <rfathullin@luxms.com>
    Date:   Wed Apr 5 19:07:12 2023 +0300
    
        fix: copy
    
    commit 85ae6b0192c38ef293bdd130c57e48d5c960d1c6
    Author: rfathullin <rfathullin@luxms.com>
    Date:   Wed Apr 5 17:36:41 2023 +0300
    
        feat: finish rename
    
    commit 39660421247845819901117e3cb166acd678909b
    Author: rfathullin <rfathullin@luxms.com>
    Date:   Wed Apr 5 17:36:34 2023 +0300
    
        refactor: copy
    
    commit 4aa45a062f6d6ab9b6df624b637aa30be61653c3
    Author: rfathullin <rfathullin@luxms.com>
    Date:   Wed Apr 5 17:18:06 2023 +0300
    
        refactor:
    
    commit 4cf4fe5b9ea953c8502a641406697d0d8f115006
    Author: rfathullin <rfathullin@luxms.com>
    Date:   Wed Apr 5 17:15:30 2023 +0300
    
        feat: finish copy folder files to files_copy
    
    commit c6c211788f480fbd31cd0f396d0e7441ca3ed1c9
    Author: rfathullin <rfathullin@luxms.com>
    Date:   Wed Apr 5 17:15:13 2023 +0300
    
        refactor: create.js
    
    commit d36864d7f3dd11120d215c8b7a11a64a04c16236
    Author: rfathullin <rfathullin@luxms.com>
    Date:   Wed Apr 5 16:31:37 2023 +0300
    
        refactor: use async write method
    
    commit acb0b47410815f9d00f72b8b9736d4fc77abb101
    Author: rfathullin <rfathullin@luxms.com>
    Date:   Wed Apr 5 16:25:22 2023 +0300
    
        feat: add file exist check
    
    commit 3ebfd93f72200269cf063b65b05042c83170b872
    Author: rfathullin <rfathullin@luxms.com>
    Date:   Wed Apr 5 12:33:42 2023 +0300
    
        feat: add create fresh funcitonality

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,38 @@
-const copy = async () => {
-    // Write your code here 
+import fs from 'fs'
+import path from 'path'
+import {fileURLToPath} from 'url'
+
+const scriptPath=fileURLToPath(import.meta.url)
+
+const dirName=path.dirname(scriptPath)
+
+const pathToOriginDir=path.join(dirName,'files')
+
+const pathToNewDir=path.join(dirName,'files_copy')
+
+
+const copy=async () => {
+
+    try {
+        const filesToCopy=fs.readdirSync(pathToOriginDir)
+
+        fs.mkdirSync(pathToNewDir)
+
+        filesToCopy.forEach((file) => {
+            const oldFilePath=path.join(pathToOriginDir, file)
+            const newFilePath=path.join(pathToNewDir, file)
+            fs.copyFileSync(oldFilePath, newFilePath)
+        })
+        
+        console.log('\nCopied successfully!')
+
+    } catch(error) {
+        throw new Error('FS operation failed\n')
+    }
+
+
+
+
 };
 
 await copy();
