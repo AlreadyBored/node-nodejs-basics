82ac7f0e280538931392f9f8889c79921957f55e
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-04-09 23:55:31 +0500

    update compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,16 +1,12 @@
 import { createGzip } from 'zlib';
-import { pipeline } from 'stream';
 import { createReadStream, createWriteStream } from 'fs';
-import { promisify } from 'util';
-
-const pipe = promisify(pipeline);
 
 const compress = async () => {
     try {
         const gzip = createGzip();
         const source = createReadStream('./files/fileToCompress.txt');
         const destination = createWriteStream('./files/archive.gz');
-        await pipe(source, gzip, destination);
+        source.pipe(gzip).pipe(destination);
     } catch (err) {
         throw err;
     }
