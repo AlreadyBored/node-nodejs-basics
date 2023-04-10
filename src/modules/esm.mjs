1f79fdbb7370053fafc53bab9907ffb60a018bfc
WilhelmYakunin <v.jakunin@inbox.ru>
2023-04-10 22:35:39 +0300

    ref: cjsToEsm.cjs to esm.mjs

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,7 +1,7 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path from 'path';
+import { release, version } from 'os';
+import { createServer: createServerHttp } from 'http';
+import './files/c';
 
 const random = Math.random();
 
@@ -33,7 +33,7 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
+export {
     unknownObject,
     myServer,
 };
