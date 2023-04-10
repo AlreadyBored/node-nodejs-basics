43aa61a0546efcaa5114260804839fe8400ba4cd
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-04-11 01:05:16 +0500

    refactor

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,13 +1,17 @@
 import fs from 'fs';
 
 const read = async () => {
-    // Create readable stream
-    const readableStream = fs.createReadStream('./files/fileToRead.txt', {
-        encoding: 'utf8',
-    });
+    try {
+        // Create readable stream
+        const readableStream = fs.createReadStream('./files/fileToRead.txt', {
+            encoding: 'utf8',
+        });
 
-    // Handle data event and write chunks to the console
-    readableStream.on('data', (chunk) => process.stdout.write(chunk));
+        // Handle data event and write chunks to the console
+        readableStream.on('data', (chunk) => process.stdout.write(chunk));
+    } catch (err) {
+        throw err;
+    }
 };
 
 await read();
