2c4f71bb52e08392dd96021d46a057de9ba895c8
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-04-11 00:43:10 +0500

    refactor

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,28 +1,31 @@
 import fs from 'fs/promises';
 
 const copy = async () => {
-  try {
-    // create 'files_copy' folder
-    await fs.mkdir('./files_copy');
-    console.log("new './files_copy' folder created.");
+    try {
+        // Create 'files_copy' directory
+        await fs.mkdir('./files_copy');
+        console.log("'files_copy' directory created");
 
-    // get filenames from inside 'files' folder
-    const fileNames = await fs.readdir('./files');
+        // Get filenames from inside 'files' directory
+        const fileNames = await fs.readdir('./files');
 
-    // copy files from 'files' folder to 'files_copy' folder
-    fileNames.forEach(async (fileName) => {
-      await fs.copyFile(`./files/${fileName}`, `./files_copy/${fileName}`);
-    });
+        // Copy files from 'files' directory to 'files_copy' directory
+        fileNames.forEach(async (fileName) => {
+            await fs.copyFile(
+                `./files/${fileName}`,
+                `./files_copy/${fileName}`
+            );
+        });
 
-    console.log("copied all files from './files' to './files_copy'");
-  } catch (err) {
-    //  catch existence error
-    if (err.code === 'EEXIST' || err.code === 'ENOENT') {
-      throw Error('FS operation failed');
-    } else {
-      throw err;
+        console.log("Copied all files from './files' to './files_copy'");
+    } catch (err) {
+        //  Catch existence error
+        if (err.code === 'EEXIST' || err.code === 'ENOENT') {
+            throw Error('FS operation failed');
+        } else {
+            throw err;
+        }
     }
-  }
 };
 
 await copy();
