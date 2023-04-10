472d477f5d1350793421b949924e7806c330fb5a
WilhelmYakunin <v.jakunin@inbox.ru>
2023-04-10 22:27:44 +0300

    feat: implement hash/calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { createHash } from 'crypto';
+import { readFile } from 'fs';
+import { stdout } from 'process';
+import path from 'path';
+
+const __dirname = path.resolve();
+const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+    await readFile(filePath, (err, file) => {
+        if (err) throw err;
+        const hexed = createHash('sha256').update(file).digest('hex');
+        stdout.write(`the hex of sha256 of fileToCalculateHashFor.txt is ${hexed}`)
+    });
 };
 
 await calculateHash();
