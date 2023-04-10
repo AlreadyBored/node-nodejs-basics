2ab8f7906236dbb0aa922d51bb9519f1a80c91e6
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-04-11 00:53:02 +0500

    refactor

--- src/cli/args.js
+++ src/cli/args.js
@@ -2,7 +2,7 @@ const parseArgs = () => {
     // Get argument variables
     const argv = process.argv.slice(2);
 
-    // Print argument variables and their values
+    // Print argument variables with their values
     for (let i = 0; i < argv.length; i += 2) {
         console.log(`${argv[i].slice(2)} is ${argv[i + 1]}`);
     }
