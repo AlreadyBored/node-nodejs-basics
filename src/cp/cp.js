179593a22a30e953bbd29c00525e48be4897e6dc
Bekhzod Shukhratov <bekhzod2204@gmail.com>
2023-04-10 12:02:21 +0500

    finish src/cp tasks

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,12 @@
+import { spawn } from "child_process";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = spawn("node", ["./src/cp/files/script.js", ...args]);
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
+    child.on("exit", function (code) {
+        console.log("Child process exited with code " + code);
+    });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["someArgument1", "someArgument2"]);
