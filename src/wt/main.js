d758a895296e2f3ed3068edb5ec74a638cd152c0
WilhelmYakunin <v.jakunin@inbox.ru>
2023-04-10 21:30:51 +0300

    feat: implement wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,36 @@
-const performCalculations = async () => {
-    // Write your code here
+import { cpus } from 'os';
+import { Worker } from 'worker_threads';
+import path from 'path';
+const __dirname = path.resolve();
+const pathToFile = path.join(__dirname, 'worker.js');
+
+export const performCalculations = async () => {
+    const workersResults = [];  
+
+    const createWokers = (num) => new Promise((resolve, reject) => {
+        const worker = new Worker(pathToFile, { workerData: 10 + num });
+
+        worker.on('message', value => {           
+            resolve(workersResults.push({status: 'resolved', data: value}));    
+        }); 
+    
+        worker.on('error', _err => {    
+            reject(workersResults.push({status: 'error', data: null}));                                                 
+        });  
+        
+        worker.on('exit', (exitCode) => {
+            if (exitCode === 0) {
+              return null;
+            }
+         
+            return reject(new Error(`Worker has stopped with code ${exitCode}`));
+          });
+    })
+
+    const cores = cpus();
+    await Promise.all(cores.map((_, i) => createWokers(i)));
+
+    console.log(workersResults);
 };
 
 await performCalculations();
