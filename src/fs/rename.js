9e5d25f1ea3d39eadaec14d60407bb0fce3f77ac
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-04-11 00:46:28 +0500

    refactor

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,24 +1,28 @@
 import fs from 'fs/promises';
 
 const rename = async () => {
-  try {
-    //  check './files/properFilename.md' file exists
-    const fileNames = await fs.readdir('./files');
-    if (fileNames.includes('properFilename.md')) {
-      throw Error('FS operation failed');
-    }
+    try {
+        //  Check 'properFilename.md' file exists
+        const fileNames = await fs.readdir('./files');
+        if (fileNames.includes('properFilename.md')) {
+            throw new Error('FS operation failed');
+        }
 
-    //  rename wrongFilename to properFilename
-    await fs.rename('./files/wrongFilename.txt', './files/properFilename.md');
+        //  Rename 'wrongFilename.txt' to 'properFilename.md'
+        await fs.rename(
+            './files/wrongFilename.txt',
+            './files/properFilename.md'
+        );
 
-    console.log('successfully renamed');
-  } catch (err) {
-    if (err.code === 'ENOENT' || err.code === 'EEXIST') {
-      throw Error('FS operation failed');
-    } else {
-      throw err;
+        console.log('Successfully renamed');
+    } catch (err) {
+        //  Catch existence error
+        if (err.code === 'ENOENT' || err.code === 'EEXIST') {
+            throw new Error('FS operation failed');
+        } else {
+            throw err;
+        }
     }
-  }
 };
 
 await rename();
