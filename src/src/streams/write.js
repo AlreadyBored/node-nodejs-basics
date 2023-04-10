03f82759090325e67d2fca8cfdf15fa62d8e54e6
Bekhzod Shukhratov <bekhzod2204@gmail.com>
2023-04-10 11:58:31 +0500

    delete direcotry

--- src/src/streams/write.js
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-
-const write = async () => {
-    const fileStream = fs.createWriteStream('./files/fileToWrite.txt');
-    process.stdout.write("Enter some text to write to file: ")
-    process.stdin.pipe(fileStream);
-};
-
-await write();
