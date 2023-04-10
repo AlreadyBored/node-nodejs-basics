72910550d626596acab2f3191934cfc9a9b6e5c7
u1810234 <s.alijanov@student.inha.uz>
2023-04-11 04:45:09 +0500

    add subtask: zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,29 @@
+import { createReadStream, createWriteStream } from "fs";
+import { createGzip } from "zlib";
+
 const compress = async () => {
-    // Write your code here 
+  // Write your code here
+  const readStream = createReadStream("src/zip/files/fileToCompress.txt");
+
+  const writeStream = createWriteStream("archive.gz");
+
+  const gzip = createGzip();
+
+  readStream.pipe(gzip).pipe(writeStream);
+
+  writeStream.on("finish", () => {
+    console.log(
+      `File fileToCompress.txt has been compressed to archive.gz successfully.`
+    );
+  });
+
+  writeStream.on("error", (err) => {
+    console.error(`Error compressing file: ${err.message}`);
+  });
+
+  readStream.on("error", (err) => {
+    console.error(`Error reading file: ${err.message}`);
+  });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,29 @@
+import { createReadStream, createWriteStream } from "fs";
+import { createGunzip } from "zlib";
+
 const decompress = async () => {
-    // Write your code here 
+  // Write your code here
+  const readStream = createReadStream("archive.gz");
+
+  const writeStream = createWriteStream("src/zip/files/fileToCompress.txt");
+
+  const gunzip = createGunzip();
+
+  readStream.pipe(gunzip).pipe(writeStream);
+
+  writeStream.on("finish", () => {
+    console.log(
+      `File "archive.gz" has been decompressed to "fileToCompress.txt" successfully.`
+    );
+  });
+
+  writeStream.on("error", (err) => {
+    console.error(`Error decompressing file: ${err.message}`);
+  });
+
+  readStream.on("error", (err) => {
+    console.error(`Error reading file: ${err.message}`);
+  });
 };
 
 await decompress();
