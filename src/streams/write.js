95b639c1f27ac43e65faf228bb208959108647e7
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-04-11 01:05:53 +0500

    refactor

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,13 +1,20 @@
 import fs from 'fs';
 
 const write = async () => {
-    // Create writeable stream
-    const writeableStream = fs.createWriteStream('./files/fileToWrite.txt', {
-        encoding: 'utf8',
-    });
+    try {
+        // Create writeable stream
+        const writeableStream = fs.createWriteStream(
+            './files/fileToWrite.txt',
+            {
+                encoding: 'utf8',
+            }
+        );
 
-    // Handle data event and write chunks to the 'fileToWrite.txt' file
-    process.stdin.on('data', (chunk) => writeableStream.write(chunk));
+        // Handle data event and write chunks to the'fileToWrite.txt' file
+        process.stdin.on('data', (chunk) => writeableStream.write(chunk));
+    } catch (err) {
+        throw err;
+    }
 };
 
 await write();
