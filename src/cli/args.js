b65a1d7749e7ce90d388994104de67bcbb003c31
u1810234 <s.alijanov@student.inha.uz>
2023-04-11 02:13:44 +0500

    add subtask: cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,24 @@
+import process from "process";
+
 const parseArgs = () => {
-    // Write your code here 
+  // Write your code here
+  const args = process.argv;
+
+  const parsedArgs = {};
+
+  for (let i = 2; i < args.length; i += 2) {
+    const propName = args[i].substring(2);
+    ``;
+    const propValue = args[i + 1];
+
+    parsedArgs[propName] = propValue;
+  }
+
+  const formattedArgs = Object.entries(parsedArgs)
+    .map(([key, value]) => `${key} is ${value}`)
+    .join(", ");
+
+  console.log(formattedArgs);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,22 @@
+import fs from "fs";
+
 const parseEnv = () => {
-    // Write your code here 
+  // Write your code here
+  const envVariables = Object.entries(process.env);
+
+  const rssVariables = envVariables.filter(([key]) => key.startsWith("RSS_"));
+
+  const formattedVariables = rssVariables.map(
+    ([key, value]) => `${key}=${value}`
+  );
+
+  if (rssVariables.length === 0) {
+    throw new Error(
+      "FS operation failed: No environment variables with prefix RSS_ found."
+    );
+  }
+
+  console.log(formattedVariables);
 };
 
 parseEnv();
