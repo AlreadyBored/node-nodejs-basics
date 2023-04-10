eede3c4e6495265ca82f0859b37cab324edd3332
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-04-11 00:50:02 +0500

    refactor

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,22 +1,22 @@
 import fs from 'fs/promises';
 
 const read = async () => {
-  try {
-    // read './files/fileToRead.txt' file
-    const content = await fs.readFile('./files/fileToRead.txt', {
-      encoding: 'utf8',
-    });
+    try {
+        // Read 'fileToRead.txt' file
+        const content = await fs.readFile('./files/fileToRead.txt', {
+            encoding: 'utf8',
+        });
 
-    // print the content
-    console.log(content);
-  } catch (err) {
-    //  catch existence error
-    if (err.code === 'ENOENT') {
-      throw Error('FS operation failed');
-    } else {
-      throw err;
+        // Print the content
+        console.log(content);
+    } catch (err) {
+        //  Catch existence error
+        if (err.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        } else {
+            throw err;
+        }
     }
-  }
 };
 
 await read();
