21a7b50bce33194da70c99575664a0a96deafcde
Jenyajk <101403726+Jenyajk@users.noreply.github.com>
2023-04-10 12:51:18 +0500

    feat: add solution cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    let output = "";
+
+    for (let i = 0; i < args.length; i += 2) {
+        const propName = args[i].slice(2);
+        const value = args[i + 1];
+        output += `${propName} is ${value}, `;
+    }
+
+    console.log(output.slice(0, -2));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+    const prefix = 'RSS_';
+    const rssEnvVariables = Object.entries(process.env)
+        .filter(([key, value]) => key.startsWith(prefix))
+        .map(([key, value]) => `${key}=${value}`)
+        .join('; ');
+    if (rssEnvVariables) {
+        console.log(rssEnvVariables);
+    } else {
+        throw new Error('FS operation failed');
+    }
 };
 
 parseEnv();
