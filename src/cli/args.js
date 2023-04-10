79b43d62a9bf2d1020202824a2cd7170f45788f0
WilhelmYakunin <v.jakunin@inbox.ru>
2023-04-10 17:09:54 +0300

    feat: implement cli/args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
+import { argv, stdout } from 'process';
+
 const parseArgs = () => {
-    // Write your code here 
+    const args = argv.slice(2);
+    const toString = args.reduce((acc, arg, i) => i % 2 === 0 ? acc.concat(arg.slice(2), ' is ') : acc.concat(arg, '\n'), '');
+    
+    stdout.write(toString);
 };
 
 parseArgs();
