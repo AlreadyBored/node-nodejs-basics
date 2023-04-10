9ce2aeed26e836101c5973ca55b1819914df7c69
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-04-11 01:06:24 +0500

    refactor

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,8 +1,12 @@
 const transform = async () => {
-    // Handle chunks from console and write them to console
-    process.stdin.on('data', (chunk) =>
-        process.stdout.write(`received chunk: ${chunk}`)
-    );
+    try {
+        // Handle chunks from console and write them to console
+        process.stdin.on('data', (chunk) =>
+            process.stdout.write(`received chunk: ${chunk}`)
+        );
+    } catch (err) {
+        throw err;
+    }
 };
 
 await transform();
