f43197afe04a7b8c71bad36e85d1f0974f09766a
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-04-11 00:53:19 +0500

    refactor

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,8 +1,8 @@
 const parseEnv = () => {
-    // get environment variables
+    // Get environment variables
     const env = process.env;
 
-    // print environment variables with their values
+    // Print environment variables with their values
     for (const variable in env) {
         console.log(`RSS_${variable}=${env[variable]}`);
     }
