8355e3e1b5eafcf90891e9e08226821904832c3a
WilhelmYakunin <v.jakunin@inbox.ru>
2023-04-10 16:44:48 +0300

    feat: implement cp spawnChildPro

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,25 @@
+import { spawn } from 'child_process';
+import path from 'path';
+
+const __dirname = path.resolve();
+const pathToScriptFile = path.join(__dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childProcess = spawn('node ' + pathToScriptFile, args, {shell: true});
+    
+    process.stdin.on('data', data => {                  
+        childProcess.stdin.write(data);        
+    });
+
+    childProcess.stdout.on('data', data => {        
+        process.stdout.write(data);           
+    });
+
+    childProcess.on('close', () => {
+        process.exit();
+    }); 
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+const mockArgs = ['Foo_0', 'Foo_1', 'Foo_2'];
+spawnChildProcess(mockArgs);
+     
