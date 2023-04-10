bfd6b191159432a2400f519c263f10c4509e7c04
WilhelmYakunin <v.jakunin@inbox.ru>
2023-04-10 22:58:24 +0300

    feat: implement zlib

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream/promises';
+import path from 'path';
+
+const __dirname = path.resolve();
+const source = path.join(__dirname, 'files', 'fileToCompress.txt');
+const destination = path.join(__dirname, 'files', 'archive.gz');
+
 const compress = async () => {
-    // Write your code here 
+    const gzip = createGzip();
+    const input = createReadStream(source);
+    const output = createWriteStream(destination);
+
+    await pipeline(input, gzip, output).catch(err => process.stdout.write(String(err)));
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createUnzip } from 'zlib';
+import { pipeline } from 'stream/promises';
+import path from 'path';
+
+const __dirname = path.resolve();
+const source = path.join(__dirname, 'files', 'archive.gz');
+const destination  = path.join(__dirname, 'files', 'fileToCompress.txt');
+
 const decompress = async () => {
-    // Write your code here 
+    const unzip = createUnzip();
+    const input = createReadStream(source);
+    const output = createWriteStream(destination );
+
+    await pipeline(input, unzip, output).catch(err => process.stdout.write(String(err)));
 };
 
 await decompress();
