f9bf1324c0c3d3bd8d8d19caee3e548203c8e78c
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-04-11 01:36:15 +0500

    refactor

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,17 +1,21 @@
 import { spawn } from 'child_process';
 
 const spawnChildProcess = async (args) => {
-    // create a new child process using `script.js` as the script to run
-    const childProcess = spawn('node', ['./files/script.js', ...args]);
+    try {
+        // Create a new child process using `script.js` as the script to run
+        const childProcess = spawn('node', ['./files/script.js', ...args]);
 
-    // pipe the stdin and stdout of the child process to the master process
-    childProcess.stdout.pipe(process.stdout);
-    process.stdin.pipe(childProcess.stdin);
+        // Pipe the stdin and stdout of the child process to the master process
+        childProcess.stdout.pipe(process.stdout);
+        process.stdin.pipe(childProcess.stdin);
 
-    // log any errors from the child process to the console
-    childProcess.on('error', (error) => {
-        console.error(`Child process error: ${error}`);
-    });
+        // Log any errors from the child process to the console
+        childProcess.on('error', (error) => {
+            console.error(`Child process error: ${error}`);
+        });
+    } catch (err) {
+        throw err;
+    }
 };
 
 spawnChildProcess(['arg1', 'arg2', 'arg3']);
