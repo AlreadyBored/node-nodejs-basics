ea48f7e7c8c5a4f8e56ccfea9df83ffb1498de7a
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-04-11 01:13:13 +0500

    refactor

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -3,9 +3,14 @@ import { createReadStream, createWriteStream } from 'fs';
 
 const compress = async () => {
     try {
+        // Create gzip compression
         const gzip = createGzip();
+
+        // Create readable stream for source and writeable stream for destination
         const source = createReadStream('./files/fileToCompress.txt');
         const destination = createWriteStream('./files/archive.gz');
+
+        // Compress data from source stream to destination stream
         source.pipe(gzip).pipe(destination);
     } catch (err) {
         throw err;
