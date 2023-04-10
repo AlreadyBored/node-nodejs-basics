9d9f76c97714022f5f8ebc08a0373e9a151bdfcf
u1810234 <s.alijanov@student.inha.uz>
2023-04-11 04:50:12 +0500

    add subtask: cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,29 @@
+import { spawn } from "child_process";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  // Write your code here
+  const childProcess = spawn("node", ["script.js", ...args]);
+
+  childProcess.stdout.on("data", (data) => {
+    process.stdout.write(data);
+  });
+
+  childProcess.on("exit", (code) => {
+    console.log(`Child process exited with code: ${code}`);
+  });
+
+  childProcess.on("error", (err) => {
+    console.error(`Failed to spawn child process: ${err.message}`);
+  });
+
+  process.stdin.on("data", (data) => {
+    childProcess.stdin.write(data);
+  });
+
+  process.stdin.on("end", () => {
+    childProcess.stdin.end();
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(/* [someArgument1, someArgument2, ...] */);
