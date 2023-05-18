517c89783696b4055cd5dc33fcf1241966e9ce73
Aleksander Chumachenko <ledich19@gmail.com>
2023-05-18 22:09:10 +0300

    feat: add copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,22 @@
+import { mkdir, access, copyFile, readdir } from "node:fs/promises";
+const projectFolder = new URL("./files_copy/", import.meta.url);
+const projectFolder1 = new URL("./files/", import.meta.url);
+
 const copy = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    await access(projectFolder1);
+    await mkdir(projectFolder, { recursive: false });
+    const files = await readdir(projectFolder1, { withFileTypes: true });
+    for (const file of files) {
+      await copyFile(
+        new URL(file.name, projectFolder1),
+        new URL(file.name, projectFolder)
+      );
+    }
+  } catch (err) {
+    console.error("FS operation failed");
+  }
 };
 
 await copy();
