58cdf3625f4cc6ccc526b869c9cccbf9a3174f29
Aleksander Chumachenko <ledich19@gmail.com>
2023-05-19 13:18:00 +0300

    feat: add delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,13 @@
+import { unlink, access } from "node:fs/promises";
+const fileForRemove = new URL("./files/fileToRemove.txt", import.meta.url);
+
 const remove = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    await unlink(fileForRemove);
+  } catch (err) {
+    throw Error("FS operation failed", err.message);
+  }
 };
 
 await remove();
