f229d965bba630b06980e6784e4d157655403821
eugene.radionov <eugene.radionov@jagaad.it>
2023-05-19 17:59:22 +0200

    TASK-3: Implement hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import {fileURLToPath} from "url";
+import path from 'path';
+import {createReadStream} from "fs";
+
 const calculateHash = async () => {
-    // Write your code here 
+    const { createHash } = await import('node:crypto');
+
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const hash = createHash('sha256');
+    const readStream = createReadStream(`${__dirname}/files/fileToCalculateHashFor.txt`);
+
+    readStream.pipe(hash)
+        .setEncoding('hex')
+        .pipe(process.stdout);
 };
 
 await calculateHash();
