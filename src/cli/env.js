b8b42d1532942cadfd935a5c143103bcdaf4042c
Aleksander Chumachenko <ledich19@gmail.com>
2023-05-19 15:24:44 +0300

    feat: add env function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+  const keys = Object.keys(process.env);
+  keys.forEach((key) => {
+    if (key.startsWith("RSS_")) {
+      console.log(`${key}=${process.env[key]}`);
+    }
+  });
+  // Write your code here
 };
 
 parseEnv();
