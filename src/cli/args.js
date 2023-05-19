a48bc2a1c40c05974401f0eb061faeeb49ebdf3a
Aleksander Chumachenko <ledich19@gmail.com>
2023-05-19 15:42:08 +0300

    feat: remove -- from propname

--- src/cli/args.js
+++ src/cli/args.js
@@ -2,7 +2,7 @@ const parseArgs = () => {;
   // Write your code here
   const { argv } = process;
   for (let i = 2; i < argv.length; i += 2) {
-    console.log(`\u001B[34m${argv[i]} is \u001B[35m${argv[i + 1]}\u001B[0m`);
+    console.log(`\u001B[34m${argv[i].slice(2)} is \u001B[35m${argv[i + 1]}\u001B[0m`);
   }
 };
 
