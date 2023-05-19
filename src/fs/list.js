4d963c663453628bb4c3e92c6e3216f87bdafa7c
Aleksander Chumachenko <ledich19@gmail.com>
2023-05-19 14:04:16 +0300

    feat: add list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import { log } from "node:console";
+import { mkdir, access, copyFile, readdir } from "node:fs/promises";
+const projectFolder = new URL("./files/", import.meta.url);
+
 const list = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    await access(projectFolder);
+    const files = await readdir(projectFolder, { withFileTypes: true });
+    for (const file of files) {
+      console.log(`\x1B[34m${file.name}\u001B[0m`);
+    }
+  } catch (err) {
+    throw Error(`\u001B[31mFS operation failed\u001B[0m ${err.message}`);
+  }
 };
 
 await list();
