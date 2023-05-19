fcb28eaac43696cbe60dbf869c9d7afd2aea76af
Aleksander Chumachenko <ledich19@gmail.com>
2023-05-19 15:40:34 +0300

    feat: add args function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,9 @@
-const parseArgs = () => {
-    // Write your code here 
+const parseArgs = () => {;
+  // Write your code here
+  const { argv } = process;
+  for (let i = 2; i < argv.length; i += 2) {
+    console.log(`\u001B[34m${argv[i]} is \u001B[35m${argv[i + 1]}\u001B[0m`);
+  }
 };
 
 parseArgs();
