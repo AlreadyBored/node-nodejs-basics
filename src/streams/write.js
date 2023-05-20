4a7eb16284c79818317408613e29a540829b66e0
Aleksander Chumachenko <ledich19@gmail.com>
2023-05-21 00:26:53 +0300

    feat: add stream write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,27 @@
+import { createWriteStream } from "fs";
+const filePath = new URL("./files/fileToWrite.txt", import.meta.url);
+
 const write = async () => {
-    // Write your code here 
+  process.stdin.setEncoding("utf8");
+  const myStream = createWriteStream(filePath);
+  // Write your code here
+
+  process.stdin.on("data", (data) => {
+    if (data.trim() === "end") {
+      process.stdin.emit("end");
+    } else {
+      myStream.write(`${data.trim()}\n`);
+    }
+  });
+
+  process.stdin.on("end", () => {
+    console.log("Reading data completed.");
+    process.stdin.pause();
+    myStream.end();
+  });
+  myStream.on("finish", () => {
+    console.log("All writes are now complete.");
+  });
 };
 
 await write();
