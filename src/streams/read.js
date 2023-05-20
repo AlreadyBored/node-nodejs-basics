b614d9b480568887806efa9c757b532eb7b75ef0
Aleksander Chumachenko <ledich19@gmail.com>
2023-05-20 23:45:42 +0300

    feat: add stream read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
+import { createReadStream } from "node:fs";
+const filePath = new URL("./files/fileToRead.txt", import.meta.url);
+
 const read = async () => {
-    // Write your code here 
+  // Write your code here
+  const readStream = createReadStream(filePath);
+  readStream.on("data", (chunk) => {
+    process.stdout.write(`${chunk.toString()}\n`);
+  });
+  readStream.on("end", () => {});
 };
 
 await read();
