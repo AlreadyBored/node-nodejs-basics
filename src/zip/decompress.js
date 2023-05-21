7d1f2798f2d9d076d3c2981fd0aff4ea677769eb
Aleksander Chumachenko <ledich19@gmail.com>
2023-05-21 11:59:38 +0300

    feat: add to decompress function

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import { createGunzip } from "node:zlib";
+import { createReadStream, createWriteStream } from "node:fs";
+import { pipeline } from "node:stream/promises";
+const filePath = new URL("./files/archive.gz", import.meta.url);
+const fileResultPath = new URL("./files/fileToCompress.txt", import.meta.url);
+
 const decompress = async () => {
-    // Write your code here 
+  const gzip = createGunzip();
+  const source = createReadStream(filePath);
+  const destination = createWriteStream(fileResultPath);
+  try {
+    await pipeline(source, gzip, destination);
+  } catch (error) {
+    throw Error(`\u001B[31mZip operation failed\u001B[0m ${error.message}`);
+  }
 };
 
 await decompress();
