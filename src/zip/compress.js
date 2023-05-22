b405b089e22447141fa07c675a789180b98ce577
eugene.radionov <eugene.radionov@jagaad.it>
2023-05-22 18:41:07 +0200

    TASK-5: Implement zlib compression and decompression

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,22 @@
+import { Transform } from 'stream';
+import { pipeline } from 'stream/promises';
+import path from "path";
+import {fileURLToPath} from "url";
+import zlib from "node:zlib";
+import fs from "fs";
+
 const compress = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+    try {
+        await pipeline(
+            fs.createReadStream(`${__dirname}/files/fileToCompress.txt`),
+            zlib.createGzip(),
+            fs.createWriteStream(`${__dirname}/files/archive.gz`)
+        );
+    } catch (error) {
+        throw new Error('Compression finished with error');
+    }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import path from "path";
+import {fileURLToPath} from "url";
+import {pipeline} from "stream/promises";
+import fs from "fs";
+import zlib from "node:zlib";
+
 const decompress = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+    try {
+        await pipeline(
+            fs.createReadStream(`${__dirname}/files/archive.gz`),
+            zlib.createGunzip(),
+            fs.createWriteStream(`${__dirname}/files/fileToCompress.txt`)
+        );
+    } catch (error) {
+        throw new Error('Decompression finished with error');
+    }
 };
 
 await decompress();
