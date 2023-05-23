0e499d9663fcb9f4d25424bdd67a8cfffbc44926
eugene.radionov <eugene.radionov@jagaad.it>
2023-05-23 17:25:50 +0200

    TASK-7: Implement child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,17 @@
+import {spawn} from 'node:child_process';
+import path from "path";
+import {fileURLToPath} from "url";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const childProcess = spawn(
+        'node',
+        [`${__dirname}/files/script.js`, ...args]
+    );
+
+    process.stdin.pipe(childProcess.stdin);
+    childProcess.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+await spawnChildProcess(['--child', '--spawn']);
