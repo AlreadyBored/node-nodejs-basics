f7ca9b6bb7455c7b486c7dc820a0c45a285c3b28
Aleksander Chumachenko <ledich19@gmail.com>
2023-05-28 18:06:15 +0300

    feat: add wt solve

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,23 @@
+import { parentPort, Worker } from "worker_threads";
+import os from "node:os";
+const filePath = new URL("./worker", import.meta.url);
+
 const performCalculations = async () => {
-    // Write your code here
+  // Write your code here
+  const cpus = os.cpus();
+  const promises = cpus.map((info, i) => {
+    return new Promise((resolve, reject) => {
+      const worker = new Worker(filePath);
+      worker.postMessage(10 + i);
+      worker.on("message", (msg) => {
+        resolve(msg);
+      });
+    });
+  });
+
+  const result = await Promise.allSettled(promises);
+  console.log(result);
+  process.exit();
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,23 @@
 // n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { parentPort } from "worker_threads";
+
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  // This function sends result of nthFibonacci computations to main thread
+  parentPort.on("message", (message) => {
+    const n = parseInt(message, 10);
+    if (!isNaN(n)) {
+      const result = nthFibonacci(n);
+      parentPort.postMessage({ status: "resolved", data: result });
+    } else {
+      parentPort.postMessage({ status: "error", data: null });
+    }
+  });
+  parentPort.on("error", (message) => {
+    parentPort.postMessage({ status: "error", data: null });
+  });
 };
 
 sendResult();
