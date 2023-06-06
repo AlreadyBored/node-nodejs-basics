acd1777a6f21a1eb0a90c9e4ba363db0f5822f7c
KirillGenin <332211op@gmail.com>
2023-06-06 18:20:09 +0300

    feat: implement function cli/args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+    const entries = process.argv.slice(2).reduce((acc, arg) => {
+        arg.startsWith('--')
+            ? acc.push(arg.slice(2))
+            : acc.push(`${acc.pop()} is ${arg}`);
+        return acc;
+    }, []);
+    console.log(entries.join(', '));
 };
 
 parseArgs();
