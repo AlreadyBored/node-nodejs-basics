0e865b9be8b3f051034a2a023df75323012ac86d
KirillGenin <332211op@gmail.com>
2023-06-06 16:43:38 +0300

    feat: implement function fs/read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { ERROR_MESSAGE } from './libs/constants.js';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const targetFile = path.resolve(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const data = await fs.readFile(targetFile, { encoding: 'utf-8' });
+    console.log(data);
+  } catch {
+    throw new Error(ERROR_MESSAGE);
+  }
 };
 
 await read();
