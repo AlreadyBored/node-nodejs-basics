2da6dc7fdea549427402700bd66c232f3a546ece
KirillGenin <332211op@gmail.com>
2023-06-06 18:32:08 +0300

    feat: implement function cli/env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+    const entries = [];
+
+    for (const key in process.env) {
+        if (key.startsWith('RSS_')) {
+            entries.push(`${key}=${process.env[key]}`);
+        }
+    }
+    console.log(entries.join('; '))
 };
 
 parseEnv();
