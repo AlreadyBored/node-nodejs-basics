dadec7cd22b9080c12728abae79c92c9404a26b9
KirillGenin <332211op@gmail.com>
2023-06-06 16:34:09 +0300

    feat: implement function fs/list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { ERROR_MESSAGE } from './libs/constants.js';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const targetDir = path.resolve(__dirname, 'files');
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const files = await fs.readdir(targetDir);
+    console.log(files);
+  } catch {
+    throw new Error(ERROR_MESSAGE);
+  }
 };
 
 await list();
