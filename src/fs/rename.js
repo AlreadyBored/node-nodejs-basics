b49734cc02160e71390dc1c3ff1344fd054f3bd9
KirillGenin <332211op@gmail.com>
2023-06-06 14:03:45 +0300

    feat: implement function fs/rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,36 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { ERROR_MESSAGE } from './libs/constants.js';
+
+class ExistError extends Error {
+  constructor(message) {
+    super(message)
+    this.name = 'ExistError'
+  }
+}
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const oldFile = path.resolve(__dirname, 'files', 'wrongFilename.txt');
+const newFile = path.resolve(__dirname, 'files', 'properFilename.md');
+
 const rename = async () => {
-    // Write your code here 
+
+  try {
+    await fs.access(newFile);
+    throw new ExistError('');
+  } catch (error) {
+    if (error.name === 'ExistError') {
+      throw new Error(ERROR_MESSAGE);
+    }
+  }
+
+  try {
+    await fs.rename(oldFile, newFile);
+  } catch {
+    throw new Error(ERROR_MESSAGE);
+  }
 };
 
 await rename();
