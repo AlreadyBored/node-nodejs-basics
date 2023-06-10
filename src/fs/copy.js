02295e9416edb6d12f620c46c065e34e4ad65c32
Pavel Sheshko <derzurb@mail.ru>
2023-06-10 23:22:14 +0300

    feat: copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,28 @@
+import { fileURLToPath } from 'node:url'
+import { dirname } from 'node:path';
+import { cp } from 'node:fs/promises'
+import { existsSync } from 'node:fs'
+
 const copy = async () => {
-    // Write your code here 
+    const errorMessage = 'FS operation failed';
+    const source = '/files';
+    const destination = '/files_copy';
+
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const dirFilesExist = existsSync(`${__dirname}${source}`);
+    const dirFilesCopyExist = existsSync(`${__dirname}${destination}`);
+
+    if (!!dirFilesExist && !dirFilesCopyExist) {
+        try {
+            await cp(`${__dirname}${source}`, `${__dirname}${destination}`, { recursive: true });
+        } catch (e) {
+            throw new Error(errorMessage);
+        }
+    } else {
+        throw new Error(errorMessage);
+    }
 };
 
 await copy();
