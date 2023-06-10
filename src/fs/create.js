044c4cf4a48c9c5edf04eaa1b3e0a4cd51c8487d
Pavel Sheshko <derzurb@mail.ru>
2023-06-10 21:01:42 +0300

    feat: create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import { writeFile } from 'node:fs/promises'
+import { fileURLToPath } from 'node:url'
+import { dirname } from 'node:path';
+
 const create = async () => {
-    // Write your code here 
+    const content = 'I am fresh and young';
+    const errorMessage = 'FS operation failed';
+    const path = '/files/fresh.txt';
+
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    try {
+        await writeFile(`${__dirname}${path}`, content, {flag: 'wx'});
+    } catch(e) {
+        throw new Error(errorMessage);
+    }
 };
 
 await create();
