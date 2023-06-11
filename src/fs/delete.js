0914307b510b7af7db842336b234a69cbb598b8d
Pavel Sheshko <derzurb@mail.ru>
2023-06-11 15:48:15 +0300

    feat: remove file function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,26 @@
+import { fileURLToPath } from 'node:url'
+import { dirname } from 'node:path';
+import { existsSync } from 'node:fs'
+import { rm } from 'node:fs/promises'
+
 const remove = async () => {
-    // Write your code here 
+    const errorMessage = 'FS operation failed';
+    const fileToRemove = '/files/fileToRemove.txt';
+
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const isFileExist = existsSync(`${__dirname}${fileToRemove}`);
+
+    if (!isFileExist) {
+        throw new Error(errorMessage);
+    }
+
+    try {
+        await rm(`${__dirname}${fileToRemove}`);
+    } catch (err) {
+        throw new Error(errorMessage);
+    }
 };
 
 await remove();
