ab2fd24a96f3df9108a5edab46c1af24dfc63820
Pavel Sheshko <derzurb@mail.ru>
2023-06-11 15:58:03 +0300

    feat: list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,27 @@
+import { fileURLToPath } from 'node:url'
+import { dirname } from 'node:path';
+import { existsSync } from 'node:fs'
+import { readdir } from 'node:fs/promises'
+
 const list = async () => {
-    // Write your code here 
+    const errorMessage = 'FS operation failed';
+    const filesDirPath = '/files';
+
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const isFileExist = existsSync(`${__dirname}${filesDirPath}`);
+
+    if (!isFileExist) {
+        throw new Error(errorMessage);
+    }
+
+    try {
+        const files = await readdir(`${__dirname}${filesDirPath}`);
+        files.forEach(e => console.log(e));
+      } catch (err) {
+        throw new Error(errorMessage);
+      }
 };
 
 await list();
