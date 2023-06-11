ffe3af56844a7bbb6349df283f29622950dd27fd
Pavel Sheshko <derzurb@mail.ru>
2023-06-11 16:49:11 +0300

    feat: env cli

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+    const variables = Object.entries(process.env).reduce((acc, variable) => {
+        const [key, value] = variable;
+
+        if (key.startsWith('RSS_')) {
+            acc.push(`${key}=${value}`);
+        }
+        return acc;
+    }, []);
+
+    console.log(variables.join('; '));
 };
 
 parseEnv();
