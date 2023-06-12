2914326e6fdbe13570d720a268986722e8d33ba8
besnadne <serpov.box@gmail.com>
2023-06-12 11:06:40 +0300

    feat: apply create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import fs from 'fs';
+
 const create = async () => {
-    
+    const filePath = './files/fresh.txt';
+    try {
+        try {
+            await fs.access(filePath);
+            throw new Error('File already exists');
+        }catch (error) {
+
+        }
+        await fs.writeFile(filePath, 'I am fresh and young');
+        console.log('Fresh file created successfully!');
+    }catch (error) {
+        console.error(error.message)
+    }
 };
 
 await create();
