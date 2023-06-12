ae2151101bf1dc5bf1500670aa5bc84a43bca9dc
Klychnikov Alexandr <klychnikov75@mail.ru>
2023-06-12 22:11:14 +0300

    feat: implement copy.js function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,31 @@
+import fs from 'fs/promises';
+import path from 'path';
+import url from 'url'
+
 const copy = async () => {
-    // Write your code here 
+    const __filename = url.fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const dir = path.join(__dirname, 'files');
+    const copyDir = path.join(__dirname, 'files_copy')
+
+    try {
+        const files = await fs.readdir(dir, {withFileTypes: true});
+        await fs.mkdir(copyDir);
+        for(let file of files){
+            try {
+                await fs.copyFile(
+                  path.join(__dirname, 'files', file.name),
+                  path.join(__dirname, 'files_copy', file.name)
+                );
+              } catch (err) {
+                console.log(`The file ${path.join(__dirname, 'files', file.name)} could not be copied`);
+              }
+        }
+      } catch (err) {
+        if (err.code === 'ENOENT' || err.code === 'EEXIST') {
+            throw new Error('FS operation failed');
+        }
+      }
 };
 
 await copy();
