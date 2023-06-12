c3eb397c1cf1b2a0a830976e4cd3a28911e7f68c
Klychnikov Alexandr <klychnikov75@mail.ru>
2023-06-12 18:40:51 +0300

    feat: implement create.js function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import fs from 'fs/promises';
+import { access, constants } from 'fs';
+import path from 'path';
+import url from 'url'
+
 const create = async () => {
-    // Write your code here 
+  const __filename = url.fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const file = path.join(__dirname, 'files', 'fresh.txt');
+  const data = 'I am fresh and young';
+  access(file, constants.F_OK, (error) => {
+    if (error) {
+        fs.writeFile(file, data, (err) => {
+            if (err) throw err;
+        });
+    } else {
+        throw new Error('FS operation failed');
+    }
+
+  });
 };
 
 await create();
