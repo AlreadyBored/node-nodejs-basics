18894f6f74782659d7cb52c611c92251e410b5b7
Pavel Sashnikov <psashnikau@datamola.com>
2023-06-12 23:28:08 +0300

    feat: create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import { writeFile } from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 const create = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  writeFile(
+    `${__dirname}/files/fresh.txt`,
+    'I am fresh and young',
+    { flag: 'wx' },
+    (err) => {
+      if (err) {
+        throw new Error('FS operation failed');
+      }
+    }
+  );
 };
 
 await create();
