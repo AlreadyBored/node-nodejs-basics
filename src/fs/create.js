d768b0ae95ad02fd8b40ef28c015efe6d66151c1
Alexander Leonov <spawnleon@gmail.com>
2023-06-12 19:27:52 +0400

    feat: implement fs/create task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import { promises as fs } from 'node:fs';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, 'files', 'fresh.txt');
+  try {
+    await fs.writeFile(filePath, 'I am fresh and young', { flag: 'wx' });
+  } catch (err) {
+    if (err.code === 'EEXIST') {
+      throw new Error('FS operation failed');
+    } else {
+      throw err;
+    }
+  }
 };
 
 await create();
