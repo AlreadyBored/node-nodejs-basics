3bfce5fd711ba4a25c8572026c0d50f4ab35db42
artzilla <artzilla.github@gmail.com>
2023-06-13 19:57:31 +0300

    feat: implemented streams/write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+
 const write = async () => {
-    // Write your code here 
+  const filePath = path.join(
+    path.dirname(new URL(import.meta.url).pathname),
+    'files',
+    'fileToWrite.txt'
+  );
+  const writableStream = fs.createWriteStream(filePath);
+
+  process.stdin.pipe(writableStream);
+
+  writableStream.on('finish', () =>
+    console.log(`Data has been written to ${filePath}`)
+  );
+
+  writableStream.on('error', (error) =>
+    console.error(`Error writing to file: ${error}`)
+  );
 };
 
 await write();
