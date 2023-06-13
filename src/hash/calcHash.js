3a96a7ab91d8c378de993c01083bfed20cee3c8f
Sergey <sergeylabetsik@datateer.com>
2023-06-13 09:15:53 -0500

    hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import { createHash } from "crypto";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash("sha256", "MySecret");
+
+  const scriptPath = fileURLToPath(import.meta.url);
+  const dir = path.dirname(scriptPath);
+  const filename = path.join(dir, "files/fileToCalculateHashFor.txt");
+  const input = fs.createReadStream(filename);
+  input.on("readable", () => {
+    const data = input.read();
+    if (data) {
+      hash.update(data);
+    } else {
+      console.log(hash.digest("hex"));
+    }
+  });
 };
 
 await calculateHash();
