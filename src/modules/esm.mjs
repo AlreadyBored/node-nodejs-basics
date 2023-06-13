224c261977c7a103a256d28e5c49e786c8559e25
banzayche <dharm.serg@gmail.com>
2023-06-13 23:20:14 +0300

    finishing with esm

---
+++ src/modules/esm.mjs
@@ -0,0 +1,39 @@
+import { sep, dirname } from 'node:path';
+import { release, version } from 'node:os';
+import { createServer as createServerHttp } from 'node:http';
+import { fileURLToPath } from 'node:url'
+import { readFile } from 'fs/promises';
+import './files/c.js';
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const random = Math.random();
+
+export let unknownObject;
+
+if (random > 0.5) {
+    unknownObject = readFile(`${__dirname}/files/a.json`);
+} else {
+    unknownObject = readFile(`${__dirname}/files/b.json`);
+}
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${sep}"`);
+
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
+
+export const myServer = createServerHttp((_, res) => {
+    res.end('Request accepted');
+});
+
+const PORT = 3000;
+
+unknownObject.then(data => console.log(JSON.parse(data)));
+
+myServer.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+    console.log('To terminate it, use Ctrl+C combination');
+});
