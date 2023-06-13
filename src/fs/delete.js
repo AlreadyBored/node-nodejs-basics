b5f75cf61e637bd537b439433375e246582ced16
Klychnikov Alexandr <klychnikov75@mail.ru>
2023-06-13 12:33:51 +0300

    feat: implement delete.js function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import fs from 'fs/promises';
+import path from 'path';
+import url from 'url'
+
 const remove = async () => {
-    // Write your code here 
+    const __filename = url.fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const fileToRemove = path.join(__dirname, 'files', 'fileToRemove.txt');
+
+    try {
+        await fs.rm(fileToRemove);
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await remove();
