931e1dc6d9a551a922868f3c86f62096487941de
Pavel Sheshko <derzurb@mail.ru>
2023-06-13 10:02:59 +0300

    feat: parseArgs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
 const parseArgs = () => {
-    // Write your code here 
+    const cleanProcessArgv = process.argv.slice(2);
+
+    const finalArgvArray = cleanProcessArgv.reduce((acc, el, index, arr) => {
+        if (el.startsWith('--') && arr[index + 1]) {
+            const cleanEl = el.slice(2);
+            const argvArrayTransformed = `${cleanEl} is ${arr[index + 1]}`;
+            acc.push(argvArrayTransformed);
+        }
+
+        return acc;
+    }, [])
+
+    console.log(finalArgvArray.join(', '));
 };
 
 parseArgs();
