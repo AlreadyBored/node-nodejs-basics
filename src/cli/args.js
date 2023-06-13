d4860bc558e15cdfb83bcdefe845b41137bb5472
banzayche <dharm.serg@gmail.com>
2023-06-13 19:04:37 +0300

    Finishing with cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
+import { argv } from 'node:process'
 const parseArgs = () => {
-    // Write your code here 
+    // Write your code here
+  try {
+    let args = '';
+    for (let item of argv) {
+      if (item.match(/^--/g)) args = `${args}${item} is ${argv[argv.indexOf(item)+1]}, `;
+    }
+
+    console.log(args);
+  } catch (e) {
+    console.error(e);
+  }
+
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
+import { env } from 'node:process';
 const parseEnv = () => {
-    // Write your code here 
+    // Write your code here
+  try {
+    let variables = '';
+    for (let key in env) {
+      if (key.match(/^RSS_/g)) variables = `${variables}${key}=${env[key]}; `;
+    }
+    console.log(variables);
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 parseEnv();
