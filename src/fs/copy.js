8511c6958126e569615aa02fbbf7aa87f604cba2
banzayche <dharm.serg@gmail.com>
2023-06-13 17:45:20 +0300

    Finishing with create and copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,29 @@
+import { existsSync } from 'node:fs';
+import { readdir, copyFile, mkdir } from 'node:fs/promises'
+
 const copy = async () => {
-    // Write your code here 
+    // Write your code here
+  const __dirname = new URL(".", import.meta.url).pathname;
+
+  try {
+
+    console.log('source.txt was copied to destination.txt');
+  } catch {
+    console.error('The file could not be copied');
+  }
+
+  try {
+    if (!existsSync(`${__dirname}files`) || existsSync(`${__dirname}files_copy`)) throw Error('FS operation failed');
+
+    await mkdir(`${__dirname}files_copy`, { recursive: true });
+
+    const files = await readdir(`${__dirname}files`);
+    for (const file of files) {
+      await copyFile(`${__dirname}files/${file}`, `${__dirname}files_copy/${file}`);
+    }
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,16 @@
+import { existsSync } from 'node:fs';
+import { appendFile } from 'node:fs/promises'
+
 const create = async () => {
-    // Write your code here 
+    // Write your code here
+    const __dirname = new URL(".", import.meta.url).pathname;
+    const path = `${__dirname}files/fresh.txt`;
+    try {
+        if (existsSync(path)) throw Error('FS operation failed')
+        await appendFile(path, 'I am fresh and young', { encoding: 'utf8' });
+    } catch (err) {
+        console.warn(err);
+    }
 };
 
 await create();
