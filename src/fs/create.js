101b583e02160d9bfcabceceb3cca1cecefa27fc
KirillGenin <332211op@gmail.com>
2023-06-13 18:04:49 +0300

    feat: implement function fs/create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { ERROR_MESSAGE } from './libs/constants.js';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const targetFile = path.resolve(__dirname, 'files', 'fresh.txt');
+const data = 'I am fresh and young';
+
 const create = async () => {
-    // Write your code here 
-};
+  try {
+    await fs.writeFile(targetFile, data, { flag: 'wx' });
+  } catch {
+    throw new Error(ERROR_MESSAGE);
+  }
+}
 
 await create();
