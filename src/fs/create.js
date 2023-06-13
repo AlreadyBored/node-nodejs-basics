2900b8a66cf5279c1e1eb130c0834587aaadc637
Ihar Berasneu <iberesnev@alfabank.ru>
2023-06-13 10:58:46 +0300

    feat: write function create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,25 @@
+
+
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'fresh.txt');
+const fileContent = 'I am fresh and young';
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await fs.access(filePath);
+    throw new Error('File already exists');
+  } catch (error) {
+    if (error.code !== 'ENOENT') {
+      throw error;
+    }
+  }  
+
+  await fs.writeFile(filePath, fileContent);
 };
 
 await create();
