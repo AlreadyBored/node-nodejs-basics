551c0979d22947b274f7a9ad03a3a1188b6f77b6
Nikita Mikhalevich <n.mikhalevich@syberry.com>
2023-06-13 16:31:10 +0300

    Done rename

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -2,7 +2,6 @@ import * as fs from 'fs';
 import path from 'path';
 import {fileURLToPath} from 'url';
 
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,17 @@
+import { log } from 'console';
+import * as fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const rename = async () => {
-    // Write your code here 
+    fs.rename(__dirname + '/files/wrongFilename.txt', __dirname + '/files/properFilename.md', err => {
+        if (err) {
+            return console.log('FS operation failed');
+        }
+     });
 };
 
 await rename();
