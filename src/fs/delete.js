10841189c969f92239368578b2ff7dbea8c99fab
SeregaNag <71101232+SeregaNag@users.noreply.github.com>
2023-06-13 19:59:43 +0300

    Delete is ready

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,22 @@
+import { unlink, access } from "node:fs/promises";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+  const filesDir = join(__dirname, "files", "fileToRemove.txt");
+
+  try {
+    await access(filesDir);
+    await unlink(filesDir);
+  } catch (error) {
+    if (error.code !== "ENOENT") {
+      throw new Error("FS operation failed");
+    }
+    throw new Error("FS operation failed");
+  }
 };
 
 await remove();
