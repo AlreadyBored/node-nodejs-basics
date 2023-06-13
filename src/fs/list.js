059c01762473a0a9bab3bb29721a1b2308eaed41
Klychnikov Alexandr <klychnikov75@mail.ru>
2023-06-13 12:47:50 +0300

    feat: implement list.js function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,24 @@
+import fs from 'fs/promises';
+import path from 'path';
+import url from 'url'
+
 const list = async () => {
-    // Write your code here 
+    const __filename = url.fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const dir = path.join(__dirname, 'files');
+    const filesArr = []
+
+    try {
+        const files = await fs.readdir(dir, {withFileTypes: true});
+        for(let file of files){
+            if (file.isFile()) {
+                filesArr.push(file.name)
+            }
+        }
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
+    console.log(filesArr);
 };
 
 await list();
