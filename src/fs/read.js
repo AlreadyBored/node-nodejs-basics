bfd0ada5b4ca0ebabcacb8a1b62e2362a8bae743
SeregaNag <71101232+SeregaNag@users.noreply.github.com>
2023-06-13 20:16:18 +0300

    File System is ready

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,23 @@
+import { readFile, access } from "node:fs/promises";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const filesDir = join(__dirname, "files", "fileToRead.txt");
+
+  try {
+    await access(filesDir);
+    const text = await readFile(filesDir, "utf8");
+    console.log(text);
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    }
+    throw new Error("FS operation failed");
+  }
 };
 
 await read();
