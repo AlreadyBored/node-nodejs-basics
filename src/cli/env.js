40a140246c4e59895187952f328f3194d6c64970
Klychnikov Alexandr <klychnikov75@mail.ru>
2023-06-14 00:13:58 +0300

    feat: implement parseEnv function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+    const rss = []
+    const vars  = process.env
+    for (let key in vars) {
+      if (key.slice(0, 4) === 'RSS_') {
+        rss.unshift(`${key}=${vars[key]}`)
+      }
+    }
+    console.log(rss.join('; '))
 };
 
 parseEnv();
