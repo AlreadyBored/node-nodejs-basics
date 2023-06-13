5a423da66e28a1022ea9213d8d66a9a31faac65b
Alexander Leonov <spawnleon@gmail.com>
2023-06-13 06:11:21 +0400

    feat: implement fs/delete task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import { promises as fs } from 'node:fs';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { isDirOrFileExist } from './../utils/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+  const fileToRemovePath = join(__dirname, 'files', 'fileToRemove.txt');
+  const doesFileToRemovePathExist = await isDirOrFileExist(fileToRemovePath);
+  if (!doesFileToRemovePathExist) {
+    throw new Error('FS operation failed');
+  }
+
+  await fs.unlink(fileToRemovePath);
 };
 
 await remove();
