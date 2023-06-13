2fbc1b2a86c6465880ae2c1a7a66019b6e260db9
artzilla <artzilla.github@gmail.com>
2023-06-13 16:12:50 +0300

    feat: implemented fs/create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+
 const create = async () => {
-    // Write your code here 
+  const currentFilePath = new URL(import.meta.url).pathname;
+  const dirname = path.dirname(currentFilePath);
+  const folderPath = path.join(dirname, 'files');
+  const filePath = path.join(folderPath, 'fresh.txt');
+
+  try {
+    if (fs.existsSync(filePath)) {
+      throw new Error('FS operation failed');
+    }
+
+    fs.writeFile(filePath, 'I am fresh and young', () => {
+      console.log('A new file has been created');
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
 };
 
 await create();
