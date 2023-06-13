f2a47a2425d77600cc15dae9c76eb38d5f9e8324
ryzhenkov.d <ideal103.5@gmail.com>
2023-06-13 15:26:20 +0400

    copy create

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,43 @@
+import path from 'path';
+import fs from 'fs';
+import url from 'url';
+
 const copy = async () => {
-    // Write your code here 
+    const fileFolder = 'files';
+    const fileFolderCopy = 'files_copy';
+
+    const __filename = url.fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename)
+
+    fs.access(path.join(__dirname, fileFolder), fs.constants.F_OK,(err) => {
+        if (err) {
+            throw new Error('FS operation failed');
+        }
+    });
+
+    fs.access(path.join(__dirname, fileFolderCopy), fs.constants.F_OK,(err) => {
+        if (!err) {
+            throw new Error('FS operation failed');
+        }
+    });
+
+    fs.mkdir(fileFolderCopy, (err) => {
+        if (err) {
+            throw new Error(err.message);
+        }
+
+        fs.readdir(fileFolder, (err, files) => {
+            if (err) {
+                throw new Error(err.message)
+            }
+
+            files.forEach(item => {
+                fs.copyFile(path.join(__dirname, fileFolder, item), path.join(__dirname, fileFolderCopy, item), () => {
+
+                });
+            });
+        });
+    });
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,27 @@
+import path from 'path';
+import fs from 'fs';
+import url from 'url';
+
 const create = async () => {
-    // Write your code here 
+    const fileName = 'fresh.txt';
+    const fileContent = 'I am fresh and young';
+    const fileFolder = 'files';
+
+    const __filename = url.fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename)
+
+    fs.access(path.join(__dirname, fileFolder, fileName), fs.constants.F_OK,(err) => {
+        if (!err) {
+            throw new Error('FS operation failed');
+        }
+    });
+
+    fs.writeFile(path.join(__dirname, fileFolder, fileName), fileContent, (err, ) => {
+        if (err) {
+            throw new Error(err.message);
+        }
+    });
+
 };
 
 await create();
