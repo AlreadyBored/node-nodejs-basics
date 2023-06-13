3edff90d0fbfc10923211d7bd584b6a475440af0
Pavel Sashnikov <psashnikau@datamola.com>
2023-06-13 12:20:07 +0300

    feat: add rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,13 @@
+import { rename as rn } from 'fs';
+import { HELPER } from './modules/helpers.mjs';
+
 const rename = async () => {
-    // Write your code here 
+  const __dirname = HELPER.getDirPath(import.meta.url);
+  rn(
+    `${__dirname}/files/wrongFilename.txt`,
+    `${__dirname}/files/properFilename.md`,
+    HELPER.fsErrCb
+  );
 };
 
 await rename();
