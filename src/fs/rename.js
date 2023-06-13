9d31a12160caa020d9788d8137cd994bd2e661f3
Mag <magzhan_birgebaiuly@epam.com>
2023-06-13 20:26:26 +0600

    feat: rename wrongFilname to properFilename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,18 @@
-const rename = async () => {
-    // Write your code here 
+import fs from "fs";
+import getPath from "../helper/getPath.js";
+
+const rename = async (fileFrom, fileTo) => {
+  return new Promise((resolve, reject) => {
+    fs.rename(fileFrom, fileTo, (error) => {
+      if (error !== null) {
+        return reject("FS operation failed");
+      }
+
+      resolve();
+    });
+  });
 };
 
-await rename();
+const wrongFile = getPath(import.meta, "files", "wrongFilename.txt");
+const properFile = getPath(import.meta, "files", "properFilename.md");
+await rename(wrongFile, properFile);
