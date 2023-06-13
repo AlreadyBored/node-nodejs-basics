cb5c058c07776921e0936f115ce2bb887eb7a385
Mag <magzhan_birgebaiuly@epam.com>
2023-06-13 22:28:33 +0600

    feat: calculate hash from file

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,13 @@
-const calculateHash = async () => {
-    // Write your code here 
+import crypto from "crypto";
+import getPath from "../helper/getPath.js";
+import read from "../fs/read.js";
+
+const calculateHash = async (filePath) => {
+  const data = await read(filePath);
+  const result = crypto.createHmac("sha256", data).digest("hex");
+
+  console.log(result);
 };
 
-await calculateHash();
+const hashFile = getPath(import.meta, "./files/fileToCalculateHashFor.txt");
+await calculateHash(hashFile);
