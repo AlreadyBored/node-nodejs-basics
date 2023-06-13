686741b03e3d835d9921ccbbed525263db17aaf0
artzilla <artzilla.github@gmail.com>
2023-06-13 17:00:22 +0300

    feat: implemented fs/delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,26 @@
+// delete.js - implement function that deletes file fileToRemove.txt
+// (if there's no file fileToRemove.txt Error with message FS operation failed must be thrown)
+import fs from 'fs';
+import path from 'path';
+
 const remove = async () => {
-    // Write your code here 
+  const currentFolder = new URL(import.meta.url).pathname;
+  const filePath = path.join(
+    path.dirname(currentFolder),
+    'files',
+    'fileToRemove.txt'
+  );
+
+  try {
+    if (!fs.existsSync(filePath)) {
+      throw new Error('FS operation failed: File does not exist');
+    }
+
+    fs.unlinkSync(filePath);
+    console.log('File has been deleted successfully.');
+  } catch (err) {
+    console.log(err.message);
+  }
 };
 
 await remove();
