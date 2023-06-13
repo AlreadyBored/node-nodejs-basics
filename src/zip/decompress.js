096ff75832d2a12d65660ba4c0a561886434ed63
artzilla <artzilla.github@gmail.com>
2023-06-13 21:26:14 +0300

    feat: implemented zip/decompress function

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,28 @@
+import fs from 'fs';
+import path from 'path';
+import zlib from 'node:zlib';
+
 const decompress = async () => {
-    // Write your code here 
+  const basedir = path.join(
+    path.dirname(new URL(import.meta.url).pathname),
+    'files'
+  );
+  const sourceFilePath = path.join(basedir, 'archive.gz');
+  const destinationFilePath = path.join(basedir, 'fileToCompress1.txt');
+
+  const unzip = zlib.createUnzip();
+  const sourceFileReadStream = fs.createReadStream(sourceFilePath);
+  const destinationFileWriteStream = fs.createWriteStream(destinationFilePath);
+
+  sourceFileReadStream.pipe(unzip).pipe(destinationFileWriteStream);
+
+  destinationFileWriteStream.on('finish', () => {
+    console.log('File has been decompressed');
+  });
+
+  destinationFileWriteStream.on('error', (error) => {
+    console.error('Failed to decompress file:', error);
+  });
 };
 
 await decompress();
