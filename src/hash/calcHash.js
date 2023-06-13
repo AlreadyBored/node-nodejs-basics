7f67cc41994956189630d2c15a035211d452b300
artzilla <artzilla.github@gmail.com>
2023-06-13 18:12:42 +0300

    feat: implemented hash/calcHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import { createReadStream } from 'fs';
+import path from 'path';
+const { createHash } = await import('crypto');
+
 const calculateHash = async () => {
-    // Write your code here 
+  const currentFilePath = new URL(import.meta.url).pathname;
+  const dirname = path.dirname(currentFilePath);
+  const folderPath = path.join(dirname, 'files');
+  const filePath = path.join(folderPath, 'fileToCalculateHashFor.txt');
+
+  const hash = createHash('sha256');
+  const input = createReadStream(filePath);
+
+  input.on('readable', () => {
+    const data = input.read();
+    if (data) {
+      hash.update(data);
+    } else {
+      console.log(`${hash.digest('hex')}`);
+    }
+  });
 };
 
 await calculateHash();
