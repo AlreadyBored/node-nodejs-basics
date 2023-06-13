c40f00f88c83fa2ec8e1de3a3873b4e8ac72379e
artzilla <artzilla.github@gmail.com>
2023-06-13 17:21:40 +0300

    feat: implemented fs/read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+
 const read = async () => {
-    // Write your code here 
+  const currentFolderPath = new URL(import.meta.url).pathname;
+  const filePath = path.join(
+    path.dirname(currentFolderPath),
+    'files',
+    'fileToRead.txt'
+  );
+
+  try {
+    if (!fs.existsSync(filePath)) {
+      throw new Error('FS operation failed: File does not exist');
+    }
+
+    const content = fs.readFileSync(filePath, 'utf-8');
+    console.log(content);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 await read();
