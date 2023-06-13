e0db83261f45904ce85711e5f46a38e4489b5f9d
Pavel Sheshko <derzurb@mail.ru>
2023-06-13 18:22:48 +0300

    fix: layout

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -8,9 +8,10 @@ const calculateHash = async () => {
 
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = dirname(__filename);
-    const fileContent = await readFile(`${__dirname}${pathToFile}`);
 
+    const fileContent = await readFile(`${__dirname}${pathToFile}`);
     const hash = createHash('sha256').update(fileContent).digest('hex');
+
     console.log('File hash <-------', hash);
 };
 
