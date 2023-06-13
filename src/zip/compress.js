e175d6fc6b835c5579ddfe6954a1d82628c7a676
Pavel Sashnikov <psashnikau@datamola.com>
2023-06-13 17:09:59 +0300

    feat: zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import { createGzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import { HELPER } from '../fs/modules/helpers.mjs';
+
 const compress = async () => {
-    // Write your code here 
+  const origFilePath =
+    HELPER.getDirPath(import.meta.url) + '/files/fileToCompress.txt';
+  const compressedFilePath =
+    HELPER.getDirPath(import.meta.url) + '/files/fileToCompress.txt.gz';
+  const gz = createGzip();
+
+  createReadStream(origFilePath)
+    .pipe(gz)
+    .pipe(createWriteStream(compressedFilePath));
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,17 @@
+import { createUnzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import { HELPER } from '../fs/modules/helpers.mjs';
+
 const decompress = async () => {
-    // Write your code here 
+  const origFilePath =
+    HELPER.getDirPath(import.meta.url) + '/files/fileToCompress.txt';
+  const compressedFilePath =
+    HELPER.getDirPath(import.meta.url) + '/files/fileToCompress.txt.gz';
+  const gz = createUnzip();
+
+  createReadStream(compressedFilePath)
+    .pipe(gz)
+    .pipe(createWriteStream(origFilePath));
 };
 
 await decompress();
