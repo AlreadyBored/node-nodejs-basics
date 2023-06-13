190d1ad4c3ebd1834a24d19e34a1ae34a433e710
maksim.khramtsov <maksim.khramtsov@btsdigital.kz>
2023-06-13 15:24:09 +0200

    feat: implement wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,44 @@
+import { joinToURL } from "../helpers.js";
+import wt from "node:worker_threads";
+import os from "node:os";
+
+os.cpus();
+
 const performCalculations = async () => {
-    // Write your code here
+  // Write your code here
+  const workerPath = joinToURL(import.meta.url, "worker.js");
+
+  const cpuQuantity = os.cpus().length;
+
+  const workerTasks = Array.from({
+    length: cpuQuantity,
+  }).map(
+    (_, i) =>
+      new Promise((resolve, reject) => {
+        const worker = new wt.Worker(workerPath, {
+          workerData: i + 10,
+        });
+
+        worker.on("message", (workerResult) => {
+          console.log(workerResult);
+          resolve({
+            status: "resolved",
+            data: workerResult,
+          });
+        });
+
+        worker.on("error", () => {
+          resolve({
+            status: "error",
+            data: null,
+          });
+        });
+      })
+  );
+
+  return await Promise.all(workerTasks);
 };
 
-await performCalculations();
+const qwe = await performCalculations();
+
+console.log(qwe);
