73b04017c2569027e6d2b212fd5fc14da2ccdb38
artzilla <artzilla.github@gmail.com>
2023-06-13 18:21:06 +0300

    feat: implemented streams/read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.join(
+    path.dirname(new URL(import.meta.url).pathname),
+    'files',
+    'fileToRead.txt'
+  );
+  const readableStream = fs.createReadStream(filePath, 'utf8');
+
+  readableStream.on('data', (dataChunk) => process.stdout.write(dataChunk));
+  readableStream.on('error', (error) =>
+    console.error(`Error reading the file: ${error}`)
+  );
 };
 
 await read();
