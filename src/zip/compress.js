63867c5de0364eaa3ca50c3139045d325a4b1800
Sergey <sergeylabetsik@datateer.com>
2023-06-13 12:54:21 -0500

    zlib tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import fs from "fs";
+import path from "path";
+import { pipeline } from "stream/promises";
+import { fileURLToPath } from "url";
+import zlib from "zlib";
+
 const compress = async () => {
-    // Write your code here 
+  const scriptPath = fileURLToPath(import.meta.url);
+  const dir = path.dirname(scriptPath);
+  const srcPath = path.join(dir, "files", "fileToCompress.txt");
+  const destPath = path.join(dir, "files", "archive.gz");
+  const readStream = fs.createReadStream(srcPath);
+  const writeStream = fs.createWriteStream(destPath);
+  await pipeline(readStream, zlib.createGzip(), writeStream);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,17 @@
+import fs from "fs";
+import path from "path";
+import { pipeline } from "stream/promises";
+import { fileURLToPath } from "url";
+import zlib from "zlib";
+
 const decompress = async () => {
-    // Write your code here 
+  const scriptPath = fileURLToPath(import.meta.url);
+  const dir = path.dirname(scriptPath);
+  const srcPath = path.join(dir, "files", "archive.gz");
+  const destPath = path.join(dir, "files", "fileToCompress.txt");
+  const readStream = fs.createReadStream(srcPath);
+  const writeStream = fs.createWriteStream(destPath);
+  await pipeline(readStream, zlib.createGunzip(), writeStream);
 };
 
 await decompress();
