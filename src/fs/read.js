bf958f331445dc8dc41a5c632da22dc5a43cc1fb
Mag <magzhan_birgebaiuly@epam.com>
2023-06-13 20:44:33 +0600

    feat: read file script

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
-const read = async () => {
-    // Write your code here 
+import fs from "fs";
+import getPath from "../helper/getPath.js";
+
+const read = async (filePath) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, "utf-8", (error, data) => {
+      if (error !== null) {
+        return reject("FS operation failed");
+      }
+
+      resolve(data);
+    });
+  });
 };
 
-await read();
+const readFile = getPath(import.meta, "files", "fileToRead.txt");
+await read(readFile).then(console.log);
