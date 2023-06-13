cdd882342c1545c859f112b195de592637772d9d
artzilla <artzilla.github@gmail.com>
2023-06-13 16:49:13 +0300

    feat: implemented fs/copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,35 @@
+import fs from 'fs';
+import path from 'path';
+
 const copy = async () => {
-    // Write your code here 
+  const currentFilePath = new URL(import.meta.url).pathname;
+  const dirname = path.dirname(currentFilePath);
+  const sourceFolderPath = path.join(dirname, 'files');
+  const destinationFolderPath = path.join(dirname, 'files_copy');
+
+  try {
+    if (
+      !fs.existsSync(sourceFolderPath) ||
+      fs.existsSync(destinationFolderPath)
+    ) {
+      throw new Error('FS operation failed');
+    }
+
+    fs.mkdirSync(destinationFolderPath);
+
+    const fileNames = fs.readdirSync(sourceFolderPath);
+    for (const fileName of fileNames) {
+      fs.copyFileSync(
+        path.join(sourceFolderPath, fileName),
+        path.join(destinationFolderPath, fileName),
+        fs.constants.COPYFILE_EXCL
+      );
+    }
+
+    console.log('Folder "files" has been copied successfully to "files_copy"');
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await copy();
