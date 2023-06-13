f2680aa1aabc43c30acca161e8b634e29b239a44
Nikita Mikhalevich <n.mikhalevich@syberry.com>
2023-06-13 16:42:02 +0300

    Done read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import * as fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    fs.readFile(__dirname + '/files/fileToRead.txt', 'utf-8', (err, data) => {
+        if (err) {
+            return console.log('FS operation failed');
+        }
+        return console.log(data);
+    })
 };
 
 await read();
