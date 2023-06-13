655273a1b0adb5c8736781e3ac18c5fc6356671b
Pavel Sashnikov <psashnikau@datamola.com>
2023-06-13 19:04:49 +0300

    feat: threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,36 @@
+import { cpus } from 'os';
+import { Worker } from 'worker_threads';
+import { HELPER } from '../fs/modules/helpers.mjs';
+
+const parseResponse = (res) => {
+  const map = {
+    fulfilled: 'resolved',
+    rejected: 'error',
+  };
+  return res.reduce((acc, el) => {
+    const obj = {
+      status: map[el.status],
+      data: el.value || null,
+    };
+    acc.push(obj);
+    return acc;
+  }, []);
+};
+
 const performCalculations = async () => {
-    // Write your code here
+  const workerFilePath = HELPER.getDirPath(import.meta.url) + '/worker.js';
+  const cpuCount = cpus().length;
+  console.log(cpuCount);
+  const workers = [];
+  for (let i = 0; i < cpuCount; i++) {
+    const prom = new Promise((res, rej) => {
+      const worker = new Worker(workerFilePath, { workerData: 10 + i });
+      worker.on('message', res);
+      worker.on('error', rej);
+    });
+    workers.push(prom);
+  }
+  Promise.allSettled(workers).then((val) => console.log(parseResponse(val)));
 };
 
 await performCalculations();
