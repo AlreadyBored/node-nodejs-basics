7969994c29f6f5c31417afa66ae2a483f24b8659
Pavel Sheshko <derzurb@mail.ru>
2023-06-13 19:03:50 +0300

    feat: read and write steams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,26 @@
+import { fileURLToPath } from 'node:url'
+import { dirname } from 'node:path'
+import { createReadStream } from 'node:fs';
+
 const read = async () => {
-    // Write your code here 
+    const pathToFile = '/files/fileToRead.txt';
+
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const readableStream = createReadStream(`${__dirname}${pathToFile}`, 'utf8');
+
+    readableStream.on('data', (data) => {
+        process.stdout.write(data);
+    });
+
+    readableStream.on('end', () => {
+        console.log('\n');
+    });
+
+    readableStream.on('error', (err) => {
+        console.error('Error reading the file:', err);
+    });
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import { fileURLToPath } from 'node:url'
+import { dirname } from 'node:path'
+import { createWriteStream } from 'node:fs';
+
 const write = async () => {
-    // Write your code here 
+    const pathToFile = '/files/fileToWrite.txt';
+
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const writeStream = createWriteStream(`${__dirname}${pathToFile}`, { flags: 'a' }); // flags: 'a' - to append the file and add additional text to it
+    process.stdin.on('data', (data) => {
+        writeStream.write(data.toString());
+    });
+
+    console.log('Write something to your terminal and then check fileToWrite.txt ...');
 };
 
 await write();
