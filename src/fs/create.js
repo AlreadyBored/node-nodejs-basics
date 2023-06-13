cb64b3c9a50eafdbbd3adc433090509422c59234
SeregaNag <71101232+SeregaNag@users.noreply.github.com>
2023-06-13 10:00:35 +0300

    Create is done

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import { writeFile, access } from "node:fs/promises";
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await access("src/fs/files/fresh.txt");
+    throw new Error("FS operation failed");
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      try {
+        await writeFile(
+          "src/fs/files/fresh.txt",
+          "I am fresh and young",
+          "utf8"
+        );
+      } catch (error) {
+        console.error(error);
+      }
+    } else throw error;
+  }
 };
 
 await create();
