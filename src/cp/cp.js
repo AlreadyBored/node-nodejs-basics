d5b3d32401c9fbbe4ae0e61b94f93cef0fab44af
maksim.khramtsov <maksim.khramtsov@btsdigital.kz>
2023-06-13 14:58:35 +0200

    feat: implement cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,18 @@
+import cp from "node:child_process";
+import { joinToURL } from "../helpers.js";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const scriptPath = joinToURL(import.meta.url, "files", "script.js");
+
+  cp.fork(scriptPath, args, {
+    stdio: ["inherit", "inherit", "inherit", "ipc"],
+  });
 };
 
+const asd = {
+  qwe: {},
+};
+asd.qwe = asd;
+
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([1, "someArgument2", asd]);
