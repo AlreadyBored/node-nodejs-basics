ac73db68cfb1516df3d8f5c8579453de13ed6af6
artzilla <artzilla.github@gmail.com>
2023-06-13 16:54:05 +0300

    feat: implemented fs/rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,27 @@
+import fs from 'fs';
+import path from 'path';
+
 const rename = async () => {
-    // Write your code here 
+  const currentFilePath = new URL(import.meta.url).pathname;
+  const dirname = path.dirname(currentFilePath);
+  const sourceFilePath = path.join(dirname, 'files', 'wrongFilename.txt');
+  const destinationFilePath = path.join(dirname, 'files', 'properFilename.md');
+
+  try {
+    if (!fs.existsSync(sourceFilePath)) {
+      throw new Error('FS operation failed: Source file does not exist');
+    }
+
+    if (fs.existsSync(destinationFilePath)) {
+      throw new Error('FS operation failed: Destination file already exists');
+    }
+
+    fs.renameSync(sourceFilePath, destinationFilePath);
+
+    console.log('File has been renamed successfully.');
+  } catch (err) {
+    throw new Error(`FS operation failed: ${err.message}`);
+  }
 };
 
 await rename();
