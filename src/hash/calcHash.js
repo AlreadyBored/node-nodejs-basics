3a907bc719c84dccacce8459227ba4935c9ec4ea
Pavel Sashnikov <psashnikau@datamola.com>
2023-06-13 16:15:41 +0300

    feat: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import { createHash } from 'crypto';
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const fileBuffer = readFileSync(
+    __dirname + '/files/fileToCalculateHashFor.txt',
+    () => {}
+  );
+  const hashSum = createHash('sha256');
+  hashSum.update(fileBuffer);
+
+  const hex = hashSum.digest('hex');
+  console.log(hex);
 };
 
 await calculateHash();
