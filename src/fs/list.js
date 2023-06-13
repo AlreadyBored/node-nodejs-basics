e438c35270a5ca2619541c6b92d1605b390db3bf
Pavel Sashnikov <psashnikau@datamola.com>
2023-06-13 12:41:27 +0300

    feat: add list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,11 @@
+import { readdir } from 'fs';
+import { HELPER } from './modules/helpers.mjs';
+
 const list = async () => {
-    // Write your code here 
+  const __dirname = HELPER.getDirPath(import.meta.url);
+  const res = readdir(`${__dirname}/files`, (err, files) =>
+    err ? HELPER.fsErrCb(err) : console.log(files)
+  );
 };
 
 await list();
