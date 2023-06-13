6a5ee529f042d3c993c19b94f850789698ae17be
Nikita Mikhalevich <n.mikhalevich@syberry.com>
2023-06-13 15:23:26 +0300

    Done create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import * as fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+    try {
+        const data = 'I am fresh and young';
+        const path = __dirname + '/files/fresh.txt';
+        fs.writeFileSync(path, data);
+    } catch {
+        console.log('FS operation failed');
+    }
 };
 
 await create();
