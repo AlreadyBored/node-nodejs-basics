b55a694ca0a1fe322c9b282ffd34ab8d94e1f7a4
artzilla <artzilla.github@gmail.com>
2023-06-13 17:10:32 +0300

    feat: implemented fs/list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+
 const list = async () => {
-    // Write your code here 
+  const currentFilePath = new URL(import.meta.url).pathname;
+  const dirname = path.dirname(currentFilePath);
+  const folderPath = path.join(dirname, 'files');
+
+  try {
+    if (!fs.existsSync(folderPath)) {
+      throw new Error('FS operation failed: Folder does not exist');
+    }
+
+    const fileNames = fs.readdirSync(folderPath);
+    for (const fileName of fileNames) {
+      console.log(fileName);
+    }
+  } catch (err) {
+    throw new Error(`FS operation failed: ${err.message}`);
+  }
 };
 
 await list();
