8c9a5a97a1c161d5def4e3d04552d0bbdd87e62f
ryzhenkov.d <ideal103.5@gmail.com>
2023-06-13 18:33:41 +0400

    rename delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,21 @@
+import path from 'path';
+import fs from 'fs';
+import url from 'url';
+
 const remove = async () => {
-    // Write your code here 
+    const fileFolder = 'files';
+
+    const __filename = url.fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    fs.access(path.join(__dirname, fileFolder, 'fileToRemove.txt'), fs.constants.F_OK,(err) => {
+        if (err) {
+            throw new Error('FS operation failed');
+        }
+    });
+
+    fs.unlink(path.join(__dirname, fileFolder, 'fileToRemove.txt'), () => {});
+
 };
 
 await remove();
