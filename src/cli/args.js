7d73da7e3aa43078b50db927e8c452628032442a
artzilla <artzilla.github@gmail.com>
2023-06-13 17:38:13 +0300

    feat: implemented cli/args function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
 const parseArgs = () => {
-    // Write your code here 
+  // Slicing arguments to exclude node executable path and the file path
+  const args = process.argv.slice(2);
+
+  for (let i = 0; i < args.length; i += 2) {
+    console.log(`${args[i].substring(2)} is ${args[i + 1]}`);
+  }
 };
 
 parseArgs();
