626bebe41ce70f39d3891c4919244f50b72eea91
ryzhenkov.d <ideal103.5@gmail.com>
2023-06-13 18:33:47 +0400

    rename delete

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,26 @@
+import path from 'path';
+import fs from 'fs';
+import url from 'url';
+
 const rename = async () => {
-    // Write your code here 
+    const fileFolder = 'files';
+
+    const __filename = url.fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    fs.access(path.join(__dirname, fileFolder, 'wrongFilename.txt'), fs.constants.F_OK,(err) => {
+        if (err) {
+            throw new Error('FS operation failed');
+        }
+    });
+
+    fs.access(path.join(__dirname, fileFolder, 'properFilename.md'), fs.constants.F_OK,(err) => {
+        if (!err) {
+            throw new Error('FS operation failed');
+        }
+    });
+
+    fs.rename(path.join(__dirname, fileFolder, 'wrongFilename.txt'), path.join(__dirname, fileFolder, 'properFilename.md'),() => {});
 };
 
 await rename();
