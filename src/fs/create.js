9e0c98737046a492ddfea60dc28f1e44057fcb2f
Mag <magzhan_birgebaiuly@epam.com>
2023-06-13 18:36:06 +0600

    feat: create fresh file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import url from "url";
+import path from "path";
+import fs from "fs";
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, "files", "fresh.txt");
+
 const create = async () => {
-    // Write your code here 
+  return new Promise((resolve, reject) => {
+    fs.writeFile(filePath, "I am fresh and young", (error) => {
+      if (error !== null) {
+        return reject("FS operation failed");
+      }
+
+      resolve();
+    });
+  });
 };
 
 await create();
