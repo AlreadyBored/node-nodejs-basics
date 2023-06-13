ad3b6f7e70d94a05ba335d20fb6760a7f253e483
Pavel Sheshko <derzurb@mail.ru>
2023-06-13 22:09:59 +0300

    feat: steam transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,22 @@
+import { Transform, pipeline } from 'node:stream';
+
 const transform = async () => {
-    // Write your code here 
+    const revertData = new Transform({
+        transform(data, encoding, callback) {
+            callback(null, data.toString().split('').reverse().join(''));
+        }
+    })
+
+    pipeline(
+        process.stdin,
+        revertData,
+        process.stdout,
+        (err) => {
+            throw err;
+        }
+    )
+
+    console.log('Enter smth to console... \n');
 };
 
 await transform();
