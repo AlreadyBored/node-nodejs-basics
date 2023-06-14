9846258c4c0ab2447687f24aab03a31d70980cd1
KirillGenin <332211op@gmail.com>
2023-06-14 16:49:08 +0300

    feat: implement function wt/main

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,27 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { Worker } from 'node:worker_threads';
+import { cpus } from 'node:os';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const performCalculations = async () => {
-    // Write your code here
+
+  const initValue = 10;
+
+  const promises = cpus().map((_, idx) => {
+    return new Promise(resolve => {
+      const worker = new Worker(path.resolve(__dirname, 'worker.js'), {
+        workerData: (initValue + idx),
+      });
+
+      worker.on('message', (data) => resolve({ status: 'resolved', data }));
+      worker.on('error', () => resolve({ status: 'error', data: null }));
+    })
+  });
+
+  const results = await Promise.all(promises);
+  console.log(results);
 };
 
 await performCalculations();
