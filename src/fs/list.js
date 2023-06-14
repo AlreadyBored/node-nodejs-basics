eef155de62c6d0ff2c04cd7b621a9efd9a14fafc
jkenjayev <javohirwebdev@gmail.com>
2023-06-14 14:53:58 +0500

    fs:list.js is ready

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { readdir, stat } from "fs/promises";
 const list = async () => {
     // Write your code here 
+    const filePath = fileURLToPath(import.meta.url);
+    const dirPath = dirname(filePath) + "/files";
+    const errMsg = 'FS operation failed'
+
+    try {
+        await stat(dirPath)
+        console.log(await readdir(dirPath))
+    } catch (err) {
+        console.log(err);
+        throw new Error(errMsg);
+    }
 };
 
 await list();
