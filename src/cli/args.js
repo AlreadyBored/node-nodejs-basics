eb5614221525236f8097fb27d077aa5cd37d8579
Klychnikov Alexandr <klychnikov75@mail.ru>
2023-06-14 09:17:49 +0300

    feat: implement parseArgs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
-};
+    const output = []
+    const args  = process.argv.slice(2)
+    args.forEach((e, i) => {
+        if (e.slice(0, 2) === '--') {
+            output.push(`${e.slice(2, e.length)} is ${args[i + 1]}`)
+        }
+    })
+    console.log(output.join(', '))
+}
 
 parseArgs();
