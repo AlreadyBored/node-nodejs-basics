07a1526ebc5c9f054fd070089dc6923824e3c42b
KirillGenin <332211op@gmail.com>
2023-06-14 12:15:41 +0300

    feat: implement function streams/write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const write = async () => {
-    // Write your code here 
+    const writeStream = createWriteStream(path.resolve(__dirname, 'files', 'fileToWrite.txt'), { flags: 'a' });
+    process.stdout.write('Введите текст:\n');
+    await pipeline(process.stdin, writeStream);
 };
 
 await write();
