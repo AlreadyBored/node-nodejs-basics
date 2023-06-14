a0718a71716afdcaa859c7483a2c77b87f160671
fizarum <fizarum@gmail.com>
2023-06-14 23:56:50 +0200

    feat: implement all tasks in 'cli' section

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,32 @@
+import { argv } from "node:process";
+
 const parseArgs = () => {
-    // Write your code here 
+  //index 0: node.exe
+  //index 1: src\cli\args.js
+  argv.splice(0, 2);
+
+  const argvStrings = extractArgs(argv);
+  const resultString = argvStrings.join(", ");
+
+  console.log(resultString);
+};
+
+/** accepts arrays of arguments (string[]) and extracts key-value pairs
+ * @returns new arrays of strings in format: '<key> is <value>'
+ */
+const extractArgs = (argv) => {
+  const argKeyPrefix = "--";
+  //array of strings, each item represents each pair of <arg key, arg value>
+  const result = [];
+
+  argv.forEach((str, index) => {
+    if (str.startsWith(argKeyPrefix)) {
+      const key = str.slice(argKeyPrefix.length, str.length);
+      result.push(`${key} is ${argv[index + 1]}`);
+    }
+  });
+
+  return result;
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,21 @@
+import { env } from "node:process";
+
+/*
+to test this function you can execute(tested on windows, powershell):
+ > $env:RSS_name1="value1"; $env:RSS_name2="value2"; $env:RSS_name3="another_value"
+
+to remove previously declared variables execute:
+ > $env:RSS_name1=''; $env:RSS_name2=''; $env:RSS_name3=''
+*/
+
 const parseEnv = () => {
-    // Write your code here 
+  const keyPrefix = "RSS_";
+
+  const varNames = Object.keys(env).filter((key) => key.startsWith(keyPrefix));
+  const varPairs = varNames.map((name) => `${name}=${env[name]}`);
+  const result = varPairs.join("; ");
+
+  console.log(result);
 };
 
 parseEnv();
