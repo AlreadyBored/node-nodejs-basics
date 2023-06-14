ccafdf2f4f4bb2a46b663a208ec5cd1ea62ce971
Mirael_No <bogdanova.karina22@gmail.com>
2023-06-14 14:41:49 +0300

    Revert "Finished create function"
    This reverts commit 6f0c7ea77faff913ad85f48657e2a3089c074828.

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,16 +1,5 @@
-import { stat, writeFile } from 'node:fs/promises';
-
 const create = async () => {
-  try {
-    await stat('src/fs/files/fresh.txt');
-    throw new Error('FS operation failed');
-  } catch (error) {
-    if (error.code === 'ENOENT') {
-      await writeFile('src/fs/files/fresh.txt', 'I am fresh and young');
-    } else {
-      throw error.message;
-    }
-  }
+    // Write your code here 
 };
 
 await create();
