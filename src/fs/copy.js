4cd61d4efdd4db626760e8d992853f1739efd2f0
Mar Kal <marc@example.com>
2023-06-14 12:34:07 +0200

    feat: finish copy.js subtask

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,30 @@
+import { readdir, mkdir, stat, copyFile, access } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const copy = async () => {
-    // Write your code here 
+  const source = 'files';
+  const destination = 'files_copy';
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const folderPatch = join(__dirname, source);
+
+  try {
+    const files = await readdir(folderPatch);
+    await mkdir(destination, { recursive: true });
+
+    for (let file of files) {
+      let sourcePath = join(source, file);
+      let destinationPath = join(destination, file);
+
+      await copyFile(sourcePath, destinationPath);
+    }
+
+    console.log('Folder copied successfully.');
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import { existsSync, writeFileSync } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, 'files', 'fresh.txt');
+
+  try {
+    const isFileExist = existsSync(filePath);
+    if (isFileExist) {
+      throw new Error('FS operation failed');
+    }
+
+    writeFileSync(filePath, 'I am fresh and young');
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await create();
