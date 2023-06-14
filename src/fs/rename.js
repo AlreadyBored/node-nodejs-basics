5b87fa2a214bc937b9c8c1610b94d693c9fc8d01
Mirael_No <bogdanova.karina22@gmail.com>
2023-06-14 16:25:15 +0300

    feat: rename function is finished

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,24 @@
+import { rename as renameFs, stat } from 'node:fs/promises';
+
 const rename = async () => {
-    // Write your code here 
+  const oldFile = 'src/fs/files/wrongFilename.txt';
+  const newFile = 'src/fs/files/properFilename.md';
+
+  try {
+    await stat(oldFile);
+    try {
+      await stat(newFile);
+      throw new Error('FS operation failed');
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        renameFs(oldFile, newFile);
+      } else {
+        throw error;
+      }
+    }
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await rename();
