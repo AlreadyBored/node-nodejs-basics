647e0d6a71657fff34da37b30f283db71d62b101
Pavel Sheshko <derzurb@mail.ru>
2023-06-14 10:14:39 +0300

    feat: compress file

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,28 @@
+import { pipeline } from 'node:stream/promises'
+import {
+    createReadStream,
+    createWriteStream,
+} from 'node:fs';
+import { createGzip } from 'node:zlib';
+import { fileURLToPath } from 'node:url'
+import { dirname } from 'node:path'
+
 const compress = async () => {
-    // Write your code here 
+    const pathToFile = '/files/fileToCompress.txt';
+
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    try {
+        await pipeline(
+            createReadStream(`${__dirname}${pathToFile}`),
+            createGzip(),
+            createWriteStream(`${__dirname}/files/archive.gz`)
+        );
+        console.log('File was compressed.');
+    } catch {
+        throw new Error('Operation error')
+    }
 };
 
 await compress();
