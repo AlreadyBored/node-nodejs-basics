967670afe194fd84422e0f8d559975ceb12387ca
KirillGenin <332211op@gmail.com>
2023-06-14 11:53:36 +0300

    feat: implement function streams/read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { createReadStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+    const readStream = createReadStream(path.resolve(__dirname, 'files', 'fileToRead.txt'));
+    await pipeline(readStream, process.stdout);
 };
 
 await read();
