6cf2f9eddb3b0205924da406e3a921de1a23baad
Mirael_No <bogdanova.karina22@gmail.com>
2023-06-14 15:07:29 +0300

    feat: copy function is finished

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,15 +1,17 @@
-import { readdir, mkdir } from 'node:fs/promises';
-import { join, dirname } from 'node:path';
+import { readdir, mkdir, rmdir, copyFile } from 'node:fs/promises';
+import { join } from 'node:path';
 
 const copy = async () => {
   try {
     const from = 'src/fs/files';
     const to = 'src/fs/files_copy';
+    const files = await readdir(from);
     await mkdir(to);
-
-    const files = readdir(from);
+    for (const file of files) {
+      copyFile(join(from, file), join(to, file));
+    }
   } catch (error) {
-    if (error.code === 'EEXIST' || error.code === 'ENOENT')
+    if (error.code === 'ENOENT' || error.code === 'EEXIST')
       throw new Error('FS operation failed');
   }
 };
