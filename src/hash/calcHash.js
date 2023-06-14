9f8868c6f5c20ecc2b01329319060be50f5fefaa
Klychnikov Alexandr <klychnikov75@mail.ru>
2023-06-14 21:15:36 +0300

    feat: implement hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import fs from 'fs';
+import path from 'path';
+import crypto from 'crypto';
+import url from 'url'
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __filename = url.fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const file = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+    fs.createReadStream(file).
+      pipe(crypto.createHash('sha256').setEncoding('hex')).
+      on('finish', function() {
+        console.log(this.read())
+      })
 };
 
 await calculateHash();
