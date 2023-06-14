f430698257514a2b64b58903912bd6cd18ea5dbe
Mirael_No <bogdanova.karina22@gmail.com>
2023-06-14 18:41:12 +0300

    feat: finish cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,8 @@
 const parseArgs = () => {
-    // Write your code here 
+  for (let i = 2; i < process.argv.length; i += 2) {
+    const name = process.argv[i].substring(2);
+    const value = process.argv[i + 1];
+    console.log(`${name} is ${value}`);
+  }
 };
-
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
-};
+  const variables = Object.entries(process.env).filter(([key]) =>
+    key.startsWith('RSS_')
+  );
 
+  for (const [key, value] of variables) {
+    console.log(`${key}=${value};`);
+  }
+};
 parseEnv();
