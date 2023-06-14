1dd7221da3e430587de28057b20f2331308858dd
jkenjayev <javohirwebdev@gmail.com>
2023-06-14 22:58:25 +0500

    env is finished

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
     // Write your code here 
+    let result = [];
+    for (const key in process.env) {
+        if (key.includes('RSS_')) {
+            result.push(`${key} = ${process.env[key]}`);
+        }
+    }
+    console.log(result.join("; "))
 };
 
 parseEnv();
