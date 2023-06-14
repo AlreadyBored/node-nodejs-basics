d578261f7197d8a02fd584130f7252faefdbdac6
KirillGenin <332211op@gmail.com>
2023-06-14 13:04:26 +0300

    feat: implement function streams/transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,19 @@
+import { Transform } from 'node:stream';
+import { pipeline } from 'node:stream/promises';
+
+const reverse = (str) => str.split('').reverse().join('');
+
 const transform = async () => {
-    // Write your code here 
+
+    const transformStream = new Transform({
+        transform(chunk, _, callback) {
+            this.push(reverse(chunk.toString()));
+            this.push('\n\n');
+            callback()
+        },
+    });
+
+    await pipeline(process.stdin, transformStream, process.stdout);
 };
 
 await transform();
