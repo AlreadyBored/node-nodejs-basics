ed95ea2160c67d978e1b488dcfd20bbcbf9ae36c
Alexander Leonov <spawnleon@gmail.com>
2023-06-14 22:52:37 +0400

    feat: implement fs/list task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,23 @@
+import { promises as fs } from 'node:fs';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { isDirOrFileExist } from './../utils/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+  const dirListPath = join(__dirname, 'files');
+  const doesDirListPathExist = await isDirOrFileExist(dirListPath);
+
+  if (!doesDirListPathExist) {
+    throw new Error('FS operation failed');
+  }
+
+  const fileList = await fs.readdir(dirListPath);
+
+  console.log(fileList);
 };
 
 await list();
