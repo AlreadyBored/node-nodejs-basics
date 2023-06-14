e551a6aa0cdcbbd17d75fde9e359cd9038301bc5
Hanna Huzava <guzovaann@gmail.com>
2023-06-15 00:10:14 +0200

    feat: add create()

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,15 @@
-const create = async () => {
-    // Write your code here 
+import { writeFile} from "node:fs/promises"
+
+export const create = async () => {
+    
+    const fileContent = 'I am fresh and young!';
+    const errorMessage = 'FS operation failed';
+
+    try {
+        await writeFile('./src/fs/files/fresh.txt', fileContent, {flag: 'wx'});
+    } catch (error) {
+        throw new Error(errorMessage);
+    }
 };
 
-await create();
+create();
