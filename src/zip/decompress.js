819d6245c8475059a3cfcb21fb95b4fd7d7c28a5
Pavel Sheshko <derzurb@mail.ru>
2023-06-14 10:22:05 +0300

    feat: decompress file

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,28 @@
+import { pipeline } from 'node:stream/promises'
+import {
+    createReadStream,
+    createWriteStream,
+} from 'node:fs';
+import { createGunzip } from 'node:zlib';
+import { fileURLToPath } from 'node:url'
+import { dirname } from 'node:path'
+
 const decompress = async () => {
-    // Write your code here 
+    const pathToFile = '/files/archive.gz';
+
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    try {
+        await pipeline(
+            createReadStream(`${__dirname}${pathToFile}`),
+            createGunzip(),
+            createWriteStream(`${__dirname}/files/fileToCompress.txt`)
+        );
+        console.log('File was decompressed.');
+    } catch {
+        throw new Error('Operation error')
+    }
 };
 
 await decompress();
