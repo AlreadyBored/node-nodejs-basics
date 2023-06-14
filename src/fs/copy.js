42ea7a35926d8618600730b18f051f848ee2640f
Max_Nikitenok <xxx250790@gmail.com>
2023-06-14 23:29:13 +0300

    feat: fs/copy splution

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,25 @@
+import { copyFile, mkdir, readdir } from 'node:fs/promises';
+import path from 'node:path';
+
+const folderPath = new URL('files', import.meta.url);
+const folderCopyPath = new URL('files_copy', import.meta.url);
+const errorMessage = 'FS operation failed';
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    const files = await readdir(folderPath);
+    await mkdir(folderCopyPath);
+    files.map(async (file) => {
+      const filePath = new URL(path.join('files', file), import.meta.url);
+      const fileCopyPath = new URL(
+        path.join('files_copy', file),
+        import.meta.url
+      );
+      await copyFile(filePath, fileCopyPath);
+    });
+  } catch {
+    throw new Error(errorMessage);
+  }
 };
 
 await copy();
