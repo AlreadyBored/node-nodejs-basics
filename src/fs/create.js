aaf0c4367f0a59fd6fa5ff36a58b0b28c66dcd8b
Anatolii Pokhylko <anatoliipokhylko@Anatoliis-MacBook-Pro.local>
2023-06-14 15:14:40 +0300

    feat: fs files create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
+import {fileURLToPath} from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files', 'fresh.txt');
+    const text = 'I am fresh and young';
+
+    fs.writeFile(
+        filePath,
+        text,
+        (err) => {
+            if (err) {
+                console.error('FS operation failed')
+            };
+        }
+    );
 };
 
 await create();
