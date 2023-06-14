5c6eb9bfa36b20cc04b3ce2ad833bd18942bbbe2
Vlad Tarnovski <vladtarnovski989@gmail.com>
2023-06-15 02:49:58 +0300

    feat: resolve create and copy tasks

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,16 @@
+import { cp } from "fs";
+import path from "path";
+
+const fileUrl = "./src/fs";
+const sourceFolder = path.join(fileUrl, "files");
+const copyFolder = path.join(fileUrl, "files_copy");
+
 const copy = async () => {
-    // Write your code here 
+  cp(sourceFolder, copyFolder, { recursive: true }, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import fs, { stat } from "fs";
+
+const fileUrl = "./src/fs/files/fresh.txt";
+const checkExisting = async () => {
+  stat(fileUrl, function (err) {
+    if (!err) {
+      throw new Error("FS operation failed");
+    }
+  });
+};
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await checkExisting();
+    fs.writeFile(fileUrl, "I am fresh and young", function (err) {
+      if (err) console.log(err);
+      console.log("File created!");
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await create();
