e9a855a54fd34259f01af392a46aadea3f14c6cd
jkenjayev <javohirwebdev@gmail.com>
2023-06-14 14:53:41 +0500

    fs:delete.js is ready

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { unlink } from "fs/promises";
+
 const remove = async () => {
     // Write your code here 
+    const filePath = fileURLToPath(import.meta.url);
+    const dirPath = dirname(filePath) + "/files";
+    const errMsg = 'FS operation failed'
+
+    try {
+        await unlink(dirPath + '/fileToRemove.txt');
+    } catch (err) {
+        throw new Error(errMsg);
+    }
 };
 
 await remove();
