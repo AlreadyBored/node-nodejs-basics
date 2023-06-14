e8279398108357a29cb3261ba44504991a873d98
Alexander Leonov <spawnleon@gmail.com>
2023-06-14 23:03:01 +0400

    feat: implement fs/read task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,22 @@
+import { promises as fs } from 'node:fs';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { isDirOrFileExist } from './../utils/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const sourcePath = join(__dirname, 'files', 'fileToRead.txt');
+
+  const isExist = await isDirOrFileExist(sourcePath);
+  if (!isExist) {
+    throw new Error('FS operation failed');
+  }
+
+  const data = await fs.readFile(sourcePath, { encoding: 'utf-8' });
+  console.log(data);
 };
 
 await read();
