92573711ba5c132b72b21e64d6a679ebf460bd09
ryzhenkov.d <ideal103.5@gmail.com>
2023-06-14 17:33:17 +0400

    calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import crypto  from "node:crypto";
+import fs from 'node:fs'
+import url from "url";
+import path from "path";
+
 const calculateHash = async () => {
-    // Write your code here 
+    const fileFolder = 'files';
+
+    const __filename = url.fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    fs.readFile(path.join(__dirname, fileFolder, 'fileToCalculateHashFor.txt'), null, (err, buffer) => {
+        if (err) {
+            throw new Error(err.message);
+        }
+
+        const hash = crypto.createHash('sha256', buffer.toString());
+        console.log(hash.digest('hex'))
+    })
+
+
 };
 
 await calculateHash();
