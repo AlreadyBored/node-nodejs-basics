e8fd7f858cc20de52dff27a447099c240b8bec27
Anatolii Pokhylko <pokhylko@gmail.com>
2023-06-15 22:00:27 +0300

    feat: streams read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import {createReadStream} from 'fs';
+import {fileURLToPath} from 'url';
+import path from 'path';
+import {pipeline} from 'stream/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    await pipeline(createReadStream(filePath), process.stdout)
 };
 
 await read();
