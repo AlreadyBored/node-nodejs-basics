bbcb75b1b4ec24b4bf5b86aa42a675193eab2d51
Nikita <nikitakemper555@gmail.com>
2023-06-15 23:16:14 +0300

    feat: add create solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import { access, writeFile } from 'fs/promises';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const create = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const pathToFile = path.join(__dirname, 'files', 'fresh.txt');
+    const text = 'I am fresh and young';
+
+    try {
+        await access(pathToFile);
+        // будет выполняться если файл есть
+        throw new Error('FS operation failed');
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            await writeFile(pathToFile, text);
+        } else {
+            throw error;
+        }
+    }
 };
 
 await create();
