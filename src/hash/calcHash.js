b8855f5241016d489df3337d3b6de1252838077e
i.bykhavets <i.bykhavets@softarex.com>
2023-06-15 19:44:06 +0200

    feat: complete hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import { readFile } from 'node:fs/promises';
+import { createHash } from 'node:crypto';
+
+import { isFileOrDirExists } from "../utils/utils.js";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const sourceFile = 'src/hash/files/fileToCalculateHashFor.txt';
+
+  try {
+    const isFileExist = isFileOrDirExists(sourceFile);
+    if (!isFileExist) throw new Error('FS operation failed');
+
+    const content = await readFile(sourceFile, 'utf8');
+    const hexHash = createHash('sha256').update(content).digest('hex');
+    console.log(hexHash);
+
+  } catch (error) {
+    throw error;
+  }
 };
 
 await calculateHash();
