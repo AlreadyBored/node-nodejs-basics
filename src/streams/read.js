1d0bcf052034464cf4734011fc2f16ff850b2e52
jkenjayev <javohirwebdev@gmail.com>
2023-06-15 16:17:16 +0500

    stream tasks finished

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import { createReadStream } from "fs";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
 const read = async () => {
     // Write your code here 
+    const __filename = fileURLToPath(import.meta.url)
+    const __dirname = dirname(__filename);
+    const rs = createReadStream(
+        __dirname + '/files/fileToRead.txt',
+        { encoding: "utf-8" }
+    )
+    rs.pipe(process.stdout);
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import { createWriteStream } from "fs";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
 const write = async () => {
     // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const ws = createWriteStream(__dirname + '/files/fileToWrite.txt', { encoding: 'utf-8' });
+    process.stdin.on('data', (data) => {
+        ws.write(data.toString());
+    })
+
+    console.log(`You can write anything it can be written in file! \n`)
 };
 
 await write();
