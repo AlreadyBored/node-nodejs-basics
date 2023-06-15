0acb0c194c7d2aedc44f739332ea56a0cd6a613f
Anatolii Pokhylko <pokhylko@gmail.com>
2023-06-15 16:19:24 +0300

    feat: fs files list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+import {fileURLToPath} from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const folderPath = path.join(__dirname, 'files');
+
 const list = async () => {
-    // Write your code here 
+    fs.readdir(folderPath,
+        (err, files) => {
+            console.log(files);
+            
+            if (err) {
+                console.error('FS operation failed');
+            }
+        }
+    );
 };
 
 await list();
