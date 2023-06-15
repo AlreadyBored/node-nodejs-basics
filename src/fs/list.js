da9c4beed60d5b098e1c7950fc086d296c35b19c
Denis Karandashev <denis.karandashev@t-systems.com>
2023-06-15 18:55:22 +0300

    feat: list of files names

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+/*
+ implement function that prints all array of filenames from files folder into console 
+ (if files folder doesn't exists Error with message FS operation failed must be thrown)
+*/
+
+import fs from 'fs';
+
 const list = async () => {
-    // Write your code here 
+    const dir = "./src/fs/files";
+
+    fs.readdir(dir, (err, files) => {
+        if (err && err.code === "ENOENT") {
+            throw new Error("FS operation failed");
+        }
+        console.log(files);
+    });
 };
 
 await list();
