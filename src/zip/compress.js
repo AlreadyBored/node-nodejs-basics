499e396655702e495ea5c0f5325fdbcfd6313665
fizarum <fizarum@gmail.com>
2023-06-15 22:33:01 +0200

    feat: implement tasks in 'zip' section

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import { createGzip } from "node:zlib";
+import { createReadStream, createWriteStream } from "node:fs";
+import { pipeline } from "node:stream/promises";
+
 const compress = async () => {
-    // Write your code here 
+  const path = "src/zip/files/";
+  const src = path + "fileToCompress.txt";
+  const dst = path + "archive.gz";
+
+  const gzip = createGzip();
+
+  const srcStream = createReadStream(src);
+  const dstStream = createWriteStream(dst);
+
+  await pipeline(srcStream, gzip, dstStream).catch((error) => {
+    console.error(`error during compressing: ${error}`);
+  });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,26 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import { pipeline } from "node:stream/promises";
+import { rename } from "node:fs/promises";
+import { createUnzip } from "node:zlib";
+
 const decompress = async () => {
-    // Write your code here 
+  const path = "src/zip/files/";
+  const archive = path + "archive.gz";
+  const src = path + "fileToCompress.txt.gz";
+  const dst = path + "fileToCompress.txt";
+
+  const unzip = createUnzip();
+
+  //we have to rename given archive to target file_name + ".gz"
+  //otherwise decompression will return empty content
+  await rename(archive, src);
+
+  const srcStream = createReadStream(src);
+  const dstStream = createWriteStream(dst);
+
+  await pipeline(srcStream, unzip, dstStream).catch((error) => {
+    console.error(`error during uncompressing: ${error}`);
+  });
 };
 
 await decompress();
