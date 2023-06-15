ffbb61492030fddf97f2cf5b5ad3328db497a027
Mar Kal <marc@example.com>
2023-06-15 18:57:55 +0200

    feat: finish subtask 6

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import { readdir, access } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+  const folderPath = join(__dirname, 'files');
+
+  try {
+    await access(folderPath);
+
+    const files = await readdir(folderPath);
+    console.log(files);
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await list();
