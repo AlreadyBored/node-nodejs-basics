39176a868ba20cf27f6015d6c363d77c843de2ee
Anatolii Pokhylko <pokhylko@gmail.com>
2023-06-15 15:37:57 +0300

    feat: fs files read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,25 @@
+import fs from 'fs';
+import {fileURLToPath} from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    fs.readFile(
+        filePath,
+        'utf-8',
+        (err, data) => {
+            if (data) {
+                console.log(data);
+            }
+            
+            if (err) {
+                console.error('FS operation failed');
+            }
+        }
+    );
 };
 
 await read();
