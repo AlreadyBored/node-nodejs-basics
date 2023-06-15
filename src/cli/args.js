2cf3c167d1fafe1832053426826a6944241c9d83
Liza <lizavetayalak2000@gmail.com>
2023-06-15 22:03:17 +0200

    feat: implement args function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,4 +1,20 @@
+import process from 'node:process';
+
 const parseArgs = () => {
+    const args = process.argv.splice(2)
+    const propArr = []
+    const valueArr = []
+    args.forEach((el, i) => {
+        if (i % 2 === 0) {
+            propArr.push(el)
+        } else {
+            valueArr.push(el)
+        }
+    })
+    const newArr = propArr.map((el, i) => {
+      return `${el} is ${valueArr[i]}`
+    })
+    console.log(newArr)
 };
 
 parseArgs();
