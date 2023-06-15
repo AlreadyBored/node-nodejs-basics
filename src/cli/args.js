45fc69484616f10326402f332fc80c93d228143f
Max_Nikitenok <xxx250790@gmail.com>
2023-06-15 21:38:37 +0300

    feat: cli/args solution

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.reduce((acc, value, index, array) => {
+    if (value.startsWith('--')) {
+      const parsedArguments = `${value.slice(2)} is ${array[index + 1]}`;
+      return [...acc, parsedArguments];
+    }
+    return acc;
+  }, []);
+  console.log(args.join(', '));
 };
 
 parseArgs();
