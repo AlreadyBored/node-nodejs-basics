508e713f1434e4defe814e37bafcbf8f1fec6c09
Anatolii Pokhylko <pokhylko@gmail.com>
2023-06-15 17:26:49 +0300

    feat: cli args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv;
+    const result = [];
+
+    for(let i = 0; i < args.length; i += 2) {
+        if (args[i].startsWith('--')) {
+            const key = args[i].slice(2);
+            
+            result.push(`${key} is ${args[i + 1]}`)
+        }
+    }
+
+    console.log(result.join(', '))
 };
 
 parseArgs();
