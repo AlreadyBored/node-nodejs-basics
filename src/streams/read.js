9edd6cb1277002066bc4b8b9152e296b68a01bfb
Alexander Leonov <spawnleon@gmail.com>
2023-06-16 00:22:20 +0400

    feat: implement streams/read task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,39 @@
+import { Readable } from 'node:stream';
+import { promises as fs } from 'node:fs';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { isDirOrFileExist } from './../utils/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+class FileReadStream extends Readable {
+  constructor(fileName, options = {}) {
+    super(options);
+    this.fileName = fileName;
+  }
+
+  async _read(size) {
+    try {
+      const data = await fs.readFile(this.fileName);
+      this.push(data);
+      this.push(null);
+    } catch (error) {
+      this.emit('error', error);
+    }
+  }
+}
+
 const read = async () => {
-    // Write your code here 
+  const sourcePath = join(__dirname, 'files', 'fileToRead.txt');
+
+  if (!await isDirOrFileExist(sourcePath)) {
+    throw new Error('File does not exist');
+  }
+
+  const fileStream = new FileReadStream(sourcePath);
+  fileStream.pipe(process.stdout);
 };
 
 await read();
