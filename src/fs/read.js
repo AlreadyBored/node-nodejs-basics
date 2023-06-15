d3c92c461bfc78335eb5a45119721ecf57808229
aliya <tusupkalievaaliya921@gmail.com>
2023-06-15 23:25:28 +0500

    add read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,14 @@
+//read.js- реализовать функцию, выводящую содержимое fileToRead.txtв консоль (если нет файла fileToRead.txt Errorс сообщением, FS operation failedто нужно кинуть)
+import fs from 'fs'
 const read = async () => {
-    // Write your code here 
-};
+    try {
+        const data = await
+            fs.promises.readFileSync('files/fileToRead.txt', 'utf-8')
+        console.log(data)
+    } catch (err) {
+        console.error('FS operation failed')
+    }
+
+}
 
 await read();
