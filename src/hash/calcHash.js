44d9cf16329aa5e14bd0829ce917b16af112ae07
fizarum <fizarum@gmail.com>
2023-06-15 02:33:31 +0200

    feat: implement task in 'hash' section

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import { readFile } from "node:fs/promises";
+//dynamic import
+const { createHash } = await import("node:crypto");
+
 const calculateHash = async () => {
-    // Write your code here 
+  const src = "src/hash/files/fileToCalculateHashFor.txt";
+  const hash = createHash("sha256");
+  const digestFormat = "hex";
+
+  try {
+    const content = await readFile(src);
+    hash.update(content);
+    console.log(hash.digest(digestFormat));
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 await calculateHash();
