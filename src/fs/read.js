14494601edb7972e4dbbc538cd5105a976e69617
Liza <lizavetayalak2000@gmail.com>
2023-06-15 18:57:11 +0200

    feat: implement read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,21 @@
+import { stat, readFile } from 'node:fs/promises'
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
 const read = async () => {
-    // Write your code here 
+    const directory = path.dirname(fileURLToPath(import.meta.url))
+    const file = path.join(directory, 'files', 'fileToRead.txt')
+    let fileNotExists = false;
+    try {
+        await stat(file)
+    } catch {
+        fileNotExists = true
+    }
+    if (fileNotExists){
+        throw new Error('FS operation failed')
+    }
+    const fileContent = await readFile(file, {encoding: 'utf-8'})
+    console.log(fileContent)
 };
 
 await read();
