93a275d5a82d6d826e8ac2702f339d873daee1b3
Denis Karandashev <denis.karandashev@t-systems.com>
2023-06-15 18:42:35 +0300

    feat: delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,27 @@
+/*
+ implement function that deletes file fileToRemove.txt 
+ (if there's no file fileToRemove.txt Error with message FS operation failed must be thrown)
+*/
+
+import fs from 'fs';
+
+const check = async (path, errMsg) => {
+        fs.access(path, function (err) {
+            if (err) {
+                throw new Error(errMsg);
+            }
+        });
+}
+
 const remove = async () => {
-    // Write your code here 
+    const FILE_TO_REMOVE = "./src/fs/files/fileToRemove.txt";
+    const ERR_MSG = "FS operation failed";
+
+    check(FILE_TO_REMOVE, ERR_MSG);
+
+    fs.unlink(FILE_TO_REMOVE, function (err) {
+        console.log(err);
+    })
 };
 
 await remove();
