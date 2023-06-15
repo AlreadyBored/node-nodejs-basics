76308972899c6f45e0da2c3cb54f855aed30bf32
Klychnikov Alexandr <klychnikov75@mail.ru>
2023-06-15 15:46:26 +0300

    feat: implement streams:read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import fs from 'fs';
+import path from 'path';
+import url from 'url'
+
 const read = async () => {
-    // Write your code here 
+    const __filename = url.fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const file = path.join(__dirname, 'files', 'fileToRead.txt');
+    let readableStream = fs.createReadStream(file, 'utf8');
+    readableStream.on('data', function (chunk) {
+      process.stdout.write(chunk);
+    });
 };
 
 await read();
