2fd26035a9960643c020aa16b773c04d045cc04e
ryzhenkov.d <ideal103.5@gmail.com>
2023-06-15 21:11:34 +0400

    transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
+import {stdout, stdin} from 'node:process';
+import {Transform} from "node:stream"
+
 const transform = async () => {
-    // Write your code here 
+    const stream = new Transform({
+        transform(chunk, encoding, callback) {
+            callback(null, chunk.toString().split('').reverse().join(''));
+        }}
+    );
+
+    stdin.pipe(stream).pipe(stdout)
 };
 
 await transform();
