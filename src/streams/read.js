c33ef64bf3313026f042d110b0adfe63a8c79b0c
ryzhenkov.d <ideal103.5@gmail.com>
2023-06-15 19:32:52 +0400

    calcHash

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import url from "node:url";
+import path from "node:path";
+import fs from "node:fs"
+import {stdout} from 'node:process'
+
 const read = async () => {
-    // Write your code here 
+    const fileFolder = 'files';
+
+    const __filename = url.fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const stream = fs.createReadStream(path.join(__dirname, fileFolder, 'fileToRead.txt'));
+
+    stream.pipe(stdout);
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import {stdin} from 'node:process';
+import url from "node:url";
+import fs from "node:fs"
+import path from "node:path";
+
 const write = async () => {
-    // Write your code here 
+    const fileFolder = 'files';
+
+    const __filename = url.fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const stream = fs.createWriteStream(path.join(__dirname, fileFolder, 'fileToWrite.txt'));
+
+    stdin.pipe(stream);
 };
 
 await write();
