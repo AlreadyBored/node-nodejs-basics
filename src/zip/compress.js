0c01e8c2344c0d28a17b1f3ff88b807963e32b8e
banzayche <dharm.serg@gmail.com>
2023-06-15 19:10:49 +0300

    Finishing with zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,16 @@
+import { createGzip } from 'node:zlib';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
+
 const compress = async () => {
-    // Write your code here 
+    // Write your code here
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    createReadStream(`${__dirname}/files/fileToCompress.txt`)
+      .pipe(createGzip())
+      .pipe(createWriteStream(`${__dirname}/files/archive.gz`));
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,16 @@
+import { createUnzip } from 'node:zlib';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
+
 const decompress = async () => {
-    // Write your code here 
+    // Write your code here
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  createReadStream(`${__dirname}/files/archive.gz`)
+    .pipe(createUnzip())
+    .pipe(createWriteStream(`${__dirname}/files/fileToCompress.txt`));
 };
 
 await decompress();
