05ee6cc3ce6894d93dbd104efa16bd7d20dc3a2c
besnadne <serpov.box@gmail.com>
2023-06-15 13:36:50 +0300

    feat: implement function that deletes file fileToRemove.txt

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,24 @@
+import { promises as fsPromises } from "fs";
+import { join, dirname } from "path";
+
 const remove = async () => {
-    // Write your code here 
+  const currentDir = dirname(new URL(import.meta.url).pathname).replace(
+    /^\/([A-Za-z]:)/,
+    "$1"
+  );
+  const filePath = join(currentDir, "files", "fileToRemove.txt");
+  try {
+    await fsPromises.access(filePath);
+
+    await fsPromises.unlink(filePath);
+    console.log("File deleted successfully!");
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    }
+    console.error("Error:", error.message);
+  }
+  // Write your code here
 };
 
 await remove();
