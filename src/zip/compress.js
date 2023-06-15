8e54962a9515925ffcb6597e70a94eb15ace215d
ryzhenkov.d <ideal103.5@gmail.com>
2023-06-15 22:04:44 +0400

    zlib

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import zlib from "zlib";
+import fs from "node:fs";
+import url from "node:url";
+import path from "node:path";
+
 const compress = async () => {
-    // Write your code here 
+    const fileFolder = 'files';
+    const __filename = url.fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const zip = zlib.createGzip();
+    const streamR = fs.createReadStream(path.join(__dirname, fileFolder, 'fileToCompress.txt'));
+    const streamW = fs.createWriteStream(path.join(__dirname, fileFolder, 'archive.gz'));
+    streamR.pipe(zip).pipe(streamW);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,17 @@
+import url from "node:url";
+import path from "node:path";
+import zlib from "zlib";
+import fs from "node:fs";
+
 const decompress = async () => {
-    // Write your code here 
+    const fileFolder = 'files';
+    const __filename = url.fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const zip = zlib.createUnzip();
+    const streamR = fs.createReadStream(path.join(__dirname, fileFolder, 'archive.gz'));
+    const streamW = fs.createWriteStream(path.join(__dirname, fileFolder, 'fileToCompress.txt'));
+    streamR.pipe(zip).pipe(streamW);
 };
 
 await decompress();
