104ec9cada0a3b837f172d07b8fc0efa7a3a216b
KirillGenin <332211op@gmail.com>
2023-06-15 10:37:52 +0300

    feat: implement function cp/cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,14 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { fork } from 'node:child_process';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const forkProcess = fork(path.resolve(__dirname, 'files', 'script.js'), args, { silent: true });
+
+    process.stdin.pipe(forkProcess.stdin);
+    forkProcess.stdout.pipe(process.stdout);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['Hi', 'Bob']);
