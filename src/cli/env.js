b53d397e61d43e85f9f072a982660dbbfaaf2471
Mar Kal <marc@example.com>
2023-06-15 19:36:46 +0200

    feat: finish subtask 8

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+  const env = Object.entries(process.env);
+
+  const filteredEnv = env.filter((obj) => obj[0].startsWith('RSS_'));
+
+  const result = filteredEnv.map(([key, value]) => `${key}=${value}`);
+  console.log('Result:', result.join('; '));
 };
 
 parseEnv();
