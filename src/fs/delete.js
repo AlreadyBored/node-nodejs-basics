b818614c0ebfa305dba50944ae151aa58a36d99e
aliya <tusupkalievaaliya921@gmail.com>
2023-06-15 13:25:24 +0500

    add delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,12 @@
+//delete.js- реализовать функцию, которая удаляет файл fileToRemove.txt(если нет файла fileToRemove.txt Errorс сообщением, FS operation failed нужно выкинуть)
+import fs from 'fs'
 const remove = async () => {
-    // Write your code here 
+    try {
+        await fs.promises.unlink('files/fileToRemove.txt')
+        console.log('file is removed')
+    } catch (err) {
+        console.error('FS operation failed')
+    }
 };
 
 await remove();
