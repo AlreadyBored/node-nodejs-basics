5d448e2e0603e660ef59f21b69ca69c130015497
Mar Kal <marc@example.com>
2023-06-15 18:46:04 +0200

    feat: finish subtask 3

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,26 @@
+import { rename as renameFile, access } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const rename = async () => {
-    // Write your code here 
+  const fileToRenamePath = join(__dirname, 'files', 'wrongFilename.txt');
+  const renamedFilePath = join(__dirname, 'files', 'properFilename.md');
+
+  try {
+    await access(fileToRenamePath);
+
+    try {
+      await access(renamedFilePath);
+      throw new Error('FS operation failed');
+    } catch (err) {
+      await renameFile(fileToRenamePath, renamedFilePath);
+    }
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await rename();
