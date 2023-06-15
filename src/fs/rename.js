2559fdae8f7d553b54e73bf68616c32aba62c786
Anatolii Pokhylko <pokhylko@gmail.com>
2023-06-15 15:44:24 +0300

    feat: fs files rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,22 @@
+import fs from 'fs';
+import {fileURLToPath} from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const wrongFilename = path.join(__dirname, 'files', 'wrongFilename.txt');
+const properFilename = path.join(__dirname, 'files', 'properFilename.md');
+
 const rename = async () => {
-    // Write your code here 
+    fs.rename(
+        wrongFilename,
+        properFilename,
+        (err) => {
+            if (err) {
+                console.error('FS operation failed');
+            };
+        }
+    );
 };
 
 await rename();
