453a0eae43c900684570a370afbca3b024643094
Klychnikov Alexandr <klychnikov75@mail.ru>
2023-06-15 21:57:48 +0300

    feat: implement streams:write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,20 @@
+import fs from 'fs';
+import path from 'path';
+import url from 'url';
+const { stdin, stdout } = process;
+
 const write = async () => {
-    // Write your code here 
+    const __filename = url.fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const file = path.join(__dirname, 'files', 'fileToWrite.txt');
+    process.on('SIGINT', () => process.exit());
+    let writeableStream = fs.createWriteStream(file, {flags: 'a'});
+    stdin.on('data', function(chunk) {
+        writeableStream.write(chunk);
+    });
+    process.on('exit', () => {
+        writeableStream.end();
+    });
 };
 
 await write();
