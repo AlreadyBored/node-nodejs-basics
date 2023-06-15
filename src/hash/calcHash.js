78a816c675c6279b22dd19b993916d0351a6b2ac
Anatolii Pokhylko <pokhylko@gmail.com>
2023-06-15 20:33:04 +0300

    feat: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import {fileURLToPath} from 'url';
+import path from 'path';
+import { createHash } from 'crypto';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+        if (err) {
+            console.error(err)
+        }
+
+        if (data) {
+            const hash = createHash('sha256').update(data).digest('hex')
+
+            console.log(hash);
+        }
+    });
 };
 
 await calculateHash();
