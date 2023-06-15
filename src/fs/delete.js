dd6c6763d78a7ab691b71d3010c188f06d33908a
Anatolii Pokhylko <pokhylko@gmail.com>
2023-06-15 16:15:07 +0300

    feat: fs files delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+import fs from 'fs';
+import {fileURLToPath} from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'fileToRemove.txt');
+
 const remove = async () => {
-    // Write your code here 
+    fs.unlink(filePath,
+        (err) => {
+            if (err) {
+                console.error('FS operation failed');
+            };
+        }
+    )
 };
 
 await remove();
