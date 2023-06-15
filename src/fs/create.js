44b906fc91ed892b9b476f7dcf412934b9ad8c37
Liza <lizavetayalak2000@gmail.com>
2023-06-15 12:57:33 +0200

    feat: implement create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import fs from 'node:fs/promises'
+import path from 'node:path'
+import { fileURLToPath } from 'url';
+
 const create = async () => {
-    // Write your code here 
+    const directory = path.dirname(fileURLToPath(import.meta.url))
+    const file = path.join(directory, 'files', 'fresh.txt');
+    let fileExists = true;
+    try {
+        await fs.stat(file)
+    } catch {
+        fileExists = false;
+    }
+
+    if (fileExists) {
+        throw new Error('FS operation failed')
+    }
+    await fs.writeFile(file, 'I am fresh and young')
 };
 
 await create();
