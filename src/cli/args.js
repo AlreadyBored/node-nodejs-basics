0d6c67cf48a36c55261c7e5b11c1190454318b9f
i.bykhavets <i.bykhavets@softarex.com>
2023-06-15 17:59:00 +0200

    feat: complete cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
 const parseArgs = () => {
-    // Write your code here 
+  try {
+    const args = process.argv.slice(2);
+    const params = args.reduce((acc, curr, currIndex, array) => {
+      if (curr.startsWith('--') && array[currIndex + 1] && !array[currIndex + 1].startsWith('--')) {
+        acc[curr.slice(2)] = array[currIndex + 1];
+      }
+      return acc;
+    }, {})
+
+    console.log(Object.entries(params).map(([name, value]) => `${name} is ${value}`).join(', '));
+  } catch (error) {
+    throw error;
+  }
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,16 @@
 const parseEnv = () => {
-    // Write your code here 
+  try {
+    const envVariables = process.env;
+    const rssVariables = Object.entries(envVariables)
+      .filter(([name, value]) => name.startsWith('RSS_'));
+
+    for (const [name, value] of rssVariables) {
+      process.stdout.write(`${name}=${value}; `);
+    }
+    console.log('');
+  } catch (error) {
+    throw error;
+  }
 };
 
 parseEnv();
