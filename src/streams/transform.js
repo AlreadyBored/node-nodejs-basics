4d43e422a82320ce4a4e71dc32a396d12ad625c5
Max_Nikitenok <xxx250790@gmail.com>
2023-06-17 00:09:35 +0300

    feat: streams/transform solution

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+import { Transform } from 'node:stream';
+
+const reversedString = (str) => [...str].reverse().join('');
+
 const transform = async () => {
-    // Write your code here 
+  console.log(
+    'Write your data\nPress "Enter" for safe\nPress "Ctrl+C" for exit'
+  );
+  const transformStream = new Transform({
+    transform(chunk, _encoding, callback) {
+      callback(null, reversedString(chunk.toString()));
+    },
+  });
+  process.stdin.pipe(transformStream).pipe(process.stdout);
 };
 
 await transform();
