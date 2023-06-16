14effc541a5dd3f5e6fbcf3874670fb86451a390
Denis Karandashev <denis.karandashev@t-systems.com>
2023-06-16 13:21:28 +0300

    feat: cli - get environment variables

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,20 @@
+/*
+implement function that parses environment variables with prefix RSS_ 
+and prints them to the console in the format RSS_name1=value1; RSS_name2=value2
+
+TEST:
+1) cd src/cli
+2) RSS_EEE=33 dddd=33333 RSS_RRRR=2324 node env.js
+Output: RSS_EEE=33; RSS_RRRR=2324
+*/
 const parseEnv = () => {
-    // Write your code here 
+    const res = [];
+    for (const v in process.env) {
+        if (v.startsWith("RSS_")) {
+            res.push(`${v}=${process.env[v]}`);
+        };
+    }
+    console.log(res.join("; "));
 };
 
 parseEnv();
