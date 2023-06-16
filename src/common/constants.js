d9ceff2592073b0007f3f3c65de9b21b00175806
Denis Karandashev <denis.karandashev@t-systems.com>
2023-06-16 18:36:40 +0300

    feat: hash

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,7 +1,12 @@
+
+// fs
 export const FILE_NAME = "fresh.txt";
 export const FILE_CONTENT = "I am fresh and young";
 export const PATH = `./src/fs/files/${FILE_NAME}`;
 export const ERR_MSG = "FS operation failed";
+
+// hash
+export const FILE_PATH_HASH = "./files/fileToCalculateHashFor.txt";
 
 // errors
 export const ERR_FILE_EXISTS = "EEXIST";
--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+/*
+ You should implement several functions in dedicated files
+
+calcHash.js - implement function that calculates SHA256 hash 
+for file fileToCalculateHashFor.txt and logs it into console as hex
+ */
+
+import { createHash } from 'crypto';
+import { readFile } from 'fs/promises';
+import { FILE_PATH_HASH } from "../common/constants.js";
+
 const calculateHash = async () => {
-    // Write your code here 
+    
+    const fileUrl = new URL(FILE_PATH_HASH, import.meta.url);
+  
+    const content = await readFile(fileUrl);
+    const data = createHash("sha256").update(content);
+    const hash = data.digest("hex");
+  
+    console.log(hash);
 };
 
 await calculateHash();
