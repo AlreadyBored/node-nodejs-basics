7e94a1062aed84451a0812efa63c15030cdc791f
Hanna Huzava <guzovaann@gmail.com>
2023-06-16 18:06:43 +0200

    feat: add calculateHash()

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
-const calculateHash = async () => {
-    // Write your code here 
+import { readFile } from 'node:fs/promises';
+import { createHash } from 'node:crypto';
+
+export const calculateHash = async () => {
+
+    try {
+        const contentOfFile = await readFile('./src/hash/files/fileToCalculateHashFor.txt');
+        const hash = createHash('sha256');
+        hash.update(contentOfFile);
+        const digest = hash.digest('hex');
+
+       console.log('Hash - ' + digest);
+
+    } catch (error) {
+        throw error;
+    }
 };
 
-await calculateHash();
+calculateHash();
