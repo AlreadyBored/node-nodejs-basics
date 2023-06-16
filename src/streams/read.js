aa798306a4d76df4379cc4d4d305747ca52b90c5
Max_Nikitenok <xxx250790@gmail.com>
2023-06-16 23:50:52 +0300

    feat: streams/write solution

--- src/streams/read.js
+++ src/streams/read.js
@@ -3,7 +3,7 @@ import { createReadStream } from 'node:fs';
 const url = new URL('./files/fileToRead.txt', import.meta.url);
 
 const read = async () => {
-    createReadStream(url).pipe(process.stdout); 
+  createReadStream(url).pipe(process.stdout);
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import { createWriteStream } from 'node:fs';
+
+const url = new URL('./files/fileToWrite.txt', import.meta.url);
+
 const write = async () => {
-    // Write your code here 
+  console.log(
+    'Write your data\nPress "Enter" for safe\nPress "Ctrl+C" for exit'
+  );
+  const writeStream = createWriteStream(url);
+  process.stdin.pipe(writeStream);
 };
 
 await write();
