7b5032f5dfc627dcdb2e49ed9b948c025c550870
Max_Nikitenok <xxx250790@gmail.com>
2023-06-16 23:36:52 +0300

    feat: streams/read solution

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,9 @@
+import { createReadStream } from 'node:fs';
+
+const url = new URL('./files/fileToRead.txt', import.meta.url);
+
 const read = async () => {
-    // Write your code here 
+    createReadStream(url).pipe(process.stdout); 
 };
 
 await read();
