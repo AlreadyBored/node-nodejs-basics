2b3bcb692f9df264c7e17935b46ffeefc32016fc
Mar Kal <marc@example.com>
2023-06-16 10:01:33 +0200

    finish subtask 10 - hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import { readFile } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { createHash } from 'crypto';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const fileToHash = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+  try {
+    const fileContents = await readFile(fileToHash);
+    const hash = createHash('sha256');
+    const hashedFile = hash.update(fileContents).digest('hex');
+    console.log(hashedFile);
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await calculateHash();
