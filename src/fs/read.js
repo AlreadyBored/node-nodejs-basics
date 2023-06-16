189bb38f3d40624ee56a26a7744811a82915cb57
besnadne <serpov.box@gmail.com>
2023-06-16 12:49:57 +0300

    feat:  implement function that prints content of the fileToRead.txt into console

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,24 @@
+import { promises as fsPromises } from "fs";
+import { join } from "path";
+import { currentDir } from "./findCurrentDir.js";
+
 const read = async () => {
-    // Write your code here 
+  const filePath = join(currentDir, "files", "fileToRead.txt");
+
+  try {
+    try {
+      await fsPromises.access(filePath);
+      const text = await fsPromises.readFile(filePath, { encoding: "utf8" });
+      console.log(text);
+    } catch (error) {
+      if (error.code === "ENOENT") {
+        throw new Error("Fs operation failed");
+      }
+    }
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+  // Write your code here
 };
 
 await read();
