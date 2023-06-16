bf33c2f70b1192a20fc501a7a74e5ada72a25912
Klychnikov Alexandr <klychnikov75@mail.ru>
2023-06-16 21:33:22 +0300

    feat: implement zip:compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,25 @@
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream';
+import fs from 'fs';
+import path from 'path';
+import url from 'url'
+
+
 const compress = async () => {
-    // Write your code here 
+    const __filename = url.fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const srcFile = path.join(__dirname, 'files', 'fileToCompress.txt');
+    const destFile = path.join(__dirname, 'files', 'archive.gz')
+    const src = fs.createReadStream(srcFile);
+    const dest = fs.createWriteStream(destFile);
+    const gzip = createGzip();
+
+    pipeline(src, gzip, dest, (err) => {
+        if (err) {
+            console.error('Произошла ошибка:', err);
+            process.exit(1);
+        }
+    });
 };
 
 await compress();
