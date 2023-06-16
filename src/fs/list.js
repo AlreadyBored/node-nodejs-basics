59b3698e88a3fc75369015f9d84808d9bb904bbd
besnadne <serpov.box@gmail.com>
2023-06-16 12:33:07 +0300

    feat:  implement function that prints all array of filenames from files

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,25 @@
+import { promises as fsPromises } from "fs";
+import { join } from "path";
+import { currentDir } from "./findCurrentDir.js";
+
 const list = async () => {
-    // Write your code here 
+  const filesPath = join(currentDir, "files");
+  try {
+    try {
+      await fsPromises.access(filesPath);
+    } catch (error) {
+      if (error.code === "ENOENT") {
+        throw new Error("Fs operation failed");
+      }
+    }
+    const files = await fsPromises.readdir(filesPath);
+    for (const file of files) {
+      console.log(file);
+    }
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+  // Write your code here
 };
 
 await list();
