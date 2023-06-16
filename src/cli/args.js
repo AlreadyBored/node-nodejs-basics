f9c11cc5346174c2bd38d5ab8b15ccfa8e664a24
Alexandra Ilyashenko <sasha.ilyashenko@onde.app>
2023-06-16 21:22:17 +0200

    feat: cli (all tasks)

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const list = [];
+
+  for (let i = 0; i<args.length; i+=2) {
+    if (args[i]) {
+      list.push(`${args[i].replace(/^--/i, "")} is ${args[i+1]}`);
+    }
+  }
+
+  console.log(list.join(", "));
 };
 
 parseArgs();
