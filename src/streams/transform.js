298e24cb85df766885eea607069b1d64ceace30c
Klychnikov Alexandr <klychnikov75@mail.ru>
2023-06-16 19:17:24 +0300

    feat: implement streams:transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
+import { Transform } from 'stream';
+const { stdin, stdout } = process;
+
 const transform = async () => {
-    // Write your code here 
+    const reverse = new Transform({
+      transform(chunk, encoding, callback) {
+        this.push(chunk.toString().split('').reverse().join('').trim() + '\n');
+        callback();
+      }
+    });
+    stdin.pipe(reverse).pipe(stdout);
 };
 
 await transform();
