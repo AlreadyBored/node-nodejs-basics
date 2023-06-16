5415730a6e6cb12d23bc2ec93554bbacac928f93
oscarishe <a.polityko@mail.ru>
2023-06-16 17:00:46 +0300

    feat: implement calcHash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import fs from "fs/promises";
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import { createHash } from 'crypto';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const file = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+
 const calculateHash = async () => {
-    // Write your code here 
+    fs.readFile(file, { encoding: 'utf8' }).then((data) => {
+        console.log(createHash('sha256').update(data).digest('hex'));
+    }).catch(() => {
+        throw new Error('FS operation failed');
+    });
 };
 
 await calculateHash();
