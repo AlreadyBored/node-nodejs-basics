b0aa3664ff6bebdd10801094915bf239ac35ace2
4ebula <4ebula@gmail.com>
2023-06-17 00:18:27 +0400

    implement worker task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,23 @@
+import { Worker } from 'node:worker_threads';
+import os from 'node:os';
+import { resolve } from 'node:path';
+import { getPath } from '../fs/utils.js';
+
 const performCalculations = async () => {
-    // Write your code here
+    const __dirname = getPath(import.meta.url);
+    const filePath = resolve(__dirname, 'worker.js');
+
+    const workerArr = Array
+        .from({ length: os.cpus().length }, (_, i) => i + 10)
+        .map(async(num) => {
+            const worker = new Worker(filePath, { workerData: { num }});
+            return new Promise(resolve => {
+                worker.on('message', res => resolve({ status: 'resolved', data: res }));
+                worker.on('error', () => resolve({ status: 'error', data: null }));
+            });
+        });
+
+    console.log(await Promise.all(workerArr));
 };
 
 await performCalculations();
