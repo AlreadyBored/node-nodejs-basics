595064c67d18a8ce45befbbfe5ee4c9ca7039c1d
Liza <lizavetayalak2000@gmail.com>
2023-06-16 13:45:58 +0200

    feat: implement hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,12 @@
+import path from 'node:path'
+import { createHash } from 'node:crypto'
+import { fileURLToPath } from 'url';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const directory = path.dirname(fileURLToPath(import.meta.url))
+    const file = path.join(directory, 'files', 'fileToCalculateHashFor.txt')
+    const hash = createHash('sha256').update(file).digest('hex')
+    console.log(hash)
 };
 
 await calculateHash();
