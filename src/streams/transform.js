7e8701425422f16ec0673c31ceb0bae7c6c0a66b
Liza <lizavetayalak2000@gmail.com>
2023-06-16 21:43:47 +0200

    feat: implement transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,13 @@
+import { Transform } from 'node:stream'
+import { pipeline } from 'node:stream/promises'
+
 const transform = async () => {
-    // Write your code here 
+    const reversed = new Transform({
+        transform(chunk, encoding, callback) {
+            callback(null, chunk.reverse())
+        }
+    })
+    await pipeline(process.stdin, reversed, process.stdout)
 };
 
 await transform();
