dcad9638e507c2fbb85bd36ba118a602ce8e33ec
Liza <lizavetayalak2000@gmail.com>
2023-06-16 13:14:43 +0200

    feat: implement modules function

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,16 +1,24 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path from 'node:path'
+import { release, version } from 'node:os'
+import { createServer as createServerHttp } from 'node:http'
+import './files/c.js'
+import * as url from 'url';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 const random = Math.random();
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+    unknownObject = await import('./files/a.json', {
+        assert: { type: 'json' }
+    });
 } else {
-    unknownObject = require('./files/b.json');
+    unknownObject = await import('./files/b.json', {
+        assert: { type: 'json' }
+    })
 }
 
 console.log(`Release ${release()}`);
@@ -33,7 +41,7 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
+export {
     unknownObject,
     myServer,
 };
