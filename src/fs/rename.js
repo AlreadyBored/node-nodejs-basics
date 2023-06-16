14ea0bb9be217e1275797cc619c8d627c4b7c1fb
Hanna Huzava <guzovaann@gmail.com>
2023-06-16 14:40:20 +0200

    feat: add rename()

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,14 @@
-const rename = async () => {
-    // Write your code here 
+import { rename as renameFile} from 'node:fs/promises';
+
+export const rename = async () => {
+
+    const errorMessage = 'FS operation failed';
+
+    try {
+        await renameFile('./src/fs/files/wrongFilename.txt', './src/fs/files/properFilename.md');
+    } catch (error) {
+        throw new Error(errorMessage);
+    }
 };
 
-await rename();
+rename();
