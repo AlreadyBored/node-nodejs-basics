6c1e10027d8fb8e1a08295b5bb1e7e20d25765cc
Klychnikov Alexandr <klychnikov75@mail.ru>
2023-06-16 22:42:30 +0300

    feat: implement zip:decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import { createGunzip } from 'zlib';
+import { pipeline } from 'stream';
+import fs from 'fs';
+import path from 'path';
+import url from 'url'
+
 const decompress = async () => {
-    // Write your code here 
+    const __filename = url.fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const destFile = path.join(__dirname, 'files', 'fileToCompress.txt');
+    const srcFile = path.join(__dirname, 'files', 'archive.gz')
+    const src = fs.createReadStream(srcFile);
+    const dest = fs.createWriteStream(destFile);
+    const gunzip = createGunzip();
+
+    pipeline(src, gunzip, dest, (err) => {
+        if (err) {
+            console.error('Произошла ошибка:', err);
+            process.exit(1);
+        }
+    });
 };
 
 await decompress();
