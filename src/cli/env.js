dac54d38efd94a1de8260660e819aec5e05b458f
Hanna Huzava <guzovaann@gmail.com>
2023-06-16 16:56:16 +0200

    feat: add parseEnv()

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,16 @@
-const parseEnv = () => {
-    // Write your code here 
+export const parseEnv = () => {
+    const prefix = 'RSS_';
+    const arrRSS = [];
+
+    for (const [key, value] of Object.entries(process.env)) {
+
+        if (key.startsWith(prefix)) {
+            const name = key.substring(prefix.length);
+            arrRSS.push(`RSS_${name}=${value}`);
+        }
+    }
+
+    console.log(arrRSS.join("; "));
 };
 
 parseEnv();
