3a382373c831483f40061dc95fe2594e5e3711f3
Nikita <nikitakemper555@gmail.com>
2023-06-17 00:12:45 +0300

    feat: add hash solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import { readFile } from 'fs/promises';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url'; 
+import { createHash } from 'crypto';
+import { stdout } from 'process';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const pathToFile = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+    try {
+        const content = await readFile(pathToFile, 'utf-8');
+        const secret = 'abcdefg';
+        const hash = createHash('sha256')
+            .update(content)
+            .digest('hex');
+        stdout.write(hash);
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await calculateHash();
