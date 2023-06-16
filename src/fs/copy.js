15ec1ca2ba4da513ed61991c05b909d2d0d368de
Hanna Huzava <guzovaann@gmail.com>
2023-06-16 14:09:46 +0200

    feat: add copy()

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,18 @@
-const copy = async () => {
-    // Write your code here 
+import { mkdir, readdir, copyFile } from 'node:fs/promises';
+
+export const copy = async () => {
+   
+    const errorMessage = 'FS operation failed';
+
+    try {
+        await mkdir('./src/fs/files_copy');
+        const filesFolder = await readdir('./src/fs/files');
+
+        await Promise.all(filesFolder.map(el => copyFile(`./src/fs/files/${el}`, `./src/fs/files_copy/${el}`)));
+    
+    } catch (error) {
+        throw new Error(errorMessage);
+    }
 };
 
-await copy();
+copy();
