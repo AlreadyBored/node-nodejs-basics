5e00133784a1d881feca06908016286b323cd168
Hanna Huzava <guzovaann@gmail.com>
2023-06-16 17:44:01 +0200

    feat: add parseArgs()

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-    // Write your code here 
+    const prefix = '--';
+    const arr = [];
+
+    for (let i = 2; i < process.argv.length; i++) {
+        const value = process.argv[i + 1];
+
+        if (process.argv[i].startsWith(prefix) && value) {
+            const name = process.argv[i].substring(prefix.length);
+
+            arr.push(`${name} is ${value}`);
+        }
+    }
+
+    console.log(arr.join(', '))
 };
 
 parseArgs();
