7efa50ea30200e425fcb77ac506200218a8e6d55
Mar Kal <marc@example.com>
2023-06-16 10:17:00 +0200

    feat: finish subtask 11 - streams read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import { createReadStream } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, 'files', 'fileToRead.txt');
+
+  const stream = createReadStream(filePath);
+
+  stream.on('data', (chunk) => {
+    process.stdout.write(chunk);
+  });
 };
 
 await read();
