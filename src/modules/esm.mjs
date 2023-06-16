2b61b130c5210dc5fd55e661f7fc843db1510206
UTYFjs <utyf-90@mail.ru>
2023-06-16 17:43:07 +0300

    feat: add resolve modules

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,16 +1,24 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path from 'path'
+import { release, version } from 'os';
+import {createServer as createServerHttp} from'http';
+import './files/c.js'
+import a from './files/a.json' assert {type: 'json'}
+import b from './files/b.json'assert {type: 'json'}
+import url from 'url'
+
+
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const random = Math.random();
 
-let unknownObject;
+export let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+    unknownObject = a;
 } else {
-    unknownObject = require('./files/b.json');
+    unknownObject = b;
 }
 
 console.log(`Release ${release()}`);
@@ -20,7 +28,7 @@ console.log(`Path segment separator is "${path.sep}"`);
 console.log(`Path to current file is ${__filename}`);
 console.log(`Path to current directory is ${__dirname}`);
 
-const myServer = createServerHttp((_, res) => {
+export const myServer = createServerHttp((_, res) => {
     res.end('Request accepted');
 });
 
@@ -33,8 +41,5 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
-    unknownObject,
-    myServer,
-};
+
 
