a4932d3da6a38304f306726c1c538c721f701265
Alexander Leonov <spawnleon@gmail.com>
2023-06-16 20:01:08 +0400

    feat: implement streams/write task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,34 @@
+import { Writable } from 'node:stream';
+import { promises as fs } from 'node:fs';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+class FileWriteStream extends Writable {
+  constructor(fileName, options = {}) {
+    super(options);
+    this.fileName = fileName;
+  }
+
+  async _write(chunk, encoding, callback) {
+    try {
+      await fs.appendFile(this.fileName, chunk);
+      callback();
+    } catch (error) {
+
+      callback(error);
+    }
+  }
+}
+
 const write = async () => {
-    // Write your code here 
+  const destinationPath = join(__dirname, 'files', 'fileToWrite.txt');
+
+  const fileStream = new FileWriteStream(destinationPath);
+  process.stdin.pipe(fileStream);
+
 };
 
 await write();
