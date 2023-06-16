317b03c2f1bda01224ead45e4096b9275f35c03b
Hanna Huzava <guzovaann@gmail.com>
2023-06-16 15:18:53 +0200

    feat: add list()

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,15 @@
+import { readdir } from 'node:fs/promises';
+
 const list = async () => {
-    // Write your code here 
+    const errorMessage = 'FS operation failed';
+
+    try {
+        const allFiles = await readdir('./src/fs/files');
+        console.log(allFiles)
+
+    } catch (error) {
+        throw new Error(errorMessage);
+    }
 };
 
 await list();
