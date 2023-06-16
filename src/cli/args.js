59936e638282a4c9cb56cb5400cc4214f9e1be13
oscarishe <a.polityko@mail.ru>
2023-06-16 15:54:05 +0300

    feat: implement cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,9 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    args.forEach((item, index) => {
+        if (item.includes('--') && args[index + 1])
+            console.log(`${item.slice(2)} is ${args[index + 1]}`);
+    })
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,8 @@
 const parseEnv = () => {
-    // Write your code here 
+    const rsVars = Object.keys(process.env).filter(item => item.includes('RSS_'));
+    rsVars.forEach(item => {
+        console.log(`${item}=${process.env[item]}`)
+    })
 };
 
 parseEnv();
