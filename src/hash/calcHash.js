8995350a6a2ac7d448e8f08c8e1920f35b2207d0
UTYFjs <utyf-90@mail.ru>
2023-06-17 11:09:17 +0300

    feat: add resolve hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,29 @@
+import fsProm from 'fs/promises';
+import path, { dirname } from 'path';
+import url from 'url';
+import crypto from 'crypto';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+
+    const fileForHash = path.join(
+      __dirname,
+      'files',
+      'fileToCalculateHashFor.txt'
+    );
+
+    try{
+        const content = await fsProm.readFile(fileForHash, {
+          encoding: 'utf8',
+        });
+        const hash = crypto.createHash('sha256').update(content).digest('hex');
+        console.log(hash);
+    }catch{
+        throw new Error('no hash file')
+    }
+
 };
 
 await calculateHash();
