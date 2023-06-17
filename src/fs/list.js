6ab917763510bbafee5953b1d867ad8bed8f9c5e
Vitaly Cheastyakov <vchistiakov@datamola.com>
2023-06-17 18:02:05 +0300

    add list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,14 @@
+const fs = require('fs');
+const currentFolder = './src/fs/files';
+
 const list = async () => {
-    // Write your code here 
+  if (!fs.existsSync(currentFolder)) {
+    console.log(new Error('FS operation failed'))
+  } else {
+    fs.readdirSync(currentFolder).forEach(file => {
+      console.log(file);
+    });
+  }
 };
 
-await list();
+list();
