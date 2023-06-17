1651cdd8244857a1523c59baae63ea9fc0cd9245
beka.dvali <beka.dvali.2@iliauni.edu.ge>
2023-06-17 20:07:12 +0400

    delete, list, read, and rename fs tasks done

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
+import fs from "fs";
+
 const list = async () => {
-    // Write your code here 
+    // Write your code here
+    const folderPath = './files'
+
+    // Check if the files folder exists otherwise throw an error
+    if (!fs.existsSync(folderPath)) {
+        throw new Error('FS operation failed');
+    }
+
+    // Get files directory filenames in array and log it
+    console.log(await fs.promises.readdir(folderPath));
 };
 
 await list();
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,14 @@
+import fs from "fs";
+
 const read = async () => {
-    // Write your code here 
+    // Write your code here
+    const readFilePath = './files/fileToRead.txt'
+
+    if (!fs.existsSync(readFilePath)) {
+        throw new Error('FS operation failed');
+    }
+
+    console.log(fs.readFileSync(readFilePath, {encoding: 'utf-8'}))
 };
 
 await read();
