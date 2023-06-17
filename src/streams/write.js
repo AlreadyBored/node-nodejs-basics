4f6756d3a8838980e0c271d5ceac6636ca78a62a
Anatolii Pokhylko <pokhylko@gmail.com>
2023-06-17 23:09:27 +0300

    feat: streams write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,27 @@
+import {createWriteStream} from 'fs';
+import {fileURLToPath} from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+    const writeStream = createWriteStream(filePath);
+
+    process.stdin.on('data', (data) => {
+        const text = data.toString('utf-8');
+   
+        writeStream.write(text);
+    });
+
+    process.stdin.on('close', () => {
+        writeStream.end();
+    });
+
+    writeStream.on('error', (err) => {
+        console.error(err);
+    });
 };
 
 await write();
