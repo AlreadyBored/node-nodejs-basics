86921a9e4562b3dd34f3eee63e0ee188e59d4f0e
Nikita <nikitakemper555@gmail.com>
2023-06-17 21:20:33 +0300

    feat: add unzip solution

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import { createUnzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { pipeline } from 'stream'; 
+
 const decompress = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const sourcePath = path.join(__dirname, 'files', 'archive.gz');
+    const destinationPath = path.join(__dirname, 'files', 'fileToCompress.txt');
+    const unzip = createUnzip();
+
+    const source = createReadStream(sourcePath);
+    const destination = createWriteStream(destinationPath);
+
+    pipeline(source, unzip, destination, (err) => {
+        if (err) {
+            console.error('An error occurred:', err);
+            process.exitCode = 1;
+        }
+    });
 };
 
 await decompress();
