2a340e81ad096f66d3e12b5bf7799d062ed33fa7
Mar Kal <marc@example.com>
2023-06-17 13:06:20 +0200

    feat: finish compression subtask

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,23 @@
+import { createWriteStream } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { pipeline } from 'stream/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+  const file = join(__dirname, 'files', 'fileToWrite.txt');
+
+  const writableStream = createWriteStream(file);
+  //console.log('Writable stream created');
+
+  try {
+    await pipeline(writableStream);
+    console.log('Data written to file successfully.');
+  } catch (error) {
+    console.error('Error writing data to file:', error);
+  }
 };
 
 await write();
--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,22 @@
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { createGzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+  const file = join(__dirname, 'files', 'fileToCompress.txt');
+  const compressed = join(__dirname, 'files', 'archive.gz');
+  const handleStream = createReadStream(file);
+
+  handleStream
+    .pipe(createGzip())
+    .pipe(createWriteStream(compressed))
+    .on('finish', () => {
+      console.log('compression done');
+    });
 };
 
 await compress();
