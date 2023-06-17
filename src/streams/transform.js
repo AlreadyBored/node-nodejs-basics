8639f5f80f5f264a91e26a8d1d2debb51cd387fb
Nikita <nikitakemper555@gmail.com>
2023-06-17 20:56:42 +0300

    refactor: refactor transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -3,20 +3,14 @@ import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { stdout, stdin } from 'process';
 
-class ReverseTransform extends Transform {
-    constructor(options) {
-        super(options);
-    }
-
-    _transform(chunk, encoding, callback) {
-        const reversedChunk = chunk.toString().split('').reverse().join('') + '\n';
-        this.push(reversedChunk);
-        callback();
-    }
-}
-
 const transform = () => {
-    const reverseTransform = new ReverseTransform();
+    const reverseTransform = new Transform({
+        transform(chunk, encoding, callback) {
+            const reversedChunk = chunk.toString().split('').reverse().join('') + '\n';
+            this.push(reversedChunk);
+            callback();
+        }
+    });
 
     stdout.write('Введите строку. Для выхода Ctrl + C.\n');
     stdin.pipe(reverseTransform).pipe(stdout);
