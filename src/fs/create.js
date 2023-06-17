4daeb0f21584b12eb47e10ec2712519308b36514
beka.dvali <beka.dvali.2@iliauni.edu.ge>
2023-06-17 13:27:53 +0400

    Did copy and create tasks

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import * as fs from "fs";
+
 const create = async () => {
-    // Write your code here 
+    // Write your code here
+    const filePath = './files/fresh.txt'
+    const fileContent = 'I am fresh and young'
+    const fileExistsError = 'FS operation failed'
+
+    // Check if file exists and if so fs.access throws an error
+    try {
+        await fs.promises.access(filePath, fs.constants.F_OK)
+        throw new Error(fileExistsError)
+    } catch (e) {
+        if (e.message === fileExistsError) {
+            throw e
+        }
+        // If file does not exist then create it
+        await fs.promises.appendFile(filePath, fileContent)
+        console.log('File created Successfully')
+    }
+
 };
 
 await create();
