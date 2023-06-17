778f6fe68f4432efc1213bb566f394f3598971ac
Anatolii Pokhylko <pokhylko@gmail.com>
2023-06-17 23:52:51 +0300

    feat: zip compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import {createReadStream, createWriteStream} from 'fs';
+import {fileURLToPath} from 'url';
+import path from 'path';
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'fileToCompress.txt');
+const archivePath = path.join(__dirname, 'files', 'archive.gz');
+
 const compress = async () => {
-    // Write your code here 
+    const source = createReadStream(filePath);
+    const gzip = createGzip();
+    const destination = createWriteStream(archivePath);
+
+    pipeline(source, gzip, destination, (err) => {
+        if (err) {
+            console.error(err);
+        }
+    });
 };
 
 await compress();
