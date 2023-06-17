f8e00bd8d5e30ae733c3dc5d123a64c448853724
Nikita <nikitakemper555@gmail.com>
2023-06-17 13:13:52 +0300

    feat: add streamReadable  solution

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import { createReadStream } from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { stdout } from 'process';
+
 const read = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const pathToFile = path.join(__dirname, 'files', 'fileToRead.txt');
+
+    const readableStream = createReadStream(pathToFile, 'utf-8');
+
+    let body = '';
+    readableStream.on('data', chunk => body += chunk);
+    readableStream.on('end', () => stdout.write(body));
+    readableStream.on('error', error => console.log('Error', error.message));
 };
 
 await read();
