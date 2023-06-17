55fc8f39144a24881831b7470aaf1d3adf85b8ae
Hanna Huzava <guzovaann@gmail.com>
2023-06-17 13:44:31 +0200

    feat add compress() decomress()

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import { createGzip } from "node:zlib";
+import { fileURLToPath } from "node:url";
+import { dirname } from 'node:path';
+
+const directoryPath = dirname(fileURLToPath(import.meta.url));
+
 const compress = async () => {
-    // Write your code here 
+    const inp = createReadStream(`${directoryPath}/files/fileToCompress.txt`);
+    const out = createWriteStream(`${directoryPath}/files/archive.gz`);
+
+    const gzip = createGzip();
+
+    inp.pipe(gzip).pipe(out);
+    console.log("fileToCompress.txt was compressed");
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import { createUnzip } from "node:zlib";
+import { fileURLToPath } from "node:url";
+import { dirname } from 'node:path';
+
+const directoryPath = dirname(fileURLToPath(import.meta.url));
+
 const decompress = async () => {
-    // Write your code here 
+    const inp = createReadStream(`${directoryPath}/files/archive.gz`);
+    const out = createWriteStream(`${directoryPath}/files/fileToCompress.txt`);
+
+    const gzip = createUnzip();
+
+    inp.pipe(gzip).pipe(out);
+    console.log("archive.gz was decompressed!");
 };
 
 await decompress();
