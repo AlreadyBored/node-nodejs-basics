a1bdd79fbb240e3eb13c2cfbb86cfa93afecc8cc
Nikita <nikitakemper555@gmail.com>
2023-06-17 13:27:39 +0300

    feat: add streamWriteable  solution

--- src/streams/read.js
+++ src/streams/read.js
@@ -3,7 +3,7 @@ import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { stdout } from 'process';
 
-const read = async () => {
+const read = () => {
     const __dirname = dirname(fileURLToPath(import.meta.url));
     const pathToFile = path.join(__dirname, 'files', 'fileToRead.txt');
 
@@ -15,4 +15,4 @@ const read = async () => {
     readableStream.on('error', error => console.log('Error', error.message));
 };
 
-await read();
+read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import { createWriteStream } from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { stdin, stdout } from 'process'; 
+
 const write = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const pathToFile = path.join(__dirname, 'files', 'fileToWrite.txt')
+
+    const writeableStream = createWriteStream(pathToFile);
+
+    stdout.write('Введите данные для записи в файл. Для выхода Ctrl + C.\n');
+    stdin.on('data', (data) => {
+        writeableStream.write(data);
+    });
 };
 
 await write();
