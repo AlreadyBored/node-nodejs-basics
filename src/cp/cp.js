c5eb9e78a1f058a56c3a4414ec343122df4ce963
Aleksandr <ledich19@gmail.com>
2023-06-17 19:22:11 +0300

    feat: add cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,14 @@
+import { spawn } from 'child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  // Write your code here
+  const filePath = new URL("./files/script", import.meta.url);
+  const childProcess = spawn('node', ['./src/cp/files/script.js', args], { stdio: ['pipe', 'pipe', process.stderr] });
+  process.stdin.pipe(childProcess.stdin);
+  childProcess.stdout.on('data', (data) => {
+    process.stdout.write(data);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2']);
