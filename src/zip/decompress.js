ead6a67124e6f33732cbba3080bed7e2a10a6a50
Max_Nikitenok <xxx250790@gmail.com>
2023-06-17 10:28:46 +0300

    feat: zip/decompress solution

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,14 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createUnzip } from 'node:zlib';
+
+const source = createReadStream(new URL('./files/archive.gz', import.meta.url));
+const destination = createWriteStream(
+  new URL('./files/fileToCompress.txt', import.meta.url)
+);
+
 const decompress = async () => {
-    // Write your code here 
+  const unzip = createUnzip();
+  source.pipe(unzip).pipe(destination);
 };
 
 await decompress();
