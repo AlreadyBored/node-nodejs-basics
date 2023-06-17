7bf74a148ddaeda1ab9b033c52df95be7b09849b
Mar Kal <marc@example.com>
2023-06-17 13:31:14 +0200

    feat: finish decompression subtask

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,29 @@
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { createGunzip } from 'zlib';
+import { createReadStream, createWriteStream, unlink } from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const decompress = async () => {
-    // Write your code here 
+  const compressed = join(__dirname, 'files', 'archive.gz');
+  const decompressed = join(__dirname, 'files', 'fileToCompress.txt');
+  const handleStream = createReadStream(compressed);
+
+  handleStream
+    .pipe(createGunzip())
+    .pipe(createWriteStream(decompressed))
+    .on('finish', () => {
+      console.log('Decompression done');
+      unlink(compressed, (error) => {
+        if (error) {
+          console.error('Error deleting file:', error);
+        } else {
+          console.log('Compressed file deleted');
+        }
+      });
+    });
 };
 
 await decompress();
