fddaee5ed3b50cf2573bfd7c80aa9e85c2215718
Vitaly Cheastyakov <vchistiakov@datamola.com>
2023-06-17 17:37:36 +0300

    add copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,17 @@
+let fs = require('fs');
+let dir = './src/fs/files'
+let dirCopy = './src/fs/files_copy'
+
 const copy = async () => {
-    // Write your code here 
+  if (!fs.existsSync(dir)) {
+    console.log(new Error('FS operation failed'))
+  } else {
+    fs.cp(dir, dirCopy, { recursive: true }, (err) => {
+      if (err) {
+        console.error(new Error('FS operation failed'));
+      }
+    });
+  }
 };
 
-await copy();
+copy();
