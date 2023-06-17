0d536d120558562268dcb59adc5f4b0d339a4575
Max_Nikitenok <xxx250790@gmail.com>
2023-06-18 00:27:23 +0300

    feat: cp solution

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,14 @@
+import { fork } from 'node:child_process';
+
+const modulePath = new URL('./files/script.js', import.meta.url);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const childProcess = fork(modulePath, args);
+
+  childProcess.send(process.stdin);
+  childProcess.on('message', (m) => {
+    process.stdout.write(m);
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -4,6 +4,8 @@ import worker from 'node:worker_threads';
 const nthFibonacci = (n) =>
   n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
+// if(Math.random() > 0.5) throw new Error('Something went wrong');
+
 const sendResult = () => {
   worker.parentPort.postMessage(nthFibonacci(worker.workerData));
 };
