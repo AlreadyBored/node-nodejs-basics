7166be41e8655cf6db612c287e244a8e1a0595cd
artkon <artemkonopelko@gmail.com>
2023-06-17 23:45:28 +0400

    feat(wt): implement worker and use it in main thread

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,35 @@
+import { Worker } from 'worker_threads';
+import { getDirPath } from '../fs/utils.js';
+import { resolve } from 'path';
+import { cpus } from 'os';
+
+
+const WORKER_FILE_NAME = 'worker.js';
+const NTH_FIBONACCI_START_NUMBER = 10;
+
+const dirPath = getDirPath(import.meta.url);
+const workerPath = resolve(dirPath, WORKER_FILE_NAME);
+
+const numOfCors = cpus().length;
+
 const performCalculations = async () => {
-    // Write your code here
+    const computations = new Array(numOfCors).fill({ });
+    let endedWorkersCount = 0;
+
+    computations.forEach((_, index) => {
+        const worker = new Worker(workerPath, { workerData: NTH_FIBONACCI_START_NUMBER + index });
+        worker.on('message', (data) => {
+            computations[index] = ({ status: 'resolved', data });
+        });
+        worker.on('error', () => {
+            computations[index] = ({ status: 'error', data: null });
+        });
+        worker.on('exit', () => {
+            endedWorkersCount++;
+
+            if (endedWorkersCount === numOfCors) console.log(computations);
+        });
+    });
 };
 
 await performCalculations();
