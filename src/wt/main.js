884c5b8147a099113190d00fef25845840c09ff5
Max_Nikitenok <xxx250790@gmail.com>
2023-06-17 11:34:36 +0300

    feat: wt solution

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,29 @@
+import { Worker } from 'node:worker_threads';
+import { cpus } from 'node:os';
+
+const filePath = new URL('./worker.js', import.meta.url);
+const number = 10;
+
 const performCalculations = async () => {
-    // Write your code here
+
+  const workerResults = await Promise.allSettled(
+    cpus().map((_cpu, index) => {
+      return new Promise((resolve, reject) => {
+        const worker = new Worker(filePath, {
+          workerData: index + number,
+        });
+        worker.on('message', resolve);
+        worker.on('error', reject);
+      });
+    })
+  );
+
+  const formattedResults = workerResults.map(({ status, value }) => ({
+    status: status === 'fulfilled' ? 'resolved' : 'error',
+    data: status === 'fulfilled' ? value : null,
+  }));
+
+  console.log(formattedResults);
 };
 
 await performCalculations();
