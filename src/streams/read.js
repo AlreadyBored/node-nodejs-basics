5f15c47bba8e95edc7b0d8fb689d06ff341db2d7
Hanna Huzava <guzovaann@gmail.com>
2023-06-17 16:19:39 +0200

    feat: add stream read and write

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import { createReadStream } from "node:fs";
+import { fileURLToPath } from "node:url";
+import { dirname } from 'node:path';
+
+const directoryPath = dirname(fileURLToPath(import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+    const inp = createReadStream(`${directoryPath}/files/fileToRead.txt`, 'utf-8');
+
+    inp.pipe(process.stdout);
+   
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import { createWriteStream } from "node:fs";
+import { fileURLToPath } from "node:url";
+import { dirname } from 'node:path';
+
+const directoryPath = dirname(fileURLToPath(import.meta.url));
+
 const write = async () => {
-    // Write your code here 
+    const writeStream = createWriteStream(`${directoryPath}/files/fileToWrite.txt`);
+
+    process.stdin.pipe(writeStream);
 };
 
 await write();
