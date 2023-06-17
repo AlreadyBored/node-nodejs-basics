dd2362ab42692b27562eb86a9ccc84fc0308c097
Anatolii Pokhylko <pokhylko@gmail.com>
2023-06-17 23:24:45 +0300

    feat: streams transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,22 @@
+import { Transform, pipeline } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+    const transform = new Transform({
+        transform(chunk, encoding, callback) {
+            const reversedChunk = chunk.toString().split('').reverse().join('');
+
+            callback(null, reversedChunk + '\n\n');
+        }
+    });
+
+    pipeline(
+        process.stdin,
+        transform,
+        process.stdout,
+        (err) => {
+            console.error(err);
+        }
+    );
 };
 
 await transform();
