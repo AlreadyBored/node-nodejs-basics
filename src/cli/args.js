97c9324da2ff1d2c04f18426762a17f444cf2a3d
jkenjayev <javohirwebdev@gmail.com>
2023-06-17 16:24:54 +0500

    args.js is modified

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,20 +1,5 @@
 const parseArgs = () => {
     // Write your code here 
-    // const vars = process.argv
-    // let properties = [];
-    // let values = [];
-    // let result = [];
-    // for (let key = 0; key < vars.length; key++) {
-    //     if (vars[key].includes('--')) {
-    //         properties.push(vars[key].slice(2))
-    //         values.push(vars[key + 1])
-    //     }
-    // }
-
-    // for (let i = 0; i < properties.length; i++) {
-    //     result.push(`${properties[i]} is ${values[i]} `);
-    // }
-    // console.log(result.join(", "))
     let result = ''
     for (let i = 0; i < process.argv.length; i++) {
         if (process.argv[i].includes('--')) {
