968c40c76afc0dc2b90fd3f8d97eb3f80134ef9c
UTYFjs <utyf-90@mail.ru>
2023-06-17 14:26:13 +0300

    feat: add resolve zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import path, { dirname } from 'path';
+import url from 'url';
+import {pipeline} from 'stream/promises';
+import {} from 'stream';
+import zlib from 'zlib';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+    const readPath = path.join(__dirname, 'files', 'fileToCompress.txt');
+    const writePath = path.join(__dirname, 'files', 'archive.gz');
+    const readStream = fs.createReadStream(readPath);
+    const writeStream = fs.createWriteStream(writePath);
+    const gzipStream = zlib.createGzip()
+    try{
+        await pipeline(readStream, gzipStream, writeStream)
+    } catch (error) {
+         console.error('An error occurred:', error);
+    }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import fs from 'fs';
+import path, { dirname } from 'path';
+import url from 'url';
+import { pipeline } from 'stream/promises';
+import {} from 'stream';
+import zlib from 'zlib';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const decompress = async () => {
-    // Write your code here 
+    const readPath = path.join(__dirname, 'files', 'archive.gz');
+    const writePath = path.join(__dirname, 'files', 'fileToCompress.txt');
+    const readStream = fs.createReadStream(readPath);
+    const writeStream = fs.createWriteStream(writePath);
+    const gunzipStream = zlib.createGunzip();
+    try {
+      await pipeline(readStream, gunzipStream, writeStream);
+    } catch (error) {
+      console.error('An error occurred:', error);
+    }
+  // Write your code here
 };
 
 await decompress();
