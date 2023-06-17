7414e748256aaa049fedd9d9d0888bae338cd136
jkenjayev <javohirwebdev@gmail.com>
2023-06-17 16:22:02 +0500

    worker threads finished

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,12 @@
+import { workerData, parentPort } from "worker_threads";
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
     // This function sends result of nthFibonacci computations to main thread
+    if (Math.random > 0.5) throw new Error("Error!");
+    parentPort.postMessage(nthFibonacci(workerData));
 };
 
 sendResult();
