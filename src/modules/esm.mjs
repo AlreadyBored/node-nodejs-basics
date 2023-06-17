aacb721b0a346691fee52b428e56fb3908d39ac9
Denis Karandashev <denis.karandashev@t-systems.com>
2023-06-17 14:11:36 +0300

    refactor: clean up

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,12 +1,12 @@
 /* 
 You should refactor file (you can add additional imports if needed)
-cjsToEsm.cjs - rewrite it to it's equivalent in ECMAScript notation (and rename it to esm.mjs)
+cjsToEsm.cjs - rewrite it to it"s equivalent in ECMAScript notation (and rename it to esm.mjs)
 */
 
-import { path } from 'path';
-import { release, version } from 'os';
-import { createServer as createServerHttp } from 'http';
-import './files/c';
+import { path } from "path";
+import { release, version } from "os";
+import { createServer as createServerHttp } from "http";
+import "./files/c";
 
 let unknownObject;
 (async () => {
@@ -28,7 +28,7 @@ console.log(`Path to current file is ${__filename}`);
 console.log(`Path to current directory is ${__dirname}`);
 
 const myServer = createServerHttp((_, res) => {
-    res.end('Request accepted');
+    res.end("Request accepted");
 });
 
 const PORT = 3000;
@@ -37,7 +37,7 @@ console.log(unknownObject);
 
 myServer.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
-    console.log('To terminate it, use Ctrl+C combination');
+    console.log("To terminate it, use Ctrl+C combination");
 });
 
 export default {
--- src/streams/read.js
+++ src/streams/read.js
@@ -1,6 +1,6 @@
 /*
 implement function that reads file fileToRead.txt content using Readable Stream 
-and prints it's content into process.stdout
+and prints it"s content into process.stdout
 */
 
 import { createReadStream } from "fs";
