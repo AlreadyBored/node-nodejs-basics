e65895ed76153f0f2bc407d96603148398312fd9
Max_Nikitenok <xxx250790@gmail.com>
2023-06-17 10:12:14 +0300

    feat: zip/compress solution

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,16 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGzip } from 'node:zlib';
+
+const source = createReadStream(
+  new URL('./files/fileToCompress.txt', import.meta.url)
+);
+const destination = createWriteStream(
+  new URL('./files/archive.gz', import.meta.url)
+);
+
 const compress = async () => {
-    // Write your code here 
+  const gzip = createGzip();
+  source.pipe(gzip).pipe(destination);
 };
 
 await compress();
