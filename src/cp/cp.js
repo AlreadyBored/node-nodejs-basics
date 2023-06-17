2bb13130aff47bec0bf9961500a067c1fed98d46
jkenjayev <javohirwebdev@gmail.com>
2023-06-17 16:23:09 +0500

    work with child processes is finished

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,14 @@
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { spawn } from "child_process";
+
 const spawnChildProcess = async (args) => {
     // Write your code here
+    const childProcess = spawn(`node`, ['--version'])
+    childProcess.stdout.on('data', (data) => {
+        console.log(data.toString());
+    })
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess();
