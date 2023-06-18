c1a16022df8bed8cd805c68cb81cdd5e78075b28
Boris Sivko <bsivko@gmail.com>
2023-06-18 20:45:43 +0300

    init: list implemented

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,34 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const dirContext = 'files';
+const fileErrorMessage = 'FS operation failed';
+
+
 const list = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const filesPath = path.join(__dirname, dirContext);
+    // Check folder presence.
+    fs.access(filesPath, err => {
+        if (err) {
+            // The folder is absent - error.
+            throw new Error(fileErrorMessage);
+        }
+
+        // Read all in folder.
+        fs.readdir(filesPath, (err, files) => {
+            if (err) {
+                // Smth scary happened.
+                throw new Error(fileErrorMessage);
+            } 
+
+            files.forEach(file => {
+                console.log(file);
+            });
+          });   
+    });
 };
 
 await list();
