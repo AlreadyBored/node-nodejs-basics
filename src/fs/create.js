19ffbdb55bf65eca18bd044238dc98838284e610
Firsova Liubov <firsova.lyub@gmail.com>
2023-06-18 17:59:30 +0300

    feat: add create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import { access, constants, appendFile } from "node:fs/promises";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const fileName = fileURLToPath(import.meta.url);
+const __dirname = dirname(fileName);
+
+const filePath = join(__dirname, "files", "fresh.txt");
+
 const create = async () => {
-    // Write your code here 
+  access(filePath, constants.F_OK)
+    .then(() => {
+      console.log("FS operation failed");
+    })
+    .catch(() => {
+      appendFile(filePath, "I am fresh and young")
+        .then(() => console.log("the file is created"))
+        .catch((err) => console.log(err.message));
+    });
 };
 
 await create();
