f959c551b925b20afebf99e26d860cac589280a1
Boris Sivko <bsivko@gmail.com>
2023-06-18 21:21:11 +0300

    init: args implemented

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,37 @@
+const prefix = '--'
+const splitter = ", ";
+
 const parseArgs = () => {
-    // Write your code here 
+
+    // Pair args to process.
+    let argv0 = ''; //< must not contain prefix at start.
+    let argv1 = '';
+
+    // Comma splitter control.
+    let first = true;
+
+    let result = '';
+
+    process.argv.forEach((val) => {
+
+        argv0 = argv1;
+        argv1 = val;
+
+        // Does the first arg in pair have the prefix?
+        if (argv0.length >= prefix.length) {
+            if (argv0.substring(0, prefix.length) === prefix) {
+                // Yes, prefix is detected.                
+                
+                if (!first)
+                    result += splitter;
+
+                result += argv0.substring(2) + " is " + argv1;
+                first = false;
+            }
+        }        
+    });  
+    
+    console.log(result);
 };
 
 parseArgs();
