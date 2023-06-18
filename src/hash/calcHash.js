fabab02c2317754ba05a74e1bacc3152ad1af7bc
aliya <tusupkalievaaliya921@gmail.com>
2023-06-18 08:02:17 +0500

    add calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+//calcHash.js- реализовать функцию, которая вычисляет хэш SHA256 для файла fileToCalculateHashFor.txt и записывает его в консоль как  hex
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto'
 const calculateHash = async () => {
-    // Write your code here 
+    const path = './files/fileToCalculateHashFor.txt'
+    const hash = createHash('SHA256')
+    const stream = createReadStream(path)
+
+    return new Promise((resolve, reject) => {
+        stream.on('error', (err) => {
+            reject(err)
+        })
+        stream.on('data', (chunk) => {
+            hash.update(chunk)
+        })
+        stream.on('end', () => {
+            const hexHash = hash.digest('hex')
+            console.log(`hash for ${path}: ${hexHash}`)
+            resolve(hexHash)
+        })
+    })
 };
 
 await calculateHash();
