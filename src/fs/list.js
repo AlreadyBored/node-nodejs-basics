2cd5120a2b20526aaff435e6eef54bd70b97dfa5
Viktor Solovyev <barn-mih@rambler.ru>
2023-06-19 02:16:13 +0300

    feat: add implement for list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import { readdir } from "fs/promises";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+  const folderPath = join(__dirname, "files");
+
+  try {
+    const files = await readdir(folderPath);
+    for (const file of files) console.log(file);
+  } catch {
+    throw "FS operation failed";
+  }
 };
 
 await list();
