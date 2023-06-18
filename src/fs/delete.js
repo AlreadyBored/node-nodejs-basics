6da5e893b28f9c8a1ee0229ee84d33a9ebd45bf3
Firsova Liubov <firsova.lyub@gmail.com>
2023-06-18 19:44:15 +0300

    feat: add delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,21 @@
+import { access, constants, unlink } from "node:fs/promises";
+
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const fileName = fileURLToPath(import.meta.url);
+const __dirname = dirname(fileName);
+
+const fileToDel = join(__dirname, "files", "fileToRemove.txt");
+
 const remove = async () => {
-    // Write your code here 
+  access(fileToDel, constants.F_OK) // check if file exists
+    .then(() => {
+      unlink(fileToDel)
+        .then(() => console.log("the file is deleted"))
+        .catch((err) => console.log(err.message));
+    })
+    .catch(() => console.log("FS operation failed"));
 };
 
 await remove();
