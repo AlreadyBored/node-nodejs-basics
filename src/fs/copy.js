5cd3a5f57761378eb90d140ddfddab2a7c464dcb
Boris Sivko <bsivko@gmail.com>
2023-06-18 20:08:28 +0300

    init: copy implemented

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,56 @@
-const copy = async () => {
-    // Write your code here 
+import * as fs from 'fs';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const inDirName = 'files';
+const outDirName = 'files_copy';
+const fileErrorMessage = 'FS operation failed';
+
+const copy = async () => 
+{
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+
+    const inPath = path.join(__dirname, inDirName);
+    const outPath = path.join(__dirname, outDirName);
+
+    // Check in-path presence.
+    fs.access(inPath, err => {
+        if (err) {
+            throw new Error(fileErrorMessage + ":'" + inPath + "' does not exists");
+        }
+
+        // in-path found.
+        // Check out-path ansence.
+        fs.access(outPath, err => {
+            if (!err) {
+                throw new Error(fileErrorMessage + ":'" + outPath + "' exists");
+            }
+            // Out-path not found.
+    
+            // Let's make out-path.
+            fs.mkdir(outPath, err => {
+                if (err) {
+                    // Make dir failed.
+                    throw new Error(fileErrorMessage + ":'" + outPath + "' is not created.");
+                }
+
+                // Read all in-path and copy.
+                fs.readdir(inPath, (err, files) => {
+                    files.forEach(file => {
+                        fs.copyFile(path.join(inPath, file), path.join(outPath, file), (err) => {
+                            if (err) {
+                              throw new Error(fileErrorMessage + " at copying of a file'" + file  + "'");
+                            }
+                            else {               
+                                // 'file' file is copied.
+                            }
+                          });
+                    });
+                  });   
+            }); // mk-dir
+        }) // out-path
+    }); // in-path
 };
 
 await copy();
