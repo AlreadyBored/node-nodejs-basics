c42107c8bb05d696f4bb21f3634d9386a539b145
Viktor Solovyev <barn-mih@rambler.ru>
2023-06-19 02:21:16 +0300

    feat: add implement for read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import { readFile } from "fs/promises";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const folderPath = join(__dirname, "files");
+  const filePath = join(folderPath, "fileToRead.txt");
+
+  try {
+    let contents = await readFile(filePath, { encoding: "utf8" });
+    console.log(contents);
+  } catch {
+    throw "FS operation failed";
+  }
 };
 
 await read();
