e9d043d19d42f422800d64739709b03ff852e197
Iuliia Mazaeva <julie_meme@mail.ru>
2023-06-18 10:49:24 +0400

    feat: add solution to delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,15 @@
+import { unlink } from 'node:fs';
+
 const remove = async () => {
-    // Write your code here 
+    // Write your code here
+    const filePath = './files/fileToRemove.txt';
+
+    unlink(filePath, (err) => {
+        if (err) {
+            throw new Error('FS operation failed');
+        }
+        console.log('File has been deleted')
+    });
 };
 
 await remove();
