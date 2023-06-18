2612606b923cb7839aa50b76821de38baf51f727
Boris Sivko <bsivko@gmail.com>
2023-06-19 00:14:41 +0300

    init: compress implemented

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,34 @@
+import fs from 'fs';
+import path from 'path';
+import zlib from 'zlib';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const dirContext = 'files';
+const fileToCompress = 'fileToCompress.txt';
+const fileCompressed = 'archive.gz';
+const fileErrorMessage = 'FS operation failed';
+
 const compress = async () => {
-    // Write your code here 
+
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const fileToCompressPath = path.join(__dirname, dirContext, fileToCompress);
+    const fileToCompressedPath = path.join(__dirname, dirContext, fileCompressed);
+
+    // check file presence
+    fs.access(fileToCompressPath, fs.F_OK, (err) => {
+        if (err) {
+            // file is not found.
+            throw new Error(fileErrorMessage);
+        }
+      
+        // Prepare zlib & streams.
+        const gzip = zlib.createGzip();
+        const inFile = fs.createReadStream(fileToCompressPath);
+        const outFile = fs.createWriteStream(fileToCompressedPath);
+        // Compressing.
+        inFile.pipe(gzip).pipe(outFile);   
+    });
 };
 
 await compress();
