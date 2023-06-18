26fa8727d8392ee5e35f12dc33418692667991fe
Boris Sivko <bsivko@gmail.com>
2023-06-18 21:07:46 +0300

    init: env implemented

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,30 @@
+const scanPrefix = "RSS_";
+const splitter = "; ";
+
 const parseEnv = () => {
-    // Write your code here 
+
+    let allVars = process.env;    
+    let result = '';
+    // Comma splitter control.
+    let first = true;
+
+    // Scan all obj fields.
+    Object.keys(allVars).forEach(function(key,value) {
+        // key: var name
+        // value: var value
+        if (key.length >= scanPrefix.length) {
+            if (key.substring(0, scanPrefix.length) === scanPrefix) {
+                
+                if (!first)
+                    result += splitter;
+
+                result += key + "=" + value;
+                first = false;
+            }                
+        }        
+    });
+    
+    console.log(result);
 };
 
 parseEnv();
