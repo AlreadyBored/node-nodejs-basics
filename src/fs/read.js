f1e6f00189eafecbf1074146c6fd3ae0be3242c8
Boris Sivko <bsivko@gmail.com>
2023-06-18 20:50:46 +0300

    init: read implemented

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,34 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const dirContext = 'files';
+const fileToRead = 'fileToRead.txt';
+const fileErrorMessage = 'FS operation failed';
+const encoding = 'utf8';
+
 const read = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+
+    const fileToReadPath = path.join(__dirname, dirContext, fileToRead);
+
+    // check file presence
+    fs.access(fileToReadPath, fs.F_OK, (err) => {
+        if (err) {
+            // file is not found.
+            throw new Error(fileErrorMessage);
+        }
+      
+        fs.readFile(fileToReadPath, encoding, (err, text) => {
+            if (err) {
+                // Smth scary happened.
+                throw new Error(fileErrorMessage);
+            } 
+
+            console.log(text);
+          });        
+    });
 };
 
 await read();
