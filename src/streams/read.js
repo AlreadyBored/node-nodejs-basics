82c83db33d61ba165511aa7caab79410061b3f3a
Vitaly Cheastyakov <vchistiakov@datamola.com>
2023-06-18 18:02:31 +0300

    add read streams with createReadStream

--- src/streams/read.js
+++ src/streams/read.js
@@ -2,9 +2,18 @@ import fs from'fs';
 let dir = './src/streams/files/fileToRead.txt'
 let fileToWrite = './src/streams/files/process.stdout'
 
+async function createProcess(readable) {
+  for await (const chunk of readable) {
+    fs.writeFileSync(fileToWrite, chunk)
+  }
+}
+
 const read = async () => {
-  let readableStream = fs.readFileSync(dir,'utf8');
-  fs.writeFileSync(fileToWrite, readableStream)
+  const readableStream = fs.createReadStream(
+    dir, {encoding: 'utf8'});
+
+  await createProcess(readableStream);
+
 };
 
 await read();
