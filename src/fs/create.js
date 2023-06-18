790e4fb1fee2fe8ae452387d0e763162bc5ae59e
Viktor Solovyev <barn-mih@rambler.ru>
2023-06-18 15:15:06 +0300

    feat: add implement for create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import { writeFile } from "fs/promises";
+
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+  const distFolderPath = join(__dirname, "files");
+  const filePath = join(distFolderPath, "fresh.txt");
+
+  try {
+    await writeFile(filePath, "I am fresh and young", {flag: 'wx'});
+  } catch (error) {
+    throw "FS operation failed";
+  }
 };
 
 await create();
