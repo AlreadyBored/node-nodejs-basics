d9379a162a1ef05409dc88122185f094e48dc496
artkon <artemkonopelko@gmail.com>
2023-06-18 13:42:52 +0400

    feat(сз): implement child_process functionality

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,18 @@
+import { fork } from 'child_process';
+import { resolve } from 'path';
+
+import { getDirPath } from '../fs/utils.js';
+import { FOLDER_NAME } from '../fs/constants.js';
+
+
+const SCRIPT_FILE_NAME = 'script.js';
+
+const dirPath = getDirPath(import.meta.url);
+const scriptPath = resolve(dirPath, FOLDER_NAME, SCRIPT_FILE_NAME);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    fork(scriptPath, args);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ['argument1', 'argument2']);
