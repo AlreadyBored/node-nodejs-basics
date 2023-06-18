8191d9bfefe35ce798c9df4518a0bbb5e437e309
Alexandra Ilyashenko <sasha.ilyashenko@onde.app>
2023-06-18 19:10:26 +0200

    feat: zlib (all tasks)

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
+import fs from 'node:fs';
+import stream from 'node:stream';
+import zlib from 'node:zlib';
+
 const decompress = async () => {
-    // Write your code here 
+  const sourceFilePath = './src/zip/files/archive.gz';
+  const targetFilePath = './src/zip/files/fileToCompress.txt';
+
+  const unzip = zlib.createUnzip();
+  const source = fs.createReadStream(sourceFilePath);
+  const destination = fs.createWriteStream(targetFilePath);
+
+  stream.pipeline(source, unzip, destination, (err) => {
+    if (err) {
+      throw new Error(err)
+    }
+  })
 };
 
 await decompress();
