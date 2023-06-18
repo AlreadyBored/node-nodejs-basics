3d60934514cee53d060de646abd4e0dd0262d3d0
Boris Sivko <bsivko@gmail.com>
2023-06-18 20:28:40 +0300

    init: rename implemented

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,40 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const dirContext = 'files';
+const fileToRename = 'wrongFilename.txt';
+const fileNewName = 'properFilename.md';
+const fileErrorMessage = 'FS operation failed';
+
 const rename = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+
+    const renameFilePath = path.join(__dirname, dirContext, fileToRename);
+    const newNameFilePath = path.join(__dirname, dirContext, fileNewName);
+
+    // check rename-file presence
+    fs.access(renameFilePath, fs.F_OK, (err) => {
+        if (err) {
+            // file is not found.
+            throw new Error(fileErrorMessage);
+        }
+      
+        // Check target-file (absence).
+        fs.access(newNameFilePath, fs.F_OK, (err) => {
+            if (!err) {
+                // file is found.
+                throw new Error(fileErrorMessage);
+            }
+
+            // Let's rename it.
+
+            fs.rename(renameFilePath, newNameFilePath, () => {
+                // File renamed.
+            });            
+      });
+    });
 };
 
 await rename();
