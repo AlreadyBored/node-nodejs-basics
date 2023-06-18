2714b5abd10d2b5b814b6c65c35a7ba39dadec86
Iuliia Mazaeva <julie_meme@mail.ru>
2023-06-18 11:31:38 +0400

    feat: add solution to read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import { readFile } from 'node:fs/promises';
+import {existsSync} from "node:fs";
+
 const read = async () => {
-    // Write your code here 
+    // Write your code here
+    const filePath = './files/fileToRead.txt';
+
+    if (existsSync(filePath)) {
+        const fileData = await readFile(filePath, {encoding: 'utf8'});
+        console.log(fileData);
+    } else {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
