9d7c6fd7c118acc0e7538fab1315fdd7fd608f38
jkenjayev <javohirwebdev@gmail.com>
2023-06-18 13:15:52 +0500

    cp.js is modified

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,14 +1,17 @@
 import { fileURLToPath } from "url";
-import { dirname } from "path";
+import { dirname, resolve } from "path";
 import { spawn } from "child_process";
 
 const spawnChildProcess = async (args) => {
     // Write your code here
-    const childProcess = spawn(`node`, ['--version'])
+
+    const scriptJs = resolve(dirname(fileURLToPath(import.meta.url)), "files", "script.js");
+
+    const childProcess = spawn(`node`, [scriptJs, ...args])
     childProcess.stdout.on('data', (args) => {
         console.log(args.toString());
     })
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess();
+spawnChildProcess(['--version']);
