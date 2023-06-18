f330e013e265ee517492fbc44fb2f064e5ab15f5
Liza <lizavetayalak2000@gmail.com>
2023-06-18 17:24:14 +0200

    feat: implement main.js function, fix worker.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,35 @@
+import { cpus } from 'node:os'
+import { Worker } from 'node:worker_threads'
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
+
 const performCalculations = async () => {
-    // Write your code here
+    const numberOfWorkers = cpus().length
+    const directory = path.dirname(fileURLToPath(import.meta.url))
+    const workerFile = path.join(directory, 'worker.js')
+    const promises = []
+    for (let i = 0; i < numberOfWorkers; i++) {
+        promises.push(new Promise((resolve, reject) => {
+            const worker = new Worker(workerFile, { workerData: i + 10 })
+            worker.on('message', (message) => {
+                resolve({
+                    status: 'resolved',
+                    data: message
+                });
+            });
+
+            worker.on('messageerror', () => {
+                reject({
+                    status: 'error',
+                    data: null
+                })
+            })
+        }))
+    }
+    let results = await Promise.allSettled(promises)
+    const finalArr = results.map(el => el.value)
+    console.log(finalArr)
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,21 +1,9 @@
-import { isMainThread, Worker, parentPort } from 'node:worker_threads'
-import path from 'node:path';
-import { fileURLToPath } from 'url';
+import { workerData, parentPort } from 'node:worker_threads'
 
-// n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    const directory = path.dirname(fileURLToPath(import.meta.url))
-    const file = path.join(directory, 'worker.js')
-    if (isMainThread) {
-        const worker = new Worker(file)
-        worker.on('message', (message)=>{
-            console.log(message)
-        } )
-    } else {
-        parentPort.postMessage(nthFibonacci(4))
-    }
+    parentPort.postMessage(nthFibonacci(workerData))
 }
 
 sendResult();
