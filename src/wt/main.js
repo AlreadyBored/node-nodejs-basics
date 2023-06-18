1675a0818bbd59206b9f35860053dbe14518c2f5
fizarum <fizarum@gmail.com>
2023-06-18 14:34:48 +0200

    feat: implement tasks in 'wt' section

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,55 @@
+import { Worker } from "node:worker_threads";
+import { cpus } from "node:os";
+
 const performCalculations = async () => {
-    // Write your code here
+  const startFbNumber = 10;
+  const coreNumber = cpus().length;
+
+  const tasks = Array.from(Array(coreNumber).keys()).map((i) => {
+    return createWorker(i, startFbNumber + i);
+  });
+
+  const tasksResult = await Promise.all(tasks);
+
+  /*
+  added sorting based in worker index provided during createWorker() call
+  its possible to sort by "data" field, since for each next task result be bigger
+  than previous one
+
+  but sorting objects by index more abstract (and stable) of exact data value
+  */
+  const result = tasksResult
+    //sort results by index
+    .sort((a, b) => {
+      return a.index - b.index;
+    })
+    //and remove indexes
+    .map((taskResult) => {
+      return { status: taskResult.status, data: taskResult.data };
+    });
+
+  console.log(result);
+};
+
+/** creates worker and executes it worker thread
+ * @param index - worker index, used for sorting results
+ * @param data - payload, used for calculating fibonacci number
+ */
+const createWorker = (index, data) => {
+  return new Promise((resolve, reject) => {
+    const worker = new Worker("./src/wt/worker.js", { workerData: data });
+    worker.on("message", (result) => {
+      resolve({ status: "resolved", data: result, index: index });
+    });
+    worker.on("error", () => {
+      reject({ status: "error", data: null, index: index });
+    });
+    worker.on("exit", (code) => {
+      if (code !== 0) {
+        reject({ status: "error", data: null, index: index });
+      }
+    });
+  });
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,13 @@
+import { workerData, parentPort } from "node:worker_threads";
+
 // n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  // This function sends result of nthFibonacci computations to main thread
+  const result = nthFibonacci(workerData);
+  parentPort.postMessage(result);
 };
 
 sendResult();
