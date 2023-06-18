5e85f3bb11640be367371776dac3501162c86d63
UTYFjs <utyf-90@mail.ru>
2023-06-18 09:00:28 +0300

    feat: add resolve worker_threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,26 @@
+import {Worker} from 'worker_threads';
+import { cpus } from 'os';
+import path, { dirname } from 'path';
+import url from 'url';
+
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const performCalculations = async () => {
-    // Write your code here
+  const INITIAL_VALUE = 10;
+  
+  const calcWorker = (workerData) => new Promise((resolve, reject)=> {
+    const worker = new Worker(path.join(__dirname, 'worker.js'), {workerData});
+    worker.on('message', (data) => resolve({status: 'resolved', data: data}));
+    worker.on('error', ()=> reject({status: 'error', data: null}))
+  })
+
+  const workerPromises = cpus().map((value, index) => calcWorker(INITIAL_VALUE + index))
+  const results = (await Promise.allSettled(workerPromises)).map((result) =>
+                  result.status === 'fulfilled' ? result.value : result.reason);  
+
+  console.log(results);  
 };
 
 await performCalculations();
