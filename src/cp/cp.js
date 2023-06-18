c7357a952527b9277a57d1a6e55da18da75245cd
Mirael_No <bogdanova.karina22@gmail.com>
2023-06-18 13:32:46 +0300

    feat: finished all tasks

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,14 @@
+import { spawn } from 'node:child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const childProcess = spawn('node', ['./src/cp/files/script.js', ...args], {
+    stdio: ['pipe', 'pipe', process.stderr, 'ipc'],
+  });
+  process.stdin.pipe(childProcess.stdin);
+  childProcess.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(
+  [1, 23, 21213, 12312312] /* [someArgument1, someArgument2, ...] */
+);
