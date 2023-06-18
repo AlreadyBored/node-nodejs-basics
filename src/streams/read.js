37642958c301fb8489f7ea1e89e5243a8579bd47
besnadne <serpov.box@gmail.com>
2023-06-18 11:03:41 +0300

    feat: implement function that reads file fileToRead.txt content using Readable Stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,24 @@
+import { createReadStream } from "fs";
+import { join, dirname } from "path";
+
 const read = async () => {
-    // Write your code here 
+  const currDir = dirname(new URL(import.meta.url).pathname).replace(
+    /^\/([A-Za-z]:)/,
+    "$1"
+  );
+  const filePath = join(currDir, "files", "fileToREad.txt");
+
+  try {
+    const read = createReadStream(filePath, { encoding: "utf8" });
+    for await (const chunk of read) {
+      process.stdout.write(chunk);
+    }
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+
+  process.stdout.write("\n");
+  // Write your code here
 };
 
 await read();
