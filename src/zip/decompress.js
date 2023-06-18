67146f81ff5c39ebaac8e196b8b5b1d67dab1dfa
Vitaly Cheastyakov <vchistiakov@datamola.com>
2023-06-18 22:13:06 +0300

    add decompressed function

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,13 @@
+import fs from"fs";
+import zlib from "zlib";
+let fileToCompress = './src/zip/files/fileToCompress.txt'
+let compressedFile = './src/zip/archive.gz'
+
 const decompress = async () => {
-    // Write your code here 
+  let prepared = fs.createReadStream(compressedFile);
+  let final = fs.createWriteStream(fileToCompress);
+  let gzip = zlib.createUnzip();
+  prepared.pipe(gzip).pipe(final);
 };
 
 await decompress();
