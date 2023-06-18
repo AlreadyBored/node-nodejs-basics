d61db8f58b0becb8c08f83586b6826333b130c50
Artem <artem95hot@gmail.com>
2023-06-18 20:56:11 +0400

    feat: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,14 @@
+import crypto from "crypto";
+import fs from "fs";
+
 const calculateHash = async () => {
-    // Write your code here 
+    const dir = 'src/hash/files/'
+    const targetFile = 'fileToCalculateHashFor.txt'
+    const fileBuffer = fs.readFileSync(dir + targetFile);
+    const hashSum = crypto.createHash('sha256');
+    hashSum.update(fileBuffer);
+    const hex = hashSum.digest('hex');
+    console.log('hex',hex)
 };
 
 await calculateHash();
