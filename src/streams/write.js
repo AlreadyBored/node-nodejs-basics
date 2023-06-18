7d4fb9ab67976ee38e90effcf6d7b99b0d4e0160
beka.dvali <beka.dvali.2@iliauni.edu.ge>
2023-06-19 02:41:31 +0400

    stream tasks done

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,24 @@
+import fs from "fs";
+
 const write = async () => {
-    // Write your code here 
+    // Write your code here
+    const writeFilePath = './files/fileToWrite.txt'
+    let inputString = ''
+
+    const writable = fs.createWriteStream(writeFilePath, {encoding: 'utf8'});
+
+    const dataPromise = new Promise((resolve) => {
+        process.stdin.on('data', (data) => {
+            inputString = data.toString().trim();
+            if (inputString) {
+                process.stdin.pause()
+                resolve()
+            }
+        });
+    })
+    await dataPromise
+    writable.write(inputString)
+    writable.end()
 };
 
 await write();
