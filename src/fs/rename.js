8ec4bcacc2020cf004a2212e3cf54c7ad282b401
Iuliia Mazaeva <julie_meme@mail.ru>
2023-06-18 10:55:01 +0400

    feat: add solution to rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,15 @@
+import {existsSync} from "node:fs";
+import {renameSync} from "fs";
+
 const rename = async () => {
-    // Write your code here 
+    // Write your code here
+    const oldFilePath = './files/wrongFilename.txt';
+    const newFilePath = './files/properFilename.md';
+
+    if(existsSync(newFilePath)){
+        throw new Error('FS operation failed');
+    }
+    renameSync(oldFilePath,newFilePath);
 };
 
 await rename();
