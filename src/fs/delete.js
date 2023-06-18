96103dda1c73b59c13a2213a2d89addbf385dd09
Boris Sivko <bsivko@gmail.com>
2023-06-18 20:35:24 +0300

    init: delete implemented

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,36 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const dirContext = 'files';
+const fileToDelete = 'fileToRemove.txt';
+const fileErrorMessage = 'FS operation failed';
+
+
 const remove = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+
+    const filePath = path.join(__dirname, dirContext, fileToDelete);
+
+    // check file presence
+    fs.access(filePath, fs.F_OK, (err) => {
+        if (err) {
+            // file is not found.
+            throw new Error(fileErrorMessage);
+        }      
+        
+        // Let's delete it.
+
+        fs.unlink(filePath, (err) => {
+            if (err) {
+                // Smth scary happened.
+                throw new Error(fileErrorMessage);
+            } 
+
+            // File deleted.
+        });
+    });
 };
 
 await remove();
