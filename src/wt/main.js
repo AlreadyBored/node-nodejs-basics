0521dc537e0df09d7825df72889b84d315cec4de
banzayche <dharm.serg@gmail.com>
2023-06-18 15:29:56 +0300

    Finishing with workers

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,35 @@
+import { cpus } from 'node:os';
+import { Worker, isMainThread, workerData } from 'node:worker_threads';
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
+
+class WorkerWrapperModel extends Worker {
+  constructor(filename, options) {
+    super(filename, options);
+    this.resultPromise = new Promise(this.registerListeners.bind(this));
+  }
+
+  registerListeners(res, rej) {
+    let data = null;
+    this.on('message', message => data = message );
+    this.on('exit', exitCode => {
+      res({ status: exitCode === 0 ? 'resolved' : 'error', data });
+    } );
+  }
+}
+
 const performCalculations = async () => {
     // Write your code here
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const workerScriptFile = `${__dirname}/worker.js`;
+
+    const workers = await Promise.all(cpus().reduce((res, val, i) => {
+      const worker = new WorkerWrapperModel(workerScriptFile, {workerData: i+10});
+      return [...res, worker.resultPromise];
+    }, []));
+
+    console.log(workers);
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,12 @@
+import { isMainThread, workerData, parentPort } from 'node:worker_threads';
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
     // This function sends result of nthFibonacci computations to main thread
+    if (!isMainThread) {
+      parentPort.postMessage(nthFibonacci(workerData));
+    }
 };
 
 sendResult();
