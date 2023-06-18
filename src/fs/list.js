c098bfc39890bfa399afb877c4e20d09f2086313
Firsova Liubov <firsova.lyub@gmail.com>
2023-06-18 20:46:06 +0300

    feat: added list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,24 @@
+import { readdirSync } from "node:fs";
+import { access, constants } from "node:fs/promises";
+
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const fileName = fileURLToPath(import.meta.url);
+const __dirname = dirname(fileName);
+
+const folderPath = join(__dirname, "files");
 const list = async () => {
-    // Write your code here 
+  access(folderPath, constants.F_OK) // check if folder files exist
+    .then(async () => {
+      try {
+        const files = await readdirSync(folderPath);
+        console.log(files);
+      } catch (err) {
+        console.log(err.message);
+      }
+    })
+    .catch(() => console.log("FS operation failed"));
 };
 
 await list();
