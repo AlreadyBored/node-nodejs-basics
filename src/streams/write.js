632d23bc9616af2ccf9cb4bba4e0588424b74d6c
Boris Sivko <bsivko@gmail.com>
2023-06-19 00:02:30 +0300

    init: stream-write implemented

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const dirContext = 'files';
+const fileToWrite = 'fileToWrite.txt';
+const encoding = 'utf8';
+
 const write = async () => {
-    // Write your code here 
+  
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const fileToWritePath = path.join(__dirname, dirContext, fileToWrite);
+
+    // Open write stream.
+    const w = fs.createWriteStream(fileToWritePath, encoding);
+
+    // On data in console tranfer it into write stream.
+    process.stdin.on('data', data => {
+        w.write(data);
+      });
 };
 
 await write();
