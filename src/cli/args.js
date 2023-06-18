f6256869e2d563a5b1bb0f9d57f35f2203237ebe
Artem <artem95hot@gmail.com>
2023-06-18 20:49:43 +0400

    feat: cli module

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+  const message =  process.argv.reduce((acc, argName,i) => {
+        if (i % 2 === 0) {
+            acc = acc + `${argName} is ${process.argv[i + 1]}, `
+            return acc
+        }
+       return  acc
+    }, '')
+    console.log(message)
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,8 @@
 const parseEnv = () => {
-    // Write your code here 
+    const envs = process.env
+    for (const envName in envs)  {
+        console.log(`RSS_${envName}=${envs[envName]}`)
+    }
 };
 
 parseEnv();
