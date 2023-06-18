345de2c914b7d845b5a785cd0a019f096e21861d
aliya <tusupkalievaaliya921@gmail.com>
2023-06-18 07:05:33 +0500

    add transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,21 @@
+//transform.js- реализовать функцию, которая считывает данные из process.stdin, переворачивает текст с помощью Transform Stream и затем записывает его в process.stdout
+import { Transform } from 'stream';
 const transform = async () => {
-    // Write your code here 
-};
-
+    return new Promise((resolve, reject) => {
+        const reverseStream = Transform({
+            transform(chunk, encoding, callback) {
+                const reverseChunk = chunk.toString().split('').reverse().join('')
+                this.push(reverseChunk)
+                callback()
+            }
+        })
+        process.stdin.pipe(reverseStream).pipe(process.stdout)
+        process.stdin.on('error', (err) => {
+            resolve()
+        })
+        process.stdout.on('error', (err) => {
+            reject(err)
+        })
+    })
+}
 await transform();
