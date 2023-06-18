3fba1934f3938c7fc0f8b1028401967ca7d74770
Iuliia Mazaeva <julie_meme@mail.ru>
2023-06-18 10:29:06 +0400

    feat: add solution to create task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,16 @@
+import { existsSync } from 'node:fs';
+import { writeFile } from 'node:fs/promises';
+
 const create = async () => {
-    // Write your code here 
+    // Write your code here
+    const filePath = './files/fresh.txt';
+    const fileDataText = 'I am fresh and young';
+
+    if (existsSync(filePath)){
+        throw new Error('This file already exists');
+    } else {
+        await writeFile(filePath, fileDataText);
+    }
 };
 
 await create();
