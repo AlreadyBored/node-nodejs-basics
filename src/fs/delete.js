2275f873b82ed38e330732f67128cd27a0032019
Viktor Solovyev <barn-mih@rambler.ru>
2023-06-19 02:12:41 +0300

    feat: add implement for delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+import { rm } from "fs/promises";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+  const folderPath = join(__dirname, "files");
+  const filePath = join(folderPath, "fileToRemove.txt");
+
+  try {
+    await rm(filePath);
+  } catch {
+    throw "FS operation failed";
+  }
 };
 
 await remove();
