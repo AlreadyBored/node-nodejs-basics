81d67b891297369271a01fae2aabb36f0d025e96
Boris Sivko <bsivko@gmail.com>
2023-06-19 02:37:56 +0300

    init: wt implemented

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,70 @@
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { cpus } from 'os';
+import { Worker } from 'worker_threads';
+
+const workerFileName = 'worker.js';
+
+let result = [];
+
+// Create one worker promise.
+function createWorker(i) {
+
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const fileWorkerPath = path.join(__dirname, workerFileName);
+
+    return new Promise(function(resolve, reject) {
+        var w = new Worker(fileWorkerPath, 
+            {
+                workerData: i+10,
+            });
+
+        w.on("message", (msg) => {
+            // Keep 'i' as index to sort at the end.
+            result.push({status: 'resolved', data: [i, msg]});
+            resolve(msg);
+        });
+
+        w.on("error", (msg) => {
+            // Keep 'i' as index to sort at the end.
+            result.push({status: 'error', data: [i, null]});
+            resolve(msg);
+        });
+    });
+}
+
 const performCalculations = async () => {
-    // Write your code here
+
+    var promises = [];
+    for(var i = 0; i < cpus().length; i++) {
+        promises.push(createWorker(i));
+    }
+
+    // Wait finishing af all.
+    Promise.all(promises)
+        .then(function(data) {  
+
+            // Sort by index.
+            result.sort(function(a, b) {
+                var keyA = new Date(a.data[0]),
+                  keyB = new Date(b.data[0]);
+                if (keyA < keyB) return -1;
+                if (keyA > keyB) return 1;
+                return 0;
+              });
+
+            // Remove indexes.
+            for(var i = 0; i < result.length; i++) {
+                result[i]['data'] = result[i]['data'].at(1);
+            }
+
+            console.log(result);
+        })
+        .catch(function(error) {            
+            console.error("Error:" + error);
+        });
+    
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,15 @@
-// n should be received from main thread
+import { workerData, parentPort } from 'node:worker_threads';
+
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+
+// Uncomment to test errors in worker:
+/*
+    if (Math.random() > 0.5) 
+        throw new Error(workerData);
+*/    
+    parentPort.postMessage(nthFibonacci(workerData));
 };
 
 sendResult();
