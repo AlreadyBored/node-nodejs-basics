4d3201610440788e53d81b55ffa8fcdfc3b0f520
Firsova Liubov <firsova.lyub@gmail.com>
2023-06-18 17:58:52 +0300

    feat: add create function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import { readFile } from "node:fs/promises";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const fileName = fileURLToPath(import.meta.url);
+const __dirname = dirname(fileName);
+
+const filePath = join(__dirname, "files", "fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+  readFile(filePath)
+    .then((data) => console.log(data.toString()))
+    .catch(() => console.log("FS operation failed"));
 };
 
 await read();
