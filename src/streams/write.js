adbe2531fb6e2cb53f76c5af1369021de6b4c180
Viktor Solovyev <barn-mih@rambler.ru>
2023-06-19 13:34:22 +0300

    feat: add implement for streams:write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,30 @@
+import { createWriteStream } from "fs";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+import * as readline from "node:readline";
+
+const { stdin: input, stdout: output } = process;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+  const folderPath = join(__dirname, "files");
+  const outputFile = createWriteStream(join(folderPath, "fileToWrite.txt"));
+  const rl = readline.createInterface({ input, output });
+
+  rl.question("Hello! Enter some text (ctrl-c for exit): \n", (text) => {
+    outputFile.write(text + "\n");
+  });
+
+  rl.on("line", (text) => {
+    outputFile.write(text + "\n");
+  })
+    .on("SIGINT", () => {
+      process.exit(0);
+    })
+    .on("close", () => {
+      process.exit(0);
+    });
 };
 
 await write();
