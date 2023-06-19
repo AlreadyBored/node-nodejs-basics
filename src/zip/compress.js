9948842c3029b2af37709b5ef73075c0c7ff18c1
Lizaveta Petrova macOS <goldglaider@gmail.com>
2023-06-19 23:49:56 +0300

    feat: add solution for zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,15 @@
+import * as fs from "fs";
+import * as zlib from "zlib";
+import { getPath } from '../utils/getPath.js';
+
 const compress = async () => {
-    // Write your code here 
+  const fileToCompress = getPath(import.meta.url, "fileToCompress.txt");
+  const archive = getPath(import.meta.url, "archive.gz");
+
+  const readStream = fs.createReadStream(fileToCompress, 'utf-8');
+  const writeStream = fs.createWriteStream(archive);
+  
+  readStream.pipe(zlib.createGzip()).pipe(writeStream);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,15 @@
+import * as fs from "fs";
+import * as zlib from "zlib";
+import { getPath } from '../utils/getPath.js';
+
 const decompress = async () => {
-    // Write your code here 
+  const fileToDecompress = getPath(import.meta.url, "archive.gz");
+  const decompressedPath = getPath(import.meta.url, "fileToCompress.txt");
+
+  const readStream = fs.createReadStream(fileToDecompress);
+  const writeStream = fs.createWriteStream(decompressedPath);
+
+  readStream.pipe(zlib.createGunzip()).pipe(writeStream);
 };
 
 await decompress();
