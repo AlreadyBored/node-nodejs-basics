6ea12c48e4480a75d2f9c51186551b13782259c6
MasoNord <112730325+MasoNord@users.noreply.github.com>
2023-06-19 13:58:12 +0300

    Update cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,35 @@
+import {spawn} from 'child_process';
+
 const spawnChildProcess = async (args) => {
     // Write your code here
-};
+    const child = spawn('node', ["./src/cp/files/script.js", ...args], {
+        stdio: ["pipe", "pipe", "pipe", "ipc"]
+    });
+
+    child.stdin.setDefaultEncoding("utf-8");
+    child.stdout.pipe(process.stdout);
+    
+    child.stdin.on("data", (data) => {
+        child.stdin.write(`stdin: ${data}`);
+    })
+    
+    child.stdin.end();
+    
+    child.stdout.on("data", (data) => {
+        console.log(`stdout: ${data}`);
+    });
+
+    child.stderr.on("data", (data) => {
+        console.error(`stderr: ${data}`);
+    });
+
+    child.on("exit", (code) => {
+        console.log(`Child process exited with code ${code}`);
+    });
+
+
+    return child
+}
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["arg1", "arg2", "arg3", "arg4"]);
