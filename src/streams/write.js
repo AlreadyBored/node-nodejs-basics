99247d035894ffa8b958789216e91d3a139be2c3
andre353 <eva595eva@gmail.com>
2023-06-19 23:59:51 +0300

    feat: add streams/write.js solution

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,27 @@
-const write = async () => {
-    // Write your code here 
+import { createWriteStream } from 'fs';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const filePath = './files/fileToWrite.txt';
+
+const write = async (fp) => {
+    
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const absoluteFilePath = path.join(__dirname, fp);
+
+    const writeStream = createWriteStream(absoluteFilePath);
+
+    process.stdin.pipe(writeStream);
+
+    return new Promise((resolve, reject) => {
+        writeStream.on('finish', () => {
+            resolve();
+        });
+
+        writeStream.on('error', (err) => {
+            reject(err);
+        });
+    });
 };
 
-await write();
+await write(filePath);
