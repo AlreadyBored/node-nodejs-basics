d89dc98cd692d4af994bd12b64cb1480dfdbd207
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-06-19 21:33:37 +0500

    done create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import path from 'node:path';
+import fs from 'node:fs/promises';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const pathToFile = path.join(__dirname, 'files', 'fresh.txt');
+const content = 'I am fresh and young';
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await fs.writeFile(pathToFile, content, { flag: 'wx' });
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await create();
