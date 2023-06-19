0f89c59e7139260b990afba0c7ce7f5b2272efb9
Volodymyr Sych <volodymyr_sych@epam.com>
2023-06-19 23:26:17 +0300

    func calculateHash added

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import { fileURLToPath } from 'node:url';
+import { join, dirname } from 'node:path';
+import fs from 'node:fs/promises';
+import crypto from 'node:crypto';
+
+// to verify in terminal, call: node --experimental-modules ./calcHash.js
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+	const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+	try {
+		const data = await fs.readFile(filePath);
+		const hash = crypto.createHash('sha256');
+		hash.update(data);
+		const hashHex = hash.digest('hex');
+
+		console.log('SHA256 hash:', hashHex);
+	} catch (error) {
+		console.error('Error calculating hash:', error);
+	}
 };
 
 await calculateHash();
