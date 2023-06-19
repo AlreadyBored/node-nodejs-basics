9bb20383072cfb3eaf6b501e0d7c58265d57a491
4ebula <4ebula@gmail.com>
2023-06-19 16:02:16 +0400

    implement cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,15 @@
+import { fork } from 'node:child_process';
+import { resolve } from 'node:path';
+import { getPath } from '../fs/utils.js';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const __dirname = getPath(import.meta.url);
+    const script = resolve(__dirname, 'files', 'script.js');
+    const child = fork(script, args, { silent: true });
+
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(process.argv);
