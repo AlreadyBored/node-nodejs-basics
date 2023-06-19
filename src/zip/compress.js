d279e45ee8f9b965961ac6b179779d61a554b0a2
Artyom Loiko <artyom@Artyoms-MacBook-Air.local>
2023-06-19 23:59:04 +0300

    add: zip done

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,23 @@
+import fs from 'fs'
+import zlib from 'zlib'
+import stream from 'stream'
+import util from 'util'
+
+const pipe = util.promisify(stream.pipeline);
+
 const compress = async () => {
-    // Write your code here 
+    try {
+        const input = new URL('files/fileToCompress.txt', import.meta.url)
+        const output = new URL('files/archive.gz', import.meta.url)
+
+        const gzip = zlib.createGzip();
+        const source = fs.createReadStream(input);
+        const destination = fs.createWriteStream(output);
+        await pipe(source, gzip, destination);
+    } catch (err) {
+        console.error('An error occurred:', err);
+        process.exitCode = 1;
+    }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,23 @@
+import fs from 'fs'
+import zlib from 'zlib'
+import stream from 'stream'
+import util from 'util'
+
+const pipe = util.promisify(stream.pipeline);
+
 const decompress = async () => {
-    // Write your code here 
+    try {
+        const input = new URL('files/archive.gz', import.meta.url)
+        const output = new URL('files/fileToCompress.txt', import.meta.url)
+
+        const gunzip = zlib.createGunzip();
+        const source = fs.createReadStream(input);
+        const destination = fs.createWriteStream(output);
+        await pipe(source, gunzip, destination);
+    } catch (err) {
+        console.error('An error occurred:', err);
+        process.exitCode = 1;
+    }
 };
 
 await decompress();
