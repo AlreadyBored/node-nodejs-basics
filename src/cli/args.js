3b9f5a1334ec7c00a89ba4fb7316f34460384247
andre353 <eva595eva@gmail.com>
2023-06-19 22:56:57 +0300

    feat: add args.js solution

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-    // Write your code here 
+    
+    const args = {};
+
+    for (let i = 2; i < process.argv.length; i += 2) {
+      const propName = process.argv[i].slice(2);
+      const propValue = process.argv[i + 1];
+      args[propName] = propValue;
+    }
+  
+    const formattedArgs = Object.entries(args)
+      .map(([propName, propValue]) => `${propName} is ${propValue}`)
+      .join(', ');
+  
+    console.log(formattedArgs);
 };
 
 parseArgs();
