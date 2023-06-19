25543e8dcb022c5ac106d82c3d9ae3a366c7a0d9
Oleksandr Parkhomenko <119164331+proff1991@users.noreply.github.com>
2023-06-19 12:24:41 +0300

    calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+import { createHash, createHash as cryptoCreateHash } from 'crypto'
+import { readFile as fsReadFile } from 'fs/promises'
+import { join as pathJoin, dirname as pathDirname } from 'path'
+import { fileURLToPath } from 'node:url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = pathDirname(__filename)
 
-await calculateHash();
+const filePath = 'files'
+const fileName = 'fileToCalculateHashFor.txt'
+const fileFullPath = pathJoin(__dirname, filePath , fileName)
+
+
+const calculateHash = async () => {
+
+    const content = await fsReadFile(fileFullPath)
+    const hashDataSrc = createHash('sha256').update(content)
+    const hashDataHex = hashDataSrc.digest('hex')
+    console.log(hashDataHex)
+
+    return hashDataHex
+}
+
+await calculateHash()
