eab40d8eb4fb125a2fb8aec1059c501755c49c8e
Mar Kal <marc@example.com>
2023-06-19 11:36:55 +0200

    feat: start subtask child proccess

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,19 @@
+import { spawn } from 'child_process';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const scriptFilePath = join(__dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = spawn('node', [scriptFilePath, ...args], { stdio: 'inherit' });
+
+  child.on('exit', (code) => {
+    console.log(`Child process exited with code ${code}`);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+const args = ['argument1', 'argument2', 'argument3'];
+spawnChildProcess(args);
