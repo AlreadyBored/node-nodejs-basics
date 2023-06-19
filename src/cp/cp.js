ef28ee4c314532ced03b76f4450736098bcf50ec
UTYFjs <utyf-90@mail.ru>
2023-06-19 12:22:07 +0300

    feat: add resolve child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,18 @@
+import { fork } from 'child_process';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+
+    const scriptPath = path.join(__dirname, 'files', 'script.js');
+
+    const child = fork(scriptPath, args, {silent: true});
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['1', 'two', 33]);
+
