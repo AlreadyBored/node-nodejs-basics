d8b3fd302a3b6d0a18e882ca1dd543db55bce124
Oleksandr Parkhomenko <119164331+proff1991@users.noreply.github.com>
2023-06-19 13:21:20 +0300

    decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
-const decompress = async () => {
-    // Write your code here 
-};
+import { pipeline as streamPipeline } from 'stream/promises'
+import { createGunzip } from 'zlib'
+import { fileURLToPath } from 'node:url'
+import { createReadStream as fsCreateReadStream, createWriteStream as fsCreateWriteStream  } from 'fs'
+import { join as pathJoin, dirname as pathDirname } from 'path'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = pathDirname(__filename)
 
-await decompress();
+const fileName = 'fileToCompress.txt'
+const filePath = 'files'
+const fileFullPath = pathJoin(__dirname, filePath , fileName)
+const archiveName = 'archive.gz'
+const archiveFullPath = pathJoin(__dirname, filePath , archiveName)
+
+const decompress = async () => {
+    await streamPipeline( fsCreateReadStream(archiveFullPath), createGunzip(), fsCreateWriteStream(fileFullPath) )
+}
+
+await decompress()
