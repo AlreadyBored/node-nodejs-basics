3fd627a5c1578d4c5301905ec3c43eb4f7841304
Viacheslav Tyshchuk <slava1000uk@gmail.com>
2023-06-20 00:06:05 +0200

    feat: add solution to read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,15 @@
-const read = async () => {
-    // Write your code here 
+
+import { readFile } from 'node:fs/promises';
+
+const filePath = './files/fileToRead.txt';
+
+const read = async (filePath) => {
+    try {
+        const contents = await readFile(filePath);
+        console.log(contents);
+    } catch (err) {
+        console.error('FS operation failed');
+    }
 };
 
-await read();
+await read(filePath);
