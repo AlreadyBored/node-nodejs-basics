ac7c8835594ecf990d94aa6087fe82035a196a5c
besnadne <serpov.box@gmail.com>
2023-06-19 16:39:10 +0300

    feat: extend given function to work with data received from main thread and implement function which sends result of the computation to the main thread

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,16 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { parentPort, workerData } from "worker_threads";
 
-const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+const nthFibonacci = (n) => {
+  return n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 };
 
-sendResult();
+const sendResult = (result) => {
+  parentPort.postMessage(result);
+};
+
+const n = workerData;
+const result = {
+  status: "resolved",
+  data: nthFibonacci(n),
+};
+sendResult(result);
