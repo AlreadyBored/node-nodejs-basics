eb9692c09321cb77eab31dc475689a75c51a269a
Madiyar Kenzhegulov <madiyar.kenzhegulov@gmail.com>
2023-06-19 16:40:11 +0600

    feat: complete cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,12 @@
+import { fork } from "node:child_process";
+import path from "node:path";
+import url from "node:url";
+
+export const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  fork(path.resolve(__dirname, "./files/script.js"), args);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["foo", "bar", "baz"]);
