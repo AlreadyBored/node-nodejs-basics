1b6580292364a82fecf2ae512a3bb69b0ceb7c10
Oleksandr Parkhomenko <119164331+proff1991@users.noreply.github.com>
2023-06-19 12:25:40 +0300

    read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import { createReadStream as fsCreateReadStream } from 'fs'
+import { pipeline } from 'stream/promises'
+import { fileURLToPath } from 'node:url'
+import { join as pathJoin, dirname as pathDirname } from 'path'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = pathDirname(__filename)
+
+const fileName = 'fileToRead.txt'
+const filePath = 'files'
+const fileFullPath = pathJoin(__dirname, filePath , fileName)
+
 const read = async () => {
-    // Write your code here 
+    
+    await pipeline(fsCreateReadStream(fileFullPath), process.stdout)
+    
 };
 
-await read();
+await read()
