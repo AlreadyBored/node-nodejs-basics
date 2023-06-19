1ebf93090fe76aedece2b953968dffd674179a8c
Pavel Dubrovskiy <paveldubrovskiyit@gmail.com>
2023-06-20 02:24:51 +0300

    feat: added the solution for env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,7 @@
 const parseEnv = () => {
-    // Write your code here 
+    const result = [];
+    const input = Object.entries(process.env).forEach(i => {if(i[0].startsWith('RSS_')) result.push(`${i[0]} = ${i[1]}`)});
+    console.log(result.join('; '));
 };
 
 parseEnv();
