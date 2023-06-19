28e06ecabd0d02c7dd7955ee3c6f287ffcf1b2a1
besnadne <serpov.box@gmail.com>
2023-06-19 15:26:43 +0300

    feat:  implement function that compresses file fileToCompress.txt to archive.gz using zlib and Streams API

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,34 @@
+import { createGzip } from "zlib";
+import { pipeline } from "stream";
+import { createReadStream, createWriteStream } from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
 const compress = async () => {
-    // Write your code here 
+  const fileName = fileURLToPath(import.meta.url);
+  const dirName = dirname(fileName);
+  console.log(dirName);
+  const currFile = join(dirName, "files", "fileToCompress.txt");
+  const compressedFile = join(dirName, "files", "archive.gz");
+  const gzip = createGzip();
+  const source = createReadStream(currFile);
+  const destination = createWriteStream(compressedFile);
+  try {
+    await new Promise((resolve, reject) => {
+      pipeline(source, gzip, destination, (error) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve();
+        }
+      });
+    });
+
+    console.log("File compressed successfully!");
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+  // Write your code here
 };
 
 await compress();
