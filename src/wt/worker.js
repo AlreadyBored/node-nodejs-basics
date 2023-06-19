bc32a2dee05787be5dc8e7217b427c9f807dc8fa
MasoNord <112730325+MasoNord@users.noreply.github.com>
2023-06-19 14:05:52 +0300

    Update worker.js

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,21 @@
+import {Worker, parentPort, workerData} from "node:worker_threads";
 // n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+
+const numbers = workerData;
+
+function nthFibonacci(n) {
+    if (n < 2)
+        return n;
+    else
+        return nthFibonacci(n - 1) + nthFibonacci(n - 2);
+}
+
+const result = nthFibonacci(numbers);
 
 const sendResult = () => {
     // This function sends result of nthFibonacci computations to main thread
+    
+    parentPort.postMessage(result);
 };
 
-sendResult();
+sendResult(); 
