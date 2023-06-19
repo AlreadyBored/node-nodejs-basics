84f77fbdaa9070460130d1f9a1afd9b4bf3186e8
Mag <magzhan_birgebaiuly@epam.com>
2023-06-19 23:00:36 +0600

    feat: unfinish

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,29 @@
+import os from "os";
+import { Worker } from "worker_threads";
+import getPath from "../helper/getPath.js";
+
+const workerPath = getPath(import.meta, "worker.js");
+
+const cores = os.cpus().length;
+
 const performCalculations = async () => {
-    // Write your code here
+  const results = await Promise.allSettled(
+    new Array(cores).map(
+      (_, i) =>
+        new Promise((resolve) => {
+          const worker = new Worker(workerPath);
+
+          worker.postMessage({ action: "request", data: 10 + i });
+
+          worker.on("message", (message) => {
+            resolve(message);
+            worker.terminate();
+          });
+        })
+    )
+  );
+
+  console.log(results);
 };
 
 await performCalculations();
