38cc0b9d5205af6a1bbcc67ae74ca05a56fa96c3
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-06-20 00:37:24 +0500

    done list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import url from 'node:url';
+import path from 'node:path';
+import fs from 'node:fs/promises';
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const folderPath = path.join(__dirname, 'files');
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const files = await fs.readdir(folderPath);
+    for (const file of files) {
+      console.log(file);
+    }
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await list();
