d2c2465d854c9eeb29bbce7066512d1be458fe87
artzilla <artzilla.github@gmail.com>
2023-06-19 23:12:28 +0300

    feat: implemented child process function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,22 @@
+import path from 'path';
+import { spawn } from 'child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const currentFilePath = new URL(import.meta.url).pathname;
+  const dirname = path.dirname(currentFilePath);
+  const scriptPath = path.join(dirname, 'files', 'script.js');
+  const child = spawn('node', [scriptPath, ...args]);
+
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
+
+  child.on('error', (err) => console.error('Child process error:', err));
+
+  child.on('exit', (code) =>
+    console.log('Child process exited with code:', code)
+  );
+
+  await new Promise((resolve) => child.on('close', () => resolve()));
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2']);
