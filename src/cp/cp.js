38e4a78de4890a48b14f87868f50782a8760f458
oscarishe <a.polityko@mail.ru>
2023-06-19 19:22:32 +0300

    fix: fix args spread

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -5,7 +5,7 @@ import path, { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const spawnChildProcess = async (...args) => {
-    const cp = fork(path.join(__dirname, 'files', 'script.js'), [...args]);
+    const cp = fork(path.join(__dirname, 'files', 'script.js'), args);
 };
 
 spawnChildProcess(3, 14);
