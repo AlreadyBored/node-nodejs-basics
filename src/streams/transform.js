edf5c852e24da6d40bfe5ff3289db5ce58d02563
Mag <magzhan_birgebaiuly@epam.com>
2023-06-19 21:31:05 +0600

    feat: transform stream

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,22 @@
+import stream from "stream";
+
 const transform = async () => {
-    // Write your code here 
+  function transform(chunk, _, callback) {
+    const data = chunk.toString().trim();
+    let reversed = "";
+
+    for (let i = data.length - 1; i >= 0; i--) {
+      reversed += data[i];
+    }
+
+    process.stdout.write(reversed);
+    this.push("\n\n");
+    callback();
+  }
+
+  const transformStream = new stream.Transform({ transform });
+
+  process.stdin.pipe(transformStream).pipe(process.stdout);
 };
 
 await transform();
