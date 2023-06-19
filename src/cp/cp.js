510d8dfa0c1889aed14aaa8394271a9add55ac28
a.kashina <a.kashina@websecret.by>
2023-06-19 22:27:30 +0200

    feat: Child Processes task solution

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,14 @@
+import { fork } from "child_process";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __fileName = fileURLToPath(import.meta.url);
+const __dirName = dirname(__fileName);
+const file = join(__dirName, "files", "script.js");
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    fork(file, args);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["NodeJS", "basics"]);
