a6431ee3f454b13e945e3266583d3192ef3be309
UTYFjs <utyf-90@mail.ru>
2023-06-19 13:56:44 +0300

    refactor: some minor refactor zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,9 +1,9 @@
-import fs from 'fs';
+import { createReadStream, createWriteStream } from 'fs';
 import path, { dirname } from 'path';
 import url from 'url';
 import {pipeline} from 'stream/promises';
 import {} from 'stream';
-import zlib from 'zlib';
+import { createGzip} from 'zlib';
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,9 +11,9 @@ const __dirname = dirname(__filename);
 const compress = async () => {
     const readPath = path.join(__dirname, 'files', 'fileToCompress.txt');
     const writePath = path.join(__dirname, 'files', 'archive.gz');
-    const readStream = fs.createReadStream(readPath);
-    const writeStream = fs.createWriteStream(writePath);
-    const gzipStream = zlib.createGzip()
+    const readStream = createReadStream(readPath);
+    const writeStream = createWriteStream(writePath);
+    const gzipStream = createGzip()
     try{
         await pipeline(readStream, gzipStream, writeStream)
     } catch (error) {
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,9 +1,9 @@
-import fs from 'fs';
+import {createReadStream, createWriteStream} from 'fs';
 import path, { dirname } from 'path';
 import url from 'url';
 import { pipeline } from 'stream/promises';
 import {} from 'stream';
-import zlib from 'zlib';
+import {createGunzip} from 'zlib';
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,9 +11,9 @@ const __dirname = dirname(__filename);
 const decompress = async () => {
     const readPath = path.join(__dirname, 'files', 'archive.gz');
     const writePath = path.join(__dirname, 'files', 'fileToCompress.txt');
-    const readStream = fs.createReadStream(readPath);
-    const writeStream = fs.createWriteStream(writePath);
-    const gunzipStream = zlib.createGunzip();
+    const readStream = createReadStream(readPath);
+    const writeStream = createWriteStream(writePath);
+    const gunzipStream = createGunzip();
     try {
       await pipeline(readStream, gunzipStream, writeStream);
     } catch (error) {
