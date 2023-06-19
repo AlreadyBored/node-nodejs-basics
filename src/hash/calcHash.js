8be91d4baa3ccfde8c766d4fd27c9d7d4488cea1
Artyom Loiko <artyom@Artyoms-MacBook-Air.local>
2023-06-19 23:53:38 +0300

    add: hash done

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,11 @@
+import crypto from 'crypto'
+import fs from 'fs'
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = new URL('files/fileToCalculateHashFor.txt', import.meta.url)
+    const buff = await fs.promises.readFile(filePath)
+    const hex = crypto.createHash('sha256').update(buff).digest('hex')
+    console.log(hex)
 };
 
 await calculateHash();
