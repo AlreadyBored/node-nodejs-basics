c125ef1fb82cfa36465b4108931c4e9ec9938ebb
Viktor Solovyev <barn-mih@rambler.ru>
2023-06-19 16:15:08 +0300

    feat: add implement for zip:compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,25 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import { createGzip } from "node:zlib";
+import { pipeline } from "node:stream";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+  const folderPath = join(__dirname, "files");
+
+  const gzip = createGzip();
+  const source = createReadStream(join(folderPath, "fileToCompress.txt"));
+  const destination = createWriteStream(join(folderPath, "archive.gz"));
+
+  pipeline(source, gzip, destination, (err) => {
+    if (err) {
+      console.error("An error occurred:", err);
+      process.exitCode = 1;
+    }
+  });
 };
 
 await compress();
