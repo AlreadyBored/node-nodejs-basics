df62e20a4c300296aa186573e041617e59853d4b
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-06-19 23:57:42 +0500

    done copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,44 @@
+import url from 'node:url';
+import path from 'node:path';
+import fs from 'node:fs/promises';
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const source = path.join(__dirname, 'files');
+const destination = path.join(__dirname, 'files_copy');
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    const isDestExist = await checkFileExist(destination);
+    if (isDestExist) {
+      throw new Error('Destination already exists');
+    } else {
+      fs.mkdir(destination);
+    }
+    const files = await readFolder(source);
+    for (const file of files) {
+      await fs.copyFile(`${source}/${file}`, `${destination}/${file}`);
+    }
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
+};
+
+const checkFileExist = async (filePath) => {
+  try {
+    await fs.access(filePath);
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
+
+const readFolder = async (folderPath) => {
+  try {
+    const files = await fs.readdir(folderPath);
+    return files;
+  } catch (err) {
+    throw err;
+  }
 };
 
 await copy();
