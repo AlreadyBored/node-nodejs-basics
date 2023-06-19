968359f75c718813bcbfd5eb2d240e86118ffebe
a.kashina <a.kashina@websecret.by>
2023-06-19 22:27:06 +0200

    feat: Worker threads tasks solution

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,36 @@
+import { Worker } from "worker_threads";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import os from "os";
+
+const __fileName = fileURLToPath(import.meta.url);
+const __dirName = dirname(__fileName);
+
+const createWorker = (data) => {
+    return new Promise((resolve, reject) => {
+        const worker = new Worker(join(__dirName, "worker.js"), {
+            workerData: data,
+        });
+        worker.on("message", (message) =>
+            resolve({ status: "resolved", data: message })
+        );
+        worker.on("error", () => reject({ status: "error", data: null }));
+    });
+};
+
 const performCalculations = async () => {
-    // Write your code here
+    let workersPull = [];
+    const availableParallelism = os.availableParallelism();
+    for (let index = 0; index < availableParallelism; index++) {
+        const worker = createWorker(index + 10000);
+        workersPull.push(worker);
+    }
+
+    const calculationResults = (await Promise.allSettled(workersPull)).map(
+        (result) =>
+            result.status === "fulfilled" ? result.value : result.reason
+    );
+    console.log(calculationResults);
 };
 
 await performCalculations();
