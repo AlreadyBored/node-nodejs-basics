c9688110453bbdcb20247cadc30f1b42ad92ccf8
Artem <artem95hot@gmail.com>
2023-06-19 22:56:25 +0400

    feat: wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,15 @@
+import {Worker, isMainThread} from 'worker_threads'
+import os from 'os'
 const performCalculations = async () => {
-    // Write your code here
+    if (isMainThread) {
+        for (let i = 0; i < os.cpus().length; i++) {
+            const worker = new Worker('./src/wt/worker.js', { workerData: i + 10 });
+            worker.on('message', message => {
+                console.log('message',message)
+            })
+        }
+    }
+
 };
 
 await performCalculations();
