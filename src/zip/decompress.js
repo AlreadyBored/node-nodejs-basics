abbd13324d00ec464327d5d34ac29c3ff9ce6c43
MasoNord <112730325+MasoNord@users.noreply.github.com>
2023-06-19 14:06:51 +0300

    Update decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,12 @@
+import fs from 'fs';
+import zlib from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+    var FromZip = zlib.createGunzip();
+    var ReadStream = fs.createReadStream("./src/zip/files/archive.gz");
+    var WriteStream = fs.createWriteStream("./src/zip/files/fileToCompress.txt");
+
+    ReadStream.pipe(FromZip).pipe(WriteStream);
 };
 
 await decompress();
