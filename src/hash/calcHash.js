ad4bf4fbf726f1cc5f43dd0a3042194a6dc3cedd
Maria Apanovich <maria.apanovich32@gmail.com>
2023-06-19 21:16:47 +0200

    Hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import { createHash } from "crypto";
+import fs from "fs";
+
+import { ERROR } from "../constants.js";
+
+const url = import.meta.url;
+const path = new URL("./files/fileToCalculateHashFor.txt", url);
+
 const calculateHash = async () => {
-    // Write your code here 
+  fs.readFile(path, "utf-8", (err, data) => {
+    if (err) {
+      throw Error(ERROR);
+    } else {
+      const hash = createHash("sha256").update(data).digest("hex");
+      console.log(hash);
+    }
+  });
 };
 
 await calculateHash();
