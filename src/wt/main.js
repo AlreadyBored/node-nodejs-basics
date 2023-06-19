d9514dee327d4834e37bcef6c972ddf4b07abebd
Oleksandr Parkhomenko <119164331+proff1991@users.noreply.github.com>
2023-06-19 13:20:03 +0300

    main.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,39 @@
-const performCalculations = async () => {
-    // Write your code here
-};
+import { cpus } from 'os'
+import { Worker } from 'worker_threads'
+import { join as pathJoin, dirname as pathDirname  } from 'path'
+import { fileURLToPath } from 'node:url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = pathDirname(__filename)
 
-await performCalculations();
+const initNumber = 10
+const statusResolved = 'resolved'
+const statusError = 'error'
+const workerFile = 'worker.js'
+const workerPath = pathJoin(__dirname, workerFile)
+
+const performCalculations = async () => {
+    
+    const calculateNthFibonacci = workerData => new Promise( resolve => {
+
+        const runWorker = new Worker(workerPath, {workerData})
+        runWorker.on('message', data => resolve({ status: statusResolved, data}))
+        runWorker.on('error', data => resolve({ status: statusError, data: null}))
+
+    })
+
+    const cores = cpus()
+    const tasks = []
+
+    for(let index = 0 ; index<cores.length ; ++index){
+
+        tasks.push(calculateNthFibonacci(index + initNumber))
+
+    }
+
+    const results = await Promise.all(tasks)
+
+    console.log(results)
+
+}
+
+await performCalculations()
