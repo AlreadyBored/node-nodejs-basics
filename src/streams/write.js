689eb947751035f84be21227cfe599b74ef12a09
Oleksandr Parkhomenko <119164331+proff1991@users.noreply.github.com>
2023-06-19 12:26:19 +0300

    write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
-const write = async () => {
-    // Write your code here 
-};
+import { createWriteStream as fsCreateWriteStream } from 'fs'
+import { pipeline as streamPipeline } from 'stream/promises'
+import { fileURLToPath } from 'node:url'
+import { join as pathJoin, dirname as pathDirname } from 'path'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = pathDirname(__filename)
 
-await write();
+const fileName = 'fileToWrite.txt'
+const filePath = 'files'
+const fileFullPath = pathJoin(__dirname, filePath , fileName)
+
+const write = async () => {
+
+    const writableStream = fsCreateWriteStream(fileFullPath)
+    await streamPipeline(process.stdin, writableStream)
+
+}
+
+await write()
