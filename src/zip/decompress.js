e2e5803b8b63e8362c787a48746402352c19c064
Viktor Solovyev <barn-mih@rambler.ru>
2023-06-19 16:23:37 +0300

    feat: add implement for zip:decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import { createGunzip } from "node:zlib";
+import { pipeline } from "node:stream";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const decompress = async () => {
-    // Write your code here 
+  const folderPath = join(__dirname, "files");
+
+  const gunzip = createGunzip();
+  const source = createReadStream(join(folderPath, "archive.gz"));
+  const destination = createWriteStream(join(folderPath, "fileToCompress.txt"));
+
+  pipeline(source, gunzip, destination, (err) => {
+    if (err) {
+      console.error("An error occurred:", err);
+      process.exitCode = 1;
+    }
+  });
 };
 
 await decompress();
