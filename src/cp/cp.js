71b8a686c5750fd4e0983d0d54764ad99968d86c
oscarishe <a.polityko@mail.ru>
2023-06-19 19:18:37 +0300

    feat: complete cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,11 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { fork } from 'child_process';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const spawnChildProcess = async (...args) => {
+    const cp = fork(path.join(__dirname, 'files', 'script.js'), [...args]);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(3, 14);
