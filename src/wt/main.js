f4e44408aa0d5432f5433567defccdf4ff62bfec
artzilla <artzilla.github@gmail.com>
2023-06-19 23:06:32 +0300

    feat: implemented worker function

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,55 @@
+import os from 'os';
+import path from 'path';
+import { Worker } from 'worker_threads';
+
+const getCPUCoreCount = () => 3; // os.cpus().length || 1;
+
 const performCalculations = async () => {
-    // Write your code here
+  const workers = [];
+  const cpuCoreCount = getCPUCoreCount();
+
+  const currentFilePath = new URL(import.meta.url).pathname;
+  const dirname = path.dirname(currentFilePath);
+  const workerPath = path.join(dirname, 'worker.js');
+
+  for (let i = 0; i < cpuCoreCount; i++) {
+    const worker = new Worker(workerPath);
+    workers.push(worker);
+  }
+
+  let counter = 10;
+
+  for (const worker of workers) {
+    worker.postMessage(counter);
+    counter++;
+  }
+
+  const results = [];
+
+  for (const worker of workers) {
+    const result = await new Promise((resolve) => {
+      worker.on('message', (data) => resolve({ status: 'success', data }));
+      worker.on('error', (error) =>
+        resolve({ status: 'error', data: null, error })
+      );
+    });
+
+    results.push(result);
+    worker.terminate();
+  }
+
+  return results;
 };
 
-await performCalculations();
+console.log('Calculations started');
+
+performCalculations()
+  .then((results) => {
+    console.log('Results:', results);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  })
+  .finally(() => {
+    console.log('Calculations completed');
+  });
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,22 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { parentPort } from 'worker_threads';
 
-const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+// Memoization cache
+const memo = new Map();
+
+// n should be received from main thread
+const nthFibonacci = (n) => {
+  if (memo.has(n)) {
+    return memo.get(n);
+  }
+
+  const result = n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+  memo.set(n, result);
+  return result;
 };
 
-sendResult();
+const sendResult = (n) => {
+  const result = nthFibonacci(n);
+  parentPort.postMessage(result);
+};
+
+parentPort.on('message', sendResult);
