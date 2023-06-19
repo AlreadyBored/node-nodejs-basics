4a177a0d9b084c33b9d3cba44b76e9db91e6fd22
a.kashina <a.kashina@websecret.by>
2023-06-19 22:22:48 +0200

    feat: Command line interface tasks solution

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+    const [_nodeExecPath, _execFilePath, ...args] = process.argv;
+
+    let propsWithValues = [];
+    for (let i = 0; i < args.length; i += 2) {
+        propsWithValues.push(`${args[i].slice(2)} is ${args[i + 1]}`);
+    }
+    if (propsWithValues.length) console.log(propsWithValues.join(", "));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+    const RSSkeys = Object.keys(process.env).filter((key) =>
+        key.startsWith("RSS_")
+    );
+    const joinedKeys = RSSkeys.map((key) => `${key}=${process.env[key]}`).join(
+        "; "
+    );
+    if (joinedKeys.length) console.log(joinedKeys);
 };
 
 parseEnv();
