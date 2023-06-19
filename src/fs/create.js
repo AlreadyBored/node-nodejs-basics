56df59d34e8a7f7ce49c5a3f2ebed662cc2b8e1b
MasoNord <112730325+MasoNord@users.noreply.github.com>
2023-06-19 13:58:54 +0300

    Update create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import fs from 'fs';
+
+const path = "./src/fs/files/fresh.txt";
+const text = "I am fresh and young";
+
 const create = async () => {
-    // Write your code here 
+    if(fs.existsSync(path)) {
+        throw new Error("FS operation failed");
+    }else {
+        fs.writeFile(path, text, function(err) {
+            if (err) throw err;
+            console.log("File is created successfully");
+        })
+    }      
 };
 
 await create();
