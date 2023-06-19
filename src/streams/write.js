b12632caae27a71a7861cc78b35a5f4d8ac3ffe2
MasoNord <112730325+MasoNord@users.noreply.github.com>
2023-06-19 14:04:43 +0300

    Update write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,28 @@
+import  Stream from 'stream'
+import process from 'process'
+import fs from 'fs';
+
+const path = "./src/streams/files/fileToWrite.txt";
+
 const write = async () => {
-    // Write your code here 
+    
+    if (!fs.existsSync(path))
+        throw new Error("The path does not exist!!!");
+    
+    const file = fs.createWriteStream(path);
+    process.stdin.setEncoding("utf-8");
+    process.stdin.on("readable", () => {
+        var input = process.stdin.read();
+        if (input !== null) {
+            
+            file.write(input + "");
+            file.end();
+
+        }else {
+            process.stdout.write(input);
+        }
+    });
+
 };
 
 await write();
