40475d307172a435f7efdc22cb5c165cef0ff452
Firsova Liubov <firsova.lyub@gmail.com>
2023-06-19 23:23:59 +0300

    feat: add solved tasks to streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,29 @@
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+import { access, constants } from "node:fs/promises";
+
+import { createReadStream } from "node:fs";
+import { stdout } from "node:process";
+
+const fileName = fileURLToPath(import.meta.url);
+const __dirname = dirname(fileName);
+
+const filePath = join(__dirname, "files", "fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+  access(filePath, constants.F_OK) // check if file exists
+    .then(() => {
+      const readableStream = createReadStream(filePath);
+      readableStream.on("data", (chunk) => {
+        // console.log(chunk.toString());
+        console.log(stdout.write(chunk));
+      });
+      readableStream.on("end", () => {
+        console.log("end of reading the stream");
+      });
+    })
+    .catch((err) => console.log(err.message));
 };
 
 await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,13 @@
+import { stdin, stdout } from "node:process";
+
 const transform = async () => {
-    // Write your code here 
+  stdin.setEncoding("utf8");
+  stdin.on("data", (chunk) => {
+    stdout.write(chunk);
+  });
+  stdin.on("end", () => {
+    console.log("end of transforming the stream");
+  });
 };
 
 await transform();
