c99af5824f4a2f6ba1bec4c9f7dac1bbce2ef493
Oleksandr Parkhomenko <119164331+proff1991@users.noreply.github.com>
2023-06-19 12:23:01 +0300

    create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,29 @@
-const create = async () => {
-    // Write your code here 
-};
+import { writeFile as fsWriteFile } from 'fs/promises';
+import { join as pathJoin, dirname as pathDirname  } from 'path'
+import { fileURLToPath } from 'node:url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = pathDirname(__filename)
 
-await create();
+
+const folderPath = 'files'
+const fileName = 'fresh.txt'
+const fileContent = 'I am fresh and young'
+const errorMessage = 'FS operation failed'
+const fullPath = pathJoin(__dirname, folderPath, fileName)
+
+
+const create = async () => {
+    
+    try{
+
+      await fsWriteFile(fullPath, fileContent, { flag: 'wx' })
+
+    }catch{
+
+      throw new Error(errorMessage)
+
+    }
+
+}
+
+await create()
