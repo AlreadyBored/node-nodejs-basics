190ee84543f2b6d8c870c541ccc33e636ab091dc
Viktor Solovyev <barn-mih@rambler.ru>
2023-06-19 12:43:51 +0300

    feat: add implement for calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+import { createHmac } from "node:crypto";
+import { readFile } from "fs/promises";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const folderPath = join(__dirname, "files");
+  const filePath = join(folderPath, "fileToCalculateHashFor.txt");
+
+  try {
+    let contents = await readFile(filePath, { encoding: "utf8" });
+    const secret = "abcdefg";
+    const hash = createHmac("sha256", secret).update(contents).digest("hex");
+    console.log(hash);
+  } catch (error) {
+    throw error;
+  }
 };
 
 await calculateHash();
