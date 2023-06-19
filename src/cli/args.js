35eb86d8fb124af920c9c953f92d6057f2a4ea50
Muhammed Abdrahman <muhammed03amin@gmail.com>
2023-06-20 00:23:16 +0600

    feat: add solution for cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
 const parseArgs = () => {
-    // Write your code here 
+    const [,,...rest] = process.argv;
+    const res = [];
+    for (let i = 1; i < rest.length; i+=2) {
+        res.push(`${rest[i-1]} is ${rest[i]}`);
+    }
+    console.log(res.join(', '));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,8 @@
+import { env } from 'process';
+
 const parseEnv = () => {
-    // Write your code here 
+    const res = Object.keys(env).filter(key => key.startsWith('RSS_')).map(val => `${val}=${env[val]}`);
+    console.log(res.join('; '));
 };
 
 parseEnv();
