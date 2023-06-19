a3e938917ba2c40420c5cef5022c5b3247f470a9
beka.dvali <beka.dvali.2@iliauni.edu.ge>
2023-06-20 00:52:17 +0400

    compress, decompress, cp, worker tasks done

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import {pipeline} from 'stream'
+import {promisify} from 'node:util'
+import {createGzip} from 'zlib'
+import {createReadStream, createWriteStream} from 'fs'
 const compress = async () => {
-    // Write your code here 
+    // Write your code here
+    const pipe = promisify(pipeline);
+    const fileToCompressPath = './files/fileToCompress.txt'
+    const destinationPath = './files/archive.gz'
+
+    async function do_gzip(input, output) {
+        const gzip = createGzip();
+        const source = createReadStream(input);
+        const destination = createWriteStream(output);
+        await pipe(source, gzip, destination);
+    }
+
+    await do_gzip(fileToCompressPath, destinationPath)
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+import {pipeline} from 'stream'
+import {promisify} from 'node:util'
+import {createGunzip} from 'zlib'
+import {createReadStream, createWriteStream} from 'fs'
+
 const decompress = async () => {
-    // Write your code here 
+    // Write your code here
+    const pipe = promisify(pipeline);
+    const fileToCompressPath = './files/fileToCompress.txt'
+    const fileToDecompressPath = './files/archive.gz'
+
+    async function do_gunzip(input, output) {
+        const gunzip = createGunzip();
+        const source = createReadStream(input);
+        const destination = createWriteStream(output);
+        await pipe(source, gunzip, destination);
+    }
+
+    await do_gunzip(fileToDecompressPath, fileToCompressPath)
 };
 
 await decompress();
