38931bbff57f8aecf2c6b30311e95bac90b9f6ed
Oleksandr Parkhomenko <119164331+proff1991@users.noreply.github.com>
2023-06-19 15:34:08 +0300

    esm.mjs

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -9,9 +9,9 @@ const __dirname = path.dirname(__filename)
 
 import './files/c.js'
 
-const random = Math.random();
+const random = Math.random()
 
-let unknownObject;
+let unknownObject
 
 if (random > 0.5) {
     unknownObject = require('./files/a.json');
@@ -19,27 +19,27 @@ if (random > 0.5) {
     unknownObject = require('./files/b.json');
 }
 
-console.log(`Release ${release()}`);
-console.log(`Version ${version()}`);
-console.log(`Path segment separator is "${path.sep}"`);
+console.log(`Release ${release()}`)
+console.log(`Version ${version()}`)
+console.log(`Path segment separator is "${path.sep}"`)
 
-console.log(`Path to current file is ${__filename}`);
-console.log(`Path to current directory is ${__dirname}`);
+console.log(`Path to current file is ${__filename}`)
+console.log(`Path to current directory is ${__dirname}`)
 
 const myServer = createServerHttp((_, res) => {
-    res.end('Request accepted');
-});
+    res.end('Request accepted')
+})
 
 const PORT = 3000;
 
 console.log(unknownObject);
 
 myServer.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-    console.log('To terminate it, use Ctrl+C combination');
-});
+    console.log(`Server is listening on port ${PORT}`)
+    console.log('To terminate it, use Ctrl+C combination')
+})
 
 export {
     unknownObject,
     myServer,
-};
+}
