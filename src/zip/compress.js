25f652fd457648f7f6791a9cbcb502cd22d0a179
Oleksandr Parkhomenko <119164331+proff1991@users.noreply.github.com>
2023-06-19 13:21:04 +0300

    compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
-const compress = async () => {
-    // Write your code here 
-};
+import { pipeline as streamPipeline } from 'stream/promises'
+import { createGzip } from 'zlib'
+import { fileURLToPath } from 'node:url'
+import { createReadStream as fsCreateReadStream, createWriteStream as fsCreateWriteStream  } from 'fs'
+import { join as pathJoin, dirname as pathDirname } from 'path'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = pathDirname(__filename)
 
-await compress();
+const fileName = 'fileToCompress.txt'
+const filePath = 'files'
+const fileFullPath = pathJoin(__dirname, filePath , fileName)
+const archiveName = 'archive.gz'
+const archiveFullPath = pathJoin(__dirname, filePath , archiveName)
+
+const compress = async () => {
+    await streamPipeline( fsCreateReadStream(fileFullPath), createGzip(), fsCreateWriteStream(archiveFullPath) )
+}
+
+await compress()
