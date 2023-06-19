6cafb9cce1a12e97e55b54e7f744e1d88362ddd6
Pavel Dubrovskiy <paveldubrovskiyit@gmail.com>
2023-06-19 11:26:24 +0300

    feat: added solution for create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import { writeFile } from 'node:fs/promises';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const ERROR_MESSAGE = 'FS operation failed';
+const TEXT = 'I am fresh and young';
+
 const create = async () => {
-    // Write your code here 
+    let pathToFile = join(__dirname, '/files/fresh.txt');
+
+    try {
+        await writeFile(pathToFile, TEXT, {flag: 'wx'});
+    } catch(err){
+        throw new Error(ERROR_MESSAGE);
+    }
 };
 
 await create();
