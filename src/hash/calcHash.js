33ee6cc8c01e598f679bdfcc8789b4468de5b933
a.kashina <a.kashina@websecret.by>
2023-06-19 22:24:13 +0200

    feat: Hash task solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import { readFile } from "fs/promises";
+import { createHash } from "crypto";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __fileName = fileURLToPath(import.meta.url);
+const __dirName = dirname(__fileName);
+const file = join(__dirName, "files", "fileToCalculateHashFor.txt");
+
 const calculateHash = async () => {
-    // Write your code here 
+    const contents = await readFile(file);
+    const hexHash = createHash("sha256").update(contents).digest("hex");
+    console.log(hexHash);
 };
 
 await calculateHash();
