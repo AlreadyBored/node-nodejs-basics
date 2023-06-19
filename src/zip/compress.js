5809947939f6da85aedfae6f5caf33e3f926f6f4
a.kashina <a.kashina@websecret.by>
2023-06-19 22:26:27 +0200

    feat: Zlib tasks solution

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,22 @@
+import { createReadStream, createWriteStream } from "fs";
+import { promisify } from "util";
+import { createGzip } from "zlib";
+import { pipeline } from "stream";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const pipe = promisify(pipeline);
+
+const __fileName = fileURLToPath(import.meta.url);
+const __dirName = dirname(__fileName);
+const input = join(__dirName, "files", "fileToCompress.txt");
+const output = join(__dirName, "files", "archive.gz");
+
 const compress = async () => {
-    // Write your code here 
+    const gzip = createGzip();
+    const source = createReadStream(input);
+    const destination = createWriteStream(output);
+    await pipe(source, gzip, destination);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+import { createReadStream, createWriteStream } from "fs";
+import { promisify } from "util";
+import { createUnzip } from "zlib";
+import { pipeline } from "stream";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const pipe = promisify(pipeline);
+
+const __fileName = fileURLToPath(import.meta.url);
+const __dirName = dirname(__fileName);
+const input = join(__dirName, "files", "archive.gz");
+const output = join(__dirName, "files", "fileToCompress.txt");
+
 const decompress = async () => {
-    // Write your code here 
+    const unzip = createUnzip();
+    const source = createReadStream(input);
+    const destination = createWriteStream(output);
+    await pipe(source, unzip, destination);
 };
 
 await decompress();
