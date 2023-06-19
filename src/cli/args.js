64a617753262e5476491591363d7bdc6f0fdcba5
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-06-20 00:51:31 +0500

    done args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
 const parseArgs = () => {
-    // Write your code here 
+  const argv = process.argv.slice(2);
+  let result = '';
+  for (let i = 0; i < argv.length; i += 2) {
+    result += `${argv[i].slice(2)} is ${argv[i + 1]}, `;
+  }
+  console.log(result.slice(0, -2));
 };
 
 parseArgs();
