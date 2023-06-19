9bbc3683d33c01673e30cfc78389314c4df59dd5
Maria Apanovich <maria.apanovich32@gmail.com>
2023-06-19 21:28:01 +0200

    Zlib

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import zlib from "zlib";
+import fs from "fs";
+
+const url = import.meta.url;
+const path = new URL("./files/fileToCompress.txt", url);
+const archive = new URL("./files/archive.gz", url);
+
 const compress = async () => {
-    // Write your code here 
+  const zip = zlib.createGzip();
+
+  const read = fs.createReadStream(path);
+  const write = fs.createWriteStream(archive);
+
+  read.pipe(zip).pipe(write);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,17 @@
+import zlib from "zlib";
+import fs from "fs";
+
+const url = import.meta.url;
+const path = new URL("./files/fileToCompress.txt", url);
+const archive = new URL("./files/archive.gz", url);
+
 const decompress = async () => {
-    // Write your code here 
+  const zip = zlib.createUnzip();
+
+  const read = fs.createReadStream(archive);
+  const write = fs.createWriteStream(path);
+
+  read.pipe(zip).pipe(write);
 };
 
 await decompress();
