e26e458e24eb83b52e2f72b17b4654d89529b4a8
Oleksandr Parkhomenko <119164331+proff1991@users.noreply.github.com>
2023-06-19 12:23:42 +0300

    read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,26 @@
-const read = async () => {
-    // Write your code here 
-};
+import { readFile as fsReadFile } from 'fs/promises'
+import { join as pathJoin, dirname as pathDirname } from 'path'
+import { fileURLToPath } from 'node:url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = pathDirname(__filename)
 
-await read();
+const currentFileName = 'fileToRead.txt'
+const errorMessage = 'FS operation failed'
+const currentFilePath = pathJoin(__dirname, currentFileName)
+
+const read = async () => {
+
+    try{
+
+        const insideContent = await fsReadFile(currentFilePath, 'utf-8')
+        console.log(insideContent)
+
+    }catch{
+
+        throw new Error(errorMessage)
+
+    }
+
+}
+
+await read()
