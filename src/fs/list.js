a2d5078b2bbc5c604e674b1418f1559b5fde69d9
MasoNord <112730325+MasoNord@users.noreply.github.com>
2023-06-19 13:59:39 +0300

    Update list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import fs from 'fs';
+
+const fileDir = "./src/fs/files";
+var result = [];
+
 const list = async () => {
-    // Write your code here 
+    if (!fs.existsSync(fileDir)) {
+        throw new Error ("FS operation failed");
+    }
+
+    fs.readdir(fileDir, (err, files) => {      
+        if (err) throw err;
+        files.forEach(file => {
+            console.log(file);
+        });
+    });
+
 };
 
 await list();
