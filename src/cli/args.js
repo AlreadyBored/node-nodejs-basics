79a8082eb7c8f76214812ddd7f9ba2834ac992eb
UTYFjs <utyf-90@mail.ru>
2023-06-19 13:46:27 +0300

    fix: add correct resolve to cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,8 +1,13 @@
 const parseArgs = () => {
     const argv = process.argv.slice(2);
-    
-    argv.forEach((item, index) => {console.log(`${item} is ${argv[index+1] || ''}`)})
 
+    const argsArr = argv.reduce((resArr, item, index, arr) => { if (item.startsWith('--')) {
+      return [...resArr, `${item.slice(2)} is ${arr[index + 1]}`];
+    } else {
+      return resArr;
+    }}, []);
+    const result = argsArr.join(', ');
+    console.log( result)
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,7 +1,17 @@
+
+
 const parseEnv = () => {
     const envVars = process.env;
-    const rssEnv = Object.keys(envVars).filter((key) => key.startsWith('RSS_'))
-    rssEnv.forEach((env)=> console.log(`${env} = ${envVars[env]}`) )
+    //process.env.RSS_12 = 12;
+    //process.env.RSS_ff = 'fff';
+    const rss = [];
+    for(let key in envVars){
+        if(key.startsWith('RSS_')){
+            rss.push(`${key}=${envVars[key]}`)
+        }
+    }
+    const result = rss.join('; ');
+    console.log(result);
     };
 
 parseEnv();
