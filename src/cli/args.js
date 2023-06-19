ddadb521c764a0870dec376fad671ffefdd4c19c
Oleksandr Parkhomenko <119164331+proff1991@users.noreply.github.com>
2023-06-19 12:20:22 +0300

    args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,24 @@
-const parseArgs = () => {
-    // Write your code here 
-};
+const prefix = '--';
 
-parseArgs();
+const parseArgs = () => {
+
+    const argsAcc = process.argv.slice(2).reduce( (acc,value,index,array) =>{
+
+        if(value.startsWith(prefix)){
+
+            const formattedProp = value.replace(prefix, '') + " is " + array[index + 1]
+            return acc ? acc + ", " + formattedProp : formattedProp
+
+        } else {
+
+            return acc
+
+        }
+
+    },'')
+
+    console.log(argsAcc)
+
+}
+
+parseArgs()
