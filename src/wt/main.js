7b7a4bfd4af29466d4d94c8f60b705f9d2a701bf
oscarishe <a.polityko@mail.ru>
2023-06-19 20:03:46 +0300

    feat: complete wt task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,26 @@
+import { Worker } from "worker_threads";
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import os from 'os';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const performCalculations = async () => {
-    // Write your code here
+    const cpuNumber = os.cpus().length;
+    const execFile = path.join(__dirname, "worker.js");
+    let n = 10;
+    const calculations = [];
+
+    for (let i = 0; i < cpuNumber; i++) {
+        const singleCalculation = new Promise((resolve, reject) => {
+            const worker = new Worker(execFile, { workerData: n++ });
+            worker.on("message", (result) => resolve({ status: "resolved", data: result }));
+            worker.on("error", (error) => reject({ status: "error", data: error }));
+        });
+        calculations.push(singleCalculation);
+    }
+
+    Promise.all(calculations).then((data) => console.log(data)).catch((e) => console.error(e));
 };
 
 await performCalculations();
