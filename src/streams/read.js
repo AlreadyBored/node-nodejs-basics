61a577f16c53213ee380f68433dddaa044187c63
MasoNord <112730325+MasoNord@users.noreply.github.com>
2023-06-19 14:03:59 +0300

    Update read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,22 @@
-const read = async () => {
-    // Write your code here 
+import  {Readable} from 'stream'
+import process from 'process'
+import fs from 'fs'
+
+const path = "./src/streams/files/fileToRead.txt";
+
+async function * generate() {
+    
+    if (!fs.existsSync(path))
+        throw new Error("The path does not exist!!!");
+    
+    yield fs.readFileSync(path) + "";
+}
+
+const read = async () => { 
+    const readable = Readable.from(generate());
+    readable.on("data", (chunk) => {
+        process.stdout.write(chunk);
+    })
 };
 
 await read();
