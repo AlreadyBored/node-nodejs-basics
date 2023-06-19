32298507f550c43491a8ed98d069f93387608f27
Firsova Liubov <firsova.lyub@gmail.com>
2023-06-19 19:50:14 +0300

    feat: add a function to hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,34 @@
+import { readFile } from "node:fs/promises";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+import { access, constants } from "node:fs/promises";
+
+import { createHmac } from "node:crypto";
+
+const fileName = fileURLToPath(import.meta.url);
+const __dirname = dirname(fileName);
+
+const filePath = join(__dirname, "files", "fileToCalculateHashFor.txt");
+
+const secret = "lou";
+let dataToHash = null;
+
 const calculateHash = async () => {
-    // Write your code here 
+  access(filePath, constants.F_OK) // check if file exists
+    .then(() => {
+      readFile(filePath) // read the file
+        .then((data) => {
+          dataToHash = data.toString();
+
+          const hash = createHmac("sha256", secret)
+            .update(dataToHash)
+            .digest("hex");
+          console.log(hash);
+        })
+        .catch((err) => console.log(err.message));
+    })
+    .catch((err) => console.log(err.message));
 };
 
 await calculateHash();
