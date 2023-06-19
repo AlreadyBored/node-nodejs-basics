eef826e2e2eccd69c2b2f93a9f590a64aa7102ee
Firsova Liubov <firsova.lyub@gmail.com>
2023-06-19 23:40:13 +0300

    feat: add solved tasks of zlib

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+import { createReadStream, createWriteStream } from "node:fs";
+import { createUnzip } from "node:zlib";
+
+const fileName = fileURLToPath(import.meta.url);
+const __dirname = dirname(fileName);
+
+const filePath = join(__dirname, "files", "fileToCompress.txt");
+
 const decompress = async () => {
-    // Write your code here 
+  const rs = createReadStream(`${filePath}.gz`);
+  rs.pipe(createUnzip())
+    .pipe(createWriteStream(fileName))
+    .on("finish", () => {
+      console.log("decompression process is finished");
+    });
 };
 
 await decompress();
