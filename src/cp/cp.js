233aa1039189b9e0ba4b40375d3f73a5b897f39d
Klychnikov Alexandr <klychnikov75@mail.ru>
2023-06-19 14:08:01 +0300

    feat: implement spawnChildProcess

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,13 @@
+import { spawn } from 'child_process';
+import path from 'path';
+import url from 'url'
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const __filename = url.fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const file = path.join(__dirname, 'files', 'script.js');
+    const childProcess = spawn('node', [file, ...args], { stdio: 'inherit' });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( [0, 1, 2, 3, 'one more'] );
