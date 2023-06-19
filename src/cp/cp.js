be2bf04088be06eeed2c481939c20c210185c1fe
Oleksandr Parkhomenko <119164331+proff1991@users.noreply.github.com>
2023-06-19 12:21:50 +0300

    cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,19 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+import { fork as child_processFork } from 'child_process'
+import { join as pathJoin, dirname as pathDirname } from 'path'
+import { fileURLToPath } from 'node:url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = pathDirname(__filename)
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+const scriptFile = 'script.js'
+const scriptFolder = 'files'
+const scriptFullPath = pathJoin(__dirname, scriptFolder, scriptFile)
+
+const spawnChildProcess = async (args) => {
+
+    const forkChildProcess = child_processFork(scriptFullPath, args, {silent: true})
+    process.stdin.pipe(forkChildProcess.stdin)
+    forkChildProcess.stdout.pipe(process.stdout)
+
+}
+
+spawnChildProcess()
