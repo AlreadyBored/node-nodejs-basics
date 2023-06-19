8a3c42a0775cb4eb33950a951752ff0c1a3349fe
AlexGradus <countr@mail.ru>
2023-06-20 01:29:01 +0300

    feat:add solution

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,41 @@
+import { Worker } from "worker_threads";
+import os from "os";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const adjustWorkerThread = (n) => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const sourcePath = path.join(__dirname, "worker.js");
+  return new Promise((resolve) => {
+    const worker = new Worker(sourcePath, { workerData: n });
+
+    worker.on("message", (result) => {
+      resolve({ status: "resolved", data: result });
+    });
+
+    worker.on("error", () => {
+      resolve({ status: "error", data: null });
+    });
+  });
+};
+
 const performCalculations = async () => {
-    // Write your code here
+  const cpuCores = os.availableParallelism();
+  const promises = [];
+
+  for (let i = 0; i < cpuCores; i++) {
+    const n = 10 + i;
+    const promise = adjustWorkerThread(n);
+    promises.push(promise);
+  }
+
+  try {
+    const results = await Promise.all(promises);
+    console.log(results);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 await performCalculations();
