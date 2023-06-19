c1a032fc8ebf757fbfafab266b620fa34b1831e6
andre353 <eva595eva@gmail.com>
2023-06-20 00:35:01 +0300

    feat: add stream/transform.js solution

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
+import { Transform } from 'stream';
+
+const reverseText = new Transform({
+    async transform(chunk) {
+        const incomingText = chunk.toString();
+        const outgoingText = incomingText.split('').reverse().join('');
+        process.stdout.write(outgoingText);
+        return Promise.resolve();
+      },
+    });
+    
 const transform = async () => {
-    // Write your code here 
+    process.stdin.pipe(reverseText);
 };
 
 await transform();
