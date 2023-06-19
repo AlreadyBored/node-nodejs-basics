f331fd347dd590d8e65c78b0b48af5ec653a4044
Volodymyr Sych <volodymyr_sych@epam.com>
2023-06-19 08:37:49 +0300

    func list added

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,27 @@
+import { promises as fs } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+	const sourceFolder = join(__dirname, 'files');
+
+	try {
+		await fs.access(sourceFolder); // Check if source folder exists
+	} catch (error) {
+		console.error('FS operation failed');
+		return; // Return early to avoid further execution
+	}
+
+	try {
+		const files = await fs.readdir(sourceFolder);
+		const filenames = files.map((file) => file);
+		console.log(filenames);
+	} catch (error) {
+		console.error('FS operation failed');
+	}
 };
 
 await list();
