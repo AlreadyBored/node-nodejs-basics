eb809f05dcd6a5ca54f9d4aef834a0af39ea2209
vladislav <unbeliever1806@gmail.com>
2023-06-19 16:46:38 +0400

    feat: fs functions

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,12 @@
+import fs from 'fs'
+
+const path = 'src/fs/files/fresh.txt';
+const content = 'I am fresh and young';
+
 const create = async () => {
-    // Write your code here 
+    if (fs.existsSync(path)) throw new Error('FS operation failed')
+
+    await fs.writeFile(path, content, { flag: 'wx' }, () => {})
 };
 
 await create();
--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,15 @@
+import fs from 'node:fs/promises'
+import { existsSync } from 'node:fs'
+
+const FILES_FOLDER = 'src/fs/files'
+
+const isFileFolderExists = existsSync(FILES_FOLDER)
+
 const list = async () => {
-    // Write your code here 
+    if (!isFileFolderExists) throw new Error('FS operation failed')
+
+    const files = await fs.readdir(FILES_FOLDER)
+    console.log(files)
 };
 
 await list();
