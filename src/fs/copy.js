572245e23e25c8087304c36627acdabbc61f0331
Oleksandr Parkhomenko <119164331+proff1991@users.noreply.github.com>
2023-06-19 12:22:30 +0300

    copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,49 @@
-const copy = async () => {
-    // Write your code here 
-};
+import { stat as fsStat, copyFile as fsCopyFile, mkdir as fsMkdir, readdir } from 'fs/promises'
+import { join as pathJoin, dirname as pathDirname  } from 'path'
+import { fileURLToPath } from 'node:url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = pathDirname(__filename)
 
-await copy();
+
+const startFolder = 'files'
+const addition = '_copy'
+const newFolder = `${startFolder}${addition}`
+const startFolderPath = pathJoin(__dirname, startFolder)
+const newFolderPath = pathJoin(__dirname, newFolder)
+const errorMessage = 'FS operation failed'
+
+
+async function exists(path){
+
+    try{
+
+        await fsStat(path)
+        return true
+
+    }catch{
+
+        return false
+
+    }
+
+}
+
+
+
+const copy = async () => {
+
+    if( !!(await exists(startFolderPath)) && !(await exists(newFolderPath))){
+
+        const [filesInStartFolder] = await Promise.all([readdir(startFolderPath), fsMkdir(newFolderPath)])
+        const promises = filesInStartFolder.map( file => fsCopyFile(pathJoin(startFolderPath, file), pathJoin(newFolderPath, file)))
+        await Promise.all(promises)
+
+    }else{
+
+      throw new Error(errorMessage)
+
+    }
+
+}
+
+await copy()
