4052a7b3d9ae278884ec237b7c22f96406c9f55e
Liza <lizavetayalak2000@gmail.com>
2023-06-19 17:51:04 +0200

    feat: implement cp.js function, fix worker.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,13 @@
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+import { spawn } from 'node:child_process'
+
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const directory = path.dirname(fileURLToPath(import.meta.url))
+    const file = path.join(directory, 'files', 'script.js')
+    const child = spawn('node',[file, ...args], { stdio: ['inherit', 'inherit', 'inherit', 'ipc'] })
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['fgh', 'rtyu']);
