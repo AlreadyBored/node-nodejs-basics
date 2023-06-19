d2c28fcb1343fc5b2af0f35e338d873551b61cd1
andre353 <eva595eva@gmail.com>
2023-06-20 02:00:24 +0300

    fix: correct file paths

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,19 +1,24 @@
 import { promises as fs } from 'fs';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
 
-const initialFile = './src/fs/files/wrongFilename.txt';
-const finalFileName = './src/fs/files/properFilename.md';
+const initialFile = './files/wrongFilename.txt';
+const finalFileName = './files/properFilename.md';
 const messageOfError =  'FS operation failed';
 
 const rename = async (file, ffn, errorMessage) => {
 
     const err = new Error(errorMessage);
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const absoluteInitialFilePath = path.join(__dirname, file);
+    const absoluteFinalFilePath = path.join(__dirname, ffn);
 
     try {
-        const exists  = !!(await fs.stat(ffn).catch(() => null));
+        const exists  = !!(await fs.stat(absoluteFinalFilePath).catch(() => null));
         if (exists) {
             throw err;
         }
-        await fs.rename(file, ffn);
+        await fs.rename(absoluteInitialFilePath, absoluteFinalFilePath);
     }   catch (error)  {
             if (error.code === 'ENOENT') {
                 throw err;            
