fa8fa00e37161025b2a4efa0e34f3faf7a19bd6c
Artyom Loiko <artyom@Artyoms-MacBook-Air.local>
2023-06-20 00:06:34 +0300

    add: wt and cp done

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,15 @@
+import { spawn } from 'child_process'
+import path from 'path'
+import * as url from 'url'
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const filePath = path.join(__dirname, 'files/script.js')
+
+    spawn('node', [filePath, ...args], { stdio: ['inherit', 'inherit', 'inherit', 'ipc'] })
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( [2, 'text', '5']);
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,32 @@
+import os from 'os'
+import { Worker } from 'worker_threads';
+
+const createWorker = (n) => {
+    return new Promise((resolve, reject) => {
+        const workerPath = new URL('worker.js', import.meta.url)
+        const worker = new Worker(workerPath, {
+            workerData: { n },
+        });
+        worker.on("message", (data) => {
+            resolve({ status: 'resolved', data });
+        });
+        worker.on("error", (msg) => {
+            reject({ status: 'error', data: null });
+        });
+    });
+}
+
 const performCalculations = async () => {
-    // Write your code here
+    const cores = os.cpus().length
+    const workers = [];
+
+    for (let i = 0; i < cores; i++) {
+        const n = i + 10
+        workers.push(createWorker(n));
+    }
+
+    const results = await Promise.allSettled(workers).then(r => r.map(i => i.value || i.reason));
+    console.log(results)
 };
 
 await performCalculations();
