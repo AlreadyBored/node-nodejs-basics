b42e33dd5e59731805ee565200d639925348b2ff
andre353 <eva595eva@gmail.com>
2023-06-19 21:04:32 +0300

    feat: add list.js solution

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,27 @@
-const list = async () => {
-    // Write your code here 
+import { promises as fs } from 'fs';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const folder = './files';
+const messageOfError =  'FS operation failed';
+
+const list = async (dirName, errorMessage) => {
+    
+    const errMessage = new Error(errorMessage);
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const absoluteFolderPath = path.join(__dirname, dirName);
+
+    try {
+        const fileList = await fs.readdir(absoluteFolderPath, { withFileTypes: true });
+        const filteredList = fileList.filter(item => !item.isDirectory()).map(item => item.name);
+        console.log(filteredList);
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            throw errMessage;    
+        } else {
+            console.error(err.message);
+        }
+    }
 };
 
-await list();
+await list(folder, messageOfError);
