6c2b505433122a0e024999f6270bc3dd3f6076a0
Lizaveta Petrova macOS <goldglaider@gmail.com>
2023-06-19 22:36:32 +0300

    feat: add solution for env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
+import { argv } from 'node:process';
+
 const parseEnv = () => {
-    // Write your code here 
+  const variables = process.env;
+  const defaultStartKey = 'RSS_';
+  const res = Object.entries(variables).filter(key => key[0].startsWith(defaultStartKey))
+  const resAfterJoin = res.map(key =>key.join('='));
+  console.log(resAfterJoin.join('; '));
 };
 
 parseEnv();
