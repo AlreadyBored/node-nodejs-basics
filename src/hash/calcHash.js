cb8170612398a8a24759d5d45c86c99559e5a1b8
Madiyar Kenzhegulov <madiyar.kenzhegulov@gmail.com>
2023-06-19 06:55:01 +0600

    feat: complete hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { createHash } from "node:crypto";
+import { readFile } from "node:fs/promises";
+import path from "node:path";
+import url from "node:url";
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
 const calculateHash = async () => {
-    // Write your code here 
+  const sha256 = createHash("sha256");
+  const content = await readFile(
+    path.resolve(__dirname, "./files/fileToCalculateHashFor.txt")
+  );
+
+  console.log(sha256.update(content).digest("hex"));
 };
 
 await calculateHash();
