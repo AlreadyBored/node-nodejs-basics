d2e0830cac6e4639059d19567b4d9e84b035d257
MasoNord <112730325+MasoNord@users.noreply.github.com>
2023-06-19 14:00:26 +0300

    Update rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+
+const filePath = "./src/fs/files/";
+
 const rename = async () => {
-    // Write your code here 
+    if (!fs.existsSync(filePath + "wrongFilename.txt") || fs.existsSync(filePath + "properFilename.md")) {
+        throw new Error("FS operation failed");
+    }else {
+        fs.rename(filePath + "wrongFilename.txt", 
+                  filePath + "properFilename.md",
+                  function(err) {
+                    if (err) throw err;
+                    console.log("Done!!!");
+                });
+    }
 };
 
 await rename();
