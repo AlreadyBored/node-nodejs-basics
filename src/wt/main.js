db16cb3c91af07284fc7644cd32694e625a5bebe
Maria Apanovich <maria.apanovich32@gmail.com>
2023-06-19 21:57:18 +0200

    Worker Threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,23 @@
+import { Worker } from "worker_threads";
+import os from "os";
+
+const url = import.meta.url;
+const numOfCpus = os.cpus().length;
+const path = new URL("./worker.js", url);
+
 const performCalculations = async () => {
-    // Write your code here
+  const start = (workerData) =>
+    new Promise((resolve) => {
+      const worker = new Worker(path, { workerData });
+
+      worker.on("message", (data) => resolve({ status: "resolved", data }));
+      worker.on("error", () => resolve({ status: "error", data: null }));
+    });
+
+  const result = await Promise.all(
+    [...new Array(numOfCpus)].map((_, i) => start(i + 10))
+  );
+  console.log(result);
 };
 
 await performCalculations();
