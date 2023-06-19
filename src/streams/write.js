fc144cf830cdaa719a8f2ba52f2e5436d3b4f388
Pavel Dubrovskiy <paveldubrovskiyit@gmail.com>
2023-06-20 01:20:03 +0300

    feat: add solution for write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,23 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { createWriteStream } from 'node:fs';
+import process from 'node:process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+    const path = join(__dirname, 'files', 'fileToWrite.txt');
+
+    try {
+        const writeStream = createWriteStream(path);
+        
+        process.stdin.on('data', (chunk) => {
+            writeStream.write(chunk.toString());
+        })
+    } catch (err) {
+        throw err;
+    }
 };
 
 await write();
