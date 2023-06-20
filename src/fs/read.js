7c9d8ba1b59772176a464d6ae449e9111014a875
sergei404 <30853345+sergei404@users.noreply.github.com>
2023-06-20 13:41:25 +0300

    Update read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,11 +1,6 @@
 import fs from "fs";
 
 const read = async () => {
-    fs.stat('./src/fs/files/fileToRead.txt', (err, stats) => {
-        if (stats) {
-          throw Error("FS operation failed");
-        }
-      });
     fs.createReadStream('./src/fs/files/fileToRead.txt').setEncoding('utf8').pipe(process.stdout)
 };
 
