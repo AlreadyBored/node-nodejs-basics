f1f1d7e9e5c5b456055c80d6ae9bf0376ff41eae
Viacheslav Tyshchuk <slava1000uk@gmail.com>
2023-06-20 06:07:08 +0200

    feat: add solution to list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import { readdir } from 'node:fs/promises';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const fileName = fileURLToPath(import.meta.url);
+const __dirname = dirname(fileName);
+
+const srcFolder = join(__dirname, 'files');
+
 const list = async () => {
-    // Write your code here 
+    try {
+        const files = await readdir(srcFolder);
+
+        files.forEach(file => console.log(file));
+    } catch (err) {
+        console.error('FS operation failed');
+    }
 };
 
 await list();
