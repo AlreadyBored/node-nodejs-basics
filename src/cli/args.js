36cf72c6e232c931d0805d1778f7fc9dffeb1796
Viacheslav Tyshchuk <slava1000uk@gmail.com>
2023-06-21 06:19:50 +0200

    feat: add solution to args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
+import process from 'node:process';
+
 const parseArgs = () => {
-    // Write your code here 
+    process.argv.
+        forEach((item, index, array) => {
+            if(item.startsWith('--')) {
+                console.log(`${item.slice(2)} is ${array[index+1]}`);
+            }
+        });
 };
 
 parseArgs();
