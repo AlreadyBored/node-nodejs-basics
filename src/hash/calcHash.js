7a243bc54d1e4749b2c0d0c64ddc548f946e9e4e
Viacheslav Tyshchuk <slava1000uk@gmail.com>
2023-06-21 08:52:13 +0200

    feat: add solution for calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { readFile } from 'node:fs/promises';
+import { createHash } from 'node:crypto';
+
+const fileName = fileURLToPath(import.meta.url);
+const __dirname = dirname(fileName);
+
+const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+    try {
+        const contents = await readFile(filePath);
+
+        let hash = createHash('SHA256').update(contents);
+        console.log(hash.digest('hex'));
+       
+    } catch (err) {
+        throw err;
+    }
 };
 
 await calculateHash();
