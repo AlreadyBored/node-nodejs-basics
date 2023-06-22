cdce4b9b8f047a76e334ad174f931d8b042dbf2c
Dima Usov <dmitry.usov@yahoo.com>
2023-06-22 22:42:21 +0400

    feat: fs:create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+const file = path.resolve('src', 'fs', 'files', 'fresh.txt');
+const text = 'I am fresh and young';
+
 const create = async () => {
-    // Write your code here 
+    // Write your code here
+    try {
+        await fs.writeFile(file, text, {flag: 'wx'});
+    }
+    catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await create();
