45dbb39a17ce0ebe8b6e8e4e335323879ac672b1
Dima Usov <dmitry.usov@yahoo.com>
2023-06-23 13:21:41 +0400

    feat: fs:copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,17 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+const pathSrc = path.resolve('src', 'fs', 'files');
+const pathDst = path.resolve('src', 'fs', 'files_copy');
+
 const copy = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    await fs.cp(pathSrc, pathDst, {recursive: true, force: false, errorOnExist:true});
+  }
+  catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await copy();
