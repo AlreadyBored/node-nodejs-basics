d569af651784e4d3c186881f0781f461789700e1
Volodymyr Sych <volodymyr_sych@epam.com>
2023-06-23 12:23:38 +0300

    function compress from zlib added

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,38 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+// get the current file's path and directory using 'import.meta.url' and 'fileURLToPath'
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Function to compress the file
 const compress = async () => {
-    // Write your code here 
+	// resolve the input and output file paths
+	const inputFilePath = resolve(__dirname, 'files/fileToCompress.txt');
+	const outputFilePath = resolve(__dirname, 'archive.gz');
+
+	// create read and write streams for file operations
+	const readStream = createReadStream(inputFilePath);
+	const writeStream = createWriteStream(outputFilePath);
+
+	// create a gzip transform stream
+	const gzip = createGzip();
+
+	// pipe the read stream through the gzip transform stream and then to the write stream
+	readStream.pipe(gzip).pipe(writeStream);
+
+	// wait for the write stream to finish writing
+	await new Promise((resolve, reject) => {
+		writeStream.on('finish', resolve);
+		writeStream.on('error', reject);
+	});
+
+	console.log('File has been compressed!');
 };
+
+// to che4k the cod I entered in the terminal: node --experimental-modules compress.js
 
 await compress();
