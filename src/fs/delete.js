677292df436c2ddd2b89664921ced2d0e9ca3b61
Dima Usov <dmitry.usov@yahoo.com>
2023-06-23 13:55:03 +0400

    feat: fs:delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,16 @@
+import fs from "fs/promises";
+import path from "path";
+
+const pathToDelete = path.resolve("src", "fs", "files", "fileToRemove.txt");
+
 const remove = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    fs.unlink(pathToDelete);
+  } 
+  catch {
+    throw new Error("FS operation failed");
+  }
 };
 
 await remove();
