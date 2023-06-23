bf019cc383d6e45d08427096635623c1f3010a7d
Dima Usov <dmitry.usov@yahoo.com>
2023-06-23 13:41:05 +0400

    feat: fs:rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,17 @@
+import fs from "fs/promises";
+import path from "path";
+
+const pathSrc = path.resolve("src", "fs", "files", "wrongFilename.txt");
+const pathDst = path.resolve("src", "fs", "files", "properFilename.md");
+
 const rename = async () => {
-    // Write your code here 
+  // Write your code here
+  try {  
+    fs.rename(pathSrc, pathDst);
+  }
+  catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await rename();
