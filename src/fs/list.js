b6ccba3ada23c26241b105a26ed6e1f335c156d0
Dima Usov <dmitry.usov@yahoo.com>
2023-06-23 14:13:55 +0400

    feat: fs:list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
+import fs from "fs/promises";
+import path from "path";
+
+const pathToList = path.resolve("src", "fs", "files");
+
 const list = async () => {
-    // Write your code here 
+  // Write your code here
+   try {
+    const fileList = await fs.readdir(pathToList);
+    console.log(fileList)
+   } catch(err) {
+    console.log(err);
+    throw new Error("FS operation failed");
+   }
 };
 
 await list();
