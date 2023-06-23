480cacdfe6957c78c9265ba8a90f022ef17e3509
Volodymyr Sych <volodymyr_sych@epam.com>
2023-06-23 10:37:33 +0300

    function transform text in streams added

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,38 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+	const reverseStream = new Transform({
+		async transform(chunk, encoding) {
+			const reversedText = chunk.toString().split('').reverse().join('');
+			this.push(reversedText); // directly push the reversed text to the stream
+		},
+	});
+
+	// Write the data from reverseStream to process.stdout
+	reverseStream.on('data', (data) => {
+		process.stdout.write(data);
+	});
+
+	// Handle error event on process.stdout
+	process.stdout.on('error', (err) => {
+		if (err.code === 'EPIPE') {
+			// Ignore EPIPE error
+			return;
+		}
+		// Handle other errors
+		console.error('Error writing to process.stdout:', err);
+	});
+
+	// Read the input text from command line argument
+	const inputText = process.argv[2];
+
+	// Write the input text to the reverse stream
+	reverseStream.write(inputText);
+
+	// End the reverse stream
+	reverseStream.end();
 };
+
+// I use the command in terminal to check the code: node --experimental-modules transform.js "To be or not to be"
 
 await transform();
