19d39775794e35ca355ce252aa63d204afb59fbc
Dima Usov <dmitry.usov@yahoo.com>
2023-06-23 21:54:39 +0400

    feat: cli:args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-    // Write your code here 
+    // Write your code here
+    const args = process.argv.slice(2);
+    const parsedArgs = {};
+
+    while(args.length > 0) {
+        const argName = args.shift().slice(2);
+        const argValue = args.shift();
+        parsedArgs[argName] = argValue;
+    }
+    const content = Object.entries(parsedArgs)
+        .map(([key, value]) => `${key} is ${value}`)
+        .join(", ");
+
+    console.log(content);
 };
 
 parseArgs();
