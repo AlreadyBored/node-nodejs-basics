cf6e0e5e849502ab51dd4109793da0907f2b8f1c
Dima Usov <dmitry.usov@yahoo.com>
2023-06-23 15:04:34 +0400

    feat: cli:env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+    // Write your code here
+    const content = Object.keys(process.env)
+        .filter(item => item.startsWith("RSS_"))
+        .map(rssVar => `${rssVar}=${process.env[rssVar]}`)
+        .join("; ");
+    console.log(content); 
 };
 
 parseEnv();
