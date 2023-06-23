50c1c22bfc2d24cd1dca861d8b65d1f52943e3db
Dima Usov <dmitry.usov@yahoo.com>
2023-06-23 14:31:54 +0400

    feat: fs:read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,15 @@
+import fs from "fs/promises";
+import path from "path";
+
+const pathToFile = path.resolve("src", "fs", "files", "fileToRead.txt");
 const read = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    const content = await fs.readFile(pathToFile, {encoding: 'utf-8'});
+    console.log(content);
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
 
 await read();
