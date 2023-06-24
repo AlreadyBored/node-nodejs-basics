2b4ae08ed15cc2ca0d53641913a98f6cc7da39c0
Volodymyr Sych <volodymyr_sych@epam.com>
2023-06-24 09:00:44 +0300

    extend func - worker-data from main thread

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,30 @@
+import { Worker } from 'worker_threads';
+
 const performCalculations = async () => {
-    // Write your code here
+	const worker = new Worker('./worker.js');
+
+	const resultPromise = new Promise((resolve, reject) => {
+		worker.on('message', (result) => {
+			resolve(result); // resolve the promise with the result received from the worker thread
+		});
+
+		worker.on('error', (error) => {
+			reject(error); // reject the promise with the error that occurred in the worker thread
+		});
+
+		worker.on('exit', (code) => {
+			if (code !== 0) {
+				reject(new Error(`Worker stopped with exit code ${code}`)); // reject the promise if the worker thread exited with a non-zero code
+			}
+		});
+	});
+
+	try {
+		const result = await resultPromise;
+		console.log('Result:', result); // log the result received from the worker thread
+	} catch (error) {
+		console.error('Error:', error); // log any errors that occurred in the worker thread
+	}
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,13 @@
+import { parentPort } from 'worker_threads'; //  to work with data received from the main thread and send the result back, I used this module in Node.js
+
 // n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+const nthFibonacci = (n) =>
+	n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+	// This function sends result of nthFibonacci computations to main thread
+	const result = nthFibonacci(10); // let calculate the 10th Fibonacci number
+	parentPort.postMessage(result); // send the result to the main thread
 };
 
-sendResult();
+sendResult(); // invoke the sendResult function to send the result to the main thread
