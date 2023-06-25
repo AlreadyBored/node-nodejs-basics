56440d8f0e0b0365c62b239e6913e52a3817e7ff
Dima Usov <dmitry.usov@yahoo.com>
2023-06-25 17:19:21 +0400

    feat: streams:write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,11 @@
+import { createWriteStream } from "fs";
+import path from "path";
+
+const pathToFile = path.resolve("src", "streams", "files", "fileToWrite.txt");
+const writeStream = createWriteStream(pathToFile);
 const write = async () => {
-    // Write your code here 
+  // Write your code here
+  process.stdin.pipe(writeStream);
 };
 
 await write();
