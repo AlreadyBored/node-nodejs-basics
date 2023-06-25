44da8cf44ccfd5d0a86d77595c3b26639d5d6fa8
Dima Usov <dmitry.usov@yahoo.com>
2023-06-25 18:10:13 +0400

    feat: zip:compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import { createReadStream, createWriteStream } from "fs";
+import { createGzip } from "zlib";
+import path from "path";
+
+const pathToFile = path.resolve("src", "zip", "files", "fileToCompress.txt");
+const pathToZip = path.resolve("src", "zip", "files", "compress.gz");
+
+const streamIn = createReadStream(pathToFile);
+const streamZip = createGzip();
+const streamOut = createWriteStream(pathToZip);
+
 const compress = async () => {
-    // Write your code here 
+  // Write your code here
+  streamIn.pipe(streamZip).pipe(streamOut);
 };
 
 await compress();
