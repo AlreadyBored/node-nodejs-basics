422bf32b63e6fc8ee47ad2fd444a125dffeb2cb1
Volodymyr Sych <volodymyr_sych@epam.com>
2023-06-26 12:57:19 +0300

    func list with simpleVar

--- src/fs/list.js
+++ src/fs/list.js
@@ -8,20 +8,29 @@ const __dirname = dirname(__filename);
 const list = async () => {
 	const sourceFolder = join(__dirname, 'files');
 
+	// simple variant
 	try {
-		await fs.access(sourceFolder); // Check if source folder exists
-	} catch (error) {
-		console.error('FS operation failed');
-		return; // Return early to avoid further execution
+		const fileList = await fs.readdir(sourceFolder);
+		console.log(fileList);
+	} catch {
+		throw new Error('FS operation failed');
 	}
 
-	try {
-		const files = await fs.readdir(sourceFolder);
-		const filenames = files.map((file) => file);
-		console.log(filenames);
-	} catch (error) {
-		console.error('FS operation failed');
-	}
+	// // more complicated variant
+	// try {
+	// 	await fs.access(sourceFolder); // Check if source folder exists
+	// } catch (error) {
+	// 	console.error('FS operation failed');
+	// 	return; // Return early to avoid further execution
+	// }
+
+	// try {
+	// 	const files = await fs.readdir(sourceFolder);
+	// 	const filenames = files.map((file) => file);
+	// 	console.log(filenames);
+	// } catch (error) {
+	// 	console.error('FS operation failed');
+	// }
 };
 
 await list();
