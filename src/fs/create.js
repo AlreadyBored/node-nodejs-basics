53d16eb2ffe7ed5d6811b815d3b8f38754993559
Volodymyr Sych <volodymyr_sych@epam.com>
2023-06-26 10:16:33 +0300

    func create with simpleVar

--- src/fs/create.js
+++ src/fs/create.js
@@ -9,24 +9,34 @@ const create = async () => {
 	const filePath = join(__dirname, 'files', 'fresh.txt');
 	const fileContent = 'I am fresh and young';
 
+	// simple variant
 	try {
-		// check with the constants.F_OK-flag if the file exists
-		await fs.access(filePath, constants.F_OK);
+		// 'wx'- open file for writing. The file is created (if it does not exist) or truncated (if it exists). But fails if the path exists.
+		await fs.writeFile(filePath, fileContent, { flag: 'wx' });
+		console.log('File created successfully');
+	} catch {
 		throw new Error('FS operation failed');
-	} catch (error) {
-		// if the file exists, it throws an error with the code 'ENOENT'
-		if (error.code === 'ENOENT') {
-			try {
-				await fs.writeFile(filePath, fileContent);
-				console.log('File created successfully');
-			} catch (error) {
-				// rethrown if any other error occurs during file creation
-				console.error('FS operation failed');
-			}
-		} else {
-			console.error('FS operation failed');
-		}
 	}
+
+	//	// more complicated variant
+	// 	try {
+	// 		// check with the constants.F_OK-flag if the file exists
+	// 		await fs.access(filePath, constants.F_OK);
+	// 		throw new Error('FS operation failed');
+	// 	} catch (error) {
+	// 		// if the file exists, it throws an error with the code 'ENOENT'
+	// 		if (error.code === 'ENOENT') {
+	// 			try {
+	// 				await fs.writeFile(filePath, fileContent);
+	// 				console.log('File created successfully');
+	// 			} catch (error) {
+	// 				// rethrown if any other error occurs during file creation
+	// 				console.error('FS operation failed');
+	// 			}
+	// 		} else {
+	// 			console.error('FS operation failed');
+	// 		}
+	// 	}
 };
 
 await create();
