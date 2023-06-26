12d093be0e1391583d3ccb6e1285ff43eff2d7fe
Volodymyr Sych <volodymyr_sych@epam.com>
2023-06-26 12:11:10 +0300

    func rename with simpleVar

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -9,24 +9,33 @@ const rename = async () => {
 	const sourceFile = join(__dirname, 'files', 'wrongFilename.txt');
 	const destinationFile = join(__dirname, 'files', 'properFilename.md');
 
+	// simple variant
 	try {
-		await fs.access(sourceFile); // Check if source file exists
-
-		try {
-			await fs.access(destinationFile); // Check if destination file already exists
-			throw new Error('FS operation failed');
-		} catch (error) {
-			if (error.code === 'ENOENT') {
-				// Destination file does not exist, proceed with renaming
-				await fs.rename(sourceFile, destinationFile);
-				console.log('File renamed successfully');
-			} else {
-				throw error; // Rethrow other errors
-			}
-		}
-	} catch (error) {
-		console.error('FS operation failed');
+		await fs.rename(sourceFile, destinationFile);
+		console.log('File renamed successfully');
+	} catch {
+		throw new Error('FS operation failed');
 	}
+
+	// // more complicated variant
+	// try {
+	// 	await fs.access(sourceFile); // Check if source file exists
+
+	// 	try {
+	// 		await fs.access(destinationFile); // Check if destination file already exists
+	// 		throw new Error('FS operation failed');
+	// 	} catch (error) {
+	// 		if (error.code === 'ENOENT') {
+	// 			// Destination file does not exist, proceed with renaming
+	// 			await fs.rename(sourceFile, destinationFile);
+	// 			console.log('File renamed successfully');
+	// 		} else {
+	// 			throw error; // Rethrow other errors
+	// 		}
+	// 	}
+	// } catch (error) {
+	// 	console.error('FS operation failed');
+	// }
 };
 
 rename();
