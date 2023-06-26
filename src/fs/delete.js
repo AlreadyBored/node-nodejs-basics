60cb4292aba818c268ad9c830014686189f06441
Volodymyr Sych <volodymyr_sych@epam.com>
2023-06-26 12:40:16 +0300

    func delete with simpleVar

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -8,19 +8,28 @@ const __dirname = dirname(__filename);
 const remove = async () => {
 	const filePath = join(__dirname, 'files', 'fileToRemove.txt');
 
+	// simple variant
 	try {
-		await fs.access(filePath); // Check if file exists
-	} catch (error) {
-		console.error('FS operation failed');
-		return;
+		await fs.rm(filePath); // or await fs.unlink(filePath);
+		console.log('File was deleted!');
+	} catch {
+		throw new Error('FS operation failed');
 	}
 
-	try {
-		await fs.unlink(filePath);
-		console.log('File was deleted!');
-	} catch (error) {
-		console.error('FS operation failed');
-	}
+	// // more complicated variant
+	// try {
+	// 	await fs.access(filePath); // Check if file exists
+	// } catch (error) {
+	// 	console.error('FS operation failed');
+	// 	return;
+	// }
+
+	// try {
+	// 	await fs.unlink(filePath);
+	// 	console.log('File was deleted!');
+	// } catch (error) {
+	// 	console.error('FS operation failed');
+	// }
 };
 
 await remove();
