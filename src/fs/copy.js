21561ebe2841eb416d1e7145525d271b208737d5
Volodymyr Sych <volodymyr_sych@epam.com>
2023-06-27 11:18:59 +0300

    func copyFolder added

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,26 @@
+import { promises as fs } from 'fs';
+
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+	const sourceFolder = join(__dirname, 'files');
+	const destinationFolder = join(__dirname, 'files_copy');
+
+	try {
+		await fs.cp(sourceFolder, destinationFolder, {
+			recursive: true, // copy directories recursively
+			force: false, // overwrite existing file or directory; the copy operation will ignore errors if set to false and the destination exists
+			errorOnExist: true, // when force is false, and the destination exists, throw an error
+		});
+		console.log('All files copied successfully\n');
+	} catch (err) {
+		console.log(err);
+		throw new Error('FS operation failed\n');
+	}
 };
 
 await copy();
