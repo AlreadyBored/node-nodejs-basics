6fef5a04a95060fb912b0c9dd411e1e4788e3758
Volodymyr Sych <volodymyr_sych@epam.com>
2023-06-27 11:34:09 +0300

    func rename added

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,22 @@
+import { promises as fs } from 'fs';
+
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const rename = async () => {
-    // Write your code here 
+	const sourceFile = join(__dirname, 'files', 'wrongFilename.txt');
+	const destinationFile = join(__dirname, 'files', 'properFilename.md');
+
+	try {
+		await fs.rename(sourceFile, destinationFile);
+		console.log('File renamed successfully\n');
+	} catch (err) {
+		console.log(err);
+		throw new Error('FS operation failed\n');
+	}
 };
 
 await rename();
