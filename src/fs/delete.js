c46a454b1883365b16934746db5f3de297e2b087
Volodymyr Sych <volodymyr_sych@epam.com>
2023-06-27 11:47:06 +0300

    func delete added

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,21 @@
+import { promises as fs } from 'fs';
+
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+	const filePath = join(__dirname, 'files', 'fileToRemove.txt');
+
+	try {
+		await fs.rm(filePath); // or might be await fs.unlink(filePath);
+		console.log('File was deleted!\n');
+	} catch (err) {
+		console.log(err);
+		throw new Error('FS operation failed\n');
+	}
 };
 
 await remove();
