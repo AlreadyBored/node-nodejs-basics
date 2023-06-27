bf321dd8a7623f3e9bb437cc91b2717843b06840
Volodymyr Sych <volodymyr_sych@epam.com>
2023-06-27 15:26:46 +0300

    parse command line args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,7 +1,8 @@
 const PREFIX = '--';
 
 const parseArgs = () => {
-	const argsParts = process.argv.reduce((acc, value, index, array) => {
+	console.log(process.argv); // process.argv - command line arguments starting from 3rd element
+	const argsParts = process.argv.slice(2).reduce((acc, value, index, array) => {
 		if (value.startsWith(PREFIX)) {
 			const formatedProps = `${value.replace(PREFIX, '')} is ${
 				array[index + 1]
