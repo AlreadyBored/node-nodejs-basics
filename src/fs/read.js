6647547f2f668287273f17d736b1ed90922be3b5
Volodymyr Sych <volodymyr_sych@epam.com>
2023-06-27 12:19:40 +0300

    func read added

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,21 @@
+import { promises as fs } from 'fs';
+
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+	const filePath = join(__dirname, 'files', 'fileToRead.txt');
+
+	try {
+		const fileContent = await fs.readFile(filePath, 'utf-8');
+		console.log(fileContent);
+	} catch (err) {
+		console.log(err);
+		throw new Error('FS operation failed\n');
+	}
 };
 
 await read();
