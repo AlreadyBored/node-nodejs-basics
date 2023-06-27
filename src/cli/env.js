d9d29ea76a0e45f5a7406131bfc4d8508b127818
Volodymyr Sych <volodymyr_sych@epam.com>
2023-06-27 15:06:20 +0300

    parse environment variables

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+	const specifiedEnvVars = Object.keys(process.env).filter((envVars) =>
+		envVars.startsWith('RSS_')
+	);
+
+	const stringifiedEnvVars = specifiedEnvVars
+		.map((envVar) => `${envVar} = ${process.env[envVar]}`)
+		.join('; ');
+
+	console.log('The environment variables are:\n', stringifiedEnvVars + '\n');
 };
 
 parseEnv();
