8444ef2dbb3d4fa624e63c53ef5bcf40217b327c
Volodymyr Sych <volodymyr_sych@epam.com>
2023-06-28 00:53:48 +0300

    func transformStr added

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+import { pipeline, Transform } from 'stream';
+
+const reverse = new Transform({
+	transform(chunk, _, callback) {
+		const string = chunk.toString().trim();
+		const reversedStr = string.split('').reverse().join('');
+		callback(null, reversedStr + '\n');
+	},
+});
+
+const cliInput = process.stdin;
+const cliOutput = process.stdout;
+
 const transform = async () => {
-    // Write your code here 
+	pipeline(cliInput, reverse, cliOutput, (err) => console.error(err));
 };
 
 await transform();
