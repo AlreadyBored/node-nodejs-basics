b99b190808fd4f5a8999e39673bedbae33380987
Volodymyr Sych <volodymyr_sych@epam.com>
2023-06-27 20:57:23 +0300

    func writeStreams added

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import { createWriteStream } from 'fs';
+
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filePath = join(__dirname, 'files', 'fileToWrite.txt');
+const writeStream = createWriteStream(filePath); // writable stream to the file
+
 const write = async () => {
-    // Write your code here 
+	process.stdin.pipe(writeStream);
 };
 
 await write();
