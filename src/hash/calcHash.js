292fbc2146822663cd828cb810d60c18e5fdbea2
Volodymyr Sych <volodymyr_sych@epam.com>
2023-06-27 19:02:37 +0300

    calculate SHA256 hash for file

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,27 @@
+import fs from 'node:fs/promises';
+import crypto from 'node:crypto'; // import the crypto module
+
+import { fileURLToPath } from 'node:url';
+import { join, dirname } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+	const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+	try {
+		const data = await fs.readFile(filePath);
+		const hash = crypto.createHash('sha256'); // create the hasher
+		hash.update(data); // hash the file; it also accepts a continuous stream of data like a buffer
+		const hashHex = hash.digest('hex'); // define the output format for the hash (it can be hex, binary or base64)
+
+		console.log('SHA256 hash:', hashHex);
+	} catch (err) {
+		console.error('Error calculating hash:', err);
+	}
 };
+
+// to check the code, in terminal call: node --experimental-modules ./calcHash.js
 
 await calculateHash();
