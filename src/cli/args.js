6a68f17273c363694d809d3a8276256d861f005b
Volodymyr Sych <volodymyr_sych@epam.com>
2023-06-27 14:40:43 +0300

    parse argsv in specified format

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,19 @@
+const PREFIX = '--';
+
 const parseArgs = () => {
-    // Write your code here 
+	const argsParts = process.argv.reduce((acc, value, index, array) => {
+		if (value.startsWith(PREFIX)) {
+			const formatedProps = `${value.replace(PREFIX, '')} is ${
+				array[index + 1]
+			}`;
+			return [...acc, formatedProps];
+		}
+		return acc;
+	}, []);
+
+	const stringifiedArgs = argsParts.join(', ');
+
+	console.log('The arguments are:\n', stringifiedArgs + '\n');
 };
 
 parseArgs();
