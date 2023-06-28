cbe2f7dcf69783f5b69530dd38e3412cedd5192f
Volodymyr Sych <volodymyr_sych@epam.com>
2023-06-28 09:53:50 +0300

    func compress added

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -2,33 +2,28 @@ import { createReadStream, createWriteStream } from 'fs';
 import { createGzip } from 'zlib';
 
 import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
+import { dirname, join } from 'path';
 
-// get the current file's path and directory using 'import.meta.url' and 'fileURLToPath'
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// Function to compress the file
+const inputFilePath = join(__dirname, 'files', 'fileToCompress.txt');
+const outputFilePath = join(__dirname, 'files', 'archive.gz');
+// create read and write streams for file operations
+const readStream = createReadStream(inputFilePath);
+const writeStream = createWriteStream(outputFilePath);
+
+const gzip = createGzip(); // create a gzip transform stream
+
 const compress = async () => {
-	// resolve the input and output file paths
-	const inputFilePath = resolve(__dirname, 'files/fileToCompress.txt');
-	const outputFilePath = resolve(__dirname, 'archive.gz');
-
-	// create read and write streams for file operations
-	const readStream = createReadStream(inputFilePath);
-	const writeStream = createWriteStream(outputFilePath);
-
-	// create a gzip transform stream
-	const gzip = createGzip();
-
 	// pipe the read stream through the gzip transform stream and then to the write stream
 	readStream.pipe(gzip).pipe(writeStream);
 
 	// wait for the write stream to finish writing
-	await new Promise((resolve, reject) => {
-		writeStream.on('finish', resolve);
-		writeStream.on('error', reject);
-	});
+	// await new Promise((resolve, reject) => {
+	// 	writeStream.on('finish', resolve);
+	// 	writeStream.on('error', reject);
+	// });
 
 	console.log('File has been compressed!');
 };
