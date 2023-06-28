4cf66d3c38c6da7b6c5878c6672cbe7a0df6adc3
Volodymyr Sych <volodymyr_sych@epam.com>
2023-06-28 10:32:14 +0300

    func decompress added

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,34 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from 'zlib';
+
+import { fileURLToPath } from 'url';
+import { resolve, dirname } from 'path';
+
+// get the current file's path and directory using 'import.meta.url' and 'fileURLToPath'
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// resolve the input and output file paths
+const inputFilePath = resolve(__dirname, 'files/archive.gz');
+const outputFilePath = resolve(__dirname, 'files/fileToCompress.txt');
+
+// create read and write streams for file operations
+const readStream = createReadStream(inputFilePath);
+const writeStream = createWriteStream(outputFilePath);
+
+const unzipStream = createGunzip(); // create a unzipStream transform stream
+
 const decompress = async () => {
-    // Write your code here 
+	// pipe the read stream through the unzipStream transform stream and then to the write stream
+	readStream.pipe(unzipStream).pipe(writeStream);
+
+	// wait for the write stream to finish writing
+	await new Promise((resolve, reject) => {
+		writeStream.on('finish', resolve);
+		writeStream.on('error', reject);
+	});
+
+	console.log('Decompression has been completed!');
 };
 
 await decompress();
