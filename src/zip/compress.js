a48e32eebcb3feebb70fd61f8edd31207d423188
Volodymyr Sych <volodymyr_sych@epam.com>
2023-06-28 10:26:31 +0300

    func compress added

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,26 @@
+import { createReadStream, createWriteStream } from 'fs'; // modules to create a readable\writable streams
+import { createGzip } from 'zlib';
+
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const inputFilePath = join(__dirname, 'files', 'fileToCompress.txt');
+const outputFilePath = join(__dirname, 'files', 'archive.gz');
+
+// create read and write streams for file operations
+const readStream = createReadStream(inputFilePath);
+const writeStream = createWriteStream(outputFilePath);
+
+const gzip = createGzip(); // create a gzip transform stream
+
 const compress = async () => {
-    // Write your code here 
+	// pipe the read stream through the gzip transform stream and then to the write stream
+	readStream.pipe(gzip).pipe(writeStream);
+
+	console.log('File has been compressed!');
 };
 
 await compress();
