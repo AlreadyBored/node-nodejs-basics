ac46bc5897cd3dfff837f8d571e96d03026b86b8
Jika1995 <jikulina2016@gmail.com>
2023-11-13 22:27:35 +0600

    first

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,45 @@
 const copy = async () => {
-    // Write your code here 
+
+    const fs = require('fs');
+    const path = require('path');
+
+    fs.access(`${__dirname}/files`, fs.constants.F_OK, (err) => {
+        console.log('\n> Checking if the files exists');
+
+        if (err) {
+            console.error('FS operation failed')
+        } else {
+
+            fs.access(`${__dirname}/files_copy`, fs.constants.F_OK, (err) => {
+
+                console.log('\n> Checking if the files_copy exists');
+
+                if (err) {
+                    fs.mkdir(path.join(__dirname, 'files_copy'),
+                        { recursive: true }, (err) => {
+                            if (err) {
+                                return console.error(err);
+                            }
+                            console.log('Directory Files_Copy created successfully!');
+                        });
+
+                    fs.cp(`${__dirname}/files`, `${__dirname}/files_copy`, { recursive: true }, (err) => {
+                        if (err) {
+                            console.error(err);
+                        } else {
+                            console.log('Copied successfully!')
+                        }
+                    });
+                } else {
+                    console.error('FS operation failed: Already exists')
+                }
+            });
+
+        }
+    });
+
+
 };
 
-await copy();
+// await 
+copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
 const create = async () => {
-    // Write your code here 
+    const fs = require('fs');
+    const data = 'I am fresh and young';
+
+    fs.exists(`${__dirname}/files/fresh.txt`, (exists) => {
+        if (exists) {
+            console.error('FS operation failed: File already exists');
+        } else {
+            fs.writeFile(`${__dirname}/files/fresh.txt`, data, (err) => {
+                if (err) {
+                    throw `${err}`
+                } else {
+                    console.log('File has been created successfully');
+
+                }
+            })
+        }
+    }
+    );
 };
 
-await create();
+// await
+create();
