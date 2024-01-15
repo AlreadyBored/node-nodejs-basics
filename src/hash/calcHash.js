9defa90c1758ee8676de2281b8a77b7886f9a8b3
AndreySavchenkov <sannndvik16@gmail.com>
2024-01-15 19:05:45 +0100

    feat: calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
-const calculateHash = async () => {
-    // Write your code here 
+import fs from 'fs';
+import crypto from 'crypto';
+
+const calculateHash = async (filePath) => {
+    const readStream = fs.createReadStream(filePath);
+    const hash = crypto.createHash('sha256');
+
+    readStream.on('data', (chunk) => {
+        hash.update(chunk);
+    });
+
+    readStream.on('end', () => {
+        const result = hash.digest('hex');
+        console.log(`SHA256 хеш файла ${filePath}: ${result}`);
+    });
+
+    readStream.on('error', (error) => {
+        console.error(`File reading error: ${error.message}`);
+    });
 };
 
-await calculateHash();
+const filePath = 'src/hash/files/fileToCalculateHashFor.txt';
+await calculateHash(filePath)
