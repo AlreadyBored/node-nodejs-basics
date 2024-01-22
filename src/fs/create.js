9b62c34e2053ddf8f50563a3e80db0dc1c06f47e
MarshallBaby <aleksey.yatsevich@gmail.com>
2024-01-22 23:30:46 +0100

    implement fs/create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import fs from "fs";
+
+const filePath = "./src/fs/files/fresh.txt";
+const phraseToWrite = "I am fresh and young";
+const fsOperationFailedErrorMessage = "FS operation failed";
+
 const create = async () => {
-    // Write your code here 
+  if (!fs.existsSync(filePath)) {
+    fs.writeFile(filePath, phraseToWrite, (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  } else {
+    throw new Error(fsOperationFailedErrorMessage);
+  }
 };
 
 await create();
