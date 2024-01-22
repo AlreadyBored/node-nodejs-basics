4f11568c487a2476a080d1822523fc232f51f0d6
AndreySavchenkov <sannndvik16@gmail.com>
2024-01-22 08:22:20 +0100

    feat: add read and write stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
-const read = async () => {
-    // Write your code here 
+import fs from "fs";
+
+const read = async (filename) => {
+  const readStream = fs.createReadStream(filename);
+
+  readStream.on("error", (err) => console.log(`Something error: ${err}`));
+  readStream.on("open", () => console.log("\nOpen writable stream"));
+  readStream.on("end", () => console.log("\nClose writable stream"));
+
+  readStream.pipe(process.stdout);
 };
 
-await read();
+await read("src/streams/files/fileToRead.txt");
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,22 @@
+import fs from "fs";
+
 const write = async () => {
-    // Write your code here 
+  try {
+    const stream = fs.createWriteStream("src/streams/files/fileToWrite.txt");
+
+    process.stdin.pipe(stream);
+
+    stream.on("error", (error) => {
+      console.error(`Error writing: ${error.message}`);
+    });
+
+    stream.on('open', () => console.log('Stream Open'));
+    stream.on('close', () => console.log('Stream Close'));
+
+
+  } catch (error) {
+    console.error(`Error creating write stream: ${error.message}`);
+  }
 };
 
 await write();
