4e8b5c62ead5ebaeefc074d9d66213c33ffa4eb7
SmellyFinger <kooolyan123@mail.ru>
2024-01-22 19:02:36 +0300

    feat: implement cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+  const procArguments = process.argv;
+  const result = [];
+
+  for (let i = 0; i < procArguments.length - 1; i++)
+    if (procArguments[i].startsWith('--')) {
+      result.push(
+        `${procArguments[i].substring(2)} is ${procArguments[i + 1]}`
+      );
+    }
+  console.log(result.join(', '));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+  const rssArrKeys = Object.keys(process.env).filter((key) => key.includes('RSS_'));
+  const stringArr = [];
+  for (let i = 0; i < rssArrKeys.length; i++)
+    stringArr.push(`${rssArrKeys[i]}=${process.env[rssArrKeys[i]]}`);
+    console.log(stringArr.join('; '))
 };
 
 parseEnv();
