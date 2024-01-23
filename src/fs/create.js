5049bcd647b95c809fc9990c7f8335aeb5a68990
sesychev <sergeysychou@gmail.com>
2024-01-23 18:40:01 +0300

    create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,31 +1,32 @@
-const fs = require("fs");
-const path = require('path');
+import { createReadStream, createWriteStream } from "fs";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const create = async () => {
-    let pathname = path.join(__dirname, "/fresh.txt"); // используйте path.join()
-    const stream = fs.createWriteStream("FILE.TXT");
-    // Write your code here 
+  const pathName = join(__dirname, "files/fresh.txt");
+  const readStream = createReadStream(pathName, {
+    encoding: "utf-8",
+  });
+
+  readStream.on("error", () => {
+    const writeStream = createWriteStream(pathName, {
+      flags: "wx",
+    });
+    writeStream.write("I am fresh and young");
+    writeStream.end();
+  });
+
+  readStream.on("data", () => {
+    throw new Error(`FS operation failed`);
+  });
 };
 
-await create();
-
-/*
-const fs = require("fs");
-const path = require("path");
-const process = require("process");
-
-const readPath = path.join(__dirname, "/text.txt");
-
-let readable = fs.createReadStream(readPath, {
-  encoding: "utf-8",
-});
-
-readable.pipe(process.stdout); //вывод текста в консоль
-
-const wtitePath = path.join(__dirname, "/temp.txt");
-
-let writable = fs.createWriteStream(wtitePath, {
-  encoding: "utf-8",
-});
-
-readable.pipe(writable); // запись в файл из потока/файла
+try {
+  await create();
+} catch (e) {
+  console.log(`${e}`);
+}
