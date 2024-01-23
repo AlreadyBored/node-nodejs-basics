a2bb38ca2ea072d0ec321c1fa6d20681f8ee25e3
Sekler <seklertyoma@gmail.com>
2024-01-23 20:31:35 +0300

    feat: implement fs:copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,25 @@
+import fs from 'fs';
+import path from 'path';
+import { getDir } from '../utils/getDir.js';
+
+const dirname = getDir(import.meta.url);
+const sourceDir = path.join(dirname, 'files');
+const destDir = path.join(dirname, 'files_copy');
+
 const copy = async () => {
-    // Write your code here 
+    fs.access(sourceDir, (e) => {
+        if(e){
+            throw new Error('FS operation failed');
+        }
+
+        fs.access(destDir, (err) => {
+            if(!err){
+                throw new Error('FS operation failed');
+            }
+            fs.mkdir(destDir, () => {});
+            fs.cp(sourceDir, destDir, {recursive: true}, () => {});
+        })
+    })
 };
 
 await copy();
