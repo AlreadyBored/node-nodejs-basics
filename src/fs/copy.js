d6cf0f8ef648321aca08176227fabfa213e05e87
Andrei <kaliarovich93@gmail.com>
2024-01-23 23:56:40 +0200

    feat: add implementation copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,17 @@
+import fs from "fs";
+
 const copy = async () => {
-    // Write your code here 
+  // Write your code here
+    if (fs.existsSync("files") && !fs.existsSync("files_copy")) {
+        const files = fs.readdirSync("./files");
+        fs.mkdirSync("./files_copy", () => {});
+        files.forEach((file) => {
+          fs.copyFileSync(`./files/${file}`, `./files_copy/${file}`);
+        });
+  
+  } else {
+    throw new Error("FS operation failed");
+  }
 };
 
 await copy();
