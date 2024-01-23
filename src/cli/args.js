9d2124a064b80d5c679c0bbb305ab74581977f57
Nikita <nikitosminsk100@gmail.com>
2024-01-23 19:36:12 +0300

    feat: implement cli:args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+  const outputArray = [];
+
+  for (let index = 2; index < process.argv.length; index += 2) {
+    const key = process.argv[index].slice(2);
+    const value = process.argv[index + 1];
+
+    outputArray.push(`${key} is ${value}`);
+  }
+
+  const outputSeparator = ', ';
+  console.log(outputArray.join(outputSeparator))
 };
 
 parseArgs();
