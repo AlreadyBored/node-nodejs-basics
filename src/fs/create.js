18f386c2c0552014c686ef1c1fb4f4f1d4c05cb0
PopovaOlha <fc.odessa.10@gmail.com>
2024-01-23 18:40:53 +0300

    init: start work with Node.js Basic

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+
 const create = async () => {
-    // Write your code here 
+  const filePath = path.join('files', 'fresh.txt');
+
+  try {
+    await fs.access(filePath, fs.constants.F_OK);
+    throw new Error('FS operation failed: File already exists');
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      const content = 'I am fresh and young';
+      await fs.writeFile(filePath, content);
+      console.log('File created successfully');
+    } else {
+      console.error('FS operation failed:', error.message);
+    }
+  }
 };
 
 await create();
