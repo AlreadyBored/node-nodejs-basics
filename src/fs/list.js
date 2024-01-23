7592780f2240addbae5802d41723dfbbdc5b9b04
mikhailyatsenko <misha.ggg@gmail.com>
2024-01-23 16:21:10 +0100

    add list func, add readfile func

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,24 @@
+import fs from "node:fs";
+
+import path from "node:path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+  const pathToFolderToListFiles = path.join(__dirname, "..", "fs", "files");
+
+  if (!fs.existsSync(pathToFolderToListFiles)) {
+    throw new Error("FS operation failed");
+  }
+
+  try {
+    const fileList = await fs.promises.readdir(pathToFolderToListFiles);
+    for (const file of fileList) console.log(file);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 await list();
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,25 @@
+import fs from "node:fs";
+
+import path from "node:path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const pathToReadFile = path.join(__dirname, "..", "fs", "files", "fileToRead.txt");
+
+  if (!fs.existsSync(pathToReadFile)) {
+    throw new Error("FS operation failed");
+  }
+
+  fs.readFile(pathToReadFile, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(data);
+  });
 };
 
 await read();
