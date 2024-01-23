cfc107e540b06c608da653e2fa8f4b4a65ed9cd6
AlexanderFraltsov <kolonessens@yandex.ru>
2024-01-24 00:04:25 +0400

    feat: implement cli part

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
+import { argv } from 'node:process';
+
 const parseArgs = () => {
-    // Write your code here 
+	const [, , ...args] = argv;
+    let argumentsString = '';
+	args.forEach((arg, index) => {
+		if (arg.startsWith('--')) {
+			argumentsString += `${arg.replace('--', '')} is `;
+		} else {
+			argumentsString += `${arg}${index === args.length - 1 ? '' : ', '}`;
+		}
+	});
+	console.log(argumentsString);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
+import { env } from 'node:process';
+
 const parseEnv = () => {
-    // Write your code here 
+    const rssEnvs = Object.entries(env)
+		.filter(([key]) => key.indexOf('RSS_') === 0)
+		.map(([key, value]) => `${key}=${value}`)
+		.join('; ');
+
+	console.log(rssEnvs);
 };
 
 parseEnv();
