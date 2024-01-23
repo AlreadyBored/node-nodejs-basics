cb0d88b5975ded7997ef627b4c364926b54e32ec
Nikita <nikitosminsk100@gmail.com>
2024-01-23 19:23:10 +0300

    feat: fs:read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.resolve(__dirname, './files/fileToRead.txt');
+
+  if (!fs.existsSync(filePath) || !fs.lstatSync(filePath).isFile()) {
+    throw new Error('FS operation failed');
+  }
+
+  const content = await fs.promises.readFile(filePath, { encoding: 'utf-8' });
+  console.log(content);
 };
 
 await read();
