8be25e3166f561a1d8f20ee5fafff5cc97214225
Sekler <seklertyoma@gmail.com>
2024-01-23 20:58:40 +0300

    feat: implement fs:read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import fs from 'fs';
+import path from 'path';
+import { getDir } from '../utils/getDir.js';
+
+const dirname = getDir(import.meta.url);
+const filename = path.join(dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    fs.readFile(filename, 'utf-8' ,(err, data) => {
+        if(err) {
+            throw new Error('FS operation failed');
+        }
+        console.log(data);
+      }); 
 };
 
 await read();
