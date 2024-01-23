367ae7bc6af52a37ea0121ee697b881a8d88dac0
Олег Зайцев <30oleg.zv94@gmail.com>
2024-01-23 22:30:05 +0300

    feat: implement cli task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
+// Implement function that parses command line arguments (given in format --propName value --prop2Name value2, you don't need to validate it) and prints them to the console in the format propName is value, prop2Name is value2
 const parseArgs = () => {
-    // Write your code here 
+  const cliArgs = process.argv.slice(2);
+  let result = "";
+
+  cliArgs.forEach((it, index) => {
+    result += index % 2 === 0 ? `${it} is ` : `${it}, `;
+  });
+  result = result.trim();
+  result.endsWith(",") ? console.log(result.slice(0, -1)) : console.log(result);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
+//Implement function that parses environment variables with prefix RSS_ and prints them to the console in the format RSS_name1=value1; RSS_name2=value2
+const PREFIX = "RSS_";
+
 const parseEnv = () => {
-    // Write your code here 
+  let result = "";
+  for (const env in process.env) {
+    if (env.startsWith(PREFIX)) {
+      result += `${env}=${process.env[env]}; `;
+    }
+  }
+  result = result.trim();
+  result.endsWith(";") ? console.log(result.slice(0, -1)) : console.log(result);
 };
 
 parseEnv();
