dc12d41d125db63ccbe20a9020ef46caad5083a6
mikhailyatsenko <misha.ggg@gmail.com>
2024-01-23 15:01:26 +0100

    add create function, add copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,23 @@
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+  const pathCopyFrom = path.join(__dirname, "..", "fs", "files");
+  const pathCopyTo = path.join(__dirname, "..", "fs", "files_copy");
+
+  if (!fs.existsSync(pathCopyFrom) || fs.existsSync(pathCopyTo)) {
+    throw new Error("FS operation failed");
+  }
+
+  fs.cp(pathCopyFrom, pathCopyTo, { recursive: true }, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,26 @@
+// const fs = require('node:fs/promises');
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+  const content = "I am fresh and young";
+  const pathToWriteFile = path.join(__dirname, "..", "fs", "files", "fresh.txt");
+
+  if (fs.existsSync(pathToWriteFile)) {
+    throw new Error("FS operation failed");
+  }
+
+  fs.writeFile(pathToWriteFile, content, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("file was created");
+    }
+  });
 };
 
 await create();
