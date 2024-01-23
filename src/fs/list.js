c9d62e2d7a27b04b0d5ed581360e7ad87d9ac69e
Sekler <seklertyoma@gmail.com>
2024-01-23 20:55:09 +0300

    feat: implement fs:list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import fs from 'fs';
+import path from 'path';
+import { getDir } from '../utils/getDir.js';
+
+const dirname = getDir(import.meta.url);
+const filesDir = path.join(dirname, 'files');
+
 const list = async () => {
-    // Write your code here 
+    fs.readdir(filesDir, (err, files) => {
+        if(err) {
+            throw new Error('FS operation failed');
+        }
+        files.forEach(file => {
+          console.log(file);
+        });
+      });
 };
 
 await list();
