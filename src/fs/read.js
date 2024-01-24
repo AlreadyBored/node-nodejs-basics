cb39ab87f947a1509f0cfa04b6d12cdccd090f33
Eugene Solomonyk <solosoft2000@mail.ru>
2024-01-24 13:51:30 +0200

    feat: read.js complete

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,22 @@
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
 const read = async () => {
     // Write your code here 
+    try {
+      const __filename = fileURLToPath(import.meta.url);
+      const __dirname = path.dirname(__filename);
+      const readFile = path.join(__dirname, 'files', 'fileToRead.txt');
+
+      if (!fs.existsSync(readFile)) {
+        throw new Error('FS operation failed');
+      }
+
+      console.log(fs.readFileSync(readFile, 'utf-8'));
+    } catch (error) {
+      console.error(error.message);
+    }
 };
 
 await read();
