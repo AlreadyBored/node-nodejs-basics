49d433ce38284fe95b41a8cc0f3c8d868c881ea6
MarshallBaby <aleksey.yatsevich@gmail.com>
2024-01-24 03:09:40 +0100

    implement fs/copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,45 @@
+import { access, cp, constants } from "node:fs/promises";
+
+const sourcePath = "./files";
+const destPath = sourcePath.concat("_copy");
+
+const fsOperationFailedErrorMessage = "FS operation failed";
+
 const copy = async () => {
-    // Write your code here 
+  await validateSourceDirExists();
+  await validateDestinationDirDoesNotExist();
+
+  doCopy();
+};
+
+const validateSourceDirExists = async () => {
+  try {
+    await access(sourcePath, constants.R_OK | constants.W_OK);
+  } catch (err) {
+    if (err.code) {
+      throw new Error(fsOperationFailedErrorMessage);
+    }
+    throw err;
+  }
+};
+
+const validateDestinationDirDoesNotExist = async () => {
+  try {
+    await access(destPath, constants.R_OK | constants.W_OK);
+    throw new Error(fsOperationFailedErrorMessage);
+  } catch (err) {
+    if (err.code !== "ENOENT") {
+      throw err;
+    }
+  }
+};
+
+const doCopy = () => {
+  cp(sourcePath, destPath, { recursive: true })
+    .then(() => console.log("Copied successfully."))
+    .catch((err) => {
+      throw err;
+    });
 };
 
 await copy();
