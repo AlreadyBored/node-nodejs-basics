21b677e1c7d8bf9d080f9ff62cde28331843b71d
Sekler <seklertyoma@gmail.com>
2024-01-24 13:09:00 +0300

    feat: implemenmt streams:write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import path from 'path';
+import { stdin } from 'process';
+import { createWriteStream } from 'fs';
+import { getDir } from '../utils/getDir.js';
+
+const dirname = getDir(import.meta.url);
+const filename = path.join(dirname, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+    const writeable = createWriteStream(filename);
+    
+    stdin.pipe(writeable);
 };
 
 await write();
