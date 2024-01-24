b3ae7b4427de48109dd7d46fae8960df7b339f71
Eugene <josumkin@yahoo.com>
2024-01-24 20:09:47 +0400

    feat: fs create task modified

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,25 +1,17 @@
-import fs from 'fs/promises';
-import path, { dirname } from 'path';
+import { dirname } from 'path';
+import { writeFile } from 'node:fs/promises';
 import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const folderPath = path.join(__dirname, 'files');
-const filePath = path.join(folderPath, 'fresh.txt');
+const currentPath = fileURLToPath(import.meta.url);
+const currentDir = dirname(currentPath);
 const fileContent = 'I am fresh and young';
 
 const create = async () => {
 
     try {
-        await fs.mkdir(folderPath, { recursive: true });
-        await fs.writeFile(filePath, fileContent, { flag: 'wx' });
-        console.log('File created successfully!');
+        await writeFile((currentDir + '/files' + '/fresh.txt'), fileContent);
       } catch (err) {
-        if (err.code === 'EEXIST') {
-          throw new Error('FS operation failed');
-        }
-        console.error(err);
+        throw new Error(err)
       }
 };
 
