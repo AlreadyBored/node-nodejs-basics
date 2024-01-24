dfa276763425bff3d3f2882182402259c1904721
iamnkt <sg23mj@gmail.com>
2024-01-24 13:38:30 +0300

    feat: add create file function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import { writeFile } from 'node:fs/promises';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const create = async () => {
-    // Write your code here 
+  try {
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const fileName = 'fresh.txt';
+    const data = 'I am fresh and young';
+    const newFile = writeFile(join(__dirname, 'files', fileName), data, { flag: 'wx' });
+
+    await newFile;
+  } catch (e) {
+    throw new Error ('FS operation failed');
+  }
 };
 
 await create();
