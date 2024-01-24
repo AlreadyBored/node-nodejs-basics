3f1b077fc03e3271bee6983acbb93f098bbdbfc6
Eugene Solomonyk <solosoft2000@mail.ru>
2024-01-24 15:12:29 +0200

    feat: args.js complete

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
     // Write your code here 
+    const args = process.argv.slice(2);
+
+    let result = '';
+    for (let i = 0; i < args.length; i = i + 2) {
+      const newName = args[i].slice(2);
+      const argString = `${newName} is ${args[i+1]}`;
+      result += result.length ? `, ${argString}` : `${argString}`;
+    }
+
+    console.log(result);
 };
 
 parseArgs();
