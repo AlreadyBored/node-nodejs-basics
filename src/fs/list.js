7cb6de0b75ae240041607ebf96b90117db7ed471
Val-d-emar <vhot@live.ru>
2024-01-24 20:12:35 +0400

    made list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,29 @@
+/*implement function that prints all array of filenames from files folder into console 
+(if files folder doesn't exists Error with message FS operation failed must be thrown)
+ */
+import { readdir} from 'fs';
+
 const list = async () => {
     // Write your code here 
+    let src = 'files';
+
+    readdir(src, (err, files) => {
+        if (!err) {
+            if (files.length > 0) {
+                for (let f of files) {
+                        console.log(f);
+                }
+            }
+        } else {
+            if (err.code === 'ENOENT') {
+                throw new Error('FS operation failed');
+            } else {
+                throw err;
+            }
+        }
+
+    });
+
 };
 
 await list();
