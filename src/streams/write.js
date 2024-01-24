05d935e1fc3554b4a2e24242317348534d74b4ca
Nikita <nikitosminsk100@gmail.com>
2024-01-24 20:34:39 +0300

    feat: streams:write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,26 @@
+import {createHash} from 'crypto';
+import fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+import {pipeline} from 'stream';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+  const filePath = path.resolve(__dirname, './files/fileToWrite.txt');
+  const fileStream = fs.createWriteStream(filePath, {encoding: 'utf-8'});
+
+  console.log('Write some text, push Enter after last symbol and terminate the process (Ctrl+C) — text will appear in file');
+
+  pipeline(process.stdin, fileStream, (err) => {
+      if (err) {
+        console.error('Pipeline failed.', err);
+      } else {
+        console.log('Pipeline succeeded.');
+      }
+    }
+  );
 };
 
 await write();
