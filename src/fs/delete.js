0ba424d6c6762faf35ff5633dc69d17f9fc5c4c5
Eugene Solomonyk <solosoft2000@mail.ru>
2024-01-24 13:21:29 +0200

    feat: delete.js complete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,23 @@
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
 const remove = async () => {
     // Write your code here 
+    try {
+      const __filename = fileURLToPath(import.meta.url);
+      const __dirname = path.dirname(__filename);
+      const removeFile = path.join(__dirname, 'files', 'fileToRemove.txt');
+
+      if (!fs.existsSync(removeFile)) {
+        throw new Error('FS operation failed');
+      }
+
+      fs.rmSync(removeFile);
+      console.log('file removed');
+    } catch (error) {
+      console.error(error.message);
+    }
 };
 
 await remove();
