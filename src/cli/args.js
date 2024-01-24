96fd5d95abc1e878d379c53d0b5d89bab055abdf
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-01-25 01:59:30 +0300

    feat: implement cli/args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    let result = '';
+    for (let i = 0; i < args.length; i += 2) {
+        result += `${args[i]} is ${args[i + 1]}, `;
+    }
+    console.log(result.slice(0, -2));
 };
 
 parseArgs();
