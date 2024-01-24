1dc78bf718c38e88fd060ba86dea7ab0704e1528
MarshallBaby <aleksey.yatsevich@gmail.com>
2024-01-24 02:38:59 +0100

    rework create.js using async approach

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,19 +1,32 @@
 import fs from "fs";
+import { constants } from "fs/promises";
 
 const filePath = "./src/fs/files/fresh.txt";
 const phraseToWrite = "I am fresh and young";
 const fsOperationFailedErrorMessage = "FS operation failed";
 
 const create = async () => {
-  if (!fs.existsSync(filePath)) {
-    fs.writeFile(filePath, phraseToWrite, (err) => {
-      if (err) {
-        console.error(err);
-      }
-    });
-  } else {
-    throw new Error(fsOperationFailedErrorMessage);
-  }
+  fs.access(filePath, constants.R_OK, (err) => {
+    if (!err) {
+      throw new Error(fsOperationFailedErrorMessage);
+    }
+
+    if (err.code === "ENOENT") {
+      writeFile();
+      return;
+    }
+
+    throw err;
+  });
+};
+
+const writeFile = () => {
+  fs.writeFile(filePath, phraseToWrite, (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log("File created successfully");
+  });
 };
 
 await create();
