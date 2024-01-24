fb7c54424310f8fff3b8e9cef2d517a94e62fa31
mikhailyatsenko <misha.ggg@gmail.com>
2024-01-24 13:57:17 +0100

    add args func

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+  const argsArray = process.argv;
+
+  let argsToPrint = "";
+
+  for (let i = 2; i < argsArray.length; i++) {
+    if (i % 2 == 0) {
+      argsToPrint += argsArray[i] + " is " + argsArray[i + 1] + ", ";
+    }
+  }
+
+  console.log(argsToPrint.slice(0, -2));
 };
 
 parseArgs();
