b87e0e5addb0afc119cab130b851b5dd60dd4036
Val-d-emar <vhot@live.ru>
2024-01-24 19:11:41 +0400

    but forget add it =)

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,42 @@
+/* copy.js - implement function that copies folder files files with all its content into 
+folder files_copy at the same level (if files folder doesn't exists or files_copy has 
+already been created Error with message 'FS operation failed' must be thrown)
+*/
+import { readdir, mkdir, copyFile } from 'fs';
+import { join as platform_path} from 'path';
+
 const copy = async () => {
     // Write your code here 
+    let src = 'files';
+    let dst = 'files_copy';
+
+    readdir(src, (err, files) => {
+        if (!err) {
+            mkdir(dst, { recursive: false }, (err) => {
+                if (err) {
+                    if (err.code === 'EEXIST') {
+                        throw new Error('FS operation failed');
+                    }
+                } else {
+                    if (files.length > 0) {
+                        for (let f of files) {
+                            copyFile(platform_path(src, f), platform_path(dst, f), (err) => {
+                                if (err) throw err;
+                                // console.log(f);
+                            });
+                        }
+                    }
+                }
+            });
+
+        } else {
+            if (err.code === 'ENOENT') {
+                throw new Error('FS operation failed');
+            }
+        }
+
+    });
+
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+/* create.js - implement function that creates new file fresh.txt with content 
+'I am fresh and young' inside of the files folder (if file already exists 
+Error with message 'FS operation failed' must be thrown) */
+import { existsSync, promises } from 'fs';
+import { join as platform_path} from 'path';
+
 const create = async () => {
-    // Write your code here 
+    // Write your code here
+    let fname = platform_path('files', 'fresh.txt'); //using posix or windows separator
+    if (existsSync(fname)) {
+        throw new Error('FS operation failed');
+    } else {
+        return promises.writeFile(fname, 'I am fresh and young');
+    }
 };
 
 await create();
