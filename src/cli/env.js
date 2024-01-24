fabf4930547b0e08d5a7d21ed5657b8d8f2513cb
mikhailyatsenko <misha.ggg@gmail.com>
2024-01-24 13:35:18 +0100

    add env func

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+  const RSS = /^RSS_/i;
+
+  const rssEnv = Object.keys(process.env)
+    .filter((key) => RSS.test(key))
+    .reduce((env, key) => {
+      env += key + "=" + process.env[key] + "; ";
+      return env;
+    }, "");
+
+  console.log(rssEnv.slice(0, -2));
 };
 
 parseEnv();
