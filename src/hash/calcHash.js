82fb98c9982957e5786005fca659d6a364f62e6e
Sekler <seklertyoma@gmail.com>
2024-01-24 12:51:38 +0300

    feat: implement hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { createReadStream } from 'fs';
+import { stdout } from 'process';
+import { createHash } from 'crypto';
+import path from 'path';
+import {getDir} from '../utils/getDir.js'
+
+const dirname = getDir(import.meta.url);
+const filename = path.join(dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256');
+    const input = createReadStream(filename);
+    
+    input.pipe(hash).setEncoding('hex').pipe(stdout);
 };
 
 await calculateHash();
