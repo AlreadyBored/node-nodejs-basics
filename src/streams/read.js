e23ec11efd47cf2074321b1c535bd49489664fc3
Sekler <seklertyoma@gmail.com>
2024-01-24 13:05:24 +0300

    feat: implement streams:read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import path from 'path';
+import { stdout } from 'process';
+import { createReadStream } from 'fs';
+import { getDir } from '../utils/getDir.js';
+
+const dirname = getDir(import.meta.url);
+const filename = path.join(dirname, 'files', 'fileToRead.txt');
+
+
 const read = async () => {
-    // Write your code here 
+    const readable = createReadStream(filename)
+    
+    readable.pipe(stdout);
 };
 
 await read();
