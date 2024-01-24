9f7638908f66a33009ffc044b9de7164cbfec6f8
Eugene Solomonyk <solosoft2000@mail.ru>
2024-01-24 16:33:34 +0200

    feat: create.read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,26 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const read = async () => {
     // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const readFile = path.join(__dirname, 'files', 'fileToRead.txt');
+
+    const readStream = fs.createReadStream(readFile);
+
+    readStream.on('data', (chunk) => {
+      process.stdout.write(chunk);
+    });
+
+    readStream.on('end', () => {
+      process.stdout.write('\n');
+    });
+
+    readStream.on('error', (error) => {
+      console.error(error.message);
+    });
 };
 
 await read();
