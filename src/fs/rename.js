7fe1b29ce86904c49d214386db51d053d8b53b2f
Val-d-emar <vhot@live.ru>
2024-01-24 19:46:00 +0400

    made create, copy, rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,29 @@
+/* implement function that renames file wrongFilename.txt to properFilename with extension .md 
+(if there's no file wrongFilename.txt or properFilename.md already exists Error with message 
+FS operation failed must be thrown)
+ */
+import { copyFile, constants, unlink } from 'fs';
+import { join as platform_path } from 'path';
+
 const rename = async () => {
     // Write your code here 
+    let src = platform_path('files', 'wrongFilename.txt');
+    let dst = platform_path('files', 'properFilename.md');
+
+    copyFile(src, dst, constants.COPYFILE_EXCL, (err) => {
+        if (err) {
+            if (err.code === 'EEXIST') {
+                throw new Error('FS operation failed');
+            } else if (err.code === 'ENOENT') {
+                throw new Error('FS operation failed');
+            }
+        } else {
+            unlink(src, (err) => {
+                if (err) throw err;
+                // console.log('OK');
+            });
+        }
+    });
 };
 
 await rename();
