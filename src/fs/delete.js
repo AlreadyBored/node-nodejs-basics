c43c862398faa8c0ecfc29e68339fbfb10186e48
Andrei <kaliarovich93@gmail.com>
2024-01-24 22:55:13 +0200

    feat: add implementation of delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,12 @@
+import fs from "fs";
+
 const remove = async () => {
-    // Write your code here 
+  // Write your code here
+  if (fs.existsSync("./files/fileToRemove.txt")) {
+    fs.rmSync("./files/fileToRemove.txt");
+  } else {
+      throw new Error('FS operation failed')
+  }
 };
 
 await remove();
