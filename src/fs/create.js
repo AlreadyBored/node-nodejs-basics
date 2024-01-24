dad4ae8465d20eeca6e2818d0f0aeeda27f24666
PopovaOlha <fc.odessa.10@gmail.com>
2024-01-24 15:37:10 +0300

    refactor: change the path to the fresh.txt

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,22 +1,19 @@
 import { promises as fs } from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 const create = async () => {
-
-  const filePath = path.join('files', 'fresh.txt');
+  
+  const currentDir = path.dirname(fileURLToPath(import.meta.url));
+  const filePath = path.join(currentDir, 'files', 'fresh.txt');
+  const content = 'I am fresh and young';
 
   try {
-    await fs.access(filePath, fs.constants.F_OK);
-    throw new Error('FS operation failed: File already exists');
+    await fs.writeFile(filePath, content, { flag: 'wx' });
+    console.log('File created successfully');
   } catch (error) {
-    if (error.code === 'ENOENT') {
-      const content = 'I am fresh and young';
-      await fs.writeFile(filePath, content);
-      console.log('File created successfully');
-    } else {
-      console.error('FS operation failed:', error.message);
-    }
+    console.error('FS operation failed:', error.message);
   }
 };
- 
+
 await create();
