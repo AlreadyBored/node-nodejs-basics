d32c1362b462b2466be66e45274e5a2ca2e26feb
Andrei <kaliarovich93@gmail.com>
2024-01-24 23:17:57 +0200

    fat: add implementation of read

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,13 @@
+import fs from "fs";
+
+
 const list = async () => {
-    // Write your code here 
+    // Write your code here
+    const files = fs.readdirSync('./files');
+
+    files.forEach((file) => {
+        console.log(file);
+    })
 };
 
 await list();
