4839a85d5ea135c7f46e46005cb04c24e631ede1
iamnkt <sg23mj@gmail.com>
2024-01-24 14:46:23 +0300

    feat: add copy folder function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,22 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { existsSync } from 'node:fs';
+import { cp } from 'node:fs/promises';
+
 const copy = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const srcPath = join(__dirname, 'files');
+  const destPath = join(__dirname, 'files_copy');
+
+  if (existsSync(destPath)) {
+    throw new Error ('FS operation failed');
+  }
+
+  try {
+    await cp(srcPath, destPath, { recursive: true });
+  } catch(e) {
+    throw new Error ('FS operation failed');
+  }
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,10 +1,11 @@
-import { writeFile } from 'node:fs/promises';
 import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { writeFile } from 'node:fs/promises';
 
 const create = async () => {
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  
   try {
-    const __dirname = dirname(fileURLToPath(import.meta.url));
     const fileName = 'fresh.txt';
     const data = 'I am fresh and young';
     const newFile = writeFile(join(__dirname, 'files', fileName), data, { flag: 'wx' });
