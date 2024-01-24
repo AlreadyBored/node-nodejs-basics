c3576d13ca1b2c32b920b1775d955028ae5aa44b
AlexanderFraltsov <kolonessens@yandex.ru>
2024-01-24 21:09:06 +0400

    feat: implement hash part

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import { createReadStream } from 'node:fs';
+import { stdout } from 'node:process';
+import { join } from 'node:path';
+const __dirname = import.meta.dirname;
+
+const {
+	createHash,
+} = await import('node:crypto');
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256');
+	const input = createReadStream(join(__dirname, 'files/fileToCalculateHashFor.txt'));
+	input.pipe(hash).setEncoding('hex').pipe(stdout)
 };
 
 await calculateHash();
