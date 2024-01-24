0b8eac06d357adf2f41febc65a15473fe477a59b
irina-mokh <75081244+irina-mokh@users.noreply.github.com>
2024-01-24 18:40:59 +0300

    feat: implement CLI functions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
+import { argv } from 'process';
+
 const parseArgs = () => {
-    // Write your code here 
+    argv.forEach((name, i) => {
+        if (i % 2 === 0) {
+            console.log(`${name.slice(2)} is ${argv[i + 1]},`);
+        }
+      });
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,8 @@
 const parseEnv = () => {
-    // Write your code here 
+    for (const variable in process.env) {
+        if (!variable.startsWith('RSS_')) continue;
+        console.log(`${variable}=${process.env[variable]};`);
+    }
 };
 
 parseEnv();
