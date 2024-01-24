a65600c3590b011ce1d53c22bb1ff637c56121ce
Eugene Solomonyk <solosoft2000@mail.ru>
2024-01-24 10:59:39 +0200

    feat: complete create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,26 @@
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
 const create = async () => {
     // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const freshFile = path.join(__dirname, 'files', 'fresh.txt');
+    if (fs.existsSync(freshFile)) {
+      throw new Error('FS operation failed');
+    }
+
+    fs.writeFile(freshFile, 'I am fresh and young', (err) => {
+      if (err) {
+        throw new Error(err);
+      }
+      console.log('file created');
+    });
 };
 
-await create();
+try {
+  await create();  
+} catch (error) {
+  console.error(error.message);
+}
