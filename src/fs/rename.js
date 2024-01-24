79c8edf8382462bbf9e871939dc85e3d2c7de4e3
Andrei <kaliarovich93@gmail.com>
2024-01-24 22:48:15 +0200

    feat: add implementation of rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,15 @@
+import fs from "fs";
+
 const rename = async () => {
-    // Write your code here 
+  // Write your code here
+  if (
+    !fs.existsSync("./files/wrongFilename.txt") ||
+    fs.existsSync("./files/properFilename.md")
+  ) {
+    throw new Error("FS operation failed");
+  } else {
+    fs.renameSync("./files/wrongFilename.txt", "./files/properFilename.md");
+  }
 };
 
 await rename();
