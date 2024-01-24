9f9cfc675259f2a566af2e939c181fc105f64bea
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-01-25 00:46:17 +0300

    feat: implement create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import { access, writeFile } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, 'files', 'fresh.txt');
+    try {
+        await access(filePath);
+        throw new Error('FS operation failed');
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            await writeFile(filePath, 'I am fresh and young');
+        } else {
+            throw error;
+        }
+    }
 };
 
 await create();
