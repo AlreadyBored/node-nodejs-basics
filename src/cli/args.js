ac4d6aa23abd6e5eb10e96953b0b5b1b8347362b
valeriia zvezdina <valeriia.zvezdina@gmail.com>
2024-01-24 15:15:19 +0100

    feat: finish CLI subtask

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+    const cliArgs = process.argv.slice(2);
+    const args = {};
+    for (let i = 0; i < cliArgs.length; i += 2) {
+        const argName = cliArgs[i].replace('--', '');
+        const argValue = cliArgs[i + 1];
+        args[argName] = argValue;
+    }
+    for (const [key, value] of Object.entries(args)) {
+        console.log(`${key} is ${value}`);
+    }
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+    const cliEnvs = process.env;
+    let envs = [];
+    for (const [ key, value ] of Object.entries(cliEnvs)) {
+        if (key.startsWith('RSS_')) {
+            envs.push(`${key}=${value}`);
+        }
+    }
+    console.log(envs.join('; '));
 };
 
 parseEnv();
