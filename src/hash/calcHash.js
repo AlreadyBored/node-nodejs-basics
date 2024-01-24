d1a38942f532968afae5a305273bc10526cc84ca
prostonau <prostonau@mail.ru>
2024-01-25 00:12:15 +0300

    feat: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,26 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const pathToFile = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash('sha256');
+  // console.log('hash = ', hash);
+  const readStream = createReadStream(pathToFile);
+
+  readStream.on('data', (chunk) => {
+    hash.update(chunk);
+  });
+
+  readStream.on('end', () => {
+    const hexHash = hash.digest('hex');
+    console.log('SHA256 Hash:', hexHash);
+  });
 };
 
 await calculateHash();
