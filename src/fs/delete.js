2b45884111b4556b15e5a87a1d530d4f085d7dc1
Val-d-emar <vhot@live.ru>
2024-01-24 20:03:51 +0400

    made delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,23 @@
+/*  implement function that deletes file fileToRemove.txt 
+(if there's no file fileToRemove.txt Error with message FS 
+*/
+import { unlink, copyFile } from 'fs';
+import { join as platform_path } from 'path';
 const remove = async () => {
     // Write your code here 
+    let src = platform_path('files', 'fileToRemove.txt');
+    // copyFile(src, src+'.0', (err) => {if (err) throw err;});
+    unlink(src, (err) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                throw new Error('FS operation failed');
+            } else {
+                throw err;
+            }            
+        } else {
+                // console.log('OK');
+        }
+    });
 };
 
 await remove();
