e175df27e6cb73233cab39a58e8bff81ade805e7
Eugene Solomonyk <solosoft2000@mail.ru>
2024-01-24 14:49:57 +0200

    feat: complete env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
     // Write your code here 
+    const envObject = Object.entries(process.env);
+    const filteredEnv = envObject.filter(([key, value]) => key.startsWith('RSS_'));
+    let result = '';
+    filteredEnv.forEach(([key, value]) => {
+      result += result.length ? `; ${key}=${value}` : `${key}=${value}`;
+    })
+    console.log(result);
 };
 
 parseEnv();
