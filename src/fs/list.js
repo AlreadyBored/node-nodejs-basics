48b0b0e427b9984f8344fe467e599f299c06d32a
Andrei <kaliarovich93@gmail.com>
2024-01-24 23:24:50 +0200

    feat: add implementation of list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,13 +1,19 @@
 import fs from "fs";
 
-
 const list = async () => {
     // Write your code here
-    const files = fs.readdirSync('./files');
+    
+
+    // I output list of names, because didnt get explanation how it must looks, like array of names or list of names. In anyway if necessary, i can implement it lika a array of names.
+  if (fs.existsSync("./files")) {
+    const files = fs.readdirSync("./files");
 
     files.forEach((file) => {
-        console.log(file);
-    })
+      console.log(file);
+    });
+  } else {
+    throw new Error("FS operation failed");
+  }
 };
 
 await list();
