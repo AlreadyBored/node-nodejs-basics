f0ba1dd3d0eb61158bd249ac419c5dad2b6ed5bb
Nikita <nikitosminsk100@gmail.com>
2024-01-24 20:12:37 +0300

    feat: implement streams:read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,31 @@
+import {createHash} from 'crypto';
+import fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.resolve(__dirname, './files/fileToRead.txt');
+
+  const data = await new Promise(resolve => {
+    const stream = fs.createReadStream(filePath);
+    let fileData = '';
+
+    stream.on('readable', () => {
+      let chunk;
+
+      while (null !== (chunk = stream.read(1))) {
+        fileData += chunk;
+      }
+    });
+
+    stream.on('end', () => resolve(fileData));
+  });
+
+
+  process.stdout.write(data);
 };
 
 await read();
