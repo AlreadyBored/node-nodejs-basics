06e6ddf1818b2e32c1021b1da8e88ca4729695a9
valeriia zvezdina <valeriia.zvezdina@gmail.com>
2024-01-24 16:32:32 +0100

    feat: finish Hah subtask

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import * as fs from 'fs/promises';
+import crypto from 'crypto';
+import { join } from 'path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+const fileName = 'fileToCalculateHashFor.txt';
+
 const calculateHash = async () => {
-    // Write your code here 
+    try {
+        const content = await fs.readFile(
+            join(__dirname, 'files', fileName),
+            'utf-8'
+        );
+        const hash = crypto.createHash('sha256');
+        hash.update(content);
+        const hex = hash.digest('hex');
+        console.log(hex);
+    } catch(err) {
+        console.log(err.message);
+    }
 };
 
 await calculateHash();
