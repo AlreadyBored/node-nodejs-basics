73c63a33534dc2c0fa6a0c988c3ea0d67b7159aa
Sekler <seklertyoma@gmail.com>
2024-01-24 12:14:38 +0300

    feat: implement cli:args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+    const [, ,...args] = process.argv;
+    const parsedArgs = {}
+
+    for(let i = 0; i < args.length; i+=2){
+        const argName = args[i].slice(2)
+        const argValue = args[i+1] 
+        parsedArgs[argName] = argValue;
+    }
+
+    const stringArgs = Object.entries(parsedArgs).map(([name, value]) => `${name} is ${value}`).join(', ');
+
+    console.log(stringArgs);
 };
 
 parseArgs();
