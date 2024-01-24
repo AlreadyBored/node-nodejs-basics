d911b0e1b8b1b27787e7f827ab6c9421d3f032df
Eugene Solomonyk <solosoft2000@mail.ru>
2024-01-24 13:46:38 +0200

    feat: complete list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,28 @@
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
 const list = async () => {
     // Write your code here 
+    try {
+      const __filename = fileURLToPath(import.meta.url);
+      const __dirname = path.dirname(__filename);
+
+      const mainPath = path.join(__dirname, 'files');
+      if (!fs.existsSync(mainPath)) {
+        throw new Error('FS operation failed');
+      }
+
+      const arrOfFiles = [];
+      fs.readdirSync(mainPath).forEach((file) => {
+        arrOfFiles.push(file);
+      });
+    
+      console.log(arrOfFiles);
+    } catch (error) {
+      console.error(error.message);
+    }
+
 };
 
 await list();
