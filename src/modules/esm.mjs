2042f435cd764b9b4adef4982b59fa5fed8735a8
Андрей Едунов <a.edunov@vk.team>
2024-01-24 14:16:32 +0300

    3. modules

---
+++ src/modules/esm.mjs
@@ -0,0 +1,31 @@
+import { createServer as createServerHttp } from "http";
+import { release, version } from "os";
+import path from "path";
+import a from "./files/a.json" assert { type: "json" };
+import b from "./files/b.json" assert { type: "json" };
+import "./files/c.js";
+
+const __dirname = import.meta.dirname;
+const __filename = import.meta.filename;
+
+export const unknownObject = Math.random() > 0.5 ? a : b;
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${path.sep}"`);
+
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
+
+export const myServer = createServerHttp((_, res) => {
+  res.end("Request accepted");
+});
+
+const PORT = 3000;
+
+console.log(unknownObject);
+
+myServer.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+  console.log("To terminate it, use Ctrl+C combination");
+});
