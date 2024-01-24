6e6c90a633c0b2ec77b5031f3638bf330b74cb18
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-01-25 01:10:48 +0300

    feat: implement rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,29 @@
+import fs from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const rename = async () => {
-    // Write your code here 
+    const oldFile = join(__dirname,'files','wrongFilename.txt');
+    const newFile = join(__dirname,'files','properFilename.md');
+
+    try {
+        await fs.access(oldFile);
+        try {
+            await fs.access(newFile);
+            throw new Error('FS operation failed');
+        } catch (error) {
+            if (error.code === 'ENOENT') {
+                await fs.rename(oldFile, newFile);
+            } else {
+                throw error;
+            }
+        }
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await rename();
