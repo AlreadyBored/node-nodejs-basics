15b89ad58ec82d3c346530b5d5373a04faf549be
Karina Shupik <77788896h@gmail.com>
2024-01-24 14:57:03 +0200

    feat: implement copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,48 @@
+import { promises as fsPromises, constants } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import * as path from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+    const sourceFolderPath = path.join(__dirname, 'files');
+    const destinationFolderPath = path.join(__dirname, 'files_copy');
+
+    // Check if the source folder exists
+    try {
+        await fsPromises.access(sourceFolderPath, constants.R_OK);
+    } catch (error) {
+        throw new Error('Source folder does not exist');
+    }
+
+    // Check if the destination folder already exists
+    try {
+        await fsPromises.access(destinationFolderPath, constants.R_OK);
+        throw new Error('Destination folder already exists');
+    } catch (error) {
+        // If the destination folder doesn't exist
+        if (error.code === 'ENOENT') {
+            await fsPromises.mkdir(destinationFolderPath);
+
+            const files = await fsPromises.readdir(sourceFolderPath);
+
+            for (const file of files) {
+                const sourceFile = path.join(sourceFolderPath, file);
+                const destinationFile = path.join(destinationFolderPath, file);
+
+                await fsPromises.copyFile(sourceFile, destinationFile);
+            }
+
+            console.log('Folder copied successfully');
+        } else {
+            throw error;
+        }
+    }
 };
 
-await copy();
+try {
+    await copy();
+} catch (error) {
+    console.error(error.message);
+}
