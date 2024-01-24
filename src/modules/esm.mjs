62aabcf45ccdca24b809680bc3380ca589c01697
Олег Зайцев <30oleg.zv94@gmail.com>
2024-01-24 22:10:14 +0300

    refactor: transform cjs into mjs module

---
+++ src/modules/esm.mjs
@@ -0,0 +1,45 @@
+//You should refactor file (you can add additional imports if needed)
+import path from "node:path";
+import { release, version } from "node:os";
+import { createServer as createServerHttp } from "node:http";
+import { fileURLToPath } from "node:url";
+import { createRequire } from "node:module";
+import c from "./files/c.js";
+
+const require = createRequire(import.meta.url);
+const random = Math.random();
+
+let unknownObject;
+
+if (random > 0.5) {
+  unknownObject = require("./files/a.json");
+} else {
+  unknownObject = require("./files/b.json");
+}
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${path.sep}"`);
+
+console.log(`Path to current file is ${fileURLToPath(import.meta.url)}`);
+console.log(
+  `Path to current directory is ${path.dirname(fileURLToPath(import.meta.url))}`
+);
+
+const myServer = createServerHttp((_, res) => {
+  res.end("Request accepted");
+});
+
+const PORT = 3000;
+
+console.log(unknownObject);
+
+myServer.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+  console.log("To terminate it, use Ctrl+C combination");
+});
+
+export default {
+  unknownObject,
+  myServer,
+};
