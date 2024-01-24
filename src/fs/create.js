77e19e59080b9e1b6953c56737d3c69cf91cd648
Dmitry Bludchy <dmitrybludchy@gmail.com>
2024-01-24 20:50:50 +0300

    feat: implement create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,27 @@
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { writeFile, access } from 'fs/promises';
+
+const pathExists = (path) => {
+  return access(path)
+    .then(() => true)
+    .catch(() => false);
+};
+
 const create = async () => {
-    // Write your code here 
+  const currentDir = dirname(fileURLToPath(import.meta.url));
+  const filePath = join(currentDir, 'files', 'fresh.txt');
+
+  const fileExists = await pathExists(filePath);
+
+  if (fileExists) throw new Error('FS operation failed');
+
+  try {
+    await writeFile(filePath, 'I am fresh and young');
+    console.log('The file has been successfully written');
+  } catch (error) {
+    console.error('Error writing the file:', error.message);
+  }
 };
 
 await create();
