9250f673ebbfe70e7b2a5f870ce48511aa4d6e05
Eugene Solomonyk <solosoft2000@mail.ru>
2024-01-24 13:15:24 +0200

    feat: rename.js complete

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,24 @@
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
 const rename = async () => {
     // Write your code here 
+    try {
+      const __filename = fileURLToPath(import.meta.url);
+      const __dirname = path.dirname(__filename);
+      const wrongFile = path.join(__dirname, 'files', 'wrongFilename.txt');
+      const propFile = path.join(__dirname, 'files', 'properFilename.md');
+
+      if (!fs.existsSync(wrongFile) || fs.existsSync(propFile)) {
+        throw new Error('FS operation failed');
+      }
+
+      fs.renameSync(wrongFile, propFile);
+      console.log('rename completed');
+    } catch (error) {
+      console.error(error.message);
+    }
 };
 
 await rename();
