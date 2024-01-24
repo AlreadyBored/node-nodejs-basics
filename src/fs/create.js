f3eeb437e769f82798d25dc814095a0c42b3eb8c
Val-d-emar <vhot@live.ru>
2024-01-24 20:39:38 +0400

    fixed create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,17 +1,23 @@
 /* create.js - implement function that creates new file fresh.txt with content 
 'I am fresh and young' inside of the files folder (if file already exists 
 Error with message 'FS operation failed' must be thrown) */
-import { existsSync, promises } from 'fs';
-import { join as platform_path} from 'path';
+
+import { access, constants, writeFile } from 'fs';
+import { join as platform_path } from 'path';
 
 const create = async () => {
     // Write your code here
     let fname = platform_path('files', 'fresh.txt'); //using posix or windows separator
-    if (existsSync(fname)) {
-        throw new Error('FS operation failed');
-    } else {
-        return promises.writeFile(fname, 'I am fresh and young');
-    }
+
+    access(fname, constants.F_OK, (err) => {
+        if (err) {
+            writeFile(fname, 'I am fresh and young', 'utf8', (err) => {
+                if (err) throw err;
+            });
+        } else {
+            throw new Error('FS operation failed');
+        }
+    });
 };
 
 await create();
