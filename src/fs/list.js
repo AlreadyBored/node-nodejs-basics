bfcb7f4be950d87cd3fcdc08a3ea47b3b14c3e89
Karina Shupik <77788896h@gmail.com>
2024-01-24 19:54:36 +0200

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,34 @@
+import { promises as fsPromises, constants } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import * as path from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+    const sourceFolderPath = path.join(__dirname, 'files');
+
+    // Check if the folder exists
+    try {
+        await fsPromises.access(sourceFolderPath, constants.F_OK);
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
+
+    // Read the list of filenames in the folder
+    const files = await fsPromises.readdir(sourceFolderPath);
+
+    const listArray  = [];
+
+    files.forEach((file) => {
+        listArray.push(file)
+    });
+
+    console.log('Filenames in the "files" folder:', listArray);
 };
 
-await list();
+try {
+    await list();
+} catch (error) {
+    console.error(error.message);
+}
