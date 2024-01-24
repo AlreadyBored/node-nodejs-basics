330d0497023273dbc003cdd42cebedfe24dcfcb7
PopovaOlha <fc.odessa.10@gmail.com>
2024-01-24 18:34:06 +0300

    feat: update list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,34 @@
+import { promises as fs, constants } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const list = async () => {
-    // Write your code here 
+  const currentDir = dirname(fileURLToPath(import.meta.url));
+
+  const filesFolderPath = join(currentDir, 'files');
+
+  const textFileNames = [];
+
+  try {
+    await fs.access(filesFolderPath, constants.F_OK);
+
+    const fileNames = await fs.readdir(filesFolderPath);
+
+    fileNames.forEach((fileName) => {
+      if (fileName.toLowerCase().endsWith('.txt')) {
+        textFileNames.push(fileName);
+      }
+    });
+
+    console.log('Array of text files in "files" folder:', textFileNames);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      console.error('FS operation failed: Folder "files" does not exist');
+      return;
+    }
+
+    console.error('FS operation failed:', error.message);
+  }
 };
 
 await list();
