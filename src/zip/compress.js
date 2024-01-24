c87bfbcc274efa47b42b5251f3a43a36cb2f1cb9
AlexanderFraltsov <kolonessens@yandex.ru>
2024-01-24 22:12:06 +0400

    feat: implement zlib part

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,22 @@
+import { pipeline } from 'node:stream';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGunzip } from 'node:zlib';
+import { join } from 'node:path';
+const __dirname = import.meta.dirname;
+
 const compress = async () => {
-    // Write your code here 
+	const gzip = createGunzip();
+	pipeline(
+		createReadStream(join(__dirname, 'files/fileToCompress.txt')),
+		gzip,
+		createWriteStream(join(__dirname, 'files/archive.gz')),
+		(err) => {
+			if (err) {
+				console.error('An error occurred:', err);
+				process.exitCode = 1;
+			}
+		},
+	)
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+import { pipeline } from 'node:stream';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createUnzip } from 'node:zlib';
+import { join } from 'node:path';
+const __dirname = import.meta.dirname;
+
 const decompress = async () => {
-    // Write your code here 
+	const unzip = createUnzip();
+	pipeline(
+		createReadStream(join(__dirname, 'files/archive.gz')),
+		unzip,
+		createWriteStream(join(__dirname, 'files/fileToCompress.txt')),
+		(err) => {
+			if (err) {
+				console.error('An error occurred:', err);
+				process.exitCode = 1;
+			}
+		},
+	)
 };
 
 await decompress();
