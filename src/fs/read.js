ba929dec04372c3b39e22c1ff22c99f1a6d88e0f
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-01-25 01:27:12 +0300

    feat: implement read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,4 +1,21 @@
+import { access, readFile } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
+    const fileToRead = join(__dirname,'files', 'fileToRead.txt');
+
+    try {
+        await access(fileToRead);
+        const content = await readFile(fileToRead, { encoding: 'utf8' });
+        console.log(content);
+    }
+    catch (error) {
+        throw new Error('FS operation failed');
+    }
     // Write your code here 
 };
 
