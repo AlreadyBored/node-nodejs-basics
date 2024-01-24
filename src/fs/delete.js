89a393fff8e85532cd3034f88450c82813901f6d
iamnkt <sg23mj@gmail.com>
2024-01-24 15:14:29 +0300

    feat: add delete file function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,16 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { rm } from 'node:fs/promises';
+
 const remove = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const destPath = join(__dirname, 'files', 'fileToRemove.txt');
+
+  try {
+    await rm(destPath);
+  } catch(e) {
+    throw new Error ('FS operation failed');
+  }
 };
 
 await remove();
