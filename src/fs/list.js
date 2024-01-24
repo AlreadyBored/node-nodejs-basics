ce57a4c3bb9c5e111da83c491562c8e212b653af
iamnkt <sg23mj@gmail.com>
2024-01-24 15:21:06 +0300

    feat: add print filenames function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { readdir } from 'node:fs/promises';
+
 const list = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+
+  try {
+    const files = await readdir(join(__dirname, 'files'));
+    console.log(files);
+  } catch(e) {
+    throw new Error ('FS operation failed');
+  }
 };
 
 await list();
