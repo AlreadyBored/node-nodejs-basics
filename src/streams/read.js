08c5c09818696098555a6770789a3e1df0b36df9
iamnkt <sg23mj@gmail.com>
2024-01-25 23:32:43 +0300

    feat: add read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'url';
+import { createReadStream } from 'node:fs';
+import { stdout } from 'node:process';
+
 const read = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+
+  const input = createReadStream(join(__dirname, 'files', 'fileToRead.txt'));
+  input.pipe(stdout);
 };
 
 await read();
