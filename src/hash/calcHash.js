7929c9d79d26c4c96dae724fa5697d87e7bce2af
Val-d-emar <vhot@live.ru>
2024-01-25 19:36:49 +0400

    done hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+/**
+ *  implement function that calculates SHA256 hash for file fileToCalculateHashFor.txt 
+ * and logs it into console as hex using Streams API
+ */
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import { join as platform_path } from 'path';
+import { stdout } from 'process';
+
 const calculateHash = async () => {
     // Write your code here 
+    let src = platform_path('files', 'fileToCalculateHashFor.txt');
+
+    const hash = createHash('sha256');
+
+    const instream = createReadStream(src);
+
+    return instream.pipe(hash).setEncoding('hex').pipe(stdout);
 };
 
 await calculateHash();
