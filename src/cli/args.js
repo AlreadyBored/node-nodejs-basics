b28b4de593df30058678894d94ff015b7ea5c320
iamnkt <sg23mj@gmail.com>
2024-01-25 21:57:15 +0300

    refactor: refactor cjsToEsm.cjs to ES6 notation

--- src/cli/args.js
+++ src/cli/args.js
@@ -2,7 +2,6 @@ const parseArgs = () => {
   const args = process.argv.slice(2);
 
   let i = 0;
-
   const result = args.map((v, i) => `prop${i+1}Name is ${v}`);
 
   console.log(result.join(', ').replace('1', ''));
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,6 @@
 const parseEnv = () => {
   const result = [];
+  
   for (const [k, v] of Object.entries(process.env)) {
     if (k.startsWith('RSS_')) {
       result.push(`${k}=${v}`);
