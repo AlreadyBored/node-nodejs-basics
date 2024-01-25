f12f4842cb22c0248cb96efa2d33dbcba7d1300a
iamnkt <sg23mj@gmail.com>
2024-01-26 02:57:00 +0300

    feat: add transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,24 @@
+import { Transform } from 'node:stream';
+import { stdin, stdout } from 'node:process';
+import { pipeline } from 'node:stream/promises';
+
+const reverseData = () => {
+  return new Transform({
+    transform(chunk, enc, cb) {
+      const reverse = chunk.toString().split('').reverse().join('');
+      cb(null, reverse);
+    }
+  });
+};
+
+const reverse = reverseData();
+
 const transform = async () => {
-    // Write your code here 
+  await pipeline(
+    stdin,
+    reverse,
+    stdout
+  );
 };
 
 await transform();
