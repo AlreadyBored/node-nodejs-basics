7bfd8b1bd6f2a3fd7a3e71ad2b680ff620a41fef
valeragorobets <valera.gorobets@gmail.com>
2024-01-25 14:33:11 +0200

    feat: Implement Command line interface module

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
+const isKey = (arg, index) => index % 2 === 0;
+
 const parseArgs = () => {
-    // Write your code here 
+	const args = process.argv.slice(2);
+	const keys = args.filter(isKey);
+	const values = args.filter((arg, index) => !isKey(arg, index));
+	const output = keys
+		.map((key, index) => `${ key } is ${ values[index] }`)
+		.join(', ');
+	console.log(output);
 };
 
 parseArgs();
