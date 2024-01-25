36476b51b21b5cc0faf693ae8081606903840e51
irina-mokh <75081244+irina-mokh@users.noreply.github.com>
2024-01-25 15:58:03 +0300

    feat: implement zip functions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import { pipeline } from 'node:stream';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGzip } from 'node:zlib';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const dir = path.dirname(fileURLToPath(import.meta.url));
+
+const inputPath = path.join(dir, './files/fileToCompress.txt');
+const outputPath = path.join(dir, './files/archive.gz');
+
 const compress = async () => {
-    // Write your code here 
+    pipeline(
+        createReadStream(inputPath),
+        createGzip(),
+        createWriteStream(outputPath),
+        (err) => { console.log(err) }
+    );
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import { pipeline } from 'node:stream';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createUnzip } from 'node:zlib';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const dir = path.dirname(fileURLToPath(import.meta.url));
+
+const outputPath = path.join(dir, './files/fileToCompress.txt');
+const inputPath = path.join(dir, './files/archive.gz'); 
+
 const decompress = async () => {
-    // Write your code here 
+    pipeline(
+        createReadStream(inputPath),
+        createUnzip(),
+        createWriteStream(outputPath),
+        (err) => { console.log(err) }
+    );
 };
 
 await decompress();
