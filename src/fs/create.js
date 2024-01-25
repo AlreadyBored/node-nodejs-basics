b3dd7633bebb69497fea3033e57c9493ae35c82b
rahunak <euhene@linnovate.net>
2024-01-26 00:48:39 +0300

    #1: create create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import { promises as fs } from "fs";
+import path from "path";
+
 const create = async () => {
-    // Write your code here 
+  const __dirname = path.dirname("fresh.txt");
+  const filePath = path.join(__dirname, "src/fs/files", "fresh.txt");
+  try {
+    await fs.access(filePath);
+    throw new Error("FS operation failed");
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      await fs.writeFile(filePath, "I am fresh and young");
+    } else {
+      throw err;
+    }
+  }
 };
 
 await create();
