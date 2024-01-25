0ea3cb61b50424fbc63c69e34d81ec5f5eefaf0d
SmellyFinger <kooolyan123@mail.ru>
2024-01-25 14:19:55 +0300

    feat: implement hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import { createReadStream } from 'node:fs';
+import { Transform } from 'node:stream';
+import { fileURLToPath } from 'node:url';
+import path from 'path';
+const { createHmac } = await import('node:crypto');
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = `${__dirname}/files/fileToCalculateHashFor.txt`;
+  const readStream = createReadStream(filePath);
+  const output = process.stdout;
+  const data = 'I love node js!';
+  const hashSHA256 = new Transform({
+    transform(chunk, encoding, callback) {
+      this.push(createHmac('sha256', chunk).update(data).digest('hex'));
+      callback();
+    },
+  });
+
+  readStream.pipe(hashSHA256).pipe(output);
 };
 
 await calculateHash();
