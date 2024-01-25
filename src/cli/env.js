b6804ec1b0c7ce86f22e9ad59203d5b488743648
Andrei <kaliarovich93@gmail.com>
2024-01-25 22:32:20 +0200

    feat: add implementation of cli/env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+  // Write your code here
+  const pairs = Object.entries(process.env);
+  const resArr = [];
+
+  pairs.forEach(([key, value]) => {
+    if (key.startsWith("RSS_")) {
+      resArr.push(`${key}=${value}`);
+    }
+  });
+  console.log(resArr.join("; "));
 };
 
 parseEnv();
