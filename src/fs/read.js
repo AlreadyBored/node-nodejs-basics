59f258941c8a8d57b1817883c20ec46b7e39fc5b
Eugene <josumkin@yahoo.com>
2024-01-25 15:53:13 +0400

    feat: fs-read implemented

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,26 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { existsSync } from 'fs';
+import { readFile } from 'fs/promises';
+
+const currentPath = fileURLToPath(import.meta.url);
+const currentDir = dirname(currentPath);
+const toReadFile = currentDir + '/files/' + 'fileToRead.txt';
+
 const read = async () => {
-    // Write your code here 
+
+    if (!existsSync(toReadFile)) {
+        console.error('source file doesnt exist');
+        return;
+    }
+    
+    try {
+        const contents = await readFile(toReadFile, { encoding: 'utf8' });
+        console.log(contents);
+    } catch (err) {
+        throw new Error(err)
+    }
+
 };
 
 await read();
