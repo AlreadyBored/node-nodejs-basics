c84451f20fbf3976d502e0d67d550105332ec68e
net-ka <a.k.fedosova@gmail.com>
2024-01-26 00:23:05 +0100

    feat: add hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import crypto from 'crypto';
+import fs from 'fs/promises';
+
 const calculateHash = async () => {
-    // Write your code here 
+    // Write your code here
+
+    const path = new URL('./files/fileToCalculateHashFor.txt', import.meta.url);
+    const hash = crypto.createHash('sha256');
+
+    const sourceData = await fs.readFile(path);
+
+    hash.update(sourceData);
+    const digest = hash.digest('hex');
+
+    console.log(digest);
 };
 
 await calculateHash();
