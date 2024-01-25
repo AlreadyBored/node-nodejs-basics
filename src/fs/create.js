f60118153f15e1a531606ed2eed5ac923a87f74f
EvgenKlo <klokovevgen@gmail.com>
2024-01-25 14:59:08 +0500

    refactor: path in create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,11 +1,19 @@
 import fs from "fs/promises";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const create = async () => {
   try {
-    await fs.access("./src/fs/files/fresh.txt");
+    await fs.access(join(__dirname, "files", "fresh.txt"));
     console.error(new Error("FS operation failed"));
   } catch (error) {
-    fs.appendFile("./src/fs/files/fresh.txt", "I am fresh and young");
+    fs.appendFile(
+      join(__dirname, "files", "fresh.txt"),
+      "I am fresh and young"
+    );
   }
 };
 
