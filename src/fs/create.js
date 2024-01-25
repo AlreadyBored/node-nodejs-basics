595992a3cf161eb2322f7d7ed768c8848677126c
Dima Chemerevsky <dchemerevsky@aras.com>
2024-01-25 23:09:07 +0100

    File system (src/fs). Create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,13 @@
+import { existsSync, appendFile } from 'node:fs';
 const create = async () => {
-    // Write your code here 
+	if (existsSync('files/fresh.txt')) {
+		throw new Error('FS operation failed');
+	}
+	await appendFile('files/fresh.txt', 'I am fresh and young', error => {
+		if (error) {
+			throw error;
+		}
+	});
 };
 
 await create();
