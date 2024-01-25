8a354e3a086fe637968f4724c83f57943b3829d9
iamnkt <sg23mj@gmail.com>
2024-01-26 01:45:12 +0300

    fix: fix copy files function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,19 +1,28 @@
 import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { existsSync } from 'node:fs';
-import { cp } from 'node:fs/promises';
+import { readdir, mkdir } from 'node:fs/promises';
+import { resolve } from 'node:path';
+import { pipeline } from 'node:stream/promises';
+import { createReadStream, createWriteStream } from 'node:fs';
 
 const copy = async () => {
   const __dirname = dirname(fileURLToPath(import.meta.url));
   const srcPath = join(__dirname, 'files');
   const destPath = join(__dirname, 'files_copy');
 
-  if (existsSync(destPath)) {
-    throw new Error ('FS operation failed');
-  }
-
   try {
-    await cp(srcPath, destPath, { recursive: true });
+    await mkdir(destPath);
+    const files = await readdir(srcPath);
+
+    for (const file of files) {
+      const curSrc = resolve(srcPath, file);
+      const curDest = resolve(destPath, file);
+
+      await pipeline(
+        createReadStream(curSrc),
+        createWriteStream(curDest)
+      );
+    }
   } catch(e) {
     throw new Error ('FS operation failed');
   }
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'url';
+import { createWriteStream } from 'node:fs';
+import { stdin } from 'node:process';
+
 const write = async () => {
-    // Write your code here 
+	const __dirname = dirname(fileURLToPath(import.meta.url));
+
+	const output = createWriteStream(join(__dirname, 'files', 'fileToWrite.txt'));
+	stdin.pipe(output);
 };
 
 await write();
