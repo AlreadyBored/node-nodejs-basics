c4af0693b332be82ff1c5c0b55b8dfde40f97af3
Eugene Solomonyk <solosoft2000@mail.ru>
2024-01-25 17:00:18 +0200

    feat: wt completed

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,38 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { cpus } from 'os';
+import { Worker } from 'worker_threads';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const workerFile = path.join(__dirname, 'worker.js');
+
 const performCalculations = async () => {
     // Write your code here
+    const numCores = cpus().length;
+    const workers = [];
+    const results = [];
+
+    const createWorker = (data) => {
+      return new Promise((resolve) => {
+        const worker = new Worker(workerFile, {workerData: data});
+
+        worker.on('message', (data) => resolve({status: 'resolved', data}));
+        worker.on('error', () => resolve({status: 'error', data: null}));
+      });
+    };
+
+    for (let i = 0; i < numCores; i++) {
+      workers.push(createWorker(i + 10));
+    }
+
+    const workerResults = await Promise.all(workers);
+
+    for (const result of workerResults) {
+      results.push(result);
+    }
+
+    console.log(results);
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,15 @@
+import { workerData, parentPort } from 'worker_threads';
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
+// Next row for test Error
+// if (Math.random() > 0.5) throw new Error('Generated Error');
+
 const sendResult = () => {
     // This function sends result of nthFibonacci computations to main thread
+    parentPort.postMessage(nthFibonacci(workerData));
 };
+
 
 sendResult();
