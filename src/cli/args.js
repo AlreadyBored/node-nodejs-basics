6fe00415cff00f968fb4b7ade7e35eaed3b005e8
Pavel Sakovich <pavel.sackovich@ingenios.io>
2024-01-25 11:43:08 +0100

    feat: implement "cli" tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+
+  const argOutput = {};
+
+  for (let i = 0; i < args.length; i += 2) {
+    const propName = args[i].replace(/^--/, "");
+    const propValue = args[i + 1];
+    argOutput[propName] = propValue;
+  }
+
+  Object.entries(argOutput).forEach(([propName, propValue]) => {
+    console.log(`${propName} is ${propValue}`);
+  });
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,18 @@
 const parseEnv = () => {
-    // Write your code here 
+  const rssVariables = Object.entries(process.env)
+    .filter(([key, value]) => key.startsWith("RSS_"))
+    .map(([key, value]) => `${key}=${value}`)
+    .join("; ");
+
+  if (rssVariables) {
+    console.log(`RSS Environment Variables: ${rssVariables}`);
+  } else {
+    console.log("No RSS Environment Variables found.");
+  }
 };
 
 parseEnv();
+
+// example
+
+// RSS_NAME1=value1 RSS_NAME2=value2 node env.js
