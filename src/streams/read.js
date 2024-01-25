3bc4cbea7586cc30aacd491744a8c73125bb2826
Pavel Sakovich <pavel.sackovich@ingenios.io>
2024-01-25 14:16:18 +0100

    feat: implement "stream" tasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,27 @@
+import fs from "fs";
+import path from "path";
+
+const targetFileName = "fileToRead.txt";
+const targetFolderName = "files";
+
+const currentFolder = process.cwd();
+
+const pathToTargetFile = path.join(
+  currentFolder,
+  targetFolderName,
+  targetFileName
+);
+
 const read = async () => {
-    // Write your code here 
+  const readStream = fs.createReadStream(pathToTargetFile);
+
+  readStream.on("data", (chunk) => {
+    process.stdout.write(chunk);
+  });
+
+  readStream.on("end", () => {
+    console.log("");
+  });
 };
 
 await read();
