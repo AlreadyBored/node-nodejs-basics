0c5873e01474cc94a06917a8f3e432bd4edd8526
AlexanderFraltsov <kolonessens@yandex.ru>
2024-01-25 17:58:01 +0400

    refactor: wt part

--- src/wt/main.js
+++ src/wt/main.js
@@ -3,31 +3,37 @@ import { join } from 'node:path';
 import os from 'node:os';
 
 const __dirname = import.meta.dirname;
+
 const STARTED_INDEX = 10;
 const WORKER_FILE = 'worker.js';
 
 const performCalculations = async () => {
-	const numberCPUs = os.cpus().length;
-	const data = Array(numberCPUs).fill(null).map((value, index) => STARTED_INDEX + index);
+	const workers = Array(os.cpus().length)
+		.fill(null)
+		.map((value, index) => STARTED_INDEX + index)
+		.map(workerData => new Worker(
+			join(__dirname, WORKER_FILE),
+			{ workerData },
+		));
 
-	const workers = data.map(workerData => new Worker(
-		join(__dirname, WORKER_FILE),
-		{ workerData }
+	const promises = workers.map(worker => new Promise(
+		(res) => {
+			worker.on('message', ({ msg, result }) => {
+				if (msg === 'RESULT') {
+					res(result);
+				}
+			});
+
+			worker.on('error', () => res({
+				data: null,
+				status: 'error',
+			}));
+		},
 	));
 
-	const resultPromises = workers.map(worker => new Promise((res) => {
-		worker.on('message', ({ msg, result }) => {
-			if (msg === 'RESULT') {
-				res(result);
-			}
-		});
-		worker.on('error', () => {
-			res({ data: null, status: 'error' });
-		})
-	}));
-
-	const result = (await Promise.allSettled(resultPromises))
+	const result = (await Promise.allSettled(promises))
 		.map(({ value }) => value);
+
 	console.log(result);
 };
 
