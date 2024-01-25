62b8f7ac8a6772552e10ed604efc66da23f9eb61
Andrei Yasinski <andrew.yasinski@gmail.com>
2024-01-25 12:26:13 +0100

    update args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,19 @@
 const parseArgs = () => {
-    // Write your code here 
+  const result = process.argv
+    .reduce((acc, item, index, array) => {
+      if (item.startsWith("--")) {
+        return [
+          ...acc,
+          { name: item.replace("--", ""), value: array[index + 1] },
+        ];
+      }
+
+      return acc;
+    }, [])
+    .map((item) => `${item.name} is ${item.value}`)
+    .join(", ");
+
+  console.log(result);
 };
 
 parseArgs();
