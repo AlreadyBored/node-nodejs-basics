c79359474e80033b86f516bd195d33a6c4e891c7
valeragorobets <valera.gorobets@gmail.com>
2024-01-25 22:40:04 +0200

    feat: Implement Stream module

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,13 @@
+import { createHash } from 'node:crypto'
+import fs from 'fs';
+
+const filePath = 'src/hash/files/fileToCalculateHashFor.txt';
 const calculateHash = async () => {
-    // Write your code here 
+	const fileContent = fs.readFileSync(filePath).toString();
+	const hex = createHash('sha256')
+		.update(fileContent)
+		.digest('hex');
+	process.stdout.write(hex);
 };
 
 await calculateHash();
