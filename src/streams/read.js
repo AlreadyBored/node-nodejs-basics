cd645d0f1b911fd077ef876cf0d21907f8b9ce28
iamnkt <sg23mj@gmail.com>
2024-01-26 01:52:37 +0300

    fix: fix read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,13 +1,16 @@
 import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'url';
-import { createReadStream } from 'node:fs';
+import { createReadStream, createWriteStream } from 'node:fs';
 import { stdout } from 'node:process';
+import { pipeline } from 'node:stream/promises';
 
 const read = async () => {
   const __dirname = dirname(fileURLToPath(import.meta.url));
 
-  const input = createReadStream(join(__dirname, 'files', 'fileToRead.txt'));
-  input.pipe(stdout);
+  await pipeline(
+    createReadStream(join(__dirname, 'files', 'fileToRead.txt')),
+    stdout
+  );
 };
 
 await read();
