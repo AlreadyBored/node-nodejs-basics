fcac85a33ae44f120c0695ffeab70ce97440fa6c
mikhailyatsenko <misha.ggg@gmail.com>
2024-01-25 23:15:17 +0100

    add read and write funcs

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const pathToFile = path.join(__dirname, "files", "fileToRead.txt");
+
+  const readStream = fs.createReadStream(pathToFile);
+
+  readStream.on("data", (chunk) => {
+    process.stdout.write(chunk);
+  });
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,23 @@
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+  const pathToWriteFile = path.join(__dirname, "files", "fileToWrite.txt");
+
+  process.stdin.on("data", (data) => {
+    fs.writeFile(pathToWriteFile, data.toString(), (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log("\nData was Writed to File fileToWrite.txt\n");
+        process.exit();
+      }
+    });
+  });
 };
 
 await write();
