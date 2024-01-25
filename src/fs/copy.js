7fb575a2f89d85bde7afb7e857d5016bd7deee90
EvgenKlo <118301743+EvgenKlo@users.noreply.github.com>
2024-01-25 21:41:00 +0500

    feat: copy.js done

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,28 @@
+import fs from "fs/promises";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    await fs.access(join(__dirname, "files_copy"));
+    console.error(new Error("FS operation failed in Copy"));
+  } catch (error) {
+    try {
+      const result = await fs.readdir(join(__dirname, "files"));
+      await fs.mkdir(join(__dirname, "files_copy"));
+      result.map((file) =>
+        fs.copyFile(
+          join(__dirname, "files", file),
+          join(__dirname, "files_copy", file)
+        )
+      );
+    } catch (error) {
+      console.error(new Error("FS operation failed"));
+    }
+  }
 };
 
 await copy();
