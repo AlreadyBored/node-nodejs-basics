e653abedcdacc07e2bde5d168d7cc55fc882d831
iamnkt <sg23mj@gmail.com>
2024-01-26 02:05:35 +0300

    feat: add write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -2,12 +2,15 @@ import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'url';
 import { createWriteStream } from 'node:fs';
 import { stdin } from 'node:process';
+import { pipeline } from 'node:stream/promises';
 
 const write = async () => {
 	const __dirname = dirname(fileURLToPath(import.meta.url));
 
-	const output = createWriteStream(join(__dirname, 'files', 'fileToWrite.txt'));
-	stdin.pipe(output);
+	await pipeline(
+		stdin,
+		createWriteStream(join(__dirname, 'files', 'fileToWrite.txt'))
+	)
 };
 
 await write();
