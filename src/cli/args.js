69e9cf9ab80ad828d05bb949e03088c1d8979685
Andrei <kaliarovich93@gmail.com>
2024-01-25 23:08:21 +0200

    feat: add implementation of cli/args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,21 @@
 const parseArgs = () => {
     // Write your code here 
+    const args = process.argv.slice(2);
+    const keys = [];
+    const values = [];
+    const resArr = [];
+
+    args.map((item) => {
+        if (item.startsWith('--')) {
+            keys.push(item)
+        } else {
+            values.push(item)
+        }
+    })
+    keys.forEach((key, index) => {
+        resArr.push(`${key.replace('--', '')} is ${values[index]}`);
+    })
+    console.log(resArr.join(', '));
 };
 
 parseArgs();
