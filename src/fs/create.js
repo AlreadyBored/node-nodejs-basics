257959f5301c09bfab73c87712b8fb8ae7f4ae2d
EvgenKlo <klokovevgen@gmail.com>
2024-01-25 14:15:03 +0500

    feat: create.js done

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,12 @@
+import fs from "fs/promises";
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await fs.access("./src/fs/files/fresh.txt");
+    console.error(new Error("FS operation failed"));
+  } catch (error) {
+    fs.appendFile("./src/fs/files/fresh.txt", "I am fresh and young");
+  }
 };
 
 await create();
