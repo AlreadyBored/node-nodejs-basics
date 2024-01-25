c30c766b3cf58c830c0e03fa0e6f68539de9a337
Eugene <josumkin@yahoo.com>
2024-01-25 17:07:19 +0400

    feat: cli-env implemented

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,16 @@
 const parseEnv = () => {
-    // Write your code here 
+    
+    const variables = process.env;
+    const prefix = 'RSS_';
+    //const prefix = 'Prog';
+    // if you dont see any vars with 'RSS_' prefix, change this prefix to 'Prog' (for example) to see, that code is working
+
+    for (const prop in variables) {
+        if (prop.slice(0, 4) === prefix) {
+            console.log(prop + ' = ' + variables[prop]);
+        }
+    }
+
 };
 
 parseEnv();
