3f848524b0cb05d752aef810a0eb07c809bb9744
Eugene Solomonyk <solosoft2000@mail.ru>
2024-01-25 09:19:25 +0200

    feat: write.js complete

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,38 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const write = async () => {
     // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const writeFile = path.join(__dirname, 'files', 'fileToWrite.txt');
+
+    const writeStream = fs.createWriteStream(writeFile);
+
+    const cursor = () => process.stdout.write('Type here (.end for stop)> ');
+
+    cursor();
+
+    process.stdin.on('data', (chunk) => {
+      const input = chunk.toString().trim();
+      if (input === '.end') {
+        writeStream.end();
+      } else {
+        writeStream.write(input);
+        writeStream.write('\n');
+        cursor();
+      }
+    })
+
+    writeStream.on('finish', () => {
+      console.log('File written');
+      process.exit();
+    })
+
+    writeStream.on('error', (error) => {
+      console.error(error.message);
+    })
 };
 
 await write();
