a50639de1876268206dfac41262cd05d086344aa
net-ka <a.k.fedosova@gmail.com>
2024-01-25 23:03:08 +0100

    feat: add parseEnv and parseArgs functions in cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+    // Write your code here
+
+    const data = process.argv
+        .reduce((acc, item, i, arr) =>
+            item.startsWith('--') ? [ ...acc, `${item.slice(2)} is ${arr[i + 1]}`] : acc, []
+        ).join(', ');
+
+    console.log(data);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+    // Write your code here
+
+    const data = Object.entries(process.env)
+        .filter(([key]) => key.startsWith('RSS_'))
+        .map(([key, value]) => `${key}=${value}`)
+        .join('; ');
+
+    console.log(data);
 };
 
 parseEnv();
