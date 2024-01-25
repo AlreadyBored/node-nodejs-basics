b167266054221d501423fe26efbbffb13118c573
Val-d-emar <vhot@live.ru>
2024-01-25 17:08:45 +0400

    made cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,22 @@
+/**
+ *  implement function that parses command line arguments (given in format 
+ * --propName value --prop2Name value2, you don't need to validate it) 
+ * and prints them to the console in the format propName is value, prop2Name is value2
+ */
+import process from 'process';
 const parseArgs = () => {
     // Write your code here 
+    let str = '';
+    process.argv.forEach((val) => {
+        if (typeof (val) === 'string' && val.length > 2 && val.startsWith("--")) {
+            str = val.slice(2);
+        } else if (str !== '') {
+            str += ` is ${val}`;
+            console.log(str);
+            str = '';
+        }
+        
+    });
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,17 @@
+/**
+ *  implement function that parses environment variables with prefix 
+ * RSS_ and prints them to the console in the format RSS_name1=value1; RSS_name2=value2
+ */
+import process from 'process';
 const parseEnv = () => {
     // Write your code here 
+    const envs = process.env;
+    for (let env in envs) {
+        if( env.startsWith('RSS_')){
+            console.log(`${env}=${envs[env]}`);
+        }
+    }
+
 };
 
 parseEnv();
