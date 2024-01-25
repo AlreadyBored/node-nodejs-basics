e1bbc258b1162567c3baab25dd4d61f9ccd1abbb
Eugene Solomonyk <solosoft2000@mail.ru>
2024-01-25 11:19:38 +0200

    feat: transform.js complete

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
+import fs from 'fs';
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+  // Write your code here
+  const reverseTranform = new Transform({
+    transform(chunk, encoding, callback) {
+      const reversedText = chunk.toString().split('').reverse().join('');
+      callback(null, reversedText+'\n');
+    },
+  });
+  process.stdout.write('Type here >');
+  process.stdin.pipe(reverseTranform).pipe(process.stdout);
 };
 
 await transform();
