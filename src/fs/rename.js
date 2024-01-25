d23adae09c2fed2246cdaccc89daa4ae6cb279e0
Eugene <josumkin@yahoo.com>
2024-01-25 15:08:23 +0400

    feat: fs-rename implemented

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,30 @@
+import { dirname } from 'path';
+import { existsSync, fileURLToPath } from 'url';
+import { renameSync } from 'fs';
+
+const currentPath = fileURLToPath(import.meta.url);
+const currentDir = dirname(currentPath);
+const srcFile = currentDir + '/files/' + 'wrongFilename.txt';
+const distFile = currentDir + '/files/' + 'properFilename.md';
+
 const rename = async () => {
-    // Write your code here 
+
+    if (!existsSync(srcFile)) {
+        console.error('source file doesnt exist');
+        return;
+    }
+
+    if (existsSync(distFile)) {
+        console.error('destination file already exist');
+        return;
+    }
+    
+    try {
+        renameSync(srcFile, distFile);
+    } catch (err) {
+        throw new Error(err)
+    }
+
 };
 
 await rename();
