edfa511cd9959b5401bd496533065d47d1abe3e8
net-ka <a.k.fedosova@gmail.com>
2024-01-25 01:11:32 +0100

    feat: add read functions in fs

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import * as fs from 'fs/promises';
+import {ERROR_MESSAGES} from "./constants/index.js";
+
 const read = async () => {
-    // Write your code here 
+    // Write your code here
+
+    const errMsg = ERROR_MESSAGES.OPERATION_FAILED;
+    const sourcePath = 'src/fs/files/fileToRead.txt';
+
+    try {
+        const content = await fs.readFile(sourcePath);
+        console.log(content.toString('utf8'));
+    } catch (err) {
+        throw new Error(errMsg);
+    }
 };
 
 await read();
