4528030e7a1b3e2cecb8cd34878ca9c1e4e0d7e7
Pavel Sakovich <pavel.sackovich@ingenios.io>
2024-01-25 11:38:15 +0100

    feat: implement "fs" tasks

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import fs from "fs";
+
+const path = "files/fresh.txt";
+
 const create = async () => {
-    // Write your code here 
+  fs.access(path, fs.constants.F_OK, (err) => {
+    if (err) {
+      fs.appendFile(path, "I am fresh and young", (err) => {
+        console.log(err);
+      });
+    } else {
+      throw new Error("FS operation failed");
+    }
+  });
 };
 
 await create();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,28 @@
+import fs from "fs/promises";
+import path from "path";
+
+const targetFile = "wrongFilename.txt";
+const outputFile = "properFilename.md";
+const sourceFolder = "files";
+const pathTotTargetFile = path.join(process.cwd(), sourceFolder, targetFile);
+const pathTotOutputFile = path.join(process.cwd(), sourceFolder, outputFile);
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    const files = await fs.readdir(sourceFolder);
+
+    const isExistTargetFile = files.includes(targetFile);
+    const isExistOutputFile = files.includes(outputFile);
+
+    if (isExistTargetFile && !isExistOutputFile) {
+      await fs.rename(pathTotTargetFile, pathTotOutputFile);
+      console.log("File renamed successfully.");
+    } else {
+      throw new Error("FS operation failed");
+    }
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await rename();
