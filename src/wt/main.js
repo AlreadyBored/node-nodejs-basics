baec55639731e02dea1c65dfe067d7ad4851040d
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-01-26 22:30:08 +0300

    feat: implement wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,36 @@
+import { Worker, isMainThread, parentPort, workerData } from "worker_threads";
+import os from "os";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const workerFile = join(__dirname, "worker.js");
+
 const performCalculations = async () => {
-    // Write your code here
+    if (isMainThread) {
+        const num = os.cpus().length;
+        const results = new Array(num);
+
+        for (let i = 0, completedWorkers = 0; i < num; i++) {
+        const worker = new Worker(workerFile, { workerData: 10 + i });
+        worker
+            .on("message", (result) => {
+                results[i] = { status: "resolved", data: result };
+                completedWorkers++;
+                if (completedWorkers === num) {
+                console.log(results);
+                }
+            })
+            .on("error", (error) => {
+                results[i] = { status: "error", data: null };
+                completedWorkers++;
+                if (completedWorkers === num) {
+                console.log(results);
+                }
+            });
+        }
+    }
 };
 
 await performCalculations();
