2c7caefc4d1b5e28eb4ae2b36e983f6f67c2fbce
Val-d-emar <vhot@live.ru>
2024-01-26 15:29:47 +0400

    done zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,26 @@
+/**
+ * implement function that compresses file fileToCompress.txt to archive.gz using zlib and Streams API
+ */
+import { access, constants, createReadStream, createWriteStream } from 'fs';
+import { createGzip } from "zlib";
+import { join as platform_path } from 'path';
+
 const compress = async () => {
     // Write your code here 
+    let infile = platform_path('files', 'fileToCompress.txt');
+    access(infile, constants.F_OK, (err) => {
+        if (err) {
+            throw new Error('FS operation failed');
+        } else {
+            return createReadStream(infile)
+                .pipe(createGzip())
+                .pipe(createWriteStream(platform_path('files', 'archive.gz')))
+                .on('error', () => {
+                    throw new Error('FS operation failed');
+                });
+        }
+    });
+
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,26 @@
+/**
+ * implement function that decompresses archive.gz back to the 
+ * fileToCompress.txt with same content as before compression using zlib and Streams API
+ */
+import { access, constants, createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from "zlib";
+import { join as platform_path } from 'path';
+
 const decompress = async () => {
     // Write your code here 
+    let gzfile = platform_path('files', 'archive.gz');
+    access(gzfile, constants.F_OK, (err) => {
+        if (err) {
+            throw new Error('FS operation failed');
+        } else {
+            return createReadStream(gzfile)
+                .pipe(createGunzip())
+                .pipe(createWriteStream(platform_path('files', 'fileToCompress0.txt')))
+                .on('error', () => {
+                    throw new Error('FS operation failed');
+                });
+        }
+    });
 };
 
 await decompress();
