417c95cffb789dc5d618080acc96fb1ae07a16c4
Dima Chemerevsky <dchemerevsky@aras.com>
2024-01-26 22:21:36 +0100

    File system (src/fs). Rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,14 @@
+import { rename as renameFile, existsSync } from 'node:fs';
 const rename = async () => {
-    // Write your code here 
+	const errorMessage = 'FS operation failed';
+	if (existsSync('files/properFilename.md') || !existsSync('files/wrongFilename.txt')) {
+		throw new Error(errorMessage);
+	}
+	await renameFile('files/wrongFilename.txt', 'files/properFilename.md', error => {
+		if (error) {
+			throw error;
+		}
+	});
 };
 
 await rename();
