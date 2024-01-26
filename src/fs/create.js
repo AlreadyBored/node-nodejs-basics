d91a5b1a454549f6f2ae942dcbf9cb5ea10b86a3
rahunak <euhene@linnovate.net>
2024-01-26 23:58:45 +0300

    #1: refactor, added comments

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,16 +1,26 @@
 import { promises as fs } from "fs";
 import path from "path";
+import { fileURLToPath } from 'url';
 
 const create = async () => {
-  const __dirname = path.dirname("fresh.txt");
-  const filePath = path.join(__dirname, "src/fs/files", "fresh.txt");
+  // Script file.
+  const __filename = fileURLToPath(import.meta.url);
+  // Folder of script file.
+  const __dirname = path.dirname(__filename);
+  // fileForCreate for creating fresh file.
+  const fileForCreate = path.join(__dirname, "files",'fresh.txt');
+
   try {
-    await fs.access(filePath);
+    // Check if file exists.
+    await fs.access(fileForCreate);
     throw new Error("FS operation failed");
   } catch (err) {
+    // Create fresh file.
     if (err.code === "ENOENT") {
-      await fs.writeFile(filePath, "I am fresh and young");
+      // "ENOENT" - No such file or directory
+      await fs.writeFile(fileForCreate, "I am fresh and young");
     } else {
+      // the rest errors.
       throw err;
     }
   }
