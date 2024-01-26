e9fbe222aeee727776d3f4a8cecbfafa055639e0
irina-mokh <75081244+irina-mokh@users.noreply.github.com>
2024-01-26 23:17:45 +0300

    feat: add worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,37 @@
+import {availableParallelism} from 'node:os';
+import { Worker } from 'node:worker_threads';
+import { getDir } from '../utils.js';
+import path from 'node:path';
+
+const dir = getDir(import.meta.url);
+const workerPath = path.join(dir, 'worker.js');
+const START = 10;
+const arr = Array(availableParallelism()).fill(0);
+
 const performCalculations = async () => {
-    // Write your code here
+    const promisesArr = arr.map((_, i) => 
+        new Promise ((resolve, reject) => {
+            const worker = new Worker(workerPath, { workerData: START + i});
+
+            worker.on('message', value => {
+                resolve({
+                    status: 'resolved',
+                    data: value 
+                });
+            });
+
+            worker.on('error', () => {
+                resolve({
+                    status: 'error',
+                    data: null
+                });
+            });
+
+        })
+    );
+    const res = await Promise.all(promisesArr);
+    console.log(res);
+    
 };
 
 await performCalculations();
