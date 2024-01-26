18ed1f5d2e4c359a4c5780f6d5916f96c2d598ab
PopovaOlha <fc.odess.10@gmail.com>
2024-01-26 19:04:18 +0200

    feat: update args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,22 @@
 const parseArgs = () => {
-    // Write your code here 
-};
-
-parseArgs();
+    const args = process.argv.slice(2);
+  
+    const parsedArgs = args.reduce((acc, arg, index, array) => {
+      if (arg.startsWith('--')) {
+        const currentKey = arg.slice(2);
+        acc[currentKey] = null;
+      } else if (index > 0 && array[index - 1].startsWith('--')) {
+        const currentKey = array[index - 1].slice(2);
+        acc[currentKey] = arg;
+      }
+      return acc;
+    }, {});
+  
+    for (const [key, value] of Object.entries(parsedArgs)) {
+      if (value !== null) {
+        console.log(`${key} is ${value}`);
+      }
+    }
+  };
+  
+  parseArgs();
