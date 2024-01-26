545c2d6ac15ed3fa625a143fb5d4c18da7b175cc
prostonau <prostonau@mail.ru>
2024-01-27 00:23:55 +0300

    feat:Zlib

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,31 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import zlib from 'zlib';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const pathToFile = join(__dirname, 'files', 'fileToCompress.txt');
+const outputPath = join(__dirname, 'files', 'archive.gz');
+
 const compress = async () => {
-    // Write your code here 
+  const readStream = createReadStream(pathToFile);
+  const writeStream = createWriteStream(outputPath);
+  const gzip = zlib.createGzip();
+
+  readStream.pipe(gzip).pipe(writeStream);
+
+  return new Promise((resolve, reject) => {
+    writeStream.on('finish', () => {
+      console.log('File compressed successfully');
+      resolve();
+    });
+    writeStream.on('error', (error) => {
+      console.log('Some error with gzip');
+      reject(error);
+    });
+  });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,31 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import zlib from 'zlib';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const pathToFile = join(__dirname, 'files', 'archive.gz');
+const outputPath = join(__dirname, 'files', 'fileToCompress.txt');
+
 const decompress = async () => {
-    // Write your code here 
+  const readStream = createReadStream(pathToFile);
+  const writeStream = createWriteStream(outputPath);
+  const gunzip = zlib.createGunzip();
+
+  readStream.pipe(gunzip).pipe(writeStream);
+
+  return new Promise((resolve, reject) => {
+    writeStream.on('finish', () => {
+      console.log('File decompressed successfully');
+      resolve();
+    });
+    writeStream.on('error', (error) => {
+      console.log('Some error with gunzip');
+      reject(error);
+    });
+  });
 };
 
 await decompress();
