83b39a718d3a27185789fbfe8227c4efbbe14383
Eugene <josumkin@yahoo.com>
2024-01-26 16:41:01 +0400

    feat: streams-read task implemented

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import { createReadStream } from 'node:fs';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const currentPath = fileURLToPath(import.meta.url);
+const currentDir = dirname(currentPath);
+const readFile = currentDir + '/files/fileToRead.txt';
+
 const read = async () => {
-    // Write your code here 
+    const stream = createReadStream(readFile);
+    stream.pipe(process.stdout);
 };
 
 await read();
