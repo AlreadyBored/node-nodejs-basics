b5c002e40c32e6cbe1c95b10b1debec90cd0a5b0
irina-mokh <75081244+irina-mokh@users.noreply.github.com>
2024-01-26 14:01:23 +0300

    fix: remove using cp as non stable

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,16 +1,32 @@
-import { cp } from 'fs/promises';
+import { copyFile, mkdir, readdir } from 'fs/promises';
 import path from 'path';
 import { getDir } from './utils.js';
 
-const src = path.join(getDir(), 'files');
-const res = path.join(getDir(), 'files_copy');
+const dir = getDir();
+const src = path.join(dir, 'files');
+const target = path.join(dir, 'files_copy');
+
+async function copyDir(src, dest) {
+    let items = await readdir(src, { recursive: true, withFileTypes: true });
+
+    for (let item of items) {
+        let srcPath = path.join(item.path, item.name);
+        let destPath = srcPath.replace(src, dest);
+        let destDir = path.dirname(destPath);
+        
+        if (item.isFile()) {
+            await mkdir(destDir, { recursive: true })
+            await copyFile(srcPath, destPath);
+        }
+    }
+}
 
 const copy = async () => {
     try {
-		await cp( src, res, {recursive: true ,force: false, errorOnExist: true});
+        copyDir(src, target);
     } catch {
         throw new Error('FS operation failed');
-    }
-};
+    }    
+}
 
 await copy();
