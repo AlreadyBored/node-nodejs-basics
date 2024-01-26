ce4a672018d9b1dc923f397710f252baeb1cb951
Andrei Ihnatsiuk <garnich@tut.by>
2024-01-26 11:56:20 +0300

    node-basics: src/hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,30 @@
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { access, constants, readFile } from 'node:fs';
+import { createHash } from 'node:crypto';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+    access(filePath, constants.F_OK, (err) => {
+        if (err) {
+            throw new Error('FS operation failed');
+        } else {
+            readFile(filePath, ((err, data) => {
+                if(err) {
+                    throw err
+                }
+
+                const hash = createHash('sha256');
+                hash.update(Buffer.from(data).toString());
+                const hex = hash.digest('hex');
+                console.log(hex);
+            }))
+        }
+    })
 };
 
 await calculateHash();
