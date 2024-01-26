8a1f9f9ccfb5f4ed09ec9be96d5fcc035c8c0d92
Andrei Ihnatsiuk <garnich@tut.by>
2024-01-26 13:32:53 +0300

    node-basics: src/wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,47 @@
+import { Worker } from 'node:worker_threads';
+import os from 'node:os';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+const workerPath = join(__dirname, 'worker.js');
+
+const STATUS = {
+    RESOLVED: 'resolved',
+    ERROR: 'error',
+  }
+
 const performCalculations = async () => {
-    // Write your code here
+    function runWorker(data) {
+        return new Promise((resolve, reject) => {
+            const worker = new Worker(workerPath, {
+                workerData: { num: data, isError: process.argv[2] }
+            })
+            
+            worker.on('message', msg => resolve({ status: STATUS.RESOLVED, data: msg }));
+            worker.on('error', () => resolve({ status: STATUS.ERROR, data: null }));
+            worker.on('exit', (code) => {
+                if (code !== 0) reject(new Error(`Worker stopped with exit code ${code}`));
+            })
+        }) 
+    }
+    
+    function createRuns() {
+        const arrOfPromises = [];
+        const numOfCpu = os.cpus().length ;
+
+        for (let i = 0;  i <= numOfCpu - 1; i++) {
+            arrOfPromises.push(runWorker(10 + i))
+        }
+
+        return arrOfPromises;
+    }
+
+    const runsArr = createRuns();
+
+    
+    await Promise.all(runsArr).then(val => console.log(val))
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,23 @@
+import { workerData, parentPort } from 'node:worker_threads';
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
     // This function sends result of nthFibonacci computations to main thread
+
+    const result = `res: ${nthFibonacci(workerData.num)}, index: ${ workerData.num - 10}`;
+
+    if (workerData.isError === '--withErrors') {
+        if(Math.random() > 0.5 ) {
+            parentPort.postMessage(result)
+        }
+        else {
+            throw new Error('Error');
+        }
+    } else {
+        parentPort.postMessage(result)
+    }
 };
 
 sendResult();
