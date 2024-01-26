9363ae16c9021f42a0bb023e7e7b7dab2b87bd1f
Dmitry Bludchy <dmitrybludchy@gmail.com>
2024-01-26 22:48:44 +0300

    feat: implement read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,28 @@
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { readFile, access } from 'fs/promises';
+
+const pathExists = (path) => {
+  return access(path)
+    .then(() => true)
+    .catch(() => false);
+};
+
 const read = async () => {
-    // Write your code here 
+  const currentDir = dirname(fileURLToPath(import.meta.url));
+  const targetDir = join(currentDir, 'files');
+  const filePath = join(targetDir, 'fileToRead.txt');
+
+  const fileExists = pathExists(filePath);
+
+  if (!fileExists) throw new Error('FS operation failed');
+
+  try {
+    const contents = await readFile(filePath, { encoding: 'utf8' });
+    console.log(contents);
+  } catch (err) {
+    console.error('Error reading the file:', err.message);
+  }
 };
 
 await read();
