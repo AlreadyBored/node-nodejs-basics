4c8f19b2fe2a31822e2ee11d59c5cf35b944bc4c
Mmishaaa <kkochin2528@gmail.com>
2024-01-26 18:30:46 +0300

    feat: add hash module

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,8 +1,17 @@
 import crypto from "crypto";
+import fs from "node:fs/promises";
+import * as url from 'url';
+import path from "path";
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const pathToFile = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
 
 const calculateHash = async () => {
-    const hash = crypto("sha256");
-    
+    const data = await fs.readFile(pathToFile);
+    const hash = crypto.createHash("SHA256")
+                        .update(data)
+                        .digest("hex");   
+    console.log(hash);
 };
 
 await calculateHash();
