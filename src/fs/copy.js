58ac2efbd3bc002809dd23c82aec5d9902ffc816
Alexey0795 <alexey0795@mail.ru>
2024-01-26 22:17:56 +0300

    feat: create and copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,26 @@
-const copy = async () => {
-    // Write your code here 
-};
+import { existsSync } from 'node:fs';
+import { cp } from 'node:fs/promises';
+import path from 'node:path';
 
+const copy = async () => {
+    const src = path.resolve('src/fs/files/')
+    const dest = path.resolve('src/fs/files_copy/')
+
+    if (!existsSync(src)) {
+        console.log('src folder not exists!\n')
+        throw new Error('FS operation failed')
+    }
+
+    if (existsSync(dest)) {
+        console.log('dest folder exists already!\n')
+        throw new Error('FS operation failed')
+    }
+
+    await cp(src, dest, {
+        recursive: true,
+        force: false,
+        errorOnExist: true,
+    });
+    console.log('copy done!')
+}
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,16 @@
+import { appendFile } from 'node:fs/promises';
+import { existsSync } from 'node:fs';
+import path from 'node:path';
+
 const create = async () => {
-    // Write your code here 
+    const filenema = path.resolve('src/fs/files/fresh.txt')
+    if (existsSync(filenema)) {
+        console.log('cap: file was not created!')
+        throw new Error('FS operation failed')
+    } else {
+        console.log('cap: file was created!')
+        await appendFile(filenema, 'I am fresh and young');
+    }
 };
 
 await create();
