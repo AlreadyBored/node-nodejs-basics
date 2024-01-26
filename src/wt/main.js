411568637d344faee7623813beda3fa428e1f24a
Fatima Abbasquluzada <faabbasquluzada@gmail.com>
2024-01-26 02:32:41 -0800

    Worker Threads (src/wt) done

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,32 @@
+import { Worker } from "worker_threads";
+import path, { resolve } from "path";
+import {cpus} from "os";
+import { getDirnameFromUrl } from "../utils/utils";
+
+const START_NUMBER = 10;
+
+const __dirname = getDirnameFromUrl(import.meta.url);
+const workerScript = path.join(__dirname, "worker.js");
+
+const fibonacciWorkerService = (number) => new Promise (resolve => {
+    const worker = new Worker(workerScript, { workerData: number});
+
+    worker.on('message', data => resolve({
+        status:"resolve",
+        data
+    }));
+
+    worker.on('error', data => resolve({
+        status:"error",
+        data:null
+    }));
+})
+
+
 const performCalculations = async () => {
-    // Write your code here
+    const workersPool = Array.from({length: cpus().length}, (_, i)=>fibonacciWorkerService(START_NUMBER + i));
+    const result = await Promise.all(workersPool);
+    console.log(result);
 };
 
 await performCalculations();
