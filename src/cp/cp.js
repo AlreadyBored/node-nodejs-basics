0451e92226a05f6bb88ee62bea3b4f6702dee726
Andrei Ihnatsiuk <garnich@tut.by>
2024-01-26 13:49:03 +0300

    node-basics: src/cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,23 @@
+import { argv, stdin } from 'node:process';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { spawn } from 'node:child_process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = join(__dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childProcess = spawn('node', [filePath, ...args], { stdio: [stdin, 'pipe'] });
+
+    childProcess.stdout.on('data', (data) => {
+        console.log(`Received from child process:\n${data}`);
+    });
+
+    childProcess.on('spawn', () => {
+        console.log('Spawn child process!');
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(argv.slice(2));
