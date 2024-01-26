3c289d0a0781b3fa1530aba0ecb1decd35d7c1aa
valeragorobets <valera.gorobets@gmail.com>
2024-01-26 17:02:46 +0200

    feat: Implement wt module

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,44 @@
+import { Worker } from 'node:worker_threads';
+import os from 'os';
+
+const threads = new Map();
+const results = new Map();
+
+const onExit = (workerId) => {
+	threads.delete(workerId);
+	if (threads.size === 0) {
+		const keys = [...results.keys()].sort();
+		const resultsForPrinting = keys.reduce((previousValue, key) => [...previousValue, results.get(key)], []);
+		console.log(resultsForPrinting);
+	}
+};
+
+const handleResult = (workerId, result) => {
+	results.set(workerId, result);
+};
+
+const createWorker = (workerId, workerData) => {
+	const worker = new Worker('./src/wt/worker.js', { workerData });
+	worker.on('message', (message) => handleResult(workerId, message));
+	worker.on('error', (error) => {
+		const res = {
+			status: 'error',
+			data: null,
+		};
+		handleResult(workerId, res);
+	});
+	worker.on('exit', () => onExit(workerId));
+	return worker;
+};
+
 const performCalculations = async () => {
-    // Write your code here
+	const threadCount = os.cpus().length;
+	const offset = 10;
+
+	for (let i = 0; i < threadCount; i++) {
+		const worker = createWorker(i, { n: offset + i });
+		threads.set(i, worker);
+	}
 };
 
 await performCalculations();
