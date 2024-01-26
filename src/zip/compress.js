73663527a19eba91296f78f8bd02bc97f784bfd3
Fatima Abbasquluzada <faabbasquluzada@gmail.com>
2024-01-26 02:04:48 -0800

    Zlib (src/zip) done

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,26 @@
+import {  createWriteStream, createReadStream  } from "fs";
+import path from "path";
+import { getDirnameFromUrl } from "../utils/utils";
+import { createGzip } from "zlib";
+import { pipeline } from "stream";
+
+const __dirname = getDirnameFromUrl(import.meta.url);
+const sourceFile = path.join(__dirname, "files", "fileToCompress.txt");
+const zippedFile = path.join(__dirname, "files", "archive.gz");
+
+
+const sourceFileStream = createReadStream(sourceFile );
+const zipStream = createGzip();
+const zippedFileStream = createWriteStream(zippedFile );
+
+
 const compress = async () => {
-    // Write your code here 
+    pipeline(
+        sourceFileStream,
+        zipStream,
+        zippedFileStream,
+        (error) => console.error(error)
+       )
 };
 
 await compress();
