3370a90e71fee88b4c4486c25f6eb0fbca018ffc
Dmitry Bludchy <dmitrybludchy@gmail.com>
2024-01-26 22:20:18 +0300

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,53 @@
+import { readdir, stat, access } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const pathExists = async (path) => {
+  try {
+    await access(path);
+    return true;
+  } catch {
+    return false;
+  }
+};
+
+const printDir = async (dirPath) => {
+  const items = await readdir(dirPath);
+
+  await Promise.all(
+    items.map(async (item) => {
+      const itemPath = join(dirPath, item);
+
+      try {
+        const itemStat = await stat(itemPath);
+
+        if (itemStat.isFile()) {
+          console.log(itemPath);
+        } else if (itemStat.isDirectory()) {
+          await printDir(itemPath);
+        }
+      } catch (error) {
+        console.error(`Error processing item ${item}: ${error.message}`);
+      }
+    })
+  );
+
+  return;
+};
+
 const list = async () => {
-    // Write your code here 
+  const currentDir = dirname(fileURLToPath(import.meta.url));
+  const srcDir = join(currentDir, 'files');
+
+  const dirExists = await pathExists(srcDir);
+
+  if (!dirExists) throw new Error('FS operation failed');
+
+  try {
+    await printDir(srcDir);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await list();
