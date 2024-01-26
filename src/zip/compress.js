7f7fa9ccffb48ffc6ba03235c065075b921e7cb3
valeragorobets <valera.gorobets@gmail.com>
2024-01-26 10:19:56 +0200

    feat: implement Zip module

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import fs from 'fs';
+import zlib from 'zlib';
+
+const filesDir = 'src/zip/files/fileToCompress.txt';
+const archiveDir = 'src/zip/archive.gz';
+
 const compress = async () => {
-    // Write your code here 
+	const gzip = zlib.createGzip();
+	const inputStream = fs.createReadStream(filesDir);
+	const outputStream = fs.createWriteStream(archiveDir);
+
+	inputStream
+		.pipe(gzip)
+		.pipe(outputStream);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,17 @@
+import fs from 'fs';
+import zlib from 'zlib';
+
+const decompressedFilesDir = 'src/zip/files/fileToCompress-decompressed.txt';
+const archiveDir = 'src/zip/archive.gz';
+
 const decompress = async () => {
-    // Write your code here 
+	const unzip = zlib.createUnzip();
+	const inputStream = fs.createReadStream(archiveDir);
+	const outputStream = fs.createWriteStream(decompressedFilesDir);
+
+	inputStream
+		.pipe(unzip)
+		.pipe(outputStream);
 };
 
 await decompress();
