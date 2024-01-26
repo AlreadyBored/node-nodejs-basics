507bc72d5462298bb1491a2d9980be5dd32b2cac
Eugene <josumkin@yahoo.com>
2024-01-26 17:06:03 +0400

    feat: streams-write task implemented

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import { createWriteStream } from 'node:fs';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const currentPath = fileURLToPath(import.meta.url);
+const currentDir = dirname(currentPath);
+const writeFile = currentDir + '/files/fileToWrite.txt';
+
 const write = async () => {
-    // Write your code here 
+    const stream = createWriteStream(writeFile);
+    process.stdin.pipe(stream);
 };
 
 await write();
