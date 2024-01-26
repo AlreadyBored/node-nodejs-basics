3d3efc16e4b0d8a9f45807cbfb6d7f40a76ee955
rahunak <euhene@linnovate.net>
2024-01-27 01:45:12 +0300

    #4: delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,29 @@
+import { promises as fs } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { readdir } from "node:fs/promises";
+import { rename as fs_rename } from "node:fs";
+async function isExist(where) {
+  try {
+    await fs.access(where, fs.constants.F_OK);
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
+}
+
 const remove = async () => {
-    // Write your code here 
+  // Script file.
+  const __filename = fileURLToPath(import.meta.url);
+  // Folder of script file.
+  const __dirname = path.dirname(__filename);
+  // check if exist file with wrong name.
+  await isExist(path.join(__dirname, "files", "fileToRemove.txt"));
+  //removes file fs.rm('pathTofile', options, callback).
+  await fs.rm(path.join(__dirname, "files", "fileToRemove.txt"), {}, (err) => {
+    if (err) {
+      throw new Error("FS operation failed");
+    }
+  });
 };
 
 await remove();
