9c58e521ba2e2be92aaaf9f77c7c76296cfd4de3
Андрей Едунов <a.edunov@vk.team>
2024-01-26 19:51:37 +0300

    6. zlib

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,12 @@
+import { createReadStream, createWriteStream } from "fs";
+import { createGzip } from "zlib";
+
 const compress = async () => {
-    // Write your code here 
+  const gzip = createGzip();
+  const rs = createReadStream("./src/zip/files/fileToCompress.txt");
+  const ws = createWriteStream("./src/zip/files/archive.gz");
+
+  rs.pipe(gzip).pipe(ws);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,12 @@
+import { createReadStream, createWriteStream } from "fs";
+import { createUnzip } from "zlib";
+
 const decompress = async () => {
-    // Write your code here 
+  const unzip = createUnzip();
+  const rs = createReadStream("./src/zip/files/archive.gz");
+  const ws = createWriteStream("./src/zip/files/fileToCompress2.txt");
+
+  rs.pipe(unzip).pipe(ws);
 };
 
 await decompress();
