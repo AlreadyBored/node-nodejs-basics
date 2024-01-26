7031687e336831fe8b79dc62e0f05b9332eb8ce2
Anthony M <aantonij@gmail.com>
2024-01-26 20:14:33 +0300

    feat: implement cli & modules tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv;
+
+    const argsArray = [];
+
+    args.forEach((value, index) => {
+        if (value.startsWith('--')) {
+            argsArray.push(`${value.substring(2)} is ${args[index + 1]}`);
+        }
+    });
+
+    console.log(argsArray.join(', '));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,17 @@
+const prefix = 'RSS_';
+
 const parseEnv = () => {
-    // Write your code here 
+    const envValues = process.env;
+
+    const print = Object.keys(envValues).reduce((result, key) => {
+        if (key.startsWith(prefix)) {
+            result += `${result.length !== 0 ? '; ' : '' }${key}=${envValues[key]}`;
+        }
+
+        return result;
+    }, '');
+
+    console.log(print);
 };
 
 parseEnv();
