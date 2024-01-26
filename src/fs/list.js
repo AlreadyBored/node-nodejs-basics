283c5e64c640e6ad9714539dc6110a3049b4f948
Dima Chemerevsky <dchemerevsky@aras.com>
2024-01-26 22:37:36 +0100

    File system (src/fs). List.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import { existsSync, readdir} from 'node:fs';
 const list = async () => {
-    // Write your code here 
+	const errorMessage = 'FS operation failed';
+	const path = 'files';
+	if (!existsSync(path)) {
+		throw new Error(errorMessage);
+	}
+	await readdir(path, (error, files) => {
+		if (error) {
+			throw error;
+		}
+		files.forEach(fileName => {
+			console.log(fileName);
+		});
+	});
 };
 
 await list();
