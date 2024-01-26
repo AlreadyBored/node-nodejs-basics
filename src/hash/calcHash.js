3a4122375d4ed09d5ebfe28e63558648cc5c3c35
Andrei Yasinski <andrew.yasinski@gmail.com>
2024-01-26 14:13:42 +0100

    update calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import fs from "fs";
+import crypto from "crypto";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const stream = fs.createReadStream(
+    "src/hash/files/fileToCalculateHashFor.txt"
+  );
+  const hash = crypto.createHash("SHA256");
+
+  stream.on("data", (data) => {
+    hash.update(data);
+  });
+
+  stream.on("end", () => {
+    console.log(hash.digest("hex"));
+  });
 };
 
 await calculateHash();
