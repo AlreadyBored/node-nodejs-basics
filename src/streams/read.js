4f81e7017c92779bc89e80db6ce5aba10c792854
Karina Shupik <77788896h@gmail.com>
2024-01-26 12:00:05 +0200

    feat: implement read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,25 @@
+import { promises as fsPromises, createReadStream } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import * as path from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const read = async () => {
-    // Write your code here 
+    const fileSourcePath = path.join(__dirname, 'files', 'fileToRead.txt')
+
+    const fileReadStream = createReadStream(fileSourcePath)
+
+    //print data to process.stdout
+    fileReadStream.on('data', (item) => {
+        process.stdout.write(item);
+    });
+
+    //event listener to handle any errors during reading
+    fileReadStream.on('error', (err) => {
+        console.error('Error reading file:', err);
+    });
+
 };
 
 await read();
