4675678728651f58b5a333cc665227fcfca6e2f2
Dima Chemerevsky <dchemerevsky@aras.com>
2024-01-26 22:42:20 +0100

    File system (src/fs). Read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import { existsSync, readFile} from 'node:fs';
 const read = async () => {
-    // Write your code here 
+	const errorMessage = 'FS operation failed';
+	const pathToFile = 'files/fileToRead.txt';
+	if (!existsSync(pathToFile)) {
+		throw new Error(errorMessage);
+	}
+	await readFile(pathToFile, 'utf8', (error, data) => {
+		if (error) {
+			throw error;
+		}
+		console.log(data);
+	});
 };
 
 await read();
