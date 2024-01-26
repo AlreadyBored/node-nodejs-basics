ef364855e7ab8a33b40dcb353472675ed8c4f68b
rahunak <euhene@linnovate.net>
2024-01-27 01:38:42 +0300

    #3: rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,34 @@
+import { promises as fs } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { readdir } from "node:fs/promises";
+import { rename as fs_rename } from "node:fs";
+
+async function isExist(where) {
+  try {
+    await fs.access(where, fs.constants.F_OK);
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
+}
+
 const rename = async () => {
-    // Write your code here 
+  // Script file.
+  const __filename = fileURLToPath(import.meta.url);
+  // Folder of script file.
+  const __dirname = path.dirname(__filename);
+  // check if exist file with wrong name.
+  await isExist(path.join(__dirname, "files", "wrongFilename.txt"));
+  // rename file.
+  fs_rename(
+    path.join(__dirname, "files", "wrongFilename.txt"),
+    path.join(__dirname, "files", "properFilename.md"),
+    (err) => {
+      if (err) {
+        throw new Error("FS operation failed");
+      }
+    }
+  );
 };
 
 await rename();
