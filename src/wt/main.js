ce5f86d1aeb6b478cbe5a7f3111d7bd79fc0fa8f
prostonau <prostonau@mail.ru>
2024-01-27 20:02:59 +0300

    feat: wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,45 @@
+import { Worker} from 'worker_threads';
+import os from 'os'
+import { fileURLToPath } from 'url';
+import { dirname, join, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const workerFilePath = join(__dirname, 'worker.js');
+
 const performCalculations = async () => {
-    // Write your code here
-};
+    let allCores = os.cpus();
+    // console.log('numCores = ', numCores);
+    // numCores = 2
+
+    let results = [];
+    let currentNumber = 10;
+
+    results = await Promise.allSettled(allCores.map(() => {
+      return new Promise((resolve, reject) => {
+        const worker = new Worker(workerFilePath, {workerData: currentNumber++});
+        worker.on('message', (message) => {          
+          //console.log(`Worker ${currentNumber}:`, message);
+          return resolve(message);
+        });
+        worker.on('error', (message) => {          
+          return reject(message);
+        });
+      });
+    }));
+
+    // console.log("results = ", results);
+
+    const output = results.map((k) => {
+      return {
+        'status': k.status === 'fulfilled' ? 'resolve' : 'error',
+        'data': k.status === 'fulfilled' ? k.value : null,
+      }
+    });
+
+    console.log("output = ", output);
+    return output;
+
+};   
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,11 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { workerData, parentPort} from 'worker_threads';
 
-const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+export const nthFibonacci = (n) => (n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2));
+
+export const sendResult = () => {
+  const result = parentPort.postMessage(nthFibonacci(workerData));
+  // console.log(`workerData = ${workerData} | result = ${result}`);
+  return parentPort.postMessage(nthFibonacci(workerData));
 };
 
 sendResult();
