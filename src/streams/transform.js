0661f4c875e3075c5746a927fbd59a7bea979711
Mmishaaa <kkochin2528@gmail.com>
2024-01-27 10:30:15 +0300

    fix: change '\n' to 'os.EOL' in transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,4 +1,5 @@
 import { Transform } from"stream";
+import os from "os";
 
 const standartInput = process.stdin;
 const standartOutput = process.stdout;
@@ -6,7 +7,7 @@ const standartOutput = process.stdout;
 const transformStream = new Transform({
     transform(chunk, _, callback) {
         const reversedChunk = chunk.toString().split('').reverse().join('');
-        callback(null, `${reversedChunk}\n`);
+        callback(null, `${reversedChunk} ${os.EOL}`);
     }
 })
 
