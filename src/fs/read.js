a2567e4c8b27bb429b6e7755004a45f965e6ad98
YKavaliou <yulian.kov@gmail.com>
2024-01-27 14:04:14 +0100

    feat: implement read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import { fileURLToPath } from 'url';
+import * as fs from 'fs';
+import path from 'path';
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.join(path.dirname(fileURLToPath(import.meta.url)), '/files/fileToRead.txt');
+
+  fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+    if (err) {
+      throw new Error('FS operation failed');
+    } else {
+      console.log(data);
+    }
+  });
 };
 
 await read();
+
