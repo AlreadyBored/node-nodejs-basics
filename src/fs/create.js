82ba72646757c94ef38712c5660df70853aa3c20
Dmitry Bludchy <dmitrybludchy@gmail.com>
2024-01-27 15:19:07 +0300

    refactor: create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -2,12 +2,12 @@ import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 import { writeFile } from 'fs/promises';
 
-const create = async () => {
-  const __dirname = dirname(fileURLToPath(import.meta.url));
-  const filePath = join(__dirname, 'files', 'fresh.txt');
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const FILE_PATH = join(__dirname, 'files', 'fresh.txt');
 
+const create = async () => {
   try {
-    await writeFile(filePath, 'I am fresh and young', { flag: 'wx' });
+    await writeFile(FILE_PATH, 'I am fresh and young', { flag: 'wx' });
     console.log('The file has been successfully written');
   } catch (error) {
     throw new Error('FS operation failed');
