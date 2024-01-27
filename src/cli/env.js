179a52d848153662b5f7fd704219a38d5ce4fd3d
Arseny Stefanov <sirsenya@yandex.ru>
2024-01-27 10:09:03 +0300

    feat: implement func that parses env vars and prints them

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+  const map = new Map(Object.entries(process.env));
+  map.forEach((value, key) => console.log(`RSS_${key}=${value}`));
 };
 
 parseEnv();
+
+//env.js - implement function that parses environment variables
+// with prefix RSS_ and prints them to the console in the format
+// RSS_name1=value1; RSS_name2=value2
