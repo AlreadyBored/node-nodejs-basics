18b6996315bb377ad13575e290ebd577cf347037
Vladimir Kirpichyov <vlasoul@mail.ru>
2024-01-27 16:31:06 +0300

    feat: add changes to cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,11 @@
+import { spawn } from 'node:child_process';
+import path from 'node:path';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childPath = path.join('cp', 'files', 'script.js');
+    const child = spawn(`node ${childPath}`, args, { shell: true, encoding: 'utf8' });
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
