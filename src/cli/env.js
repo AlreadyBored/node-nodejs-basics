79d085c0a2a9e85579992252da7f73858843f26e
msultanovdev <mukhammadamin.sultanov@gmail.com>
2024-01-27 09:33:36 +0500

    feat: add solutions to cli tasks

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,7 @@
 const parseEnv = () => {
-    // Write your code here 
+    const allEnvVarsWithPrefixRSS = Object.keys(process.env).filter(key => key.startsWith("RSS_"));
+    const res = allEnvVarsWithPrefixRSS.map(varName => `${varName}=${process.env[varName]}`).join("; ");
+    console.log(res);
 };
 
 parseEnv();
