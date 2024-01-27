a0298248f5162e03aa88ca877b70a25071813d8f
Arseny Stefanov <sirsenya@yandex.ru>
2024-01-27 10:07:59 +0300

    feat: implement func that parses CLA and prints them

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv;
+  const map = new Map();
+  for (let i = 0; i < args.length; i++) {
+    let arg = args[i];
+    if (arg.startsWith("--")) {
+      map.set(arg.slice(2), args[i + 1]);
+    }
+  }
+  map.forEach((value, key) => console.log(`${key} is ${value}`));
 };
 
 parseArgs();
+
+//args.js - implement function that parses command
+// line arguments (given in format --propName value --prop2Name value2,
+// you don't need to validate it) and prints them
+//to the console in the format propName is value, prop2Name is value2
