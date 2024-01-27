7d36a22352ca8377dd39023431a11c1f851d6f01
net-ka <a.k.fedosova@gmail.com>
2024-01-27 19:35:41 +0100

    feat: add compress and decompress functions to zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import { pipeline } from 'stream/promises';
+import { createGzip } from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+    // Write your code here
+
+    const srcPath = new URL('./files/fileToCompress.txt', import.meta.url);
+    const destinationPath = new URL('./files/archive.gz', import.meta.url);
+
+    const readStream = fs.createReadStream(srcPath);
+    const writeStream = fs.createWriteStream(destinationPath);
+
+    await pipeline(readStream, createGzip(), writeStream);
+
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,17 @@
+import fs from "fs";
+import { pipeline } from "stream/promises";
+import { createGunzip } from "zlib";
+
 const decompress = async () => {
-    // Write your code here 
+    // Write your code here
+
+    const srcPath = new URL('./files/archive.gz', import.meta.url);
+    const destinationPath = new URL('./files/fileToCompress.txt', import.meta.url);
+
+    const readStream = fs.createReadStream(srcPath);
+    const writeStream = fs.createWriteStream(destinationPath);
+
+    await pipeline(readStream, createGunzip(), writeStream);
 };
 
 await decompress();
