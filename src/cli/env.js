18ec774a16480be451856589a692e72736429399
rahunak <euhene@linnovate.net>
2024-01-27 10:53:02 +0300

    #7: cli part - env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+  // Get all environment variables
+  const allEnvVariables = process.env;
+  let res = [];
+  for (let field in allEnvVariables) {
+    if (field.startsWith("RSS_")) {
+      res.push(field + "=" + allEnvVariables[field]);
+    }
+  }
+  console.log(res.join("; "));
 };
 
 parseEnv();
