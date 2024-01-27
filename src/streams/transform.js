2ad792f64b724e5635bceb60fc2575c3c2f2a140
Arseny Stefanov <sirsenya@yandex.ru>
2024-01-27 10:15:27 +0300

    feat: implement func that reads stdin, reverts text and writes into stdout

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,15 @@
+import { Transform } from "stream";
+
 const transform = async () => {
-    // Write your code here 
+  const reverseStr = new Transform({
+    transform(chunk) {
+      this.push(chunk.toString().split("").reverse().join(""));
+    },
+  });
+  process.stdin.pipe(reverseStr).pipe(process.stdout);
 };
 
 await transform();
+
+//transform.js - implement function that reads data from process.stdin,
+//reverses text using Transform Stream and then writes it into process.stdout
