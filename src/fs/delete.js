e50e564ba8430e41efb353be8f2d91c2c0edd2b7
Dmitry Bludchy <dmitrybludchy@gmail.com>
2024-01-27 15:32:43 +0300

    refactor: delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,27 +1,16 @@
-import { access, rm } from 'fs/promises';
+import { rm } from 'fs/promises';
 import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
 
-const pathExists = (path) => {
-  return access(path)
-    .then(() => true)
-    .catch(() => false);
-};
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const TARGET_FILE = join(__dirname, 'files', 'fileToRemove.txt')
 
 const remove = async () => {
-  const currentDir = dirname(fileURLToPath(import.meta.url));
-  const targetDir = join(currentDir, 'files');
-  const targetFilePath = join(targetDir, 'fileToRemove.txt');
-
-  const fileExists = await pathExists(targetFilePath);
-
-  if (!fileExists) throw new Error('FS operation failed');
-
   try {
-    await rm(targetFilePath);
+    await rm(TARGET_FILE);
     console.log('File removed successfully');
   } catch (error) {
-    console.error('Error removing the file:', error.message);
+    throw new Error('FS operation failed');
   }
 };
 
