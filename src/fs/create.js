30a79e7e9cefa7393e3898e35741c9b84fa0fd6c
Olena Datso <elena.shcheglova@gmail.com>
2024-01-27 15:54:38 -0700

    feat: add create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,25 @@
+import fs  from 'node:fs';
+
+const folderName = 'src/fs/files';
+const fileName = `${folderName}/fresh.txt`;
+const content = 'I am fresh and young';
+const err = new Error('FS operation failed');
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await fs.promises.stat(fileName);
+    throw err;
+  } catch (e) {
+    if (e.code === 'ENOENT') {
+      try {
+        await fs.promises.writeFile(fileName, content);
+      }  catch (writeError) {
+        console.error(writeError);
+      }
+    } else {
+      console.error(e);
+    }
+  }
 };
 
 await create();
