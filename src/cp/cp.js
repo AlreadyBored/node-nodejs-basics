6ab8a04dc902c4e079b87ee78235c07099932402
ivan.osipau <ivan.osipau@sap.com>
2024-01-27 14:46:54 +0300

    cp: use spawn instead of fork

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,12 +1,23 @@
-import { fork } from 'child_process'
+import { spawn } from 'child_process'
+import {fileURLToPath} from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const spawnChildProcess = async (args) => {
-    const child = fork('./files/script.js', args)
+    const childFile = path.resolve(__dirname, './files/script.js')
+    const childProcess = spawn("node", [childFile, ...args]);
 
-    child.on('exit', (code) => {
-        console.log('Child exit with code = ', code)
+    process.stdin.pipe(childProcess.stdin);
+
+    childProcess.stdout.on("data",  (data) => {
+        process.stdout.write(data.toString());
+    });
+
+    childProcess.on("exit", (code) => {
+        console.log(`ChildProcess exit with code: ${code}`)
     })
 };
 
-// Put your arguments in function call to test this functionality
 await spawnChildProcess( ['someArgument1', 'someArgument2']);
