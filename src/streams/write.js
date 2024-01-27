2421ede90fc09e6f1f82dbbb2735972b7c05da94
Arseny Stefanov <sirsenya@yandex.ru>
2024-01-27 10:16:27 +0300

    feat: implement func that writes stdin data to file

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import fs from "node:fs";
+
 const write = async () => {
-    // Write your code here 
+  const path = "files/fileToWrite.txt";
+  const stream = fs.createWriteStream(path);
+  stream.on("error", (error) => {
+    throw error;
+  });
+
+  process.stdin.on("data", (data) => stream.write(data));
 };
 
 await write();
+
+//write.js - implement function that writes process.stdin data into file fileToWrite.txt content using Writable Stream
