951ec09ea2c363961c700b4b65a95d22a7d61615
ivan.osipau <ivan.osipau@sap.com>
2024-01-27 11:17:22 +0300

    cp: completed

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,12 @@
+import { fork } from 'child_process'
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = fork('./files/script.js', args)
+
+    child.on('exit', (code) => {
+        console.log('Child exit with code = ', code)
+    })
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+await spawnChildProcess( ['someArgument1', 'someArgument2']);
