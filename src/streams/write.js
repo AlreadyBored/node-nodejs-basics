9c90a95a8fd7edbd54eb534cf78adf510dd0f47c
rahunak <euhene@linnovate.net>
2024-01-27 22:53:04 +0300

    #12: stream - write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,27 @@
+import crypto from "crypto";
+import fs from "fs";
+import { fileURLToPath } from "url";
+import path from "path";
+import process from "node:process";
+
 const write = async () => {
-    // Write your code here 
+  // Script file.
+  const __filename = fileURLToPath(import.meta.url);
+  // Folder of script file.
+  const __dirname = path.dirname(__filename);
+  // Creates write stream for writing data to file.
+  const writeStream = fs.createWriteStream(
+    path.join(__dirname, "files", "fileToWrite.txt")
+  );
+  // Watch what will be written in Command Line.
+  process.stdin.on("data", (data) => {
+    writeStream.write(data);
+  });
+  // Close app,close stream if Cntrl+C.
+  process.on("SIGINT", () => {
+    process.exit();
+    writeStream.close();
+  });
 };
 
 await write();
