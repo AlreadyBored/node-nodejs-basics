ca8eeb1e79802f8c175b5723c78836febdc95152
rahunak <euhene@linnovate.net>
2024-01-27 23:47:04 +0300

    #14: gunzip: decompress.js .gz

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import fs from "fs";
+import zlib from "zlib";
+import path from "path";
+import { fileURLToPath } from "url";
+
 const decompress = async () => {
-    // Write your code here 
+  // Script file.
+  const __filename = fileURLToPath(import.meta.url);
+  // Folder of script file.
+  const __dirname = path.dirname(__filename);
+  // Read data from file.
+  let readableStream = fs.createReadStream(
+    path.join(__dirname, "files", "archive.gz")
+  );
+  // Write data to file.
+  let writeableStream = fs.createWriteStream(
+    path.join(__dirname, "files", "fileToCompress.txt"), { encoding: 'utf8' }
+  );
+  // unCompress data by 'Gunzip'!
+  let gzip = zlib.createGunzip();
+  //Use pipe bind read and write streams.
+  readableStream.pipe(gzip).pipe(writeableStream);
 };
 
 await decompress();
