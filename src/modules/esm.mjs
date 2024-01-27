c9561ec0c5ee8e7d50cf4adce5e03711cd696943
ivan.osipau <ivan.osipau@sap.com>
2024-01-27 12:39:31 +0300

    modules: completed

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,16 +1,21 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path from "path";
+import { fileURLToPath } from 'url'
+import { release, version } from 'os'
+import { createServer as createServerHttp } from 'http'
+import './files/c.js'
+import aData from './files/a.json' assert {type: "json"}
+import bData from './files/b.json' assert {type: "json"}
 
 const random = Math.random();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+    unknownObject = aData;
 } else {
-    unknownObject = require('./files/b.json');
+    unknownObject = bData;
 }
 
 console.log(`Release ${release()}`);
@@ -33,7 +38,7 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
+export default {
     unknownObject,
     myServer,
 };
