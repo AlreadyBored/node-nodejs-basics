0728849adf5810dfc03c4f650c80c2f4cedd9594
Karina Shupik <77788896h@gmail.com>
2024-01-27 12:57:19 +0200

    feat: implement transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
+import { Transform } from 'node:stream';
+
 const transform = async () => {
-    // Write your code here 
+    const reverseTransform = new Transform({
+        transform(chunk, encoding, callback) {
+            // Reverse the text in each chunk
+            const reversedText = chunk.toString().split('').reverse().join('')
+            this.push(reversedText)
+            callback()
+        }
+    });
+
+    process.stdin.pipe(reverseTransform).pipe(process.stdout)
 };
 
 await transform();
