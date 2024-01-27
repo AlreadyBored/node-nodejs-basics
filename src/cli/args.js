00f96ccafed73ec4eeb6b60dd8c21ae8c4b42823
rahunak <euhene@linnovate.net>
2024-01-27 11:01:00 +0300

    #8: cli: args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+    let argsFromCLI = process.argv.slice(2);
+    let resArr = [];
+    argsFromCLI.map(function (val, index) {
+      if(val.startsWith('--')) {
+       resArr.push(val.slice(2)+' is '+argsFromCLI[index+1]);
+      }
+    });
+
+    console.log(resArr.join(', '));
 };
 
 parseArgs();
