7ea6c9be4bad5825561ebbd9693d917f9b01f583
Vladimir Kirpichyov <vlasoul@mail.ru>
2024-01-27 16:26:28 +0300

    feat: add changes to cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+  let start = false;
+  const params = process.argv.reduce((acc, element, index, array) => {
+    let result = '';
+    if (!start && (element.endsWith('args') || element.endsWith('args.js'))) start = true;
+    else if (start) {
+      if (element.startsWith('--')) {
+        result = `${acc ? ', ' : ''}${element.substring(2)} is ${array[index + 1] ?? ''}`;
+      }
+    }
+    return acc + result;
+  }, '');
+  console.log(params);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+  const entries = Object.entries(process.env).filter(([param]) => param.startsWith('RSS_')); 
+  const result = entries.reduce((acc, [name, value]) => {
+    const result = `${acc ? '; ' : ''}${name}=${value}`  
+    return acc + result; 
+  }, '')
+  console.log(result);
 };
 
 parseEnv();
