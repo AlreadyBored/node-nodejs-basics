fd4affa85aa6cafa86ef8d0a64eac38695ccb068
Arseny Stefanov <sirsenya@yandex.ru>
2024-01-27 10:11:35 +0300

    feat: implement a func that creates a child process, add some random args

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,20 @@
+import { spawn } from "node:child_process";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  spawn("node", ["script.js", ...args], {
+    cwd: "files",
+    stdio: ["inherit", "inherit", "inherit", "ipc"],
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["scris", "dsds", "dsd"]);
+
+// You should implement several functions in dedicated files
+
+// cp.js - implement function spawnChildProcess that receives array
+//of arguments args and creates child process from file script.js,
+//passing these args to it. This function should create IPC-channel
+// between stdin and stdout of master process and child process:
+// child process stdin should receive input from master process stdin
+// child process stdout should send data to master process stdout
