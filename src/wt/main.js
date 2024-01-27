7e5fdef338311830eb50bb65d85bf481459d0e10
Pavel Sakovich <pavel.sackovich@ingenios.io>
2024-01-27 14:24:41 +0100

    feat: implement "wt" tasks

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,47 @@
+import os from "os";
+
+import { Worker, isMainThread, parentPort, workerData } from "worker_threads";
+import path from "path";
+
+const currentFolder = process.cwd();
+const targetFileWorker = "worker.js";
+
+const pathToTargetFIleWorker = path.join(currentFolder, targetFileWorker);
+const numberOfCores = os.cpus().length;
+
 const performCalculations = async () => {
-    // Write your code here
+  const arrRes = [];
+  let numberOfCompletedWorkers = 0;
+
+  for (let i = 0; i < numberOfCores; i++) {
+    const worker = new Worker(pathToTargetFIleWorker, {
+      workerData: 10 + i,
+    });
+
+    const workerInfo = {
+      status: null,
+      data: null,
+    };
+
+    worker.on("message", (msg) => {
+      workerInfo.status = "resolved";
+      workerInfo.data = msg;
+    });
+
+    worker.on("error", () => {
+      workerInfo.status = "error";
+      workerInfo.data = null;
+    });
+
+    worker.on("exit", () => {
+      numberOfCompletedWorkers++;
+      arrRes.push(workerInfo);
+
+      if (numberOfCompletedWorkers === numberOfCores) {
+        console.log(arrRes);
+      }
+    });
+  }
 };
 
 await performCalculations();
