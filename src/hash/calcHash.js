219c0edb92d674be53acb05b0cf2504d113519dc
PopovaOlha <fc.odess.10@gmail.com>
2024-01-27 10:48:26 +0200

    feat: update calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const fileName = 'fileToCalculateHashFor.txt';
+  const currentDir = path.dirname(fileURLToPath(import.meta.url));
+  const filePath = path.join(currentDir, 'files', fileName);
+
+  try {
+    const stream = createReadStream(filePath);
+    const hash = createHash('sha256');
+
+    stream.on('data', (data) => {
+      hash.update(data);
+    });
+
+    stream.on('end', () => {
+      const hashResult = hash.digest('hex');
+      console.log(`SHA256 hash for ${fileName}: ${hashResult}`);
+    });
+  } catch (error) {
+    console.error('Error reading the file:', error.message);
+  }
 };
 
 await calculateHash();
