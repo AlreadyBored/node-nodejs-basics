9687e70da97729a9c7d8cfe1c39eee1352b13542
ivan.osipau <ivan.osipau@sap.com>
2024-01-27 12:40:25 +0300

    wt: completed

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,30 @@
+import { Worker } from 'worker_threads'
+import { cpus } from 'os'
+
+const createWorker = (index) =>
+    new Promise((res) => {
+        const worker = new Worker('./worker.js')
+        worker.postMessage(10 + index)
+        worker.on('message', (data) => {
+            res({
+                status: 'resolved',
+                data
+            })
+        })
+        worker.on('error', () => {
+            res({
+                status: 'error',
+                data: null
+            })
+        })
+    })
+
 const performCalculations = async () => {
-    // Write your code here
+    const workers = cpus().map((_, index) => createWorker(index))
+    const result = await Promise.all(workers)
+
+    console.log(result)
+    process.exit()
 };
 
 await performCalculations();
