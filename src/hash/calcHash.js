5e462ef6781883f756289704eac108e4b2f48806
Arseny Stefanov <sirsenya@yandex.ru>
2024-01-27 10:13:10 +0300

    feat: implement func that calculates hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,15 @@
+import fs from "fs";
+import { createHash } from "node:crypto";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const path = "files/fileToCalculateHashFor.txt";
+  const stream = fs.createReadStream(path);
+  stream.on("data", (chunk) =>
+    console.log(createHash("sha3-256").update(chunk).digest("hex"))
+  );
 };
 
 await calculateHash();
+
+//calcHash.js - implement function that calculates SHA256 hash
+// for file fileToCalculateHashFor.txt and logs it into console as hex using Streams API
