de4d99c3763cfd48e60cfd0fcef7cd0256657070
ivan.osipau <ivan.osipau@sap.com>
2024-01-27 12:55:23 +0300

    zip: completed

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import { createGzip } from 'zlib'
+import { createWriteStream, createReadStream } from 'fs'
+import { pipeline } from 'stream'
+import { unlink } from "fs/promises";
+
+const file = './files/fileToCompress.txt'
+const archive = './files/archive.gz'
+
 const compress = async () => {
-    // Write your code here 
+    const readStream = createReadStream(file)
+    const writeStream = createWriteStream(archive)
+    const gzip = createGzip()
+
+    pipeline(readStream, gzip, writeStream, (err) => {
+        if (!err) {
+            unlink(file)
+        }
+    })
 };
 
 await compress();
