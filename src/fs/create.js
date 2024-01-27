3508097d8149a35539fc02758fa6320af75dd608
h4cktivist <popovvadim0605@gmail.com>
2024-01-27 15:20:14 +0500

    fix: fix empty fresh.txt creation if it is already exists

--- src/fs/create.js
+++ src/fs/create.js
@@ -4,9 +4,11 @@ const create = async () => {
     const filePath = 'src/fs/files/fresh.txt'
     fs.access(filePath, fs.constants.F_OK, (err) => {
         if (!err) throw new Error('FS operation failed');
-    });
-    fs.writeFile(filePath, 'I am fresh and young', (err) => {
-        if (err) throw err;
+        else {
+            fs.writeFile(filePath, 'I am fresh and young', (err) => {
+                if (err) throw err;
+            });
+        }
     });
 };
 
