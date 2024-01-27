830d6db4be4f4a900ed25e971155849c71856725
YKavaliou <yulian.kov@gmail.com>
2024-01-27 14:12:55 +0100

    feat: implement env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+  Object.keys(process.env).forEach((key) => {
+    if (key.startsWith('RSS_')) {
+      console.log(`${key}=${process.env[key]}`)
+    }
+  })
 };
 
 parseEnv();
