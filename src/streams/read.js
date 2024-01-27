43cbd06365406c1c036d0013f8aa2c07e960a9c6
msultanovdev <mukhammadamin.sultanov@gmail.com>
2024-01-27 11:47:24 +0500

    feat: add solutions to streams tasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import { createReadStream } from "fs";
+import path, {dirname} from "path";
+import { fileURLToPath } from "url";
+import { stdout } from "process";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const readStream = createReadStream(path.join(__dirname, "files", "fileToRead.txt"));
+
 const read = async () => {
-    // Write your code here 
+    try {
+        readStream.pipe(stdout);
+    } catch {
+        throw new Error("FS operation failed");
+    }
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import fs from "fs";
+import path, {dirname} from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const writeStream = fs.createWriteStream(path.join(__dirname, "files", "fileToWrite.txt"));
+
 const write = async () => {
-    // Write your code here 
+    try {
+        process.stdin.pipe(writeStream);
+    } catch (e) {
+        console.log(e);
+        throw new Error("FS operation failed");
+    }
 };
 
 await write();
