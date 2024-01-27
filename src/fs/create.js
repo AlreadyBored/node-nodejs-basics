ea61cbf794177987968be66cb5a3669ecbe9b7c3
Arseny Stefanov <sirsenya@yandex.ru>
2024-01-27 09:58:04 +0300

    feat: add creation of fresh.txt with tests

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,13 @@
-const create = async () => {
-    // Write your code here 
-};
+import fs from "fs";
 
+const create = async () => {
+  const content = "I am fresh and young";
+  const path = "files/fresh.txt";
+  const error = Error("FS operation failed");
+  fs.access(path, fs.constants.F_OK, (err) => {
+    if (!err) {
+      throw error;
+    } else fs.writeFile(path, content, (err) => {});
+  });
+};
 await create();
