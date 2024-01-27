ed936730f69ddfe2bda19e834001f724af29eac0
rahunak <euhene@linnovate.net>
2024-01-27 19:26:58 +0300

    #11: stream: read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,31 @@
+import crypto from "crypto";
+import fs from "fs";
+import { fileURLToPath } from "url";
+import path from "path";
+import process from "node:process";
+
 const read = async () => {
-    // Write your code here 
+  // Script file.
+  const __filename = fileURLToPath(import.meta.url);
+  // Folder of script file.
+  const __dirname = path.dirname(__filename);
+  // Creates read stream for reading data from file.
+  const readStream = fs.createReadStream(
+    path.join(__dirname, "files", "fileToRead.txt")
+  );
+  let data = "";
+  // When data will be read, it will be added to data variable.
+  readStream.on("data", (chunk) => {
+    data += chunk;
+  });
+  // when data will be read, it will be printed to output stream.(inner method of console.log)
+  readStream.on("end", (chunk) => {
+    process.stdout.write(data);
+  });
+  // If there will be an error, it will be printed to console.
+  readStream.on("error", (error) =>
+    console.log("Error in readStream:", error.message)
+  );
 };
 
 await read();
