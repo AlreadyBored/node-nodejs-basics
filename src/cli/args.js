6c18c836ccc81d257d98a18a64f7dcdd9fe4d6a7
Dima Chemerevsky <dchemerevsky@aras.com>
2024-01-27 22:39:04 +0100

    Command line interface(src/cli). Args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+	const outputArray = [];
+	for (let i = 2; i < process.argv.length;) {
+		outputArray.push(`${process.argv[i].slice(2)} is ${process.argv[i + 1]}`);
+		i += 2;
+	};
+
+	console.log(outputArray.join(', '));
 };
 
 parseArgs();
