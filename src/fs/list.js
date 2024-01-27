56dab4055da120c2d07f823ff43cb9f0ae95743e
YKavaliou <yulian.kov@gmail.com>
2024-01-27 13:59:23 +0100

    feat: implement list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import { fileURLToPath } from 'url';
+import * as fs from 'fs';
+import path from 'path';
+
 const list = async () => {
-    // Write your code here 
+  const folderPath = path.join(path.dirname(fileURLToPath(import.meta.url)), '/files');
+
+  fs.readdir(folderPath, (err, files) => {
+    if (err) {
+      throw new Error('FS operation failed');
+    }
+
+    files.forEach(file => {
+      console.log(file);
+    });
+  });
 };
 
 await list();
