4b782871329f1967746b4618e1e4c186666dcdf9
Dmitry Bludchy <dmitrybludchy@gmail.com>
2024-01-27 20:21:28 +0300

    feat: implement parseargs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const formattedArgs = args.map((arg, index, array) => {
+    if (arg.startsWith('--')) {
+      const command = arg.slice(2);
+      const value = array[index + 1];
+      return `${command} is ${value}`;
+    }
+  });
+  const stringArgs = formattedArgs.join(', ');
+  console.log(stringArgs);
 };
 
 parseArgs();
