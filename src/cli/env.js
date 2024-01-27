b3f2a08d20927b6d283359d7544d74abb130dd8e
Dima Chemerevsky <dchemerevsky@aras.com>
2024-01-27 11:24:30 +0100

    Command line interface(src/cli). Env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+	const outArray = [];
+	Object.keys(process.env).forEach(key => {
+		if (key.startsWith('RSS_')) {
+			outArray.push(`${key}=${process.env[key]}`);
+		}
+	});
+	console.log(outArray.join('; '));
 };
 
 parseEnv();
