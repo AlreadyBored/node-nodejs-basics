d664a3f2421dcd164e4882733e8180e9e5878529
Dima Chemerevsky <dchemerevsky@aras.com>
2024-01-27 23:41:06 +0100

    Hash (src/hash). CalcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import * as crypto from 'crypto';
+import * as fs from 'fs';
+
 const calculateHash = async () => {
-    // Write your code here 
+	const hash = crypto.createHash('sha256');
+	hash.setEncoding('hex');
+	const file = fs.createReadStream('files/fileToCalculateHashFor.txt');
+
+	file.on('end', () => {
+		hash.end();
+		console.log(hash.read());
+	});
+
+	file.pipe(hash);
 };
 
 await calculateHash();
