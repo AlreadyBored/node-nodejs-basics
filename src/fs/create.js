9c2a3ddb519aeab48387e695922e9234f7010de6
YKavaliou <yulian.kov@gmail.com>
2024-01-27 11:59:41 +0100

    feat: implement create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import { fileURLToPath } from 'url';
+import * as fs from 'fs';
+import path from 'path';
+
 const create = async () => {
-    // Write your code here 
+  const pathToFile = path.join(path.dirname(fileURLToPath(import.meta.url)), '/files/fresh.txt');
+  const fileContent = 'I am fresh and young';
+  const existError = 'FS operation failed';
+
+  fs.open(pathToFile, 'wx', function(err, file) {
+    if (err) {
+      throw new Error(existError);
+    }
+
+    fs.writeFile(file, fileContent, function(err) {
+      if (err) {
+        throw new Error(err);
+      }
+    });
+  })
 };
 
 await create();
