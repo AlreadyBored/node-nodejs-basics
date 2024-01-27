0e2b290295addb27eea5bb1f51d34585f038d589
Arseny Stefanov <sirsenya@yandex.ru>
2024-01-27 10:14:11 +0300

    feat: implement func that reads and prints with streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import fs from "node:fs";
+
 const read = async () => {
-    // Write your code here 
+  // Write your code here
+  const path = "files/fileToRead.txt";
+  const stream = fs.createReadStream(path);
+  stream.on("error", (error) => {
+    throw error;
+  });
+  stream.on("data", (chunk) => process.stdout.write(chunk.toString()));
+  // process.stdout.write();
 };
 
 await read();
+
+// read.js - implement function that reads file fileToRead.txt content using Readable Stream and prints it's content into process.stdout
