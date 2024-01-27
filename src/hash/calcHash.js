d9a483ee98a2cab10470e360b947968346dc2b87
Vladimir Kirpichyov <vlasoul@mail.ru>
2024-01-27 16:37:29 +0300

    feat: add changes to hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,13 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import crypto from 'node:crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const file = path.join(path.resolve(''), 'hash', 'files', 'fileToCalculateHashFor.txt');
+  const stream = fs.createReadStream(file);
+  const hash = crypto.createHash('sha256', file);
+  stream.on('data', (data) => hash.update(data))
+  stream.on('end', () => console.log(hash.digest('hex')))
 };
 
 await calculateHash();
