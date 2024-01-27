408ea7ec2d865ffa66b63a194d66f4a026d6c039
Eugene <josumkin@yahoo.com>
2024-01-27 16:37:39 +0400

    feat: child processes implemented

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,18 @@
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { spawn } from 'child_process';
+
+const currentFolder = dirname(fileURLToPath(import.meta.url));
+const pathToScript = path.join(currentFolder, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    
+    const child =spawn('node', [pathToScript, ...args]);
+
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
