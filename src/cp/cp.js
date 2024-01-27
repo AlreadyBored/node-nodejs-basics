0205ec0c15c894ca0da92cb280a3f4e9ff4d1597
Mmishaaa <kkochin2528@gmail.com>
2024-01-27 22:47:12 +0300

    feat: add cp functions

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,13 @@
+import child_process from 'child_process';
+import * as url from 'url';
+import path from "path";
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const modulePath = path.join(__dirname, "files", "script.js");
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    child_process.fork(modulePath, args);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["Hello", 10, 100, "Node.js", "!"]);
