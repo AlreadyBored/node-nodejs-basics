39c5692875e6eac86426f9bddd7d463d9665cbab
Karina Shupik <77788896h@gmail.com>
2024-01-27 12:37:53 +0200

    feat: implement write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,34 @@
+import { promises as fsPromises, createWriteStream } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import * as path from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const write = async () => {
-    // Write your code here 
+    const fileSourcePath = path.join(__dirname, 'fileToWrite.txt');
+
+    const fileWriteStream = createWriteStream(fileSourcePath);
+
+    process.stdin.on('readable', () => {
+        const chunk = process.stdin.read();
+        if (chunk !== null) {
+            fileWriteStream.write(chunk);
+        }
+    });
+
+    process.stdin.on('end', () => {
+        // Close the writable stream when the input from stdin ends
+        fileWriteStream.end()
+    });
+
+    fileWriteStream.on('finish', () => {
+        console.log(`Data has been written to ${fileSourcePath}`)
+    });
+    
+    fileWriteStream.on('error', (err) => {
+        console.error('Error:', err)
+    });
 };
 
-await write();
+await write()
