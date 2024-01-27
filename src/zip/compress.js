51bf4db588303e058a4a120cefce1b1046b7e948
Vladimir Kirpichyov <vlasoul@mail.ru>
2024-01-27 16:45:31 +0300

    feat: add changes to zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,25 @@
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import {
+  createReadStream,
+  createWriteStream,
+} from 'node:fs';
+import path from 'node:path';
+
 const compress = async () => {
-    // Write your code here 
+  const gzip = createGzip();
+  const from = path.join(path.resolve(''), 'zip', 'files', 'fileToCompress.txt');
+  const to = path.join(path.resolve(''), 'zip', 'files', 'archive.gz');
+  const source = createReadStream(from);
+  const destination = createWriteStream(to);
+
+  pipeline(source, gzip, destination, (err) => {
+    if (err) {
+      console.error('An error occurred:', err);
+      process.exitCode = 1;
+    }
+  });
+   
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import { createGunzip, createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import {
+  createReadStream,
+  createWriteStream,
+} from 'node:fs';
+import path from 'node:path';
+
 const decompress = async () => {
-    // Write your code here 
+  const gunzip = createGunzip();
+  const from = path.join(path.resolve(''), 'zip', 'files', 'archive.gz');
+  const to = path.join(path.resolve(''), 'zip', 'files', 'fileToCompress1.txt');
+  const source = createReadStream(from);
+  const destination = createWriteStream(to);
+
+  pipeline(source, gunzip, destination, (err) => {
+      if (err) {
+      console.error('An error occurred:', err);
+      process.exitCode = 1;
+      }
+  });
 };
 
 await decompress();
