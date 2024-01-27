82c843b8320b1ce293957464cecb1b87d6d32ecd
PopovaOlha <fc.odess.10@gmail.com>
2024-01-27 21:56:42 +0200

    feat: update zip/compress.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -2,20 +2,16 @@ import { Transform } from 'stream';
 
 const transform = async () => {
   try {
-    // Создаем Transform Stream
     const reverseTransform = new Transform({
       transform(chunk, _, callback) {
-        // Реверс данных и отправка их обратно
         const reversedChunk = chunk.toString().split('').reverse().join('');
         this.push(reversedChunk);
         callback();
       },
     });
 
-    // Подключаем stdin к Transform Stream
     process.stdin.pipe(reverseTransform);
 
-    // Подключаем Transform Stream к stdout
     reverseTransform.pipe(process.stdout);
 
     console.log('Enter text to reverse (Ctrl+C to end):');
--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,29 @@
+import { createReadStream, createWriteStream } from 'fs';
+import zlib from 'zlib';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const compress = async () => {
-    // Write your code here 
+  const fileName = 'fileToCompress.txt';
+  const currentDir = path.dirname(fileURLToPath(import.meta.url));
+  const sourceFilePath = path.join(currentDir, 'files', fileName);
+  const destinationFilePath = path.join(currentDir, 'files', 'archive.gz');
+
+  try {
+    const input = createReadStream(sourceFilePath);
+    const output = createWriteStream(destinationFilePath);
+    const gzip = zlib.createGzip();
+
+    await new Promise((resolve, reject) => {
+      input.pipe(gzip).pipe(output);
+      output.on('finish', resolve);
+      output.on('error', reject);
+    });
+
+    console.log(`File compressed successfully. Archive saved to ${destinationFilePath}`);
+  } catch (error) {
+    console.error('Compression failed:', error.message);
+  }
 };
 
 await compress();
