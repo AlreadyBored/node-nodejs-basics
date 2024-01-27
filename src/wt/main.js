b7c586dee6b72f43b94b6c9904d0629532208b3e
Mmishaaa <kkochin2528@gmail.com>
2024-01-27 17:08:36 +0300

    feat: add wt functions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,49 @@
+import os from "os"
+import * as url from 'url';
+import path from "path";
+import { Worker } from "worker_threads";
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const workerPath = path.join(__dirname, "worker.js");
+const numCPUs = os.cpus().length;
+const startNumber = 10;
+
+function createWorkerThreads(number) {
+    const promises = [];
+
+    for(let i = 0; i < numCPUs; i++) {
+        const currentNumber = number + i;
+
+        const promise = new Promise((resolve, reject) => {
+            const worker = new Worker(workerPath, {
+                workerData: currentNumber,
+            });
+
+            worker.on('message', (data) => resolve({
+                status: "resolved",
+                data: data,
+            }
+            ));
+
+            worker.on('error', () => resolve ({
+                status: "error",
+                data: null,
+            }
+            ));
+        }
+        )
+        promises.push(promise);
+    }
+    return promises;
+};
+
 const performCalculations = async () => {
-    // Write your code here
+    const workersPool = createWorkerThreads(startNumber);
+    const answer = await Promise.all(workersPool);
+    console.log(answer);
 };
 
 await performCalculations();
+
+
+
