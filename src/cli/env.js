c4fb9e2851e44bd493efa462c20c283df9b64cdb
Dmitry Bludchy <dmitrybludchy@gmail.com>
2024-01-27 19:07:05 +0300

    feat: implement parseenv function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+  const env = process.env;
+  const rssVar = Object.entries(env)
+    .filter(([key]) => key.startsWith('RSS_'))
+    .map(([key, value]) => `${key}=${value}`)
+    .join('; ');
+
+  console.log(rssVar);
 };
 
 parseEnv();
