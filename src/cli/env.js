7fd40a5d317e3176c864f55cc1fd62e049e14b40
alex.pranevich <freennnn@gmail.com>
2024-01-28 15:40:24 +0100

    feat: add cli tasks

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envArr = Object.keys(process.env);
+  let envs = envArr.filter((env) => env.startsWith("RSS_"));
+  let resultString = "";
+  if (envs.length > 0) {
+    resultString += envs[0] + "=" + process.env[envs[0]];
+  }
+  for (let i=1; i< envs.length; i++) {
+    resultString += `; ${envs[i]}=${process.env[envs[i]]}`;
+  }
+  console.log(resultString);
 };
 
 parseEnv();
