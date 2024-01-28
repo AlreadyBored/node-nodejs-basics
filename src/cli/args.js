09e2ff9b00f16e0c229bcf839e7d08f07a76e599
Biarslan <thebiarslan@gmail.com>
2024-01-29 00:43:19 +0300

    feat: add cli part

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+    const inputArgs = process.argv.slice(2);
+
+    const formattedArgs = inputArgs.reduce((argsAcc, arg, index, array) => {
+        const isKey = arg.startsWith("--");
+        const value = array[index + 1];
+        return isKey && value !== undefined ? [...argsAcc, `${arg.slice(2)} is ${value}`] : argsAcc;
+    }, [])
+
+    console.log(formattedArgs.join(", "))
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+
+    const rssVariables = Object.entries(process.env)
+        .filter(([key, _]) => key.startsWith('RSS_'))
+        .map(([key, value]) => `${key}=${value}`)
+
+    console.log(rssVariables.join("; "))
 };
 
 parseEnv();
