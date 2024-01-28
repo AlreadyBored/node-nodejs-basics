bf7e2edfac0b48b0234900029af05f3f98fe0895
Lapitskiy <hiimisha@mail.ru>
2024-01-29 00:29:37 +0300

    feat: finish fs module "create" function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,26 @@
+import fs from "fs/promises";
+
 const create = async () => {
-    // Write your code here 
+    const filePath = 'src/fs/files/fresh.txt';
+    const text = 'I am fresh and young';
+
+    let fileExists = true;
+
+    try {
+        await fs.access(filePath);
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            fileExists = false;
+        } else {
+            throw err;
+        }
+    }
+
+    if (fileExists) {
+        throw new Error('FS operation failed');
+    } else {
+        await fs.writeFile(filePath, text);
+    }
 };
 
 await create();
