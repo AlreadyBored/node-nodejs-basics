ebd1de9e8cbadc7b041563a517c0c8bc2e3230cf
Mikhail <nodeprog@gmail.com>
2024-01-28 09:02:20 +0300

    feat: implement function "compress"

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import { fs, getFilePath } from '../utils/fs.js';
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+
+const fileToCompress = getFilePath(
+    import.meta.url, 'files', 'fileToCompress.txt'
+);
+const archive = getFilePath(
+    import.meta.url, 'files', 'archive.gz'
+);
+
 const compress = async () => {
-    // Write your code here 
+    const gzip = createGzip();
+    const readSteam = fs.createReadStream(fileToCompress);
+    const writeStream = fs.createWriteStream(archive);
+    pipeline(readSteam, gzip, writeStream, (err) => {
+        if (err) throw err;
+    });
 };
 
 await compress();
