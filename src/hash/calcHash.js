d2c3df6e02dae29b01011429995804cde1da9abf
YKavaliou <yulian.kov@gmail.com>
2024-01-28 11:02:52 +0100

    feat: implement hash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import path from "path";
+import {fileURLToPath} from "url";
+import fs from "fs";
+import { createHash } from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = path.join(path.dirname(fileURLToPath(import.meta.url)), '/files/fileToCalculateHashFor.txt');
+  const hash = createHash('sha256')
+
+  fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+    if (err) {
+      throw new Error('FS operation failed');
+    } else {
+      if (data) {
+        hash.update(data);
+        console.log(hash.digest('hex'));
+      }
+    }
+  });
 };
 
 await calculateHash();
