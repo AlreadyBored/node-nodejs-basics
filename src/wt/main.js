6805c8a2308513666a27e1fd9edc4dc5452d1d8c
rahunak <euhene@linnovate.net>
2024-01-28 20:56:24 +0300

    #16: worker.js and main.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,76 @@
+import {
+  Worker,
+  isMainThread,
+  setEnvironmentData,
+  getEnvironmentData,
+  parentPort,
+  workerData,
+} from "node:worker_threads";
+import path from "path";
+import { fileURLToPath } from "url";
+import os from "os";
+import process from "node:process";
+
+async function createWorkerThread(inputData) {
+  // Script file.
+  const __filename = fileURLToPath(import.meta.url);
+  // Folder of script file.
+  const __dirname = path.dirname(__filename);
+  //workder thread should be as promise
+  return new Promise((resolve, reject) => {
+    const worker = new Worker(path.join(__dirname, "worker.js"), {
+      workerData: inputData,
+    });
+    //send data to workder.
+    worker.postMessage(inputData);
+    //add listeners for worker.
+    worker.on("message", (message) => {
+      if (message.status === "resolved") {
+        resolve({ status: "resolved", data: message.data });
+      } else if (message.status === "error") {
+        resolve({ status: "error", data: null });
+      }
+    });
+
+    worker.on("error", (err) => {
+      resolve({ status: "error", data: null });
+    });
+
+    worker.on("exit", (code) => {
+      if (code !== 0) {
+        resolve({ status: "error", data: null });
+      }
+    });
+  });
+}
+
 const performCalculations = async () => {
-    // Write your code here
+  //Cntrl + C = exit
+  process.on("SIGINT", () => {
+    process.exit();
+  });
+
+  // Script file.
+  const __filename = fileURLToPath(import.meta.url);
+  // Folder of script file.
+  const __dirname = path.dirname(__filename);
+  const allWorkers = [];
+  let cpusCount = os.cpus().length;
+  for (let i = 0; i < cpusCount; i++) {
+    //Create workers
+    const myWorker = createWorkerThread(10 + i);
+    allWorkers.push(myWorker);
+  }
+  //Wait when all will resolved or rejected.
+  Promise.allSettled(allWorkers).then((data) => {
+    let res = [];
+    // Prepares output data
+    data.reduce((acc, cur) => {
+      return res.push({ status: cur.value.status, data: cur.value.data });
+    }, res);
+    //output
+    console.log(res);
+  });
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,33 @@
+import {
+  Worker,
+  isMainThread,
+  setEnvironmentData,
+  getEnvironmentData,
+  parentPort,
+} from "node:worker_threads";
+import path from "path";
+
 // n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  // parentPort is parent stream.
+  // This function sends result of nthFibonacci computations to main thread
+  parentPort.on("message", (data) => {
+    // Data have come from main thread
+    parentPort.postMessage({ status: "resolved", data: nthFibonacci(data) });
+    //rejected example
+    // if (+data == 12) { return parentPort.postMessage({ status: "error", data: nthFibonacci(data) }); }
+  });
+  parentPort.on("error", (data) => {
+    parentPort.postMessage({ status: "error", data: null });
+  });
+  parentPort.on("exit", (data) => {
+    if (code !== 0) {
+      parentPort.postMessage({ status: "error", data: null });
+    }
+  });
 };
 
 sendResult();
