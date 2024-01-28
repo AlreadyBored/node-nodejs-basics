8cc768e665086440fbd8d208d95a15328ed60c64
Mikhail <nodeprog@gmail.com>
2024-01-28 04:38:42 +0300

    feat: implement function "delete"

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,11 @@
+import { fs, getFilePath } from "../utils/fs.js";
+
+const fileToRemove = getFilePath(import.meta.url, 'files', 'fileToRemove.txt');
+
 const remove = async () => {
-    // Write your code here 
+    fs.rm(fileToRemove, err => {
+        if (err) throw new Error('FS operation failed');
+    })
 };
 
 await remove();
