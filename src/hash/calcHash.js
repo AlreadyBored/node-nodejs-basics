626f67ee0a0d892214ce191ae8757f4863068a36
Dmitry Bludchy <dmitrybludchy@gmail.com>
2024-01-28 18:45:25 +0300

    feat: implement calculatehash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,15 @@
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { readFile } from 'fs/promises';
+import { createHash } from 'crypto';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+  const content = await readFile(filePath);
+  const hash = createHash('sha256').update(content).digest('hex');
+  console.log(hash);
 };
 
 await calculateHash();
