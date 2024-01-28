a6f4c0e772800794201602a2bec353e17c1dbc45
Dima Chemerevsky <dchemerevsky@aras.com>
2024-01-28 13:25:26 +0100

    Streams (src/streams). Read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,9 @@
+import fs from 'fs';
+import { stdout } from 'process';
+
 const read = async () => {
-    // Write your code here 
+	const fileToRead = fs.createReadStream('files/fileToRead.txt');
+	fileToRead.pipe(stdout);
 };
 
 await read();
