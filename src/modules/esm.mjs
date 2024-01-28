72008d81f9992c29d9d39df44027762520cd1a6e
Biarslan <thebiarslan@gmail.com>
2024-01-29 01:09:25 +0300

    feat: add module task

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,16 +1,20 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path from "path";
+import { release, version } from "os";
+import { createServer as createServerHttp } from "http" ;
+import { fileURLToPath } from 'url';
+import "./files/c.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const random = Math.random();
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+    unknownObject = (await import('./files/a.json', { assert: { type: "json" } })).default;
 } else {
-    unknownObject = require('./files/b.json');
+    unknownObject = (await import('./files/b.json', { assert: { type: "json" } })).default;
 }
 
 console.log(`Release ${release()}`);
@@ -33,8 +37,4 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
-    unknownObject,
-    myServer,
-};
-
+export { unknownObject, myServer }
