d60ac812f98d4dc778a3f67c9454661c2b4dcb87
valeriia zvezdina <valeriia.zvezdina@gmail.com>
2024-01-28 17:06:01 +0100

    feat: finish Zlib subtask;

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,29 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGzip } from 'node:zlib';
+import { join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { pipeline } from 'node:stream';
+import { promisify } from 'node:util';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+const fileNameToCompress = 'fileToCompress.txt';
+const compressedFileName = 'archive.gz';
+
 const compress = async () => {
-    // Write your code here 
+    const pipe = promisify(pipeline);
+    const gzip = createGzip();
+    const source = createReadStream(
+        join(__dirname, 'files', fileNameToCompress)
+    );
+    const destination = createWriteStream(
+        join(__dirname, 'files', compressedFileName)
+    );
+    try {
+        await pipe(source, gzip, destination); 
+    } catch(err) {
+        console.log(err.message);
+    }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,29 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGunzip } from 'node:zlib';
+import { join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { pipeline } from 'node:stream';
+import { promisify } from 'node:util';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+const fileName = 'fileToCompress.txt';
+const compressedFileName = 'archive.gz';
+
 const decompress = async () => {
-    // Write your code here 
+    const pipe = promisify(pipeline);
+    const gunzip = createGunzip();
+    const source = createReadStream(
+        join(__dirname, 'files', compressedFileName)
+    );
+    const destination = createWriteStream(
+        join(__dirname, 'files', fileName)
+    );
+    try {
+        await pipe(source, gunzip, destination);
+    } catch (err) {
+        console.error(err.message);
+    }
 };
 
 await decompress();
