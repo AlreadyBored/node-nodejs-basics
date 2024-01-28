b9717bd3bc5d858f9d2aeaf0e0601f3a69d4cb6a
Andrei Yasinski <andrew.yasinski@gmail.com>
2024-01-28 12:17:46 +0100

    update wt dir

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,15 @@
+import { Worker } from "worker_threads";
+import { availableParallelism } from "os";
+
+const numCPU = availableParallelism();
+
 const performCalculations = async () => {
-    // Write your code here
+  const workers = Array.from({ length: numCPU }, (_, index) => {
+    const worker = new Worker("./src/wt/worker.js", { workerData: index + 10 });
+    return new Promise((res) => worker.on("message", (msg) => res(msg)));
+  });
+  const result = await Promise.all(workers);
+  console.log(result);
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,16 @@
+import { parentPort, workerData } from "worker_threads";
+
 // n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  try {
+    const data = nthFibonacci(workerData);
+    parentPort.postMessage({ data, status: "resolved" });
+  } catch (error) {
+    parentPort.postMessage({ data: null, status: "error" });
+  }
 };
 
 sendResult();
