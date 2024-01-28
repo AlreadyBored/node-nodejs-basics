ab132d694aac3c03ffe6eb3fd466fe8a83c9993f
brasoares <nocsoares385@gmail.com>
2024-01-28 14:16:04 -0300

    Updating...

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,14 +1,14 @@
 const create = async () => {
-    const fs = require('fs').promises; // Importing fs module
-    const path = require('path'); // Importing the path module for path work
-    const dataToWrite = 'I am fresh and young'; // To write to fresh.txt
-    const filePath = path.join(__dirname, 'files', 'fresh.txt'); // Grabs path and points to 'files'
+    const fs = require('fs').promises; 
+    const path = require('path'); 
+    const dataToWrite = 'I am fresh and young'; 
+    const filePath = path.join(__dirname, 'files', 'fresh.txt'); 
 
     try {
-        await fs.writeFile(filePath, dataToWrite) // Writing data to above described path: filePath
+        await fs.writeFile(filePath, dataToWrite) 
         console.log('File "${filePath}" has been created with success!');
     } catch (error) {
-        if(error.code === 'EEXIST') { // 'EEXIST' means if 'fresh.txt' already exists, in this context
+        if(error.code === 'EEXIST') { 
             throw new Error('FS operation failed')
         } else {
             console.error(`Error: ${error.message}`);
