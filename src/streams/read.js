0ca5b43773a17e9b15725cfa4716460c2ff32c41
YKavaliou <yulian.kov@gmail.com>
2024-01-28 12:08:51 +0100

    feat: implement streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import fs from "fs";
+import path from "path";
+import {fileURLToPath} from "url";
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.join(path.dirname(fileURLToPath(import.meta.url)), '/files/fileToRead.txt');
+  let string = "";
+
+  const stream = fs.createReadStream(filePath, {encoding: 'utf8'});
+  stream.on('data', data => {
+    string += data
+  });
+
+  stream.on('end', () => {
+    process.stdout.write(string + '\n')
+  })
 };
 
 await read();
