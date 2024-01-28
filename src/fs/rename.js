b44b8a86af073a4f181750d76ac3dfd03182fd01
Olena Datso <elena.shcheglova@gmail.com>
2024-01-27 20:46:06 -0700

    feat: add rename func

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,29 @@
+import fsPromises from 'node:fs/promises';
+
+const folderPath = 'src/fs/files';
+const error = new Error('FS operation failed');
+
 const rename = async () => {
-    // Write your code here 
+  try {
+  await fsPromises
+    .readdir(folderPath)
+    .then((arr) => arr.map(async (name) =>  {
+      if (
+        name === 'properFilename.md' ||
+        (!arr.includes('wrongFilename.txt') &&
+          !arr.includes('properFilename.md'))
+      ) {
+        throw error;
+      }
+      else if (name === 'wrongFilename.txt')
+          await fsPromises.rename(
+            `${folderPath}/${name}`,
+            `${folderPath}/properFilename.md`
+          );
+    }));
+  } catch {
+    throw new Error (error)
+  }
 };
 
 await rename();
