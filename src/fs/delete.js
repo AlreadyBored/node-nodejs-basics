7f2e0bbe23276e5befa7742883c74a6100ba99e5
Lapitskiy <hiimisha@mail.ru>
2024-01-29 00:44:33 +0300

    feat: finish fs module "delete" function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import fs from "fs/promises";
+
 const remove = async () => {
-    // Write your code here 
+    const filePath = 'src/fs/files/fileToRemove.txt';
+
+    try {
+        await fs.access(filePath);
+        await fs.unlink(filePath);
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        } else {
+            throw err;
+        }
+    }
 };
 
 await remove();
