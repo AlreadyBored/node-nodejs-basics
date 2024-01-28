a388a668ea53604aec8f70cd9d1215fcaa132f31
net-ka <a.k.fedosova@gmail.com>
2024-01-28 19:43:16 +0100

    feat: add spawnChildProcess function to cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,10 @@
+import { fork } from 'child_process';
+
 const spawnChildProcess = async (args) => {
     // Write your code here
+
+    const path = new URL('./files/script.js', import.meta.url);
+    fork(path, args);
 };
 
 // Put your arguments in function call to test this functionality
