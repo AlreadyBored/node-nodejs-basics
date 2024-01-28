954ba9fade894053bf5bff04b9854bd60428aa56
Lapitskiy <hiimisha@mail.ru>
2024-01-29 01:34:22 +0300

    feat: finish hash module "hash" function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,31 @@
+import crypto from 'crypto';
+import fs from 'fs';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = 'src/hash/files/fileToCalculateHashFor.txt';
+    const hash = crypto.createHash('sha256');
+
+    try {
+        const input = fs.createReadStream(filePath);
+
+        input.on('readable', () => {
+            const data = input.read();
+
+            if (data)
+                hash.update(data);
+            else {
+                console.log(hash.digest('hex'));
+            }
+        });
+
+        input.on('error', (err) => {
+            console.error(`Error reading ${filePath}`);
+            throw err;
+        });
+    } catch (err) {
+        console.error(`Error in calculateHash function`);
+        throw err;
+    }
 };
 
 await calculateHash();
