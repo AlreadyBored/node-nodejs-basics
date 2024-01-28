82459f6c17ac688a4bda9bbd58cb2d55f08d5322
Olena Datso <elena.shcheglova@gmail.com>
2024-01-27 21:10:17 -0700

    feat: add list func

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,30 @@
+import fsPromises from 'node:fs/promises';
+
+const folderPath = 'src/fs/files';
+const error = new Error('FS operation failed');
+
 const list = async () => {
-    // Write your code here 
+  try {
+      const files = await fsPromises.readdir(folderPath);
+      const trueFiles = [];
+      await Promise.all(
+        files.map(async (file) => {
+          try {
+            const isFile = (
+              await fsPromises.stat(`${folderPath}/${file}`)
+            ).isFile();
+            if (isFile) {
+              trueFiles.push(file);
+            }
+          } catch (e) {
+            console.error(e);
+          }
+        })
+      );
+      console.log(trueFiles);
+  } catch {
+    throw error;
+  }
 };
 
 await list();
