7c15dca7e4862c7f99cd5167be9b319b6ad49463
Оля Брикса <oly.briksa@gmail.com>
2024-01-29 00:11:29 +0300

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,15 @@
+import { readdir } from 'node:fs/promises';
+
+const errorMessage = 'FS operation failed';
+const filesPath = new URL('files', import.meta.url);
+
 const list = async () => {
-    // Write your code here 
+    try{
+        const filesList = await readdir(filesPath);
+        console.log('File list: ', filesList);
+    } catch (err) {
+        throw new Error (errorMessage);
+    }
 };
 
 await list();
