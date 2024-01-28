963c29c8472da85257fa9935600a9a3868900c56
Lapitskiy <hiimisha@mail.ru>
2024-01-29 00:16:30 +0300

    feat: finish fs module "copy" function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,34 @@
+import fs from 'fs/promises';
+import path from 'path';
+
 const copy = async () => {
-    // Write your code here 
+    const originalDir = 'src/fs/files';
+    const copyDir = 'src/fs/files_copy';
+
+    try {
+        await fs.access(originalDir);
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        await fs.access(copyDir);
+        throw new Error('FS operation failed');
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            await fs.mkdir(copyDir);
+        } else {
+            throw err;
+        }
+    }
+
+    const files = await fs.readdir(originalDir);
+
+    for (let file of files) {
+        const srcFile = path.join(originalDir, file);
+        const destFile = path.join(copyDir, file);
+        await fs.copyFile(srcFile, destFile);
+    }
 };
 
 await copy();
