b352822098739a872baec715aa0ba22803b5ee7e
Eugene Solomonyk <solosoft2000@mail.ru>
2024-01-28 11:27:22 +0200

    feat: cp completed

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,16 @@
+import path from 'path';
+import { fork } from 'child_process';
+import { fileURLToPath } from 'url';
+
 const spawnChildProcess = async (args) => {
     // Write your code here
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const scriptFile = path.join(__dirname, 'files', 'script.js');
+
+    const cp = fork(scriptFile, args, {silent: false});
+
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ['q','w','e']);
