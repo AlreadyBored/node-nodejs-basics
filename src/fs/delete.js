6e9b0ba01a9d317c5fcab3ce36ea1aba45d1295c
brasoares <nocsoares385@gmail.com>
2024-01-28 16:39:29 -0300

    Updating...

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,14 @@
 const remove = async () => {
-    // Write your code here 
+    const fs = require('fs').promises;
+    const path = require('path');
+    const filePath = path.join(__dirname, 'fileToRemove.txt');
+
+    try {
+        await fs.access(filePath);
+        await fs.unlink(filePath);
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await remove();
