83312cacf59f6254f1a47be4b9d2dea528f5eabd
Dima Chemerevsky <dchemerevsky@aras.com>
2024-01-28 14:16:59 +0100

    Streams (src/streams). Transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,13 @@
+import { stdout, stdin } from 'process';
+import { Transform } from 'stream';
 const transform = async () => {
-    // Write your code here 
+	const reverseInput = new Transform({
+		transform(chunk, encoding, callback) {
+			this.push(chunk.reverse());
+			callback();
+		}
+	});
+	stdin.pipe(reverseInput).pipe(stdout);
 };
 
 await transform();
