05781a2569c796ce60e4a59b1e8956d6783c0d2f
Olena Datso <elena.shcheglova@gmail.com>
2024-01-27 21:22:35 -0700

    feat: add read func

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+
+import fsPromises from 'node:fs/promises';
+
+const folderPath = 'src/fs/files';
+const fileName = 'fileToRead.txt';
+const error = new Error('FS operation failed');
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const data = await fsPromises.readFile(`${folderPath}/${fileName}`, {
+      encoding: 'utf8',
+    });
+    console.log(data);
+  } catch {
+    throw error;
+  }
 };
 
 await read();
