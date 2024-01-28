710bc394e4d2fc1d424d921209c4fff73389bb8c
Andrei <kaliarovich93@gmail.com>
2024-01-28 14:46:21 +0200

    feat: add implementation of streams/write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import fs from "fs";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const write = async () => {
-    // Write your code here 
+  // Write your code here
+
+  const pathToFile = path.join(__dirname, "files", "fileToWrite.txt");
+  const writeStream = fs.createWriteStream(pathToFile);
+
+  process.stdin.on("data", (data) => {
+    writeStream.write(data);
+  });
+    
 };
 
 await write();
