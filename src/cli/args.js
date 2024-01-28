58ed61a4b7a76f78f68c0ceb1cc675260b2813b1
Mikhail <nodeprog@gmail.com>
2024-01-28 06:30:15 +0300

    feat: implement function "args"

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    const res = [];
+
+    for (let i = 0; i < args.length; i += 2) {
+        if (args[i].startsWith('--')) {
+            res.push(`${args[i].slice(2)} is ${args[i + 1]}`);
+        }
+    }
+    console.log(res.join(', '))
 };
 
 parseArgs();
