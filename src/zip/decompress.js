ad46279f0bba9ae29ef9a914ac102831c6779ce7
msultanovdev <mukhammadamin.sultanov@gmail.com>
2024-01-28 15:10:51 +0500

    feat: add solutions to zip tasks

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+import fs from "fs";
+import path, {dirname} from "path";
+import { fileURLToPath } from "url";
+import { createUnzip } from "zlib";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const readStream = fs.createReadStream(path.join(__dirname, "files", "archive.gz"));
+const writeStream = fs.createWriteStream(path.join(__dirname, "files", "fileToCompress.txt"));
+const unGzip = createUnzip();
+
 const decompress = async () => {
-    // Write your code here 
+    try {
+        readStream.pipe(unGzip).pipe(writeStream);
+    } catch (e) {
+        throw new Error("FS operation failed");
+    }
 };
 
 await decompress();
