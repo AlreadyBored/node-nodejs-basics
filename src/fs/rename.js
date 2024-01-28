66e3f9ab08f0964e62a1c7b970a2aade5798f096
Lapitskiy <hiimisha@mail.ru>
2024-01-29 01:07:22 +0300

    feat: finish fs module "rename" function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,27 @@
+import fs from "fs/promises";
+
 const rename = async () => {
-    // Write your code here 
+    const oldPathFile = 'src/fs/files/wrongFilename.txt';
+    const newPathFile = 'src/fs/files/properFilename.md';
+
+    let fileExists = false;
+
+    try {
+        await fs.access(oldPathFile);
+        fileExists = true;
+
+        await fs.access(newPathFile);
+    } catch (err) {
+        if (err.code !== 'ENOENT') {
+            throw err;
+        }
+    }
+
+    if (fileExists) {
+        throw new Error('FS operation failed');
+    } else {
+        await fs.rename(oldPathFile, newPathFile);
+    }
 };
 
 await rename();
