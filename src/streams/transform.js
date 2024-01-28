8bb3a29c4ccf7334de560d2c326b71b14b5061af
Dmitry Bludchy <dmitrybludchy@gmail.com>
2024-01-28 23:33:26 +0300

    feat: implement transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
+import { Transform } from 'stream';
+import { pipeline } from 'stream/promises';
+
+const reversedText = (str) => Array.from(str).reverse().join('');
+
 const transform = async () => {
-    // Write your code here 
+  const transformStream = new Transform({
+    transform(chunk, encoding, callback) {
+      callback(null, reversedText(chunk.toString()));
+    },
+  });
+
+  await pipeline(process.stdin, transformStream, process.stdout);
 };
 
 await transform();
