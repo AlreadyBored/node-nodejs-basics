cabf3b28347afe746f5f71b1c80cfa20c23bdddb
brasoares <nocsoares385@gmail.com>
2024-01-28 17:05:08 -0300

    Updating...

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,9 +1,20 @@
 const read = async () => {
-    // Write your code here 
+    const fs = require('fs');
+
+    try {
+        const fileContent = await fs.promises.readFile('fileToRead.txt', 'utf8');
+        console.log(fileContent);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        } else {
+            throw error; // Good practice
+        }
+    }
 };
 
 await read();
 
 /* read.js - implement function that prints content of the fileToRead.txt into
-console (if there's no file fileToRead.txt Error with message
- FS operation failed must be thrown) */
+* console (if there's no file fileToRead.txt Error with message
+* FS operation failed must be thrown) */
