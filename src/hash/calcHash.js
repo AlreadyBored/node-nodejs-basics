bac946ee3da5801272b8e369e0c4dbd493bd3487
buterbrot359 <batowka359@gmail.com>
2024-01-28 22:28:22 +0600

    finish: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,44 @@
+import { createHash } from "crypto";
+import fs from "fs";
+import path from "path";
+
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+
+  try {
+    // Write your code here
+    const fileData = await readFileWithStream(filePath);
+
+    const hash = createHash("sha256").update(fileData).digest("hex");
+
+    console.log(hash);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const readFileWithStream = async (filePath) => {
+  return new Promise((res, rej) => {
+    let chunks = [];
+    const stream = fs.createReadStream(filePath, { encoding: "utf8" });
+
+    stream.on("data", (chunk) => {
+      chunks.push(chunk);
+    });
+
+    stream.on("error", (err) => {
+      rej(err);
+    });
+
+    stream.on("close", () => {
+      res(chunks.join(""));
+    });
+  });
 };
 
 await calculateHash();
