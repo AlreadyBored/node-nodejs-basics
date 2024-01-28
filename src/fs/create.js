eff7371ed3cd747be3428d8290826069e59123a3
VoitehovichXenia <voitehovichkg@gmail.com>
2024-01-28 18:46:49 +0300

    feat: implement FS create.js task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,13 @@
+// implement function that creates new file fresh.txt with content I am fresh and young inside of the files folder (if file already exists Error with message FS operation failed must be thrown)
+import { writeFile, readdir } from 'node:fs/promises';
+
 const create = async () => {
-    // Write your code here 
+    const files = await readdir('files')
+    if (files.includes('fresh.txt')) {
+        throw new Error('FS operation failed')
+    } else  {
+        writeFile('files/fresh.txt', 'I am fresh and young ')
+    }
 };
 
 await create();
