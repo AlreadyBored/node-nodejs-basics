ff3836d43905eabff5306164e7a8d77e896606b5
Dadash <huseynzade.dadas@gmail.com>
2024-01-28 21:33:35 +0400

    add file deletion and read functionality

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import fs from 'fs/promises';
+import path from 'path';
+
 const remove = async () => {
-    // Write your code here 
+    const filePath = path.join('files', 'fileToRemove.txt');
+    try {
+        await fs.access(filePath);
+        await fs.unlink(filePath);
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        } else {
+            throw err;
+        }
+    }
 };
 
 await remove();
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import fs from 'fs/promises';
+import path from 'path';
+
 const read = async () => {
-    // Write your code here 
+    const filesPath = path.join('files', 'fileToRead.txt');
+    try {
+        await fs.access(filesPath)
+        const fileContent = await fs.readFile(filesPath, { encoding: 'utf-8' });
+        console.log(fileContent);
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        } else {
+            throw err;
+        }
+    }
 };
 
 await read();
