cc933270ab0d864fbf853a087ec98d9a0e015faf
Julia Usanova <juliausanova@gmail.com>
2024-01-28 23:58:37 +0200

    feat: implement read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import { readFile } from 'node:fs';
+
 const read = async () => {
-    // Write your code here 
+    readFile('./files/fileToRead.txt', { encoding: 'utf8' }, function (err, data) {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                throw 'FS operation failed';
+            }
+            
+            throw err;
+        }
+
+        console.log(data);
+    })
 };
 
 await read();
