3f530a6e5ec11d89e5b2114d1dfa2ebf2510545c
buterbrot359 <batowka359@gmail.com>
2024-01-28 21:58:21 +0600

    finish: cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,24 @@
 const parseArgs = () => {
-    // Write your code here 
+  // Write your code here
+  const args = process.argv.slice(2);
+
+  const dict = {};
+
+  args.forEach((el, index, arr) => {
+    if (el.startsWith("--")) {
+      const propName = el.split("").slice(2).join("");
+
+      dict[propName] = arr[index + 1];
+    }
+  });
+
+  let ans = [];
+
+  for (let prop in dict) {
+    ans.push(`${prop} value is ${dict[prop]}`);
+  }
+
+  console.log(ans.join(", "));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,16 @@
 const parseEnv = () => {
-    // Write your code here 
+  // Write your code here
+  const envVars = process.env;
+  const prefixToFind = "RSS_";
+  let needVars = [];
+
+  for (let key in envVars) {
+    if (key.startsWith(prefixToFind)) {
+      needVars.push(`${key}=${envVars[key]}`);
+    }
+  }
+
+  console.log(needVars.join("; "));
 };
 
 parseEnv();
