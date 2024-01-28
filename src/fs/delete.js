cc09e2fca6814753a27d1957439b059a9efb2315
alex.pranevich <freennnn@gmail.com>
2024-01-28 08:53:36 +0100

    feat: fs:delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fsPromises from 'fs/promises';
+import * as utils from './utils.js';
+
+export const dirName = path.dirname(fileURLToPath(import.meta.url));
+const sourcePath = path.join(dirName, "files", "fileToRemove.txt");
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    await fsPromises.rm(sourcePath);
+  } catch (error) {
+    //console.log(error);
+    throw Error(utils.fsErrorMsg);
+  }
 };
 
 await remove();
