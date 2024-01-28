39d487547ff39bc8d0ddbe17f7969d8592311e23
Mikhail <nodeprog@gmail.com>
2024-01-28 08:17:11 +0300

    feat: implement function "streams/write"

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import { fs, getFilePath } from "../utils/fs.js";
+import { pipeline } from 'node:stream';
+
+const filePath = getFilePath(import.meta.url, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+    const fileStream = fs.createWriteStream(filePath);
+
+    pipeline(process.stdin, fileStream, err => {
+        if (err) throw err;
+    })
 };
 
 await write();
