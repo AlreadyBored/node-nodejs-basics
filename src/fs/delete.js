e6ee6188bbf62a4fe6d624f8def3434229d44db4
Olena Datso <elena.shcheglova@gmail.com>
2024-01-27 20:52:30 -0700

    feat: add delete func

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,14 @@
+import fsPromises from 'node:fs/promises';
+
+const folderPath = 'src/fs/files';
+const error = new Error('FS operation failed');
+
 const remove = async () => {
-    // Write your code here 
+  try {
+      await fsPromises.rm(`${folderPath}/fileToRemove.txt`);
+  } catch {
+    throw error;
+  }
 };
 
 await remove();
