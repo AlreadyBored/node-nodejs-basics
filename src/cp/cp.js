b79c4fb890978fc4cfc98b9d0f430ea83b645008
Eugene Solomonyk <solosoft2000@mail.ru>
2024-01-28 13:00:04 +0200

    fix: add events to cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -10,6 +10,13 @@ const spawnChildProcess = async (args) => {
 
     const cp = fork(scriptFile, args, {silent: false});
 
+    cp.on('message', (message) => {
+      console.log(`Received from child process: ${message}`);
+    });
+
+    cp.on('close', (code) => {
+      console.log(`Child process exited with code ${code}`);
+    });
 };
 
 // Put your arguments in function call to test this functionality
