f71d60da2343aeb812d95a06c06090d7d9d63472
Andrei <kaliarovich93@gmail.com>
2024-01-28 22:44:39 +0200

    feat: add implementation of zip/decopress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import { createGunzip } from "zlib";
+import { pipeline } from "stream";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const decompress = async () => {
-    // Write your code here 
+  // Write your code here
+
+  const pathToArchivedFile = path.join(__dirname, "files", "fileToCompress.txt");
+  const pathToFile = path.join(__dirname, "files", "archive.gz");
+
+  const gunZip = createGunzip();
+  const sourceFile = fs.createReadStream(pathToFile);
+  const destinationFile = fs.createWriteStream(pathToArchivedFile);
+
+  pipeline(sourceFile, gunZip, destinationFile, () => {
+    console.log("Decompression completed");
+  });
 };
 
 await decompress();
