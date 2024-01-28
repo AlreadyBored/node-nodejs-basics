7aa09c1bdd8f22bdd6a60b26f12bc7c23ce75337
Mikhail <nodeprog@gmail.com>
2024-01-28 05:48:23 +0300

    refactor: cut flag default "r"

--- src/fs/read.js
+++ src/fs/read.js
@@ -3,7 +3,7 @@ import { fs, getFilePath } from "../utils/fs.js";
 const fileToRead = getFilePath(import.meta.url, 'files', 'fileToRead.txt');
 
 const read = async () => {
-    const fileData = fs.readFile(fileToRead, { flag: 'r', encoding: 'utf-8' }, (err, data) => {
+    const fileData = fs.readFile(fileToRead, { encoding: 'utf-8' }, (err, data) => {
         if (err) { throw new Error('FS operation failed') }
         console.log(data);
     })
