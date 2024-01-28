537916edecbfc647e95917db435e94808d23b0b9
Оля Брикса <oly.briksa@gmail.com>
2024-01-28 22:07:03 +0300

    feat: implement rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,19 @@
+import { rename as renameFile } from 'node:fs/promises';
+
+const errorMessage = 'FS operation failed';
+
+const oldName = new URL('files/wrongFilename.txt', import.meta.url);
+const newName = new URL('files/properFilename.md', import.meta.url);
+
 const rename = async () => {
-    // Write your code here 
+
+    try{
+        await renameFile(oldName, newName);
+        console.log('File \'wrongFilename.txt\' renamed');
+    } catch (err) {
+        throw new Error (errorMessage);
+    }
+
 };
 
 await rename();
