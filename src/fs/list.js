d27383521e0e2e0017bbf05bdb245877c5c21e58
Lapitskiy <hiimisha@mail.ru>
2024-01-29 00:53:06 +0300

    feat: finish fs module "list" function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import fs from "fs/promises";
+
 const list = async () => {
-    // Write your code here 
+    const dirPath = 'src/fs/files';
+
+    try {
+        await fs.access(dirPath);
+
+        const files = await fs.readdir(dirPath);
+        console.log(files);
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        } else {
+            throw err;
+        }
+    }
 };
 
 await list();
