44e848d5234727aeb22a142925aa92106c93e9fe
Dadash <huseynzade.dadas@gmail.com>
2024-01-28 22:56:35 +0400

    parse env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+    for (const key in process.env) {
+        if (key.startsWith('RSS_')) {
+            console.log(`${key}=${process.env[key]}`);
+        }
+    }
 };
 
 parseEnv();
