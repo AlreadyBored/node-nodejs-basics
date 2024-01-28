f94ba0417d26a110971621da7046f64e554751c6
Kirill Shishov <skireal@mail.ru>
2024-01-28 20:09:06 +0300

    refactor: change dist

--- src/fs/create.js
+++ src/fs/create.js
@@ -6,14 +6,13 @@ const { writeFile } = promises;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const file = path.join(__dirname, 'files', 'fresh.txt');
 
 const create = async () => {
   try {
-    await writeFile(file, 'I am fresh and young', { flag: 'wx' });
+    await writeFile(path.join(__dirname, 'files/fresh.txt'), 'I am fresh and young', { flag: 'wx' });
   } catch (err) {
     if (err.code === 'EEXIST') {
-      console.error('FS operation failed');
+      throw new Error('FS operation failed');
     } else {
       console.error(err);
     }
