7df60586fb881216e1b24b4ff99f37c8eaf9eabf
brasoares <nocsoares385@gmail.com>
2024-01-28 15:30:38 -0300

    Updating...

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,9 @@
 const rename = async () => {
-    
+    const fs = require('fs');
+    const path = require('path');
+
+    const oldPath = path.join(__dirname, 'wrongFilename.txt');
+    const newPath = path.join(__dirname, 'properFilename.md');
 };
 
 await rename();
