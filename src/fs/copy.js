8f2bbc16a4f9b7740a3744746326140a8bf4fe8e
VoitehovichXenia <voitehovichkg@gmail.com>
2024-01-28 23:10:48 +0300

    Implement a FS copy.js task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,24 @@
+// implement function that copies folder files files with all its content into folder files_copy at the same level (if files folder doesn't exists or files_copy has already been created Error with message FS operation failed must be thrown)
+import { access, constants, statfs, cp, mkdir } from 'node:fs/promises'
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        await statfs('files');
+    } catch (err) {
+        throw new Error ('FS operation failed');
+    }
+  
+    try {
+        await cp('./files', './files_copy', {
+            force: false,
+            errorOnExist: true,
+            recursive: true
+        });
+    } catch (err) {
+        if (err.code == 'ERR_FS_CP_EEXIST') {
+            throw new Error ('FS operation failed');
+        }
+    }
 };
 
 await copy();
