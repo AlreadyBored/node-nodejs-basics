0b80e06638ba80f787160721f27cb3ceaff3981d
Dima Chemerevsky <dchemerevsky@aras.com>
2024-01-28 14:34:15 +0100

    Zlib (src/zip). Compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,10 @@
+import fs from 'fs';
+import zlib from 'zlib';
 const compress = async () => {
-    // Write your code here 
+	const readFile = fs.createReadStream('files/fileToCompress.txt');
+	const zip = zlib.createGzip();
+	const writeFile = fs.createWriteStream('files/archive.gz');
+	readFile.pipe(zip).pipe(writeFile); 
 };
 
 await compress();
