e0f223f7f67685ca7b9f03a74962c25378a9b8fd
baranovdv <baranov.d.v.1988@gmail.com>
2024-01-29 04:26:45 +0700

    feat: add cli solution

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
 const parseArgs = () => {
-    // Write your code here 
+  const envArgs = process.argv
+  const envArgsParsed = envArgs.reduce((acc, arg, index, array) => {
+    if (arg.startsWith('--')) {
+      acc.push(`${arg.slice(2)} is ${array[index + 1]}`)
+    }
+    return acc
+  }, [])
+
+  console.log(envArgsParsed.join(', '))
 };
 
 parseArgs();
+
+// args.js - implement function that parses command line arguments 
+// (given in format --propName value --prop2Name value2, you don't need to validate it) and prints them to 
+// the console in the format propName is value, prop2Name is value2
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+    const envVarsKeys = Object.keys(process.env)
+    const RSSEntries = envVarsKeys.filter((key) => key.startsWith('RSS_'))
+    const RSSEntriesConsoleString = RSSEntries.map((key => `${key}=${process.env[key]}`)).join('; ')
+
+    console.log(RSSEntriesConsoleString)
 };
 
 parseEnv();
+
+// env.js - implement function that parses environment variables with prefix RSS_ and prints them to the 
+// console in the format RSS_name1=value1; RSS_name2=value2
