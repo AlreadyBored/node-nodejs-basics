1653629045341ce408fe2fec6164dca7d7ecde23
rahunak <euhene@linnovate.net>
2024-01-28 16:16:01 +0300

    #15: stream - transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+import process from "node:process";
+import { Transform } from "stream";
+
 const transform = async () => {
-    // Write your code here 
+  // Creates transform stream for transforming data from file.
+  // Set transform function to change incoming data.
+  const transStr = new Transform({
+    async transform(chunk, encoding, callback) {
+      //read more https://nodejs.org/api/stream.html#transform_transformchunk-encoding-callback
+      this.push(chunk.toString().split("").reverse().join("") + '\n');
+      callback();
+    },
+  });
+  // Read stream, then data changes in Transform stream, then prints to output.
+  process.stdin.pipe(transStr).pipe(process.stdout);
 };
 
 await transform();
