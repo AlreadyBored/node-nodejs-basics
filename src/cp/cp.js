c130ea68e1552ba5005c13a5eed3bff48f982fe3
Mikhail <nodeprog@gmail.com>
2024-01-28 10:34:53 +0300

    feat: implement function "cp"

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,14 @@
+import { getFilePath } from '../utils/fs.js';
+import { fork } from 'node:child_process';
+
+const childPath = getFilePath(import.meta.url, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = fork(childPath, args);
+    child.on("close", (code) => {
+        console.log("child process exited with code " + code);
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2']);
