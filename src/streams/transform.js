63c55bfb78b17b5ae1a33888c8696924d3c18c72
Andrei <kaliarovich93@gmail.com>
2024-01-28 15:30:09 +0200

    feat: add implementation of streams/transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+    // Write your code here
+
+    const reverseValue= new Transform({
+        transform(chunk, encoding, callback) {
+            this.push(chunk.toString().trim().split('').reverse().join('') + '\n');
+          callback();
+        },
+    });
+    
+    process.stdin.pipe(reverseValue).pipe(process.stdout);
+
+
 };
 
 await transform();
