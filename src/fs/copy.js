89e645895c1599cb77c499121c86d9c5d5c3ba86
Mikhail <nodeprog@gmail.com>
2024-01-28 04:13:16 +0300

    feat: implement function "copy"

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,16 @@
+import { fs, getFilePath } from "../utils/fs.js";
+
+const srcForder = getFilePath(import.meta.url, 'files');
+const destForder = getFilePath(import.meta.url, 'files_copy');
+
 const copy = async () => {
-    // Write your code here 
+    if (fs.existsSync(destForder)) {
+        throw new Error('FS operation failed');
+        return;
+    }
+    fs.cp(srcForder, destForder, { recursive: true }, (err) => {
+        if (err) throw new Error('FS operation failed');
+    })
 };
 
 await copy();
