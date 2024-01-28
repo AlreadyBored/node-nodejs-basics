56de2a2dab60327a93c8df4050d568b24479232e
rahunak <euhene@linnovate.net>
2024-01-28 23:32:37 +0300

    #17: child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,35 @@
+import child_process from "child_process";
+import process from "node:process";
+import path from "path";
+import { fileURLToPath } from "url";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  console.log("spawnChildProcess args", args);
+  //Cntrl + C = exit
+  process.on("SIGINT", () => {
+    process.exit();
+  });
+
+  // Script file.
+  const __filename = fileURLToPath(import.meta.url);
+  // Folder of script file.
+  const __dirname = path.dirname(__filename);
+
+  // Gets the absolute path to the script.js file
+  const scriptPath = path.join(__dirname, "files", "script.js");
+
+  // Spawns a child process using the script.js file
+  // Read more https://nodejs.org/api/child_process.html#optionsstdio
+  // .spawn variant
+  // const child = child_process.fork("node", [scriptPath, ...args]);
+  // .fork variant
+  const child = child_process.fork(scriptPath, args);
+
+  // Handle events when the child process exits
+  child.on("exit", (code, signal) => {
+    process.exit(code);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["someArgument1", "someArgument2", "someArgument3"]);
