9e10415b74bc106fdce4d6af0637f7b777d48786
Dmitry Bludchy <dmitrybludchy@gmail.com>
2024-01-28 22:35:05 +0300

    feat: implement write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,26 @@
+import { createWriteStream } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { pipeline } from 'stream/promises';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const filePath = join(__dirname, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+  const writableStream = createWriteStream(filePath, { flags: 'a' });
+
+  process.on('SIGINT', () => {
+    console.log('\nReceived SIGINT. Closing streams and exiting...');
+    writableStream.end();
+    process.exit();
+  });
+
+  try {
+    console.log('Writing to file completed successfully.');
+    await pipeline(process.stdin, writableStream);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await write();
