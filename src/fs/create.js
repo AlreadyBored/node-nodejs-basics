ce295cee21c2167e3896e3532e6ad7b7b6daeab1
Kirill Shishov <skireal@mail.ru>
2024-01-28 19:31:52 +0300

    feat: implement create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { promises } from 'node:fs';
+
+const { access, writeFile } = promises;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const file = path.join(__dirname, 'files', 'fresh.txt');
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await access(file);
+    throw new Error('FS operation failed');
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      await writeFile(file, 'I am fresh and young');
+    } else {
+      console.error(err);
+    }
+  }
 };
 
 await create();
