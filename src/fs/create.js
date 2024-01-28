d250f2f548dee4dc181f363aa11bfa722995a654
Оля Брикса <oly.briksa@gmail.com>
2024-01-28 13:40:45 +0300

    feat: implement create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import { appendFile } from 'node:fs/promises';
+
+const data = 'I am fresh and young';
+const path = 'src/fs/files/fresh.txt';
+const errorMessage = 'FS operation failed';
+
 const create = async () => {
-    // Write your code here 
+
+    try{
+        await appendFile(path, data, {flag: 'wx'});
+        console.log('File \'fresh.txt\' created successfully');
+    } catch (err) {
+        throw new Error (errorMessage);
+    }
+
 };
 
 await create();
