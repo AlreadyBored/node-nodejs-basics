5e23a37c0e28db5fc3e857a13b9c64de65760b43
Karina Shupik <77788896h@gmail.com>
2024-01-28 10:21:54 +0200

    feat: implement wt function

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,52 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { Worker } from 'node:worker_threads';
+import os from 'node:os';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const workerFilePath = path.join(__dirname, 'worker.js');
+
+const numCores = os.cpus().length;
+
+const runWorker = (data) => new Promise((resolve, reject) => {
+    const worker = new Worker(workerFilePath, {
+        workerData: data
+    });
+
+    worker.on('message', (message) => {
+        if (message.status === 'resolved') {
+            resolve({ status: 'resolved', data: message.data });
+        } else {
+            reject({ status: 'error', data: null });
+        }
+    });
+
+    worker.on('error', (error) => {
+        reject({ status: 'error', data: null });
+    });
+
+    worker.on('exit', (code) => {
+        if (code !== 0) {
+            reject({ status: 'error', data: null });
+        }
+    });
+});
+
 const performCalculations = async () => {
-    // Write your code here
+    const workers = [];
+
+    try {
+        for (let i = 1; i <= numCores; i++) {
+            workers.push(runWorker(i + 10));
+        }
+
+        const result = await Promise.all(workers);
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,11 @@
+import { parentPort, workerData } from 'worker_threads';
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    const result = nthFibonacci(workerData);
+    parentPort.postMessage({ status: 'resolved', data: result });
 };
 
 sendResult();
