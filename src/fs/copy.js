86a3b60ba9c0e1a181b600bc2ccd8ca5cbfd15bc
Dadash <huseynzade.dadas@gmail.com>
2024-01-28 21:15:55 +0400

    copy,create,rename function implemented

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,29 @@
+import fs from 'fs/promises';
+import path from 'path';
+
 const copy = async () => {
-    // Write your code here 
+    const sourcePath = path.join('files');
+    const destinationPath = path.join('files_copy');
+
+    try {
+        await fs.access(sourcePath);
+        await fs.access(destinationPath);
+        throw new Error('FS operation failed');
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            if (err.path === sourcePath) {
+                throw new Error('FS operation failed');
+            } else {
+                await fs.mkdir(destinationPath);
+                const files = await fs.readdir(sourcePath);
+                await Promise.all(files.map(file =>
+                    fs.copyFile(path.join(sourcePath, file), path.join(destinationPath, file))
+                ));
+            }
+        } else {
+            throw err;
+        }
+    }
 };
 
 await copy();
