2d72df4c0d720e0595b2edca309a7f4bee9b0ed5
Mikhail <nodeprog@gmail.com>
2024-01-28 08:04:29 +0300

    feat: implement function "streams/read"

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import { fs, getFilePath } from '../utils/fs.js';
+
+const filePath = getFilePath(import.meta.url, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    const fileStream = fs.createReadStream(filePath);
+
+    fileStream.pipe(process.stdout);
+    fileStream.on('end', () => {
+        process.stdout.end('\n');
+    });
 };
 
 await read();
