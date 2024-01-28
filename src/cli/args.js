4d6d5f058adbe33ab3329f6932d37776f3675479
Lapitskiy <hiimisha@mail.ru>
2024-01-28 23:09:46 +0300

    feat: finish "parseArgs" function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,22 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    let output = '';
+
+    for (let i = 0; i < args.length; i++) {
+        const arg = args[i];
+
+        if (arg.startsWith('--')) {
+            const propName = arg.substring(2);
+            let value = 'undefined';
+
+            if (i + 1 < args.length && !args[i + 1].startsWith('--')) {
+                value = args[++i];
+            }
+
+            output += `${propName} is ${value}, `;
+        }
+    }
+    console.log(output.slice(0, -2));
 };
 
 parseArgs();
