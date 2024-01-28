a84001ae72d5dcf3b6ddc6e05568af68e01b899d
Dmitry Bludchy <dmitrybludchy@gmail.com>
2024-01-28 22:07:38 +0300

    feat: implement read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import { createReadStream } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const filePath = join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const readableStream = createReadStream(filePath, { encoding: 'utf-8' });
+
+    for await (const chunk of readableStream) {
+      process.stdout.write(chunk);
+    }
+  } catch (error) {
+    console.error('An error occurred while reading the file:', error.message);
+  }
 };
 
 await read();
