5eec375a686d50dab73c5f5b41876e5a7c1b37dc
Mikhail <nodeprog@gmail.com>
2024-01-28 06:13:23 +0300

    feat: implement function "env"

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+    let rssProps = [];
+
+    for (let prop in process.env) {
+        if (prop.startsWith('RSS_')) {
+            rssProps.push(`${prop}=${process.env[prop]}`);
+        }
+    }
+
+    console.log(rssProps.join('; '));
 };
 
 parseEnv();
