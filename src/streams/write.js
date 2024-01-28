336c0fba9ddf62bfee0393cc40f9c69ee05cbc64
YKavaliou <yulian.kov@gmail.com>
2024-01-28 12:40:57 +0100

    feat: implement streams/write.txtx

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import path from "path";
+import fs from "fs";
+import {fileURLToPath} from "url";
+
 const write = async () => {
-    // Write your code here 
+  const filePath = path.join(path.dirname(fileURLToPath(import.meta.url)), '/files/fileToWrite.txt');
+  const stream = fs.createWriteStream(filePath, {flags: 'a'});
+
+  process.stdin.on('data', data => {
+    stream.write(data);
+    process.exit();
+  });
 };
 
 await write();
