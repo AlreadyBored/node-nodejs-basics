a00e7996165ab4666a150727240d7977417e3ee6
brasoares <nocsoares385@gmail.com>
2024-01-28 15:33:13 -0300

    Updating...

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -4,6 +4,18 @@ const rename = async () => {
 
     const oldPath = path.join(__dirname, 'wrongFilename.txt');
     const newPath = path.join(__dirname, 'properFilename.md');
+
+    try {
+        await fs.promises.access(oldPath, fs.constants.F_OK);
+        await fs.promises.access(newPath, fs.constants.F_OK);
+        throw new Error('FS operation failed');
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            await fs.promises.rename(oldPath, newPath);
+        } else {
+            throw error;
+        }
+    }
 };
 
 await rename();
