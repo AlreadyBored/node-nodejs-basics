5f2c5013c24787e18ce257d17b712d929fafc79a
Julia Usanova <juliausanova@gmail.com>
2024-01-28 23:28:06 +0200

    feat: implement rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,14 @@
+import { rename as changeName, existsSync } from 'node:fs';
+
 const rename = async () => {
-    // Write your code here 
+
+    if (existsSync('./files/properFilename.txt') || !existsSync('./files/wrongFilename.txt')) {
+        throw 'FS operation failed';
+    }
+    
+    changeName('./files/wrongFilename.txt', './files/properFilename.txt', function (err) {
+        if (err) throw err;
+    });
 };
 
 await rename();
