f0d81a91e867f47e75aa2342728657898f2e4428
Maryia Velesiuk <m.velesiuk@andersenlab.com>
2024-01-29 18:16:08 +0200

    feat: add copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,20 @@
+import { access, constants, cp } from "node:fs";
+
 const copy = async () => {
-    // Write your code here 
+  // Write your code here
+  access("src/fs/files", constants.F_OK, (err) => {
+    if (err) {
+      throw new Error("FS operation failed");
+    }
+  });
+  access("src/fs/files_copy", constants.F_OK, (err) => {
+    if (!err) {
+      throw new Error("FS operation failed");
+    }
+  });
+  cp("src/fs/files", "src/fs/files_copy", { recursive: true }, (err) => {
+    if (err) throw err;
+  });
 };
 
 await copy();
