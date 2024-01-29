be0b29ffead2ce91955a2adc27c146569c90f2c1
DinislamFromUfa <isberdindinislam@gmail.com>
2024-01-30 00:00:53 +0500

    feat: add solution on zip folder

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url';
+import zlib from 'zlib';
+import {pipeline} from 'stream';
 const decompress = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const gzip = zlib.createUnzip();
+    const StreamForRead = fs.createReadStream(path.resolve(__dirname, 'files', 'archive.gz'));
+    const StreamForWrite = fs.createWriteStream(path.resolve(__dirname, 'files', 'fileToCompress.txt'));
+    pipeline(StreamForRead, gzip, StreamForWrite, (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
 };
 
 await decompress();
