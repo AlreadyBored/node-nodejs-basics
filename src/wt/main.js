41cdc05f8a15876e40ed588be6d0434ed33d5917
Lapitskiy <hiimisha@mail.ru>
2024-01-30 02:51:45 +0300

    feat: edit quotes "wt" task

--- src/wt/main.js
+++ src/wt/main.js
@@ -7,22 +7,22 @@ const performCalculations = async () => {
   const numCPUs = os.cpus().length;
   let promises = [];
   const __dirname = dirname(fileURLToPath(import.meta.url));
-  const workerFile = join(__dirname, "worker.js");
+  const workerFile = join(__dirname, 'worker.js');
 
   for (let i = 0; i < numCPUs; i++) {
     const worker = new Worker(workerFile, { workerData: { n: 10 + i } });
 
     const promise = new Promise((resolve, reject) => {
-      worker.on("message", (result) => {
+      worker.on('message', (result) => {
         resolve({
-          status: "resolved",
+          status: 'resolved',
           data: result,
         });
       });
 
-      worker.on("error", () => {
+      worker.on('error', () => {
         reject({
-          status: "error",
+          status: 'error',
           data: null,
         });
       });
