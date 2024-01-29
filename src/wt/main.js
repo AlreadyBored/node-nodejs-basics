35f9b962aef847b652c4543641a5f9911e17ed3a
DinislamFromUfa <isberdindinislam@gmail.com>
2024-01-29 23:59:10 +0500

    feat: add solution on wt folder

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,38 @@
+import {Worker, parentPort, workerData} from 'worker_threads'
+import os from "os";
+import path, { resolve } from 'path';
+import { fileURLToPath } from 'url';
 const performCalculations = async () => {
     // Write your code here
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const cpuNumb = os.cpus().length;
+    const workers = [];
+    const promises = [];
+    for (let i = 0; i < cpuNumb; i += 1) {
+      const worker = new Worker(path.resolve(__dirname, 'worker.js'), {workerData: i + 10});
+
+      const promise = new Promise((resolve, reject) => {
+        worker.on('message', (msg) => {
+          workers[i] = {
+            status: 'resolved',
+            data: msg,
+          }
+          resolve();
+        });
+
+        worker.on('error', (err) => {
+          workers[i] = {
+            status: 'error',
+            data: null,
+          }
+          resolve();
+        });
+      });
+      promises.push(promise);
+    }
+    Promise.all(promises).then(() => {
+      console.log(workers);
+    })
 };
 
 await performCalculations();
