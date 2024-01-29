0e45d2d5fda0127b7fe3486d458afdb152cc7e6b
Julia Usanova <juliausanova@gmail.com>
2024-01-29 20:56:04 +0200

    feat: implement compress / decompress functions

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import { createUnzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import { createReadStream, createWriteStream } from 'node:fs';
+
 const decompress = async () => {
-    // Write your code here 
+    pipeline(
+        createReadStream('./files/archive.gz'),
+        createUnzip(),
+        createWriteStream('./files/fileToCompress.txt'),
+        (err) => {
+            if (err) {
+                console.error('Failed to decompress.', err);
+            }
+        }
+    )
 };
 
 await decompress();
