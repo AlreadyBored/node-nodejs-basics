92e941f021e3ffa8501a1e44c3aa7813a8de1747
max <mcnic@users.noreply.github.com>
2024-01-29 13:06:48 -0800

    feat: implementation 'zip:decompress'

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createUnzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import { promisify } from 'node:util';
+import path from 'node:path';
+
 const decompress = async () => {
-    // Write your code here 
+    const rootPath = path.join(process.cwd(), 'src/zip/files');
+    const input = path.join(rootPath, 'archive.gz');
+    const output = path.join(rootPath, 'fileToCompress.txt');
+
+    const source = createReadStream(input);
+    const destination = createWriteStream(output);
+    const decompress = createUnzip();
+    const pipe = promisify(pipeline);
+
+    await pipe(source, decompress, destination);
 };
 
 await decompress();
