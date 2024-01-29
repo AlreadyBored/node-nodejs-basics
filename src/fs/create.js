3e684efb90f10d59446be9644a6eb2132b88fdce
anton-glebov <anton.glebov@useinsider.com>
2024-01-30 01:26:59 +0300

    feat: implement create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import { writeFile } from "node:fs";
+import * as path from "node:path";
+import { fileURLToPath } from 'node:url';
+
 const create = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const content = 'I am fresh and young';
+    const savePath = path.join(__dirname, 'files', 'fresh.txt');
+
+    await writeFile(savePath, content, { flag: 'wx' }, err => {
+        if (err) {
+            throw new Error('FS operation failed');
+        }
+    });
 };
 
 await create();
