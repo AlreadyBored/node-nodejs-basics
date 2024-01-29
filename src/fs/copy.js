c8878ebfa260bc09118a666b7fb25b5726bb34ba
anton-glebov <anton.glebov@useinsider.com>
2024-01-30 01:27:15 +0300

    feat: implement copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,18 @@
+import fs from "node:fs"
+import * as path from "node:path";
+import { fileURLToPath } from 'node:url';
+
 const copy = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const inPath = path.join(__dirname, 'files');
+    const postfix = '_copy'
+    const outPath = inPath + postfix;
+
+    fs.cp(inPath, outPath, { force: false, errorOnExist: true, recursive: true }, (err) => {
+        if (err) {
+            throw new Error('FS operation failed');
+        }
+    });
 };
 
 await copy();
