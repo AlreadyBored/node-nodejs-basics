568a26e05ca5df72cce803a03533c02ddb4a0441
Denis Slavenko <denis-slavenko1999@mail.ru>
2024-01-30 02:24:51 +0300

    feat(wt): implemented logic in wt functions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,49 @@
+import path, { resolve } from 'path';
+import url from 'node:url';
+import { Worker } from 'node:worker_threads';
+import os from 'node:os';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const workerFilePath = path.join(__dirname, 'worker.js');
+const numberOfLocalCores = os.cpus().length;
+
+const createWorker = (data) => new Promise((resolve, reject) => {
+    const worker = new Worker(workerFilePath, {
+        workerData: data
+    });
+
+    worker.on('message', (msg) => {
+        if (msg.status === 'resolved') {
+            resolve({ status: 'resolved', data: msg.data });
+        } else if (msg.status === 'error') {
+            reject({ status: 'error', data: null });
+        } else {
+            reject({ status: 'unknown', data: null });
+        }
+    });
+
+    worker.on('error', (err) => {
+        reject({ status: 'error', data: null });
+    });
+
+    worker.on('exit', (code) => {
+        if (code !== 0) {
+            reject({ status: 'error', data: null });
+        }
+    });
+});
+
 const performCalculations = async () => {
-    // Write your code here
+    const workersArr = [];
+
+    for (let i = 0; i < numberOfLocalCores; i++) {
+        workersArr.push(createWorker(i + 10));
+    }
+
+    const res = await Promise.all(workersArr);
+    console.log(res);
 };
 
 await performCalculations();
