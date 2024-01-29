1b6129f5bdd2bb9f5faf68f8c68ffcb637d4d705
valeriia zvezdina <valeriia.zvezdina@gmail.com>
2024-01-29 20:56:01 +0100

    feat: partially finish Worker Thread and Child Processes subtasks

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,28 @@
+import { Worker, isMainThread, parentPort } from 'node:worker_threads';
+import * as OS from 'node:os';
+import workerFunctions from './worker.js';
+const { nthFibonacci, sendResult } = workerFunctions;
+
 const performCalculations = async () => {
-    // Write your code here
+    if (isMainThread) {
+        const numCores = OS.cpus().length;
+        const workerPromises = [];
+        for (let i = 0; i < numCores; i++) {
+            const worker = new Worker('./worker.js', { workerData: 10 + i });
+            const workerPromise = new Promise((resolve) => {
+                worker.on('message', (message) => {
+                    resolve(message);
+            });
+          });
+            workerPromises.push(workerPromise);
+        }
+        const results = await Promise.all(workerPromises);
+        console.log(results);
+    } else {
+        const n = parentPort.workerData;
+        const result = nthFibonacci(n);
+        sendResult(result);
+    }
 };
 
 await performCalculations();
