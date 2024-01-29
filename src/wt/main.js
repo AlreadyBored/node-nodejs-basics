4b6f6dbb987d497d26b8c93b0d383fabb3988621
Lapitskiy <hiimisha@mail.ru>
2024-01-30 02:12:15 +0300

    feat: finish "workers" task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,38 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
 const performCalculations = async () => {
-    // Write your code here
+  const numCPUs = os.cpus().length;
+  let promises = [];
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const workerFile = join(__dirname, "worker.js");
+
+  for (let i = 0; i < numCPUs; i++) {
+    const worker = new Worker(workerFile, { workerData: { n: 10 + i } });
+
+    const promise = new Promise((resolve, reject) => {
+      worker.on("message", (result) => {
+        resolve({
+          status: "resolved",
+          data: result,
+        });
+      });
+
+      worker.on("error", () => {
+        reject({
+          status: "error",
+          data: null,
+        });
+      });
+    });
+
+    promises.push(promise);
+  }
+
+  const promisesResult = await Promise.allSettled(promises);
+  console.log(promisesResult.map(result => result.value));
 };
 
 await performCalculations();
