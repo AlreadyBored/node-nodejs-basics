ac75a0f82c0922f777c5bec49d3239d90a9754ad
VoitehovichXenia <voitehovichkg@gmail.com>
2024-01-29 17:01:35 +0300

    feat: implement ZIP compress.js task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+// implement function that compresses file fileToCompress.txt to archive.gz using zlib and Streams API
+import { pipeline } from 'node:stream';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGzip } from 'node:zlib';
+
 const compress = async () => {
-    // Write your code here 
+    const gzip = createGzip();
+    const readable = createReadStream('./files/fileToCompress.txt');
+    const writable = createWriteStream('./files/archive.gz');
+    
+    pipeline(
+        readable,
+        gzip,
+        writable,
+        (err) => err && console.log(err)
+    );
 };
 
 await compress();
