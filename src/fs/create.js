ce74755c04d22f24ea33da22e1e0ed4229fbfebc
Svetlana Plinda <sv.plinda@icloud.com>
2024-01-29 22:21:44 +0100

    feat: resolve task create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import fs from "fs";
+import url from "node:url";
+import path from "path";
+
 const create = async () => {
-    // Write your code here 
+  const _fileName = url.fileURLToPath(import.meta.url);
+  const dirname = path.dirname(_fileName);
+  const filePath = path.join(dirname, "files", "fresh.txt");
+
+  try {
+    await fs.promises.writeFile(filePath, "I am fresh and young", {
+      encoding: "utf-8",
+      flag: "wx",
+    });
+  } catch (error) {
+    throw error;
+  }
 };
 
 await create();
