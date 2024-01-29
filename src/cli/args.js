72a44d5fbd4689fdd97e021fa969af3534d849f2
oleg filkin <oleg.filkin.by@gmail.com>
2024-01-29 16:35:04 +0300

    Args.js: implemented

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+    const isVariable = (argument) => argument.indexOf('--') === 0 && argument.length > 2;
+    const getFormattedResult = (entries) => entries.map(([varName, value]) => `${varName} is ${value}`).join(', ');
+    
+    const entries = []
+    
+    process.argv.forEach((argument, index, arr) => isVariable(argument) && entries.push([argument, arr[index + 1]]));
+    
+    console.log(getFormattedResult(entries));
 };
 
 parseArgs();
