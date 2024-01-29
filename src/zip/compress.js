cd1f61a6c49d45e22a7b8814306f799332fa8a02
brasoares <nocsoares385@gmail.com>
2024-01-29 13:34:19 -0300

    compress.js & decompress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
 const compress = async () => {
-    // Write your code here 
+	try {
+		const {
+			createReadStream,
+			createWriteStream
+		} = await import('fs');
+		const zlib = await import('zlib');
+		const pathOf = './files/fileToCompress.txt';
+		const readStream = createReadStream(pathOf);
+		const writeStream = createWriteStream('./files/archive.gz')
+		const gzip = zlib.createGzip();
+
+		readStream.pipe(gzip).pipe(writeStream);
+
+		console.log('File compress operation: success!');
+	} catch (error) {
+		console.error('Error: ', error.message);
+	}
 };
 
 await compress();
+
+/* compress.js - implement function that compresses file fileToCompress.txt
+ * to archive.gz using zlib and Streams API */
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
 const decompress = async () => {
-    // Write your code here 
+	try {
+		const {
+			createReadStream,
+			createWriteStream
+		} = await import('fs');
+		const zlib = await import('zlib');
+		const archivePath = './files/archive.gz';
+		const readStream = createReadStream(archivePath);
+		const writeStream = createWriteStream('./files/fileToCompress.txt');
+		const gunzip = zlib.createGunzip();
+
+		readStream.pipe(gunzip).pipe(writeStream);
+
+		console.log('Operations succeded!');
+	} catch (error) {
+		console.error('Error: ', error.message);
+	}
 };
 
 await decompress();
