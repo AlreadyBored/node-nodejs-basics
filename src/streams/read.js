fc9f32c00cd332528f5f1e4335ae6f9e8520b1ee
Andrew Motko <andrew.motko@gmail.com>
2024-01-29 08:08:11 +0200

    feat: add tasks solutions

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
+import fs from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const input = fs.createReadStream(join(__dirname, "files", "fileToRead.txt"));
+  input.pipe(process.stdout);
 };
 
 await read();
