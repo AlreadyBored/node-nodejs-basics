98ae047244ffe1c6856ea345a4c2139c200efb72
a.karavaychik <a.karavaychik@gmail.com>
2024-01-29 16:53:13 +0300

    feat: imlement hash module

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
-const calculateHash = async () => {
-    // Write your code here 
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import crypto from "crypto";
+
+export const calculateHash = async () => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const subPath = path.join(__dirname, "files");
+  const hashSum = crypto.createHash("sha256");
+  fs.readFile(path.join(subPath, "fileToCalculateHashFor.txt"), (err, data) => {
+    if (err) throw Error(err);
+    hashSum.update(data);
+    const result = hashSum.digest("hex");
+    console.log(result);
+    return result;
+  });
 };
 
-await calculateHash();
+calculateHash();
