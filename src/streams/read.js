18e326101e0739ad8b58d4e0ecdcbb05ba4cd087
Olena Datso <elena.shcheglova@gmail.com>
2024-01-28 20:03:26 -0700

    feat: add streams read func

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,25 @@
+import fs from 'fs';
+
 const read = async () => {
-    // Write your code here 
+
+    function readFileContent(filePath) {
+      const readStream = fs.createReadStream(filePath);
+
+      readStream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+      });
+
+      readStream.on('error', (error) => {
+        console.error(`Error reading the file: ${error.message}`);
+      });
+
+      readStream.on('end', () => {
+        console.log('\nEnd of file.');
+      });
+    }
+
+    readFileContent('./src/streams/files/fileToRead.txt');
+
 };
 
 await read();
