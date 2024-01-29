deb6caeb62a1b631ed9ab63e216d666f4b58e024
anton-glebov <anton.glebov@useinsider.com>
2024-01-30 01:27:41 +0300

    feat: implement rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,18 @@
+import fs from "node:fs";
+import * as path from "node:path";
+import { fileURLToPath } from 'node:url';
+
 const rename = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const basePath = path.join(__dirname, 'files');
+    const oldPath = path.join(basePath, 'wrongFilename.txt');
+    const newPath = path.join(basePath, 'properFilename.md');
+
+    fs.rename(oldPath, newPath, (err) => {
+        if (err) {
+            throw new Error('FS operation failed');
+        }
+    });
 };
 
 await rename();
