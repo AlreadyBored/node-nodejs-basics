07ff6a9e0823b498b3d27d15c4131ee2bd5cb883
valeriia zvezdina <valeriia.zvezdina@gmail.com>
2024-01-29 20:55:37 +0100

    feat: partially finish Worker Thread and Child Processes subtasks

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,22 @@
+import { spawn } from 'node:child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childProcess = spawn('node', ['script.js', ...args], {
+        stdio: ['pipe', 'pipe', 'pipe', 'ipc']
+    });
+
+    process.stdin.pipe(childProcess.stdin);
+    childProcess.stdout.pipe(process.stdout);
+
+    childProcess.on('exit', (code, signal) => {
+        console.log(`Child process exited with code ${code} and signal ${signal}`);
+        process.exit();
+    });
+
+    childProcess.on('error', (err) => {
+        console.log(err.message);
+        process.exit(1);
+    });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([ 'someArgument1', 'someArgument2' ]);
