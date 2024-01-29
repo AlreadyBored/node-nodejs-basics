144ec39874b49da8439355a22729762e0d6cf4e0
max <mcnic@users.noreply.github.com>
2024-01-29 13:08:05 -0800

    feat: implementation 'wt'

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,35 @@
+import { Worker, isMainThread } from 'node:worker_threads';
+import path from 'node:path';
+import os from 'os';
+
 const performCalculations = async () => {
-    // Write your code here
+    if (!isMainThread) return;
+
+    const fileName = path.join(process.cwd(), 'src/wt/worker.js');
+
+    function createWorker(workerData) {
+        return new Promise(function (resolve, reject) {
+            const worker = new Worker(fileName, {
+                workerData,
+            });
+            worker.on('message', (data) => {
+                resolve({ status: 'resolved', data });
+                worker.terminate();
+            });
+            worker.on('error', (msg) => {
+                reject({ status: 'error', data: null });
+                worker.terminate();
+            });
+        });
+    }
+
+    const workerThreads = [];
+
+    for (let i = 0; i < os.cpus().length; i++) {
+        workerThreads.push(createWorker(10 + i));
+    }
+    const results = await Promise.all(workerThreads);
+    console.log(results);
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,20 @@
+import { isMainThread, workerData, parentPort } from 'node:worker_threads';
+
 // n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+const nthFibonacci = (n) =>
+    n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    if (isMainThread) return;
+
+    parentPort.on('message', (message) => {
+        // infility loop
+    });
+
+    // calculate & send result
+    parentPort.postMessage(nthFibonacci(workerData));
 };
 
 sendResult();
+
+export default sendResult;
