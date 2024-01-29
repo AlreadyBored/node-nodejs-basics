ce108b0f04d7fc23442bfc4d806c45f810955960
DinislamFromUfa <isberdindinislam@gmail.com>
2024-01-29 23:50:55 +0500

    feat: add solutions on fs folder

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 const create = async () => {
-    // Write your code here 
+    // Write your code here
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    let content = 'I am fresh and young';
+    fs.access(path.resolve(__dirname, 'files', 'fresh.txt'), (err) => {
+        if (err) {
+            fs.writeFile(path.resolve(__dirname, 'files', 'fresh.txt'), content, (err) => {
+                if (err) {
+                    console.log(err);
+                }
+                    console.log('File was writed on folder files');
+            });
+        } else {
+            console.log('FS operation failed');
+        }
+    });
 };
 
 await create();
