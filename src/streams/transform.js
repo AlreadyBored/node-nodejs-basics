7c0cfaf7fd61dd176bace43ee15a2920dc56bf37
Shuhrat <shuhratillon@gmail.com>
2024-01-29 23:53:34 +0500

    streams/transform.js is implemented

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
+import { Transform, pipeline } from 'stream';
+import { promisify } from 'util';
+
 const transform = async () => {
-    // Write your code here 
+  const pipelineAsync = promisify(pipeline);
+  const reverseTransform = new Transform({
+    transform(chunk, encoding, callback) {
+      const reversedChunk = chunk.toString().split('').reverse().join('');
+      this.push(reversedChunk);
+      callback();
+    },
+  });
+  await pipelineAsync(process.stdin, reverseTransform, process.stdout);
 };
 
 await transform();
