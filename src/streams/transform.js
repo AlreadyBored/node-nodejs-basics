6d91186405f2922d09e1095f09289168dfa268e2
Olena Datso <elena.shcheglova@gmail.com>
2024-01-28 20:15:36 -0700

    feat: add transform func

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,22 @@
+import { Transform }from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+
+  const reverseTransform = new Transform({
+    transform(chunk, encoding, callback) {
+      // Convert chunk to string, reverse it, and pass it to the next handler
+      this.push(chunk.toString().split('').reverse().join(''));
+      callback();
+    },
+  });
+
+  console.log(
+    "Please enter text to reverse. Press 'Ctrl + D' to end the input."
+  );
+
+  process.stdin
+    .pipe(reverseTransform) // Pipe input through the reverse transform
+    .pipe(process.stdout); // Then pipe it to stdout
 };
 
 await transform();
