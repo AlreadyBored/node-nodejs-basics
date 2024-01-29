623ae97c6043b0c9ccdedfca7934a2186cd61f26
ABiryulin <abiryulin@mmtr.ru>
2024-01-29 22:52:34 +0300

    Solved hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
+import { createHash } from 'crypto'
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const FILES_PATH = 'files';
+const CALCULATE_TO_HASH_FILE = 'fileToCalculateHashFor.txt'
+const pathCalculateToHashFile = path.join(__dirname, FILES_PATH, CALCULATE_TO_HASH_FILE)
+
+const calculateFileToHash = (path) => {
+    const hash = createHash('sha256')
+    const readStream = fs.createReadStream(path)
+
+    readStream.pipe(hash);
+
+    readStream.on('end', () => {
+        const fileHash = hash.digest('hex');
+
+        console.log(fileHash);
+    });
+}
+
 const calculateHash = async () => {
-    // Write your code here 
+    calculateFileToHash(pathCalculateToHashFile)
 };
 
 await calculateHash();
