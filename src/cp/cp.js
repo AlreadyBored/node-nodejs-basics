99466309449cf46a761996d9265d99507562b72c
Олег Зайцев <30oleg.zv94@gmail.com>
2024-01-29 22:40:44 +0300

    feat: implement child process task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,15 @@
+//https://github.com/AlreadyBored/nodejs-assignments/blob/main/assignments/nodejs-basics/assignment.md#child-processes-srccp
+
+import { fork } from "node:child_process";
+import { getURLPath } from "../lib.js";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const childProcess = fork(getURLPath("./cp/files/script.js"));
+  childProcess.send(args);
+  childProcess.on("message", (msg) => {
+    process.stdout.write(`Received from child process: ${msg}`);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["arg1, arg2"]);
