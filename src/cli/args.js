7da900b2d892e9b0db57ba73bb90e6f321101a20
ABiryulin <abiryulin@mmtr.ru>
2024-01-29 17:17:42 +0300

    Solved cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,24 @@
+
+const PREFIX = '--'
+const SEPARATOR = ', '
+
+const getPrefixArgs = () => {
+    const result = {}
+
+    process.argv.forEach(arg => {
+        if (arg.startsWith(PREFIX)) {
+            result[arg.slice(2)] = process.argv[i + 1]
+            i++
+        }
+    })
+
+    return result
+}
+
 const parseArgs = () => {
-    // Write your code here 
+    const args = getPrefixArgs()
+
+    console.log(Object.keys(args).map(key => `${key} is ${args[key]}`).join(SEPARATOR))
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,16 @@
+
+const PREFIX = 'RSS_'
+const SEPARATOR = '; '
+
+// const getPrefixEnvs = () => Object.keys(process.env)
+//     .flatMap(key => key.startsWith(PREFIX) ? `${key}:${process.env[key]}` : [])
+//     .join('; ')
+
+const getPrefixEnvs = () => Object.keys(process.env)
+    .filter(key => key.startsWith(PREFIX))
+
 const parseEnv = () => {
-    // Write your code here 
+    console.log(getPrefixEnvs().map((key) => `${key}:${process.env[key]}`).join('; '))
 };
 
 parseEnv();
