bd0cee0acc77d661339951b590a41a687f2eb16a
Maryia Velesiuk <m.velesiuk@andersenlab.com>
2024-01-29 18:00:09 +0200

    feat: add create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,14 @@
+import { appendFile, access, constants } from "node:fs";
+
 const create = async () => {
-    // Write your code here 
+  access("src/fs/files/fresh.txt", constants.F_OK, (err) => {
+    if (!err) {
+      throw new Error("FS operation failed");
+    }
+  });
+  appendFile("src/fs/files/fresh.txt", "I am fresh and young", function (err) {
+    if (err) throw err;
+  });
 };
 
 await create();
