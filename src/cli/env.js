191534d3eb009e8255990ea87bed1e65cceb2026
Kirill Shishov <skireal@mail.ru>
2024-01-29 22:47:22 +0300

    feat: implement parseEnv function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+  const rssVariables = Object.entries(process.env)
+    .filter(([key]) => key.startsWith('RSS_'))
+    .map(([key, value]) => `${key}=${value}`)
+    .join('; ');
+
+  if (rssVariables.length > 0) {
+    console.log(rssVariables);
+  } else {
+    console.log('No RSS environment variables have found');
+  }
 };
 
 parseEnv();
