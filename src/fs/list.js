d1eb85a59b9e81a98d93df62995199b5cbdfc16d
Kirill Shishov <skireal@mail.ru>
2024-01-29 22:32:04 +0300

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,25 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { promises } from 'fs';
+import { checkFileExists } from './utils.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+  const filesDir = path.join(__dirname, 'files');
+
+  try {
+    const existsFilesDir = await checkFileExists(filesDir);
+    if (existsFilesDir) {
+      const fileNames = await promises.readdir(filesDir);
+      console.log(fileNames);
+    } else {
+      throw new Error('FS operation failed');
+    }
+  } catch (err) {
+    throw err;
+  }
 };
 
 await list();
