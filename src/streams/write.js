97ccdfc28e144fd04b052189707f4b9cebfb4b57
Govor Kirill <k.govor@21vek.by>
2024-01-29 21:05:13 +0100

    feat: implement stream tasks

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const FILE_NAME = "fileToWrite.txt";
+const DIR = "files";
+
 const write = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, DIR, FILE_NAME);
+  const stream = fs.createWriteStream(filePath);
+
+  process.stdin.pipe(stream);
 };
 
 await write();
