c3346a7af77f26c0948232f25d11b30f9d95e002
Alexey0795 <alexey0795@mail.ru>
2024-01-29 21:21:13 +0300

    fix: ecs modules

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,24 +1,25 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
+import { createRequire } from 'node:module';
 import { release, version } from 'os';
 import { createServer as createServerHttp } from 'http';
-import './files/c';
+import './files/c.js';
 
 const random = Math.random();
 
 let unknownObject;
-
+const require = createRequire(import.meta.url);
 if (random > 0.5) {
     unknownObject = require('./files/a.json');
 } else {
     unknownObject = require('./files/b.json');
 }
-
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
 console.log(`Path segment separator is "${path.sep}"`);
-
-console.log(`Path to current file is ${__filename}`);
-console.log(`Path to current directory is ${__dirname}`);
+const thisFilePath = fileURLToPath(import.meta.url);
+console.log(`Path to current file is ${thisFilePath}`);
+console.log(`Path to current directory is ${path.dirname(thisFilePath)}`);
 
 const myServer = createServerHttp((_, res) => {
     res.end('Request accepted');
