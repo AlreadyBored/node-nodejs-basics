d4ff3234254e015057419b831156ba065fd3f023
Olena Datso <elena.shcheglova@gmail.com>
2024-01-28 20:16:19 -0700

    refactor: remove comments

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -4,7 +4,6 @@ const transform = async () => {
 
   const reverseTransform = new Transform({
     transform(chunk, encoding, callback) {
-      // Convert chunk to string, reverse it, and pass it to the next handler
       this.push(chunk.toString().split('').reverse().join(''));
       callback();
     },
@@ -15,8 +14,8 @@ const transform = async () => {
   );
 
   process.stdin
-    .pipe(reverseTransform) // Pipe input through the reverse transform
-    .pipe(process.stdout); // Then pipe it to stdout
+    .pipe(reverseTransform)
+    .pipe(process.stdout);
 };
 
 await transform();
