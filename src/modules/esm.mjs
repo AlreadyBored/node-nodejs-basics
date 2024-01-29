c1c67e16fa732f81d2ccfcd5164914274bfdbac3
ABiryulin <abiryulin@mmtr.ru>
2024-01-29 22:31:34 +0300

    Solved module task

---
+++ src/modules/esm.mjs
@@ -0,0 +1,49 @@
+import fsPromises from 'fs/promises';
+import { createServer as createServerHttp } from 'http';
+import { release, version } from 'os';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import './files/c.js';
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
+
+const FILES_PATH = 'files';
+
+const random = Math.random();
+
+let unknownObject;
+
+if (random > 0.5) {
+    unknownObject = JSON.parse(await fsPromises.readFile(
+        path.join(__dirname, FILES_PATH, 'a.json'), { encoding: 'utf-8' }
+    ));
+} else {
+    unknownObject = JSON.parse(await fsPromises.readFile(
+        path.join(__dirname, FILES_PATH, 'b.json'), { encoding: 'utf-8' }
+    ));
+}
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${path.sep}"`);
+
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
+
+const myServer = createServerHttp((_, res) => {
+    res.end('Request accepted');
+});
+
+const PORT = 3000;
+
+console.log(unknownObject);
+
+myServer.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+    console.log('To terminate it, use Ctrl+C combination');
+});
+
+export { unknownObject, myServer };
+
