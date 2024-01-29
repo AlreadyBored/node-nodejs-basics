c8727106f0c94c4628ac05cbec2bfc438f3e52e1
Govor Kirill <k.govor@21vek.by>
2024-01-29 21:02:44 +0100

    feat: implement cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  let output = [];
+
+  for (let i = 0; i < args.length; i += 2) {
+    const key = args[i].startsWith("--") ? args[i].slice(2) : args[i];
+    const value = args[i + 1];
+
+    output.push(`${key} is ${value}`);
+  }
+
+  console.log(output.join(", "));
 };
 
 parseArgs();
