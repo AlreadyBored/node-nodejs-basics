8250852da4c39e3adecc80443d43d37ea3669643
Denis Slavenko <denis-slavenko1999@mail.ru>
2024-01-29 04:17:06 +0300

    feat(hash): implemented logic in hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,31 @@
+import cr from 'node:crypto';
+import fs from 'node:fs';
+import url from 'node:url';
+import path from 'node:path';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+    let readStream = fs.createReadStream(filePath);
+    let hash = cr.createHash('sha256');
+    hash.setEncoding('hex');
+
+    readStream.on('data', (chunk) => {
+        hash.update(chunk);
+    })
+
+    readStream.on('error', (err) => {
+        throw err;
+    })
+
+    readStream.on('end', () => {
+        let finalHash = hash.digest('hex');
+
+        console.log(finalHash);
+    })
 };
 
 await calculateHash();
