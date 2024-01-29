46c8c9a1751e40421c6cc2e327ae2716ed8535cb
max <mcnic@users.noreply.github.com>
2024-01-29 13:03:00 -0800

    feat: implementation 'fs:create'

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import { existsSync } from 'node:fs';
+import { writeFile } from 'node:fs/promises';
+import path from 'node:path';
+
 const create = async () => {
-    // Write your code here 
+    const filePath = path.join(process.cwd(), 'src/fs/files/fresh.txt');
+    const text = 'I am fresh and young';
+
+    if (existsSync(filePath)) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        await writeFile(filePath, text);
+    } catch (err) {
+        console.error(err);
+    }
 };
 
 await create();
