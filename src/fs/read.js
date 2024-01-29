4820a292c6de63a7326a2d67b625f9e0cd180b7c
max <mcnic@users.noreply.github.com>
2024-01-29 13:04:34 -0800

    feat: implementation 'fs:read'

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,22 @@
+import { existsSync } from 'node:fs';
+import { open } from 'node:fs/promises';
+import path from 'node:path';
+
 const read = async () => {
-    // Write your code here 
+    const fileName = path.join(process.cwd(), 'src/fs/files/fileToRead.txt');
+
+    if (!existsSync(fileName)) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        const file = await open(fileName);
+        for await (const line of file.readLines()) {
+            console.log(line);
+        }
+    } catch (err) {
+        console.error(err);
+    }
 };
 
 await read();
