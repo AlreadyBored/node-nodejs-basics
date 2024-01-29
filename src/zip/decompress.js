47c44a21203629cbb0bb55522360fd060b2a1e9e
Govor Kirill <k.govor@21vek.by>
2024-01-29 21:05:43 +0100

    feat: implement zip task

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,27 @@
+import fs from 'fs/promises';
+import { createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from 'zlib';
+import { pipeline } from 'stream/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const FILE_NAME = 'fileToCompress.txt';
+const DIR = "files";
+const ARCHIVE_NAME = 'archive.gz';
+
 const decompress = async () => {
-    // Write your code here 
+    const inputFilePath = path.join(__dirname,ARCHIVE_NAME);
+    const outputFilePath = path.join(__dirname, DIR, FILE_NAME);
+
+    await fs.access(inputFilePath);
+
+    const readStream = createReadStream(inputFilePath);
+    const writeStream = createWriteStream(outputFilePath);
+    const gunZip = createGunzip();
+
+    await pipeline(readStream, gunZip, writeStream);
 };
 
 await decompress();
