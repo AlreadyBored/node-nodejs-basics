c9f4a3a1be41158f5cd0b72af7724af933a8bf32
alex.pranevich <freennnn@gmail.com>
2024-01-29 21:48:56 +0100

    feat: add cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,25 @@
+import { fork, spawn } from 'child_process'
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { pipeline } from 'stream';
+
+const dirName = path.dirname(fileURLToPath(import.meta.url));
+const childPath = path.join(dirName, "files", "script.js");
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  let child = fork(childPath, args);
+
+  process.stdin.on('message', (data) => child.send(data));
+  child.on('message', (data) => console.log(`Received from child process to main stdout: ${data}`));
+
+  // If we used spawn or {options.stdio : 'pipe'/'inherit'} we would have stoud/stdin in child process
+  // process.stdin.pipe(child.stdin);
+  // child.stdout.pipe(process.stdout);
+  // child.stdout.on('data', (data) => {
+  //   console.log(`from child to master stdout:\n ${data.toString()}`);
+  // });
+
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['one', 'two']);
