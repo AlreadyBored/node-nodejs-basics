ce345034349c0a13074bccf4c440a1acf689e23d
max <mcnic@users.noreply.github.com>
2024-01-29 13:04:21 -0800

    feat: implementation 'fs:copy'

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,25 @@
+import { existsSync } from 'node:fs';
+import { mkdir, readdir, copyFile } from 'node:fs/promises';
+import path from 'node:path';
+
 const copy = async () => {
-    // Write your code here 
+    const srcDir = path.join(process.cwd(), 'src/fs/files');
+    const destDir = path.join(process.cwd(), 'src/fs/files_copy');
+
+    if (existsSync(destDir) || !existsSync(srcDir)) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        await mkdir(destDir);
+
+        const files = await readdir(srcDir);
+        for (const file of files) {
+            await copyFile(path.join(srcDir, file), path.join(destDir, file));
+        }
+    } catch (err) {
+        console.error(err);
+    }
 };
 
 await copy();
