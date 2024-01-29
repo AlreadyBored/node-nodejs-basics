dfe6be99b7fab5b1d39606817ce723336d94cd87
Lapitskiy <hiimisha@mail.ru>
2024-01-30 02:44:41 +0300

    feat: finish "cp" task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,16 @@
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { spawn } from 'child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const script = join(__dirname, "files", "script.js");
+
+  const child = spawn('node', [script, ...args], { stdio: ['pipe', 'pipe', 'pipe', 'ipc'] });
+
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["someArgument1", "someArgument1231232"]);
