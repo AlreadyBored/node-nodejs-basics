1ff8ee265dbb90e3955138e01cd8053d6141906c
a.karavaychik <a.karavaychik@gmail.com>
2024-01-29 18:31:38 +0300

    feat: implement cp module

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,23 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import path from "path";
+import { fileURLToPath } from "url";
+import cp from "child_process";
+
+export const spawnChildProcess = async (args) => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const subPath = path.join(__dirname, "files");
+  const childProcess = cp.spawn("node", [
+    path.join(subPath, "script.js"),
+    ...args,
+  ]);
+
+  process.stdin.pipe(childProcess.stdin);
+
+  childProcess.stdout.setEncoding("utf8");
+
+  childProcess.stdout.on("data", (data) => {
+    process.stdout.write(data);
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["Hello", "world"]);
