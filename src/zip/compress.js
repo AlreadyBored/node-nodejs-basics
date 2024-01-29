f1c028a096e496e4e4853a68f3ff4ae91ddf2a4d
Shuhrat <shuhratillon@gmail.com>
2024-01-29 23:57:27 +0500

    zip/compress.js is implemented

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+import fs from 'fs';
+import zlib from 'zlib';
+import { pipeline } from 'stream';
+import { promisify } from 'util';
+
+
 const compress = async () => {
-    // Write your code here 
+  const pipelineAsync = promisify(pipeline);
+  const inputFilePath = 'src/zip/files/fileToCompress.txt';
+  const outputFilePath = 'src/zip/files/archive.gz';
+
+  const readStream = fs.createReadStream(inputFilePath);
+  const gzipStream = zlib.createGzip();
+  const writeStream = fs.createWriteStream(outputFilePath);
+
+  await pipelineAsync(readStream, gzipStream, writeStream);
 };
 
 await compress();
