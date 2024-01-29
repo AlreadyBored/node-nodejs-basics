43c603dd9a272ef5a21eb2f5ee5ed6083912f35a
Govor Kirill <k.govor@21vek.by>
2024-01-29 21:06:08 +0100

    feat: implement cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,29 @@
+import { spawn } from 'child_process';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const DIR = "files";
+const FILE_NAME = "script.js";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const scriptPath = path.join(__dirname, DIR, FILE_NAME);
+  const child = spawn("node", [scriptPath, ...args], {
+    stdio: ["pipe", "pipe", "inherit", "ipc"],
+  });
+
+  process.stdin.pipe(child.stdin);
+
+  child.stdout.pipe(process.stdout);
+
+  child.on("error", (error) => {
+    console.error(`Error in child process: ${error.message}`);
+  });
+
+  child.on("exit", (code) => {
+    console.log(`Child process exited with code ${code}`);
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([1, 2, 3, 4, 5]);
