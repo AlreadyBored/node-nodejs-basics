7c8310c8200552d19965f2848a4c222e7a53e332
VoitehovichXenia <voitehovichkg@gmail.com>
2024-01-29 16:19:41 +0300

    feat: implement streams write.js task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,10 @@
+// implement function that writes process.stdin data into file fileToWrite.txt content using Writable Stream
+import { createWriteStream } from 'node:fs'
+
 const write = async () => {
-    // Write your code here 
+    const file = createWriteStream('./files/fileToWrite.txt');
+    process.stdin.pipe(file);
+    process.stdin.resume();
 };
 
 await write();
