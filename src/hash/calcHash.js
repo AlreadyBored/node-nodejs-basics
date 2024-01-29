bdfa2de91ea11359a347360ad1f089126c433e9b
Shuhrat <shuhratillon@gmail.com>
2024-01-29 23:34:10 +0500

    calculateHash.js is implemented

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import { createReadStream } from "fs";
+import { createHash } from "crypto";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash("sha256");
+  const filePath = "src/hash/files/fileToCalculateHashFor.txt";
+  const fileStream = createReadStream(filePath);
+  return new Promise((resolve, reject) => {
+    fileStream.on('data', (chunk) => {
+      hash.update(chunk);
+    });
+    fileStream.on('end', () => {
+      const hexHash = hash.digest('hex');
+      console.log(hexHash);
+      resolve(hexHash);
+    });
+    fileStream.on('error', (error) => {
+      reject(error);
+    });
+  });
 };
 
 await calculateHash();
