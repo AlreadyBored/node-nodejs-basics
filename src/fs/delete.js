beb411b45ef7120aa7eade56c54cb1842d7393db
Maryia Velesiuk <m.velesiuk@andersenlab.com>
2024-01-29 18:40:54 +0200

    feat: add remove function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,9 @@
+import { unlink } from "node:fs";
+
 const remove = async () => {
-    // Write your code here 
+  unlink("src/fs/files/fileToRemove.txt", (err) => {
+    if (err) throw new Error("FS operation failed");
+  });
 };
 
 await remove();
