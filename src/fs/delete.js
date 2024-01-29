9fcea4f4314a3d8a617b44df114f7921f156985c
VoitehovichXenia <voitehovichkg@gmail.com>
2024-01-29 12:31:14 +0300

    feat: implement FS delete.js task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,12 @@
+// implement function that deletes file fileToRemove.txt (if there's no file fileToRemove.txt Error with message FS operation failed must be thrown)
+import { rm } from 'node:fs/promises'
+
 const remove = async () => {
-    // Write your code here 
+    try {
+        await rm('./files/fileToRemove.txt')
+    } catch {
+        throw new Error('FS operation failed')
+    }
 };
 
 await remove();
