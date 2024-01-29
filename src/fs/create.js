59f690dc205975b286f5e5ba6410175ea9942209
Bekhzod Turdialiev <bekhzodturdialiev@gmail.com>
2024-01-29 15:33:28 +0500

    feat: add create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import { existsSync, writeFile } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import { join, dirname } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const file = join(__dirname, `files/fresh.txt`)
+
 const create = async () => {
-    // Write your code here 
+    if(existsSync(file)) {
+        throw new Error('FS operation failed')
+    }
+
+    writeFile(file, 'I am fresh and young', () => {})
 };
 
 await create();
