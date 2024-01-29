d4e62d65a158d837b902c3f021cacff645615164
khusanov-95 <fa.khusano@gmail.com>
2024-01-29 23:31:39 +0500

    add worker

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,51 @@
+import { Worker, isMainThread, workerData, parentPort } from "worker_threads";
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import os from 'os';
+
 const performCalculations = async () => {
-    // Write your code here
-};
+  const numberOfCores = os.cpus().length;
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  let workerDataNumber = 10;
+
+  return new Promise((resolve, reject) => {
+    const results = [];
+    const promises = [];
+
+
+    for (let i = 0; i < numberOfCores; i++) {
+        promises[i] = new Promise((workerResolve, workerReject) => {
+          const worker = new Worker(__dirname + '/worker.js', {
+            workerData: workerDataNumber++,
+          });
+  
+          worker.on('message', (data) => {
+            console.log(data)
+            results[i] = {status: 'resolved', data };
+            workerResolve();
+          });
+  
+          worker.on('error', () => {
+            results[i] = {status: 'error', data: null };
+            workerReject();
+          });
+  
+        //   worker.on('exit', (code) => {
+        //     if (code !== 0) {
+        //       workerReject(new Error(`Worker stopped with exit code ${code}`));
+        //     }
+        //   });
+        });
+      }
+
+      Promise.allSettled(promises).then(() => {
+        console.log(results)
+      })
+  });
+}
 
 await performCalculations();
+
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,22 @@
+import { workerData, parentPort } from "worker_threads";
+
 // n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+const nthFibonacci = (n) =>  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+
+
+// To check order and error
+// const nthFibonacci = (n) =>  {
+//     if(n === 15) {
+//         throw new Error;
+//     }
+//     return n;
+// };
 
 const sendResult = () => {
     // This function sends result of nthFibonacci computations to main thread
+    const nthFib = nthFibonacci(workerData);
+    parentPort.postMessage(nthFib);
+  
 };
 
 sendResult();
