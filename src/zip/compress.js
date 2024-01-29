47b241323ca022da09a495f222baf6ac3018d0bb
max <mcnic@users.noreply.github.com>
2024-01-29 13:06:37 -0800

    feat: implementation 'zip:compress'

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import { promisify } from 'node:util';
+import path from 'node:path';
+
 const compress = async () => {
-    // Write your code here 
+    const rootPath = path.join(process.cwd(), 'src/zip/files');
+    const input = path.join(rootPath, 'fileToCompress.txt');
+    const output = path.join(rootPath, 'archive.gz');
+
+    const gzip = createGzip();
+    const source = createReadStream(input);
+    const destination = createWriteStream(output);
+    const pipe = promisify(pipeline);
+
+    await pipe(source, gzip, destination);
 };
 
 await compress();
