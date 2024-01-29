47109cf0a38300d681aa70986c63d288b0f139bc
Lapitskiy <hiimisha@mail.ru>
2024-01-29 23:37:57 +0300

    feat: finish "write" task in "streams" module

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,10 @@
+import fs from 'fs';
+import { stdin } from 'process';
+
 const write = async () => {
-    // Write your code here 
+  const stream = fs.createWriteStream('src/streams/files/fileToWrite.txt');
+
+  stdin.pipe(stream);
 };
 
 await write();
