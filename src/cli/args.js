0b84506ae9ab42be6959e87ca229330ccf840f6c
Denis Slavenko <denis-slavenko1999@mail.ru>
2024-01-29 03:14:27 +0300

    feat(cli): implemented logic in cli functions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,20 @@
+import pr from 'node:process';
+
+const cliArgs = pr.argv.slice(2);
+
 const parseArgs = () => {
-    // Write your code here 
+    console.log(cliArgs);
+
+    let arrToDisplay = [];
+
+    for (let i = 0; i < cliArgs.length; i += 2) {
+        let prop = `${cliArgs[i].substring(2)} is ${cliArgs[i + 1]}`;
+        arrToDisplay.push(prop);
+    }
+
+    arrToDisplay.join(', ');
+
+    console.log(arrToDisplay);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,19 @@
+import pr from 'node:process';
+
+const envVariables = pr.env;
+const prefixToCheck = "RSS_";
+
 const parseEnv = () => {
-    // Write your code here 
+    let varsToPrint = [];
+
+    for (let key in envVariables) {
+        if (key.startsWith(prefixToCheck)) {
+            varsToPrint.push(`${key}=${envVariables[key]}`)
+        }
+    }
+
+    varsToPrint = varsToPrint.join('; ');
+    console.log(varsToPrint);
 };
 
 parseEnv();
