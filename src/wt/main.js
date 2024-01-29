fc3e9b0b687ad6d264f05208254e85c7910c7400
Volha Papkouskaya <o.papkovskaya@gmail.com>
2024-01-29 22:31:56 +0500

    feat: wt solution

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,41 @@
+import os from 'os';
+import { Worker } from 'worker_threads';
+import path from 'path';
+
+const numberOfWorkers = os.cpus().length;
+const fullPath = path.resolve('src/wt/worker.js');
+
+function createPromise(workerData) {
+    return new Promise((resolve, reject) => {
+        const worker = new Worker(fullPath, { workerData });
+        worker.on('message', resolve);
+        worker.on('error', reject);
+    });
+}
+
 const performCalculations = async () => {
-    // Write your code here
+    const array = [];
+
+    for (let i = 0; i < numberOfWorkers; i++) {
+        array.push(createPromise(10 + i));
+    }
+
+    await Promise.allSettled(array).then((results) => {
+        const consoleResponse = results.map((result) => {
+            if (result.status === 'fulfilled') {
+                return {
+                    status: 'resolved',
+                    data: result.value
+                };
+            } else if (result.status === 'rejected') {
+                return {
+                    status: 'error',
+                    data: null
+                };
+            }
+        });
+        console.log(consoleResponse);
+    });
 };
 
 await performCalculations();
