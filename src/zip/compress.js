451de7f62345f7637661b2d70edc6c599fd554a4
ABiryulin <abiryulin@mmtr.ru>
2024-01-29 22:21:54 +0300

    Solved zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,25 @@
+import zlib from 'zlib';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const FILES_PATH = 'files';
+const COMPRESS_FILE = 'fileToCompress.txt';
+const ARCHIVE_FILE = 'archive.gz'
+const compressFilePath = path.join(__dirname, FILES_PATH, COMPRESS_FILE)
+const archiveFilePath = path.join(__dirname, FILES_PATH, ARCHIVE_FILE)
+
+const compressFile = (inputPath, outputPath) => {
+    const readStream = fs.createReadStream(inputPath)
+    const gzipStream = zlib.createGzip()
+    const writeStream = fs.createWriteStream(outputPath)
+
+    readStream.pipe(gzipStream).pipe(writeStream)
+}
+
 const compress = async () => {
-    // Write your code here 
+    compressFile(compressFilePath, archiveFilePath)
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import zlib from 'zlib';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const FILES_PATH = 'files';
+const COMPRESS_FILE = 'fileToCompress.txt';
+const ARCHIVE_FILE = 'archive.gz'
+const compressFilePath = path.join(__dirname, FILES_PATH, COMPRESS_FILE)
+const archiveFilePath = path.join(__dirname, FILES_PATH, ARCHIVE_FILE)
+
+const compressFile = (inputPath, outputPath) => {
+    const readStream = fs.createReadStream(inputPath)
+    const gunzipStream = zlib.createGunzip()
+    const writeStream = fs.createWriteStream(outputPath)
+
+    readStream.pipe(gunzipStream).pipe(writeStream)
+}
+
 const decompress = async () => {
-    // Write your code here 
+    compressFile(archiveFilePath, compressFilePath)
 };
 
 await decompress();
