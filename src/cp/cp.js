25bc670e662ee4556424775ec7fe34269a98dcf3
max <mcnic@users.noreply.github.com>
2024-01-29 13:08:18 -0800

    feat: implementation 'cp'

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,21 @@
+import { spawn } from 'node:child_process';
+import path from 'node:path';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const fileName = path.join(process.cwd(), 'src/cp/files/script.js');
+
+    const child = spawn(process.argv[0], [fileName]);
+
+    process.stdin.on('data', (data) => {
+        child.stdin.write(data);
+    });
+
+    child.stdout.on('data', (chunk) => {
+        process.stdout.write(
+            `Received from child process: ${chunk.toString()}\n`
+        );
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(/* [someArgument1, someArgument2, ...] */);
