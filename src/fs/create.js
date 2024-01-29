35d2c48f91b1addf67aa12456669c8746ec625f5
Maksym <tarasovcad@gmail.com>
2024-01-28 22:28:20 -0600

    feat: add create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,16 @@
+import fs from 'fs';
 const create = async () => {
     // Write your code here 
+    const content = 'I am fresh and young';
+    const dirPath = './files';
+    const filePath = `${dirPath}/fresh.txt`;
+    if (fs.existsSync(filePath)) {
+        throw new Error('FS operation failed');
+    }
+    if (!fs.existsSync(dirPath)) {
+        fs.mkdirSync(dirPath);
+    }
+    fs.writeFileSync(filePath, content);
 };
 
 await create();
