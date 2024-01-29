f00afb76eb0f2e402fac6daafdb15b92a6e5c3a9
anton-glebov <anton.glebov@useinsider.com>
2024-01-30 01:30:00 +0300

    feat: implement args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,19 @@
+import { argv } from 'node:process'
+
 const parseArgs = () => {
-    // Write your code here 
+    const argsArr = argv.slice(2);
+    const result = [];
+
+    for (let i = 0; i < argsArr.length; i += 2) {
+        const key = argsArr[i];
+        if (key.startsWith('--')) {
+            result.push(`${key.slice(2)} is ${argsArr[i + 1]}`);
+        } else {
+            result.push(`${key} is ${argsArr[i + 1]}`);
+        }
+    }
+
+    console.log(result.join(', '));
 };
 
 parseArgs();
