5f558d2863b5423ea1b547cdf3325e32c6e4f801
prostonau <prostonau@mail.ru>
2024-01-30 00:11:18 +0300

    feat: cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,21 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { spawn } from 'child_process';
+import process from 'node:process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const pathToFile = join(__dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const allArgs = [pathToFile].concat(args);
+    //console.log('allArgs = ', allArgs);
+    const childProcess = spawn('node', [pathToFile, ...args], {stdio: ["pipe", "pipe", "pipe", "ipc"]});
+
+    process.stdin.pipe(childProcess.stdin);
+
+    childProcess.stdout.pipe(process.stdout);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['--some-arg value1', '--other 1337', '--arg2 42']);
