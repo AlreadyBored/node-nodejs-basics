29925a94e3132c83f92140926e46116278ac1934
Dawit Hagos <Dawith305@gmail.com>
2024-01-29 16:04:02 +0300

    feat: implement create file method

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import fs from "fs";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+
 const create = async () => {
-    // Write your code here 
+  const basePath = dirname(fileURLToPath(import.meta.url));
+  const createFilePath = path.join(basePath, "/files/fresh.txt");
+
+
+  const fileContent = "I am fresh and young";
+  const errorMessage = "FS operation failed";
+
+  try {
+    await fs.promises.writeFile(createFilePath, fileContent, { flag: "wx" });
+  } catch (e) {
+    throw new Error(errorMessage);
+  }
 };
 
 await create();
