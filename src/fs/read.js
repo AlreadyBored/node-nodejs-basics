a39bc7c0199f0a5ab531493b3ca86994a6e69356
Maryia Velesiuk <m.velesiuk@andersenlab.com>
2024-01-29 18:54:35 +0200

    feat: add read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,10 @@
+import { readFile } from "node:fs";
+
 const read = async () => {
-    // Write your code here 
+  readFile("src/fs/files/fileToRead.txt", "utf8", (err, data) => {
+    if (err) throw new Error("FS operation failed");
+    console.log(data);
+  });
 };
 
 await read();
