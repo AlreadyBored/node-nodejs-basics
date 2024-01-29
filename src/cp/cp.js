2455575358dfc9802d99dc1e542fd05deed9a7a0
SmellyFinger <kooolyan123@mail.ru>
2024-01-29 16:30:41 +0300

    feat: implement child process task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,25 @@
+import { spawn } from 'node:child_process';
+import { fileURLToPath } from 'node:url';
+import path from 'path';
+
+process.stdin.setEncoding('utf-8');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const childPath = path.join(__dirname, 'files', 'script.js');
+  const child = spawn('node', [childPath, ...args], {
+    stdio: ['inherit', 'inherit', 'inherit', 'ipc'],
+  });
+
+  process.stdin.on('data', (data) => {
+    child.send(data);
+  });
+
+  child.on('message', (msg) => {
+    if (msg.includes('CLOSE')) process.exit(0);
+    console.log('Received from child process: ', msg);
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([1, 2]);
--- src/cp/files/script.js
+++ src/cp/files/script.js
@@ -4,9 +4,13 @@ console.log(`Total number of arguments is ${args.length}`);
 console.log(`Arguments: ${JSON.stringify(args)}`);
 
 const echoInput = (chunk) => {
-    const chunkStringified = chunk.toString();
-    if (chunkStringified.includes('CLOSE')) process.exit(0);
-    process.stdout.write(`Received from master process: ${chunk.toString()}\n`)
+  const chunkStringified = chunk.toString();
+  if (chunkStringified.includes('CLOSE')) {
+    process.send('CLOSE');
+    process.exit(0);
+  }
+  process.stdout.write(`Received from master process: ${chunk.toString()}\n`);
+  process.send('Msg from child process');
 };
 
-process.stdin.on('data', echoInput);
+process.on('message', echoInput);
