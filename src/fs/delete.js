537478d69ea3f35f3ac79605843aa16c8c83f81f
anton-glebov <anton.glebov@useinsider.com>
2024-01-30 01:28:12 +0300

    feat: implement delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import { unlink } from "node:fs";
+import * as path from "node:path";
+import { fileURLToPath } from 'node:url';
+
 const remove = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const deletedFileName = 'fileToRemove.txt';
+    const deletedFile = path.join(__dirname, 'files', deletedFileName);
+
+    unlink(deletedFile, (err) => {
+        if (err) {
+            throw new Error('FS operation failed');
+        }
+    });
 };
 
 await remove();
