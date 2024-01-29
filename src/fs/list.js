4ed87a24280e3165f2ad3521aac079366384148f
Volha Chamlai <volha.chamlai@gmail.com>
2024-01-29 19:41:02 +0100

    01: add list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import fs from "fs/promises";
+import path from "path";
+import { FILES_PATH } from "./consts/index.js";
+import { checkIfExists, throwFSError } from "./utils/index.js";
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const isFolderExists = await checkIfExists(FILES_PATH);
+
+    if (!isFolderExists) {
+      throwFSError();
+    }
+
+    const filesNames = await fs.readdir(FILES_PATH);
+    console.log(filesNames);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 await list();
