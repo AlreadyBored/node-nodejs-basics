a179060233fa95bf7ee3dd4a60a4010473974e2f
Olena Datso <elena.shcheglova@gmail.com>
2024-01-28 19:32:42 -0700

    feat: add rewrite to ECMA

---
+++ src/modules/esm.mjs
@@ -0,0 +1,38 @@
+import path from 'path';
+import os from 'os';
+import http from 'http';
+
+import a from './files/a.json' assert { type: 'json' };
+import b from './files/b.json' assert { type: 'json' };
+
+const random = Math.random();
+
+let unknownObject;
+
+if (random > 0.5) {
+  unknownObject = a;
+} else {
+  unknownObject = b;
+}
+
+console.log(`Release ${os.release()}`);
+console.log(`Version ${os.version()}`);
+console.log(`Path segment separator is "${path.sep}"`);
+
+console.log(`Path to current file is ${import.meta.url}`);
+console.log(`Path to current directory is ${path.dirname(import.meta.url)}`);
+
+const myServer = http.createServer((_, res) => {
+  res.end('Request accepted');
+});
+
+const PORT = 3000;
+
+console.log(unknownObject);
+
+myServer.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+  console.log('To terminate it, use Ctrl+C combination');
+});
+
+export { unknownObject, myServer };
