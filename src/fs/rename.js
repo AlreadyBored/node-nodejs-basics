e882d0e022db1a174d423dd5c22f3431050db1d7
zhuravlevanatalia <zhuravleva.977@yandex.by>
2024-01-29 22:49:49 +0100

    init: start node-js task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,40 @@
-const rename = async () => {
-    // Write your code here 
-};
+import { promises as fsPromises } from 'fs';
+import { fileURLToPath } from 'url';
+import { join, dirname, extname } from 'path';
 
-await rename();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const rename = async () => {
+    const sourceFolderPath = join(__dirname, './files');
+    const sourceFileName = 'wrongFilename.txt';
+    const destinationFileName = 'properFilename.md';
+
+    const sourceFilePath = join(sourceFolderPath, sourceFileName);
+    const destinationFilePath = join(__dirname, destinationFileName);
+
+    try {
+        await fsPromises.access(sourceFilePath);
+
+        await fsPromises.access(destinationFilePath);
+
+        throw new Error('FS operation failed: Destination file already exists.');
+    } catch (error) {
+        if (error.code !== 'ENOENT') {
+            throw error;
+        }
+    }
+
+    const fileContent = await fsPromises.readFile(sourceFilePath);
+
+    await fsPromises.writeFile(destinationFilePath, fileContent);
+
+    await fsPromises.unlink(sourceFilePath);
+
+    console.log('File renamed successfully.');
+}
+
+rename().catch((error) => {
+    console.error(error.message);
+    process.exit(1);
+});
