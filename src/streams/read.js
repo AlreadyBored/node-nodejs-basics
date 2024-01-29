93fa0f9aacb511b7ab245d04fafc37b81ec66834
Shuhrat <shuhratillon@gmail.com>
2024-01-29 23:40:37 +0500

    streams/read.js is implemented

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import { createReadStream } from "fs";
+
 const read = async () => {
-    // Write your code here 
+  const filePath = "src/streams/files/fileToRead.txt";
+  const fileStream = createReadStream(filePath, { encoding: 'utf8' });
+  fileStream.pipe(process.stdout);
+  return new Promise((resolve, reject) => {
+    fileStream.on('error', (error) => {
+      reject(error);
+    });
+    fileStream.on('end', () => {
+      resolve();
+    });
+  });
 };
 
 await read();
