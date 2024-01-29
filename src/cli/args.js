211b6e9ce272cecd1bd39a889a7b1c06b909b8c9
DinislamFromUfa <isberdindinislam@gmail.com>
2024-01-29 23:52:52 +0500

    feat: add solutions on cli folder

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
+import process from 'process'
 const parseArgs = () => {
-    // Write your code here 
+    // Write your code here
+    const arg = process.argv;
+    let count = 0;
+    for (let i = 0; i < arg.length; i +=1) {
+        if (arg[i].startsWith('--')) {
+            count += 1;
+            if (count > 1) {
+                console.log('prop' + count + 'Name is ', arg[i]);
+            } else {
+                console.log('propName is ', arg[i]);
+            }
+        }
+    }
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
+import process from 'process'
 const parseEnv = () => {
-    // Write your code here 
+    // Write your code here
+    const result = [];
+    for (const i in process.env) {
+        if (i.startsWith('RSS_')) {
+            result[i] += process.env[i];
+        }
+    }
+    console.log(result);
 };
 
 parseEnv();
