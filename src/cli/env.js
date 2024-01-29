cb5d98a37840c50e1e6a56ce04135b3e74c86539
Shuhrat <shuhratillon@gmail.com>
2024-01-29 23:11:38 +0500

    env.js implemented

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envVariables = process.env;
+  const rssVariables = Object.entries(envVariables)
+    .filter(([key]) => key.startsWith('RSS_'))
+    .map(([key, value]) => `${key}=${value}`);
+  console.log(rssVariables.join('; '));
 };
 
 parseEnv();
