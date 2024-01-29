236e0e15e1c4cec76c3fdb6a27793e97810d4d8a
max <mcnic@users.noreply.github.com>
2024-01-29 13:05:21 -0800

    feat: implementation 'cli:args'

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = {};
+
+    process.argv.forEach((val, key) => {
+        if (val.includes('--')) {
+            args[val.slice(2)] = process.argv[key + 1];
+        }
+    });
+
+    const res = Object.keys(args).map((key) => `${key} is ${args[key]}`);
+
+    console.log(res.join(', '));
 };
 
 parseArgs();
