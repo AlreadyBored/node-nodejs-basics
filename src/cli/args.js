4fbd992df7cdd5df3b577d4c01de0afbb431ef7c
Оля Брикса <oly.briksa@gmail.com>
2024-01-29 05:25:03 +0300

    feat: implement parseArgs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+    const prefix = '--';
+    const argvArr = process.argv;
+    var rssVariables = '';
+
+    argvArr.forEach((argv, index, argvArr) => {
+        if(argv.startsWith(prefix)){
+            rssVariables = rssVariables + argv.replace(prefix, '') + ' is ' + argvArr[index + 1] + ', ';
+        }
+    });
+    console.log(rssVariables);
 };
 
 parseArgs();
