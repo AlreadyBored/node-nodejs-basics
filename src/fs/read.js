ec3524fb9a45469945562650214f387de9224c9d
VoitehovichXenia <voitehovichkg@gmail.com>
2024-01-29 12:51:11 +0300

    feat: implement FS read.js task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,13 @@
+// implement function that prints content of the fileToRead.txt into console (if there's no file fileToRead.txt Error with message FS operation failed must be thrown)
+import { readFile } from 'node:fs/promises';
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const content = await readFile('./files/fileToRead.txt', { encoding: 'utf-8' })
+        console.log(content)
+    } catch {
+        throw new Error('FS operation failed')
+    }
 };
 
 await read();
