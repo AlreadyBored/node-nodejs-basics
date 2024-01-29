1e3df6222fb10c06d7e34595d0f1e5eeb37dcd46
PavelKonstantinovich <pashakonstantinovich@gmail.com>
2024-01-29 23:58:36 +0300

    feat: add solution create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,15 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+
+const __dirname = import.meta.dirname;
+const textFile = path.join(__dirname, 'files', 'fresh.txt');
+
 const create = async () => {
-    // Write your code here 
+    try {
+        await fs.writeFile(textFile, 'I am fresh and young',{ flag: 'ax' })
+    } catch {
+        throw new Error('FS operation failed')
+    }
 };
 
 await create();
