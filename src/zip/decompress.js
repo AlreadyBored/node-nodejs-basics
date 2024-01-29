b864c8513de55a12a0c2cfa6019962de1f88fb42
VoitehovichXenia <voitehovichkg@gmail.com>
2024-01-29 17:39:39 +0300

    feat: implement ZIP decompress.js task

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+//  implement function that decompresses archive.gz back to the fileToCompress.txt with same content as before compression using zlib and Streams API
+import { pipeline } from 'node:stream';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGunzip } from 'node:zlib';
+
 const decompress = async () => {
-    // Write your code here 
+    const gunzip = createGunzip()
+    const readable = createReadStream('./files/archive.gz')
+    const writable = createWriteStream('./files/fileToCompress.txt')
+
+    pipeline(
+        readable,
+        gunzip,
+        writable,
+        (err) => err && console.log(err)
+    )
 };
 
 await decompress();
