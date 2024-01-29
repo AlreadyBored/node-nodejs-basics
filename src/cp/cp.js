d95a1eaaeb17fbb800e57c25e884ff0cc2cc2055
Dima Chemerevsky <dchemerevsky@aras.com>
2024-01-29 22:25:24 +0100

    Child Processes (src/cp). Cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,9 @@
+import { spawn } from 'child_process';
 const spawnChildProcess = async (args) => {
-    // Write your code here
+	const childProcess = spawn('node', ['files/script.js'].concat(args));
+	process.stdin.pipe(childProcess.stdin);
+	childProcess.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['cewc', 'ewfwe']);
