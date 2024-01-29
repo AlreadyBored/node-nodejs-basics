55f13c2756f241ae3df6c5bed697dadd48b26602
Dawit Hagos <Dawith305@gmail.com>
2024-01-29 23:07:15 +0300

    feat: copy folder method

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,28 @@
+import fsPromises from "fs/promises";
+import fs from "fs";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
 const copy = async () => {
-    // Write your code here 
+  const basePath = dirname(fileURLToPath(import.meta.url));
+  const sourceFolderPath = path.join(basePath, "/files");
+  const destinationFolderPath = path.join(basePath, "/files_copy");
+
+  const errorMessage = "FS operation failed";
+
+  if (!fs.existsSync(sourceFolderPath)) {
+    throw new Error(errorMessage);
+  }
+
+  if (fs.existsSync(destinationFolderPath)) {
+    throw new Error(errorMessage);
+  }
+
+  try {
+    await fsPromises.cp(sourceFolderPath, destinationFolderPath, {recursive: true});
+  } catch (error) {
+    throw new Error(errorMessage);
+  }
 };
 
 await copy();
