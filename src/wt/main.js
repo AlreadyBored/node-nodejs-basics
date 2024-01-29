db8e7eba16ffb3ef3f17d9eed76f7306b63267fe
Dmitry Bludchy <dmitrybludchy@gmail.com>
2024-01-29 19:26:46 +0300

    feat: implement performCalculations function

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,39 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { cpus } from 'os';
+import { Worker } from 'worker_threads';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const workerPath = join(__dirname, 'worker.js');
+
 const performCalculations = async () => {
-    // Write your code here
+  const numCores = cpus().length;
+
+  const results = await Promise.all(
+    Array.from({ length: numCores }, async (_, index) => {
+      const additionalNumber = index + 10;
+
+      return new Promise((resolve) => {
+        const worker = new Worker(workerPath, { workerData: additionalNumber });
+
+        worker.once('message', (result) => {
+          resolve({ status: 'resolved', data: result });
+        });
+
+        worker.once('error', (error) => {
+          resolve({ status: 'error', data: null });
+        });
+
+        worker.once('exit', (code) => {
+          if (code !== 0) {
+            resolve({ status: 'error', data: null });
+          }
+        });
+      });
+    })
+  );
+
+  console.log(results);
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -3,6 +3,8 @@ import { parentPort, workerData } from 'worker_threads';
 const nthFibonacci = (n) =>
   n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
+if (Math.random() > 0.5) throw new Error('Ooops')
+
 const sendResult = () => {
   parentPort.postMessage(nthFibonacci(workerData));
 };
