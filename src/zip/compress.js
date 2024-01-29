a673a8789001bd02ff97cf406d67430fc6c1acd1
chyrgal <cobernadoss@gmail.com>
2024-01-29 17:45:17 +0300

    zip done

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,22 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import { createGzip } from "node:zlib";
+
+const txtFile = import.meta.dirname + "/files/fileToCompress.txt";
+const zipFile = import.meta.dirname + "/files/archive.gz";
+
 const compress = async () => {
-    // Write your code here 
+  try {
+    const archiveStream = createGzip();
+    const readStream = createReadStream(txtFile);
+    const writeStream = createWriteStream(zipFile);
+    readStream.pipe(archiveStream).pipe(writeStream);
+
+    writeStream.on("finish", () => {
+      console.log("File compressed successfully");
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+import { createReadStream, createWriteStream, read, write } from "node:fs";
+import { createGunzip } from "node:zlib";
+
+const txtFile = import.meta.dirname + "/files/fileToCompress.txt";
+const zipFile = import.meta.dirname + "/files/archive.gz";
+
 const decompress = async () => {
-    // Write your code here 
+  try {
+    const archiveStream = createGunzip();
+    const readStream = createReadStream(zipFile);
+    const writeStream = createWriteStream(txtFile);
+    readStream.pipe(archiveStream).pipe(writeStream);
+
+    writeStream.on("finish", () => {
+      console.log("File decompressed successfully");
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 await decompress();
