74dd066223035d3be543c4940d2300581a8b3705
EvgenKlo <118301743+EvgenKlo@users.noreply.github.com>
2024-01-30 00:18:11 +0500

    feat: calcHash.js done

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import { createHash } from "node:crypto";
+import fs from "fs/promises";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const hash = createHash("sha256");
+
 const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const result = await fs.readFile(
+      join(__dirname, "files", "fileToCalculateHashFor.txt"),
+      {
+        encoding: "utf8",
+      }
+    );
+    console.log(hash.update(result).digest("hex"));
+  } catch (error) {
+    console.error(new Error("Operation failed"));
+  }
 };
 
 await calculateHash();
