70a1b925559d95f3d5aa424b7941f9f0d3a49a77
Dawit Hagos <Dawith305@gmail.com>
2024-01-29 23:53:25 +0300

    feat: implement read file method

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import fsPromises from "fs/promises";
+import fs from "fs";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
 const read = async () => {
-    // Write your code here 
+  const basePath = dirname(fileURLToPath(import.meta.url));
+  const fileToRead = path.join(basePath, "/files/fileToRead.txt");
+
+  const errorMessage = "FS operation failed";
+
+  if (!fs.existsSync(fileToRead)) {
+    throw new Error(errorMessage);
+  }
+
+  console.log(await fsPromises.readFile(fileToRead, "utf8"));
 };
 
 await read();
