f3f5dfcec1e76bd145f9cd0e2143fd3581798cf6
max <mcnic@users.noreply.github.com>
2024-01-29 13:04:57 -0800

    feat: implementation 'hash'

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,29 @@
+import { createReadStream } from 'node:fs';
+const { createHash } = await import('node:crypto');
+import path from 'node:path';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256');
+    const file = path.join(
+        process.cwd(),
+        'src/hash/files/fileToCalculateHashFor.txt'
+    );
+
+    const myReadStream = createReadStream(file);
+    let content = '';
+
+    myReadStream.on('data', function (chunk) {
+        content += chunk;
+    });
+
+    myReadStream.on('error', function (err) {
+        console.error(err);
+    });
+
+    myReadStream.on('end', function () {
+        const data = hash.update(content, 'utf-8');
+        console.log(data.digest('hex'));
+    });
 };
 
 await calculateHash();
