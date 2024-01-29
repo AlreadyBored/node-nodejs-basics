c77c69cb12d27e1593852b2e21e040a539f3e745
Julia Usanova <juliausanova@gmail.com>
2024-01-29 15:38:14 +0200

    feature: implement calcHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,12 @@
+import { createHash } from 'node:crypto';
+import { createReadStream } from 'node:fs';
+import { stdout } from 'node:process';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256');
+    const content = createReadStream('./files/fileToCalculateHashFor.txt');
+    
+    content.pipe(hash).setEncoding('hex').pipe(stdout);
 };
 
 await calculateHash();
