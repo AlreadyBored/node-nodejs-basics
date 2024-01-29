574d8dd448c9181e55216f15963f782d5be5053c
Olena Datso <elena.shcheglova@gmail.com>
2024-01-28 19:58:07 -0700

    feat: add hash func

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,26 @@
+import fs from 'fs';
+import crypto  from 'crypto';
 const calculateHash = async () => {
-    // Write your code here 
+
+function calculateSHA256HashWithStreams(filePath) {
+  const readStream = fs.createReadStream(filePath);
+  const hash = crypto.createHash('sha256');
+
+  readStream.on('data', (chunk) => {
+    hash.update(chunk);
+  });
+
+  readStream.on('end', () => {
+    const fileHash = hash.digest('hex');
+    console.log(`SHA256 Hash of the file: ${fileHash}`);
+  });
+
+  readStream.on('error', (error) => {
+    console.error(`Error reading the file: ${error.message}`);
+  });
+}
+
+calculateSHA256HashWithStreams('src/hash/files/fileToCalculateHashFor.txt');
 };
 
 await calculateHash();
