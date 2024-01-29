4c7121a24a5bf994f2a65997c24c641178677c9f
Kirill Shishov <skireal@mail.ru>
2024-01-29 22:51:38 +0300

    feat: implement parseArgs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,19 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+
+  if (args.length > 0) {
+    const parsedArgs = [];
+
+    for (let i = 0; i < args.length; i += 2) {
+      const propName = args[i].replace(/^--/, '');
+      const propValue = args[i + 1];
+      parsedArgs.push(`${propName} is ${propValue}`);
+    }
+
+    console.log(parsedArgs.join(', '));
+  } else {
+    console.log('No command line arguments have found');
+  }
 };
 
 parseArgs();
