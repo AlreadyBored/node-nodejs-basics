6b7dbedbd8affd58a6488a1811288f664bce35ad
VoitehovichXenia <voitehovichkg@gmail.com>
2024-01-29 13:37:09 +0300

    feat: implement CLI env.js task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
+// implement function that parses environment variables with prefix RSS_ and prints them to the console in the format RSS_name1=value1; RSS_name2=value2
 const parseEnv = () => {
-    // Write your code here 
+    let envOutput = [];
+    for (const envVar in process.env) {
+        if (envVar.startsWith('RSS_')) {
+            envOutput.push(
+                `${envVar}=${process.env[envVar]};`
+            );
+        }
+    }
+    envOutput.length && console.log(envOutput.join(' '));
 };
 
 parseEnv();
