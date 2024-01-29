08d0df51a200bac4db9d36627aa06200732a9571
Kirill Shishov <skireal@mail.ru>
2024-01-29 22:16:30 +0300

    feat: implement delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,25 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { promises } from 'fs';
+import { checkFileExists } from './utils.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+  const fileToRemove = path.join(__dirname, 'files', 'fileToRemove.txt');
+
+  try {
+    const existsFileToRemove = await checkFileExists(fileToRemove);
+
+    if (existsFileToRemove) {
+      await promises.unlink(fileToRemove);
+    } else {
+      throw new Error('FS operation failed');
+    }
+  } catch (err) {
+    throw err;
+  }
 };
 
 await remove();
