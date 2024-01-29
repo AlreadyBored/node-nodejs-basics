ee75088db5faa6fdc039177bea23cc84427fde44
max <mcnic@users.noreply.github.com>
2024-01-29 13:03:47 -0800

    feat: implementation 'fs:rename'

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,20 @@
+import { existsSync } from 'node:fs';
+import { rename as asyncRename } from 'node:fs/promises';
+import path from 'node:path';
+
 const rename = async () => {
-    // Write your code here 
+    const srcFile = path.join(process.cwd(), 'src/fs/files/wrongFilename.txt');
+    const destFile = path.join(process.cwd(), 'src/fs/files/properFilename.md');
+
+    if (existsSync(destFile) || !existsSync(srcFile)) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        await asyncRename(srcFile, destFile);
+    } catch (err) {
+        console.error(err);
+    }
 };
 
 await rename();
