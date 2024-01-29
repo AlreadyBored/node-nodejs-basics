0e32c19de2863361ceb6c65a09196aa8f515241e
Shuhrat <shuhratillon@gmail.com>
2024-01-29 22:33:17 +0500

    rename task implemented

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,31 @@
+import fs from 'fs/promises';
+
 const rename = async () => {
-    // Write your code here 
+    const sourceFileName = './src/fs/files/wrongFilename.txt';
+    const targetFileName = './src/fs/files/properFilename.md';
+  
+    try {
+      await fs.access(sourceFileName, fs.constants.F_OK);
+  
+      try {
+        await fs.access(targetFileName, fs.constants.F_OK);
+        throw new Error('FS operation failed');
+      } catch (err) {
+        if (err.code !== 'ENOENT') {
+          throw new Error('FS operation failed');
+        }
+      }
+  
+      await fs.rename(sourceFileName, targetFileName);
+  
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        throw new Error('FS operation failed');
+      } else {
+        throw new Error('FS operation failed');
+      }
+    }
+  
 };
 
 await rename();
