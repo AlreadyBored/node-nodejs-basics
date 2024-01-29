b4ad49ea04bd11428c3c272df47e26d5ea8dedeb
Shuhrat <shuhratillon@gmail.com>
2024-01-29 21:29:07 +0500

    fs -> folder create.js task function implemented

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import fs from 'fs';
+import path from 'path';
+
+
 const create = async () => {
-    // Write your code here 
+    const root = './src/fs/files';
+    const name = 'fresh.txt';
+    const file = path.join(root, name);
+
+    if (fs.existsSync(file)) {
+        throw new Error('FS operation failed');
+    }
+    fs.writeFileSync(file, 'I am fresh and young');
+
 };
 
 await create();
