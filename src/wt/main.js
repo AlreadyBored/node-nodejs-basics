814150807193d5538190403765121be85ba0d775
ABiryulin <abiryulin@mmtr.ru>
2024-01-29 23:20:29 +0300

    Implemented first task wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,11 @@
+import { Worker } from 'node:worker_threads'
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const performCalculations = async () => {
-    // Write your code here
+    const worker = new Worker(path.join(__dirname, 'worker.js'), { workerData: Math.round(Math.random() * 10) })
 };
 
 await performCalculations();
