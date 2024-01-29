0f5084edaf5887302076c06cb4d5606adf5761f2
Andrei_Dzenicenko <denisenko.andrei.da@gmail.com>
2024-01-29 22:22:09 +0100

    feature: copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,30 @@
+import fs from 'fs/promises'
+
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+// Get the URL of the current module
+const currentModuleUrl = import.meta.url;
+
+// Convert the URL to a file path
+const currentModulePath = fileURLToPath(currentModuleUrl);
+
+// Get the directory name
+const currentDirectory = dirname(currentModulePath);
+
+const sourceDir = path.join(currentDirectory, "files");
+const targetDir = path.join(currentDirectory, "files_copy")
 const copy = async () => {
-    // Write your code here 
-};
+
+    try {
+        await fs.cp(sourceDir, targetDir, {
+            recursive: true,
+            force: false,
+            errorOnExist: true });
+    } catch {
+        throw new Error ("operation failed");
+    }
+
+}
 
 await copy();
