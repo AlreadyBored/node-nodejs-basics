3417e3d6fbdc65261809f02f568e125a72980714
a.karavaychik <a.karavaychik@gmail.com>
2024-01-29 18:24:22 +0300

    feat: implement wt modules

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,30 @@
-const performCalculations = async () => {
-    // Write your code here
+import { Worker } from "worker_threads";
+import os from "os";
+import path from "path";
+import { fileURLToPath } from "url";
+
+export const performCalculations = async () => {
+  let promises = [];
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  for (let i = 0; i < os.cpus().length; i++) {
+    promises.push(
+      new Promise((resolve, reject) => {
+        const worker = new Worker(path.join(__dirname, "worker.js"), {
+          workerData: 10 + i,
+        });
+        worker.on("message", (msg) =>
+          resolve({ status: "resolved", data: msg })
+        );
+        worker.on("error", (err) => resolve({ status: "error", data: null }));
+      })
+    );
+  }
+
+  await Promise.all(promises).then((data) => {
+    console.log(data);
+  });
 };
 
-await performCalculations();
+performCalculations();
