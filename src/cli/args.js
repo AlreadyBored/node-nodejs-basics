f239c5ca32bb8cc86c15f94d683cea4bc64a8f21
Julia Usanova <juliausanova@gmail.com>
2024-01-29 13:14:38 +0200

    feat: implement args function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,9 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+
+    for(let i = 1; i < args.length; i+=2) {
+        console.log(`${args[i-1]} is ${args[i]}`);
+    }
 };
 
 parseArgs();
