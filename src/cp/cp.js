771b91ec0a5aa6182b19859c7ac9b9b3f526e03e
msultanovdev <mukhammadamin.sultanov@gmail.com>
2024-01-29 18:44:15 +0500

    feat: add solutions to wt & cp tasks

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,12 @@
+import { fork } from "child_process";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const path = "./files/script.js";
+  const url = new URL(path, import.meta.url);
+
+  const childProcess = fork(url, args, { silent: true });
+  process.stdin.pipe(childProcess.stdin);
+  childProcess.stdout.pipe(process.stdout);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess();
