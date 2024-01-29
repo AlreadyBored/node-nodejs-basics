8fe52cb5f19826315cea371f36ca398a43c1c163
Olena Datso <elena.shcheglova@gmail.com>
2024-01-28 20:31:39 -0700

    feat: add decompress func

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import fs from 'fs';
+import zlib from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+ const directory = './src/zip/files/';
+  function decompressFile(inputFilePath, outputFilePath) {
+    const readStream = fs.createReadStream(inputFilePath);
+
+    const writeStream = fs.createWriteStream(outputFilePath);
+
+    const gunzip = zlib.createGunzip();
+
+    readStream
+      .pipe(gunzip)
+      .pipe(writeStream)
+      .on('finish', () => console.log('File decompression completed.'))
+      .on('error', (error) =>
+        console.error('Error during decompression:', error)
+      );
+  }
+
+  decompressFile(`${directory}archive.gz`, `${directory}fileToCompress.txt`);
 };
 
 await decompress();
