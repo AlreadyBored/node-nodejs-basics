7b4032eaa4d1b5178cf33be024a15a27c092e45b
Dmitry Bludchy <dmitrybludchy@gmail.com>
2024-01-29 14:08:31 +0300

    feat: implement spawnChildProcess function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,16 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { fork } from 'child_process';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const SCRIPT_PATH = join(__dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const childProcess = fork(SCRIPT_PATH, args, { silent: true });
+
+  process.stdin.pipe(childProcess.stdin);
+  childProcess.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(/* [someArgument1, someArgument2, ...] */);
