f901addbeead39a92689a8c84cb885f353ca2ece
Оля Брикса <oly.briksa@gmail.com>
2024-01-29 09:33:52 +0300

    feat: implement calculateHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,12 @@
+import { readFile } from 'node:fs/promises';
+import { createHash } from 'crypto';
+
+const filePath = new URL('files/fileToCalculateHashFor.txt', import.meta.url);
+
 const calculateHash = async () => {
-    // Write your code here 
+    const fileContent = await readFile(filePath);
+    const hashData = createHash('sha256').update(fileContent).digest('hex');
+    console.log(hashData);
 };
 
 await calculateHash();
