71bab05d7232228c0a5cc3acff70b689b07c9dcf
Shuhrat <shuhratillon@gmail.com>
2024-01-29 23:15:06 +0500

    cli/args.js implemented

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const formattedArgs = args.reduce((result, arg, index, array) => {
+    if (arg.startsWith('--')) {
+      const propName = arg.slice(2);
+      const propValue = array[index + 1];
+      result.push(`${propName} is ${propValue}`);
+    }
+    return result;
+  }, []);
+  console.log(formattedArgs.join(', '));
 };
 
 parseArgs();
