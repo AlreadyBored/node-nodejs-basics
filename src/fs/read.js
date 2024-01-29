734277eb736568ab0faf742348c6169dfab75583
anton-glebov <anton.glebov@useinsider.com>
2024-01-30 01:28:47 +0300

    feat: implement read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import { readFile } from "node:fs";
+import * as path from "node:path";
+import { fileURLToPath } from 'node:url';
+
 const read = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const encoding = 'utf8';
+    const fileToRead = path.join(__dirname, 'files', 'fileToRead.txt');
+
+    readFile(fileToRead, encoding, (err, file) => {
+        if (err) {
+            throw new Error('FS operation failed');
+        } else {
+            console.log(file);
+        }
+    })
 };
 
 await read();
