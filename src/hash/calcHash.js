7bca0ec182260709efea240c5635081b21b5e09d
VoitehovichXenia <voitehovichkg@gmail.com>
2024-01-29 15:41:42 +0300

    feat: implement hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+// implement function that calculates SHA256 hash for file fileToCalculateHashFor.txt and logs it into console as hex using Streams API
+import { pipeline } from 'node:stream/promises';
+import { createReadStream } from 'node:fs';
+import { createHash } from 'node:crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256');
+    hash.setEncoding('hex');
+    
+    const fileToHashReadableStream = createReadStream('./files/fileToCalculateHashFor.txt');
+    fileToHashReadableStream.on('end', () => {
+        hash.end();
+        console.log(hash.read());
+    });
+
+    pipeline (
+        fileToHashReadableStream,
+        hash
+    );
 };
 
 await calculateHash();
