3c18cd86e53f46dbaa008e78cb6eff0e8a56259b
Olena Datso <elena.shcheglova@gmail.com>
2024-01-28 20:08:05 -0700

    feat: add streams write func

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
+
 const write = async () => {
-    // Write your code here 
+
+        function writeStdinToFile(filePath) {
+          const writeStream = fs.createWriteStream(filePath);
+
+          process.stdin.pipe(writeStream);
+
+          process.stdin.on('end', () => {
+            writeStream.end();
+            console.log('Finished writing to file.');
+          });
+
+          writeStream.on('error', (error) => {
+            console.error(`Error writing to the file: ${error.message}`);
+          });
+        }
+
+        writeStdinToFile('./src/streams/files/fileToWrite.txt');
 };
 
 await write();
