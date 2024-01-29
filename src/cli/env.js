786097dca1085b9a0227c061db26fe486cce539a
max <mcnic@users.noreply.github.com>
2024-01-29 13:05:31 -0800

    feat: implementation 'cli:env'

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+    const res = Object.keys(process.env)
+        .filter((key) => key.includes('RSS_'))
+        .map((key) => `${key}=${process.env[key]}`);
+
+    console.log(res.join('; '));
 };
 
 parseEnv();
