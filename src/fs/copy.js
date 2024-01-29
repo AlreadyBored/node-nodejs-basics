839bfaf314050f3a4a8a5d34e7b510b942f55ec8
VoitehovichXenia <voitehovichkg@gmail.com>
2024-01-29 12:33:02 +0300

    refactor: remove unnecessary imports from copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,5 @@
 // implement function that copies folder files files with all its content into folder files_copy at the same level (if files folder doesn't exists or files_copy has already been created Error with message FS operation failed must be thrown)
-import { access, constants, statfs, cp, mkdir } from 'node:fs/promises'
+import { statfs, cp } from 'node:fs/promises'
 
 const copy = async () => {
     try {
