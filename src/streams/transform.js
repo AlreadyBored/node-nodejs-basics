5a0d88d97133c5bc97beac6078662b1886f1d460
VoitehovichXenia <voitehovichkg@gmail.com>
2024-01-29 16:29:37 +0300

    feat: implement streams transform.js task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,25 @@
+// implement function that reads data from process.stdin, reverses text using Transform Stream and then writes it into process.stdout
+import { Transform, pipeline } from 'node:stream'
+
 const transform = async () => {
-    // Write your code here 
+    const transform = new Transform({
+        transform(chunck, encoding, callback) {
+            const reversedChunk = chunck
+                .toString()
+                .trim()
+                .split('')
+                .reverse()
+                .join('');
+            this.push(reversedChunk)
+            callback()
+        }
+    })
+    pipeline(
+        process.stdin,
+        transform,
+        process.stdout,
+        (err) => console.log(err)
+    )
 };
 
 await transform();
