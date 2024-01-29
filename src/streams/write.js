1e61b4af633829814b356b80936f2451e8830307
Shuhrat <shuhratillon@gmail.com>
2024-01-29 23:48:24 +0500

    streams/write.js implemented

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,35 @@
+import fs from "fs";
+import { Writable } from "stream";
+
 const write = async () => {
-    // Write your code here 
+  const fileToWrite = "src/streams/files/fileToWrite.txt";
+  const writableStream = fs.createWriteStream(fileToWrite);
+  const writeDataToFile = new Writable({
+    write(chunk, encoding, callback) {
+      // Write the data to the file
+      writableStream.write(chunk, encoding);
+      callback();
+    },
+  });
+
+  process.stdin.pipe(writeDataToFile);
+
+  writeDataToFile.on("finish", () => {
+    writableStream.end();
+  });
+
+  writeDataToFile.on("error", (err) => {
+    throw new Error("")
+  });
+
+  process.on("exit", () => {
+    writableStream.end();
+  });
+
+  process.on("SIGINT", () => {
+    writableStream.end();
+    process.exit();
+  });
 };
 
 await write();
