df5efba5fc5b7b629d2a6b3257226386f91405e2
max <mcnic@users.noreply.github.com>
2024-01-29 13:03:32 -0800

    feat: implementation 'fs:list'

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import { existsSync } from 'node:fs';
+import { readdir } from 'node:fs/promises';
+import path from 'node:path';
+
 const list = async () => {
-    // Write your code here 
+    const srcDir = path.join(process.cwd(), 'src/fs/files');
+
+    if (!existsSync(srcDir)) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        const files = await readdir(srcDir);
+        files.map((file) => console.log(file));
+    } catch (err) {
+        console.error(err);
+    }
 };
 
 await list();
