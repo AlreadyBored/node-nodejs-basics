b501f210b742ba2865772f9209e765e673c165f9
alex.pranevich <freennnn@gmail.com>
2024-01-29 12:49:44 +0100

    feat: add streams

--- src/fs/read.js
+++ src/fs/read.js
@@ -3,7 +3,7 @@ import { fileURLToPath } from 'url';
 import fsPromises from 'fs/promises';
 import * as utils from './utils.js';
 
-export const dirName = path.dirname(fileURLToPath(import.meta.url));
+const dirName = path.dirname(fileURLToPath(import.meta.url));
 const sourcePath = path.join(dirName, "files", "fileToRead.txt");
 
 const read = async () => {
