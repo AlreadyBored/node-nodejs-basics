a88584783f31d22c5f2cff56b5cc2d46e9b38a0d
Biarslan <thebiarslan@gmail.com>
2024-01-29 21:16:31 +0300

    feat: add child process task completed

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,16 @@
+import { fork } from 'child_process';
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const scriptPath = path.join(__dirname, "files", "script.js");
+    const childProcess = fork(scriptPath, args, { stdio: "pipe" });
+
+    process.stdin.pipe(childProcess.stdin);
+    childProcess.stdout.pipe(process.stdout);
+
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["--first_argument", "--second_argument"]);
