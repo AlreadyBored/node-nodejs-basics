4d9192e782e12802d1634394296a4207ec82fc55
Olena Datso <elena.shcheglova@gmail.com>
2024-01-28 20:26:02 -0700

    feat: add compress func

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,28 @@
+import fs from 'fs';
+import zlib from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+
+  const directory = './src/zip/files/';
+
+    function compressFile(inputFilePath, outputFilePath) {
+      const readStream = fs.createReadStream(inputFilePath);
+
+      const writeStream = fs.createWriteStream(outputFilePath);
+
+      const gzip = zlib.createGzip();
+
+      readStream
+        .pipe(gzip)
+        .pipe(writeStream)
+        .on('finish', () => console.log('File compression completed.'))
+        .on('error', (error) =>
+          console.error('Error during compression:', error)
+        );
+    }
+
+    compressFile(`${directory}fileToCompress.txt`, `${directory}archive.gz`);
+
 };
 
 await compress();
