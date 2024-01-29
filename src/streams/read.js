ab7f4cc1fc2c8d387d9b407d87e82aaac945b073
VoitehovichXenia <voitehovichkg@gmail.com>
2024-01-29 16:15:05 +0300

    feat: implement streams read.js task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+// implement function that reads file fileToRead.txt content using Readable Stream and prints it's content into process.stdout
+import { createReadStream } from 'node:fs'
+import { Readable } from 'node:stream'
+
 const read = async () => {
-    // Write your code here 
+    const result =''
+    const readable = createReadStream('./files/fileToRead.txt')
+    readable.on('redable', (chunck) => {
+        result += chunck
+    })
+    readable.on('end', () => console.log(result))
+    readable.pipe(process.stdout)
 };
 
 await read();
