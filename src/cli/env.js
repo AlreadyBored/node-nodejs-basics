965c60eec655f831fb7899c9ae18c18a5a74b280
Julia Usanova <juliausanova@gmail.com>
2024-01-29 12:47:02 +0200

    feat: implement parseEnv function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+    for (const key in process.env) {
+        if (Object.hasOwnProperty.call(process.env, key) && key.startsWith('RSS_')) {
+            const value = process.env[key];
+            console.log(`${key}=${value}`)
+        }
+    }
 };
 
 parseEnv();
