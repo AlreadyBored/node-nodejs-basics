6d7f6a15e9a5cdc4f0aa96202c7d863d59a44c11
baranovdv <baranov.d.v.1988@gmail.com>
2024-01-30 00:51:03 +0700

    feat: add wt data check

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -6,6 +6,8 @@ const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2
 const sendResult = () => {
     const id = workerData
 
+    if (isNaN(id)) throw new Error()
+
     const result = nthFibonacci(id + 10)
 
     parentPort.postMessage({ result: result, id: id})
