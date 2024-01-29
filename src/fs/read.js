a3df41229497d3e19af3c839f75331bd697e0569
Shuhrat <shuhratillon@gmail.com>
2024-01-29 22:57:10 +0500

    read task implemented

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import fs from "fs/promises";
+
 const read = async () => {
-    // Write your code here 
+  const fileName = "./src/fs/files/fileToRead.txt";
+  try {
+    const fileContent = await fs.readFile(fileName, "utf-8");
+    console.log(fileContent);
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    } else {
+        throw new Error("FS operation failed");
+    }
+  }
 };
 
 await read();
