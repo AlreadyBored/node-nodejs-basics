235dd7151fe65747483d6e7a22a108b8df7accc2
Dawit Hagos <Dawith305@gmail.com>
2024-01-29 23:48:21 +0300

    feat: implement list files method

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,23 @@
+import fsPromises from "fs/promises";
+import fs from "fs";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
 const list = async () => {
-    // Write your code here 
+  const basePath = dirname(fileURLToPath(import.meta.url));
+  const filesDirectory = path.join(basePath, "/files");
+
+  const errorMessage = "FS operation failed";
+
+  if (!fs.existsSync(filesDirectory)) {
+    throw new Error(errorMessage);
+  }
+
+  const filesList = await fsPromises.readdir(filesDirectory);
+
+  filesList.forEach((file) => {
+    console.log(file);
+  });
 };
 
 await list();
