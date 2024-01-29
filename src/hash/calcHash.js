fd4e2eb5448a7de6192975478b57dbbd6b556d17
Almas Kilichov <itsmywaytoamerica@gmail.com>
2024-01-30 02:52:10 +0600

    feat: add calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import { createHash } from "crypto";
+import { createReadStream } from "fs";
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash("sha256");
+    const fileStream = createReadStream("./files/fileToCalculateHashFor.txt");
+  
+    fileStream.on('error', err => console.error('File Stream Error:', err));
+    hash.on('error', err => console.error('Hash Stream Error:', err));
+  
+    fileStream.pipe(hash);
+  
+    const chunks = [];
+    hash.on('data', chunk => chunks.push(chunk));
+  
+    hash.on('end', () => {
+      const result = Buffer.concat(chunks).toString('hex');
+      console.log(result);
+    });
 };
 
 await calculateHash();
