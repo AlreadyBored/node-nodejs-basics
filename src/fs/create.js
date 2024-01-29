0b8d40eb66913c250e541251b33b1c2876e9c6af
SergeyObodovsky <s.obodovsky@softteco.com>
2024-01-30 01:35:23 +0300

    init: start fs/create file task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,27 @@
-const create = async () => {
-    // Write your code here 
+import { access, mkdir } from 'node:fs/promises';
+import { createWriteStream } from 'node:fs';
+import { join } from 'node:path';
+
+const create = async (folderName, fileName, fileContent) => {
+    await mkdir(folderName, { recursive: true });
+    const filePath = join(folderName, fileName);
+
+    try {
+        await access(filePath);
+
+        throw new Error('FS operation failed');
+    } catch (error) {
+        const writeStream = createWriteStream(filePath);
+
+        writeStream.on('open', () => {
+            writeStream.write(fileContent);
+            writeStream.end();
+        });
+
+        writeStream.on('error', (error) => {
+            console.error(`FS operation failed: ${error.message}`);
+        });
+    }
 };
 
-await create();
+await create('files',  'fresh.txt', 'I am fresh and young');
