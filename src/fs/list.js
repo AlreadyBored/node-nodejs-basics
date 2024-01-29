26b479829dac8d5068a65a713f944784945b9aa0
VoitehovichXenia <voitehovichkg@gmail.com>
2024-01-29 12:44:56 +0300

    feat: implement FS list.js task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,15 @@
+// implement function that prints all array of filenames from files folder into console (if files folder doesn't exists Error with message FS operation failed must be thrown)
+import { readdir } from 'node:fs/promises'
+
 const list = async () => {
-    // Write your code here 
+    try {
+        const filenames = await readdir('./files')
+        for (const name of filenames) {
+            console.log(name)
+        }
+    } catch {
+        throw new Error('FS operation failed')
+    }
 };
 
 await list();
