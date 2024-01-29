32b0aba929e5ea0ea82dcae0bf8d936f11362172
Оля Брикса <oly.briksa@gmail.com>
2024-01-29 23:41:48 +0300

    feat: implement compress function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,13 +1,10 @@
 import { Transform } from 'node:stream';
 import { pipeline } from 'node:stream/promises';
 
-const revertString = (str) =>
-    str.split('').reverse().join('');
-
 const transform = async () => {
   const transformInput = new Transform({
     transform(chunk, encoding, callback) {
-      callback(null, revertString(chunk.toString()));
+      callback(null, (chunk.toString()).split('').reverse().join(''));
     },
   });
 
