8cdbd0dbcdcfa7239cf6906a54fee3d51fdd7df3
behzodjon <behzodjontuit1998@gmail.com>
2024-01-29 17:20:05 +0500

    feat: add solutiuons

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import path from "path";
+
+
 const create = async () => {
-    // Write your code here 
+    const filePath = new URL(path.resolve('./files/fresh.txt'), import.meta.url).pathname;
+
+    const content = 'I am fresh and young';
+
+    try {
+        // check if file already exists
+        if (fs.existsSync(filePath)) {
+            throw new Error('FS operation failed');
+        }
+
+        // create new file with content
+        fs.writeFileSync(filePath, content);
+        console.log('File created successfully!');
+    } catch (err) {
+        console.error(err.message);
+    }
 };
 
 await create();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,19 @@
+import fs from 'fs';
+import path from "path";
+
 const rename = async () => {
-    // Write your code here 
+    const sourcePath = new URL(path.resolve('./files/wrongFilename.txt'), import.meta.url).pathname;
+    const destinationPath = new URL(path.resolve('./files/properFilename.md'), import.meta.url).pathname;
+
+    if (!fs.existsSync(sourcePath)) {
+        throw new Error('FS operation failed: source file does not exist');
+    }
+
+    if (fs.existsSync(destinationPath)) {
+        throw new Error('FS operation failed: destination file already exists');
+    }
+
+    fs.renameSync(sourcePath, destinationPath);
 };
 
 await rename();
