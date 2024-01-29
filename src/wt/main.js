499f24019b4528df0392df9d36802bc420e3ef63
PopovaOlha <fc.odess.10@gmail.com>
2024-01-29 18:32:06 +0200

    feat: update wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,28 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const performCalculations = async () => {
-    // Write your code here
+  const numCores = os.cpus().length;
+  const results = [];
+
+  const workers = Array.from({ length: numCores }, (_, index) => {
+    const currentDir = path.dirname(fileURLToPath(import.meta.url));
+    const workerPath = path.resolve(currentDir, 'worker.js');
+    const worker = new Worker(workerPath, { workerData: 10 + index });
+
+    worker.on('message', (message) => {
+      results.push(message);
+      if (results.length === numCores) {
+        console.log(results);
+      }
+    });
+
+    return worker;
+  });
+
+  await Promise.all(workers.map((worker) => new Promise((resolve) => worker.on('exit', resolve))));
 };
 
 await performCalculations();
