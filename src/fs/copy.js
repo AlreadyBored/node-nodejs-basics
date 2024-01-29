64cf05f1d742b96fbf0cd526527914337c811349
oleg filkin <oleg.filkin.by@gmail.com>
2024-01-29 14:02:54 +0300

    Copy.js: implemented

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,34 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { FsOperationFailed } from './errors.js';
+
 const copy = async () => {
-    // Write your code here 
+    const filesDirPath = path.resolve('src', 'fs', 'files');
+    const filesDirCopyPath = path.resolve('src', 'fs', 'files_copy');
+    let isFilesExist = true;
+    let isCopied = true;
+
+    try {
+        await fs.stat(filesDirPath);
+    } catch (error) {
+        isFilesExist = false;
+    }
+
+    try {
+        await fs.stat(filesDirCopyPath);
+    } catch (error) {
+        isCopied = false;
+    }
+    
+    if (!isFilesExist || isCopied) {
+        throw new FsOperationFailed();
+    }
+ 
+    try {
+        await fs.cp(filesDirPath, filesDirCopyPath, {recursive: true});    
+    } catch (error) {
+        console.log(error)
+    }
 };
 
 await copy();
