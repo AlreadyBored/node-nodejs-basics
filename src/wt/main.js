34b7d7c1bf0ab5328573614fcab93caeecf1ea4d
Оля Брикса <oly.briksa@gmail.com>
2024-01-30 01:46:10 +0300

    feat: implement functions in wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,21 @@
+import { cpus } from 'os';
+import { Worker } from 'node:worker_threads';
+
 const performCalculations = async () => {
-    // Write your code here
+    const workerPath = new URL('./worker.js', import.meta.url);
+
+    const calculateNthFibonacci = (workerData) => new Promise((resolve) => {
+        const worker = new Worker(workerPath, { workerData });
+    
+        worker.on('message', (data) => resolve({ status: 'resolve', data }));
+        worker.on('error', () => resolve({ status: 'error', data: null }));
+    });
+
+    var resultArray = new Array(cpus().length).fill(null);
+    resultArray = resultArray.map((value, index) => calculateNthFibonacci(index + 10));
+    const data = await Promise.all(resultArray);
+
+    console.log(data);
 };
 
 await performCalculations();
