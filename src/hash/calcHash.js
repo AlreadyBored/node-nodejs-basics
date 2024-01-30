482fa12bba8090253ac11d8bfa79b6900672529e
Dawit Hagos <Dawith305@gmail.com>
2024-01-30 09:58:20 +0300

    feat: implement calculate hash for file

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
+import { createReadStream } from "fs";
+import { createHash } from "crypto";
+
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const basePath = dirname(fileURLToPath(import.meta.url));
+  const filePath = path.join(basePath, "files/fileToCalculateHashFor.txt");
+
+  const fileStream = createReadStream(filePath);
+  const hash = createHash("sha256");
+
+  fileStream.on("data", (chunk) => {
+    hash.update(chunk);
+  });
+
+  fileStream.on("end", () => {
+    const hashResult = hash.digest("hex");
+    console.log(hashResult);
+  });
+
+  fileStream.on("error", (error) => {
+    console.error('Error occurred while reading ' + error.message);
+  });
 };
 
 await calculateHash();
