29c3ae61241a48d144e6c7dff66cac0c97be3ca7
SergeyObodovsky <s.obodovsky@softteco.com>
2024-01-30 03:12:40 +0300

    init: add fs/copy task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,39 @@
-const copy = async () => {
-    // Write your code here 
+import { copyFileSync, existsSync, mkdirSync, readdirSync, statSync } from 'node:fs';
+import { join } from 'node:path';
+
+const isDirectoryAndExist = (name) => {
+    const exists = existsSync(name);
+    const stats = exists && statSync(name);
+
+    return  exists && stats.isDirectory();
+}
+
+const copyRecursive = (src, dest) => {
+    const isDirectory = isDirectoryAndExist(src);
+
+    if (isDirectory) {
+        mkdirSync(dest);
+        readdirSync(src).forEach(function(childItemName) {
+            copyRecursive(join(src, childItemName),
+                join(dest, childItemName));
+        });
+    } else {
+        copyFileSync(src, dest);
+    }
+}
+
+const copy = async (src, dest) => {
+    const isSrcDirectoryAndExist = isDirectoryAndExist(src);
+    const isDestDirectoryAndExist = isDirectoryAndExist(dest);
+
+    console.log(isDestDirectoryAndExist, !isSrcDirectoryAndExist)
+    if(!isSrcDirectoryAndExist || isDestDirectoryAndExist){
+        throw new Error('FS operation failed')
+    } else {
+        copyRecursive(src, dest)
+    }
+
+
 };
 
-await copy();
+await copy('files', 'files_copy');
