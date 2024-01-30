16e190d7242e9a92c62f1c124ad35ccc0f730419
SergeyObodovsky <s.obodovsky@softteco.com>
2024-01-30 03:37:10 +0300

    init: add fs/rename task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,20 @@
-const rename = async () => {
-    // Write your code here 
+import { existsSync, rename as renameFS } from 'node:fs';
+import { join } from 'node:path';
+
+const rename = async (oldName, newName, folder) => {
+    const oldFilePath = join(folder, oldName);
+    const newFilePath = join(folder, newName);
+    const oldNameExists = existsSync(oldFilePath);
+    const newNameExists = existsSync(newFilePath);
+
+
+    if(!oldNameExists || newNameExists) {
+        throw new Error('FS operation failed')
+    } else {
+        renameFS(oldFilePath, newFilePath, (err) => {
+            if (err) throw new Error(err.message);
+        });
+    }
 };
 
-await rename();
+await rename('wrongFilename.txt', 'properFilename.md', 'files');
