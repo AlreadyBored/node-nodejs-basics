2a81e0db34ce1ce62ad1f38c24c46f4c0db35b9f
zhuravlevanatalia <zhuravleva.977@yandex.by>
2024-01-30 08:33:24 +0100

    feat: complete src/fs task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,35 @@
-const remove = async () => {
-    // Write your code here 
-};
+import { promises as fsPromises } from 'fs';
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
 
-await remove();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+async function remove() {
+    const sourceFolderPath = join(__dirname, './files');
+    const fileToRemove = 'fileToRemove.txt';
+    const filePath = join(sourceFolderPath, fileToRemove);
+
+    try {
+        // Check if the file exists
+        await fsPromises.access(filePath);
+
+        // Delete the file
+        await fsPromises.unlink(filePath);
+
+        console.log('File deleted successfully.');
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            // If the error is "not found", throw an error
+            throw new Error('FS operation failed: File not found.');
+        } else {
+            // Propagate other errors
+            throw error;
+        }
+    }
+}
+
+remove().catch((error) => {
+    console.error(error.message);
+    process.exit(1);
+});
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,31 @@
-const read = async () => {
-    // Write your code here 
-};
+import { promises as fsPromises } from 'fs';
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
 
-await read();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+async function read() {
+    const sourceFolderPath = join(__dirname, './files');
+    const fileToRead = 'fileToRead.txt';
+    const filePath = join(sourceFolderPath, fileToRead);
+
+    try {
+        await fsPromises.access(filePath);
+
+        const fileContent = await fsPromises.readFile(filePath, 'utf-8');
+
+        console.log('Content of fileToRead.txt:', fileContent);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed: File not found.');
+        } else {
+            throw error;
+        }
+    }
+}
+
+readFileContent().catch((error) => {
+    console.error(error.message);
+    process.exit(1);
+});
