84ed0e947e58976f976572243fecec5fd5b11641
talgatmendekov <talgatmendekov@gmail.com>
2024-01-30 23:16:10 +0600

    feat:implement function that prints all array of filenames from files folder into console (if files folder doesn't exists Error with message FS operation failed must be thrown)

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,24 @@
+import { readdir, access, constants } from 'fs/promises';
+import { join } from 'path';
+
 const list = async () => {
-    // Write your code here 
+  const folderName = 'files';
+
+  const currentDir = process.cwd();
+  const folderPath = join(currentDir, folderName);
+
+  // Check if the folder exists
+  try {
+    await access(folderPath, constants.F_OK);
+  } catch (folderError) {
+    console.error(`FS operation failed: Folder "${folderName}" does not exist`);
+    throw folderError;
+  }
+
+  // Read the filenames from the folder and print to console
+  const filenames = await readdir(folderPath);
+  console.log('List of filenames in "files" folder:', filenames);
 };
 
+// Example usage
 await list();
