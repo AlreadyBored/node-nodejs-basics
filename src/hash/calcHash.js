98eb61d374853490b05cf5f5e845ec1fdc7c817c
VoitehovichXenia <voitehovichkg@gmail.com>
2024-02-01 13:16:43 +0300

    fix: update pathes in Hash task to make it work in different OS

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -2,12 +2,16 @@
 import { pipeline } from 'node:stream/promises';
 import { createReadStream } from 'node:fs';
 import { createHash } from 'node:crypto';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 const calculateHash = async () => {
     const hash = createHash('sha256');
     hash.setEncoding('hex');
     
-    const fileToHashReadableStream = createReadStream('./files/fileToCalculateHashFor.txt');
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const fileToHashPath = path.resolve(__dirname, 'files', 'fileToCalculateHashFor.txt');
+    const fileToHashReadableStream = createReadStream(fileToHashPath);
     fileToHashReadableStream.on('end', () => {
         hash.end();
         console.log(hash.read());
