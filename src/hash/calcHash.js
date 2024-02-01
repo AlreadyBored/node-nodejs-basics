456ef2842f8d159a7931793f468bf4689ec4f905
VoitehovichXenia <voitehovichkg@gmail.com>
2024-02-01 14:04:51 +0300

    feat: add another implementation of hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -14,9 +14,11 @@ const calculateHash = async () => {
     const fileToHashReadableStream = createReadStream(fileToHashPath);
     fileToHashReadableStream.on('end', () => {
         hash.end();
+        // hash reads data
         console.log(hash.read());
     });
 
+    // read file than pass data to hash 
     pipeline (
         fileToHashReadableStream,
         hash
@@ -24,3 +26,24 @@ const calculateHash = async () => {
 };
 
 await calculateHash();
+
+const calculateHash2 = async () => {
+    const hash = createHash('sha256');
+
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const fileToHashPath = path.resolve(__dirname, 'files', 'fileToCalculateHashFor.txt');
+    const fileToHashReadableStream = createReadStream(fileToHashPath);
+
+    fileToHashReadableStream.on('data', (chunck) => {
+        // Updates the hash content with the given data
+        hash.update(chunck)
+    })
+
+    fileToHashReadableStream.on('end', () => {
+        // Calculates the digest of all of the data passed to be hashed
+        const hexHash = hash.digest('hex')
+        console.log(hexHash);
+    });
+};
+
+await calculateHash2();
