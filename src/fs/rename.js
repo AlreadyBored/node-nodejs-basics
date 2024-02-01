d75d8e31639f860bba1aae8b088a07bb42a1a058
VoitehovichXenia <voitehovichkg@gmail.com>
2024-02-01 13:56:27 +0300

    refactor: simplify FS rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,5 @@
 // implement function that renames file wrongFilename.txt to properFilename with extension .md (if there's no file wrongFilename.txt or properFilename.md already exists Error with message FS operation failed must be thrown)
-import * as fs from 'node:fs/promises';
+import { rename as fsRename } from 'node:fs/promises';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
@@ -8,17 +8,9 @@ const rename = async () => {
     const wrongFilenamePath = path.resolve(__dirname, 'files', 'wrongFilename.txt');
     const properFilenamePath = path.resolve(__dirname, 'files', 'properFilename.txt');
     try {
-        await fs.statfs(wrongFilenamePath);
-    } catch {
-        throw new Error('FS operation failed');
-    }
-    try {
-        await fs.statfs(properFilenamePath);
-        throw new Error('FS operation failed');
+        await fsRename(wrongFilenamePath, properFilenamePath);
     } catch (err) {
-        if (err.code === 'ENOENT') {
-            await fs.rename(wrongFilenamePath, properFilenamePath);
-        }
+        throw new Error('FS operation failed');
     }
 
 };
