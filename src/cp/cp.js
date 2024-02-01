790d30270a8507ba0baf8bcb48bc956cb068f1a2
Irakli Orjonikidze <irakliorjo92@gmail.com>
2024-02-01 17:41:52 +0400

    update

--- src/cp/cp.js
+++
@@ -1,6 +0,0 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
-};
-
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
