a169a015e084c3ffd75bdde1bb1d3554943fbdf4
VoitehovichXenia <voitehovichkg@gmail.com>
2024-02-01 13:34:06 +0300

    refactor: simplify FS create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,19 +1,14 @@
 // implement function that creates new file fresh.txt with content I am fresh and young inside of the files folder (if file already exists Error with message FS operation failed must be thrown)
-import { writeFile, readdir } from 'node:fs/promises';
+import { writeFile } from 'node:fs/promises';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 const create = async () => {
     const __dirname = path.dirname(fileURLToPath(import.meta.url));
-    const filesFolderPath = path.resolve(__dirname, 'files');
     const createdFilePath = path.resolve(__dirname, 'files', 'fresh.txt');
     try {
-        const files = await readdir(filesFolderPath);
-        if (files.includes('fresh.txt')) {
-            throw new Error('FS operation failed');
-        } else  {
-            writeFile(createdFilePath, 'I am fresh and young ');
-        }
+        // wx flag - fails if file exists
+        await writeFile(createdFilePath, 'I am fresh and young ', { flag: 'wx' });
     } catch {
         throw new Error('FS operation failed');
     }
