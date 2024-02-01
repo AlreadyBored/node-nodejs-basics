e5b5b31ddee078accdbc9031e50b9071a06df359
VoitehovichXenia <voitehovichkg@gmail.com>
2024-02-01 14:11:50 +0300

    refactor: simplify Streams read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,18 +1,31 @@
 // implement function that reads file fileToRead.txt content using Readable Stream and prints it's content into process.stdout
 import { createReadStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 const read = async () => {
     const __dirname = path.dirname(fileURLToPath(import.meta.url));
     const fileToReadPath = path.resolve(__dirname, 'files', 'fileToRead.txt');
-    const result = '';
-    const readable = createReadStream(fileToReadPath);
-    readable.on('redable', (chunck) => {
-        result += chunck;
-    })
-    readable.on('end', () => console.log(result));
-    readable.pipe(process.stdout);
+    
+    const readableStream = createReadStream(fileToReadPath);
+    readableStream.on('data', (chunck) => process.stdout.write(chunck));
+    readableStream.on('end', () => process.stdout.write('\n'));
 };
 
 await read();
+
+const read2 = async () => {
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const fileToReadPath = path.resolve(__dirname, 'files', 'fileToRead.txt');
+    
+    const readableStream = createReadStream(fileToReadPath);
+    readableStream.on('end', () => process.stdout.write('\n'));
+
+    await pipeline(
+        readableStream,
+        process.stdout
+    );
+};
+
+// await read2();
