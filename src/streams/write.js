f006678a94b7ead15089d5e5d40144a2d6a3c72a
VoitehovichXenia <voitehovichkg@gmail.com>
2024-02-01 14:38:17 +0300

    refactor: simplify Streams write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,5 @@
 // implement function that writes process.stdin data into file fileToWrite.txt content using Writable Stream
-import { appendFile, createWriteStream } from 'node:fs';
+import { createWriteStream } from 'node:fs';
 import path from 'node:path';
 import { pipeline } from 'node:stream/promises';
 import { fileURLToPath } from 'node:url';
@@ -8,10 +8,6 @@ const write = async () => {
     const __dirname = path.dirname(fileURLToPath(import.meta.url));
     const fileToWritePath = path.resolve(__dirname, 'files', 'fileToWrite.txt');
     const writableStream = createWriteStream(fileToWritePath)
-
-    writableStream.on('data', (chunck) => {
-        appendFile(fileToWritePath, chunck)
-    })
 
     await pipeline (
         process.stdin,
