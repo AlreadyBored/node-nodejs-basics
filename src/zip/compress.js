021a4f75b840d87c7e54b75128346af3c3952a60
Sasha <sasha@Antons-MacBook-Air.local>
2024-02-03 00:09:05 +0100

    Zlib (src/zip) module done

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,25 @@
+import fs from "fs";
+import {createGzip} from "zlib";
+import {pipeline} from "stream/promises";
+import path from "path";
+import {fileURLToPath} from "url";
+
 const compress = async () => {
-    // Write your code here 
+  // Write your code here
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const folderPath = path.join(__dirname, "files");
+  const filePath = path.join(folderPath, "fileToCompress.txt");
+  const fileDest = path.join(folderPath, "archive.gz");
+  const gzip = createGzip();
+  const source = fs.createReadStream(filePath);
+  const destination = fs.createWriteStream(fileDest);
+  try {
+    await pipeline(source, gzip, destination);
+    console.log("File successfully gzipped!");
+  } catch (err) {
+    console.error("An error occurred:", err);
+    process.exitCode = 1;
+  }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,26 @@
+import fs from "fs";
+import zlib from "zlib";
+import path from "path";
+import {fileURLToPath} from "url";
+
 const decompress = async () => {
-    // Write your code here 
+  // Write your code here
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const folderPath = path.join(__dirname, "files");
+  const fileArchive = path.join(folderPath, "archive.gz");
+  const readStream = fs.createReadStream(fileArchive);
+  const writeStream = fs.createWriteStream(
+    path.join(folderPath, "fileToCompress.txt")
+  );
+  const unzip = zlib.createUnzip();
+  try {
+    readStream.pipe(unzip).pipe(writeStream);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 await decompress();
+
+// decompress.js - implement function that decompresses archive.gz
+// back to the fileToCompress.txt with same content as before compression using zlib and Streams API
