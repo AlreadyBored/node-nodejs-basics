dfe4b76a99cdc13b0449438f7a00c96d75b6ff74
LITinx <kuruchbekov2006dan@gmail.com>
2024-02-05 20:49:35 +0600

    added functionality to args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
-const parseArgs = () => {
-    // Write your code here 
-};
+import { argv } from 'node:process'
 
-parseArgs();
+const parseArgs = () => {
+	const cliArguments = argv.slice(2)
+	for (let i = 0; i < cliArguments.length; i = i + 2) {
+		const propName = cliArguments[i]
+		const value = cliArguments[i + 1]
+
+		process.stdout.write(`${propName} is ${value}, `)
+	}
+}
+
+parseArgs()
