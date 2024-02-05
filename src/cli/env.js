430687db16e8d8c7d2fd72d9896edef9c3d9c0d0
LITinx <kuruchbekov2006dan@gmail.com>
2024-02-05 19:33:25 +0600

    Add functionality to env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
-const parseEnv = () => {
-    // Write your code here 
-};
+import { env } from 'node:process'
 
-parseEnv();
+const parseEnv = () => {
+	const rssVariables = Object.keys(env).filter((key) => key.startsWith('RSS_'))
+	for (let i = 0; i < rssVariables.length; i++) {
+		process.stdout.write(`${rssVariables[i]}=${env[rssVariables[i]]}; `)
+	}
+}
+
+parseEnv()
