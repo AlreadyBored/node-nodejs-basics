209a87692b8e49f89a4d592c2793fb6075f52cae
LITinx <kuruchbekov2006dan@gmail.com>
2024-02-08 22:11:09 +0600

    delete unnecessary dependency

--- src/cli/env.js
+++ src/cli/env.js
@@ -5,9 +5,6 @@ const parseEnv = () => {
 		.filter((key) => key.startsWith('RSS_'))
 		.map((variable) => `${variable}=${env[variable]}`)
 		.join('; ')
-	// for (let i = 0; i < rssVariables.length; i++) {
-	// 	process.stdout.write(`${rssVariables[i]}=${env[rssVariables[i]]}; `)
-	// }
 	console.log(result)
 }
 
