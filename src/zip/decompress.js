d0fbb0323ce1ea8a8860b260b5a3654e33911b8b
LITinx <kuruchbekov2006dan@gmail.com>
2024-02-08 19:39:56 +0600

    zip functions added

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
-const decompress = async () => {
-    // Write your code here 
-};
+import { createReadStream, createWriteStream } from 'fs'
+import path from 'path'
+import { pipeline } from 'stream/promises'
+import { createGunzip } from 'zlib'
+import { getFilePath } from '../tools/filepath.js'
 
-await decompress();
+const decompress = async () => {
+	try {
+		const { __dirname } = getFilePath(import.meta.url)
+		const source = createReadStream(path.join(__dirname, 'files', 'archive.gz'))
+		const destination = createWriteStream(
+			path.join(__dirname, 'files', 'fileToCompress.txt'),
+		)
+		const gzip = createGunzip()
+		await pipeline(source, gzip, destination)
+	} catch {
+		throw new Error('Decompress operation failed')
+	}
+}
+
+await decompress()
