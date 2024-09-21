f85aaadb3852ce2f6de3238bd5ef347934097327
4-13 <danilmatvienko@gmail.com>
2024-09-22 02:27:43 +0300

    feat: add fs/copy solution

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,24 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+
+import { getDirPath } from '../utils/getDirPath.js';
+
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        const dirPath = getDirPath(import.meta.url);
+        const srcPath = path.join(dirPath, 'files', '/');
+        const destPath = path.join(dirPath, 'files_copy');
+        await fs.cp(srcPath, destPath, { errorOnExist: true, recursive: true, force: false })
+    } catch {
+        throw Error('FS operation failed');
+    }
 };
 
-await copy();
+copy()
+    .then(() => {
+        console.info('Success')
+    })
+    .catch((err) => {
+        console.error(err.message);
+    });
