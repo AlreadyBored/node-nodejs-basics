aa515309841b1ecbb42b05f3f354cb84628869ab
4-13 <danilmatvienko@gmail.com>
2024-09-22 01:55:00 +0300

    feat: add fs/create solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,31 @@
-const create = async () => {
-    // Write your code here 
-};
+import { promises as fs } from 'fs';
+import path from 'path';
 
-await create();
+import { getDirPath } from '../utils/getDirPath.js';
+import { checkFileExists } from '../utils/checkFileExists.js';
+
+
+export async function createFreshFile() {
+    try {
+        const dirPath = getDirPath(import.meta.url)
+        const filePath = path.join(dirPath, 'files', 'fresh.txt')
+        const isExists = await checkFileExists(filePath);
+
+        if (isExists) {
+            throw Error('FS operation failed');
+        }
+
+        const content = 'I am fresh and young';
+        await fs.writeFile(filePath, content, 'utf-8')
+    } catch (err) {
+        throw err
+    }
+}
+
+createFreshFile()
+    .then(() => {
+        console.log('File created successfully!');
+    })
+    .catch((error) => {
+        console.error(error.message);
+    });
