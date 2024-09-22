cadbd2cd7d20b9c58f0e2b3b84f2d371d60508d5
4-13 <danilmatvienko@gmail.com>
2024-09-22 07:48:01 +0300

    feat: add hash solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import crypto from 'crypto';
+import path from 'path';
+import { createReadStream } from 'fs';
+
+import { getDirPath } from '../utils/getDirPath.js';
+
+
 const calculateHash = async () => {
-    // Write your code here 
+    const fileStream = createReadStream(path.join(getDirPath(import.meta.url), 'files', 'fileToCalculateHashFor.txt'));
+    const hash = crypto.createHash('sha256')
+
+    fileStream
+        .on("data", (data) => {
+            hash.update(data);
+        })
+        .on("end", () => {
+            console.log(hash.digest('hex'));
+        })
+        .on('error', (err) => {
+            console.error(`Error while reading file: ${err.message}`);
+        });
 };
 
 await calculateHash();
