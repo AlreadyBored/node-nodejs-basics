9da45825a44e2f2330def14441b26c3dca4a6343
4-13 <danilmatvienko@gmail.com>
2024-09-22 05:54:51 +0300

    feat: add fs/read solution

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,27 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+
+import { getDirPath } from '../utils/getDirPath.js';
+
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const dirPath = getDirPath(import.meta.url);
+        const filePath = path.join(dirPath, 'files', 'fileToRead.txt');
+        const content = await fs.readFile(filePath, { encoding: "utf-8" });
+
+        console.log(content)
+
+        return content;
+    } catch {
+        throw Error('FS operation failed');
+    }
 };
 
-await read();
+read()
+    .then(() => {
+        console.info('Success');
+    })
+    .catch(err => {
+        console.error(err.message);
+    });
