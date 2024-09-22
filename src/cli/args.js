2d8011dc5e8ac4f0e287b4362a8b8fa11709d3d9
4-13 <danilmatvienko@gmail.com>
2024-09-22 06:37:55 +0300

    feat: add cli/args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    const result = [];
+
+    for (let i = 0; i < args.length; i += 2) {
+        const key = args[i].slice(2);
+        const val = args[i + 1];
+        const keyValStr = `${key} is ${val}`;
+
+        result.push(keyValStr);
+    }
+
+    console.log(result.join(', '))
 };
 
 parseArgs();
