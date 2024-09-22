a2d047b23b80c0315a812b83ad112ac045b712f9
4-13 <danilmatvienko@gmail.com>
2024-09-22 06:25:25 +0300

    feat: add cli/env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+    const VAR_PREFIX = 'RSS';
+
+    const rssEntries = Object.entries(process.env).filter(([k, _]) => k.startsWith(VAR_PREFIX));
+    const transformedEntries = rssEntries.map(([k, v]) => `${k}=${v}`);
+    const result = transformedEntries.join('; ')
+
+    if (result.length) {
+        console.log(result)
+    }
 };
 
 parseEnv();
