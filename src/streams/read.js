a94a07eda1b460a68741d2924c7ee47021bfb9a6
4-13 <danilmatvienko@gmail.com>
2024-09-23 04:38:45 +0300

    feat: add streams/read solution

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,23 @@
+
+import { promises as fs, createReadStream } from 'fs';
+import path from 'path';
+
+import { getDirPath } from '../utils/getDirPath.js';
+
+
 const read = async () => {
-    // Write your code here 
+    const dirPath = getDirPath(import.meta.url);
+    const filePath = path.join(dirPath, 'files', 'fileToRead.txt');
+
+    const readableStream = createReadStream(filePath);
+    readableStream.pipe(process.stdout);
+    readableStream
+        .on('end', () => {
+            process.stdout.write('\n');
+        })
+        .on('error', (err) => {
+            console.error(`Error occured while reading file: ${err.message}`);
+        });
 };
 
 await read();
