8ac8ad35de46b5323f6e8572a17d9bc7dc08c299
4-13 <danilmatvienko@gmail.com>
2024-09-24 01:13:25 +0300

    feat: add wt solution

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,40 @@
-const performCalculations = async () => {
-    // Write your code here
-};
+import { Worker } from 'node:worker_threads';
+import path from 'path';
+import os from 'node:os';
 
-await performCalculations();
+import { getDirPath } from '../utils/getDirPath.js';
+
+
+const createWorker = (filePath, workerData) => {
+    return new Promise((resolve) => {
+        const worker = new Worker(filePath, { workerData });
+
+        worker.on('message', (val) => {
+            resolve({
+                status: 'resolved',
+                data: val,
+            })
+        });
+
+        worker.on('error', () => {
+            resolve({
+                status: 'error',
+                data: null
+            })
+        });
+    });
+}
+
+const performCalculations = async () => {
+    const dirPath = getDirPath(import.meta.url);
+    const filePath = path.join(dirPath, 'worker.js');
+    const cpuAmount = os.availableParallelism();
+    const initialValue = 10;
+    const workers = Array.from({ length: cpuAmount }, (_, index) => {
+        return createWorker(filePath, initialValue + index);
+    })
+
+    return Promise.all(workers)
+}
+
+performCalculations().then(console.log);
