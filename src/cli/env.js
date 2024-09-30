84ed15a6d7dda7bad9c5af39eb5ac2fbad98e97f
Zrrg <hiroaritillwhen@gmail.com>
2024-10-01 00:35:08 +0300

    feat: env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
+
 const parseEnv = () => {
-    // Write your code here 
+    const rssVars = Object.entries(process.env)
+                            .filter(([key]) => key.startsWith('RSS_'))
+                            .map(([key, value]) => `${key}=${value}`);
+    console.log(rssVars.join('; '));
+    
 };
 
 parseEnv();
