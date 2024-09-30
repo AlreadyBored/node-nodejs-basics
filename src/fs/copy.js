656f4c9986986231d10a28086fadd738cb8b2632
Sergei Kornev <kornev.se@intechs.by>
2024-09-30 12:57:38 +0300

    copy/create

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,39 @@
+const fs = require('fs');
+
+const ERROR_MESSAGE = 'FS operation failed';
+const pathForCopy = './src/fs/files';
+const copyPath = pathForCopy + '_copy';
+
 const copy = async () => {
-    // Write your code here 
+    const existOriginalPath = fs.existsSync(pathForCopy);
+    const copiedPathExist = fs.existsSync(copyPath);
+    if (!existOriginalPath || copiedPathExist) {
+        console.log(ERROR_MESSAGE);
+        return;
+    }
+    await createCopiedDir();
 };
 
-await copy();
+async function createCopiedDir() {
+    await fs.mkdir(copyPath, (e) => console.log(ERROR_MESSAGE));
+
+    await fs.readdir(pathForCopy, (err, entries) => {
+        if (err) {
+            return console.log(err)
+        }
+        entries.forEach(entry => {
+            const copyFilePath = pathForCopy + '/' + entry;
+            const newFilePath = copyPath + '/' + entry;
+
+            fs.copyFile(copyFilePath, newFilePath, (e) => {
+                if (e) {
+                    console.log(ERROR_MESSAGE);
+                }
+            });
+
+        })
+    })
+
+}
+
+copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,25 @@
+const fs = require('fs');
+
 const create = async () => {
-    // Write your code here 
+    const filePath = './src/fs/files/fresh.txt';
+    const fileContent = 'I am fresh and young';
+    const errorMessage = 'FS operation failed';
+
+    const existFile = fs.existsSync(filePath);
+
+    if (existFile) {
+        console.log(errorMessage);
+    } else  {
+        await writeFile(filePath, fileContent, errorMessage);
+    }
 };
 
-await create();
+async function writeFile(filePath, fileContent, errorMessage = 'error') {
+    return fs.writeFile(filePath, fileContent, (error) => {
+        if (error) {
+            console.log(errorMessage);
+        }
+    })
+}
+
+create();
