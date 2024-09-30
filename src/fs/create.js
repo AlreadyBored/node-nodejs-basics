0e0df0fd992b226905220e7f07a3436357874955
Zrrg <hiroaritillwhen@gmail.com>
2024-09-30 21:26:52 +0300

    init: create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import fs from 'fs'
+
+const foldername = "./files/";
+const filename = "fresh.txt";
+const content = "I am fresh and young";
+
 const create = async () => {
-    // Write your code here 
+    fs.open(foldername + filename, "wx", (err, descriptor) => {                  // wx flag creates and opens, throws an error if exist
+        if (err) {
+            console.log("FS operation failed", err);
+        } else {
+           //console.log(descriptor);
+            fs.write(descriptor, content, (err, bytes) => {          
+                if (err) {
+                    console.log("FS operation failed", err);
+                } else {
+                    console.log("FS operation succeded");
+                }
+            })
+        }
+    })
 };
 
 await create();
