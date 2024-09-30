195d156f9a4c57b3233badcb5872d0feeeb7c675
Zrrg <hiroaritillwhen@gmail.com>
2024-09-30 22:48:10 +0300

    feat: copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
+const source = "./src/fs/files";
+const destination = "./src/fs/files_copy";
+
+ 
+// fs.cp(src, dest[, options], callback) = Asynchronously copies the entire directory structure from src to dest, including subdirectories and files.
+// throws error if source don't exist by default
+// error on exist throws error if destination exists
+
 const copy = async () => {
-    // Write your code here 
-};
+        fs.cp(source, destination, {force:false, errorOnExist:true, recursive:true }, (err) => {
+            if (err) {
+                console.log("FS operation failed", err)
+            } else {
+                console.log("Copied")
+            }
+        } )
+    
+    }
+
+
 
 await copy();
