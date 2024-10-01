ef098cb70c4bb91eb00c13a09f90e770db56d825
Zrrg <hiroaritillwhen@gmail.com>
2024-10-01 23:41:34 +0300

    feat:  args and hash functions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,24 @@
 const parseArgs = () => {
-    // Write your code here 
+    const parsedArgs = process.argv.slice(2);
+    const output = {};
+
+    for (let i = 0; i < parsedArgs.length; i += 2) {
+        const key = parsedArgs[i].replace("--", "");
+        const value = parsedArgs[i + 1];
+        output[key] = value;
+        
+    }
+
+    // for (const [key, value] of Object.entries(output)) {
+    //     console.log(`${key} is ${value}`);
+    // }
+
+
+    const result = Object.entries(output)
+                         .map(([key, value]) => `${key} is ${value}`);
+    console.log(result.join(', '));
+
+    
 };
 
 parseArgs();
