43c0349c7d19a0dce7a0e0242801ebee84c13f5b
castetus <kboikov@mail.ru>
2024-10-01 12:57:45 +0200

    args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,21 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv;
+  let resultString = '';
+
+  args.forEach((arg, index) => {
+    if (!arg.startsWith('--')) {
+      return;
+    }
+    const key = arg.slice(2);
+    const value = args[index + 1];
+    resultString += `${key} is ${value}, `;
+  });
+
+  if (resultString.endsWith(', ')) {
+    resultString = resultString.slice(0, -2);
+  }
+
+  console.log(resultString);
 };
 
 parseArgs();
--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import { getFilesFolderPath } from "../utils.js";
+import { pipeline } from 'node:stream/promises';
+import { createReadStream } from 'node:fs';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const fileName = 'fileToCalculateHashFor.txt';
+  const filePath = `${getFilesFolderPath('hash')}/${fileName}`;
+  const { createHash } = await import('node:crypto');
+
+  await pipeline(
+    createReadStream(filePath),
+    createHash('SHA256').setEncoding('hex'),
+    process.stdout
+  );
+  console.log('ok');
 };
 
 await calculateHash();
