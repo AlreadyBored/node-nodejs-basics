f6961868f36268f85a6074235031ff17fa7b09c0
Zrrg <hiroaritillwhen@gmail.com>
2024-10-01 23:41:34 +0300

    Implemented args and hash functions.

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
+import crypto from 'crypto';
+const file = "src/hash/files/fileToCalculateHashFor.txt"
+
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = crypto.createHash('sha256');
+    const readStream = fs.createReadStream(file);
+        readStream.on('error', (err) => {
+            console.error('Crypto operation failed ', err);
+        })
+
+        readStream.on('data', (chunk) => {
+            hash.update(chunk);
+        })
+
+        readStream.on('end', () => {
+            const hashHex = hash.digest('hex');
+            console.log(hashHex);
+        })
 };
 
 await calculateHash();
