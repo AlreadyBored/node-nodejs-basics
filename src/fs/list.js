22eadf0858deedcd11b80d39c4ba7f4d1689e75a
castetus <kboikov@mail.ru>
2024-10-01 09:50:18 +0200

    read

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,4 @@
 import { checkIfPathExists, errorText, getFilesList, getFilesFolderPath } from "./utils.js";
-import * as fsPromises from "node:fs/promises";
 
 const list = async () => {
   if (!checkIfPathExists(getFilesFolderPath())) {
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import { checkIfPathExists, errorText, getFilesFolderPath } from "./utils.js";
+import * as fsPromises from "node:fs/promises";
+
 const read = async () => {
-    // Write your code here 
+  const fileToReadName = 'fileToRead.txt';
+  const fileToReadPath = `${getFilesFolderPath()}/${fileToReadName}`;
+
+  if (!checkIfPathExists(fileToReadPath)) {
+    throw new Error(errorText);
+  }
+
+  const fileContent = await fsPromises.readFile(fileToReadPath);
+  console.log(fileContent.toString());
 };
 
 await read();
