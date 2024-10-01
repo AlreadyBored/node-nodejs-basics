0b839f86dedf3584ab38fe2caf75035fdcd3c087
castetus <kboikov@mail.ru>
2024-10-01 08:51:42 +0200

    create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import * as fsPromises from "node:fs/promises";
+import { getCurrentPath, checkIfFileExists, filesFolder, errorText } from "./utils.js";
+
 const create = async () => {
-    // Write your code here 
+  const fileName = 'fresh.txt';
+  const fileContent = 'I am fresh and young';
+  
+  const currentUrl = getCurrentPath();
+  const fullFilePath = `${currentUrl}${filesFolder}/${fileName}`;
+  
+  if (checkIfFileExists(fullFilePath)) {
+    throw new Error(errorText);
+  }
+
+  await fsPromises.writeFile(fullFilePath, fileContent);
+
 };
 
 await create();
---
+++ src/fs/utils.js
@@ -0,0 +1,15 @@
+import * as path from "node:path";
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+export const filesFolder = '/files';
+export const errorText = 'FS operation failed';
+
+export const getCurrentPath = () => {
+  const __filename = fileURLToPath(import.meta.url);
+  return path.dirname(__filename);
+};
+
+export const checkIfFileExists = (filePath) => {
+  return fs.existsSync(filePath);
+};
