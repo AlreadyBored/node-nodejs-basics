6c220ff930c87e94c9da8c88a9cec5ef98710e38
castetus <kboikov@mail.ru>
2024-10-01 09:29:48 +0200

    copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,27 @@
+import { getCurrentPath, checkIfFileExists, filesFolder, errorText } from "./utils.js";
+import * as fsPromises from "node:fs/promises";
+
 const copy = async () => {
-    // Write your code here 
+  const newFolderName = '/files_copy';
+  const oldFolderPath = getCurrentPath() + filesFolder;
+  const newFolderPath = getCurrentPath() + newFolderName;
+
+  if (checkIfFileExists(newFolderPath) || !checkIfFileExists(oldFolderPath)) {
+    throw new Error(errorText);
+  }
+
+  await fsPromises.mkdir(newFolderPath);
+
+  const copyFile = async (fileName) => {
+    const fileContent = await fsPromises.readFile(`${oldFolderPath}/${fileName}`);
+    await fsPromises.writeFile(`${newFolderPath}/${fileName}`, fileContent);
+  };
+
+  const filesArray = await fsPromises.readdir(oldFolderPath);
+
+  for (let file of filesArray) {
+    await copyFile(file);
+  }
 };
 
 await copy();
