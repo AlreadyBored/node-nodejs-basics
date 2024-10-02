87f28397dcffcf72216cedad84993b33c730c652
Asadbek <asadbekjon8899@gmail.com>
2024-10-02 23:57:18 +0500

    feat: create.js task finish

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
+import fs from "node:fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const content = "I am fresh and young";
+
 const create = async () => {
-    // Write your code here 
+  const filePath = `${__dirname}/files/fresh.txt`;
+  try {
+    await fs.promises.writeFile(filePath, content, {
+      flag: "wx",
+    });
+  } catch (error) {
+    if (error.code === "EEXIST") {
+      console.log("FS operation failed");
+    }
+  }
 };
 
 await create();
