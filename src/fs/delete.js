7caba09df89c2440302e573ee9e6bd46452a44ea
spankov <spankov@ikod.kz>
2024-10-02 19:13:54 +0500

    feat: implemented delete solution

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,24 @@
+import { access, rm, constants } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+
 const remove = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+	const __dirname = dirname(__filename);
+    
+    const directory = join(__dirname, 'files');
+    const fileName = 'fileToRemove.txt';
+    const filePath = join(directory, fileName);
+
+    try {
+        await access(filePath, constants.F_OK);
+
+        await rm(filePath);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        }
+    }
 };
 
 await remove();
