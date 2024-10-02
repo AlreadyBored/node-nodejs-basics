0edb7c34ca8173b90f7dc73102137dda3e689f84
giorgi khoshtaria <99177635+g1org11@users.noreply.github.com>
2024-10-02 16:21:34 +0400

    add cli

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
 const parseEnv = () => {
-    // Write your code here 
+  const envVars = [];
+  for (const [key, value] of Object.entries(process.env)) {
+    if (key.startsWith("RSS_")) {
+      envVars.push(`${key}=${value}`);
+    }
+  }
+  console.log(envVars.join(", "));
 };
 
 parseEnv();
