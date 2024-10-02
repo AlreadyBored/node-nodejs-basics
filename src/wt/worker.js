b31d928e981eac36b3f0acd14691a1989ca03ed6
giorgi khoshtaria <99177635+g1org11@users.noreply.github.com>
2024-10-02 22:13:06 +0400

    add modules functionality

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,15 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { parentPort } from "worker_threads";
 
-const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+
+const sendResult = (result) => {
+  // Send result of nthFibonacci computations to main thread
+  parentPort.postMessage(result);
 };
 
-sendResult();
+// Listen for messages from the main thread
+parentPort.on("message", (n) => {
+  const result = nthFibonacci(n);
+  sendResult(result);
+});
