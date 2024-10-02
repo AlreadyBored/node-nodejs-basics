28f60ac44f31e3c94db2fbb675cd6eaf589391ae
Anastasiia Perova <perovaa.24@gmail.com>
2024-10-02 18:12:07 +0400

    init: start nodejs_basics task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,12 @@
+import fs from "node:fs/promises";
+
 const create = async () => {
-    // Write your code here 
+  try {
+    const content = "I am fresh and young";
+    await fs.writeFile("src/fs/files/fresh.txt", content, { flag: "wx+" });
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await create();
