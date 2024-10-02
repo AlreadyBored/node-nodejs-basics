90964d7bb21d9e16c8ef07a5c80ebf3c7b5909de
galinavikst <galinavikst@gmail.com>
2024-10-02 17:40:39 +0300

    feat: add cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
 const parseArgs = () => {
-    // Write your code here 
+  // Write your code here
+  const argv = process.argv.slice(2);
+
+  for (let i = 0; i < argv.length; i++) {
+    if (argv[i].startsWith("--")) console.log(`${argv[i]} is ${argv[i + 1]}`);
+  }
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,8 @@
 const parseEnv = () => {
-    // Write your code here 
+  // Write your code here
+  for (const key in process.env) {
+    if (key.startsWith("RSS_")) console.log(`${key}=${process.env[key]}`);
+  }
 };
 
 parseEnv();
