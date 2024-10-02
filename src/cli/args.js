605733535707b12c406a0b290d06f99cb583a68b
Andrei Bernatski <ABernatskiy@alfabank.ru>
2024-10-02 14:59:03 +0300

    feat: resolve cli section

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
+const VALID_ENV_PREFIX = '--';
+
 const parseArgs = () => {
-    // Write your code here 
+  const argsParts = process.argv.reduce((result, currentEnvValue, index, arr) => {
+    if (currentEnvValue.startsWith(VALID_ENV_PREFIX)) {
+      const envData = `${currentEnvValue.replace(VALID_ENV_PREFIX, '')} is ${arr[index + 1]}`;
+      return [...result, envData];
+    }
+
+    return result;
+  }, []);
+
+  const result = argsParts.join(', ');
+  console.log(result);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envs = Object.entries(process.env)
+    .reduce((result, [key, value]) => (key.startsWith('RSS_') ? [...result, `${key}=${value}`] : result), [])
+    .join('; ');
+
+  console.log(envs);
 };
 
 parseEnv();
