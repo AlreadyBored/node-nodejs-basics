eb3d4cf7303bed3cb449f588634643afc8dac732
giorgi khoshtaria <99177635+g1org11@users.noreply.github.com>
2024-10-02 16:48:06 +0400

    add hash functionality

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
+import fs from "fs";
+import crypto from "crypto";
+import path from "path";
+
+const __dirname = path.resolve();
+
 const calculateHash = async () => {
-    // Write your code here 
+  // Write your code here
+  const filePath = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+  const hash = crypto.createHash("sha256");
+
+  const fileStream = fs.createReadStream(filePath);
+
+  fileStream.on("data", (chunk) => {
+    hash.update(chunk);
+  });
+
+  fileStream.on("end", () => {
+    const result = hash.digest("hex");
+    console.log(result);
+  });
+
+  fileStream.on("error", (error) => {
+    console.error("Error reading file:", error);
+  });
 };
 
 await calculateHash();
