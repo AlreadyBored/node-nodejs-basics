065e9793c17cbacd645201416f61a9e6bb80afb9
Andrei Bernatski <ABernatskiy@alfabank.ru>
2024-10-02 15:33:22 +0300

    feat: resolve hash section

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import path from 'path';
+import fs from 'fs/promises';
+import { createHash } from 'crypto';
+
+const filePath = path.resolve('src', 'hash', 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+  const fileUrl = new URL(filePath, import.meta.url);
+
+  const fileData = await fs.readFile(fileUrl);
+  const hashData = createHash('sha256').update(fileData);
+
+  console.log(hashData.digest('hex'));
 };
 
 await calculateHash();
