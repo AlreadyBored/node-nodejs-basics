3101e913323545e8098ef334ab6534508a843bee
bslevchenko <bslevchenko@sberbank.ru>
2024-10-03 15:36:45 +0300

    add function list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
+//  implement function that prints all array of filenames from files folder 
+// into console (if files folder doesn't exists Error with message FS operation failed must be thrown)
+import { access, readdir } from 'fs/promises'
+
 const list = async () => {
     // Write your code here 
+    try {
+        await access('files')
+        const files = await readdir('files');
+        for (const file of files) console.log(file);
+    } catch(err) {
+        throw new Error('FS operation failed')
+    }
 };
 
 await list();
