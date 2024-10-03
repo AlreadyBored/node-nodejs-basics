fa5c85a2134e0878f4bd252e2e8a38da0be3e494
spankov <spankov@ikod.kz>
2024-10-03 10:41:58 +0500

    feat: implemented read in streams solution

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { createReadStream } from 'node:fs';
+import { stdout } from 'node:process';
+
 const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const filePath = join(__dirname, 'files/fileToRead.txt');
+
+    const stream = createReadStream(filePath, { encoding: 'utf-8' });
+
+    stream.pipe(stdout);
+
+    stream.on('end', () => {
+        stdout.write('\nFinished reading file.\n');
+    });
 };
 
 await read();
