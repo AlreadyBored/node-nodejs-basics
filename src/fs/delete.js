5905130d151b741feedbcad2d4575c82e2c46d86
bslevchenko <bslevchenko@sberbank.ru>
2024-10-03 14:42:15 +0300

    add function remove

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,16 @@
+
+ // implement function that deletes file fileToRemove.txt 
+ // (if there's no file fileToRemove.txt
+ // Error with message FS operation failed must be thrown)
+
+import { access, rm } from 'fs/promises'
 const remove = async () => {
-    // Write your code here 
+    try {
+        await access('files/fileToRemove.txt')
+        await rm('files/fileToRemove.txt')
+    } catch(err) {
+        throw new Error('FS operation failed')
+    }
 };
 
 await remove();
