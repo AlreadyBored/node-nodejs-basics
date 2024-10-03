c9b32f297f509393703acfc9cb97312bf2f0e261
Anastasiia Perova <perovaa.24@gmail.com>
2024-10-03 16:14:24 +0400

    feat: create rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,20 @@
+import fs from "node:fs/promises";
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    await fs.access("src/fs/files/wrongFilename.txt", fs.constants.F_OK);
+    try {
+      await fs.access("src/fs/files/properFilename.md", fs.constants.F_OK);
+      throw new Error();
+    } catch {
+      await fs.rename(
+        "src/fs/files/wrongFilename.txt",
+        "src/fs/files/properFilename.md"
+      );
+    }
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await rename();
