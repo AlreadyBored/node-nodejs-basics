5cbc891d5683d585bad9f1fb822675c7042d28ce
spankov <spankov@ikod.kz>
2024-10-03 08:59:43 +0500

    feat: implemented list solution

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,26 @@
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { readdir } from 'node:fs/promises';
+
 const list = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const directory = join(__dirname, 'files');
+
+    try {
+        const files = await readdir(directory);
+
+        for (const file of files) {
+            console.log(file);
+        }
+    } catch(error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        }
+
+        throw error;
+    }
 };
 
 await list();
