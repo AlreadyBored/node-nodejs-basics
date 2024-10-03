3ba39ba0b52f97a69e77d605a2be7befabe34dce
Zrrg <hiroaritillwhen@gmail.com>
2024-10-03 21:36:34 +0300

    implemented streams and zip

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,21 @@
+import { Transform } from 'stream';
+import { pipeline } from 'stream/promises';
+
 const transform = async () => {
-    // Write your code here 
+    class ReverseTransform extends Transform {
+        _transform(chunk, encoding, callback) {
+            const reversed = chunk.toString().split('').reverse().join('');
+            this.push(reversed);
+            callback();
+        }
+    }
+    const reverseTransform = new ReverseTransform();
+
+    await pipeline(
+        process.stdin,
+        reverseTransform,
+        process.stdout
+    );
 };
 
 await transform();
