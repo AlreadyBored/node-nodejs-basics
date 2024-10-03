b8570d86e92d3cfa60ed916c81afe4e57491ca17
bslevchenko <bslevchenko@sberbank.ru>
2024-10-03 16:00:40 +0300

    add function read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,15 @@
+// implement function that prints content of the fileToRead.txt 
+// into console (if there's no file fileToRead.txt 
+// Error with message FS operation failed must be thrown)
+import { access, readFile } from 'fs/promises'
+
 const read = async () => {
-    // Write your code here 
+    try {
+        await access('files/fileToRead.txt')
+        await readFile('files/fileToRead.txt', 'utf8').then((data) => console.log(data))
+    } catch(err) {
+        throw new Error('FS operation failed')
+    }
 };
 
 await read();
