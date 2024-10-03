d66404b3ca883d7bef365c86c49adfdf505fa0a2
spankov <spankov@ikod.kz>
2024-10-03 09:18:08 +0500

    feat: implemented args solution

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
+import * as process from 'node:process';
+
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    const results = {};
+
+    for (let i = 0; i < args.length; i += 2) {
+        const key = args[i].replace('--', '');
+        const value = args[i + 1];
+
+        results[key] = value;
+    }
+
+    Object.entries(results).forEach(([key, value]) => console.log(`${key} is ${value}`));
 };
 
 parseArgs();
