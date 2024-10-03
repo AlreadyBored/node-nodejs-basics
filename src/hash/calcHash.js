0c1d3c5615c75e61adc49e858c65eebd2ea683b1
spankov <spankov@ikod.kz>
2024-10-03 10:18:08 +0500

    feat: implemented hash solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import { createHash } from 'node:crypto';
+import { createReadStream } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+import { join } from 'node:path';
+import { log } from 'node:console';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const filePath = join(__dirname, 'files/fileToCalculateHashFor.txt');
+
+    const hash = createHash('sha256');
+    const stream = createReadStream(filePath);
+
+    stream.on('data', (chunk) => {
+        hash.update(chunk);
+    });
+
+    stream.on('end', () => {
+        console.log(hash.digest('hex'));
+    });
 };
 
 await calculateHash();
