9256e0eadbc8dd9d02cb2bc4e4eca174e1fb57d1
castetus <kboikov@mail.ru>
2024-10-03 08:33:13 +0200

    compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,23 @@
+import { getFilesFolderPath } from "../utils.js";
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream/promises';
+
 const compress = async () => {
-    // Write your code here 
+  const sourceName = 'fileToCompress.txt';
+  const destinationName = 'archive.gz';
+  const sourcePath = `${getFilesFolderPath('zip')}/${sourceName}`;
+  const destinationPath = `${getFilesFolderPath('zip')}/${destinationName}`;
+
+  const readStream = createReadStream(sourcePath);
+  const compressStream = createGzip();
+  const writeStream = createWriteStream(destinationPath);
+
+  await pipeline(
+    readStream,
+    compressStream,
+    writeStream,
+  );
 };
 
 await compress();
