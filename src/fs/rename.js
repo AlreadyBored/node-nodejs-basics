2182e55462afaa0410ad578b0d65e129af900416
bslevchenko <bslevchenko@sberbank.ru>
2024-10-03 14:35:10 +0300

    add function rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,24 @@
+// implement function that renames file wrongFilename.txt 
+// to properFilename with extension .md 
+// (if there's no file wrongFilename.txt or 
+// properFilename.md already exists Error with message FS operation failed must be thrown)
+import { access, rename as renameAsync } from 'fs/promises';
+
 const rename = async () => {
-    // Write your code here 
+    try {
+        await access('files/wrongFilename.txt').catch(() => {
+            throw new Error('FS operation failed')
+        })
+        await access('files/properFilename.md').then(() => {
+            throw new Error('FS operation failed')
+        })
+    } catch(err) {
+        if (err.code === 'ENOENT') {
+            await renameAsync('files/wrongFilename.txt', 'files/properFilename.md')
+        } else {
+            throw new Error('FS operation failed')
+        }
+    }
 };
 
 await rename();
