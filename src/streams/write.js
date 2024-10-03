8754036cd6a45d5e9805985264e1353b1bb82f4c
spankov <spankov@ikod.kz>
2024-10-03 10:46:48 +0500

    feat: implemented write in streams solution

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { createWriteStream } from 'node:fs';
+import { stdin, stdout } from 'node:process';
+
 const write = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const filePath = join(__dirname, 'files/fileToWrite.txt');
+
+    const stream = createWriteStream(filePath, { encoding: 'utf-8' });
+
+    stdin.pipe(stream);
+
+    stdout.write('Please write something\n');
 };
 
 await write();
