e55b057491fca5232a0e133f440f1be9f2dfcc80
Asadbek <asadbekjon8899@gmail.com>
2024-10-03 18:20:20 +0500

    feat: env.js task finish

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,16 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envVariables = process.env;
+
+  //   console.log(process.env.SOME);
+  //   const test = Object.entries(envVariables);
+  //   console.log(test);
+
+  const filtrVariables = Object.entries(envVariables)
+    .filter(([key, value]) => key.startsWith("RSS_"))
+    .map(([key, value]) => `${key}=${value}`)
+    .join("; ");
+
+  console.log(filtrVariables);
 };
 
 parseEnv();
