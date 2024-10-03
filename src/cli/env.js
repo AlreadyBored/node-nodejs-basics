4584e064577dea39f620d7b56d0db6e1a40a81e6
bslevchenko <bslevchenko@sberbank.ru>
2024-10-03 17:00:11 +0300

    add parse env function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
+
+// implement function that parses environment variables with prefix 
+// RSS_ and prints them to the console in the format RSS_name1=value1; RSS_name2=value2
+import { env } from 'process'
+
 const parseEnv = () => {
-    // Write your code here 
+    const keysEnv = Object.entries(env)
+    keysEnv.forEach((item, i) => {
+        if(item[0].startsWith('RSS_')) {
+            console.log(`${item[0]}${i}=${item[1]}; `)
+        }
+    })
 };
 
 parseEnv();
