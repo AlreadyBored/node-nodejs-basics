a28010d29fc508503ee9fa6e169e0aad10446fb3
Asadbek <asadbekjon8899@gmail.com>
2024-10-03 22:01:33 +0500

    feat: write.js task finish in streams

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,36 @@
+import { createWriteStream } from "fs";
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+  const fileToWrite = `${__dirname}/files/fileToWrite.txt`;
+
+  try {
+    const writeStream = createWriteStream(fileToWrite, { encoding: "utf8" });
+
+    console.log("Write something and press Enter:");
+
+    process.stdin.on("data", (data) => {
+      writeStream.write(data);
+      if (data.toString().includes("\n")) {
+        process.stdin.end();
+        writeStream.end();
+      }
+    });
+
+    writeStream.on("finish", () => {
+      console.log("Good Job : Plese press Ctrl + C ");
+    });
+
+    writeStream.on("error", (err) => {
+      console.error("Error while reading", err.message);
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await write();
