20a0e004a05d0b6575b2d7949747d3f28bbaeae3
castetus <kboikov@mail.ru>
2024-10-03 10:39:11 +0200

    streams:read

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -5,14 +5,18 @@ import { createReadStream } from 'node:fs';
 const calculateHash = async () => {
   const fileName = 'fileToCalculateHashFor.txt';
   const filePath = `${getFilesFolderPath('hash')}/${fileName}`;
+  const readStream = createReadStream(filePath);
   const { createHash } = await import('node:crypto');
 
-  await pipeline(
-    createReadStream(filePath),
-    createHash('SHA256').setEncoding('hex'),
-    process.stdout
-  );
-  console.log('ok');
-};
+  // await pipeline(
+  //   readStream,
+  //   createHash('SHA256').setEncoding('hex'),
+  //   process.stdout
+  // );
+
+  readStream.on('end', () => process.stdout.write('/n'));
+
+  readStream.pipe(process.stdout)
+}   
 
 await calculateHash();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,9 +1,7 @@
 import { createReadStream, createWriteStream } from 'fs';
 
 const transform = async () => {
-  // const readStream = createReadStream(process.stdin);
   
-  process.stdin.pipe(process.stdout)
 };
 
 await transform();
