1f5256841576281db6230637f6c755ac796367fb
bslevchenko <bslevchenko@sberbank.ru>
2024-10-03 14:20:25 +0300

    add function copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,21 @@
+import { access, cp, mkdir } from 'fs/promises'
+
 const copy = async () => {
-    // Write your code here 
+    try{
+        await access('files').catch(() => {
+            throw new Error('FS operation failed')
+        })
+        await access('files_copy').then(() => {
+            throw new Error('FS operation failed')
+        })
+    } catch(err) {
+        if (err.code === 'ENOENT') {
+            await mkdir('files_copy')
+            await cp('files', 'files_copy', { recursive: true })
+        } else {
+            throw new Error('FS operation failed')
+        }
+    }
 };
 
 await copy();
