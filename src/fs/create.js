ba42b81654405c7c07f0a10d3a3eb4bbaf7d998e
bslevchenko <bslevchenko@sberbank.ru>
2024-10-03 11:58:42 +0300

    add fn create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import { writeFile } from 'fs/promises'
+import { existsSync } from 'fs'
+import { resolve } from 'path'
+
+
 const create = async () => {
-    // Write your code here 
+    try { 
+        const text = 'I am fresh and young'
+        if(existsSync('files/fresh.txt')){
+            throw Error('FS operation failed')
+        }
+        await writeFile(
+            resolve(process.env.PWD, 'files', 'fresh.txt'), 
+            text, 
+            'utf8'
+        )
+    } catch(err) {
+        throw err
+    }
 };
 
 await create();
