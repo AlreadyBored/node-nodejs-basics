ba17f13f78ccc3e90b8f1b12066be8c9ebb6ca4a
spankov <spankov@ikod.kz>
2024-10-03 10:55:48 +0500

    feat: implemented transform solution

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,19 @@
+import { stdin, stdout } from 'node:process';
+import { Transform } from 'node:stream';
+
 const transform = async () => {
-    // Write your code here 
+    const myTransform = new Transform({
+        writableObjectMode: false,
+        transform(chunk, encoding, callback) {
+            this.push(`${chunk.toString().split('').reverse().join('')}\n`);
+
+            callback();
+        }
+    });
+
+    myTransform.setEncoding('utf-8');
+
+    stdin.pipe(myTransform).pipe(stdout);
 };
 
 await transform();
