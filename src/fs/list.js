7acb1dbec089928a89883745968825b34cbf3eb4
Asadbek <asadbekjon8899@gmail.com>
2024-10-03 12:08:54 +0500

    feat: list.js task finish

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,22 @@
+import fs from "node:fs";
+import path from "node:path";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+  const folderToPath = `${__dirname}/files`;
+  try {
+    const files = fs.readdirSync(folderToPath);
+
+    files.forEach((fileName) => {
+      console.log(fileName);
+    });
+  } catch (error) {
+    console.error("FS operation failed", error.message);
+  }
 };
 
 await list();
