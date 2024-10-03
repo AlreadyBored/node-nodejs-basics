84b33c98bc6bbacb740863192a2b10e6b06b05d9
Asadbek <asadbekjon8899@gmail.com>
2024-10-03 22:00:26 +0500

    feat: read.js task finish in streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,26 @@
+import { createReadStream } from "fs";
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
 const read = async () => {
-    // Write your code here 
+  const fileToRead = `${__dirname}/files/fileToRead.txt`;
+  try {
+    const readStream = createReadStream(fileToRead, { encoding: "utf8" });
+
+    readStream.pipe(process.stdout);
+    readStream.on("error", (err) => {
+      console.error("Error while reading file", err.message);
+    });
+    readStream.on("end", () => {
+      console.log("\nFile reading finished ");
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await read();
