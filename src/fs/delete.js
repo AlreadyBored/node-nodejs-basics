a8ecad6123d6993b01a21577d4a4c9815ab8e823
Asadbek <asadbekjon8899@gmail.com>
2024-10-03 08:38:44 +0500

    feat: delete.js finish

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import { unlink } from "node:fs/promises";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+  const fileToRemove = "fileToRemove.txt";
+  const fileToRemoved = `${__dirname}/files/${fileToRemove}`;
+
+  try {
+    await unlink(fileToRemoved);
+  } catch (error) {
+    console.error("FS operation failed");
+  }
 };
 
 await remove();
