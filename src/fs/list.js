5df447cbbc6b0ecd2c10f8ff602bc82c633d5508
Anastasiia Perova <perovaa.24@gmail.com>
2024-10-03 16:48:30 +0400

    feat: create list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,12 @@
+import fs from "node:fs/promises";
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const files = await fs.readdir("src/fs/files");
+    for (const file of files) console.log(file);
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await list();
