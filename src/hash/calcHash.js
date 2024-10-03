7be7d4a756df1b287b8977ceec0aebfe8d04c554
Asadbek <asadbekjon8899@gmail.com>
2024-10-03 22:50:57 +0500

    feat: calcHash.js task finish from hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,31 @@
+import { createReadStream } from "fs";
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
+import crypto from "node:crypto";
+import { Buffer } from "node:buffer";
+import { promisify } from "util";
+import { pipeline } from "stream";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const pipelineAsync = promisify(pipeline);
+
 const calculateHash = async () => {
-    // Write your code here 
+  // 1 implement function that makes plain text to SHA 256
+  const filePath = `${__dirname}/files/fileToCalculateHashFor.txt`;
+  const hash = crypto.createHash("sha256");
+
+  // 2 log it in  hex  with stream api
+  try {
+    await pipelineAsync(createReadStream(filePath), hash.setEncoding("hex"));
+
+    hash.setEncoding("hex");
+
+    console.log(hash.read());
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await calculateHash();
