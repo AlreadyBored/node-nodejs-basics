9481eb8c2147730e69634ce9609ee8a1f636066d
kaliganoff <kaliganoff@yahoo.com>
2024-10-04 15:45:31 +0300

    feat: Add copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,58 @@
+import * as fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+    if (fs.existsSync(path.join(__dirname, 'files'))) {
+        await copyDir();
+    } else {
+        console.error('FS operation failed');
+    }
 };
 
 await copy();
+
+async function copyDir() {
+    try {
+        fs.access(path.join(__dirname, 'files-copy'), (error) => {
+            if (error && error.code !== 'ENOENT') {
+                console.log(error);
+            } else if (error && error.code === 'ENOENT') {
+                makeDir().then(() => {
+                    fs.readdir(path.join(__dirname, 'files'), (error, files) => {
+                        if (error) 
+                            console.log(error); 
+                        else {
+                            for (let file of files) {
+                                fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), (error) => {
+                                    if (error) {
+                                        console.log(error);
+                                    }
+                                });
+                            }
+                        }
+                    });
+                });
+            } else {
+                console.error('FS operation failed');
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function makeDir() {
+    return new Promise((resolve, reject) => {
+        fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (error) => {
+            if (error) {
+                console.log(error);
+                return reject(error);
+            }
+            resolve();
+        });
+    });
+}
