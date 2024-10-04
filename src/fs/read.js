3cdc6b721d39557b71a702f61bad9b9d5ac484d7
Ekaterina Prianishnikova <li.liliana@yandex.ru>
2024-10-04 20:06:48 +0400

    fs: add read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,14 @@
+import fs from 'fs';
+
 const read = async () => {
-    // Write your code here 
+  const PATH = './files/fileToRead.txt';
+  fs.stat(PATH, function (err, stat) {
+    if (err === null) {
+      fs.readFile(PATH, 'utf-8', (err, data) => console.log(data));
+    } else if (err.code === 'ENOENT') {
+      throw new Error('FS operation failed');
+    }
+  }, () => { });
 };
 
 await read();
