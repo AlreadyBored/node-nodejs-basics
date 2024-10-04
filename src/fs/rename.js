09430d47b38b604eaa12e7ae2fd58ca811495af7
kaliganoff <kaliganoff@yahoo.com>
2024-10-04 19:21:46 +0300

    feat: Add rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,16 @@
+import fs from "fs"
+
 const rename = async () => {
-    // Write your code here 
+    const exists = fs.existsSync('src/fs/files/properFilename.md');
+    if (!exists) {
+    try {
+    await fs.promises.rename('src/fs/files/wrongFilename.txt', 'src/fs/files/properFilename.md')
+    } catch (error) {
+        console.error('FS operation failed')
+    }
+} else {
+    console.error('FS operation failed')
+}
 };
 
 await rename();
