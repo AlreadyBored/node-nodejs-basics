5ebd66a6c2b617967ed1066c9d6fc05aaab9c882
castetus <kboikov@mail.ru>
2024-10-04 12:28:14 +0200

    wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,45 @@
+import { Worker } from 'worker_threads';
+import { getCurrentPath } from '../utils.js';
+import { cpus } from 'os';
+
 const performCalculations = async () => {
-    // Write your code here
+  const workerPath = `${getCurrentPath('wt')}/worker.js`;
+  const initialNumber = 10;
+  const numCores = cpus().length;
+
+  const promises = [];
+
+  const handleWorker = async (num) => {
+    
+    const worker = new Worker(workerPath, { workerData: initialNumber + num });
+
+    const result = await new Promise((res, rej) => {
+      
+      worker.on('message', (data) => {
+        res({
+          status: 'resolved',
+          data,
+        });
+      });
+
+      worker.on('error', (error) => {
+        rej({
+          status: 'error',
+          data: null,
+        });
+      });
+    });
+    
+    return result;
+  };
+
+  for (let number = 0; number < numCores; number += 1) {
+    promises.push(handleWorker(number));
+  }
+
+  const result = await Promise.all(promises);
+
+  console.log(result);
 };
 
 await performCalculations();
