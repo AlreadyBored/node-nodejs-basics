d51c34d68f4d31ed45c13111cf7ed887b111d2bc
av-mukhortova <av.mukhortova@gmail.com>
2024-10-04 14:27:12 +0300

    feat: implement wt part

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,30 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { availableParallelism  } from 'os';
+import { Worker } from 'worker_threads';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const workerFile = path.join(__dirname, 'worker.js');
+
 const performCalculations = async () => {
-    // Write your code here
+    const numCores = availableParallelism();
+    const workers = [];
+
+    const createWorker = (data) => {
+        return new Promise((resolve) => {
+            const worker = new Worker(workerFile, {workerData: data});
+            worker.on('message', (data) => resolve({status: 'resolved', data}));
+            worker.on('error', () => resolve({status: 'error', data: null}));
+        });
+    };
+
+    for (let i = 0; i < numCores; i++) {
+        workers.push(createWorker(i + 10));
+    }
+
+    const result = await Promise.all(workers);
+    console.log(result);
 };
 
 await performCalculations();
