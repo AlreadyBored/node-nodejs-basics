4cc67f991ded463910e4f67155055ce0f2b531d8
av-mukhortova <av.mukhortova@gmail.com>
2024-10-04 13:27:40 +0300

    feat: implement cp part

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,17 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { fork } from 'child_process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const scriptFile = path.join(__dirname,'files','script.js');
+    const cp = fork(scriptFile,args);
+    cp.on('close', (code) => {
+        console.log(`Child process exited with code ${code}`);
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['a', 'b', 'c', 'd']);
