380122a7f27f348e6da585e01075b4f1598811cd
Ekaterina Prianishnikova <li.liliana@yandex.ru>
2024-10-04 19:58:24 +0400

    fs: add create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,14 @@
+import fs from 'fs';
+
 const create = async () => {
-    // Write your code here 
+  const PATH = './files/fresh.txt';
+  fs.stat(PATH, function (err, stat) {
+    if (err === null) {
+      throw new Error('FS operation failed')
+    } else if (err.code === 'ENOENT') {
+      fs.writeFile(PATH, 'I am fresh and young', () => { });
+    }
+  }, () => { });
 };
 
 await create();
