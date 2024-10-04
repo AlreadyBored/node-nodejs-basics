fa8a4ef7a349945e14643b0c8a711381c84ad1e0
Anastasiia Perova <perovaa.24@gmail.com>
2024-10-04 11:48:08 +0400

    feat: add function env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envs = process.env;
+  const result = [];
+
+  for (const [key, value] of Object.entries(envs)) {
+    if (key.startsWith("RSS_")) {
+      result.push(`${key}=${value};`);
+    }
+  }
+  console.log(result.join(" "));
 };
 
 parseEnv();
