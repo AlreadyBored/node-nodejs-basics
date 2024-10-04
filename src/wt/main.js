3e06a183d0dea2a77564ef25a87186a35d90ca98
Zrrg <hiroaritillwhen@gmail.com>
2024-10-04 07:31:06 +0300

    worker threads implemented

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,39 @@
+import { Worker, isMainThread } from 'node:worker_threads';
+import os from 'os';
+const workerscript = './src/wt/worker.js'
+
+const createWorkers = async (numWorkers) => {
+    const workers = [];
+    const resultsPromises = [];
+
+    for (let i = 0; i < numWorkers; i++) {
+        const worker = new Worker(workerscript);
+        workers.push(worker);
+
+        const workerData = 10 + i;  
+
+        const resultPromise = new Promise((resolve) => {
+            worker.on('message', (result) => {
+                resolve(result);
+            });
+            worker.postMessage(workerData);
+        });
+
+        resultsPromises.push(resultPromise);
+    }
+
+    // Wait for all workers to finish and return the results in order
+    return Promise.all(resultsPromises);
+};
+
 const performCalculations = async () => {
-    // Write your code here
+
+    if (isMainThread) {
+        const numCores = os.cpus().length;
+        const results = await createWorkers(numCores);
+
+        console.log('Results:', results);
+    }
 };
 
 await performCalculations();
