72d99743ac3ed5a40ff5af68d6dc289625aab814
FABIAN RAMIREZ <fabian.ramirez@nowports.com>
2024-10-04 00:24:09 -0500

    feat:implemented cli subtasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const parsedArgs = [];
+
+  for (let i = 0; i < args.length; i += 2) {
+    const propName = args[i].replace("--", "");
+    const value = args[i + 1];
+    parsedArgs.push(`${propName} is ${value}`);
+  }
+
+  console.log(parsedArgs.join(", "));
 };
 
 parseArgs();
