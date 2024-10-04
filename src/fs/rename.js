391f7e52f12c48ac18dda9aea7dd3400f231b75a
Ekaterina Prianishnikova <li.liliana@yandex.ru>
2024-10-04 20:13:04 +0400

    fs: add rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+
 const rename = async () => {
-    // Write your code here 
+  const PATH = './files/wrongFilename.txt';
+  const RENAME_PATH = './files/properFilename.md';
+  fs.stat(PATH, function (err, stat) {
+    if (err === null) {
+      fs.stat(RENAME_PATH, function (err, stat) {
+        if (err === null) {
+          throw new Error('FS operation failed');
+        } else if (err.code === 'ENOENT') {
+          fs.rename(PATH, RENAME_PATH, () => { });
+        }
+      });
+    } else if (err.code === 'ENOENT') {
+      throw new Error('FS operation failed');
+    }
+  }, () => { });
 };
 
 await rename();
