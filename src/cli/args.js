ed053f5671e448a9c52b39650767e8031a274861
“Anatoli <humanities5050@gmail.com>
2024-10-04 23:11:53 +0300

    feat: complete cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+  const parsedArg = process.argv.slice(2);
+  let result = [];
+  for (let i = 0; i < parsedArg.length; i += 2) {
+    const transformedName = parsedArg[i].replace("--", "");
+    const value = parsedArg[i + 1];
+    result.push(`${transformedName} is ${value}`);
+  }
+  console.log(result.join(", "));
 };
-
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+  let result = [];
+  for (const key in process.env) {
+    if (key.startsWith("RSS_")) {
+      result.push(`${key}=${process.env[key]}`);
+    }
+  }
+  console.log(result.join("; "));
 };
 
 parseEnv();
