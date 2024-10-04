0ab569f0dad1600644d00f91b23778762c28ef88
Anastasiia Perova <perovaa.24@gmail.com>
2024-10-04 12:36:36 +0400

    feat: add function args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+  const result = [];
+  for (let i = 0; i < process.argv.length; ++i) {
+    if (process.argv[i].startsWith("--")) {
+      result.push(`${process.argv[i]} is ${process.argv[i + 1]}`);
+    }
+  }
+  console.log(result.join(", "));
 };
 
 parseArgs();
