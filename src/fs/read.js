4a4d75711540d17cdeea2898bc5475218791f0dd
Zhenya Holub <zhenya.holub@rdigital.by>
2024-10-05 16:34:39 +0300

    feat: implement read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,14 @@
+import { readFile } from 'node:fs/promises';
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const file = await readFile('./files/fileToRead.txt', {
+            encoding: 'utf8'
+        });
+        console.log(file);
+    } catch {
+        console.log(new Error('FS operation failed'));
+    }
 };
 
 await read();
