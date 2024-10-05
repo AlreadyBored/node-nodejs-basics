da6d87b2d7dfa4fe4b12e165a1761d00a1defbc0
inessa <inessasmorgunova@gmail.com>
2024-10-04 20:14:25 -0400

    feat: add decision

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+    const arg = process.argv.slice(2);
+    for (let i = 0; i < arg.length; i = i + 2) {
+        if (arg[i].includes('--')) {
+            console.log(`${arg[i].replace('--', '')} is ${arg[i + 1]}`)
+        }
+    }
+
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+    const envVariables = process.env;
+    const rssVariables = Object.keys(envVariables)
+        .filter(key => key.startsWith('RSS_'))
+        .map(key => `${key}=${envVariables[key]}`)
+
+    console.log(rssVariables.join('; '))
 };
 
 parseEnv();
