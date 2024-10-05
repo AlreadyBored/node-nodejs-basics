9351ac37f32943219b810ec6122ceeccc3394946
Elena Pavlova <lenlho22@gmail.com>
2024-10-05 22:44:58 +0300

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,15 @@
+import fs from "fs/promises";
+
 const list = async () => {
-    // Write your code here 
+  const folderPath = "src/fs/files";
+
+  try {
+    await fs.access(folderPath, fs.constants.F_OK);
+    const files = await fs.readdir(folderPath);
+    console.log(files.join("\n"));
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await list();
