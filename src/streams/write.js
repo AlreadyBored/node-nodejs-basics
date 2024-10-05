0c12395262433506e340551a362e86d8f82748e6
bslevchenko <bslevchenko@sberbank.ru>
2024-10-05 19:30:51 +0300

    add funcction write file

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+// implement function that writes process.stdin data into file fileToWrite.txt 
+// content using Writable Stream
+
+import { open } from 'fs/promises'
+import { stdin } from 'process'
 const write = async () => {
-    // Write your code here 
+    try {
+        const fh = await open('files/fileToWrite.txt', 'w')
+        const ws = fh.createWriteStream()
+    
+        stdin.pipe(ws)
+    } catch(err) {
+        fh.close()
+        throw new Error(err)
+    }
 };
 
 await write();
