70c2d06ce8990e5b2221032a8029651e9145acdc
Elina <elina50505051@yandex.ru>
2024-10-05 19:28:51 +0300

    feat: implement functions (modules)

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,21 +1,25 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path from 'node:path'
+import { release, version } from 'node:os';
+import { createServer as createServerHttp } from 'node:http'
+import './files/c.js'
+import {fileURLToPath} from "node:url";
 
 const random = Math.random();
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+    unknownObject = await import('./files/a.json', { with: { type: 'json'}});
 } else {
-    unknownObject = require('./files/b.json');
+    unknownObject = await import('./files/b.json', { with: { type: 'json'}});
 }
 
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
 console.log(`Path segment separator is "${path.sep}"`);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 console.log(`Path to current file is ${__filename}`);
 console.log(`Path to current directory is ${__dirname}`);
@@ -33,7 +37,7 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
+export {
     unknownObject,
     myServer,
 };
