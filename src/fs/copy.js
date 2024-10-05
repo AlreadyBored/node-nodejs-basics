b4e8d2ad4aa51295c3b28c840fc2064387eee2d7
Ekaterina Prianishnikova <li.liliana@yandex.ru>
2024-10-05 15:56:01 +0400

    fs: add copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,28 @@
+import fs from 'fs';
+
 const copy = async () => {
-    // Write your code here 
+  const PATH = './files';
+  const COPY_PATH = './files_copy';
+
+  fs.stat(PATH, function (err, stat) {
+    if (err === null) {
+      fs.stat(COPY_PATH, function (err, stat) {
+        if (err === null) {
+          throw new Error('FS operation failed');
+        } else if (err.code === 'ENOENT') {
+          fs.mkdir(COPY_PATH, () => {
+            fs.readdir(PATH, (err, files) => {
+              files.forEach((file) => {
+                fs.copyFile(`${PATH}/${file}`, `${COPY_PATH}/${file}`, () => { });
+              })
+            });
+          });
+        }
+      }, () => { });
+    } else if (err.code === 'ENOENT') {
+      throw new Error('FS operation failed');
+    }
+  }, () => { });
 };
 
 await copy();
