c7dcd807dc7965676aade1a456a3ecac905ea41e
Ekaterina Prianishnikova <li.liliana@yandex.ru>
2024-10-05 16:16:35 +0400

    cli: parse env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+  let ans = '';
+  Object.entries(process.env).map(([key, value]) => {
+    if (key.startsWith('RSS_')) {
+      ans += `${key}=${value}; `;
+    }
+  });
+  console.log(ans.slice(0, ans.length - 2));
 };
 
 parseEnv();
