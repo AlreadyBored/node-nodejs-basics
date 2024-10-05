2b30869c06437c3bd5a4b7f2d86ad3f3da08e0b4
Zhenya Holub <zhenya.holub@rdigital.by>
2024-10-05 16:31:20 +0300

    feat: implement rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
+import * as fs from 'node:fs/promises';
+
 const rename = async () => {
-    // Write your code here 
+    const error = new Error('FS operation failed');
+
+    try {
+        await fs.readFile('./files/wrongFilename.txt');
+        fs.readFile('./files/properFilename.md')
+            .then(() => console.log(error))
+            .catch(() => {
+                fs.rename(
+                    './files/wrongFilename.txt',
+                    './files/properFilename.md'
+                );
+            });
+    } catch {
+        console.log(error);
+    }
 };
 
 await rename();
