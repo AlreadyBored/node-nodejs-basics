c02aa65f4315af810f8f02eb53f50744ca3aa012
Zrrg <hiroaritillwhen@gmail.com>
2024-10-05 20:31:27 +0300

    implemented child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,33 @@
+import {spawn} from 'child_process'
+const script = './src/cp/files/script.js'
+const input = 'Lorem ipsum dolor sit amet'
+//const input = 'CLOSE'
+
+
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+
+    if (!Array.isArray(args)) {
+        console.error('Incorrect arguments');
+        return;
+    }
+
+    const childProcess = spawn('node', [script, ...args], {
+        stdio: ['pipe', 'pipe', 'inherit'] // stdin and stdout for IPC
+    });
+
+    process.stdin.pipe(childProcess.stdin);
+    childProcess.stdout.pipe(process.stdout);
+  
+    childProcess.on('message', (msg) => {
+        console.log(`Message from child:  ${msg}`);
+    })
+
+    childProcess.stdin.write(input);
+
+
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+await spawnChildProcess(['arg1', 'arg2', 'arg3']);
+ 
