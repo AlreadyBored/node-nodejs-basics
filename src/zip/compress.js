ff7ce353c3de4e39cead0b093845c2e0e2c0c08b
Zhenya Holub <zhenya.holub@rdigital.by>
2024-10-05 16:49:19 +0300

    feat: implement compress function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,15 @@
+import { createGzip } from 'node:zlib';
+import { promisify } from 'node:util';
+import { pipeline } from 'node:stream';
+
+import { createReadStream, createWriteStream } from 'node:fs';
+
 const compress = async () => {
-    // Write your code here 
+    const pipe = promisify(pipeline);
+    const gzip = createGzip();
+    const source = createReadStream('./src/zip/files/fileToCompress.txt');
+    const destination = createWriteStream('./src/zip/files/archive.gz');
+    await pipe(source, gzip, destination);
 };
 
 await compress();
