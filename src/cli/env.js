07777c2346d3d7ab38975175dfc110595693ad50
kaliganoff <kaliganoff@yahoo.com>
2024-10-06 00:27:34 +0300

    feat: Add env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,5 @@
 const parseEnv = () => {
-    // Write your code here 
+    console.log(Object.entries(process.env).filter(entry => entry[0].includes('RSS_')).map(entry => `${entry[0]}=${entry[1]}`).join('; '))
 };
 
 parseEnv();
