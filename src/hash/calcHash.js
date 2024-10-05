368aea1a5bca1a2dd9987f613bf0e642cfd32950
Zhenya Holub <zhenya.holub@rdigital.by>
2024-10-05 16:43:50 +0300

    feat: implement calculateHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import { createReadStream } from 'node:fs';
+import { createHash } from 'node:crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const readableStream = createReadStream(
+        './src/hash/files/fileToCalculateHashFor.txt',
+        {
+            encoding: 'utf-8'
+        }
+    );
+    readableStream.on('data', (text) => {
+        console.log(createHash('sha256').update(text).digest('hex'));
+    });
 };
 
 await calculateHash();
