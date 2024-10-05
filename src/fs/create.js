0ffa92cfbcc291ad7024b016148b3c6bd8bc5ca2
Alexey Abramov <aal@renue.ru>
2024-10-05 14:33:16 +0400

    feat: implement create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import { access, writeFile } from 'fs/promises';
+import path from "path";
+
 const create = async () => {
-    // Write your code here 
+    const filePath = path.join(process.cwd(), 'src/fs/files', 'fresh.txt');
+
+    try {
+        await access(filePath);
+        throw new Error('FS operation failed');
+    } catch (error) {
+        if (error.code !== 'ENOENT') {
+            throw error;
+        }
+    }
+
+    await writeFile(filePath, 'I am fresh and young', 'utf8');
 };
 
 await create();
