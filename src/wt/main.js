0ac6b73b315449caa4fca1c159f5a88fda444fd5
Asadbek <asadbekjon8899@gmail.com>
2024-10-05 23:16:48 +0500

    feat: Worker Threads (src/wt) task finish

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,47 @@
+import { Worker } from "worker_threads";
+import os from "os";
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const performCalculations = async () => {
-    // Write your code here
+  const countCpuCore = os.cpus().length;
+  const results = new Array(countCpuCore).fill(null);
+
+  const createWorker = (index, number) => {
+    const pathToWorker = `${__dirname}/worker.js`;
+    return new Promise((resolve, reject) => {
+      const worker = new Worker(pathToWorker, { type: "module" });
+
+      worker.postMessage(number);
+
+      worker.on("message", (message) => {
+        resolve({ index, result: message });
+        worker.terminate();
+      });
+
+      worker.on("error", (error) => {
+        resolve({ index, result: { status: "error", data: null } });
+        worker.terminate();
+      });
+    });
+  };
+
+  const promises = [];
+
+  for (let i = 0; i < countCpuCore; i++) {
+    promises.push(createWorker(i, 10 + i));
+  }
+
+  const resultsArray = await Promise.all(promises);
+
+  resultsArray.forEach(({ index, result }) => {
+    results[index] = result;
+  });
+
+  console.log(results);
 };
 
-await performCalculations();
+performCalculations();
