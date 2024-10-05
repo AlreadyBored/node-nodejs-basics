64a8c7d600418ebe960bf8a7485c94d04374ce8b
bslevchenko <bslevchenko@sberbank.ru>
2024-10-05 21:40:23 +0300

    add function compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,26 @@
+
+//  implement function that compresses file fileToCompress.txt 
+// to archive.gz using zlib and Streams API
+import { createGzip } from 'zlib'
+import { open } from 'fs/promises'
+import { createWriteStream } from 'fs'
+import { pipeline } from 'stream/promises'
+
 const compress = async () => {
-    // Write your code here 
+    try {
+        const fh = await open('files/fileToCompress.txt')
+        const rs = fh.createReadStream()
+
+        const gzip = createGzip()
+
+        await pipeline(
+            rs,
+            gzip,
+            createWriteStream('archive.gz'),
+        )
+    } catch(err) {
+        throw new Error(err)
+    }
 };
 
 await compress();
