d80a5ce6092eb3e18a2608c29c78f1c4950569f9
bslevchenko <bslevchenko@sberbank.ru>
2024-10-05 21:07:18 +0300

    add transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,26 @@
+import { stdin, stdout } from 'process'
+import { pipeline, Transform } from 'stream'
+
+const transformText = new Transform({
+        transform: (chunck, encoding, callback) => {
+            const tranformData = `${chunck.reverse()} `
+            callback(null, tranformData)
+        }
+    })
+
 const transform = async () => {
-    // Write your code here 
+    await pipeline(
+        stdin,
+        transformText,        
+        stdout,
+        (err) => {
+            if (err) {
+                console.error('Pipeline failed:', err);
+            } else {
+                console.log('Pipeline succeeded.');
+            }
+        }
+    )
 };
 
 await transform();
