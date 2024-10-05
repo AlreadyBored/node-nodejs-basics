bfde286da89f0597eb1f16633d041d453b017f49
bslevchenko <bslevchenko@sberbank.ru>
2024-10-05 18:31:03 +0300

    add function calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+// implement function that calculates SHA256 hash 
+// for file fileToCalculateHashFor.txt and 
+// logs it into console as hex using Streams API
+
+// Попробовать использовать какой нибудь стрим
+
+import { access, readFile } from 'fs/promises'
+import { createHash } from 'crypto'
+
 const calculateHash = async () => {
-    // Write your code here 
+    try {
+        await access('files/fileToCalculateHashFor.txt')
+        const data = await readFile('files/fileToCalculateHashFor.txt')
+        const hash = createHash('sha256')
+        console.log(hash.update(data).digest('hex'))
+    } catch(e) {
+        throw new Error(e)
+    }
+
 };
 
 await calculateHash();
