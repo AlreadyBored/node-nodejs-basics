bbcfaaad9baee5311fd13a21788ae62e490c0a43
Elena Pavlova <lenlho22@gmail.com>
2024-10-05 22:56:31 +0300

    feat: implement read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,15 @@
+import fs from "fs/promises";
+
 const read = async () => {
-    // Write your code here 
+  const filePath = "src/fs/files/fileToRead.txt";
+
+  try {
+    await fs.access(filePath, fs.constants.F_OK);
+    const content = await fs.readFile(filePath, "utf8");
+    console.log(content);
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await read();
