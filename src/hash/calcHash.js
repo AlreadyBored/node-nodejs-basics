c47172b5943cebd86f0a01ef633c9eaf2b0247ce
FABIAN RAMIREZ <fabian.ramirez@nowports.com>
2024-10-04 21:51:18 -0500

    refactor:implemented hash subtask

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,8 +1,14 @@
 import { createHash } from "crypto";
 import { createReadStream } from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 
 const calculateHash = async () => {
-  const readStream = createReadStream("./files/fileToCalculateHashFor.txt");
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const filePath = join(__dirname, "files", "fileToCalculateHashFor.txt");
+
+  const readStream = createReadStream(filePath);
   const hash = createHash("sha256");
 
   readStream
