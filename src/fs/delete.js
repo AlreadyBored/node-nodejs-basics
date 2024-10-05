e4912c3638b3155fadf938b0625c375fd9c1dd85
Elena Pavlova <lenlho22@gmail.com>
2024-10-05 22:28:49 +0300

    refactor: exlude repeating file to remove path

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,9 +1,11 @@
 import fs from "fs/promises";
 
 const remove = async () => {
+  const fileToRemovePath = "src/fs/files/fileToRemove.txt";
+
   try {
-    await fs.access("src/fs/files/fileToRemove.txt", fs.constants.F_OK);
-    await fs.rm("src/fs/files/fileToRemove.txt");
+    await fs.access(fileToRemovePath, fs.constants.F_OK);
+    await fs.rm(fileToRemovePath);
   } catch (err) {
     throw new Error("FS operation failed");
   }
