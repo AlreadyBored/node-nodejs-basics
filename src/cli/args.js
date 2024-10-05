77f2f3a2b6ba89636d97276d78f8ad9d064217aa
Elina <elina50505051@yandex.ru>
2024-10-05 19:46:35 +0300

    feat: implement functions (cli)

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
+import process from "node:process";
+
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  args.forEach((arg, idx) => {
+    if (arg.startsWith('--')) {
+      console.log(`${arg} is ${args[idx + 1]}`);
+    }
+  })
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
+import process from "node:process";
+
 const parseEnv = () => {
-    // Write your code here 
+  const keys = Object.keys(process.env).filter((key) => key.startsWith("RSS_"));
+  keys.forEach((key) => {
+    console.log(`${key}=${process.env[key]}`);
+  })
 };
 
 parseEnv();
