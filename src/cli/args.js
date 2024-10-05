6c161a8e77e68eb064101541d45a10c4cca00d0b
kaliganoff <kaliganoff@yahoo.com>
2024-10-06 00:42:54 +0300

    feat: Add args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,5 @@
 const parseArgs = () => {
-    // Write your code here 
+    console.log(process.argv.slice(2).map(value => value.includes('--') ? value.replace('--', '') + ' is ' : value + ', ').join('').slice(0, -2))
 };
 
 parseArgs();
