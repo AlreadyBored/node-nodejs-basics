06f94d890574a66243050a10d5bfedcd519b6ed9
Zhenya Holub <zhenya.holub@rdigital.by>
2024-10-05 16:49:43 +0300

    feat: implement decompress function

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,14 @@
+import { createUnzip } from 'node:zlib';
+import { promisify } from 'node:util';
+import { pipeline } from 'node:stream';
+import { createReadStream, createWriteStream } from 'node:fs';
+
 const decompress = async () => {
-    // Write your code here 
+    const pipe = promisify(pipeline);
+    const gzip = createUnzip();
+    const source = createReadStream('./src/zip/files/archive.gz');
+    const destination = createWriteStream('./src/zip/files/fileToCompress.txt');
+    await pipe(source, gzip, destination);
 };
 
 await decompress();
