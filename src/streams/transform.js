47b03d0145415b6f29c5a30bbd16a2ef922603a4
Zhenya Holub <zhenya.holub@rdigital.by>
2024-10-05 16:47:50 +0300

    feat: implement transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,19 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+    const reverse = new Transform({
+        transform(chunk, encoding, callback) {
+            callback(null, chunk.toString().split('').reverse('').join(''));
+        }
+    });
+
+    reverse.on('data', (data) => {
+        process.stdout.write(data.toString());
+    });
+
+    process.stdin.on('data', (data) => {
+        reverse.write(data);
+    });
 };
 
 await transform();
