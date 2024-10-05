5747ff11560c34be7afbc9071eef6136331d3a4e
Asadbek <asadbekjon8899@gmail.com>
2024-10-05 14:00:45 +0500

    feat cjsToEsm.cjs task finish from modules

---
+++ src/modules/esm.mjs
@@ -0,0 +1,45 @@
+import path from "node:path";
+import { release, version } from "os";
+import { createServer } from "http";
+import { readFile } from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const random = Math.random();
+
+
+let unknownObject;
+
+if (random > 0.5) {
+  unknownObject = JSON.parse(
+    await readFile(`${__dirname}/files/a.json`, { encoding: "utf-8" })
+  );
+} else {
+  unknownObject = JSON.parse(
+    await readFile(`${__dirname}/files/b.json`, { encoding: "utf-8" })
+  );
+}
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${path.sep}"`);
+
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
+
+const myServer = createServer((req, res) => {
+  res.end("Request accepted");
+});
+
+const PORT = 3000;
+console.log(unknownObject);
+
+myServer.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+  console.log("To terminate it, use Ctrl+C combination");
+});
+
+export { unknownObject, myServer };
