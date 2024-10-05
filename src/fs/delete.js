a63f60e106ed7d6818b98c844e7af87e1160c196
Alexey Abramov <aal@renue.ru>
2024-10-05 17:32:49 +0400

    feat: implement delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,16 @@
+import { access, unlink } from 'fs/promises';
+import path from "path";
+
 const remove = async () => {
-    // Write your code here 
+    const filePath = path.join(process.cwd(), 'src/fs/files/fileToRemove.txt');
+
+    try {
+        await access(filePath);
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
+
+    await unlink(filePath);
 };
 
 await remove();
