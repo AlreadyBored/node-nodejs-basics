4cd32eb41023987d574edd52b49e3b781d522575
Zhenya Holub <zhenya.holub@rdigital.by>
2024-10-05 16:32:03 +0300

    feat: implement remove function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,11 @@
+import { rm } from 'node:fs/promises';
+
 const remove = async () => {
-    // Write your code here 
+    try {
+        await rm('./files/fileToRemove.txt');
+    } catch {
+        console.log(new Error('FS operation failed'));
+    }
 };
 
 await remove();
