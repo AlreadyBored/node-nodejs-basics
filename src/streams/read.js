4830274eb2e5b93f713b1d14e239bc34ac3c8284
bslevchenko <bslevchenko@sberbank.ru>
2024-10-05 19:01:55 +0300

    add function read file

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import { open } from 'fs/promises'
+import { stdout } from 'process';
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const fh = await open('files/fileToRead.txt')
+        const rs = fh.createReadStream('utf8')
+        rs.pipe(stdout)
+    } catch(err) {
+        fh.close()
+        throw new Error(err)
+    }
 };
 
 await read();
