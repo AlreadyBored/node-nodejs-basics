8781c5a5fd319688194c4d7a1830d73261412f3e
Elena Pavlova <lenlho22@gmail.com>
2024-10-05 22:26:19 +0300

    refactor: exlude repeating file to rename path

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,12 +1,11 @@
 import fs from "fs/promises";
 
 const rename = async () => {
+  const fileToRenamePath = "src/fs/files/wrongFilename.txt";
+
   try {
-    await fs.access("src/fs/files/wrongFilename.txt", fs.constants.F_OK);
-    await fs.rename(
-      "src/fs/files/wrongFilename.txt",
-      "src/fs/files/properFilename.md"
-    );
+    await fs.access(fileToRenamePath, fs.constants.F_OK);
+    await fs.rename(fileToRenamePath, "src/fs/files/properFilename.md");
   } catch (err) {
     throw new Error("FS operation failed");
   }
