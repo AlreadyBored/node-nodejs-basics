c64554a14d889c4f9ad0450647cb5c33a9a22a23
Ekaterina Prianishnikova <li.liliana@yandex.ru>
2024-10-05 16:09:54 +0400

    cli: parse args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+  let ans = '';
+  for (let i = 2; i < process.argv.length; ++i) {
+    const arg = process.argv[i];
+    if (arg.startsWith('--')) {
+      ans += arg.slice(2);
+    } else {
+      ans += ` is ${arg}${i < process.argv.length - 1 ? ', ' : ''}`;
+    }
+  }
+  console.log(ans);
 };
 
 parseArgs();
