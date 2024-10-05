8ee337883c5fa05bab997026f5f5c8b12352de81
Elena Pavlova <lenlho22@gmail.com>
2024-10-05 21:43:09 +0300

    feat: implement copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,15 @@
+import fs from "fs/promises";
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    await fs.cp("src/fs/files", "src/fs/files_copy", {
+      recursive: true,
+      force: false,
+      errorOnExist: true,
+    });
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await copy();
