6d72ec22e9d3f3f514b2c6d9e8b0a0b09b9c19ae
bslevchenko <bslevchenko@sberbank.ru>
2024-10-05 21:40:43 +0300

    add function decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,29 @@
+
+//  implement function that decompresses archive.gz back to the fileToCompress.txt
+//  with same content as before compression using zlib and Streams API
+import { open } from 'fs/promises'
+import { pipeline } from 'stream/promises'
+import { createWriteStream } from 'fs'
+import { createGunzip } from 'zlib'
+
 const decompress = async () => {
-    // Write your code here 
+    try {
+        const fh = await open('archive.gz')
+        const rs = fh.createReadStream()
+        const ws = createWriteStream('files/fileToCompress.txt')
+
+        const gunzip = createGunzip()
+    
+        await pipeline(
+            rs,
+            gunzip,
+            ws
+        )
+
+    } catch(err) {
+
+    }
+
 };
 
 await decompress();
