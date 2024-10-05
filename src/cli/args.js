6558ab34bae46e73487266f11ae8bb7cc50e8308
Zhenya Holub <zhenya.holub@rdigital.by>
2024-10-05 16:37:20 +0300

    feat: implement parseArgs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+    const [execPath, execFile, ...rest] = process.argv;
+    let args = [];
+    for (let i = 0; i < rest.length; i++) {
+        if (i % 2 !== 0) {
+            continue;
+        }
+        args.push(`${rest[i]} is ${rest[i + 1]}`);
+    }
+    console.log(args.join(', '));
 };
 
 parseArgs();
