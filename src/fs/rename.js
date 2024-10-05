04b8853a2ce90971beadec3bafbbea676e437dea
Alexey Abramov <aal@renue.ru>
2024-10-05 17:12:36 +0400

    feat: implement rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,26 @@
+import { access, rename as renameFile } from 'fs/promises';
+import path from "path";
+
 const rename = async () => {
-    // Write your code here 
+    const oldFile = path.join(process.cwd(), 'src/fs/files/wrongFilename.txt');
+    const newFile = path.join(process.cwd(), 'src/fs/files/properFilename.md');
+
+    try {
+        await access(oldFile)
+    } catch (error) {
+        throw Error('FS operation failed');
+    }
+
+    try {
+        await access(newFile)
+        throw Error('FS operation failed');
+    } catch (error) {
+        if (error.code !== 'ENOENT') {
+            throw error;
+        }
+    }
+
+    renameFile(oldFile, newFile);
 };
 
 await rename();
