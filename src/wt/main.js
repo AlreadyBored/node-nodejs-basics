beb0d04dfcc657d04fe26db8ebab0a82518d48c2
Zhenya Holub <zhenya.holub@rdigital.by>
2024-10-05 19:54:12 +0300

    feat: implement performCalculations and sendResult functions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,27 @@
+import os from 'os';
+import { Worker } from 'node:worker_threads';
+
 const performCalculations = async () => {
-    // Write your code here
+    const amountOfCores = os.cpus().length;
+
+    let promises = [];
+    for (let i = 0; i < amountOfCores; i++) {
+        promises.push(
+            new Promise((resolve, reject) => {
+                const worker = new Worker('./src/wt/worker.js', {
+                    workerData: 10 + i
+                });
+                worker.on('message', (data) => {
+                    resolve({ status: 'resolved', data });
+                });
+                worker.on('error', () =>
+                    resolve({ status: 'error', data: null })
+                );
+            })
+        );
+    }
+    const result = await Promise.all(promises);
+    console.log(result);
 };
 
 await performCalculations();
