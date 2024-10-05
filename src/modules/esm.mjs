06500f891d0d4cc9b3e375c8105e698bb38c5bb3
Zhenya Holub <zhenya.holub@rdigital.by>
2024-10-05 16:41:21 +0300

    refactor: change cjsToEsm.js to esm.mjs and inner to ECMAScript

---
+++ src/modules/esm.mjs
@@ -0,0 +1,43 @@
+import * as path from 'path';
+import { release, version } from 'os';
+import { createServer as createServerHttp } from 'http';
+import { fileURLToPath } from 'url';
+import './files/c.js';
+import a from './files/a.json' with { type: "json" };
+import b from './files/b.json' with { type: "json" };
+
+const random = Math.random();
+const __filename = fileURLToPath(import.meta.url);
+
+let unknownObject;
+
+if (random > 0.5) {
+    unknownObject = a;
+} else {
+    unknownObject = b;
+}
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${path.sep}"`);
+
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${path.dirname(__filename)}`);
+
+const myServer = createServerHttp((_, res) => {
+    res.end('Request accepted');
+});
+
+const PORT = 3000;
+
+console.log(unknownObject);
+
+myServer.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+    console.log('To terminate it, use Ctrl+C combination');
+});
+
+export {
+    unknownObject,
+    myServer
+};
