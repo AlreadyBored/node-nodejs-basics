077f8b42478ee23b2e296c2249cf457b2234fa80
Alexey Abramov <aal@renue.ru>
2024-10-05 17:50:40 +0400

    feat: implement read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import { access, readFile } from 'fs/promises';
+import path from "path";
+
 const read = async () => {
-    // Write your code here 
+    const filePath = path.join(process.cwd(), 'src/fs/files/fileToRead.txt');
+
+    try {
+        await access(filePath)
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
+
+    const content = await readFile(filePath, 'utf8');
+    console.log(content);
 };
 
 await read();
