7453b077ae18f0e3f31e9c63872d800d42573416
Alexey Abramov <aal@renue.ru>
2024-10-05 16:53:03 +0400

    feat: implement copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,26 @@
+import { access, cp, mkdir, readdir, copyFile } from 'fs/promises';
+import path from "path";
+
 const copy = async () => {
-    // Write your code here 
+    const sourceDir = path.join(process.cwd(), 'src/fs/files');
+    const destDir = path.join(process.cwd(), 'src/fs/files_copy');
+      
+    try {
+        await access(sourceDir);
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        await access(destDir);
+        throw new Error('FS operation failed');
+    } catch (error) {
+        if (error.code !== 'ENOENT') {
+            throw error;
+        }
+    }
+
+    cp(sourceDir, destDir, {recursive: true});
 };
 
 await copy();
