e5b1cf7cd9522dc3d64733b65267b463f2b51f13
Andrei Bernatski <ABernatskiy@alfabank.ru>
2024-10-06 17:33:05 +0300

    feat: resolve cp section

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,17 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import cp from 'child_process';
+import path from 'path';
+
+const SCRIPT_PATH = path.resolve('src', 'cp', 'files', 'script.js');
+
+const spawnChildProcess = async (...args) => {
+  const childProcess = cp.fork(SCRIPT_PATH, args, { silent: true });
+
+  process.stdin.pipe(childProcess.stdin);
+  childProcess.stdout.pipe(process.stdout);
+
+  childProcess.stdout.on('data', (chunk) => {
+    process.stdout.write(`Received from child process: ${chunk}`);
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(1,2,3);
