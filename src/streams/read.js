57663413c874f0b6680fdfcea96d93c337595baa
Alexey Abramov <aal@renue.ru>
2024-10-06 18:19:37 +0400

    feat: implement read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import { createReadStream  } from 'fs';
+import path from "path";
+import { pipeline } from 'stream';
+
 const read = async () => {
-    // Write your code here 
+    const filePath = path.join(process.cwd(), 'src/fs/files/fileToRead.txt');
+
+    const stream = createReadStream(filePath);
+
+    pipeline(stream, process.stdout, (error) => {
+            if (error) {
+                console.error(error.message);
+            }
+        }
+    )
 };
 
 await read();
