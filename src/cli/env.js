195f502eea208fe407bfb0154162077fd87dd22a
Podexus23 <santon23@gmail.com>
2024-10-06 20:19:02 +0300

    feat: env done

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,7 @@
 const parseEnv = () => {
-    // Write your code here 
+  // Write your code here
+  const rssNames = Object.keys(process.env).filter((e) => e.startsWith("RSS_"));
+  console.log(rssNames.map((e) => `${e}=${process.env[e]};`).join(" "));
 };
 
 parseEnv();
