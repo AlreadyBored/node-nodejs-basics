2fa60db77b0fdc3ca58334c842cc6f923dccf95d
Andrei Bernatski <ABernatskiy@alfabank.ru>
2024-10-06 16:59:55 +0300

    feat: resolve zip section

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,15 @@
+import streams from 'stream/promises';
+import zlib from 'zlib';
+import fs from 'fs';
+
+import { ZIP_FILE_PATH, COMPRESSED_FILE_PATH } from '../../lib/constants/zip.js';
+
 const compress = async () => {
-    // Write your code here 
+  await streams.pipeline(
+    fs.createReadStream(ZIP_FILE_PATH),
+    zlib.createGzip(),
+    fs.createWriteStream(COMPRESSED_FILE_PATH)
+  );
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,15 @@
+import streams from 'stream/promises';
+import zlib from 'zlib';
+import fs from 'fs';
+
+import { ZIP_FILE_PATH, COMPRESSED_FILE_PATH } from '../../lib/constants/zip.js';
+
 const decompress = async () => {
-    // Write your code here 
+  await streams.pipeline(
+    fs.createReadStream(COMPRESSED_FILE_PATH),
+    zlib.createGunzip(),
+    fs.createWriteStream(ZIP_FILE_PATH)
+  );
 };
 
 await decompress();
