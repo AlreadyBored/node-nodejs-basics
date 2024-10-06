b5fcf6102d5c77df63b5f212eb9beb6062e2aede
Podexus23 <santon23@gmail.com>
2024-10-06 20:18:56 +0300

    feat: args done

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,23 @@
 const parseArgs = () => {
-    // Write your code here 
+  // Write your code here
+  const args = process.argv;
+
+  const specArgs = [];
+  let prev = false;
+  let prevName = "";
+
+  args.forEach((elem, i) => {
+    if (prev && !elem.startsWith("--")) {
+      specArgs.push(`${prevName} is ${elem}`);
+      prev = false;
+      prevName = "";
+    } else if (elem.startsWith("--")) {
+      prev = true;
+      prevName = elem.slice(2);
+    }
+  });
+
+  console.log(specArgs.join(", "));
 };
 
 parseArgs();
