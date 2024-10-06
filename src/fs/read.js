14c6c8a359e18cf8c8e328b5acf1271eebff7e75
oleg filkin <oleg.filkin.by@gmail.com>
2024-10-06 21:38:41 +0300

    feat: add fs:read solution

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,29 @@
-const read = async () => {
-    // Write your code here 
+import fs from 'fs/promises';
+import path from 'path';
+import { FsOperationFailed } from './errors.js';
+
+ const read = async () => {
+    const filesPath = path.resolve('src', 'fs', 'files', 'fileToRead.txt');
+    let isFilesDirExist = true;
+
+    try {
+        await fs.stat(filesPath);
+    } catch (error) {
+        isFilesDirExist = false;
+    }
+
+    if (!isFilesDirExist) {
+        throw new FsOperationFailed();
+    }
+ 
+    try {
+        const files = await fs.readdir(filesPath);    
+        for (const file of files) {
+            console.log(file)
+        }
+    } catch (error) {
+        console.log(error)
+    }
 };
 
 await read();
