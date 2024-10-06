c5c082fd2551c8a1cd5a78100f982c5e9d1cd993
Edward <e.arechwa@gmail.com>
2024-10-06 11:43:57 +0200

    feat: implement delete js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,30 @@
+import fs from 'fs/promises'; 
+import path from 'path'; 
+
+// there was error about __dirname, get this solution from gpt
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 const remove = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files', 'fileToRemove.txt'); 
+
+    try {
+        // check if file exists
+        await fs.access(filePath); 
+    } catch (error) {
+        console.log(error)
+        throw new Error('FS operation failed'); 
+    }
+
+    try {
+        await fs.unlink(filePath);
+        console.log('File deleted successfully'); 
+    } catch (error) {
+        console.log(error)
+        throw new Error('FS operation failed');
+    }
 };
 
 await remove();
