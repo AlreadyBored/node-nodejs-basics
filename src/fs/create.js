32ce86cb8171db0daec59189b61ea364994a47a8
vera-mal <k(k(kkkpG286(ABGDzmq}>
2024-10-06 17:47:18 +0200

    feat: implement function that creates new file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,15 @@
+import fs from 'fs';
+
 const create = async () => {
-    // Write your code here 
+    const target = './files/fresh.txt';
+
+    fs.readFile(target, (err, data) => {
+        if (err?.code === 'ENOENT' && !data) {
+            fs.writeFile(target, 'I am fresh and young', () => {});
+        } else if (data) {
+            throw Error ('FS operation failed');
+        }
+    });
 };
 
 await create();
