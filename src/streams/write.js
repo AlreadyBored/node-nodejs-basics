f5a7cc2df866266c578ff47ca830079ec4883187
kaliganoff <kaliganoff@yahoo.com>
2024-10-06 21:32:10 +0300

    feat: Add write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,28 @@
+import path from 'path';
+import os from 'os';
+import { createWriteStream } from 'fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const { stdin, stdout } = process;
+
+const writeStream = createWriteStream(path.join(__dirname, 'files/fileToWrite.txt'));
+
+
 const write = async () => {
-    // Write your code here 
-};
+    stdout.write('Hi! Please, enter text: (you can type "exit" to exit)\n')
+    stdin.on("data", (data) => {
+        if (data.toString().includes('exit')) {
+            stdout.write('Goodbye!' + os.EOL);
+            process.exit();
+        };
+        writeStream.write(data);
+    });
+    process.on('SIGINT', () => {
+        stdout.write('Goodbye!');
+        process.exit();
+    })};
 
 await write();
