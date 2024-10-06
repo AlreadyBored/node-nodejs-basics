6f376c5d6d756eba37662e0f4635a0f4adec0dbe
bslevchenko <bslevchenko@sberbank.ru>
2024-10-06 15:53:45 +0300

    add function cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,13 @@
+
+import { fork } from 'child_process'
+
 const spawnChildProcess = async (args) => {
     // Write your code here
+    fork('files/script.js', args, {
+        stdio: [0, 1, 2, 'ipc']
+    })
+    
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ['someArgument1', 'someArgument2']);
