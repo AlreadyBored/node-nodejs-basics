69ac6cf07c1db7dc9e35f349d3564dd64bc22b8e
oleg filkin <oleg.filkin.by@gmail.com>
2024-10-06 22:32:06 +0300

    feat: add streams:read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import path from 'path';
+import { pipeline } from 'node:stream/promises';
+import { createReadStream } from 'node:fs';
+
 const read = async () => {
-    // Write your code here 
+    const readFilePath = path.resolve('src', 'streams', 'files', 'fileToRead.txt');
+    
+    await pipeline(
+        createReadStream(readFilePath, 'utf8'),
+        async function* (source) {
+            for await (const chunk of source) {
+                yield process.stdout.write(chunk);
+            }
+        },
+    );    
+
 };
 
 await read();
