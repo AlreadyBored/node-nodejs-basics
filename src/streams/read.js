2c2dfc0b241e1a585797f996b3304cbc9a82087d
Edward <e.arechwa@gmail.com>
2024-10-06 12:13:26 +0200

    feat: implement read js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,23 @@
+import { createReadStream } from 'fs'; 
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url'; 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
     // Write your code here 
+    const filePath = resolve(__dirname, 'files', 'fileToRead.txt');
+
+    // create a readable stream from the file
+    const fileStream = createReadStream(filePath, { encoding: 'utf-8' });
+
+    fileStream.on('error', (error) => {
+        console.error('Error reading the file:', error.message);
+        throw new Error('FS operation failed');
+    });
+
+    fileStream.pipe(process.stdout);
 };
 
 await read();
