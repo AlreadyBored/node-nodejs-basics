78ed8ee4cef86d496f2681012315ada857f71870
Anastasiia Perova <perovaa.24@gmail.com>
2024-10-06 15:00:22 +0400

    feat: add read stream function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,22 @@
+import { createReadStream } from "node:fs";
+
 const read = async () => {
-    // Write your code here 
+  const input = createReadStream("src/streams/files/fileToRead.txt", {
+    encoding: "utf-8",
+  });
+  input
+    .on("readable", () => {
+      let chunk;
+      while (null !== (chunk = input.read())) {
+        console.log(chunk);
+      }
+    })
+    .on("end", () => {
+      console.log("");
+    })
+    .on("error", (error) => {
+      console.error(`Error occurred: ${error.message}`);
+    });
 };
 
 await read();
