134b816edc7c825a220e2c92818300c402f3f512
Maria Riazanova <mariya.ryazanova@iqvia.com>
2024-10-06 17:21:12 +0200

    feat: add methods for wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,37 @@
+import { cpus } from 'os';
+import { dirname} from 'path';
+import { fileURLToPath } from 'url';
+import { Worker } from 'worker_threads';
+
+const workerPath = dirname(fileURLToPath(import.meta.url)) + '/worker.js';
+
 const performCalculations = async () => {
-    // Write your code here
+  const cpuCores = cpus();
+  let startNumber = 10;
+
+  const workerResults = await Promise.allSettled(cpuCores.map(() => {
+    return new Promise((resolve, reject) => {
+      const worker = new Worker(workerPath, {
+        workerData: startNumber++,
+      });
+
+      worker.on('message', (message) => {
+        resolve(message);
+      });
+      worker.on('error', (message) => {
+        reject(message);
+      });
+    })
+  }))
+
+  const results = workerResults.map(({ status, value }) => ({
+   status: status === 'fulfilled' ? 'resolved' : 'error',
+   data: status === 'fulfilled' ? value : null
+  }));
+
+  console.log(results);
+
+  return results;
 };
 
 await performCalculations();
