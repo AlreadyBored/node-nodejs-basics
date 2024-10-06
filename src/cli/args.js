1219810f075db0f7db4860a455ac409f2e62f5cc
diyora <diyoramekhmonova@gmail.com>
2024-10-06 22:05:30 +0800

    feat: cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,8 @@
 const parseArgs = () => {
-    // Write your code here 
+  const variables = Object.keys(process.env);
+  variables.forEach((v) => {
+    console.log(`${v} is ${process.env[v]},`);
+  });
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+  const variables = Object.keys(process.env);
+  const prefix = "NVM";
+  variables.forEach((v) => {
+    if (v.startsWith(prefix)) {
+      console.log(`${v}=${process.env[v]};`);
+    }
+  });
 };
 
 parseEnv();
