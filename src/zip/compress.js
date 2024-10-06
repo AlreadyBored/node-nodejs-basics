7ba8dc9fbffec6d667ea7e6067cb34b493d36a7a
Kyrylo Doma <kyrylodoma@pro.local>
2024-10-06 23:50:31 +0700

    feat: zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { pipeline } from "stream/promises";
+import zlib from "zlib";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, "files", "fileToCompress.txt");
+
 const compress = async () => {
-    // Write your code here 
+  await pipeline(
+    fs.createReadStream(filePath),
+    zlib.createGzip(),
+    fs.createWriteStream(filePath + ".gz")
+  );
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
-const decompress = async () => {
-    // Write your code here 
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { pipeline } from "stream/promises";
+import zlib from "zlib";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, "files", "fileToCompress.txt.gz");
+
+const compress = async () => {
+  await pipeline(
+    fs.createReadStream(filePath),
+    zlib.createGunzip(),
+    fs.createWriteStream(filePath.replace(".gz", ""))
+  );
 };
 
-await decompress();
+await compress();
