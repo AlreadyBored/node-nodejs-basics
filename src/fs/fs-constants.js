072cc5c679c19dae3ba561ed3398565ba646d334
Maria Riazanova <mariya.ryazanova@iqvia.com>
2024-10-06 17:41:08 +0200

    fix: fix constants

--- src/fs/fs-constants.js
+++ src/fs/fs-constants.js
@@ -2,17 +2,14 @@ import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import { stat } from 'fs/promises';
 
-// const __filename = fileURLToPath(import.meta.url);
-// export const __dirname = dirname(__filename);
 export const customError = 'FS operation failed';
 
 export const getPath = (url, fileName = '', folderName = 'files') => {
-    const __filename = fileURLToPath(url);
-    const __dirname = dirname(__filename);
-    const filePath = fileName ? join(__dirname, folderName, fileName) : join(__dirname, folderName);
+  const __filename = fileURLToPath(url);
+  const __dirname = dirname(__filename);
+  const filePath = fileName ? join(__dirname, folderName, fileName) : join(__dirname, folderName);
 
-
-    return filePath;
+  return filePath;
 };
 
 export const isFileExists = async (url) => {
