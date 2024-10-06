07860b2583b20d1d131fa14192a9ffb43621ac1e
Ygrec <brygrec@gmail.com>
2024-10-07 00:26:46 +0300

    Fix error in the creare implementation

--- src/fs/create.js
+++ src/fs/create.js
@@ -2,7 +2,17 @@ import fs from 'node:fs';
 
 const create = async () => {
     try {
-        const fd = fs.openSync('test.txt', 'w+');
+        const filename = "./src/fs/files/fresh.txt";
+        if (fs.existsSync(filename)) {
+            throw new Error("FS operation failed");
+        }
+
+        const usefullText = 'I am fresh and young';
+        fs.writeFile(filename, usefullText, (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+          }); 
+
       } catch (err) {
         console.error(err);
       }};
