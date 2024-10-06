2f6107786d9db2c97a54529a11647e9493cce704
oleg filkin <oleg.filkin.by@gmail.com>
2024-10-06 22:35:22 +0300

    feat: add streams:transform solution

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,20 @@
+import path from 'path';
+import { pipeline } from 'node:stream/promises';
+import { createWriteStream } from 'node:fs';
+
 const transform = async () => {
-    // Write your code here 
+    const writeFilePath = path.resolve('src', 'streams', 'files', 'fileToWrite.txt');
+    
+    await pipeline(
+        process.stdin,
+        async function* (source) {
+            source.setEncoding('utf8');
+            for await (const chunk of source) {
+                yield chunk.split('').reverse().join('');
+            }
+        },
+        createWriteStream(writeFilePath, 'utf8'),
+    );
 };
 
 await transform();
