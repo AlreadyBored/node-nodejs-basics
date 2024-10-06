88b57eac4c7d78bef6567dd1a33ab824a633a32f
Podexus23 <santon23@gmail.com>
2024-10-06 20:22:02 +0300

    feat: child process done

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,33 @@
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+import { spawn, fork, exec, execFile } from "child_process";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const scriptPath = path.join(__dirname, "files", "script.js");
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const proc = spawn(`node`, [scriptPath, args]);
+
+    process.stdin.pipe(proc.stdin);
+
+    proc.stdout.on("data", (data) => {
+        console.log(Buffer.from(data).toString());
+    });
+
+    proc.stderr.on('data', (data) => {
+        console.log(`stderr: ${data}`)
+    })
+
+    proc.on("close", (code) => {
+        console.log("Child process exited with code ", code);
+    });
+
+    proc.on('error', (err) => {
+        console.log(err)
+    })
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+const arr = new Array(10).fill(1).map((e) => Math.floor(Math.random() * 1000));
+spawnChildProcess(arr);
