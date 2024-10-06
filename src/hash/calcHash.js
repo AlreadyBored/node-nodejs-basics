271a2e7da1eeda8681db7521ff7c0e6b68d4d62a
AndreiValasevich <andreivalasevich@gmail.com>
2024-10-06 19:46:42 +0200

    feat: add hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,29 @@
+import * as path from "node:path";
+import { createHash } from "node:crypto";
+import { createReadStream } from "node:fs";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __dirname = import.meta.dirname;
+  const filePath = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+  const hash = createHash("sha256");
+
+  try {
+    const readStream = createReadStream(filePath);
+    readStream.on("data", (chunk) => {
+      hash.update(chunk);
+    });
+
+    readStream.on("end", () => {
+      const fileHash = hash.digest("hex");
+      console.log("fileHash is -> ", fileHash);
+    });
+
+    readStream.on("error", (err) => {
+      console.error("Error while reading file:", err);
+    });
+  } catch (err) {
+    console.error("Error while calculating hash:", err);
+  }
 };
 
 await calculateHash();
