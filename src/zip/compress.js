1f6d0f5b4b715e093b59eec437eeb958bea8b6b8
Maria Riazanova <mariya.ryazanova@iqvia.com>
2024-10-06 17:13:55 +0200

    feat: add methods for zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,16 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+import { getPath } from '../fs/fs-constants.js';
+
+const filePath = getPath(import.meta.url, 'fileToCompress.txt');
+const destinationPath = getPath(import.meta.url, 'archive.gz');
+
 const compress = async () => {
-    // Write your code here 
+  const readableStream = createReadStream(filePath);
+  const writableStream = createWriteStream(destinationPath);
+  const compressedStream = createGzip();
+
+  readableStream.pipe(compressedStream).pipe(writableStream);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,16 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createUnzip } from 'zlib';
+import { getPath } from '../fs/fs-constants.js';
+
+const filePath = getPath(import.meta.url, 'archive.gz');
+const destinationPath = getPath(import.meta.url, 'fileToCompress.txt');
+
 const decompress = async () => {
-    // Write your code here 
+  const readableStream = createReadStream(filePath);
+  const writableStream = createWriteStream(destinationPath);
+  const decompressedStream = createUnzip();
+
+  readableStream.pipe(decompressedStream).pipe(writableStream);
 };
 
 await decompress();
