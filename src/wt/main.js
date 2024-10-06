614a40965100a1782cb74e18a67a1288583dedce
AndreiValasevich <andreivalasevich@gmail.com>
2024-10-06 20:27:25 +0200

    feat: add wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,42 @@
+import { Worker } from "node:worker_threads";
+import os from "node:os";
+import path from "node:path";
+
 const performCalculations = async () => {
-    // Write your code here
+  const __dirname = import.meta.dirname;
+
+  const numCPUs = os.cpus().length;
+  const workers = [];
+  const results = [];
+  const scriptPath = path.resolve(__dirname, "worker.js");
+
+  const createWorker = (workerData, index) => {
+    return new Promise((resolve) => {
+      const worker = new Worker(scriptPath);
+      worker.postMessage(workerData);
+
+      worker.on("message", (message) => {
+        results[index] = message;
+        resolve();
+        worker.terminate();
+      });
+
+      worker.on("error", () => {
+        results[index] = { status: "error", data: null };
+        resolve();
+        worker.terminate();
+      });
+    });
+  };
+
+  for (let i = 0; i < numCPUs; i++) {
+    const workerData = 10 + i;
+    workers.push(createWorker(workerData, i));
+  }
+
+  await Promise.all(workers);
+
+  console.log("results -> ", results);
 };
 
 await performCalculations();
