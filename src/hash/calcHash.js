26088f8b66fd5699e8354e41f96ab24eff4a2157
Ekaterina Prianishnikova <li.liliana@yandex.ru>
2024-10-06 16:16:12 +0400

    hash: calc hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import fs from 'fs';
+import crypto from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const readable = fs.createReadStream('./files/fileToCalculateHashFor.txt', { encoding: 'utf8' });
+
+  const hash = await new Promise((resolve, reject) => {
+    const hash = crypto.createHash('sha256');
+    hash.once('finish', () => resolve(hash.read().toString('hex')));
+    readable.pipe(hash);
+  });
+
+  console.log(hash);
 };
 
 await calculateHash();
