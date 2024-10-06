816cf24e17c412497cc07cf4d460be1c8c4835a2
oleg filkin <oleg.filkin.by@gmail.com>
2024-10-06 22:42:02 +0300

    feat: add zip:compress and zip:decompress solutions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import path from 'path';
+import { pipeline } from 'node:stream/promises';
+import { createReadStream, createWriteStream } from 'node:fs';
+import zlib from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+    const readFilePath = path.resolve('src', 'zip', 'files', 'fileToCompress.txt');
+    const writeFilePath = path.resolve('src', 'zip', 'files', 'archive.gz');
+    const gzip = zlib.createGzip();
+    
+    await pipeline(
+        createReadStream(readFilePath, 'utf8'),
+        gzip,
+        createWriteStream(writeFilePath)
+    );
 };
 
 await compress();
