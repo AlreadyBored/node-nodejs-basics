314c033040b6be107f8d6e04cab6d17dc4e82dea
Evgeniy Dolgov <dolgov.e.1991@gmail.com>
2024-10-06 12:29:32 +0300

    feat: added create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import { writeFile } from "node:fs/promises";
+import path from "node:path";
+import url from "node:url";
+
 const create = async () => {
-    // Write your code here 
+  const __filename = url.fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const fileDirectory = path.join(__dirname, "files", "fresh.txt");
+
+  try {
+    await writeFile(fileDirectory, "I am fresh and young", { flag: "ax" });
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await create();
