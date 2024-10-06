43f69937f07d5dac473d83938d226a9f1e95c215
bslevchenko <bslevchenko@sberbank.ru>
2024-10-06 14:53:33 +0300

    add functions main and worker

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,36 @@
+import { availableParallelism } from 'os'
+import { Worker } from 'worker_threads'
+
+const runWorker = (workerData) => {
+    return new Promise((res, rej) => {
+        const worker = new Worker('./worker.js', { workerData })
+        worker.on('message', (value) => {
+            res({
+                status: 'resolve',
+                data: value
+            })
+        })
+        worker.on('error', (err) => rej({
+            status: err,
+            data: null
+        }))
+    })
+}
+
 const performCalculations = async () => {
-    // Write your code here
+    try {
+        const countCpuCore = availableParallelism()
+        const treads = []
+    
+        for(let i = 0; i < countCpuCore; i++ ) {
+            treads.push(runWorker({treadId: i, fib: i+10}))
+        }
+    
+        const results = await Promise.all(treads)
+        console.log('performCalculations', results)
+    } catch(err) {
+        throw new Error(err)
+    }
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,12 @@
 // n should be received from main thread
+import { parentPort, workerData } from 'worker_threads'
+
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
     // This function sends result of nthFibonacci computations to main thread
+    const res = nthFibonacci(workerData.fib)
+    parentPort.postMessage(res)
 };
 
 sendResult();
