2141b79013c732079afd083f0ad133786bd88201
Maria Riazanova <mariya.ryazanova@iqvia.com>
2024-10-06 16:52:08 +0200

    feat: add methods for modules

---
+++ src/modules/esm.mjs
@@ -0,0 +1,40 @@
+import path, { dirname } from 'path';
+import { release, version }  from 'os';
+import { createServer as createServerHttp }  from 'http';
+import './files/c.js';
+import { fileURLToPath } from 'url';
+
+const random = Math.random();
+
+const { default: unknownObject } = random > 0.5
+  ? await import('./files/a.json', { assert: { type: 'json' } })
+  : await import('./files/b.json', { assert: { type: 'json' } });
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${path.sep}"`);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
+
+const myServer = createServerHttp((_, res) => {
+  res.end('Request accepted');
+});
+
+const PORT = 3000;
+
+console.log(unknownObject);
+
+myServer.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+  console.log('To terminate it, use Ctrl+C combination');
+});
+
+export {
+  unknownObject,
+  myServer,
+};
+
