90cbbaf445c680b98a4c33ec7e7227103fae2a11
Ekaterina Prianishnikova <li.liliana@yandex.ru>
2024-10-06 20:00:16 +0400

    wt: add worker

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,20 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
+
 const performCalculations = async () => {
-    // Write your code here
+  const runThread = (workerData) => new Promise((resolve, reject) => {
+    const worker = new Worker('./worker.js', { workerData });
+    worker.on('message', (data) => resolve({ status: 'resolved', data }));
+    worker.on('error', () => reject({ status: 'error', data: null }));
+  });
+
+  const cpuCount = os.cpus().length;
+  const promises = [];
+  for (let i = 0; i < cpuCount; i++) {
+    promises.push(runThread(10 + i));
+  }
+  const ans = await Promise.all(promises);
+  console.log(ans);
 };
 
 await performCalculations();
