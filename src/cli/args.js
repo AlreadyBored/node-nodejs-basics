1824862acf0b680cceb7409bceadd622bc9214f3
Alexey Abramov <aal@renue.ru>
2024-10-06 14:41:13 +0400

    feat: implement args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+
+    const arrTransformArgs = args.reduce((acc, val, index, array) => {
+        if (val.startsWith("--")) {
+            const field = val.slice(2)
+            const value = array[index + 1];
+            const resultStr = `${field} is ${value}`
+            acc.push(resultStr);
+        }
+        return acc
+    }, [])
+
+    console.log(arrTransformArgs.join(", "));
 };
 
 parseArgs();
