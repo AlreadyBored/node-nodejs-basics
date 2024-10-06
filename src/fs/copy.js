fe1921b76e8bcd4fe2e976dbeb929bfe8d47a3b5
Evgeniy Dolgov <dolgov.e.1991@gmail.com>
2024-10-06 17:08:25 +0300

    feat: added copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,29 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+import url from "node:url";
+
 const copy = async () => {
-    // Write your code here 
+  const __filename = url.fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const fileDir = path.join(__dirname, "files");
+  const copyDir = path.join(__dirname, "files_copy");
+
+  try {
+    await fs.mkdir(copyDir, { recursive: false });
+    await fs.access(fileDir, fs.constants.F_OK);
+  } catch {
+    throw new Error("FS operation failed");
+  }
+
+  const files = await fs.readdir(fileDir);
+
+  for (const file of files) {
+    const sourcePath = path.join(fileDir, file);
+    const targetPath = path.join(copyDir, file);
+
+    await fs.copyFile(sourcePath, targetPath);
+  }
 };
 
 await copy();
