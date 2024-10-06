96980a13da6a3a9457cbda3968d9c26b90976132
Podexus23 <santon23@gmail.com>
2024-10-06 20:21:44 +0300

    feat: hash done

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+import { createReadStream } from "fs";
+import { createHash } from "crypto";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const file = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+  const rs = createReadStream(file);
+  const hash = createHash("sha256");
+  rs.on("error", (err) => err);
+  rs.on("data", (chunk) => hash.update(chunk));
+  rs.on("end", () => console.log(hash.digest("hex")));
 };
 
 await calculateHash();
