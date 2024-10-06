f4c8f1df85b7430c68185cbab72be5ba8bc0b3f0
kaliganoff <kaliganoff@yahoo.com>
2024-10-06 21:24:18 +0300

    fix: Fix read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,4 +1,5 @@
 import path from 'path';
+import os from 'os';
 import { createReadStream } from 'fs';
 import { fileURLToPath } from 'url';
 
@@ -8,7 +9,8 @@ const __dirname = path.dirname(__filename);
 const read = async () => {
     const readStream = createReadStream(path.join(__dirname, 'files/fileToRead.txt'), 'utf-8');
 
-    readStream.on('data', (chunk) => console.log(chunk));
+    readStream.on('data', (chunk) => process.stdout.write(chunk));
+    readStream.on('end', () => process.stdout.write(os.EOL));
 };
 
 await read();
