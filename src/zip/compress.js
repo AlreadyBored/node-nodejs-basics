3ebbe2a2ccdac1970f5166954228e01d8f9cc9cc
samvel <s.saakyana@trainingspace.ru>
2024-10-07 02:40:33 +0400

    feat: zip subtask

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+import path from 'path';
+
 const compress = async () => {
-    // Write your code here 
+  const file = path.join(process.cwd(), 'files', 'fileToCompress.txt');
+  const archive_file = path.join(process.cwd(), 'files', 'archive.gz');
+
+  const read_stream = createReadStream(file);
+  const write_stream = createWriteStream(archive_file);
+  const gzip = createGzip();
+
+  read_stream
+    .pipe(gzip)
+    .pipe(write_stream)
+    .on('error', (error) => {
+      console.error(error);
+    });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from 'zlib';
+import path from 'path';
+
 const decompress = async () => {
-    // Write your code here 
+  const file = path.join(process.cwd(), 'files', 'fileToCompress.txt');
+  const archive_file = path.join(process.cwd(), 'files', 'archive.gz');
+
+  const read_stream = createReadStream(archive_file);
+  const write_stream = createWriteStream(file);
+  const gunzip = createGunzip();
+
+  read_stream
+    .pipe(gunzip)
+    .pipe(write_stream)
+    .on('error', (error) => {
+      console.error(error);
+    });
 };
 
 await decompress();
