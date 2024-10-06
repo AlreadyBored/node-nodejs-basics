4420b12d723ec00565bcb60d1f9ccd529b7ff6c2
Alexey Abramov <aal@renue.ru>
2024-10-06 17:47:35 +0400

    feat: implement calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import { createReadStream  } from 'fs';
+import path from "path";
+import { createHash } from 'crypto';
+import { pipeline } from 'stream';
+import { promisify } from 'util';
+
+const pipelineAsync = promisify(pipeline);
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = path.join(process.cwd(), 'src/hash/files/fileToCalculateHashFor.txt');
+    const stream = createReadStream(filePath);
+    const hash = createHash('sha256');
+
+    pipeline(stream, hash, (error) => {
+        if (!error) {
+            const hexHash = hash.digest('hex');
+            console.log(hexHash);
+        }
+    });
 };
 
 await calculateHash();
