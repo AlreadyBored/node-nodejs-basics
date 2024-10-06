b7807c5ad4224b96b7a2883311ebf72f081ce0b3
oleg filkin <oleg.filkin.by@gmail.com>
2024-10-06 22:29:10 +0300

    feat: add hash solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,13 @@
+import { createHash } from 'crypto';
+import fs from 'fs';
+import path from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const stream = fs.createReadStream(path.resolve('src', 'hash', 'files', 'fileToCalculateHashFor.txt'))
+    
+    stream
+        .on('data', (chunk) => console.log(createHash('sha256').update(chunk.toString()).digest('hex')));
+
 };
 
 await calculateHash();
