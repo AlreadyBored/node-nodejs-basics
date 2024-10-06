d29fc1d2e5f86c64a8fe9b12ca23badede1fabaf
kaliganoff <kaliganoff@yahoo.com>
2024-10-06 20:59:35 +0300

    feat: Add hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+
 const calculateHash = async () => {
-    // Write your code here 
-};
+    const getHash = path => new Promise((resolve, reject) => {
+        const hash = createHash('sha256');
+        const rs = createReadStream(path);
+        rs.on('error', reject);
+        rs.on('data', chunk => hash.update(chunk));
+        rs.on('end', () => resolve(hash.digest('hex')));
+       })
+       
+       try {
+        const hashValue = await getHash('src/hash/files/fileToCalculateHashFor.txt');
+        console.log(hashValue);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
 
 await calculateHash();
