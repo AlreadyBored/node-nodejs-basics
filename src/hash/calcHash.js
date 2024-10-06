693482894c857fab5e531857cc79001c6d22a7bc
samvel <s.saakyana@trainingspace.ru>
2024-10-07 01:38:56 +0400

    feat: hash subtask

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import path from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const file = path.join(process.cwd(), 'files', 'fileToCalculateHashFor.txt');
+  const hash = createHash('sha256');
+  const fileStream = createReadStream(file);
+
+  fileStream.on('data', (chunk) => {
+    hash.update(chunk);
+  });
+
+  fileStream.on('end', () => {
+    const hashHex = hash.digest('hex');
+    console.log(hashHex);
+  });
 };
 
 await calculateHash();
