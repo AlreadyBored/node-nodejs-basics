b1b3f256bf9c457751613264d8433d95cb71d7d1
oleg filkin <oleg.filkin.by@gmail.com>
2024-10-06 22:45:36 +0300

    feat: add wt solution

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,18 @@
+import os from 'os';
+import { Worker } from 'worker_threads';
+import path from 'path';
+
 const performCalculations = async () => {
-    // Write your code here
+    const result = await Promise.all(os.cpus().map(
+        (_, i) => new Promise(
+            (resolve, reject) => {
+                const worker = new Worker(path.resolve('src', 'wt', 'worker.js'), { workerData: i + 10});
+                worker.on('message', (message) => resolve({status: 'resolved', data: message}));
+                worker.on('error', reject);
+            }
+        ))
+    )
+    console.log(result);
 };
 
 await performCalculations();
