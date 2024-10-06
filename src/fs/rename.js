6c0f508034de2a96f085a061c53d87ad544dc249
Ygrec <brygrec@gmail.com>
2024-10-07 00:27:09 +0300

    Add rename functionality

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,22 @@
+import fs from 'node:fs'
+
 const rename = async () => {
-    // Write your code here 
+    let fileSourcePath = "src/fs/files/";
+    let fileOriginalName = "wrongFilename.txt";
+    let fileProperName = "properFilename.md";
+
+    try {
+        console.log("Original file path is " + fileSourcePath + fileOriginalName);
+        if (!fs.existsSync(fileSourcePath + fileOriginalName)) {
+          throw new Error("FS operation failed");
+        }
+
+        fs.copyFileSync(fileSourcePath + fileOriginalName, fileSourcePath + fileProperName, fs.constants.COPYFILE_EXCL);
+
+      } catch (err) {
+        console.error(err);
+      }
+
 };
 
 await rename();
