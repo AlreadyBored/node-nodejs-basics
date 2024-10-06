f2135645bc1a7a2650bf6853e8f5f7aebb66ccc4
Elena Pavlova <lenlho22@gmail.com>
2024-10-06 18:09:07 +0300

    feat: implement args function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,19 @@
 const parseArgs = () => {
-    // Write your code here 
+  const variableFlag = "--";
+  const outArgs = process.argv;
+
+  const result = [];
+
+  for (let i = 0; i < outArgs.length; i++) {
+    const item = outArgs[i];
+    if (item.startsWith(variableFlag)) {
+      const str = `${item.replace(variableFlag, "")} is ${outArgs[i + 1]}`;
+      result.push(str);
+      i += 1;
+    }
+  }
+
+  console.log(result.join(", "));
 };
 
 parseArgs();
