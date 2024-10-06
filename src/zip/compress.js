71d8581865a747214c1329089bda5758969c1c4b
Ekaterina Prianishnikova <li.liliana@yandex.ru>
2024-10-06 15:58:16 +0400

    zip: add zip utils

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,12 @@
+import fs from 'fs';
+import { createGzip } from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+  const gzip = createGzip();
+  const source = fs.createReadStream('files/fileToCompress.txt');
+  const destination = fs.createWriteStream('files/archive.gz');
+
+  source.pipe(gzip).pipe(destination);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,12 @@
+import fs from 'fs';
+import { createGunzip } from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+  const unzip = createGunzip();
+  const source = fs.createReadStream('files/archive.gz');
+  const destination = fs.createWriteStream('files/fileToCompress.txt');
+
+  source.pipe(unzip).pipe(destination);
 };
 
 await decompress();
