a8b508e466d4688099781a7ba62fbd34a0251580
Edward <e.arechwa@gmail.com>
2024-10-06 11:49:54 +0200

    feat: implement env file

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
     // Write your code here 
+    const rssEnvVars = Object.entries(process.env)
+    .filter(([key]) => key.startsWith('RSS_')) 
+    .map(([key, value]) => `${key}=${value}`); 
+
+    console.log(rssEnvVars.join('; '));
 };
 
 parseEnv();
