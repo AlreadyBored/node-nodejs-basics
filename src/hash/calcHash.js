4ccf9067442bcda311124d268de6d496bb54a3aa
Maria Riazanova <mariya.ryazanova@iqvia.com>
2024-10-06 17:00:33 +0200

    feat: add methods for hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,29 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import { readFile } from 'fs/promises';
+import { getPath } from '../fs/fs-constants.js';
+
+
+const filePath = getPath(import.meta.url, 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash('sha256');
+  const fileStream = createReadStream(filePath);
+
+  // const fileContent = await readFile(filePath, { encoding: 'utf8' });
+  // const hash = createHash('sha256').update(fileContent).digest('hex');
+  //
+  // console.log(`Hash: ${ hash }`);
+
+  fileStream.pipe(hash);
+
+  hash.on('readable', () => {
+    const data = hash.read();
+
+    if (data) {
+      console.log(`Hash: ${ data.toString('hex') }`);
+    }
+  });
 };
 
 await calculateHash();
