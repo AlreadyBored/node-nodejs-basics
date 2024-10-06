bb649b9607fac46e40f6db7d17d9b29ed5a18040
spankov <spankov@ikod.kz>
2024-10-06 18:05:51 +0500

    feat: implemented cp solution

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,23 @@
+import { fork } from 'node:child_process';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { stdin, stdout } from 'node:process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const scriptPath = join(__dirname, 'files/script.js');
+
+    const childProcess = fork(scriptPath, args, { stdio: ['pipe', 'pipe', 'pipe', 'ipc']});
+
+    stdin.pipe(childProcess.stdin);
+
+    childProcess.stdout.pipe(stdout);
+
+    stdin.on('data', (data) => {
+        childProcess.send({ cmd: 'echo', message: data });
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['arg 1', 'arg 2']);
