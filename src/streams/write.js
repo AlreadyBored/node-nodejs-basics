1dc252ce59faf983c51ba69586141921a2c85f5f
Alexey Abramov <aal@renue.ru>
2024-10-06 18:54:15 +0400

    feat: implement write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import { createWriteStream  } from 'fs';
+import path from "path";
+import { pipeline } from 'stream';
+import readline from 'readline';
+
 const write = async () => {
-    // Write your code here 
+    const filePath = path.join(process.cwd(), 'src/streams/files/fileToWrite.txt');
+    const writeStream = createWriteStream(filePath);
+
+    pipeline(process.stdin, writeStream, (error) => {
+        if (error) {
+            console.error(error.message);
+        }
+    });
+
+    console.log('Please enter data (press Ctrl + C to finish):')
 };
 
 await write();
