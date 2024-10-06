f49be5edf612da7ec14245ea58b1c6d92b2cab59
oleg filkin <oleg.filkin.by@gmail.com>
2024-10-06 21:32:38 +0300

    feat: add rename solution

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,36 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { FsOperationFailed } from './errors.js';
+
 const rename = async () => {
-    // Write your code here 
+    // Write your code here
+    const beforeRenamePath = path.resolve('src', 'fs', 'files', 'wrongFilename.txt');
+    const afterRenamePath = path.resolve('src', 'fs', 'files', 'properFilename.md');
+    let isWrongFileExist = true;
+    let isFileRenamed = true;
+
+    try {
+        await fs.stat(beforeRenamePath);
+    } catch (error) {
+        isWrongFileExist = false;
+    }
+
+    try {
+        await fs.stat(afterRenamePath);
+    } catch (error) {
+        isFileRenamed = false;
+    }
+    
+    if (!isWrongFileExist || isFileRenamed) {
+        throw new FsOperationFailed();
+    }
+ 
+    try {
+        await fs.rename(beforeRenamePath, afterRenamePath);    
+    } catch (error) {
+        console.log(error)
+    }
+
 };
 
 await rename();
