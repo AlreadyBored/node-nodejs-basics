8c8ea887f1adc64b3d1bd625d85d450da0b5cf07
giorgi khoshtaria <99177635+g1org11@users.noreply.github.com>
2024-10-07 00:11:11 +0400

    add wt

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -2,14 +2,21 @@ import { parentPort } from "worker_threads";
 
 const nthFibonacci = (n) =>
   n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
-
 const sendResult = (result) => {
-  // Send result of nthFibonacci computations to main thread
-  parentPort.postMessage(result);
+  parentPort.postMessage({ status: "success", data: result });
 };
 
-// Listen for messages from the main thread
-parentPort.on("message", (n) => {
-  const result = nthFibonacci(n);
-  sendResult(result);
+parentPort.on("message", (message) => {
+  try {
+    const n = message?.number;
+
+    if (typeof n === "number" && n >= 0) {
+      const result = nthFibonacci(n);
+      sendResult(result);
+    } else {
+      parentPort.postMessage({ status: "error", message: "Invalid input" });
+    }
+  } catch (error) {
+    parentPort.postMessage({ status: "error", message: error.message });
+  }
 });
