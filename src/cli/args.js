4a5921b4950e4716a50890e7bcf201148c3d0e62
Aliaksei Varabyou <aliaksei_varabyou@epam.com>
2024-10-06 14:53:04 +0200

    cli functions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,23 @@
+/*
+* implement function that parses command line arguments (given in format --propName value --prop2Name value2,
+* you don't need to validate it) and prints them to the console in the format 
+* propName is value, prop2Name is value2
+*/
+
 const parseArgs = () => {
-    // Write your code here 
+	let result = "";
+	process.argv.slice(2).forEach(val => {
+		if (val.substring(0, 2) === "--") {
+			result += val.substring(2) + " is ";
+		} else {
+			result += val + ", ";
+		}
+	});
+	if (result[result.length - 2] === ",") {
+		result = result.substring(0, result.length - 2);
+	}
+
+	console.log(result);
 };
 
 parseArgs();
