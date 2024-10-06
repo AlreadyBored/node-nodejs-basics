c7ffd4286e8a73fb01e3a96498bb48445e11bef7
samvel <s.saakyana@trainingspace.ru>
2024-10-07 01:01:41 +0400

    feat: cli subtask

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const result = [];
+
+  for (let i = 0; i < args.length; i += 2) {
+    result.push(`${args[i]} is ${args[i + 1]}`);
+  }
+
+  console.log(result);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
 const parseEnv = () => {
-    // Write your code here 
+  const parsed_envs = Object.entries(process.env)
+    .reduce((acc, [key, value]) => {
+      if (key.startsWith('RSS_')) {
+        acc.push(`${key}=${value}`);
+      }
+
+      return acc;
+    }, [])
+    .join('; ');
+
+  console.log(parsed_envs);
 };
 
 parseEnv();
