300b79e0762da2241f0b23eb8a69470c70d0a01c
Kyrylo Doma <kyrylodoma@pro.local>
2024-10-06 17:05:11 +0700

    feat: calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { promises as fs } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import crypto from "crypto";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+
 const calculateHash = async () => {
-    // Write your code here 
+  const file = await fs.readFile(filePath);
+  const hash = crypto.createHash("sha256");
+  hash.update(file);
+  const hashValue = hash.digest("hex");
+  console.log(hashValue);
 };
 
 await calculateHash();
