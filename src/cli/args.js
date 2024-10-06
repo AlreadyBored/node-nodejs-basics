1c2ac66245cbf80b6b85da6e2ae2fbd30658650a
Maria Riazanova <mariya.ryazanova@iqvia.com>
2024-10-06 16:49:11 +0200

    feat: add methods for cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+
+  const res  = args.reduce((acc, curr, index, array) => {
+    if (index % 2 !== 1) {
+      const propName = curr.slice(2);
+      const value = array[index + 1];
+
+      acc.push(`${propName} is ${value}`);
+    }
+
+    return acc;
+  }, []);
+
+  console.log(res.join(', '));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
+const prefix = 'RSS_';
+
 const parseEnv = () => {
-    // Write your code here 
+  let rssVariables = [];
+
+    Object.entries(process.env)
+        .filter(([key]) => key.startsWith(prefix))
+        .forEach(([key, value]) => rssVariables.push(`${key}=${value}`));
+
+  console.log(rssVariables.join('; '));
 };
 
 parseEnv();
