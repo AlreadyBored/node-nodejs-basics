ea70a2c9dce2512ca9db86c030741c8bdc84c3d7
oleg filkin <oleg.filkin.by@gmail.com>
2024-10-06 21:23:53 +0300

    feat: add create solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,28 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { FsOperationFailed } from './errors.js';
+
 const create = async () => {
-    // Write your code here 
+    const filePath = path.resolve('src', 'fs', 'files', 'fresh.txt');
+    let isExist = false;
+    console.log('filePath: ', filePath)
+
+    try {
+        isExist = await fs.stat(filePath);
+    } catch (error) {
+        console.log(error)
+    }
+
+    if (isExist) {
+        throw new FsOperationFailed();
+    }
+
+    const content = 'I am fresh and young';
+    try {
+        await fs.writeFile(filePath, content);
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 await create();
