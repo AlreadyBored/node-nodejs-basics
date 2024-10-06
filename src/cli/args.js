28c6710e40e78d134ce39c18636076497e9341bc
Edward <e.arechwa@gmail.com>
2024-10-06 11:51:47 +0200

    feat: implement args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
     // Write your code here 
+    const args = process.argv.slice(2); 
+
+    const result = [];
+
+    for (let i = 0; i < args.length; i += 2) {
+        const propName = args[i].replace('--', ''); 
+        const propValue = args[i + 1]; 
+        result.push(`${propName} is ${propValue}`);
+    }
+
+    console.log(result.join(', '));
 };
 
 parseArgs();
