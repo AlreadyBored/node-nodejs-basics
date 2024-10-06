19fe595b8bdaa8ab3433e0a621ed01a69c5f3d14
oleg filkin <oleg.filkin.by@gmail.com>
2024-10-06 22:16:28 +0300

    feat: add cli:env solution

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+    const PATERN = 'RSS_';
+    let result = Object.entries(process.env)
+        .filter(([key]) => key.indexOf(PATERN) === 0)
+        .map(([key, value]) => `${key}=${value}`)
+        .join('; ');
+    
+    console.log(result);
 };
 
 parseEnv();
