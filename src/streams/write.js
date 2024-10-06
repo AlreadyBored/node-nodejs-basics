c00b4d93052e9a5cb82691ac4caf89f8030b639a
Edward <e.arechwa@gmail.com>
2024-10-06 12:15:59 +0200

    feat: implement write file

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,26 @@
+import { createWriteStream } from 'fs'; 
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url'; 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const write = async () => {
     // Write your code here 
+    const filePath = resolve(__dirname, 'files', 'fileToWrite.txt');
+    
+    const writeStream = createWriteStream(filePath, { flags: 'a' });
+
+    writeStream.on('error', (error) => {
+        console.error('Error writing to the file:', error.message);
+        throw new Error('FS operation failed');
+    });
+
+    process.stdin.pipe(writeStream);
+
+    writeStream.on('finish', () => {
+        console.log('Data written to fileToWrite.txt successfully.');
+    });
 };
 
 await write();
