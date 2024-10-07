63177abf6b9c43602e95a2625caa39309ca1e521
AndreyTishchenko <Tishchenko.Andrew@yandex.ru>
2024-10-08 02:11:31 +0400

    feat: done readWrite task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,10 @@
+import * as fs from 'fs';
+import * as process from 'process';
+import * as path from 'path';
+const __dirname = import.meta.dirname
+let FilePath = path.join(__dirname, 'files', 'fileToWrite.txt');
 const write = async () => {
-    // Write your code here 
+    process.stdin.pipe(fs.createWriteStream(FilePath));
 };
 
 await write();
