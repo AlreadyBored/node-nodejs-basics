7c0c42a22717ad3dcf916fdf6b074760a7c284fc
Alexey Abramov <aal@renue.ru>
2024-10-07 22:58:23 +0400

    feat: implement cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,24 @@
+import { spawn } from'child_process';
+import path from "path";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const pathToScript = path.join(process.cwd(), 'src/cp/files/script.js');
+
+    const child = spawn('node', [pathToScript, ...args], {
+        stdio: ['pipe', 'pipe', 'inherit']
+    });
+
+    child.on('error', (error) => {
+        reject(`Failed to start child process: ${error.message}`);
+    });
+
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
+
+    child.on('exit', (code) => {
+        resolve(`Child process exited with code ${code}`);
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2']);
