d5b0a603d116b09cb14db4c29baff7db63092111
Maxim Semikov <maxim.semikov87@gmail.com>
2024-10-07 13:52:39 +0500

    feat: Zlib

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import { join } from "path";
+import { createGzip } from "zlib";
+import { pipeline } from "stream/promises";
+import { createReadStream, createWriteStream } from "fs";
+import {getExecutedFileDirname} from "../../helpers.js";
+
+const __dirname = getExecutedFileDirname(import.meta.url);
+const fileToComposePath = join(__dirname, "files", "fileToCompress.txt");
+const destinationFilePath = join(__dirname, "files", "archive.gz");
+
 const compress = async () => {
-    // Write your code here 
+  try {
+    await pipeline(
+      createReadStream(fileToComposePath),
+      createGzip(),
+      createWriteStream(destinationFilePath)
+    );
+    console.log("gZip succeeded");
+  } catch (err) {
+    console.error("Something went wrong! ", err);
+  }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import { join } from "path";
+import { createGunzip } from "zlib";
+import { pipeline } from "stream/promises";
+import { createReadStream, createWriteStream } from "fs";
+import {getExecutedFileDirname} from "../../helpers.js";
+
+const __dirname = getExecutedFileDirname(import.meta.url);
+const fileToDecomposePath = join(__dirname, "files", "archive.gz");
+const destinationFilePath = join(__dirname, "files", "fileToCompress.txt");
+
 const decompress = async () => {
-    // Write your code here 
+  try {
+    await pipeline(
+      createReadStream(fileToDecomposePath),
+      createGunzip(),
+      createWriteStream(destinationFilePath)
+    );
+    console.log("gUnzip succeeded");
+  } catch (err) {
+    console.error("Something went wrong! ", err);
+  }
 };
 
 await decompress();
