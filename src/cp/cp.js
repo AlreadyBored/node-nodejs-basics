22705603a166f9c3c7b4dea395ddec968200a353
FABIAN RAMIREZ <fabian.ramirez@nowports.com>
2024-10-06 21:11:49 -0500

    refactor:implemented child processes subtasks

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,18 @@
+import { fork } from "child_process";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = fork("./files/script.js", args, {
+    stdio: ["pipe", "pipe", "pipe", "ipc"],
+  });
+
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
+  child.stderr.pipe(process.stderr);
+
+  child.on("close", (code) => {
+    console.log(`Child process exited with code ${code}`);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["Argument One", "Argument two", "Argument three"]);
