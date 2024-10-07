c251ecbfc0850dafb11d1e5fb79fff5b8e73c7c3
Тыщук Вячеслав <tischook@yandex.ru>
2024-10-08 02:06:34 +0300

    feat: read file

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import { readFile } from 'node:fs/promises';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+
+const fileName = fileURLToPath(import.meta.url);
+const __dirname = dirname(fileName);
+
+const filePath = join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const contents = await readFile(filePath);
+        console.log(contents.toString());
+    } catch (err) {
+        console.error('FS operation failed');
+    }
 };
 
 await read();
