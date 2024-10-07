782be15781b8fa0433d316e405d913a06e145f11
Pavel Drazdou <p.drazdou@godeltech.com>
2024-10-07 23:44:46 +0300

    Command line interface(src/cli)

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  let result = [];
+  for (let i = 0; i < args.length; i += 2) {
+    const propName = args[i].replace('--', '');
+    const propValue = args[i + 1];
+    result.push(`${propName} is ${propValue}`);
+  }
+  console.log(result.join(', '));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envVars = process.env;
+  const rssVars = Object.keys(envVars)
+    .filter(key => key.startsWith('RSS_'))
+    .map(key => `${key}=${envVars[key]}`);
+  console.log(rssVars.join('; '));
 };
 
 parseEnv();
