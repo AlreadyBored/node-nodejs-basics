38ac8ada87988d742477f8d8317476f730dd2b3b
samvel <s.saakyana@trainingspace.ru>
2024-10-07 11:40:05 +0400

    feat: cp subtask

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,34 @@
+import { fork } from 'child_process';
+import process from 'process';
+import path from 'path';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const file = path.join(process.cwd(), 'files', 'script.js');
+
+  const controller = new AbortController();
+  const { signal } = controller;
+  //   Создаем дочерний процесс
+  const child = fork(file, args, {
+    stdio: ['ipc', 'pipe', 'pipe'],
+    signal,
+  });
+
+  // Передаем данные из stdin master процесса в stdin дочернего процесса
+  process.stdin.on('data', (data) => {
+    child.stdin.write(data);
+  });
+
+  // Передаем данные из stdout дочернего процесса в stdout master процесса
+  child.stdout.on('data', (data) => {
+    process.stdout.write(data);
+  });
+
+  // Закрытие процесса по команде CLOSE
+  child.on('exit', (code) => {
+    console.log(`Child process exited with code ${code}`);
+    process.exit(code);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['x', 'y', 'z', 'CLOSE', 'qwerty']);
