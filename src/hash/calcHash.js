c63dd2d3f993ac3366a47b437952da6cf279598e
Elina <elina50505051@yandex.ru>
2024-10-07 23:19:50 +0300

    feat: implement functions (hash)

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import { createHash } from 'node:crypto';
+import {fileURLToPath} from "node:url";
+import path from "node:path";
+import {createReadStream} from "node:fs";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.resolve(__dirname, 'files', 'fileToCalculateHashFor.txt');
+  const readStream = createReadStream(filePath);
+  const hash = createHash('sha256');
+
+  readStream.pipe(hash).on('finish', () => {
+    console.log(hash.digest("hex"));
+  })
 };
 
 await calculateHash();
