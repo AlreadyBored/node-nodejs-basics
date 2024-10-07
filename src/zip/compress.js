79d019127397e0837092a553232e583eaad83722
AndreyTishchenko <Tishchenko.Andrew@yandex.ru>
2024-10-08 02:12:26 +0400

    feat: done compress task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,14 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as zlib from 'zlib';
+const __dirname = import.meta.dirname
+let FilePath = path.join(__dirname, 'files', 'fileToCompress.txt');
+let Zlib =  zlib.createGzip();
 const compress = async () => {
-    // Write your code here 
+    let inStream = fs.createReadStream(FilePath);
+    let outStream = fs.createWriteStream(path.join(__dirname, 'files', 'archive.gz'));
+    let gZip = zlib.createGzip();
+    inStream.pipe(gZip).pipe(outStream);
 };
 
 await compress();
