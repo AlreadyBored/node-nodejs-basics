a1e828e92b64933da89a218c7820dfb3af063707
dariana <iadaria.best@gmail.com>
2024-10-07 21:50:48 +0400

    feat: implement read func

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,22 @@
+import { readFile } from 'node:fs/promises';
+import { pathToFiles } from './envs.js';
+import { ErrorToShow } from './libs.js';
+import { join } from 'node:path';
+
+const FILE_NAME_TO_READ = 'fileToRead1.txt';
+const fileToRead = join(pathToFiles, FILE_NAME_TO_READ);
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const contents = await readFile(fileToRead, { encoding: 'utf8'});
+    console.log(contents);
+  } catch (error) {
+    let existingError = ErrorToShow[error.code];
+    if (existingError) {
+      throw new Error(existingError);
+    }
+    console.log(error);
+  }
 };
 
 await read();
