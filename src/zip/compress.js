e1620163593d4416f04bcdc92ad6dcf536c5b799
galinavikst <galinavikst@gmail.com>
2024-10-07 14:00:00 +0300

    feat: add zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,23 @@
+import zlib from "zlib";
+import fs from "fs";
+import stream from "stream";
+
 const compress = async () => {
-    // Write your code here 
+  // Write your code here
+  const gzlib = zlib.createGzip();
+  const sourse = fs.createReadStream(
+    import.meta.dirname + "/files/fileToCompress.txt"
+  );
+  const destination = fs.createWriteStream(
+    import.meta.dirname + "/files/archive.gz"
+  );
+
+  stream.pipeline(sourse, gzlib, destination, (err) => {
+    if (err) {
+      console.log(err);
+      process.exitCode = 1; // exit with error (0 - success)
+    }
+  });
 };
 
 await compress();
