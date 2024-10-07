4ad3f453b241c57aaa43b2cb15071c867e1d97e7
mykhailojs <mykhailo.2024.ffs@gmail.com>
2024-10-07 23:25:06 +0300

    feat: finish child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,18 @@
+import { spawn } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const pathToScript = path.join(__dirname, 'files', 'script.js');
+    const spawnProcessScript = spawn('node', [pathToScript, ...args], {
+        stdio: ['pipe', 'pipe', 'pipe', 'ipc']
+    });
+    process.stdin.pipe(spawnProcessScript.stdin)
+    spawnProcessScript.stdout.pipe(process.stdout)
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ["someArgument1", "someArgument2", ] );
