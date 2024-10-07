fe7805d8a94ffb98fc890ae9f22687435296431b
samvel <s.saakyana@trainingspace.ru>
2024-10-07 11:05:18 +0400

    feat: wt subtask

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,31 @@
+import os from 'os';
+import { Worker } from 'worker_threads';
+import path from 'path';
+
 const performCalculations = async () => {
-    // Write your code here
+  const cpu_len = os.cpus().length;
+  const workers = [];
+  const results = [];
+  const file = path.join(process.cwd(), 'worker.js');
+
+  return new Promise((resolve) => {
+    for (let i = 0; i < cpu_len; i++) {
+      const worker = new Worker(file);
+      const number_to_send = 10 + i;
+
+      worker.postMessage(number_to_send);
+
+      worker.on('message', (result) => {
+        results[i] = result;
+
+        if (results.length === cpu_len) {
+          resolve(results);
+        }
+      });
+
+      workers.push(worker);
+    }
+  });
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,20 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { parentPort } from 'worker_threads';
 
-const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+// n should be received from main thread
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+
+const sendResult = (result) => {
+  // This function sends result of nthFibonacci computations to main thread
+  if (parentPort) {
+    parentPort.postMessage(result);
+  }
 };
 
-sendResult();
+// Слушаем сообщения от основного потока
+if (parentPort) {
+  parentPort.on('message', (n) => {
+    const result = nthFibonacci(n);
+    sendResult(result);
+  });
+}
