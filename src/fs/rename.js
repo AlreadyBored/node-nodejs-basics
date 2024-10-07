e3dcd9e5d78a037f97f73ec91ace12835ec56521
Gennady Kashin <gennady.kashin94@gmail.com>
2024-10-07 15:44:34 +0300

    feat: fs rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
+import path from 'path';
+import fs from 'fs/promises'
+import { fileURLToPath } from 'url';
+import { FSError } from '../utils/fs_error.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const WRONG_FILENAME = 'wrongFilename.txt';
+const PROPER_FILENAME = 'properFilename.md';
+
 const rename = async () => {
-    // Write your code here 
+    try {
+        const pathToWrongFile = path.join(__dirname, 'files', WRONG_FILENAME);
+        const pathToRenamedFile = path.join(__dirname, 'files', PROPER_FILENAME);
+        await fs.rename(pathToWrongFile, pathToRenamedFile)
+    } catch {
+        throw new FSError();
+    }
 };
 
 await rename();
