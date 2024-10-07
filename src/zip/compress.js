16a99231ee368548c87c7c09fa8348e091206f9d
DarkSpace76 <84933249+DarkSpace76@users.noreply.github.com>
2024-10-07 18:21:07 +0300

    featt: add gzip function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import path from 'node:path'
+import zip from 'node:zlib';
+import fs from 'node:fs';
+import stream from 'node:stream';
+
 const compress = async () => {
-    // Write your code here 
+    const pathToCompress = path.resolve('src', 'zip', 'files', 'fileToCompress.txt');
+    const pathToGzip = path.resolve('src', 'zip', 'files', 'archive.gz');
+
+    const gzip = zip.createGzip();
+    const source = fs.createReadStream(pathToCompress);
+    const dstn = fs.createWriteStream(pathToGzip);
+
+    stream.pipeline(source, gzip, dstn, (err) => {
+        if (err) {
+            console.log('GZip operation failed');
+            return;
+        }
+        console.log('GZip operation success');
+    });
+
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import path from 'node:path'
+import zip from 'node:zlib';
+import fs from 'node:fs';
+import stream from 'node:stream';
+
 const decompress = async () => {
-    // Write your code here 
+    const pathToDecompress = path.resolve('src', 'zip', 'files', 'fileToCompress1.txt');
+    const pathToGzip = path.resolve('src', 'zip', 'files', 'archive.gz');
+
+    const createReadStream = fs.createReadStream(pathToGzip);
+    const writeStream = fs.createWriteStream(pathToDecompress);
+    const unzip = zip.createGunzip()
+
+    createReadStream.pipe(unzip).pipe(writeStream)
+    stream.pipeline(createReadStream, unzip, writeStream, (err) => {
+        if (err) {
+            console.log('GUNZip operation failed');
+            return;
+        }
+        console.log('GUNZip operation success');
+    })
+
 };
 
 await decompress();
