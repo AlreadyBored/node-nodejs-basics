17e15ca0a1b82bc4109b6a4e547f547e15e551fd
Pavel Drazdou <p.drazdou@godeltech.com>
2024-10-07 23:46:05 +0300

    Worker Threads (src/wt)

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,28 @@
+import { Worker } from 'worker_threads';
+import path from 'path';
+import os from 'os';
+
+const runWorker = (number) => {
+  return new Promise((resolve) => {
+    const worker = new Worker(path.resolve('src/wt/worker.js'));
+    worker.on('message', (message) => {
+      resolve(message);
+    });
+    worker.postMessage(number);
+  });
+};
+
 const performCalculations = async () => {
-    // Write your code here
+  const numCPUs = os.cpus().length;
+  const promises = [];
+
+  for (let i = 0; i < numCPUs; i++) {
+    const number = 10 + i;
+    promises.push(runWorker(number));
+  }
+
+  const results = await Promise.all(promises);
+  console.log(results);
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,17 @@
+import { parentPort } from 'worker_threads';
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  parentPort.on('message', (number) => {
+    try {
+      const result = nthFibonacci(number);
+      parentPort.postMessage({ status: 'resolved', data: result });
+    } catch (error) {
+      parentPort.postMessage({ status: 'error', data: null });
+    }
+  });
 };
 
 sendResult();
