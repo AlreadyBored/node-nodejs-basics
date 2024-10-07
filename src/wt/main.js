54c16c741bb6cc81e0ebde9065d3b0b7079da0df
kaliganoff <kaliganoff@yahoo.com>
2024-10-07 17:39:21 +0300

    feat: Add main and worker

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,30 @@
+import { cpus } from "os";
+import { Worker } from "worker_threads";
+
 const performCalculations = async () => {
-    // Write your code here
+    const cpuss = cpus();
+    let number = 10;
+    let completedWorkers = 0;
+    const resultArray = [];
+    for (let cpu of cpuss) {
+        function logIfDone() {
+            completedWorkers++;
+            if (cpuss.length === completedWorkers) {
+                console.log(resultArray);
+            }
+        }
+        const worker = new Worker("./src/wt/worker.js");
+        worker.postMessage(number++);
+        worker.on('message', (result) => {
+            resultArray[result[0] - 10] = { status: 'resolved', data: result[1]};
+            logIfDone();
+        });
+        worker.on('error', (result) => {
+            resultArray[result[0] - 10] = { status: 'error', data: null};
+            logIfDone();
+        })
+    }
+   
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,15 @@
+import { parentPort } from 'worker_threads';
+ 
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
-const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+
+
+const sendResult = (result) => {
+    parentPort.postMessage(result);
 };
 
-sendResult();
+parentPort.on('message', (number) => {
+    const result = nthFibonacci(number);
+    sendResult([number, result]);
+})
