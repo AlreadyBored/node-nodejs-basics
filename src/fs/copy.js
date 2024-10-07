f899bb9a97ea4179bd4eacbdd41270ad6bf543d1
Eduardo Barbosa <edo@mothervolcano.com>
2024-10-07 11:22:47 +0100

    complete copy task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,25 @@
+import { mkdir, readdir, copyFile } from "fs/promises";
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        const files = await readdir("./files");
+        await mkdir("./files_copy");
+
+        const promises = files.map( f => {
+            copyFile(`./files/${f}`, `./files_copy/${f}`);
+        });
+
+        Promise.all(promises);
+
+    } catch (error) {
+        if (error.code === "EEXIST" && error.path === "./files_copy") {
+            throw new Error("FS operation failed: <files_copy> folder already exists")
+        } else if (error.code === "ENOENT" && error.path === "./files") {
+            throw new Error("FS operation failed: <files> folder doesn't exist");
+        } else {
+            throw error;
+        }
+    }
 };
 
 await copy();
