e1704457f2e83b52ef0c69ccade70b282263d5ab
ssstarss <s.s.star@mail.ru>
2024-10-07 20:45:50 +0700

    last edition

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,9 +1,17 @@
-import childProcess from('child_process')
+import { execFile } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const spawnChildProcess = async (args) => {
-    
+  const currentDir = fileURLToPath(import.meta.url);
+  const pathToChild = path.join(path.dirname(currentDir), 'files', 'script.js');
+  //const childProcess = spawn(pathToChild, [...args], {});
+  const child = execFile(pathToChild, args, (error, stdout, stderr) => {
+    if (error) {
+      console.log('err');
+    }
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
-
+spawnChildProcess([25, 56, 778]);
--- src/modules/Esm.mjs
+++ src/modules/Esm.mjs
@@ -3,7 +3,6 @@ import { release, version } from 'os';
 import { createServer as createServerHttp } from 'http';
 import './files/c.js';
 import { fileURLToPath } from 'url';
-import process from 'process';
 
 const random = Math.random();
 
@@ -20,7 +19,7 @@ console.log(`Version ${version()}`);
 console.log(`Path segment separator is "${path.sep}"`);
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = process.cwd();
+const __dirname = path.dirname(__filename);
 
 console.log(`Path to current file is ${__filename}`);
 console.log(`Path to current directory is ${__dirname}`);
