09124015acb0d1a5fba1f100456b3921a07ab943
vera-mal <k(k(kkkpG286(ABGDzmq}>
2024-10-07 20:10:38 +0200

    feat: implement reverse text

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
-const transform = async () => {
-    // Write your code here 
+import { Transform } from 'stream';
+
+    const transform = async () => {
+
+    const readable = process.stdin;
+    const writable = process.stdout;
+
+    const tr = new Transform({
+        transform(chunk, enc, cb) {
+            cb(null, chunk.toString().trim().split('').reverse().join('') + '\n');
+        }
+    });
+
+    readable.pipe(tr).pipe(writable);
 };
 
 await transform();
