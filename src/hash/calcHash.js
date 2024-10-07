259ff034e5eee69381ef8bbe281dee33445f3551
diyora <diyoramekhmonova@gmail.com>
2024-10-07 14:03:08 +0800

    feat: hashing tasks

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
+import { Writable } from "stream";
+import { createHash } from "crypto";
+import fs from "fs";
+
 const calculateHash = async () => {
-    // Write your code here 
+  fs.readFile("./files/fileToCalculateHashFor.txt", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    let hashed = createHash("sha256").update("diyora").digest();
+    // one way
+    let toLog = Buffer.from(hashed).toString("hex");
+    console.log(toLog);
+
+    // second way
+    const hexStream = new Writable({
+      write(chunk, encoding, callback) {
+        console.log(chunk.toString("hex"));
+        callback();
+      },
+    });
+    hexStream.write(hashed);
+  });
 };
 
 await calculateHash();
