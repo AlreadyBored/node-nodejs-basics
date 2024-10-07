d48567345ce390e50136ee53527d178e55bb2232
vera-mal <k(k(kkkpG286(ABGDzmq}>
2024-10-07 19:44:26 +0200

    feat: implement write to file

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import { createWriteStream } from 'fs';
+
 const write = async () => {
-    // Write your code here 
+    const writable = createWriteStream('./files/fileToWrite.txt')
+
+    console.log('Type something and press Enter:');
+    process.stdin.on('data', data => { 
+        writable.write(data.toString());
+        process.exit(); 
+    });
 };
 
 await write();
