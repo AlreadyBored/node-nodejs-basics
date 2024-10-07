6659d1ca9993be270690b7c2100d3978200467db
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-10-08 04:54:08 +0500

    Try to solve Worker Threads task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,44 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
+
 const performCalculations = async () => {
-    // Write your code here
+  const countCPUs = os.cpus().length;
+  const results = new Array(countCPUs);
+  let workersDone = 0;
+
+  for (let i = 0; i < countCPUs; i++) {
+    const worker = new Worker('./worker.js');
+    const n = 10 + i;
+
+    worker.on('message', (result) => {
+      results[i] = { status: 'resolved', data: result};
+      workersDone++;
+      if (workersDone === countCPUs) {
+        console.log(results);
+        process.exit(0);
+      }
+    });
+
+    worker.on('error', (error) => {
+      results[i] = { status: 'error', data: null };
+      workersDone++;
+      if (workersDone === countCPUs) {
+        console.log(results);
+        process.exit(0);
+      }
+    });
+
+    worker.on('exit', (code) => {
+      results[i] = { status: 'error', data: null };
+      workersDone++;
+      if (workersDone === countCPUs) {
+        console.log(results);
+        process.exit(0);
+      }
+    });
+
+    worker.postMessage(n);
+  }
 };
 
 await performCalculations();
