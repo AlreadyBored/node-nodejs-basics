9d20d4a8bb1d689fa368acfdcfef16af1dd9394e
Aliaksei Varabyou <aliaksei_varabyou@epam.com>
2024-10-07 15:55:24 +0200

    zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,26 @@
+/*
+* implement function that compresses file fileToCompress.txt to archive.gz using zlib and Streams API
+*/
+import { createGzip } from 'node:zlib';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream';
+
 const compress = async () => {
-    // Write your code here 
+    const filePath = './src/zip/files/fileToCompress.txt';
+    const fileZip = './src/zip/files/archive.gz';
+
+    const gzip = createGzip();
+    const source = createReadStream(filePath);
+    const destination = createWriteStream(fileZip);
+
+    pipeline(source, gzip, destination, (err) => {
+        if (err) {
+          console.error('An error occurred:', err);
+          process.exitCode = 1;
+        } else {
+            console.log('File was successfully compressed!')
+        }
+    });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,27 @@
+/*
+* implement function that decompresses archive.gz back to the fileToCompress.txt
+* with same content as before compression using zlib and Streams API
+*/
+import { createGunzip } from 'node:zlib';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream';
+
 const decompress = async () => {
-    // Write your code here 
+    const filePath = './src/zip/files/fileToCompress.txt';
+    const fileZip = './src/zip/files/archive.gz';
+
+    const gunzip = createGunzip();
+    const source = createReadStream(fileZip);
+    const destination = createWriteStream(filePath);
+
+    pipeline(source, gunzip, destination, (err) => {
+        if (err) {
+          console.error('An error occurred:', err);
+          process.exitCode = 1;
+        } else {
+            console.log('File was successfully decompressed!')
+        }
+    });
 };
 
 await decompress();
