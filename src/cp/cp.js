f6e6bf07c7f5cdd4aeeabb14c3a41d72283d3153
diyora <diyoramekhmonova@gmail.com>
2024-10-07 21:30:58 +0800

    feat: child process tasks

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,13 @@
+import { fork, spawn } from "child_process";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = fork("./files/script.js", args, {
+    stdio: ["pipe", "pipe", "pipe", "ipc"],
+  });
+
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["someArgument1", "someArgument2"]);
