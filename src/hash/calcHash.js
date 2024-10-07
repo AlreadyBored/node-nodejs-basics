12eb632b3cd3fea4eb6169f386c06196bee0d68a
renat <murzinarenata@gmail.com>
2024-10-07 12:41:17 -0700

    feat: add calcHAsh

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+import crypto from 'crypto';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+    try {
+        const filePath = path.join(dirname, 'files', 'fileToCalculateHashFor.txt');
+        const text = fs.readFileSync(filePath);
+        const hash = crypto.createHash('sha256');
+        hash.update(text);
+        console.log(hash.digest('hex'));
+    } catch (error) {
+        throw new error
+    }
 };
 
 await calculateHash();
