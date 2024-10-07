739891bc18c198ccb4e37377f56b02a3577ba64d
Тыщук Вячеслав <tischook@yandex.ru>
2024-10-08 01:54:38 +0300

    feat: create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import { writeFile } from 'node:fs/promises';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const DATA = 'I am fresh and young';
+
+const fileName = fileURLToPath(import.meta.url);
+const __dirname = dirname(fileName);
+
+const filePath = join(__dirname, 'files', 'fresh.txt');
+
 const create = async () => {
-    // Write your code here 
+    try {
+        await writeFile(filePath, DATA, { flag: 'wx'});
+    } catch(err) {
+        console.error('FS operation failed');
+    }
+    
 };
 
 await create();
