6560c444469b220bb4745765badc56c95e2458c5
Eduardo Barbosa <edo@mothervolcano.com>
2024-10-07 16:18:54 +0100

    complete stream:transform task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,13 @@
 const transform = async () => {
-    // Write your code here 
+    try {
+        process.stdin.setEncoding('utf-8');
+        process.stdin.on("data", (input) => {
+            const reversedInput = input.trim().split('').reverse().join('');
+            process.stdout.write(`${reversedInput}\n`);
+        })
+    } catch (error) {
+        throw error;
+    }
 };
 
 await transform();
