0e407f9646f88fa5cf825379ba059e6ce8cf03a6
diyora <diyoramekhmonova@gmail.com>
2024-10-07 09:53:32 +0800

    feat: module tasks

---
+++ src/modules/cjsToEsm.mjs
@@ -0,0 +1,39 @@
+import "./files/c.js";
+
+import { release, version } from "os";
+
+import { createServer as createServerHttp } from "http";
+import path from "path";
+
+const random = Math.random();
+let unknownObject;
+
+(async () => {
+  if (random > 0.5) {
+    unknownObject = await import("./files/a.json", { with: { type: "json" } });
+    console.log("unknownObject", unknownObject);
+  } else {
+    unknownObject = await import("./files/b.json", { with: { type: "json" } });
+    console.log("unknownObject", unknownObject);
+  }
+})();
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${path.sep}"`);
+
+console.log(`Path to current file is ${import.meta.filename}`);
+console.log(`Path to current directory is ${import.meta.dirname}`);
+
+const myServer = createServerHttp((_, res) => {
+  res.end("Request accepted");
+});
+
+const PORT = 3000;
+
+myServer.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+  console.log("To terminate it, use Ctrl+C combination");
+});
+
+export { unknownObject, myServer };
