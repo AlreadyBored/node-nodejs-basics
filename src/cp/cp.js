44ac669cc36514a3ada430b237b7d8105a097f60
renat <murzinarenata@gmail.com>
2024-10-07 13:01:27 -0700

    feat: add cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,21 @@
+import { spawn } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = spawn('node', [path.join(dirname, 'files', 'script.js'), ...args], {
+        stdio: ['pipe', 'pipe', 'pipe']
+    });
+
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
+
+    child.on('exit', (code) => {
+        console.log(`Child process exited with code ${code}`);
+    });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ['Привет', 'Хорошего', 'Тебе', 'Дня :)']);
