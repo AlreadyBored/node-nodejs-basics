f191585bedb312c1e0ff9ee8f6c065994f958586
Juan David Amaya <jamaya@serempre.com>
2024-10-07 18:47:51 -0500

    Subtask: Worker Threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,34 @@
+import { Worker } from "worker_threads";
+import os from "os";
+
+const workerPath = new URL("worker.js", import.meta.url);
+
 const performCalculations = async () => {
-    // Write your code here
+	const number = 10;
+    const promises = [];
+	for (let i = 0; i < os.cpus().length; i++) {
+		const promise = new Promise((resolve) => {
+			const worker = new Worker(workerPath, {
+				workerData: number + i,
+			});
+
+			worker.on("message", (data) => {
+				resolve({
+				    status: "resolved",
+					data,
+				});
+			});
+
+			worker.on("error", () => {
+				resolve({
+					status: "error",
+					data: null,
+				});
+			});
+		});
+        promises.push(promise);
+	}
+    console.log(await Promise.all(promises));
 };
 
 await performCalculations();
