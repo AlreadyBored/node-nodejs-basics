13f8eb3c9ac11bd49dcff31f928e2d0714c37ac6
AndreyTishchenko <Tishchenko.Andrew@yandex.ru>
2024-10-08 02:03:22 +0400

    feat: done copy task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,17 @@
+import * as fs from 'fs';
+import * as path from 'path';
+const __dirname = import.meta.dirname
+let { COPYFILE_EXCL } = fs.constants;
+let FilePath = path.join(__dirname, 'files_copy');
 const copy = async () => {
-    // Write your code here 
+    fs.cp(path.join(__dirname, 'files'), path.join(__dirname, 'files_copy'), {
+        recursive: true,
+        force: false,
+        errorOnExist: true,
+      }, (err)=>{
+        if(err) throw err;
+    })
+
 };
 
 await copy();
