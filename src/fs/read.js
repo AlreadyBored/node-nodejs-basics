c11bcfabdcc0f204dc04cfc4ac33b45250d91ba0
AndreyTishchenko <Tishchenko.Andrew@yandex.ru>
2024-10-08 02:06:34 +0400

    feat: done read task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,12 @@
+import * as fs from 'fs';
+import * as path from 'path';
+const __dirname = import.meta.dirname
+let FilePath = path.join(__dirname, 'files', 'fileToRead.txt');
 const read = async () => {
-    // Write your code here 
+    fs.readFile(FilePath, 'utf8', function(err, data) {
+        if (err) throw Error('FS operation failed');
+        console.log(data)
+    });
 };
 
 await read();
