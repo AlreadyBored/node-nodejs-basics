3829cbf2366c41e635e57c1103399eefedccb0be
vera-mal <k(k(kkkpG286(ABGDzmq}>
2024-10-07 16:48:20 +0200

    feat: implement function that prints content of file

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,13 @@
+import fs from 'fs';
+
 const read = async () => {
-    // Write your code here 
+    fs.readFile('./files/fileToRead.txt', 'utf8', (err, data) => {
+        if (err?.code === 'ENOENT') {
+            throw Error ('FS operation failed');
+        } else if (data) {
+            console.log(data);            
+        }
+    });
 };
 
 await read();
