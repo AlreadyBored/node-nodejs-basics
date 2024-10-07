28753becdeac31d64eea1d9ee4d83dfbe59b4b89
Edward <e.arechwa@gmail.com>
2024-10-07 09:13:23 +0200

    feat: implement compress file

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,28 @@
+import fs from 'fs';
+import zlib from 'zlib';
+import path from 'path';
+
+// there was error about __dirname, get this solution from gpt
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const compress = async () => {
     // Write your code here 
+    const inputFilePath = path.join(__dirname, 'files', 'fileToCompress.txt'); 
+    const outputFilePath = path.join(__dirname, 'files', 'archive.gz'); 
+
+    const readableStream = fs.createReadStream(inputFilePath);
+    
+    const writableStream = fs.createWriteStream(outputFilePath);
+
+    const gzip = zlib.createGzip();
+
+    readableStream
+        .pipe(gzip) 
+        .pipe(writableStream) 
+        .on('finish', () => console.log('File successfully compressed.')) 
+        .on('error', (err) => console.error('Error during compression:', err));
 };
 
 await compress();
