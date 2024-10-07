aea10f767daa7e65f47b967bb0a5103f5dee97b5
Gennady Kashin <gennady.kashin94@gmail.com>
2024-10-07 15:48:08 +0300

    feat: fs delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import path from 'path';
+import fs from 'fs/promises'
+import { fileURLToPath } from 'url';
+import { FSError } from '../utils/fs_error.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const FILENAME_TO_REMOVE = 'fileToRemove.txt';
 const remove = async () => {
-    // Write your code here 
+    try {
+        const pathToFileToDelete = path.join(__dirname, 'files', FILENAME_TO_REMOVE);
+        await fs.rm(pathToFileToDelete);
+    } catch {
+        throw new FSError();
+    }
 };
 
 await remove();
