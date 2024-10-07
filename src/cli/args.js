86252c7534fcda163db8b052720bcd1cbb0e6b89
mykhailojs <mykhailo.2024.ffs@gmail.com>
2024-10-07 19:39:02 +0300

    feat: finish cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
 const parseArgs = () => {
-    // Write your code here 
+    let currentIndex;
+    const args = process.argv.filter((arg, index) => {
+        if (arg.startsWith('--')) {
+            currentIndex = index;
+            return true;
+        }
+        return index === currentIndex + 1;
+    })
+    for (let i = 0; i < args.length; i += 2) {
+        const propName = args[i].replace('--', '');
+        const value = args[i + 1];
+        console.log(`${propName} is ${value}`);
+    }
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+    const processEnv = process.env
+    const env = Object.keys(processEnv)
+        .filter((val) => val.startsWith('RSS_')).map((key) => {
+        return `${key}=${processEnv[key]}`
+    }).join('; ');
+    console.log(env);
 };
 
 parseEnv();
