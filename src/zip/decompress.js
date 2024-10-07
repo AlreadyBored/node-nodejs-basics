35918f5dfefb06f80b8244fa75fcf4238b2c8b9e
Edward <e.arechwa@gmail.com>
2024-10-07 09:15:33 +0200

    feat: implement decompress file

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,28 @@
+import fs from 'fs';
+import zlib from 'zlib';
+import path from 'path';
+
+// there was error about __dirname, get this solution from gpt
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const decompress = async () => {
     // Write your code here 
+    const inputFilePath = path.join(__dirname, 'files', 'archive.gz'); 
+    const outputFilePath = path.join(__dirname, 'files', 'fileToCompress.txt'); 
+
+    const readableStream = fs.createReadStream(inputFilePath);
+    
+    const writableStream = fs.createWriteStream(outputFilePath);
+
+    const gunzip = zlib.createGunzip();
+
+    readableStream
+        .pipe(gunzip) 
+        .pipe(writableStream) 
+        .on('finish', () => console.log('File successfully decompressed.')) 
+        .on('error', (err) => console.error('Error during decompression:', err)); 
 };
 
 await decompress();
