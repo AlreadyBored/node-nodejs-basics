723e37f9a8b43df24c97bbb1f69ef30114baa3e7
Maxim Semikov <maxim.semikov87@gmail.com>
2024-10-07 13:38:49 +0500

    feat: Hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,30 @@
+import fs from 'fs';
+import {join} from 'path'
+import crypto from 'crypto';
+import {getExecutedFileDirname} from "../../helpers.js";
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = join(getExecutedFileDirname(import.meta.url), 'files', 'fileToCalculateHashFor.txt') ;
+    try {
+        const hash = crypto.createHash('sha256');
+        const readStream = fs.createReadStream(filePath);
+
+        readStream.on('data', (chunk)=> {
+            hash.update(chunk);
+        })
+
+        readStream.on('end', () => {
+            console.log(hash.digest('hex'));
+        })
+
+        readStream.on('error', (err) => {
+            console.log(`Error reading file: ${err}`);
+        });
+
+    } catch (err) {
+        console.log('ERROR: ', err)
+    }
+
 };
 
 await calculateHash();
