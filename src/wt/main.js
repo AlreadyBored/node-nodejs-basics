9b9c35a184578a42c3b7cc89939aba74ab1af60d
Maxim Semikov <maxim.semikov87@gmail.com>
2024-10-07 14:14:39 +0500

    feat: Worker Threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,27 @@
+import os from "os";
+import { Worker } from "worker_threads";
+import { join } from "path";
+import {getExecutedFileDirname} from "../../helpers.js";
+
+const workerFilePath = join(getExecutedFileDirname(import.meta.url), "worker.js");
+
 const performCalculations = async () => {
-    // Write your code here
+  const cpus = os.cpus();
+  let nthFibonacciNumber = 10;
+
+  const workersResults = await Promise.all(
+    cpus.map(() => {
+      return new Promise(resolve => {
+        const worker = new Worker(workerFilePath, {
+          workerData: nthFibonacciNumber++,
+        });
+        worker.on("message", data => resolve({ status: "resolved", data }));
+        worker.on("error", () =>  resolve({ status: "error", data: null }));
+      });
+    })
+  );
+
+  console.log(workersResults);
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,11 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { parentPort, workerData } from "worker_threads";
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  if (Math.random() > 0.5) throw new Error("Something went wrong");
+
+  parentPort.postMessage(nthFibonacci(workerData));
 };
 
 sendResult();
