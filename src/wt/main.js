8480102bb54ccf9320e5b8f25421eff83f2c4978
mykhailojs <mykhailo.2024.ffs@gmail.com>
2024-10-07 22:48:01 +0300

    feat: finish wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,29 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
 const performCalculations = async () => {
-    // Write your code here
+    const cores = os.cpus().length;
+    const currentWorkers = [];
+    for (let i = 0; i < cores; i++) {
+        const messageToProcess = 10 + i;
+        currentWorkers.push(
+            new Promise((resolve) => {
+                const worker = new Worker('./worker.js', { workerData: messageToProcess });
+                worker.on('message', (data) => {
+                    resolve({ status: 'resolved', data });
+                });
+                worker.on('error', () => {
+                    resolve({ status: 'error', data: null });
+                });
+                worker.on('exit', (code) => {
+                    if (code !== 0) {
+                        resolve({ status: 'error', data: null });
+                    }
+                });
+            })
+        );
+    }
+    const getFinalResult = await Promise.all(currentWorkers);
+    console.log(getFinalResult);
 };
 
 await performCalculations();
