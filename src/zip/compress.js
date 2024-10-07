e4c182ef228bd5e88c8523c1db0c0b94f173d63f
kaliganoff <kaliganoff@yahoo.com>
2024-10-07 14:57:59 +0300

    feat: Add compress & decompress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import { createGzip } from 'zlib';
+import { pipeline }from 'stream';
+import {
+  createReadStream,
+  createWriteStream,
+} from 'fs';
+
 const compress = async () => {
-    // Write your code here 
+    const gzip = createGzip();
+    const source = createReadStream('src/zip/files/fileToCompress.txt');
+    const destination = createWriteStream('src/zip/files/archive.gz');
+
+    pipeline(source, gzip, destination, (err) => {
+        if (err) {
+          console.error('An error occurred:', err);
+          process.exitCode = 1;
+        }
+      });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import { createGunzip } from 'zlib';
+import { pipeline }from 'stream';
+import {
+  createReadStream,
+  createWriteStream,
+} from 'fs';
+
 const decompress = async () => {
-    // Write your code here 
+    const gunzip = createGunzip();
+    const source = createReadStream('src/zip/files/archive.gz');
+    const destination = createWriteStream('src/zip/files/fileToCompress.txt');
+
+    pipeline(source, gunzip, destination, (err) => {
+        if (err) {
+          console.error('An error occurred:', err);
+          process.exitCode = 1;
+        }
+      });
 };
 
 await decompress();
