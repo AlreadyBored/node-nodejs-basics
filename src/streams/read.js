8cbb33202472788eb29c3d66a882bcd23b34aab1
vera-mal <k(k(kkkpG286(ABGDzmq}>
2024-10-07 19:50:12 +0200

    feat: implement reading from file

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,11 @@
+import { createReadStream } from 'fs';
+
 const read = async () => {
-    // Write your code here 
+    const readableStream = createReadStream('./files/fileToRead.txt');
+
+    readableStream.on('data', (chunk) => {
+        process.stdout.write(chunk.toString());
+    });
 };
 
 await read();
