a9f76144a38ed2e6d500e9984dc9f2b0986c5848
Eduardo Barbosa <edo@mothervolcano.com>
2024-10-07 21:22:18 +0100

    complete zip:decompress task

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,39 @@
+import fs from 'fs/promises';
+import { createGunzip } from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+    try {
+        const fhRead = await fs.open('./files/archive.gz');
+        const fhWrite = await fs.open('./files/fileDecompressed.txt', 'w');
+        
+        const rstream = fhRead.createReadStream();
+        const decompressTransform = createGunzip();
+        const wstream = fhWrite.createWriteStream();
+
+        rstream.on('data', (chunk) => {
+            decompressTransform.write(chunk);
+        });
+
+        decompressTransform.on('data', (chunk) => {
+            wstream.write(chunk);
+        });
+
+        rstream.on('end', () => {
+            decompressTransform.end();
+        });
+
+        decompressTransform.on('end', () => {
+            wstream.end();
+        });
+
+        wstream.on('end', async () => {
+            await fhRead.close();
+            await fhWrite.close();
+        });
+
+    } catch (error) {
+        throw error;
+    }
 };
 
 await decompress();
