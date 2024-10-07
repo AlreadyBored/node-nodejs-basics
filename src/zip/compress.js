0d367b2704ea81fc3bdc9165a27a0eee107dd8a3
MaksimNK <maksmozir@gmail.com>
2024-10-07 03:02:51 +0300

    implement zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,37 @@
+import fs from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+import { pipeline } from 'node:stream';
+import path  from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+import zlib from 'node:zlib';
+
+
 const compress = async () => {
-    // Write your code here 
+    try {
+
+        const pathToFile = path.join(__dirname, 'files', 'fileToCompress.txt');
+        const pathArchive = path.join(__dirname, 'files', 'archive.gz');
+        
+        const readStream = fs.createReadStream(pathToFile);
+        const gzip = zlib.createGzip();
+        const writeStream = fs.createWriteStream(pathArchive);
+    
+    
+        pipeline(readStream, gzip, writeStream, (err) => {
+            if(err) {
+                console.log(err);
+            }
+        });
+
+    } catch (error) {
+        throw error;
+    }
+
+
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,33 @@
+import fs from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+import { pipeline } from 'node:stream';
+import path  from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+import zlib from 'node:zlib';   
+
 const decompress = async () => {
-    // Write your code here 
+    try {
+        const pathToFile = path.join(__dirname, 'files', 'fileToCompress.txt');
+        const pathArchive = path.join(__dirname, 'files', 'archive.gz');
+
+        const readStream = fs.createReadStream(pathArchive);
+        const gunzip = zlib.createGunzip();
+        const writeStream = fs.createWriteStream(pathToFile);
+        
+        pipeline(readStream, gunzip, writeStream, (err) => {
+            if(err) {
+                console.log(err);
+            }
+        });
+
+    } catch (error) {
+        throw error;
+    }
+
 };
 
 await decompress();
