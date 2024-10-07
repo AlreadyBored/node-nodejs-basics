f6963e47ffffb084d9c5f48baf8edb22bd71d63b
Aliaksei Varabyou <aliaksei_varabyou@epam.com>
2024-10-07 15:29:09 +0200

    hash and streams

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,27 @@
+/*
+* implement function that calculates SHA256 hash for file fileToCalculateHashFor.txt 
+* and logs it into console as hex using Streams API
+*/
+import { createHash } from 'node:crypto';
+import fs from 'node:fs';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = "./src/hash/files/fileToCalculateHashFor.txt"
+    let content = '';
+
+    fs.readFile(
+        filePath,
+        "utf8",
+        (err, data) => {
+            if (err) throw err;
+            else content = data;
+        }
+    );
+    const result = createHash('sha256')
+        .update(content)
+        .digest('hex');
+
+    console.log(result);
 };
 
 await calculateHash();
