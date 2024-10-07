241ad7862978cd81fafce8777e8d207b7084c36c
kaliganoff <kaliganoff@yahoo.com>
2024-10-07 20:58:01 +0300

    feat: Add cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,13 @@
+import { fork } from 'child_process'
+import { stdout } from 'process';
+import os from 'os';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const forkedChildProcess = fork('src/cp/files/script.js', args);
+    forkedChildProcess.on('message', (message) => {
+        stdout.write(message);
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2']);
