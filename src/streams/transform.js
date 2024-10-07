48e1edb5fc3bdb4f6f7f85beadbd2443865ca7c1
AndreyTishchenko <Tishchenko.Andrew@yandex.ru>
2024-10-08 02:11:06 +0400

    feat: done readTransform task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+import * as fs from 'fs';
+import * as process from 'process';
+import * as path from 'path';
+import { Transform } from 'stream';
+const __dirname = import.meta.dirname
+let FilePath = path.join(__dirname, 'files', 'fileToRead.txt');
+const reverse = new Transform({
+    transform(chunk, encoding, callback) {
+        this.push(chunk.toString().split('').reverse().join(''));
+        callback();
+    }
+});
 const transform = async () => {
-    // Write your code here 
+    process.stdin.pipe(reverse).pipe(process.stdout);
 };
 
 await transform();
