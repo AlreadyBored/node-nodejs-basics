7d16f3eaba95d7fb1b5221ba309b669069cd1ba5
renat <murzinarenata@gmail.com>
2024-10-07 12:13:23 -0700

    feat: add copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,23 @@
+import fsPromises from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
 const copy = async () => {
-    // Write your code here 
+    const sourDir = path.join(dirname, 'files');
+    const targetDir = path.join(dirname, 'files_copy');
+
+    try {
+        await fsPromises.access(sourDir);
+        await fsPromises.access(targetDir);
+        throw new Error('FS operation failed');
+    } catch (e) {
+        if (e.code !== 'ENOENT') throw new Error('FS operation failed');
+    }
+
+    await fsPromises.cp(sourDir, targetDir, { recursive: true });
 };
 
 await copy();
