cd7d2eb24443606c169ef999119352b2b6e6a8bb
mykhailojs <mykhailo.2024.ffs@gmail.com>
2024-10-07 20:41:20 +0300

    feat: finish hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const calculateHash = async () => {
-    // Write your code here 
+    const pathToFile = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+    const readSteamFile = createReadStream(pathToFile);
+
+    readSteamFile.on('data', (chunk) => {
+        createHash('sha256').update(chunk);
+    });
+
+    readSteamFile.on('end', () => {
+        console.log(createHash('sha256').digest('hex'));
+    });
 };
 
 await calculateHash();
