b7056c2b936b8b32f54686fd7a63b85f528560c5
vera-mal <k(k(kkkpG286(ABGDzmq}>
2024-10-07 19:17:39 +0200

    feat: implement function that parses command line arguments

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+    let args = process.argv.slice(2);
+    
+    let output = [];
+    for (let i = 0; i < args.length; i += 2) {
+        output = [...output, args[i].slice(2) + ' is ' + args[i + 1]];
+    }
+    
+    console.log(output.join(', '));
+    
 };
 
 parseArgs();
