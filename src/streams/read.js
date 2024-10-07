993bca5ca22512887abcd5087847b66191e3c4ab
renat <murzinarenata@gmail.com>
2024-10-07 12:43:12 -0700

    feat: add read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
 const read = async () => {
-    // Write your code here 
+    const filePath = path.join(dirname, 'files', 'fileToRead.txt');
+    const readStream = fs.createReadStream(filePath, { encoding: 'utf8' });
+    readStream.pipe(process.stdout);
+    readStream.on('error', console.error);
 };
 
 await read();
