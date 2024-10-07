3e191a4225c5ccb88e4e5836343c9e51e8e7c363
0404233 <andrey0404233@gmail.com>
2024-10-07 23:55:21 +0300

    feat: implement function copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,18 @@
+import { access, cp } from 'fs/promises';
+import { constants } from 'fs';
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        try {
+            await access('./files_copy', constants.F_OK);
+            console.log('FS operation failed');
+          } catch (err) {
+            await cp('./files', './files_copy', { recursive: true });
+            console.log('Folder copied');
+          }
+      } catch (err) {
+        console.error('FS operation failed');
+      } 
 };
 
 await copy();
