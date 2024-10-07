545b5d48314ab769b8fc7a8b1e4f6841a8765bb4
0404233 <andrey0404233@gmail.com>
2024-10-08 00:37:17 +0300

    fest: implement function rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,13 @@
+import { promises as fs, access } from 'fs';
+
 const rename = async () => {
-    // Write your code here 
+    try {
+        await fs.rename('./files/wrongFilename.txt', './files/properFilename.md');
+        console.log('File renamed successfully');
+
+    } catch (err) {
+        console.error('FS operation failed');
+    }
 };
 
 await rename();
