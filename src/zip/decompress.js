71e409137cbc71dee80ebf2c0ba316bc473f6848
renat <murzinarenata@gmail.com>
2024-10-07 12:46:58 -0700

    feat: add decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,32 @@
+import fs from 'fs';
+import zlib from 'zlib';
+import path from 'path';
+import { pipeline } from 'stream';
+import { fileURLToPath } from 'url';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
 const decompress = async () => {
-    // Write your code here 
+    const inputFile = path.join(dirname, 'files', 'archive.gz');
+    const outputFile = path.join(dirname, 'files', 'fileToCompress.txt');
+
+    const readStream = fs.createReadStream(inputFile);
+    const writeStream = fs.createWriteStream(outputFile);
+    const gzip = zlib.createGunzip();
+
+    pipeline(
+        readStream,
+        gzip,
+        writeStream,
+        (err) => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log('Файл успешно распакован');
+            }
+        }
+    );
 };
 
 await decompress();
