1f0e6bfacd4a4f60e8987bc3c822271cbc5aed25
Gennady Kashin <gennady.kashin94@gmail.com>
2024-10-07 20:03:17 +0300

    feat: streams complete

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+    const transformStream = new Transform({
+        transform(chunk, _encoding, callback) {
+          const reversedChunk = chunk.toString().split('').reverse().join('');
+          callback(null, reversedChunk);
+        },
+      });
+    
+      process.stdin.pipe(transformStream).pipe(process.stdout);
 };
 
 await transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import path from 'path';
+import fs from 'fs'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const FILENAME_TO_WRITE = 'fileToWrite.txt';
+
 const write = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files', FILENAME_TO_WRITE);
+    const writeStream = fs.createWriteStream(filePath);
+    process.stdin.pipe(writeStream);
 };
 
 await write();
