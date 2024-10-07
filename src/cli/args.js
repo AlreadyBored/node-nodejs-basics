90774d38454180daf658233915e8d399352cabc6
DarkSpace76 <84933249+DarkSpace76@users.noreply.github.com>
2024-10-07 16:33:06 +0300

    feat: add function read envs and args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+
+    process.argv.forEach((val, idx, arr) => {
+        if (val.includes('--')) {
+            console.log(val.substring(2), 'is', arr[idx + 1]);
+        }
+
+    });
+
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
-};
+
+    Object.entries(process.env).map(([key, value]) => {
+        if (key.includes('RSS_')) {
+            console.log(key, '=', value);
+        }
+
+    });
+
+
+}
 
 parseEnv();
