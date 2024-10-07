d660a1ed35846a886cebf50b220af9f2514d81f2
Gennady Kashin <gennady.kashin94@gmail.com>
2024-10-07 20:14:46 +0300

    feat: worker threads complete

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,32 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import {availableParallelism} from 'os';
+import { Worker } from 'worker_threads';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const WORKER_PATH = path.join(__dirname, 'worker.js');
+
 const performCalculations = async () => {
-    // Write your code here
+    const availableCores = availableParallelism();
+    const workers = [];
+    for (let i = 0; i < availableCores; i += 1) {
+        workers.push(workerFactory(i + 5));
+    }
+    const result = await Promise.allSettled(workers).then((res) =>
+        res.map((obj) => obj.value));
+    
+    console.log(result);
+    
 };
+
+const workerFactory = (n) => {
+    return new Promise((resolve, reject) => {
+        const worker = new Worker(WORKER_PATH, {workerData: n});
+        worker.on("message", (msg) => resolve(msg));
+        worker.on("error", (err) => reject(err));
+    })
+}
 
 await performCalculations();
