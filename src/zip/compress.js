d2688f1b1347d2311937de39f6750223e4f9fa26
Yury Kuzmich <yurakuzmich@gmail.com>
2024-10-07 16:21:36 +0500

    feat compress files

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,29 @@
+import fs from 'fs';
+import path from 'path';
+import url from 'url';
+import zlib from 'zlib';
+
 const compress = async () => {
     // Write your code here 
+    const __filename = url.fileURLToPath(import.meta.url);
+    const file = path.join(path.dirname(__filename), 'files', 'fileToCompress.txt');
+    const output = path.join(path.dirname(__filename), 'files', 'archive.gz');
+
+    const gzip = zlib.createGzip();
+    const readStream = fs.createReadStream(file);
+    const writeStream = fs.createWriteStream(output);
+
+    writeStream.on('finish', () => {
+        console.log('Compressed.');
+    });
+    readStream.on('error', (err) => {
+        console.error('Error while reading file:', err);
+    });
+    writeStream.on('error', (err) => {
+        console.error('Error while writing compressed file:', err);
+    });
+
+    readStream.pipe(gzip).pipe(writeStream);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,29 @@
+import fs from 'fs';
+import path from 'path';
+import url from 'url';
+import zlib from 'zlib';
+
 const decompress = async () => {
     // Write your code here 
+    const __filename = url.fileURLToPath(import.meta.url);
+    const file = path.join(path.dirname(__filename), 'files', 'archive.gz');
+    const output = path.join(path.dirname(__filename), 'files', 'fileToCompress.txt');
+
+    const gzip = zlib.createGunzip();
+    const readStream = fs.createReadStream(file);
+    const writeStream = fs.createWriteStream(output);
+
+    writeStream.on('finish', () => {
+        console.log('Decompressed.');
+    });
+    readStream.on('error', (err) => {
+        console.error('Error while reading file:', err);
+    });
+    writeStream.on('error', (err) => {
+        console.error('Error while writing decompressed file:', err);
+    });
+
+    readStream.pipe(gzip).pipe(writeStream);
 };
 
 await decompress();
