b63d94496ef017adc40655be2c3e150f2c2921e6
Kyrylo Doma <kyrylodoma@pro.local>
2024-10-07 15:40:27 +0700

    feat: child

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,19 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { spawn } from "child_process";
+
+const spawnChildProcess = (args) => {
+  const scriptPath = new URL("./files/script.js", import.meta.url).pathname;
+  const child = spawn("node", [scriptPath, ...args]);
+
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
+
+  child.on("error", (err) => {
+    console.error("Failed to start child process:", err);
+  });
+
+  child.on("exit", (code) => {
+    console.log(`Child process exited with code ${code}`);
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["arg1", "arg2"]);
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,11 +1,9 @@
 import { parentPort } from "worker_threads";
 
-// n should be received from main thread
 const nthFibonacci = (n) =>
   n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-  // This function sends result of nthFibonacci computations to main thread
   parentPort.on("message", (n) => {
     try {
       const result = nthFibonacci(n);
