0a5c1b6b9732c960437d53dfebb5c1b2cc2aed83
Mukhamed-Talaspaev <mukhamed.talaspayev@gmail.com>
2024-10-07 22:53:09 +0300

    feat: add final basic scope

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,22 @@
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import { createGzip } from "zlib";
+import fs, { createReadStream, createWriteStream } from "fs";
+import { pipeline } from "stream";
+import { promisify } from "util";
+
 const compress = async () => {
-    // Write your code here 
+  // Write your code here
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const sourcePath = path.join(__dirname, "files", "fileToCompress.txt");
+  const destinationPath = path.join(__dirname, "files", "archive.gz");
+  const gzip = createGzip();
+  const sourceStream = createReadStream(sourcePath);
+  const destinationStream = createWriteStream(destinationPath);
+  const pipelinePromise = promisify(pipeline);
+  await pipelinePromise(sourceStream, gzip, destinationStream);
+  fs.unlinkSync(sourcePath);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import { createGunzip } from "zlib";
+import fs, { createReadStream, createWriteStream } from "fs";
+import { pipeline } from "stream";
+import { promisify } from "util";
 const decompress = async () => {
-    // Write your code here 
+  // Write your code here
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const sourcePath = path.join(__dirname, "files", "archive.gz");
+  const destinationPath = path.join(__dirname, "files", "fileToCompress.txt");
+  const gunzip = createGunzip();
+  const sourceStream = createReadStream(sourcePath);
+  const destinationStream = createWriteStream(destinationPath);
+  const pipelinePromise = promisify(pipeline);
+  await pipelinePromise(sourceStream, gunzip, destinationStream);
+  fs.unlinkSync(sourcePath);
 };
 
 await decompress();
