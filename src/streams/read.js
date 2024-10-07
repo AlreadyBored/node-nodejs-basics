3ba716f7170e9c4b95cec8681417b1a460f7741d
AndreyTishchenko <Tishchenko.Andrew@yandex.ru>
2024-10-08 02:10:40 +0400

    feat: done readStream task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,10 @@
+import * as fs from 'fs';
+import * as process from 'process';
+import * as path from 'path';
+const __dirname = import.meta.dirname
+let FilePath = path.join(__dirname, 'files', 'fileToRead.txt');
 const read = async () => {
-    // Write your code here 
+    fs.createReadStream(FilePath).pipe(process.stdout);
 };
 
 await read();
