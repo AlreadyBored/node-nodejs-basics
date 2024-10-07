9267fd2f51132bd080f78030b263b27a6d6c39b7
AndreyTishchenko <Tishchenko.Andrew@yandex.ru>
2024-10-08 02:05:31 +0400

    feat: done delete task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,11 +1,11 @@
 import * as fs from 'fs';
 import * as path from 'path';
 const __dirname = import.meta.dirname
-let FilePath = path.join(__dirname, 'files', 'fileToRemove.txt');
-const remove = async () => {
-    fs.rm(FilePath, {'flag': 'wx'}, (err)=>{
+let FilePath = path.join(__dirname, 'files', 'fresh.txt');
+const create = async () => {
+    fs.writeFile(FilePath, 'I am fresh and young', {'flag': 'wx'}, (err)=>{
         if(err) throw Error('FS operation failed');
     })
 };
 
-await remove();
+await create();
--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,11 @@
+import * as fs from 'fs';
+import * as path from 'path';
+const __dirname = import.meta.dirname
+let FilePath = path.join(__dirname, 'files', 'fileToRemove.txt');
 const remove = async () => {
-    // Write your code here 
+    fs.rm(FilePath, {'flag': 'wx'}, (err)=>{
+        if(err) throw Error('FS operation failed');
+    })
 };
 
 await remove();
