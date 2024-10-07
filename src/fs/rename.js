71f25ad6950168abb6b91d8b001751c5e91f09ff
Eduardo Barbosa <edo@mothervolcano.com>
2024-10-07 11:23:15 +0100

    complete rename task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,15 @@
+import { rename as fsRename } from 'fs/promises';
+
 const rename = async () => {
-    // Write your code here 
+    try {
+        await fsRename("./files/wrongFilename.txt", "./files/properFilename.md");
+    } catch (error) {
+        if (error.code === "ENOENT") {
+            throw new Error("FS operation failed: <wrongFilename.txt> doesn't exist");
+        } else if (error.code === "EEXISTS") {
+            throw new Error("FS operation failed: <properFilename.md> file already exists");
+        }
+    }
 };
 
 await rename();
