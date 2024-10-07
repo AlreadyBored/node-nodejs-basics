f5de9b6634de920c35202af1b069c34e4c77c55e
Yury Kuzmich <yurakuzmich@gmail.com>
2024-10-07 16:12:38 +0500

    feat fs in progress

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,15 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const copy = async () => {
     // Write your code here 
+    const srcPath = path.join(__dirname, 'files');
+    const destPath = path.join(__dirname, 'files_copy');
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,26 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+    // Write your code here
+    const filePath = path.join(__dirname, 'files', 'fresh.txt');;
+    const fileData = 'I am fresh and young';
+
+    fs.access(filePath, fs.constants.F_OK)
+        .then(() => {
+            throw new Error();
+        })
+        .catch(async (error) => {
+            if (error) throw new Error('FS operation failed');;
+                
+            await fs.writeFile(filePath, fileData);
+            console.log('File created successfully');
+        });
 };
 
 await create();
