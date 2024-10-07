d4d5c9848b1502fd9cc03acf02d7f23063fe774f
dariana <iadaria.best@gmail.com>
2024-10-08 00:48:44 +0400

    feat: implement stream rad

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,21 @@
+import fs from 'node:fs';
+import { join } from 'node:path';
+
+const FILE_NAME = 'fileToRead.txt';
+export const FOLDER = '/files';
+
+export const fullName = join(import.meta.dirname, FOLDER, FILE_NAME);
+
 const read = async () => {
-    // Write your code here 
+  let readStream = fs.createReadStream(fullName, {
+    encoding: 'utf8',
+  });
+
+  readStream.pipe(process.stdout);
+
+  readStream.on('error', error => {
+    console.error(error);
+  });
 };
 
 await read();
