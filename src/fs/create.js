c7995987938bc43b9347976c6527d030be6ed9f4
dariana <iadaria.best@gmail.com>
2024-10-07 18:16:48 +0400

    feat: implement create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,29 @@
+import { open } from 'node:fs/promises';
+import { ErrorToShow } from './libs.js';
+
+const ErrorToShow = {
+    'EEXIST':'FS operation failed'
+};
+
+const content = 'I am fresh and young';
+const fileName = "fresh.txt"
+const filePath = "src/fs/files/"
+const fileForCreating = filePath.concat(fileName);
+
 const create = async () => {
-    // Write your code here 
+    let fileHandler;
+    try {
+        fileHandler = await open(fileForCreating, 'wx');
+        await fileHandler.writeFile(content);
+    } catch (error) {
+        let existingError = ErrorToShow[error.code];
+        if (existingError) {
+            throw new Error(existingError);
+        }
+        console.log(error);
+    } finally {
+        fileHandler?.close();
+    }
 };
 
 await create();
