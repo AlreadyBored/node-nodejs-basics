2b61102ce19d42a55b10c4708e4a91749c7148d8
DarkSpace76 <84933249+DarkSpace76@users.noreply.github.com>
2024-10-07 16:25:00 +0300

    feat: add Calculate hash fucntion

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import fs from 'node:fs'
+import path from 'node:path';
+import crypto from 'node:crypto'
+
 const calculateHash = async () => {
-    // Write your code here 
+    const pathFile = path.resolve('src', 'hash', 'files', 'fileToCalculateHashFor.txt');
+    const hash = crypto.createHash('sha256');
+
+    fs.readFile(pathFile, (err, data) => {
+        if (err) {
+            console.log('File read error');
+            return;
+        }
+        console.log(hash.update(data).digest('hex'));
+    });
 };
 
 await calculateHash();
