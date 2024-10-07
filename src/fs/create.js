a1a9bbb92a0997077077bec09c7805790bff3458
Gennady Kashin <gennady.kashin94@gmail.com>
2024-10-07 14:58:27 +0300

    feat: fs create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import path from 'path';
+import fs from 'fs/promises'
+import { fileURLToPath } from 'url';
+import { FSError } from '../utils/fs_error.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+    try {
+        const pathToCreatedFile = path.join(__dirname, 'files', 'fresh.txt');
+        await fs.writeFile(pathToCreatedFile, 'I am fresh and young', { flag: 'wx' })
+    } catch {
+        throw new FSError();
+    }
 };
 
 await create();
