117f9f4bd0de41fc885b29a9b3730fa716b2a338
Eduardo Barbosa <edo@mothervolcano.com>
2024-10-07 12:48:13 +0100

    complete cli:args task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,22 @@
 const parseArgs = () => {
-    // Write your code here 
+    const rawArgs = process.argv.slice(2);
+    const parsedArgs = JSON.parse(JSON.stringify(rawArgs));
+
+    let arg = null;
+    const args = parsedArgs.reduce( (acc, val) => {
+        if (val.startsWith("--")) {
+            arg = { 
+                name: val.slice(2), 
+                value: null 
+            };
+            acc.push(arg);   
+        } else {
+            arg.value = val;
+        }
+        return acc;
+    }, []);
+
+    args.forEach( arg => console.log(`${arg.name} is ${arg.value}`) );
 };
 
 parseArgs();
