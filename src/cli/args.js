3d907caba4156f299f05b26a39fffd483b1a491e
renat <murzinarenata@gmail.com>
2024-10-07 12:29:32 -0700

    feat: add args.js and env.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    const result = [];
+
+    for (let i = 0; i < args.length; i += 2) {
+        const name = args[i].substring(2);
+        const value = args[i + 1];
+        result.push(`${name} is ${value}`);
+    }
+    console.log(result.join(', '));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+    const rssArg = [];
+
+    for (let key in process.env) {
+        if (key.startsWith('RSS_')){
+            rssArg.push(`${key}=${process.env[key]}`);
+        }
+    }
+    console.log(rssArg.join(';'));
 };
 
 parseEnv();
