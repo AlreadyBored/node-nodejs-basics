3d45fec270fa8f11f2adb8e272d2010632c3084b
Eduardo Barbosa <edo@mothervolcano.com>
2024-10-07 23:47:01 +0100

    complete wt:main task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,40 @@
+import os from 'os';
+import { Worker } from 'worker_threads';
+
 const performCalculations = async () => {
-    // Write your code here
+    const cores = os.cpus();
+
+    const workerReport = new Array(cores.length);
+
+    let count = 0;
+    const logResult = (status, result, id) => {
+        count++;
+
+        const log = {
+            status,
+            data: result
+        }
+
+        workerReport[id] = log;
+
+        if (count === cores.length) {
+            console.log("Worker's work done: ", workerReport);
+        }
+    }
+
+    cores.forEach( (c, i) => {
+        const worker = new Worker('./worker.js', {
+            workerData: 10 + i,
+        });
+
+        worker.on('message', (result) => {
+            logResult('resolved', result, i);
+        });
+
+        worker.on('error', () => {
+            logResult('error', null, i);
+        })
+    });
 };
 
 await performCalculations();
