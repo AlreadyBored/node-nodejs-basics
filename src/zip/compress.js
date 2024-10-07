70b97751853f27f8581e31db7bfcc8fefe947d77
Juan David Amaya <jamaya@serempre.com>
2024-10-07 18:22:06 -0500

    Zlib subtask

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,14 @@
+import fs from 'fs';
+import { createGzip } from 'zlib';
+
+const filePath = new URL('files/fileToCompress.txt', import.meta.url);
+const compressedFilePath = new URL('files/archive.gz', import.meta.url);
 const compress = async () => {
     // Write your code here 
+    const fileStream = fs.createReadStream(filePath);
+    const gzipStream = createGzip();
+
+    fileStream.pipe(gzipStream).pipe(fs.createWriteStream(compressedFilePath));
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,14 @@
+import fs from 'fs';
+import { createGunzip } from 'zlib';
+
+const filePath = new URL('files/archive.gz', import.meta.url);
+const decompressedFilePath = new URL('files/fileToCompress.txt', import.meta.url);
 const decompress = async () => {
     // Write your code here 
+    const fileStream = fs.createReadStream(filePath);
+    const gunzipStream = createGunzip();
+
+    fileStream.pipe(gunzipStream).pipe(fs.createWriteStream(decompressedFilePath));
 };
 
 await decompress();
