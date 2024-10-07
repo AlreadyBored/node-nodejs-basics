14a824a1e55f816b0ca668f396763cb2bb87e2c9
vera-mal <k(k(kkkpG286(ABGDzmq}>
2024-10-07 16:44:32 +0200

    feat: implement function that prints array of filenames

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,14 @@
+import fs from 'fs';
+
 const list = async () => {
-    // Write your code here 
+    fs.readdir('./files', (err, files) => {
+        if (files) {
+            console.log(files);
+        }
+        if (err?.code === 'ENOENT') {
+            throw Error ('FS operation failed');
+        }        
+    })
 };
 
 await list();
