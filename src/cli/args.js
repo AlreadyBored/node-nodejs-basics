7e0642cfb7e172ad5c70c55c54445d06562abe94
MaksimNK <maksmozir@gmail.com>
2024-10-07 12:06:52 +0300

    implement cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+    //console.log(process.argv);
+    const argv = process.argv;
+
+    const result = [];
+
+    for (let index = 2; index < argv.length; index+=2) {
+        const argName = argv[index].replace('--', '');
+        const value = argv[index + 1];
+        result.push(`${argName} is ${value}, `);
+    }
+
+    console.log(result.join(''));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,19 @@
 const parseEnv = () => {
-    // Write your code here 
+
+    // function that check RSS_ prefix
+    const checkRss = (arg) => {
+        const rss = arg.substr(0, 4);
+        if(rss === 'RSS_') return true;
+        else false;
+    }
+
+    const result = [];
+    for(var arg in process.env) {
+        if(checkRss(arg)) {
+            result.push(`${arg}=${process.env[arg]}; `);
+        }
+    }
+    console.log(result.join(''));
 };
 
 parseEnv();
