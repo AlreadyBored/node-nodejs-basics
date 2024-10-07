27c3ab2ae27301d216cd236617cf80a8a2cc9245
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-10-08 04:27:47 +0500

    Solve tasks on compression/decompression using 'zlib' module and streams API

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+import fs from 'fs';
+import zlib from 'zlib';
+
+const __dirname = import.meta.dirname;
+
 const decompress = async () => {
-    // Write your code here 
+  const inputPath = `${__dirname}/files/archive.gz`;
+  const outputPath = `${__dirname}/files/fileToCompress.txt`;
+  const gzip = zlib.createGunzip();
+  const handleError = (err) => console.error(err.message);
+  const readStream = fs.createReadStream(inputPath);
+  const writeStream = fs.createWriteStream(outputPath);
+
+  readStream.pipe(gzip).pipe(writeStream);
+  writeStream.on('finish', () => console.log(`Success!`));
+  
+  gzip.on('error', handleError);
+  readStream.on('error', handleError);
+  writeStream.on('error', handleError);
 };
 
 await decompress();
