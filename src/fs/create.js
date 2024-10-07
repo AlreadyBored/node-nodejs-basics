2b5cab13ac77e2b2beb6168032fbc4dd72ab973d
AndreyTishchenko <Tishchenko.Andrew@yandex.ru>
2024-10-08 02:03:57 +0400

    feat: done create task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,11 @@
+import * as fs from 'fs';
+import * as path from 'path';
+const __dirname = import.meta.dirname
+let FilePath = path.join(__dirname, 'files', 'fresh.txt');
 const create = async () => {
-    // Write your code here 
+    fs.writeFile(FilePath, 'I am fresh and young', {'flag': 'wx'}, (err)=>{
+        if(err) throw Error('FS operation failed');
+    })
 };
 
 await create();
