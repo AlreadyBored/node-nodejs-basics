61fe6ee93a055731e1fbfbf78c870370b435eff3
AndreyTishchenko <Tishchenko.Andrew@yandex.ru>
2024-10-08 02:07:19 +0400

    feat: done rename task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,11 @@
+import * as fs from 'fs';
+import * as path from 'path';
+const __dirname = import.meta.dirname
+let FilePath = path.join(__dirname, 'files', 'wrongFilename.txt');
 const rename = async () => {
-    // Write your code here 
+    fs.rename(FilePath, path.join(__dirname, 'files', 'properFilename'), (err) => {
+        if (err) throw Error('FS operation failed');
+    });
 };
 
 await rename();
