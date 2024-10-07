f41dd859ff7b52dcc4ef4792829a37bd32680599
Yury Kuzmich <yurakuzmich@gmail.com>
2024-10-07 17:27:52 +0500

    feat args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
     // Write your code here 
+
+    const args = process.argv.slice(2);
+    const result = [];
+    args.forEach((arg, index) => {
+        if(arg.startsWith('--')) {
+            // console.log(`${arg.slice(2)}: ${args[index + 1]}`);
+            result.push(`${arg.slice(2)}: ${args[index + 1]}`);
+        }
+    });
+
+    console.log(result.join(', '));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
     // Write your code here 
+    // console.log(process.env);
+
+    for(const key in process.env) {
+        if (key.startsWith('RSS_')) {
+            console.log(`${key}=${process.env[key]};`);
+        }
+    }
 };
 
 parseEnv();
