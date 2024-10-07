0f2a15e163538a258b161eba6bc8ab1a7ce36e68
MaksimNK <maksmozir@gmail.com>
2024-10-07 03:01:40 +0300

    implement calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,29 @@
+import crypto from 'crypto'
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+import path  from 'node:path';
+import fs from 'node:fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+    try {
+        const pathToFile = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+        const hash = crypto.createHash('sha256');
+        const readStream =  fs.createReadStream(pathToFile);
+
+        readStream.on('data', chunk => {
+            hash.update(chunk);
+        });
+
+        readStream.on('end', () =>{
+            console.log(hash.digest('hex'));
+        })
+
+    } catch (error) {
+        throw error;
+    }
 };
 
 await calculateHash();
