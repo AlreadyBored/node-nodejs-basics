de1ac8dfe5ebd5c873c878f559b44f783673e8ab
Eduardo Barbosa <edo@mothervolcano.com>
2024-10-07 11:22:07 +0100

    complete create task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import {readFile, writeFile} from "fs/promises";
+
 const create = async () => {
-    // Write your code here 
+    try {
+        const fileCheck = await readFile("./files/fresh.txt");
+        if (fileCheck) {
+            throw new Error("FS operation failed: file already exists");
+        }
+    } catch (error) {
+        if (error.code === 'ENOENT' && error.path === "./files/fresh.txt") {
+            await writeFile("./files/fresh.txt", "I am fresh and young");
+        } else {
+            throw error;
+        }
+    }
 };
 
 await create();
