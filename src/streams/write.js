0e737d4cb607e688a9f1426524384bfafbf6835e
Тыщук Вячеслав <tischook@yandex.ru>
2024-10-08 02:57:29 +0300

    feat: write stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,22 @@
+import { createWriteStream } from 'node:fs';
+import process from 'node:process';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+
+const fileName = fileURLToPath(import.meta.url);
+const __dirname = dirname(fileName);
+
+const filePath = join(__dirname, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+
+    try {
+        const writeStream = createWriteStream(filePath);
+        process.stdin.on('data', (chunk) => writeStream.write(chunk.toString()) );
+    } catch (err) {
+        console.error(err.message);
+    }
 };
 
 await write();
