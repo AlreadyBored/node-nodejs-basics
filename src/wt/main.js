a9249ec98bd1cc5893e16ace3f2cb37b8dfc9ca9
MaksimNK <maksmozir@gmail.com>
2024-10-07 14:08:36 +0300

    implement working threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,49 @@
+import {Worker} from 'node:worker_threads';
+import os from 'node:os';
+
+import { fileURLToPath } from 'node:url';
+import path, { dirname, join } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
 const performCalculations = async () => {
-    // Write your code here
+
+    const pathToWorkerFile = path.join(__dirname, 'worker.js');
+
+    // array of worker threads
+    const workerPromises = [];
+
+    // function that creates and execute threads
+    const createWorker = (number) => {
+        return new Promise((resolve, reject) => {
+            const worker = new Worker(pathToWorkerFile);
+            
+            // call workers.js
+            worker.postMessage(number);
+            
+            worker.on('message', (data) => {
+                resolve(data);
+                worker.terminate();
+            });
+            worker.on('error', (error) => {
+                reject(error);
+                worker.terminate();
+            });
+            
+        });
+    }
+    // iterate by logic cpu length
+    for (let index = 0; index < os.cpus().length; index++) {
+        const incrementalNum = index + 10;
+        workerPromises.push(createWorker(incrementalNum));
+    }
+
+    const results = await Promise.all(workerPromises);
+    console.log(results.join(' '));
+
+
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,15 @@
-// n should be received from main thread
+import {parentPort} from 'node:worker_threads';
+
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    parentPort.on('message', data => {
+        try {
+            parentPort.postMessage(nthFibonacci(data));
+        } catch (error) {
+            throw error;
+        }
+    })
 };
 
 sendResult();
