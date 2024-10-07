fe636d896785b56aec7b31eaacb2925687ac78f4
Juan David Amaya <jamaya@serempre.com>
2024-10-06 23:59:27 -0500

    Subtask: CLI

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
 const parseArgs = () => {
     // Write your code here 
+    // Get the command line arguments
+    const args = process.argv.slice(2);
+    let result = [];
+    // Print the arguments
+    let i= 0;
+    for (const arg of args) {
+        if(arg.startsWith('--')) {
+            result.push(arg.slice(2) + ' is ' + args[i+1]);
+        }
+        i++;
+    }
+    console.log(result.join(', '));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,18 @@
 const parseEnv = () => {
     // Write your code here 
+
+    const envVars = process.env;
+
+    // Filter the env vars that start with 'RSS_'
+    const rssVars = Object.keys(envVars)
+        .filter(key => key.startsWith('RSS_'))
+        .map(key => `${key}=${envVars[key]}`);
+
+    // Join the env vars into a string
+    const result = rssVars.join('; ');
+
+    // Print the result
+    console.log(result);
 };
 
 parseEnv();
