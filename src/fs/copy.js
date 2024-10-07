24dd8bdb715bb334f7950ac3a48abea829b69769
vera-mal <k(k(kkkpG286(ABGDzmq}>
2024-10-07 16:27:08 +0200

    feat: implement function that copies folder

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,11 @@
+import fs from 'fs';
+
 const copy = async () => {
-    // Write your code here 
+    fs.cp('./files', './files_copy', { recursive: true, errorOnExist: true, force: false }, (err) => {
+        if (err?.code === 'ERR_FS_CP_EEXIST' || err?.code === 'ENOENT') {
+            throw Error ('FS operation failed');
+        }        
+    });
 };
 
 await copy();
