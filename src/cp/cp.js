d5f7946d004a5da9b8cc43d7adada30f0506db21
Aleksandr Kisel <51911663+Alex991995@users.noreply.github.com>
2024-10-07 12:28:18 +0300

    feat: worker and child

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,20 @@
+import { fork } from 'child_process';
+import __dirname from './__dirname.js';
+import path from 'path';
+
+const file = path.join(__dirname,'files', 'script.js')
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const forkProcess = fork(file, args, {silent: true})
+
+    process.stdin.on('data', data => {
+        forkProcess.stdin.write(data)
+    })
+
+    forkProcess.stdout.on('data', msg => {
+        console.log("parent " + msg)
+    })
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+await spawnChildProcess( ['someArgument1', 'someArgument1']);
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,40 @@
+import os from 'node:os';
+import { Worker } from 'node:worker_threads';
+import __dirname from './__dirname.js';
+import { join } from 'node:path';
+
+const numberOfCPU = os.cpus().length;
+const workerFile = join(__dirname, 'worker.js');
+
+function d(numWorkers) {
+  return new Promise((resolve, reject) => {
+    const worker = new Worker(workerFile);
+    worker.postMessage(numWorkers);
+
+    worker.on('message', data => {
+      resolve(data);
+    });
+    worker.on('error', err => {
+      reject(err);
+    });
+  });
+}
+
 const performCalculations = async () => {
-    // Write your code here
+  const arr = [];
+  const initialValue = 10;
+  const number = initialValue + numberOfCPU;
+
+  for (let i = initialValue; i <= number; i++) {
+    arr.push(d(i));
+  }
+  const result = await Promise.allSettled(arr);
+  const objResults = result.map(item => ({
+    status: item.status === 'fulfilled' ? 'resolved' : 'error',
+    data: item.value || null,
+  }));
+  console.log(objResults);
+  process.exit();
 };
 
 await performCalculations();
