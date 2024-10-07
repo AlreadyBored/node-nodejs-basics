c0584306c29e3344748ce056143f67fed0a2e77a
renat <murzinarenata@gmail.com>
2024-10-07 12:44:05 -0700

    feat: add transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,21 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+
+    const reverseTransform = new Transform({
+        transform(chunk, encoding, callback) {
+            const reversed = chunk.toString().split('').reverse().join('');
+            this.push(reversed);
+            callback();
+        }
+    });
+
+    reverseTransform.pipe(process.stdout);
+
+    process.stdin.on('data', (data) => {
+        reverseTransform.write(data);
+    });
 };
 
+console.log('Введите текст. Нажмите Enter:');
 await transform();
