0f8b0ea775adc2c36bc39a1a610a76d47d7fdb0a
Eduardo Barbosa <edo@mothervolcano.com>
2024-10-07 16:18:36 +0100

    complete streams:read task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,21 @@
+import fs from 'fs/promises';
+
 const read = async () => {
-    // Write your code here 
+    let fh;
+    try {
+        fh = await fs.open('./files/fileToRead.txt');
+        const fstream = fh.createReadStream();
+        fstream.on("data", (chunk) => {
+            process.stdout.write(`${chunk}\n`);
+        });
+        fstream.on("end", async () => {
+            process.stdout.end();
+            await fh.close();         
+        });
+
+    } catch (error) {
+        throw error;
+    }
 };
 
 await read();
