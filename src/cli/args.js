24e78ffaae17c8f3872abf12a0e42f8a61680671
Maxim Semikov <maxim.semikov87@gmail.com>
2024-10-07 13:16:16 +0500

    feat: Command line interface

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+    const propNamePrefix = '--'
+    const args = process.argv.reduce((acc, arg, index) => {
+        const nextElement = process.argv.length !== index+1 ? process.argv[index+1] : null;
+
+        if(arg.startsWith(propNamePrefix) && nextElement && !nextElement.startsWith(propNamePrefix)){
+            acc.push(`${arg} is ${nextElement}`)
+        }
+        return acc;
+    }, []).join(', ');
+
+    console.log(args);
+
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+    const availableEnv = Object.keys(process.env)
+        .filter(key => key.startsWith('RSS_'))
+        .map(key => `${key}=${process.env[key]}`)
+        .join('; ');
+
+    console.log(availableEnv)
 };
 
 parseEnv();
