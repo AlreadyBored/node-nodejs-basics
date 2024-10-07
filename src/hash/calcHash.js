ffa53df42028b55716aeb815b6397fe5b8ad26c3
Eduardo Barbosa <edo@mothervolcano.com>
2024-10-07 13:41:34 +0100

    complete hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
+import { createHash } from 'crypto';
+import fs from 'fs/promises';
+
 const calculateHash = async () => {
-    // Write your code here 
+    try {
+        const fh = await fs.open('./files/fileToCalculateHashFor.txt');
+        const fst = await fh.stat();
+        const fdata = await fh.readFile();
+        const hash = createHash('sha256');
+
+        hash.on('readable', () => {
+            const hashData = hash.read();
+            if (hashData) {
+                console.log(hashData.toString('hex'));
+            }
+        });
+        
+        hash.write(fdata);
+        hash.end();
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('File not found!')
+        }
+        throw error;
+    }
 };
 
 await calculateHash();
