2e02be191315602d0dafd3e50c2ef289d5edb594
Yury Kuzmich <yurakuzmich@gmail.com>
2024-10-07 15:32:19 +0500

    feat hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import { createHash } from 'crypto';
+import fs from 'fs';
+import path from 'path';
+import url from 'url';
+
 const calculateHash = async () => {
     // Write your code here 
+    const __filename = url.fileURLToPath(import.meta.url);
+    const file = path.join(path.dirname(__filename), 'files', 'fileToCalculateHashFor.txt');
+    const fileReadStream = fs.createReadStream(file);
+
+    const hash = createHash('sha256');
+
+    fileReadStream.on('data', (data) => {
+        hash.update(data);
+    });
+
+    fileReadStream.on('end', () => {
+        const fileHash = hash.digest('hex');
+        console.log(fileHash);
+    });
 };
 
 await calculateHash();
