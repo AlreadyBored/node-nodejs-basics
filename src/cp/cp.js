8e61ea7c0817348e8cf813e7b5936ae651184d90
oleg filkin <oleg.filkin.by@gmail.com>
2024-10-07 21:28:45 +0300

    feat: add cp solution

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,24 @@
+import { spawn } from 'node:child_process';
+import path from 'path';
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const scriptPath = path.resolve('src', 'cp', 'files', 'script.js');
+    const child = spawn('node', [scriptPath, ...args]);
+
+    child.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+
+    child.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
+
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
+
+    process.stdout.on('data', (data) => {
+        console.log('Main process recived data from child process: ', data.toString());
+    })
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([1, 2, 3, 4]);
