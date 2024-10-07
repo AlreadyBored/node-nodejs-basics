472d7d61f966064b10a6f90605bdca7a680df66d
AndreyTishchenko <Tishchenko.Andrew@yandex.ru>
2024-10-08 02:08:20 +0400

    feat: done calculate hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,13 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as crypto from 'crypto';
+const __dirname = import.meta.dirname
+let FilePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = crypto.createHash('sha256');
+    const rs = fs.createReadStream(FilePath);
+    rs.on('data', chunk => hash.update(chunk));
+    rs.on('end', () => console.log(hash.digest('hex')));
 };
 
 await calculateHash();
