52552026624776823c12d0ec3dfa24f54646eb7f
renat <murzinarenata@gmail.com>
2024-10-07 12:06:20 -0700

    feat: add create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import fsPromises from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+
 const create = async () => {
-    // Write your code here 
+    const filePath = path.join(dirname, 'files', 'fresh.txt');
+
+    try {
+        await fsPromises.writeFile(filePath, 'I am fresh and young');
+    } catch (e) {
+        if (e.code === 'EEXIST') {
+            throw new Error('FS operation failed');
+        }
+        throw e;
+    }
 };
 
 await create();
