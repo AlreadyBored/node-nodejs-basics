a472f1e8af855094486a9f04cbaee94b7df8884f
Edward <e.arechwa@gmail.com>
2024-10-07 09:10:35 +0200

    feat: implement transform file

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,22 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
     // Write your code here 
+    const reverseTransform = new Transform({
+        transform(chunk, encoding, callback) {
+            const reversedText = chunk.toString().split('').reverse().join('');
+            this.push(reversedText); 
+            callback(); 
+        }
+    });
+
+    await new Promise((resolve, reject) => {
+        process.stdin
+            .pipe(reverseTransform) 
+            .pipe(process.stdout)   
+            .on('finish', resolve) 
+            .on('error', reject);   
+    });
 };
 
 await transform();
