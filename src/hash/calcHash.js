344bdf27a28dcd246c934febb59505b3bfb7ed0e
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-10-08 03:56:20 +0500

    Solve Hash task with 'crypto' module streams API

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import { createHash } from 'crypto';
+import fs from 'fs';
+
+const __dirname = import.meta.dirname;
+
 const calculateHash = async () => {
-    // Write your code here 
-};
+  const filePath = `${__dirname}/files/fileToCalculateHashFor.txt`;
+  const hash = createHash('sha256');
+
+  const readStream = fs.createReadStream(filePath, 'utf-8');
+  readStream.on('data', (chunk) => {
+    console.log(hash.update(chunk).digest('hex'));
+  });
+}
 
 await calculateHash();
