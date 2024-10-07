3a379b4cd7cfceb32192ae55498006e9cda89a20
Pavel Drazdou <p.drazdou@godeltech.com>
2024-10-07 23:46:32 +0300

    Child Processes (src/cp)

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,34 @@
+import { spawn } from 'child_process';
+import path from 'path';
+import { createInterface } from 'readline';
+import {fileURLToPath} from "url";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const scriptPath = path.join(__dirname, 'files', 'script.js');
+  const child = spawn('node', [scriptPath, ...args], {
+    stdio: ['pipe', 'pipe', 'inherit'],
+  });
+
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.on('line', (input) => {
+    child.stdin.write(input + '\n');
+  });
+
+  child.stdout.on('data', (data) => {
+    process.stdout.write(data);
+  });
+
+  child.on('exit', (code) => {
+    console.log(`Child process exited with code ${code}`);
+    rl.close();
+  });
+
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["arg1","arg2"]);
