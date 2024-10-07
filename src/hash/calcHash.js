170a0a6da95ee6ad4974d53fee745e7a835495f6
vera-mal <k(k(kkkpG286(ABGDzmq}>
2024-10-07 18:13:58 +0200

    feat:  implement function that calculates SHA256 hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,14 @@
+import fs from 'fs';
+import crypto from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+    fs.readFile('./files/fileToCalculateHashFor.txt', (err, data) => {
+        if (err?.code === 'ENOENT') {
+            throw Error ('FS operation failed');
+        } else if (data) {
+            process.stdout.write(crypto.createHash('sha256').update(data).digest('hex'));
+        }
+    });
 };
 
 await calculateHash();
