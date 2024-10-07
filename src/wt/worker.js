1445d9a7b42a3c38a04da4023ddbe4d015407a7d
vera-mal <k(k(kkkpG286(ABGDzmq}>
2024-10-07 21:03:59 +0200

    feat: implement worker

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,19 @@
-// n should be received from main thread
+
+import { Worker, isMainThread, parentPort } from 'worker_threads';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    if (isMainThread) {
+        const worker = new Worker(__filename);
+        worker.on('message', (n) => { console.log(nthFibonacci(n)); });
+    } else {
+        // 4th fibonacci
+        parentPort.postMessage(4);
+    }
 };
 
 sendResult();
