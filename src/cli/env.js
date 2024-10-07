63587321d95dab0c2db8d8fc4e36499dd5c803ea
Eduardo Barbosa <edo@mothervolcano.com>
2024-10-07 12:48:30 +0100

    complete cli:env task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+    const envs = process.env;
+
+    const RSS_envs = Object.keys(envs)
+        .filter( env => env.startsWith("RSS"))
+        .map( k => ({ name: k, value: envs[k]})); 
+
+    RSS_envs.forEach( v => console.log(`${v.name}=${v.value}`));
 };
 
 parseEnv();
