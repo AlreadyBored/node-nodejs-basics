534a9080c7ec311edc49efc5b1eeec0cebfd127c
Pavel Drazdou <p.drazdou@godeltech.com>
2024-10-07 23:45:09 +0300

    Hash (src/hash)

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,26 @@
+import { createReadStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { createHash } from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+  const hash = createHash('sha256');
+  const readStream = createReadStream(filePath);
+
+  readStream.on('data', (chunk) => {
+    hash.update(chunk);
+  });
+
+  readStream.on('end', () => {
+    const hashValue = hash.digest('hex');
+    console.log(hashValue);
+  });
+
+  readStream.on('error', (error) => {
+    console.error('Error reading the file:', error.message);
+  });
 };
 
 await calculateHash();
