5db0b460dbb2d9bf35b5c08f6b7996582e3d3cd3
AndreyTishchenko <Tishchenko.Andrew@yandex.ru>
2024-10-08 02:06:03 +0400

    feat: done list task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,14 @@
+import * as fs from 'fs';
+import * as path from 'path';
+const __dirname = import.meta.dirname
+let FilePath = path.join(__dirname, 'files');
 const list = async () => {
-    // Write your code here 
+    fs.readdir(FilePath, (err, files) => {
+        if (err) throw Error('FS operation failed');
+        files.forEach(file => {
+          console.log(file);
+        });
+    });
 };
 
 await list();
