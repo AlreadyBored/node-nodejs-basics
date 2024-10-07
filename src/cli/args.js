a4799268e0444068383f201b38f48113b6d0c629
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-10-08 03:54:48 +0500

    Solve command line interface tasks using process.env and process.argv methods

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
-};
+  const arg = process.argv.slice(2);
 
+  for (let i = 0; i < arg.length; i += 2) {
+    if (arg[i].startsWith('--')) {
+      console.log(`${arg[i].slice(2)} is ${arg[i + 1]}`);
+    }
+  }
+};
+ 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+  const environmentVars = process.env;
+
+  const filteredVars = Object.keys(environmentVars)
+    .filter((key) => key.startsWith('RSS_'))
+    .map((key) => console.log(`${key}=${environmentVars[key]};`))
+
+  if (filteredVars.length === 0) {
+    console.log("No environment variables with prefix 'RSS_'");
+  }
 };
 
 parseEnv();
