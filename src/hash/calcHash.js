f5bfd5652691e95e65446f127d210e33e43b5efb
Ygrec <brygrec@gmail.com>
2024-10-08 01:28:32 +0300

    Add hash calculation

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
-const calculateHash = async () => {
-    // Write your code here 
+
+import {
+    createReadStream,
+  } from 'node:fs';
+
+  const {
+    createHash,
+  } = await import('node:crypto');
+  
+  const calculateHash = async () => {
+
+  const filename = './src/hash/files/fileToCalculateHashFor.txt';
+  
+  const hash = createHash('sha256');
+  
+  const input = createReadStream(filename);
+  input.on('readable', () => {
+    const data = input.read();
+    if (data)
+      hash.update(data);
+    else {
+      console.log(`${hash.digest('hex')}`);
+    }
+  });
+
 };
 
-await calculateHash();
+ await calculateHash();
