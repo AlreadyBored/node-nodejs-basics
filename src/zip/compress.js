7a3a7bcba70e37757600c0b8c03cfabfa055e2cf
Eduardo Barbosa <edo@mothervolcano.com>
2024-10-07 21:21:40 +0100

    complete zip:compress task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,39 @@
+import fs from 'fs/promises';
+import { createGzip } from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+    try {
+        const fhRead = await fs.open('./files/fileToCompress.txt');
+        const fhWrite = await fs.open('./files/archive.gz', 'w');
+
+        const rstream = fhRead.createReadStream();
+        const compressTransform = createGzip();
+        const wstream = fhWrite.createWriteStream();
+
+        rstream.on('data', (chunk) => {
+            compressTransform.write(chunk);
+        });
+
+        compressTransform.on('data', (chunk) => {
+            wstream.write(chunk);
+        });
+        
+        rstream.on('end', () => {
+            compressTransform.end();
+        });
+
+        compressTransform.on('end', () => {
+            wstream.end();        
+        });
+
+        wstream.on('end', async () => {
+            await fhRead.close();
+            await fhWrite.close();
+        });
+
+    } catch (error) {
+        throw error;
+    }
 };
 
 await compress();
