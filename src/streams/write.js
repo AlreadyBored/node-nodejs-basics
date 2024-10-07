e73a2f26981d3448ff72720da8b837eb9a074bb3
Eduardo Barbosa <edo@mothervolcano.com>
2024-10-07 16:18:10 +0100

    complete write task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,27 @@
+import fs from 'fs/promises';
+
 const write = async () => {
-    // Write your code here 
+    try {
+        const fh = await fs.open('./files/fileToWrite.txt', 'a');
+        const fstream = fh.createWriteStream();
+
+        process.stdin.setEncoding('utf8');
+        process.stdin.on('data', (input) => {
+            fstream.write(input);
+        });
+
+        process.stdin.on('end', async () => {
+            console.log('that\'s all folks!');
+            await fh.close();
+        });
+
+        process.stdin.on('error', (error) => {
+            console.log('failed to read inputL ', error);
+        });
+
+    } catch (error) {
+        throw error;
+    }
 };
 
 await write();
