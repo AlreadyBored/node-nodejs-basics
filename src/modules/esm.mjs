ca46e7bf2a8964055b0ec7ed0a8fea301ae6be8b
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-10-08 03:55:42 +0500

    Solve Modules tasks by changing old code to ES6 syntax

---
+++ src/modules/esm.mjs
@@ -0,0 +1,42 @@
+import path from 'path';
+import { release, version } from 'os';
+import { createServer as createServerHttp } from 'http';
+import * as c from './files/c.js';
+import * as a from './files/a.json' assert { type: "json" };
+import * as b from './files/b.json' assert { type: "json" };
+
+const __dirname = import.meta.dirname;
+const __filename = import.meta.filename;
+
+const random = Math.random();
+
+let unknownObject;
+
+if (random > 0.5) {
+    unknownObject = a;
+} else {
+    unknownObject = b;
+}
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${path.sep}"`);
+
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
+
+const myServer = createServerHttp((_, res) => {
+    res.end('Request accepted');
+});
+
+const PORT = 3000;
+
+console.log(unknownObject);
+
+myServer.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+    console.log('To terminate it, use Ctrl+C combination');
+});
+
+export { unknownObject, myServer };
+
