604b6bd952b992cf419e7b4142d13943cfc51685
DarkSpace76 <84933249+DarkSpace76@users.noreply.github.com>
2024-10-07 18:20:06 +0300

    feat: add worker

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,32 @@
+import {
+    Worker,
+
+} from 'node:worker_threads';
+import path from 'node:path';
+import os from 'node:os'
+
+const worker = (i) => new Promise((resolve, rejects) => {
+    const pathWorker = path.resolve('src', 'wt', 'worker.js');
+    const worker = new Worker(pathWorker, { workerData: i + 10 }).on('message', msg => {
+        resolve(msg);
+    });
+}
+);
+
+
 const performCalculations = async () => {
-    // Write your code here
+
+    const arr = new Array();
+    for (let i = 0; i < os.cpus().length; i++) {
+        arr.push(worker(i));
+    }
+
+    Promise.all(arr).then((val) => {
+        console.log(val);
+    });
+
+
+
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,16 @@
-// n should be received from main thread
+import {
+    parentPort,
+    workerData
+} from 'node:worker_threads';
+
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    try {
+        parentPort.postMessage({ status: 'resolved', date: nthFibonacci(workerData) });
+    } catch (e) {
+        parentPort.postMessage({ status: 'error', date: null });
+    }
 };
 
 sendResult();
