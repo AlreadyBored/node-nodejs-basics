4e9e06ffb67ab64460539be89826ca3f23af5e74
dariana <iadaria.best@gmail.com>
2024-10-08 01:08:34 +0400

    feat: implement transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
+import { Transform } from 'node:stream';
+import { stdin, stdout } from 'node:process';
+
 const transform = async () => {
-    // Write your code here 
+  const transformStream = new Transform({
+    transform(chunk, _, callback) {
+      const str = Array.from(chunk.toString('utf8')).reverse().join('');
+      callback(null, str);
+    },
+    encoding: 'utf8',
+  });
+
+  stdin.pipe(transformStream).pipe(stdout);
 };
 
 await transform();
