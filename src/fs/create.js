7db22af0eb48ec8f071367926bd830b58d18fd00
0404233 <andrey0404233@gmail.com>
2024-10-07 23:17:05 +0300

    feat: implement function create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,13 @@
+import { writeFile, access } from 'fs/promises';
+
 const create = async () => {
-    // Write your code here 
+    try {
+        await access('files/fresh.txt');
+        console.log('FS operation failed');
+    } catch (err) {
+        await writeFile('files/fresh.txt', 'I am fresh and young');
+        console.log('File created');
+    }
 };
 
 await create();
