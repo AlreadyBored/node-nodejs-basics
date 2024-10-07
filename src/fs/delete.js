88aef4c419b19ded4e5172e78e9a44cc0be1e68c
Eduardo Barbosa <edo@mothervolcano.com>
2024-10-07 11:23:40 +0100

    complete delete task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,16 @@
+import { rm } from 'fs/promises';
+
 const remove = async () => {
-    // Write your code here 
+    try {
+        await rm("./files/fileToRemoveNot.txt");
+
+    } catch (error) {
+        if (error.code === "ENOENT") {
+            throw new Error(`FS operation failed: file doesn't exist`);
+        } else {
+            throw error;
+        }
+    }
 };
 
 await remove();
