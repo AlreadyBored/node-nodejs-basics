3c73e4d08ea58ac29fd4ca8c69dc6a0e522fa2ad
Aleksandr Kisel <51911663+Alex991995@users.noreply.github.com>
2024-10-07 12:05:44 +0300

    feat: zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import { createGzip } from 'node:zlib';
+import fs from 'node:fs';
+import { join } from 'node:path';
+import __dirname from './__dirname.js';
+
+const fileTxt = join(__dirname, 'files', 'fileToCompress.txt');
+const fileZg = join(__dirname, 'files', 'archive.gz');
+
+const readableStream = fs.createReadStream(fileTxt, 'utf8');
+const writeableStream = fs.createWriteStream(fileZg);
+
+const gzip = createGzip();
+
 const compress = async () => {
-    // Write your code here 
+  readableStream.pipe(gzip).pipe(writeableStream);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import { createUnzip } from 'node:zlib';
+import fs from 'node:fs';
+import { join } from 'node:path';
+import __dirname from './__dirname.js';
+
+const fileTxt = join(__dirname, 'files', 'fileToCompress.txt');
+const fileZg = join(__dirname, 'files', 'archive.gz');
+
+const readableStream = fs.createReadStream(fileZg);
+const writeableStream = fs.createWriteStream(fileTxt);
+
+const unZip = createUnzip();
+
 const decompress = async () => {
-    // Write your code here 
+  readableStream.pipe(unZip).pipe(writeableStream);
 };
 
 await decompress();
