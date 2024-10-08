5d2e2db52350ac295ae7d657113ee00585f0af34
MaksimNK <maksmozir@gmail.com>
2024-10-08 13:43:21 +0300

    implement cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,28 @@
+import { exec, spawn, fork} from 'node:child_process';
+
+
+import { fileURLToPath } from 'node:url';
+import path, { dirname } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    
+    const pathToScript = path.join(__dirname, 'files', 'script.js');
+    const script = spawn('node', [pathToScript, ...args]);
+
+    process.stdin.on('data',  data => {
+        script.stdin.write(data);
+    });
+
+    script.stdout.on('data', (data) => {
+        console.log(data.toString());
+    })
+
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ['someArgument1', 'someArgument2']);
+
