12dfa9f992ac02e96e505e64f33f287249bf1f6c
Тыщук Вячеслав <tischook@yandex.ru>
2024-10-08 04:20:35 +0300

    feat: spawn child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,22 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { spawn } from 'node:child_process'
+import process from 'node:process';
+
+
+const fileName = fileURLToPath(import.meta.url);
+const __dirname = dirname(fileName);
+
+const filePath = join(__dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childProcess = spawn('node', [filePath, ...args]);
+
+    childProcess.stdout.on('data', (data) => process.stdout.write(data));
+
+    process.stdin.pipe(childProcess.stdin);
+
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+
+spawnChildProcess( ['someArgument1', 'someArgument2'] );
