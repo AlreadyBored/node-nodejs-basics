08fd72c764dd1d926a5c71a9f0b8b819a8cb69f6
Тыщук Вячеслав <tischook@yandex.ru>
2024-10-08 03:27:50 +0300

    feat: decopmress file

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,23 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { createUnzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import { createReadStream, createWriteStream } from 'node:fs';
+
+
+const fileName = fileURLToPath(import.meta.url);
+const __dirname = dirname(fileName);
+
+const fileToDecompress = join(__dirname, 'files', 'archive.gz');
+const decompressedFile = join(__dirname, 'files', 'fileToCompress.txt');
+
 const decompress = async () => {
-    // Write your code here 
+    pipeline(
+        createReadStream(fileToDecompress),
+        createUnzip(),
+        createWriteStream(decompressedFile),
+        (err) => console.error(err)
+    );
 };
 
 await decompress();
