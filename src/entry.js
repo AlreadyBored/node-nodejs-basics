7231b9d2a38915a368e43201617d004c25be187b
Edward <e.arechwa@gmail.com>
2024-10-14 10:04:08 +0200

    ft: implement commands

--- src/entry.js
+++ src/entry.js
@@ -14,17 +14,25 @@ const userName = parseArguments(process.argv);
 console.log(`Welcome to the File Manager, ${userName}!`);
 console.log(`You are currently in ${process.cwd()}`);
 
+const commands = {
+  ".exit": (process) => {
+    console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
+    process.exit();
+  },
+};
+
+process.stdin.on("data", (data) => {
+  const input = data.toString().trim();
+
+  if (commands[input]) {
+    commands[input](process);
+  } else {
+    console.log("Invalid input.");
+  }
+});
+
 //  Ctrl+C
 process.on("SIGINT", () => {
   console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
   process.exit();
-});
-
-//  .exit
-process.stdin.on("data", (data) => {
-  const input = data.toString().trim();
-  if (input === ".exit") {
-    console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
-    process.exit();
-  }
 });
