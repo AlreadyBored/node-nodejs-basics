a6f34c08e8238a36c7e2c6557e1d3c3aaf231aba
Tursynkhan Tursunov <tursynkhan001@gmail.com>
2024-10-15 04:50:46 +0500

    update: move

--- src/fs/move.js
+++ src/fs/move.js
@@ -5,20 +5,17 @@ const moveFile = (src, dest) => {
   const sourcePath = path.resolve(src);
   const destinationPath = path.resolve(dest);
 
-  // Проверяем существование исходного файла
   fs.access(sourcePath, fs.constants.F_OK, (err) => {
     if (err) {
       console.log('Operation failed: Source file does not exist');
       return;
     }
 
-    // Копируем файл с использованием потоков
     const readStream = fs.createReadStream(sourcePath);
     const writeStream = fs.createWriteStream(destinationPath);
 
     readStream.pipe(writeStream)
       .on('finish', () => {
-        // После копирования удаляем исходный файл
         fs.unlink(sourcePath, (err) => {
           if (err) {
             console.log('Operation failed: Unable to delete source file');
