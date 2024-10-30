18e203287f3bc7761b89203d198a94c2a291addf
siloksan <tutel07@gmail.com>
2024-10-30 21:23:36 +0300

    feat: implement create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,15 @@
+import { open } from "node:fs/promises";
+import { join } from 'path';
+
 const create = async () => {
-    // Write your code here 
+	const pathName = join(import.meta.dirname, 'files', "fresh.txt");
+	try {
+		const fileCreator = await open(pathName, "wx");
+		await fileCreator.write("I am fresh and young");
+		await fileCreator.close();
+	} catch (error) {
+		console.error("FS operation failed");
+	}
 };
 
 await create();
