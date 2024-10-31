35b3069488b9c8622aa7f394f59f5e08613a454d
siloksan <tutel07@gmail.com>
2024-10-31 09:26:25 +0300

    feat: implement list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,14 @@
+import { readdir } from "fs/promises";
+import { join } from "path";
+
 const list = async () => {
-    // Write your code here 
+	const dirname = join(import.meta.dirname, "files");
+	try {
+		const files = await readdir(dirname);
+		for (const file of files) console.log(file);
+	} catch (err) {
+		console.error(err);
+	}
 };
 
 await list();
