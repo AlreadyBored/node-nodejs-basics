c175f8b9a6ccbbdce77d2ea44424a5d0041d52fb
siloksan <tutel07@gmail.com>
2024-11-04 22:24:36 +0300

    feat: implement write stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import { createWriteStream } from 'fs';
+import { join } from 'path';
+
 const write = async () => {
-    // Write your code here 
+	const filePath = join(import.meta.dirname, 'files', 'fileToWrite.txt');
+
+	const writeStream = createWriteStream(filePath);
+
+	process.stdin.pipe(writeStream);
+
+	writeStream.on('error', (error) => {
+		console.error('[ERROR]:', error);
+	});
 };
 
 await write();
