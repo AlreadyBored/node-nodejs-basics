5f1d35e42ea63491acae37ec6f390179fbd13021
siloksan <tutel07@gmail.com>
2024-11-04 22:19:14 +0300

    feat: implement read stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import { createReadStream } from 'fs';
+import { join } from 'path';
+
 const read = async () => {
-    // Write your code here 
+	const filePath = join(import.meta.dirname, 'files', 'fileToRead.txt');
+	const readStream = createReadStream(filePath);
+
+	readStream.pipe(process.stdout)
+	readStream.on('error', (err) => {
+		console.error('[Error]', err);
+	})
 };
 
 await read();
