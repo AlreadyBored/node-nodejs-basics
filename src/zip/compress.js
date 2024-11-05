97bd760abc49ebff82993f47594553078cef18ac
siloksan <tutel07@gmail.com>
2024-11-05 22:26:46 +0600

    feat: implement compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import { createWriteStream, createReadStream } from 'fs';
+import { createGzip } from 'zlib';
+import { join } from 'path';
+import { pipeline } from 'stream';
+
 const compress = async () => {
-    // Write your code here 
+	const gzip = createGzip();
+	const source = join(import.meta.dirname, 'files', 'fileToCompress.txt');
+	const destination = join(import.meta.dirname, 'files', 'archive.gz');
+	const readStream = createReadStream(source);
+	const writeStream = createWriteStream(destination);
+
+	pipeline(readStream, gzip, writeStream, (err) => {
+		if (err) {
+			console.error('An error occurred:', err);
+			process.exitCode = 1;
+		}
+	});
 };
 
 await compress();
