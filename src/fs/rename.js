04c2a1b1e8cccf78130d45ede2872a240f785d16
Julia Kocur <juliakocur@mail.com>
2025-04-06 22:33:15 +0200

    feat: add solution for rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,22 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import fs from 'node:fs';
+
 const rename = async () => {
-    // Write your code here 
+    const fileName = fileURLToPath(import.meta.url);
+    const dirName = dirname(fileName);
+    const pathOriginFile = join(dirName, 'files', 'wrongFilename.txt');
+    const pathRenameFile = join(dirName, 'files', 'properFilename.md');
+
+    if ((!fs.existsSync(pathOriginFile)) || (fs.existsSync(pathRenameFile))) {
+        throw new Error('FS operation failed')
+    } else {
+        fs.rename(pathOriginFile, pathRenameFile, (error) => {
+            if (error) {
+                throw new Error('FS operation failed')
+            }
+        }) 
+    }
 };
 
 await rename();
