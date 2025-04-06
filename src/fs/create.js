1941889469ed39cd17116c95ad419b2482921e16
Julia Kocur <juliakocur@mail.com>
2025-04-06 12:51:54 +0200

    feat: add solution for create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import fs from 'node:fs';
+
 const create = async () => {
-    // Write your code here 
+    const fileName = fileURLToPath(import.meta.url);
+    const dirName = dirname(fileName);
+    const path = join(dirName, 'files', 'fresh.txt');
+
+    if (fs.existsSync(path)) {
+        throw new Error('FS operation failed')
+    } else {
+        fs.writeFile(path, 'I am fresh and young', () => {
+        })     
+    }
 };
 
 await create();
