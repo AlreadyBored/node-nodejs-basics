a294f7e15d40a9678c6c391045454f9164cf85d1
Julia Kocur <juliakocur@mail.com>
2025-04-07 21:29:53 +0200

    refactor: edit create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,17 +1,15 @@
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
-import fs from 'node:fs';
+import { writeFile } from 'node:fs/promises';
 
 const create = async () => {
     const fileName = fileURLToPath(import.meta.url);
     const dirName = dirname(fileName);
     const path = join(dirName, 'files', 'fresh.txt');
-
-    if (fs.existsSync(path)) {
-        throw new Error('FS operation failed')
-    } else {
-        fs.writeFile(path, 'I am fresh and young', () => {
-        })     
+    try {
+        await writeFile(path,'I am fresh and young', { flag: 'wx' });
+    } catch {
+        throw new Error('FS operation failed');
     }
 };
 
