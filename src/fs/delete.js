d000fd4c430bf1f17dba41127994d644813ed7b1
Julia Kocur <juliakocur@mail.com>
2025-04-07 21:45:18 +0200

    refactor: edit delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,16 +1,15 @@
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
-import fs from 'node:fs';
+import { rm } from 'node:fs/promises';
 
 const remove = async () => {
     const fileName = fileURLToPath(import.meta.url);
     const dirName = dirname(fileName);
     const path = join(dirName, 'files', 'fileToRemove.txt');
-
-    if (!fs.existsSync(path)) {
-        throw new Error('FS operation failed')
-    } else {
-        fs.unlink(path, () => {})
+    try {
+        await rm(path);
+    } catch {
+        throw new Error('FS operation failed');
     }
 };
 
