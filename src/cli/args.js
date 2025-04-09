26145e84e47f64f6390bda2fd2df549e3d097955
Julia Kocur <juliakocur@mail.com>
2025-04-09 17:40:03 +0200

    feat: add solution for args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+    const variables = process.argv.slice(2);
+    let result = [];
+    for (let i = 0; i < variables.length; i += 2) {
+        const key = variables[i].replace('--', '');
+        const value = variables[i + 1];
+        result.push(`${key} is ${value}`);  
+    }
+    console.log(result.join(', '));
 };
 
 parseArgs();
