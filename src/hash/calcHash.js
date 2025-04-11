7d528d34ced3391ef636c44703f8c87efca1cc15
Julia Kocur <juliakocur@mail.com>
2025-04-11 22:07:48 +0200

    feat: add solution for calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,14 @@
+import crypto from 'crypto';
+import path from 'node:path';
+import {readFile} from 'node:fs/promises';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = path.resolve('src/hash/files/fileToCalculateHashFor.txt');
+    const data = await readFile(filePath, 'utf8');
+    const hash = crypto.createHash('sha256')
+                    .update(data)
+                    .digest('hex');
+    console.log(hash);
 };
 
 await calculateHash();
