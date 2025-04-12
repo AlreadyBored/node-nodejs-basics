467c1f63cd9082d114fc3bf5c81de987d6d5e05d
Julia Kocur <juliakocur@mail.com>
2025-04-12 07:37:09 +0200

    feat: add solution for write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import path from 'node:path';
+import fs from 'node:fs';
+const filePath = path.resolve('src/streams/files/fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+    const writableStream = fs.createWriteStream(filePath, 'utf-8');
+    process.stdin.pipe(writableStream);
+    writableStream.on('finish', () => {
+        console.log('Data has been written');
+    });
+    writableStream.on('error', (err) => {
+        console.error(err.message)
+    });
 };
 
 await write();
