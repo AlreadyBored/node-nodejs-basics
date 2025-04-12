f83e3bcfa54b9076289dcc1deadf7d147a173051
Julia Kocur <juliakocur@mail.com>
2025-04-12 07:55:30 +0200

    feat: add solution for transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
+import {  Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+  const transformStream = new Transform({
+    transform(chunk, encoding, callback) {
+        const reverse = chunk.toString().split('').reverse().join('');
+        this.push(reverse);
+        callback();
+    }
+  });
+  process.stdin.pipe(transformStream).pipe(process.stdout);
 };
 
 await transform();
