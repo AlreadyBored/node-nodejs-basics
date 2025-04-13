2be8dae366be1e74b744123ffcb968665865b0ba
Julia Kocur <juliakocur@mail.com>
2025-04-13 16:12:21 +0200

    feat: add solution for worker.js and main.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,34 @@
+import { Worker } from 'worker_threads';
+import path from 'node:path';
+import { cpus } from 'os';
+
 const performCalculations = async () => {
-    // Write your code here
+    const filePath = path.resolve('src/wt/worker.js');
+    const coresNum = cpus().length;
+    const workers = [];
+
+    for (let i = 0; i < coresNum; i++) {
+        const num = 10 + i;
+        const workerThreads = new Promise((resolve) => {
+            const worker = new Worker(filePath, {workerData: num});
+        
+            worker.on('message', (data) => {
+                resolve({status: 'resolved', data});
+            });
+            worker.on('error', () => {
+                resolve({status: 'error', data: null});
+            });
+            worker.on('exit', (code) => {
+                if (code !== 0) {
+                    resolve({status: 'error', data: null});
+                }
+            });
+        });
+        
+        workers.push(workerThreads);
+    }
+    const result = await Promise.all(workers);
+    console.log(result);
 };
 
 await performCalculations();
