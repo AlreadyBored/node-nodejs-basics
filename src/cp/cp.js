bffd90acbfac5d59cb54638a1c69c76ee9485eb1
Julia Kocur <juliakocur@mail.com>
2025-04-17 08:10:57 +0200

    feat: add solution for cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,16 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+import { spawn } from 'child_process';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+const fileName = fileURLToPath(import.meta.url);
+const dirName = dirname(fileName);
+const path = join(dirName, 'files', 'script.js');
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+const spawnChildProcess = async (args) => {
+    const child = spawn('node', [path, ...args], {
+        stdio: ['pipe', 'pipe', 'inherit']
+    });
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
+}
+
+spawnChildProcess(['someArgument1', 'someArgument2', 'someArgument3']);
