fb171bd401b9b7c727521db86daa3e85fa971992
Sultonali Jalolov <jssalijalol@gmail.com>
2025-04-18 11:35:54 +0500

    feat: implement create file with content if not exist

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import fs from "fs/promises";
+
 const create = async () => {
-    // Write your code here 
+  // Write your code here
+  const fileName = "fresh.txt";
+  const fileContent = "I am fresh and young";
+
+  try {
+    await fs.access("./files/" + fileName);
+
+    throw new Error("FS operation failed");
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      await fs.writeFile("./files/" + fileName, fileContent);
+    } else {
+      throw err;
+    }
+  }
 };
 
 await create();
