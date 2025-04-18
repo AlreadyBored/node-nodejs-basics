ea11733ac902c7aa9ec3e6dce220f9ec60f2d0b7
Sultonali Jalolov <jssalijalol@gmail.com>
2025-04-18 11:37:09 +0500

    feat: implement two variants of copy directory with files

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,39 @@
+import fs from "fs/promises";
+
 const copy = async () => {
-    // Write your code here 
+  // Write your code here
+  const sourceDir = "./files";
+  const destDir = "./files_copy";
+  try {
+    await fs.access(sourceDir);
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    }
+  }
+
+  try {
+    await fs.access(destDir);
+    throw new Error("FS operation failed");
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      // Variant #1
+      // await fs.mkdir("./files_copy");
+
+      // const files = await fs.readdir("./files");
+
+      // files.forEach((fileName) => {
+      //   const source = "./files/" + fileName;
+      //   const dest = destDir + "/" + fileName;
+      //   fs.copyFile(source, dest);
+      // });
+
+      // Variant #2
+      await fs.cp(sourceDir, destDir, { recursive: true });
+    } else {
+      throw error;
+    }
+  }
 };
 
 await copy();
