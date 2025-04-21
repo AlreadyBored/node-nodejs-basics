a818fec84a5397cd3d237280a4a36a7419b18578
alima987 <alima1996b@gmail.com>
2025-04-21 12:30:38 +0500

    feat: complete wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,34 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
+import path from 'path';
 const performCalculations = async () => {
-    // Write your code here
+    const cpuCount = os.cpus().length;
+    const results = new Array(cpuCount)
+    const workers = []
+    const createWorker = (index, nums) => {
+        return new Promise((resolve) => {
+            const worker = new Worker(path.resolve('src/wt/worker.js'))
+            workers.push(worker)
+            worker.once('message', (message) => {
+              results[index] = message
+              resolve()
+            })
+            worker.once('error', () => {
+                results[index] = { status: 'error', data: null }
+                resolve()
+            })
+            worker.postMessage(nums)
+        })
+    }
+    const promises = [];
+
+    for (let i = 0; i < cpuCount; i++) {
+      const number = 10 + i;
+      promises.push(createWorker(i, number));
+    }
+
+    await Promise.all(promises);
+    console.log(results);
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -3,13 +3,15 @@ import { parentPort } from 'worker_threads';
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    process.stdin.on('data', (data) => {
-        // Process the data (in this case, just convert to uppercase)
-        const result = data.toString().toUpperCase();
-      
-        // Send the result back to the main thread via stdout
-        process.stdout.write(result);
-    });
+    parentPort.on('message', (n) => {
+      try {
+        const result = nthFibonacci(n)
+        parentPort.postMessage({ status: 'resolved', data: result })
+
+      } catch(err) {
+        parentPort.postMessage({ status: 'error', data: null })
+      }
+    })
     // This function sends result of nthFibonacci computations to main thread
 };
 
