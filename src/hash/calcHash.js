360ec43d66e66b9a377d8faca4bb41e792704ddb
Mikhail Nosov <manosov1984@gmail.com>
2025-04-22 22:58:50 +0300

    feat: implement calcHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,29 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { createHash } from 'node:crypto'
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const fileToCalculateHashFor = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+    const hash = createHash('sha256');
+
+    const readSteam = fs.createReadStream(fileToCalculateHashFor);
+
+    readSteam.on('data', (chunk) => {
+        hash.update(chunk);
+    });
+
+    readSteam.on('end', () => {
+        console.log(hash.digest('hex'));
+    });
+
+    readSteam.on('error', (err) => {
+        console.error(err);
+    });
 };
 
 await calculateHash();
