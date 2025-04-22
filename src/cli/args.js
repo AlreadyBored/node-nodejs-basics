0ba06f7b2f309e30867077664c6155429a5db9ae
Mikhail Nosov <manosov1984@gmail.com>
2025-04-22 22:04:42 +0300

    feat: implement cli/args function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2).reduce((acc, item, index, array) => (
+        item.startsWith('--')
+        ?  { ...acc, [item.slice(2)]: array[index + 1] }
+        : acc
+    ), {});
+
+    const outputArgsArr = Object.entries(args)
+        .map(([propName, value]) => `${propName} is ${value}`)
+        .join(', ');
+
+    console.log(outputArgsArr);
+    // process.stdout.write(outputArgsArr + '\n');
 };
 
 parseArgs();
