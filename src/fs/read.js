b7533ad31519f8e45b5dd6048c6b295e3936dd83
Mikhail Nosov <manosov1984@gmail.com>
2025-04-22 21:07:46 +0300

    feat: implement read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,24 @@
+import fs from 'fs/promises';
+import { constants } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const fileToRead = path.join(__dirname, 'files', 'fileToRead.txt');
+
+    try {
+        await fs.access(fileToRead, constants.F_OK);
+        await fs.access(fileToRead, constants.R_OK);
+
+        const data = await fs.readFile(fileToRead, 'utf8');
+        console.log(data);
+
+    }  catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
