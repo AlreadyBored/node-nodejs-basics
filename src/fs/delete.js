952a0d43bd892e9a86bb47df0141d21d698da0ee
Sultonali Jalolov <jssalijalol@gmail.com>
2025-04-22 16:15:54 +0500

    feat: implement delete file if it exists

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,14 @@
+import fs from "fs/promises";
+
 const remove = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    await fs.rm("./files/fileToRemove.txt");
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    }
+  }
 };
 
 await remove();
