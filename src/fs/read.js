4f4c89afe214f42cb9ccce8d815f554c83436301
Sultonali Jalolov <jssalijalol@gmail.com>
2025-04-22 17:53:24 +0500

    feat: implement read file content if file exists

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import fs from "fs/promises";
+
 const read = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    await fs.access("./files/fileToRead.txt");
+
+    const content = await fs.readFile("./files/fileToRead.txt", {
+      encoding: "utf8",
+    });
+    console.log(content);
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    }
+  }
 };
 
 await read();
