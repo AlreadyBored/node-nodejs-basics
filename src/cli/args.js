39e73a3e0553cf18f34ea0d90f9f3504f973692f
Sultonali Jalolov <jssalijalol@gmail.com>
2025-04-22 18:47:50 +0500

    feat: implement reading command line args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+  // Write your code here
+  const args = process.argv.slice(2);
+
+  const resultList = [];
+
+  for (let i = 0; i < args.length; i += 2) {
+    resultList.push(`${args[i].slice(2)} is ${args[i + 1] ?? ""}`);
+  }
+
+  console.log(resultList.join(", "));
 };
 
 parseArgs();
