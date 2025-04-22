2e1d46f2e62b0c125eccc965041a30660a6843ea
Anton Belski <anton.belski@gmail.com>
2025-04-22 17:03:35 +0300

    feat: add create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, "files", "fresh.txt");
+  try {
+    await fs.promises.access(filePath);
+    throw new Error("FS operation failed");
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      await fs.promises.writeFile(filePath, "I am fresh and young", "utf8");
+    } else {
+      throw new Error("FS operation failed");
+    }
+  }
 };
 
 await create();
