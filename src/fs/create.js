89a56d461b2472a6b297f3895fb0c94731af0439
Artur Khelshtein <artur.khelshtein@live.com>
2025-04-23 00:24:23 +0300

    feat: create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import path from 'path';
+import { promises } from 'fs';
+import { fileURLToPath } from 'url';
+
 const create = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const filePath = path.join(__dirname, 'files', 'fresh.txt');
+
+    try {
+        await promises.access(filePath);
+        console.error('FS operation failed');
+    } catch (error) {
+        if (error.code === 'ENOENT' && error.path === filePath) {
+            await promises.writeFile(filePath, 'I am fresh and young');
+        }
+    }
 };
 
 await create();
