f0d85941396d95dabbbdd82f2177e6203103f5df
Sultonali Jalolov <jssalijalol@gmail.com>
2025-04-22 17:43:15 +0500

    feat: implement printing list of files

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import fs from "fs/promises";
+
 const list = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    await fs.access("./files");
+
+    const files = await fs.readdir("./files");
+
+    files.forEach((fileName) => {
+      console.log(fileName);
+    });
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    }
+  }
 };
 
 await list();
