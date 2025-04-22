6faf8238facc3b7aca2a7380e8955f20bb3f8ca8
Mikhail Nosov <manosov1984@gmail.com>
2025-04-22 21:00:33 +0300

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,26 @@
+import fs from 'fs/promises';
+import { constants } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const list = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const filesFolder = path.join(__dirname, 'files');
+
+    try {
+        await fs.access(filesFolder, constants.F_OK);
+
+        const files = await fs.readdir(filesFolder);
+
+        await Promise.all(files.map(async (file) => {
+            console.log(file);
+        }));
+
+    }  catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await list();
