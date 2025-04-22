ff1af14ebc650f2dc37e4bfb732345fab0aeffab
Sultonali Jalolov <jssalijalol@gmail.com>
2025-04-22 18:19:03 +0500

    feat: implement reading env variables

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
 const parseEnv = () => {
-    // Write your code here 
+  // Write your code here
+  const prefix = "RSS_";
+  const list = Object.keys(process.env).filter((item) =>
+    item.startsWith(prefix)
+  );
+  const text = list.reduce((acc, cur) => {
+    return !!acc
+      ? acc + "; " + `${cur}=${process.env[cur]}`
+      : acc + `${cur}=${process.env[cur]}`;
+  }, "");
+  console.log(text);
 };
 
 parseEnv();
