3bf796835e5ecb4b01183579ba55f4672aea15f0
Mikhail Nosov <manosov1984@gmail.com>
2025-04-22 21:34:31 +0300

    feat: implement cli/env function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
 const parseEnv = () => {
-    // Write your code here 
+    try {
+        const rssEnvVars = Object.entries(process.env).reduce((acc, item) => (
+            item[0].includes('RSS')
+            ?  [...acc, `${item[0]}=${item[1]}`]
+            : acc
+        ), []);
+
+        console.log(rssEnvVars.join('; '));
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 parseEnv();
