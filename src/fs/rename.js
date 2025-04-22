d0ec84e070daeb82b38e1b3d1ec88ccf41666cf5
Sultonali Jalolov <jssalijalol@gmail.com>
2025-04-22 16:08:44 +0500

    feat: implement rename of file

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,26 @@
+import fs from "fs/promises";
+
 const rename = async () => {
-    // Write your code here 
+  // Write your code here
+
+  try {
+    await fs.access("./files/wrongFilename.txt");
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    }
+  }
+
+  try {
+    await fs.access("./files/properFilename.md");
+    throw new Error("FS operation failed");
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      await fs.rename("./files/wrongFilename.txt", "./files/properFilename.md");
+    } else {
+      throw error;
+    }
+  }
 };
 
 await rename();
