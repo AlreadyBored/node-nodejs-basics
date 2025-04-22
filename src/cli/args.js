6dd83c9c33486087ea82a400e1dbdc3d8f50354d
MiSFiTS <hamit.magic@gmail.com>
2025-04-22 17:24:44 +0500

    feat: cli done

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
     // Write your code here 
+    const result=[];
+    const variables = process.argv.slice(2)
+    for (let i=0; i<variables.length; i=i+2) {
+        if (variables[i+1])result.push(`${variables[i]} is ${variables[i+1]}`)
+        else result.push(variables[i]+ ' is undefined')
+    }
+    console.log(result.join(', '))
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,8 @@
 const parseEnv = () => {
-    // Write your code here 
+    const result=[];
+    const variables = Object.keys(process.env).filter(key => key.startsWith('RSS_'))
+    variables.forEach(key => result.push(`${key}=${process.env[key]}`))
+    console.log(result.join('; '))
 };
 
 parseEnv();
