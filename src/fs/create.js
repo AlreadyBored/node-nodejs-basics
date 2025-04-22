c88bcf1c615739fb14f8c9c03e246076ab2e844f
Jesper Høgsted <81517227+JesperHogstedDk@users.noreply.github.com>
2025-04-22 23:40:26 +0200

    create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import * as fs from 'node:fs/promises';
+import path from 'path';
+
 const create = async () => {
     // Write your code here 
+    console.log('Creating a file...');
+    const filePath = path.join('./src/fs/files', 'fresh.txt');
+    console.log('File path:', filePath);
+    const content = 'I am fresh and young!';
+    try {
+        await fs.writeFile(filePath, content, { flag: 'ax' })
+        console.log('File created successfully!');        
+    }
+    catch (error) {
+        if (error.code === 'EEXIST') {
+            console.error('File already exists!');
+        } else {
+            console.error('Error creating file:', error);
+        }
+    }
 };
 
 await create();
