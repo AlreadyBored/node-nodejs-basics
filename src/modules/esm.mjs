830ab241b97ce2abcfb20374369879bfe70463ed
MiSFiTS <hamit.magic@gmail.com>
2025-04-23 15:55:17 +0500

    feat: modules done

---
+++ src/modules/esm.mjs
@@ -0,0 +1,40 @@
+import path from 'path';
+import { release, version } from 'os'
+import { createServer } from 'http';
+import('./files/c.js');
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const random = Math.random();
+
+export let unknownObject;
+
+if (random > 0.5) {
+    unknownObject = await import('./files/a.json', {with: {type: 'json'}});
+} else {
+    unknownObject = await import('./files/b.json', {with: {type: 'json'}});
+}
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${path.sep}"`);
+
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
+
+export const myServer = createServer((_, res) => {
+    res.end('Request accepted');
+});
+
+const PORT = 3000;
+
+console.log(unknownObject);
+
+myServer.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+    console.log('To terminate it, use Ctrl+C combination');
+});
