27a2c2ddb00fd2034b38bfc7499938d818f89154
Jesper Høgsted <81517227+JesperHogstedDk@users.noreply.github.com>
2025-04-23 08:19:50 +0200

    copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,15 @@
+import * as fs from 'fs/promises';
+
 const copy = async () => {
     // Write your code here 
+    const pathFrom = './src/fs/files'
+    const pathTo = './src/fs/files_copy'
+    console.log(pathFrom)
+    try {
+        await fs.cp(pathFrom, pathTo, { recursive: true, force: false, errorOnExist: true})
+    } catch (error) {
+        console.log('Error: FS operation failed:', error)
+    }
 };
 
 await copy();
