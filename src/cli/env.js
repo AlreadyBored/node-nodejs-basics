e7763f5f72b17d775fbd7f38d6f605433891c665
alima987 <alima1996b@gmail.com>
2025-04-23 11:25:33 +0500

    feat: complete cli, wt

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,2 +1,8 @@
 const parseEnv = () => {
+  const env = process.env;
+  const rss = Object.entries(env)
+  .filter(([key]) => key.startsWith('RSS_'))
+  .map(([key, value]) => `${key}=${value}`)
+  console.log(rss.join('; '))
 };
+parseEnv()
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,5 +1,5 @@
 import { parentPort } from 'worker_threads';
-// n should be received from main thread
+
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
@@ -12,7 +12,6 @@ const sendResult = () => {
         parentPort.postMessage({ status: 'error', data: null })
       }
     })
-    // This function sends result of nthFibonacci computations to main thread
 };
 
 sendResult();
