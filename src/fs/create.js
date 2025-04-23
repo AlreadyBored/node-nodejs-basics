21c53d22355fbbe4134a2a19f0e990d39f8a29a7
Bogdan Ilin <starkavichi@gmail.com>
2025-04-23 21:55:07 +0300

    feat: implement file creation in create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+
+const __dirname = import.meta.dirname;
+
 const create = async () => {
-    // Write your code here 
+  const newFilePath = path.join(__dirname, "files", "fresh.txt");
+
+  try {
+    await fs.writeFile(newFilePath, "I am fresh and young", { flag: "wx" });
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await create();
+
