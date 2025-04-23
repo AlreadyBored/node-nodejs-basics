ea0774387563809776028b569848fc7e042ffa87
Mikhail Nosov <manosov1984@gmail.com>
2025-04-23 22:18:44 +0300

    feat: implement steams/write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
+import { constants } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const write = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const fileToWrite = path.join(__dirname, 'files', 'fileToWrite.txt');
+
+    try {
+        await fs.promises.access(fileToWrite, constants.F_OK);
+    
+        const writeStream = fs.createWriteStream(fileToWrite);
+
+        process.stdin.pipe(writeStream);
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await write();
