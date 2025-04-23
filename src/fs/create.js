b59ac5cacf2379a08f80dcb5c5b691ce15971657
oanagrecu <oana.alexandra.grecu@gmail.com>
2025-04-23 19:04:29 +0200

    Implement create.js to create fresh.txt with specific content

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import {writeFile, access, mkdir} from "fs/promises";
+import {dirname, join} from "path";
+import {fileURLToPath} from "url";
+
 const create = async () => {
-    // Write your code here 
+	const __dirname = dirname(fileURLToPath(import.meta.url));
+	const filePath = join(__dirname, "files", "fresh.txt");
+
+	try {
+		await access(filePath);
+		throw new Error("FS operation failed");
+	} catch {
+		await mkdir(dirname(filePath), {recursive: true});
+		await writeFile(filePath, "I am fresh and young");
+		console.log("File created:", filePath);
+	}
 };
 
 await create();
