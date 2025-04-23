0be5db6ed95c37bcded87ceec6b2b1353fb21b33
Mikhail Nosov <manosov1984@gmail.com>
2025-04-23 22:38:14 +0300

    feat: implement steams/transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+    const reversedSteam = new Transform({
+        transform(chunk, encoding, callback) {
+            const reversed = chunk.toString().split('').reverse().join('');
+            callback(null, reversed + '\n');
+        }
+    });
+
+    console.log('Enter text to flip (press Ctrl+D or Enter to complete input):');
+
+    process.stdin.pipe(reversedSteam).pipe(process.stdout);
 };
 
 await transform();
