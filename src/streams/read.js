f591d0bb8d76b3bdb6c42008faae1572a95accc7
Mikhail Nosov <manosov1984@gmail.com>
2025-04-23 22:01:38 +0300

    feat: implement steams/read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,29 @@
+import fs from 'fs';
+import { constants } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const fileToRead = path.join(__dirname, 'files', 'fileToRead.txt');
+
+    try {
+        await fs.promises.access(fileToRead, constants.F_OK);
+    
+        const readStream = fs.createReadStream(fileToRead);
+
+        readStream.on('data', data => {
+            process.stdout.write(data + '\n');
+        });
+
+        readStream.on('error', err => {
+            process.stdout.write(err + '\n');
+        });
+    }  catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
