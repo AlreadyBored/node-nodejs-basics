0b7be6329e223bb9e249f618f98f9b4809a452d0
Aliaksandr Torkhau <aliaksandr@sageinclusion.com>
2025-04-23 11:20:14 +0200

    feat: implement file copy and creation functions with error handling

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,25 @@
+import { access, cp } from 'fs/promises';
+
 const copy = async () => {
-    // Write your code here 
+  const files = './src/fs/files';
+  const filesCopy = './src/fs/files_copy';
+  const error = new Error('FS operation failed');
+
+  try {
+    await access(files);
+  } catch {
+    throw error;
+  }
+
+  try {
+    await access(filesCopy);
+
+    throw error;
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      await cp(files, filesCopy, { recursive: true });
+    } else throw error;
+  }
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import { access, writeFile } from 'fs/promises';
+
 const create = async () => {
-    // Write your code here 
+  const file = './src/fs/files/fresh.txt';
+  const content = 'I am fresh and young';
+  const error = new Error('FS operation failed');
+
+  try {
+    await access(file);
+
+    throw error;
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      await writeFile(file, content);
+    } else throw error;
+  }
 };
 
 await create();
