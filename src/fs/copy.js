7eab89164ce23950d254de6299f9a985b2533c95
Dmitry Lotkov <dmitry.lotkov@gmail.com>
2025-04-25 00:11:25 +0300

    feat: add copy.js and create.js tasks

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,33 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
 const copy = async () => {
-    // Write your code here 
+    const source = path.join(dirname, 'files');
+    const destination = `${source}_copy`;
+
+    try {
+        await fs.access(source);
+
+        try {
+            await fs.access(destination);
+            throw new Error('FS operation failed');
+        } catch (err) {
+            if (err.code !== 'ENOENT') {
+                // ошибка означающая нет такой папки "Error NO ENTry"
+                throw err;
+            }
+        }
+
+        // copy existing files
+        await fs.cp(source, destination, { recursive: true });
+        console.log('Folder copied successfully!');
+    } catch (err) {
+        console.log(err)
+    }
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import fs from 'fs/promises';
+
 const create = async () => {
-    // Write your code here 
+    const dir = 'src/fs/files';
+    const filePath = `${dir}/fresh.txt`;
+    const content = 'I am fresh and young';
+
+        // Check if file exists
+        try {
+            await fs.access(filePath);
+            throw new Error('FS operation failed'); // file already exists
+        } catch (err) {
+            console.log(err.message);
+        }
+
+        await fs.writeFile(filePath, content);
+    console.log('success')
 };
 
 await create();
