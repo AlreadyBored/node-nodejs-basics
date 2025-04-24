9c2408c3642bbdf1478b314c6e8879dd0c578af7
Anton Belski <anton.belski@gmail.com>
2025-04-24 22:43:00 +0300

    feat: add delete and copy file

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,23 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+  const srcDir = path.join(__dirname, "files");
+  const copyFiles = path.join(__dirname, "files_copy");
+  try {
+    await fs.promises.access(srcDir);
+    await fs.promises.access(copyFiles);
+    throw new Error("FS operation failed");
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      await fs.promises.cp(srcDir, copyFiles, { recursive: true });
+    } else {
+      throw new Error("FS operation failed");
+    }
+  }
 };
 
 await copy();
--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,15 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const remove = async () => {
-    // Write your code here 
+  const delFile = path.join(__dirname, "files", "fileToRemove.txt");
+  try {
+    await fs.promises.rm(delFile);
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await remove();
