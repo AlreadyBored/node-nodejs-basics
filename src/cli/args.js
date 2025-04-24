4d7f4852c38a9994c826752a17353a66363f04e5
arislan <kootlook.applications@gmail.com>
2025-04-24 15:04:30 +0500

    feat:  parse and print command line args in --key value format

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,20 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+
+  const parsedArgs = {};
+
+  for (let i = 0; i < args.length; i += 2) {
+    const propName = args[i].slice(2);
+    const value = args[i + 1];
+
+    parsedArgs[propName] = value;
+  }
+
+  const formattedOutput = Object.entries(parsedArgs)
+    .map(([key, value]) => `${key} is ${value}`)
+    .join(", ");
+
+  console.log(formattedOutput);
 };
 
 parseArgs();
