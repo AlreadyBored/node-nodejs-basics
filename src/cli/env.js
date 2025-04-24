2a6f26682d7b618f36874144ad5696152a50378a
arislan <kootlook.applications@gmail.com>
2025-04-24 14:53:02 +0500

    feat: implement parser of environment variables starting with prefix rss_

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envVars = process.env;
+
+  const RSS_PREFIX = "RSS_";
+  const rssVars = Object.entries(envVars)
+    .filter(([key]) => key.startsWith(RSS_PREFIX))
+    .map(([key, value]) => `${key}=${value}`)
+    .join("; ");
+
+  console.log(rssVars);
 };
 
 parseEnv();
