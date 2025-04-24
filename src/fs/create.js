14b341aef3a65ebaca062252d7bc4f3b9052f801
arislan <kootlook.applications@gmail.com>
2025-04-24 13:27:17 +0500

    feat: add file creation functionality

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import fs from "fs/promises";
+import path from "path";
+
 const create = async () => {
-    // Write your code here 
+  try {
+    const dir = path.join(process.cwd(), "files");
+    const filePath = path.join(dir, "fresh.txt");
+
+    try {
+      await fs.access(filePath);
+      throw new Error("FS operation failed");
+    } catch (error) {
+      if (error.code === "ENOENT") {
+        await fs.writeFile(filePath, "I am fresh and young");
+      } else {
+        throw error;
+      }
+    }
+  } catch (error) {
+    throw error;
+  }
 };
 
 await create();
