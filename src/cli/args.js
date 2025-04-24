ac548f4ef6185facafb061becea42a552bb83917
LeraPetrovich <valeria.petrovich@northitgroup.eu>
2025-04-24 16:00:10 +0300

    create cli folder

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+  const argsData = process.argv.slice(2);
+  const res = [];
+
+  for (let i = 0; i < argsData.length; i += 2) {
+    const name = argsData[i].replace(/^--/, "");
+    const value = argsData[i + 1];
+    res.push(`${name} is ${value}`);
+  }
+
+  console.log(res.join(", "));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,32 @@
+import fc from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const parseEnv = () => {
-    // Write your code here 
+  const envPath = path.resolve(__dirname, "../../", ".env");
+  fc.readFile(envPath, "utf8", (error, data) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    const lines = data.split("\n");
+    const rssLines = lines
+      .map((line) => line.trim())
+      .filter(
+        (line) =>
+          line.startsWith("RSS_") && !line.startsWith("#") && line.includes("=")
+      );
+
+    if (rssLines.length > 0) {
+      const output = rssLines.join("; ");
+      console.log(output);
+    } else {
+      console.log("env not has values with RSS_");
+    }
+  });
 };
 
 parseEnv();
