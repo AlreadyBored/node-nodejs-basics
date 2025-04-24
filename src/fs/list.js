4fdcc08f38ec32ae6e418afc60f1cad760359611
arislan <kootlook.applications@gmail.com>
2025-04-24 14:36:39 +0500

    feat: implement list files functionality with error handling

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import { promises as fs } from "fs";
+
 const list = async () => {
-    // Write your code here 
+  const folderPath = "files";
+
+  try {
+    try {
+      await fs.access(folderPath);
+    } catch (error) {
+      throw new Error("FS operation failed");
+    }
+
+    const files = await fs.readdir(folderPath);
+
+    console.log(files);
+  } catch (error) {
+    throw error;
+  }
 };
 
 await list();
