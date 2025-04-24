eb8e9dd265f1e8f6ba2a32f9d270555d05c0bd32
arislan <kootlook.applications@gmail.com>
2025-04-24 21:21:20 +0500

    feat: implement stdin to file writing with streams

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import { createWriteStream } from "fs";
+import { pipeline } from "stream/promises";
+
 const write = async () => {
-    // Write your code here 
+  try {
+    const fileStream = createWriteStream("files/fileToWrite.txt");
+
+    await pipeline(process.stdin, fileStream);
+
+    console.log("Data has been written to fileToWrite.txt");
+  } catch (error) {
+    console.error("Error writing to file:", error.message);
+    throw error;
+  }
 };
 
 await write();
