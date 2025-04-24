1fe198d5271af162076ce7fac35a2252b56f1124
Sultonali Jalolov <jssalijalol@gmail.com>
2025-04-24 18:56:25 +0500

    feat: implement calculating file sha256 hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import { createHash } from "node:crypto";
+import { createReadStream } from "node:fs";
+import path from "node:path";
+
 const calculateHash = async () => {
-    // Write your code here 
+  // Write your code here'
+
+  const fileName = path.resolve("./src/hash/files/fileToCalculateHashFor.txt");
+  const hash = createHash("sha256");
+  const input = createReadStream(fileName);
+
+  input.on("readable", () => {
+    const data = input.read();
+
+    if (data) {
+      hash.update(data);
+    } else {
+      console.log(`File hash: ${hash.digest("hex")}`);
+    }
+  });
 };
 
 await calculateHash();
