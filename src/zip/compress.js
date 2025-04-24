410f6ae0cad8dd5c00646369e51d6c503095231a
Mikhail Nosov <manosov1984@gmail.com>
2025-04-24 20:12:19 +0300

    feat: implement compress function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,34 @@
+import fs, { createReadStream, createWriteStream } from 'fs';
+import { constants } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream';
+
 const compress = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const fileToCompress = path.join(__dirname, 'files', 'fileToCompress.txt');
+    const archive = path.join(__dirname, 'files', 'archive.gz');
+
+
+    try {
+        await fs.promises.access(fileToCompress, constants.F_OK);
+
+        const gzip = createGzip();
+        const source = createReadStream(fileToCompress);
+        const destination = createWriteStream(archive);
+
+        pipeline(source, gzip, destination, (err) => {
+            if (err) {
+                console.error(err);
+                process.exitCode = 1;
+            }
+        });
+    } catch (error) {
+        console.error('FS operation failed');
+    }
 };
 
 await compress();
