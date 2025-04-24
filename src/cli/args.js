7c3309c0f16609aa88294500c90a3eaeee28aa63
buterbrot359 <batowka359@gmail.com>
2025-04-24 19:00:03 +0500

    feat: finish cli section

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,30 @@
 const parseArgs = () => {
-    // Write your code here 
+  const argsArr = process.argv.slice(2);
+
+  const resultObj = {};
+  for (let i = 0; i < argsArr.length - 1; i++) {
+    const arg = argsArr[i];
+    const argWithoutPrefix = arg.slice(2);
+
+    const nextArg = argsArr?.[i + 1] || null;
+
+    if (!arg.startsWith("--")) continue;
+
+    if (nextArg.startsWith("--")) {
+      continue;
+    }
+
+    resultObj[argWithoutPrefix] = nextArg;
+  }
+
+  const resultArr = [];
+  for (let [key, value] of Object.entries(resultObj)) {
+    resultArr.push(`${key} is ${value}`);
+  }
+
+  const resultString = resultArr.join(", ");
+
+  console.log(resultString);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,18 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envVarsObject = process?.env || {};
+  const allEnvKeys = Object.keys(envVarsObject);
+  const rssPrefixKeysList = allEnvKeys.filter((key) => key.startsWith("RSS_"));
+
+  const resultArr = [];
+  for (const [key, value] of Object.entries(envVarsObject)) {
+    if (!rssPrefixKeysList.includes(key)) continue;
+
+    resultArr.push(`${key}=${value}`);
+  }
+
+  const resultStr = resultArr.join("; ");
+
+  console.log(resultStr);
 };
 
 parseEnv();
