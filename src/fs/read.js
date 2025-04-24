56fefcceb2a7b2b5e9915d59d92de0747c7180ce
arislan <kootlook.applications@gmail.com>
2025-04-24 14:38:54 +0500

    feat: implement file read functionality with error handling

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,21 @@
+import { promises as fs } from "fs";
+
 const read = async () => {
-    // Write your code here 
+  const filename = "files/fileToRead.txt";
+
+  try {
+    try {
+      await fs.access(filename);
+    } catch (error) {
+      throw new Error("FS operation failed");
+    }
+
+    const content = await fs.readFile(filename, "utf8");
+
+    console.log(content);
+  } catch (error) {
+    throw error;
+  }
 };
 
 await read();
