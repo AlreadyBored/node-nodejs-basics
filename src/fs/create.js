627fd529463528d7249baa1eb401cc7c68c08212
Andrii Shopiak <andriyshopyak@gmail.com>
2025-04-24 20:19:24 +0300

    feat: add create solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,30 @@
+import { writeFile, access } from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+  const folderPath = path.join(__dirname, 'files');
+  const filePath = path.join(folderPath, 'fresh.txt');
+  
+  try {
+    await access(filePath);
+    throw new Error('FS operation failed: file already exists');
+  } catch (error) {
+    if (error.message.includes('file already exists')) {
+      console.error(error.message);
+      return;
+    }
+  }
+
+  try {
+    await writeFile(filePath, 'I am fresh and young');
+    console.log('File created successfully.');
+  } catch (writeErr) {
+    console.error('Error writing file:', writeErr.message);
+  }
 };
 
 await create();
