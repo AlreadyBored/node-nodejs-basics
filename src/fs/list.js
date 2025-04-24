f33512a8244dead44d597bd55d12a7fcddd1799f
Bogdan Ilin <starkavichi@gmail.com>
2025-04-24 21:29:05 +0300

    feat: implement file listing functionality in list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import path from "node:path";
+import { readdir } from "node:fs/promises";
+
+const dirname = import.meta.dirname;
+
 const list = async () => {
-    // Write your code here 
+  const dirPath = path.join(dirname, "files");
+
+  try {
+    const files = await readdir(dirPath);
+
+    for (const file of files) {
+      console.log(file);
+    }
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await list();
+
