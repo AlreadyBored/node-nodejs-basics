059279d1ba4bf806063e3df3537669ac7b62ed61
Mikhail Nosov <manosov1984@gmail.com>
2025-04-24 21:53:56 +0300

    feat: implemet worker functions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,32 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { Worker } from 'worker_threads';
+import os from 'os';
+
 const performCalculations = async () => {
-    // Write your code here
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const cpuCount = os.cpus().length;
+
+    const workers = Array.from({ length: cpuCount }, (_, i) => {
+        const worker = new Worker(path.join(__dirname, 'worker.js'));
+
+        worker.postMessage(10 + i);
+    
+        return new Promise(resolve => {
+            worker.on('message', data => {
+                resolve({ status: 'resolved', data });
+                worker.terminate();
+            });
+            worker.on('error', () => resolve({ status: 'error', data: null }));
+            worker.on('exit', code => code !== 0 && resolve({ status: 'error', data: null }));
+        });
+    });
+
+    const results = await Promise.all(workers);
+
+    console.log(results);
+    // process.stdout.write(JSON.stringify(results, null, 2) + '\n');
 };
 
-await performCalculations();
+performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,17 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { parentPort } from 'worker_threads';
+
+const nthFibonacci = (n) => (n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2));
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    parentPort.on('message', n => {
+        try {
+            const result = nthFibonacci(n);
+
+            parentPort.postMessage(result);
+        } catch (error) {
+            parentPort.postMessage(null);
+        }
+    });
 };
 
 sendResult();
