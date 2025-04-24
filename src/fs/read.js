9a711d108591e82a7675f4e6f7f170136aaf2f92
Bogdan Ilin <starkavichi@gmail.com>
2025-04-24 21:41:06 +0300

    feat: implement file reading functionality in read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+
+const dirname = import.meta.dirname;
+const filePath = path.join(dirname, "files", "fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const content = await fs.readFile(filePath, { encoding: "utf-8" });
+    console.log(content);
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await read();
+
