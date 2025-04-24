54d331ab5174b73abe9518e549eb7608ab135bec
LeraPetrovich <valeria.petrovich@northitgroup.eu>
2025-04-24 16:37:51 +0300

    create hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,29 @@
+import path from "path";
+import { constants } from "fs";
+import { createReadStream } from "fs";
+import { createHash } from "crypto";
+import fs from "fs/promises";
+import { pipeline } from "stream/promises";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const pathToFile = path.resolve("files", "fileToCalculateHashFor.txt");
+
+  try {
+    await fs.access(pathToFile, constants.F_OK);
+
+    const hash = createHash("sha256");
+
+    await pipeline(createReadStream(pathToFile), async function* (source) {
+      for await (const chunk of source) {
+        hash.update(chunk);
+      }
+    });
+
+    const result = hash.digest("hex");
+    console.log(result);
+  } catch (error) {
+    console.error("Ошибка:", error);
+  }
 };
 
 await calculateHash();
