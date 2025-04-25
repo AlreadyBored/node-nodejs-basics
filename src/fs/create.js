78d7fde0f3558e9ffe4d06f54706dab46e0db648
Eduard Simanenka <e.simonenko@imedia.by>
2025-04-25 17:46:04 +0300

    feat: add fs/create task solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import {writeFile, access, constants} from "fs/promises";
+import {fileURLToPath} from "url";
+import path from "path";
+
 const create = async () => {
-    // Write your code here 
+    const __fileName = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__fileName);
+    const newFilePath = path.join(__dirname, "files", "fresh.txt");
+
+    try {
+        await access(newFilePath, constants.W_OK);
+        throw new Error("The file already exists!");
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            await writeFile(newFilePath, "I am fresh and young");
+            return;
+        }
+
+        throw new Error("FS operation failed");
+    }
 };
 
 await create();
