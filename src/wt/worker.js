e6ec5e1a6efe4a75122a50d31895fe91d5b9d52c
arislan <kootlook.applications@gmail.com>
2025-04-25 12:42:51 +0500

    feat: implement worker thread for Fibonacci calculation

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,15 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { parentPort } from "worker_threads";
+
+const nthFibonacci = (n) => (n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2));
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  parentPort.on("message", (message) => {
+    const n = message.n;
+
+    const result = nthFibonacci(n);
+
+    parentPort.postMessage({ result });
+  });
 };
 
 sendResult();
