a2185e10227332a3210d857bd890a3989ec6bd80
Anton Belski <anton.belski@gmail.com>
2025-04-25 21:36:59 +0300

    feat: add hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,27 @@
+import { createHash } from "crypto";
+import { createReadStream } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const hashFilePath = path.join(
+  __dirname,
+  "files",
+  "fileToCalculateHashFor.txt"
+);
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash("sha256");
+  const hashReadStream = createReadStream(hashFilePath);
+
+  hashReadStream.on("data", (chunk) => {
+    hash.update(chunk);
+  });
+
+  hashReadStream.on("end", () => {
+    const result = hash.digest("hex");
+    console.log(result);
+  });
 };
 
 await calculateHash();
