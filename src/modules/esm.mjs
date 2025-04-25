1cd7a37f3e0e6b1612c627fe45135192fb92b490
Olha Popova <fc.odessa.10@gmail.com>
2025-04-25 14:51:23 -0700

    refactor: rewrite to ESM notation

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,16 +1,19 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import * as path from 'node:path';
+import { release, version } from 'node:os';
+import { createServer as createServerHttp } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import './files/c.cjs';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const random = Math.random();
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+    unknownObject = import('./files/a.json', { assert: { type: 'json' } });
 } else {
-    unknownObject = require('./files/b.json');
+    unknownObject = import('./files/b.json', { assert: { type: 'json' } });
 }
 
 console.log(`Release ${release()}`);
@@ -33,8 +36,6 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
-    unknownObject,
-    myServer,
-};
+export default { unknownObject, myServer };
+
 
