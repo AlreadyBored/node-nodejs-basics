92a918352583a5de259725990194514410d564c9
Sultonali Jalolov <jssalijalol@gmail.com>
2025-04-25 11:41:06 +0500

    feat: implement transform text by streams

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+import { pipeline } from "stream/promises";
+import { stdin, stdout } from "process";
+import { Transform } from "stream";
+
 const transform = async () => {
-    // Write your code here 
+  // Write your code here
+  const transformStream = new Transform({
+    transform(chunk, _, callback) {
+      const reversedText = String(chunk).split("").reverse().join("");
+      callback(null, reversedText);
+    },
+  });
+
+  await pipeline(stdin, transformStream, stdout);
 };
 
 await transform();
