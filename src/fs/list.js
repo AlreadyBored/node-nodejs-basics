4cb7360117cd68fea183ac464f8453eef60cfc9c
Andrii Shopiak <andriyshopyak@gmail.com>
2025-04-25 17:56:01 +0300

    feat: implement file listing functionality

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,22 @@
+import { access, readdir } from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+  const folderPath = path.join(__dirname, 'files');
+
+  try {
+    await access(folderPath);
+
+    const files = await readdir(folderPath);
+
+    console.log(files);
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await list();
