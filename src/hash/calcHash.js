0115a30535edf72d3f43cbda54dfe5e81b05a3a5
buterbrot359 <batowka359@gmail.com>
2025-04-25 23:22:32 +0500

    feat: finish hash section

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,34 @@
+import { createHash } from "crypto";
+import { utilsService } from "../utils.js";
+
+const CURRENT_DIRNAME = import.meta.dirname;
+
 const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const fileName = "fileToCalculateHashFor.txt";
+    const path = utilsService.createPath(CURRENT_DIRNAME, "files", fileName);
+
+    const isFileExist = await utilsService.isSrcExist(path);
+
+    if (!isFileExist) {
+      throw new Error("FS operation failed");
+    }
+
+    const content = await utilsService.readFileContent(path);
+    const data = await getHash(content);
+
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getHash = async (content) => {
+  return new Promise((res) => {
+    const hash = createHash("sha256");
+    hash.update(content);
+    res(hash.digest("hex"));
+  });
 };
 
 await calculateHash();
