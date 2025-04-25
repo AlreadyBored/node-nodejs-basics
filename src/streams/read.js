eb7e0ff291f5b367c0ed074802b890ff37b245c4
Anton Belski <anton.belski@gmail.com>
2025-04-25 23:06:43 +0300

    feat: add stream functions for read, write, transform

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import { createReadStream } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(__dirname, "files", "fileToRead.txt");
 const read = async () => {
-    // Write your code here 
+  const readStream = createReadStream(filePath, { encoding: "utf-8" });
+  readStream.pipe(process.stdout);
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import { createWriteStream } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(__dirname, "files", "fileToWrite.txt");
 const write = async () => {
-    // Write your code here 
+  const writeStream = createWriteStream(filePath);
+  process.stdin.pipe(writeStream);
 };
 
 await write();
