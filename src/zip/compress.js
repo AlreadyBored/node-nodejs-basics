47834605460a5e08731dc03a72db827fb959fce3
Kamil Mugutdinov <mugukamil@gmail.com>
2025-04-25 23:53:28 +0300

    feat: finish zlib

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,13 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import { createGzip } from "node:zlib";
+import { pipeline } from "node:stream/promises";
+
 const compress = async () => {
-    // Write your code here 
+    const gzip = createGzip();
+    const source = createReadStream(import.meta.dirname + "/files/fileToCompress.txt");
+    const destination = createWriteStream(import.meta.dirname + "/files/archive.gz");
+
+    await pipeline(source, gzip, destination);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,14 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import { createGunzip } from "node:zlib";
+import { pipeline } from "node:stream/promises";
+import { promisify } from "node:util";
+
 const decompress = async () => {
-    // Write your code here 
+    const gzip = createGunzip();
+    const source = createReadStream(import.meta.dirname + "/files/archive.gz");
+    const destination = createWriteStream(import.meta.dirname + "/files/fileToCompress.txt");
+
+    await pipeline(source, gzip, destination);
 };
 
 await decompress();
