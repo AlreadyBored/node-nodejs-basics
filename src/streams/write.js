8a2cc3aa9d826d9693bb1af16ea19402a042a99d
Sultonali Jalolov <jssalijalol@gmail.com>
2025-04-25 11:23:38 +0500

    feat: implement write to file by streams

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import { stdin } from "process";
+import { pipeline } from "stream/promises";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
 const write = async () => {
-    // Write your code here 
+  // Write your code here
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+  await pipeline(
+    stdin,
+    fs.createWriteStream(path.resolve(__dirname, "./files/fileToWrite.txt"))
+  );
 };
 
 await write();
