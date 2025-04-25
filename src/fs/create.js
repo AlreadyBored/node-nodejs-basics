d9642abd0302e91e1690cc79f6f8c4dd5b62059f
Oscarraizer <boltenkovs36@gmail.com>
2025-04-26 00:01:08 +0700

    feat: implement 'create' function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,12 @@
+import { writeFile } from "node:fs/promises";
+
 const create = async () => {
-    // Write your code here 
+  const filePath = "./src/fs/files/fresh.txt";
+  try {
+    await writeFile(filePath, "I am fresh and young", { flag: "wx" });
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await create();
