f211bf209ba64bf5be18cca9eced00e19f393cca
Ruben Martirosyan <rubboss1992@gmail.com>
2025-04-25 16:57:34 +0400

    feat(cli): add env vars and args parsing

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    
+    const result = [];
+    for (let i = 0; i < args.length; i += 2) {
+        const prop = args[i].slice(2);
+        const value = args[i + 1];
+        result.push(`${prop} is ${value}`);
+    }
+    
+    console.log(result.join(', '));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
+import { env } from 'process';
+
 const parseEnv = () => {
-    // Write your code here 
+    const rssVars = Object.entries(env)
+        .filter(([key]) => key.startsWith('RSS_'))
+        .map(([key, value]) => `${key}=${value}`)
+        .join('; ');
+    
+    console.log(rssVars);
 };
 
 parseEnv();
