31b66504dba38e18a7b184ae45e63703be773e73
Anton Belski <anton.belski@gmail.com>
2025-04-25 17:11:29 +0300

    feat: add command line interface(src/cli)

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const res = [];
+
+  for (let i = 0; i < args.length; i += 2) {
+    const key = args[i].replace("--", "");
+    const value = args[i + 1];
+    res.push(`${key} is ${value}`);
+  }
+  console.log(res.join(", "));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
 const parseEnv = () => {
-    // Write your code here 
+ const allEnv = process.env;
+ const res = [];
+
+  for (const key in allEnv) {
+    if (key.startsWith('RSS_')) {
+        res.push(`${key}=${allEnv[key]}`); 
+    }
+  }
+
+  console.log(res.join('; '));
+
 };
 
 parseEnv();
