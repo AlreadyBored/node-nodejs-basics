4fcbbd5bf0cc1a935ff141db1dc3d3c4f3b68a88
Oscarraizer <boltenkovs36@gmail.com>
2025-04-26 03:52:10 +0700

    feat: implement 'rename' function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,14 @@
+import { rename as fsRename } from "node:fs/promises";
+
 const rename = async () => {
-    // Write your code here 
+  const oldPath = "./src/fs/files/wrongFilename.txt";
+  const newPath = "./src/fs/files/properFilename.md";
+
+  try {
+    await fsRename(oldPath, newPath);
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await rename();
