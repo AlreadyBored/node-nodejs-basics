c9a939dbdc0c6a4b2bbd4ad4a2293fe252f10a7f
Sasha Kalko <kalko.alexandr.dg@gmail.com>
2025-04-25 22:06:59 +0300

    feat: implement list read

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,15 @@
+import { access, readdir} from 'node:fs/promises';
+
+
 const list = async () => {
-    // Write your code here 
+    const folder = './files';
+    try {
+        await access(folder)
+        const listFiles = await readdir(folder);
+        console.log(listFiles);
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await list();
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,14 @@
+import { access, readFile } from 'node:fs/promises';
+
 const read = async () => {
-    // Write your code here 
+    const fileRead = './files/fileToRead.txt';
+    try {
+        await access(fileRead);
+        const contents = await readFile(fileRead, { encoding: 'utf8' });
+        console.log(contents);
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
