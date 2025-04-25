71f319cf558d9b2fff44cbb72d66894266f1386c
iurii-oleinik <iurii.oleinik@alpeso.io>
2025-04-25 17:08:33 +0300

    feat: update cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,19 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv;
+    
+    const formattedArgs = [];
+
+    for (let i = 0; i < args.length; i += 2) {
+        if (args[i].startsWith('--')) {
+            const propName = args[i].slice(2);
+            const value = args[i + 1] || '';
+            formattedArgs.push(`${propName} is ${value}`);
+        }
+    }
+
+    if (formattedArgs.length > 0) {
+        console.log(formattedArgs.join(', '));
+    }
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+    const envVars = Object.entries(process.env)
+        .filter(([key]) => key.startsWith('RSS_'))
+        .map(([key, value]) => `${key}=${value}`)
+        .join('; ');
+
+    console.log(envVars);
 };
 
 parseEnv();
