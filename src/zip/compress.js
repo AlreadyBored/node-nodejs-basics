ade12ffc0a29a169d476321faba21a85db5dd698
Sultonali Jalolov <jssalijalol@gmail.com>
2025-04-25 15:01:18 +0500

    feat: implement compress to zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import { createReadStream, createWriteStream } from "fs";
+import path from "path";
+import { pipeline } from "stream/promises";
+import { fileURLToPath } from "url";
+import { createGzip } from "zlib";
+
 const compress = async () => {
-    // Write your code here 
+  // Write your code here
+
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const readable = createReadStream(
+    path.resolve(__dirname, "./files/fileToCompress.txt")
+  );
+  const writable = createWriteStream(
+    path.resolve(__dirname, "./files/archive.gz")
+  );
+
+  await pipeline(readable, createGzip(), writable);
 };
 
 await compress();
