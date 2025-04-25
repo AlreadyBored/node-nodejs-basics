1e413a42b90664f385e401e899e39ac04155f3e6
buterbrot359 <batowka359@gmail.com>
2025-04-25 13:46:18 +0500

    feat: finish modules section

---
+++ src/modules/esm.mjs
@@ -0,0 +1,41 @@
+import path from "path";
+import { release, version } from "os";
+import { createServer as createServerHttp } from "http";
+import aJson from "./files/a.json" with { type: "json" };
+import bJson from "./files/b.json" with { type: "json" };
+
+import "./files/c.cjs"
+
+const random = Math.random();
+const __dirname = import.meta.dirname;
+const __filename = import.meta.filename;
+
+let unknownObject;
+
+if (random > 0.5) {
+  unknownObject = aJson;
+} else {
+  unknownObject = bJson;
+}
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${path.sep}"`);
+
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
+
+const myServer = createServerHttp((_, res) => {
+  res.end("Request accepted");
+});
+
+const PORT = 3000;
+
+console.log(unknownObject);
+
+myServer.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+  console.log("To terminate it, use Ctrl+C combination");
+});
+
+export { unknownObject, myServer };
