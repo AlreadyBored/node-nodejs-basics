16e16b2bfa4da337e0cac25604868fe7274fd91b
Olha Popova <fc.odessa.10@gmail.com>
2025-04-24 18:43:30 -0700

    fix: correct source folder path in copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -8,8 +8,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const copy = async () => {
-    const srcFolder = join(__dirname, '..', 'files');
-    const destFolder = join(__dirname, '..', 'files_copy');
+    const srcFolder = join(__dirname, 'files');
+    const destFolder = join(__dirname, 'files_copy');
 
     try {
         await access(srcFolder, constants.F_OK);
@@ -18,9 +18,7 @@ const copy = async () => {
             await access(destFolder, constants.F_OK);
             throw new Error('FS operation failed');
         } catch (err) {
-            if (err.code !== 'ENOENT') {
-                throw new Error('FS operation failed');
-            }
+            if (err.code !== 'ENOENT') throw new Error('FS operation failed');
         }
 
         await cp(srcFolder, destFolder, { recursive: true });
