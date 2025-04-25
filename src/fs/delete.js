d52a12ebeceeb45bc0e34bbb1aa0d8311112502d
Sasha Kalko <kalko.alexandr.dg@gmail.com>
2025-04-25 21:47:54 +0300

    feat: implement delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,15 @@
+import { access, rm } from 'node:fs/promises';
+
+
 const remove = async () => {
-    // Write your code here 
+    const fileDelete = './files/fileToRemove.txt'
+    try {
+        await access(fileDelete);
+        await rm(fileDelete);
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
+    
 };
 
 await remove();
