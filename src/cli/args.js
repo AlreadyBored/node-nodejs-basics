c21000f95ddf7291d17c06b55aa259cd12e69f60
Olha Popova <fc.odessa.10@gmail.com>
2025-04-24 20:31:25 -0700

    feat: implement parseArgs to handle and log command line arguments

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,23 @@
 const parseArgs = () => {
-    // Write your code here 
+    const inputArgs = process.argv.slice(2);
+
+    const argsObj = {};
+
+    for (let i = 0; i < inputArgs.length; i++) {
+        if (inputArgs[i].startsWith('--')) {
+            const propName = inputArgs[i].slice(2);
+            if (i + 1 < inputArgs.length && !inputArgs[i + 1].startsWith('--')) {
+                argsObj[propName] = inputArgs[i + 1];
+                i++;
+            }
+        }
+    }
+
+    const output = Object.entries(argsObj)
+        .map(([key, value]) => `${key} is ${value}`)
+        .join(', ');
+
+    console.log(output);
 };
 
 parseArgs();
