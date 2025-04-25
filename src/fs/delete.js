3e516c069dbac7a7764b9016a8b5d10d6a08af34
Andrii Shopiak <andriyshopyak@gmail.com>
2025-04-25 17:44:45 +0300

    feat: implement file removal functionality

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,21 @@
+import { access, unlink } from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, 'files', 'fileToRemove.txt');
+
+  try {
+    await access(filePath);
+    await unlink(filePath);
+
+    console.log('File removed successfully.');
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await remove();
