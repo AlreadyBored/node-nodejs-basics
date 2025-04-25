81c04bc20f697c7f05ecf97f07a270800f773436
Sultonali Jalolov <jssalijalol@gmail.com>
2025-04-25 10:56:08 +0500

    feat: implement read from file by streams api

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,29 @@
+import { stdout } from "process";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
 const read = async () => {
-    // Write your code here 
+  // Write your code here
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const filePath = path.resolve(__dirname, "files", "fileToRead.txt");
+
+  const readable = fs.createReadStream(filePath);
+
+  readable.on("data", (chunk) => {
+    stdout.write(chunk);
+  });
+
+  readable.on("end", () => {
+    stdout.write("\n");
+  });
+
+  readable.on("error", (err) => {
+    console.error(err);
+  });
 };
 
 await read();
