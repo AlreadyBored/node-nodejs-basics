7b9b9d161da52d968d87902c7a5bf52a7f396636
Sultonali Jalolov <jssalijalol@gmail.com>
2025-04-25 16:04:47 +0500

    feat: implement decompress of file

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,29 @@
+import { createReadStream, createWriteStream } from "fs";
+import { access } from "fs/promises";
+import path from "path";
+import { createUnzip } from "zlib";
+import { pipeline } from "stream/promises";
+import { fileURLToPath } from "url";
+
 const decompress = async () => {
-    // Write your code here 
+  // Write your code here
+
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const compressedFile = path.resolve(__dirname, "./files/archive.gz");
+  const fileName = path.resolve(__dirname, "./files/uncompressedFile.txt");
+
+  try {
+    await access(compressedFile);
+
+    const readable = createReadStream(compressedFile);
+    const writable = createWriteStream(fileName);
+
+    await pipeline(readable, createUnzip(), writable);
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      console.log("Compressed file not found");
+    }
+  }
 };
 
 await decompress();
