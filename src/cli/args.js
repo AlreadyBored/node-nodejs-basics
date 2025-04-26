8ba8104d60b2a51e8923c9fc32b2894a7421d00c
UnluckyDjentleman <v.goroshchenja@gmail.com>
2025-04-26 15:09:30 +0300

    fix: trynna fix commits

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-     
+     const argumentsArray=process.argv.slice(2)
+     for(let i=0;i<argumentsArray;i++){
+        if(argumentsArray[i].startsWith('--')){
+            
+        }
+        else{
+
+        }
+     }
 };
 
 parseArgs();
