359a02fa1cb43ac816f2c70d06496355ba497399
UnluckyDjentleman <v.goroshchenja@gmail.com>
2025-04-26 13:28:51 +0300

    init: start module node.js basics, feat: add work with file system

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(path.join(__dirname, 'files'), 'fileToRemove.txt');
+
 const remove = async () => {
-    // Write your code here 
+    if(!fs.existsSync(filePath)){
+        throw new Error('FS operation failed');
+    }
+    fs.rm(filePath, (err) => {
+        if (err) throw err;
+    });
 };
 
 await remove();
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+//define file to read
+const filePath = path.join(path.join(__dirname, 'files'), 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    if(!fs.existsSync(filePath)){
+            throw new Error('FS operation failed');
+    }
+    fs.readFile(filePath, (err,data) => {
+        if (err) throw err;
+        console.log(data.toString());
+    });
 };
 
 await read();
