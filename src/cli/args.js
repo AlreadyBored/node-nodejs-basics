f78f5a3806b699303409dc703c552d837b9c2f37
UnluckyDjentleman <v.goroshchenja@gmail.com>
2025-04-26 15:35:23 +0300

    feat: add args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,13 +1,17 @@
 const parseArgs = () => {
      const argumentsArray=process.argv.slice(2)
-     for(let i=0;i<argumentsArray;i++){
-        if(argumentsArray[i].startsWith('--')){
-            
-        }
-        else{
+     const obj={}
 
-        }
+     //make an object to parse
+     for(let i=0;i<argumentsArray.length;i+=2){
+      let fieldKey=argumentsArray[i].substring(2);
+      obj[fieldKey]=argumentsArray[i+1]
      }
+
+     //output
+     console.log(Object.entries(obj)
+     .map(([key,value])=>`${key} is ${value}`)
+     .join(', '));
 };
 
 parseArgs();
