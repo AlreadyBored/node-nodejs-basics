7170848209826e0d556ca87814cd30dcfa163ea3
Bogdan Ilin <starkavichi@gmail.com>
2025-04-26 10:27:33 +0300

    feat: implement argument parsing functionality in args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,20 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const keySign = "--";
+  const parsedArgs = [];
+
+  for (let i = 0; i < args.length; i++) {
+    const arg = args[i];
+
+    if (arg.startsWith(keySign)) {
+      const key = arg.replace(keySign, "");
+      const value = args[i + 1];
+      parsedArgs.push(`${key} is ${value}`);
+    }
+  }
+
+  console.log(parsedArgs.join(", "));
 };
 
 parseArgs();
+
