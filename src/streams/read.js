64f512f2ae45e17dc4963014f334cae55801debd
Bogdan Ilin <starkavichi@gmail.com>
2025-04-26 13:51:40 +0300

    feat: implement file reading functionality using Streams API in read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import fs from "node:fs";
+import path from "node:path";
+
 const read = async () => {
-    // Write your code here 
+  const pathFile = path.join(import.meta.dirname, "files", "fileToRead.txt");
+  const readStream = fs.createReadStream(pathFile, { encoding: "utf-8" });
+
+  readStream.on("error", (error) => {
+    console.error("Failed to read file:", error);
+  });
+
+  readStream.pipe(process.stdout);
 };
 
 await read();
+
