d518af6f913410a4e7278b389e62679c929cbd26
Jesper Høgsted <81517227+JesperHogstedDk@users.noreply.github.com>
2025-04-26 10:30:42 +0200

    feat: implement read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,13 @@
+import * as fs from 'node:fs'
+
 const read = async () => {
     // Write your code here 
+    fs.readFile('./src/fs//files/fileToRead.txt', { encoding: 'utf-8' }, (err, content) => {
+        if (err) {
+            throw Error('FS operation failed', { cause: err.message })
+        }
+        console.log(content);
+    })
 };
 
 await read();
