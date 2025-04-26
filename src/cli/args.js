d0786a623713c5fd40585f3f795200ec859ef21e
Anastasiya Stashevskaya <anastasiya@vibez.io>
2025-04-26 13:45:23 +0300

    create cli functions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const parsedArgs = [];
+
+  for (let i = 0; i < args.length; i += 2) {
+    const key = args[i].replace(/^--/, "");
+    const value = args[i + 1];
+    if (key && value) {
+      parsedArgs.push(`${key} is ${value}`);
+    }
+  }
+
+  console.log(parsedArgs.join(", "));
 };
 
 parseArgs();
