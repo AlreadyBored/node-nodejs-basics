fcbcae7996c0ae430c19068264ed5cab5e4a6d4b
AndreevSV <andreevsv@gmail.com>
2025-04-26 14:29:58 +0300

    Basics cli done

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,16 @@
+// env.js - implement function that parses environment variables
+// with prefix RSS_ and prints them to the console in the format
+// RSS_name1=value1; RSS_name2=value2
+
 const parseEnv = () => {
-    // Write your code here 
+    // Write your code here
+    // console.log(process.env);
+
+    for (let variable in process.env) {
+        if (variable.startsWith('RSS_')) {
+            console.log(`${variable}=${process.env[variable]}`);
+        }
+    }
 };
 
 parseEnv();
