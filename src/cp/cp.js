2d0ca9e3390b5ed46e1cbf2d7d645f38162cb063
Kamil Mugutdinov <mugukamil@gmail.com>
2025-04-26 14:03:38 +0300

    feat: finish child_process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,8 @@
+import { fork } from "node:child_process";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    fork(`${import.meta.dirname}/files/script.js`, args);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["someArgument1", "someArgument2"]);
