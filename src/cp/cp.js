8ab665140687df6b293266fd97b99ef8c1537dcd
MiSFiTS <hamit.magic@gmail.com>
2025-04-26 12:48:52 +0500

    feat: cp done

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,8 +1,11 @@
-import { spawn } from 'node:child_process';
-
+import { spawn } from 'child_process';
 const spawnChildProcess = async (args) => {
     // Write your code here
-    console.log(spawn('node', args))
+    const data = spawn('node', ['./src/cp/files/script.js', ...args]);
+
+    data.stdout.on('data', (d) => process.stdout.write(d))
+    data.stderr.on('data', (d) => process.stdout.write(d))
+    data.on('close', (code) => process.stdout.write(code))
 };
 
 // Put your arguments in function call to test this functionality
