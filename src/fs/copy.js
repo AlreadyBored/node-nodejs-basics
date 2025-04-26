44dbd7e832eb4679e0c3f7472ed7d9aeabaca6b5
UnluckyDjentleman <v.goroshchenja@gmail.com>
2025-04-26 13:41:11 +0300

    init: start nodejs basics, feat: add fs module

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+//define source and destination
+const sourceDir=path.join(__dirname,'files')
+const destinationDir=path.join(__dirname,'files_copy')
+
 const copy = async () => {
-    // Write your code here 
+    if(fs.existsSync(destinationDir)||!fs.existsSync(sourceDir)){
+        throw new Error('FS operation failed')
+    }
+    fs.cp(sourceDir, destinationDir,{recursive: true},(err)=>{
+        if (err) throw err;
+    });
 };
 
 await copy();
--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+//define directory
+const directoryToList=path.join(__dirname,'files')
+
 const list = async () => {
-    // Write your code here 
+    if(!fs.existsSync(directoryToList)){
+        throw new Error('FS operation failed')
+    } 
+    fs.readdir(directoryToList,(err, data)=>{
+        if(err) throw err
+        data.forEach(file=>{
+            console.log(file);
+        })
+    })
 };
 
 await list();
