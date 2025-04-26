351293db8f168ca8590f52adf0d4b7337e4c804c
Anton Belski <anton.belski@gmail.com>
2025-04-26 10:54:30 +0300

    feat: add worker threads to compute Fibonacci

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,19 @@
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const performCalculations = async () => {
-    // Write your code here
+    return await Promise.all(
+        cpus().map((_, i) => new Promise((resolve) => {
+            const worker = new Worker(join(__dirname, 'worker.js'));
+            worker.postMessage(10 + i);
+            worker.on('message', (msg) => resolve(msg));
+            worker.on('error', () => resolve({ status: 'error', data: null }));
+        }))
+    );
 };
 
-await performCalculations();
+performCalculations().then(console.log);
