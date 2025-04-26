0381c67950341fc5d277c7ac7e30ff610549bb60
Anton Belski <anton.belski@gmail.com>
2025-04-26 11:25:14 +0300

    feat: setup child process for stdin/stdout interaction

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,16 @@
+import { spawn } from 'child_process';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = spawn('node', [join(__dirname,'files', 'script.js'), ...args], {
+        stdio: ['pipe', 'pipe', 'inherit'] 
+      });
+      process.stdin.pipe(child.stdin);
+      child.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['arg1', 'arg2']);
