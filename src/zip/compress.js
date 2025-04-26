b042a80b2471f823600d29511f01a6e0d7dd15c2
Yaroslav <y.silkin@netrika.ru>
2025-04-26 20:25:40 +0300

    feat: add zip solutions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,28 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { dirname, join } from "node:path";
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const compress = async () => {
-    // Write your code here 
+    const source = join(__dirname, 'files', 'fileToCompress.txt');
+    const destination = join(__dirname, 'files', 'archive.gz');
+
+    const readStream = createReadStream(source);
+    const writeStream = createWriteStream(destination);
+    const gzip = createGzip();
+
+    try {
+        await pipeline(
+            readStream,
+            gzip,
+            writeStream
+        );
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,28 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { dirname, join } from "node:path";
+import { createGunzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const decompress = async () => {
-    // Write your code here 
+    const source = join(__dirname, 'files', 'archive.gz');
+    const destination = join(__dirname, 'files', 'fileToCompress2.txt');
+
+    const readStream = createReadStream(source);
+    const writeStream = createWriteStream(destination);
+    const gunzip = createGunzip();
+
+    try {
+        await pipeline(
+            readStream,
+            gunzip,
+            writeStream
+        );
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await decompress();
