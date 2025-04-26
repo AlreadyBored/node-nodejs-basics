cf6cd5174201dba4afd9c5382b8cca906b0a5db2
Bogdan Ilin <starkavichi@gmail.com>
2025-04-26 13:02:44 +0300

    feat: implement file hash calculation functionality in calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import path from "node:path";
+import fs from "node:fs";
+import crypto from "node:crypto";
+import stream from "node:stream/promises";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = path.join(import.meta.dirname, "files", "fileToCalculateHashFor.txt");
+
+  const hash = crypto.createHash("sha256");
+  const readStream = fs.createReadStream(filePath);
+
+  try {
+    await stream.pipeline(readStream, hash);
+
+    console.log(hash.digest("hex"));
+  } catch (error) {
+    console.error("Failed to calculate hash:", error);
+  }
 };
 
 await calculateHash();
+
