58ebb4fff01d0471b8ae8a7f2db5a4d108d44db7
UnluckyDjentleman <v.goroshchenja@gmail.com>
2025-04-26 15:45:06 +0300

    feat: add calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import fs from 'fs';
+import path from 'path';
+import crypto from 'crypto';
+import { stdout } from 'process';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(path.join(__dirname, 'files'), 'fileToCalculateHashFor.txt');
 const calculateHash = async () => {
-    // Write your code here 
+     var rS=fs.createReadStream(filePath);
+     var hash=crypto.createHash('sha1');
+     rS.pipe(hash).setEncoding('hex').pipe(stdout);
 };
 
 await calculateHash();
