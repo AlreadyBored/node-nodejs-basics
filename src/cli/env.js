3ef9cdb1277421d343528381361dff2db4db0b7b
Jesper Høgsted <81517227+JesperHogstedDk@users.noreply.github.com>
2025-04-26 21:57:50 +0200

    refactor: remove last ';' from output in env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -2,15 +2,11 @@ import * as ev from 'node:process'
 
 const parseEnv = () => {
     // Write your code here 
-    const processorPreFix = 'PROCESSOR_'
     const rSSPreFix = 'RSS_'
     const envVariables = process.env
-
     const rSSVariables = findPrefix(envVariables, rSSPreFix)
-    console.log('Environment variables starting with', rSSPreFix, ':\n', rSSVariables.length > 0 ? print(rSSVariables) : 'RSS variables not present in this session!')
-
-    const processorVaiables = findPrefix(envVariables, processorPreFix)
-    console.log('Environment variables starting with', processorPreFix, ':\n', print(processorVaiables))
+    console.log('Environment variables starting with', rSSPreFix, ':\n', 
+        rSSVariables.length > 0 ? print(rSSVariables) : 'RSS variables not present in this session!')
 };
 
 function findPrefix(envVariables, preFix) {
@@ -28,7 +24,8 @@ const print = (list) => {
     var toPrint = ''
     for (let index = 0; index < list.length; index++) {
         const element = list[index];
-        toPrint += element + '; '
+        const semiColon = (index < list.length -1 === true) ? '; ' : ''
+        toPrint += element + semiColon
     }
     return toPrint
 }
