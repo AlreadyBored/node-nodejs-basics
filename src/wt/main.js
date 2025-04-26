8545071941c7be9c9d6f0558287ab4652f73f586
LeraPetrovich <valeria.petrovich@northitgroup.eu>
2025-04-26 13:02:11 +0300

    create workers task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,35 @@
+import { Worker } from "worker_threads";
+import path, { resolve } from "path";
+import os from "os";
+
 const performCalculations = async () => {
-    // Write your code here
+  const cpus = os.cpus();
+  const startNum = 10;
+
+  const promises = cpus.map((_, index) => {
+    return new Promise((resolve) => {
+      const worker = new Worker(path.resolve("worker.js"), { type: "module" });
+
+      worker.postMessage(startNum + index);
+
+      worker.on("message", (result) => {
+        resolve({
+          status: "resolved",
+          data: result,
+        });
+      });
+
+      worker.on("error", () => {
+        resolve({
+          status: "error",
+          data: null,
+        });
+      });
+    });
+  });
+
+  const results = await Promise.all(promises);
+  console.log(results);
 };
 
 await performCalculations();
