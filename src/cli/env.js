21b12833efc2454d1c54b902f8a69a0df14acc6e
Jesper Høgsted <81517227+JesperHogstedDk@users.noreply.github.com>
2025-04-26 10:40:00 +0200

    feat: implement parseEnv in env.js
    refactor: minor cleanup in list.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,37 @@
+import * as ev from 'node:process'
+
 const parseEnv = () => {
     // Write your code here 
+    const processorPreFix = 'PROCESSOR_'
+    const rSSPreFix = 'RSS_'
+    const envVariables = process.env
+
+    const rSSVariables = findPrefix(envVariables, rSSPreFix)
+    console.log('Environment variables starting with', rSSPreFix, ':\n', rSSVariables.length > 0 ? print(rSSVariables) : 'RSS variables not present in this session!')
+
+    const processorVaiables = findPrefix(envVariables, processorPreFix)
+    console.log('Environment variables starting with', processorPreFix, ':\n', print(processorVaiables))
 };
 
+function findPrefix(envVariables, preFix) {
+    const foundList = [];
+    for (const [key, value] of Object.entries(envVariables)) {
+        const preFixExist = key.startsWith(preFix);
+        if (preFixExist) {
+            foundList.push(key + '=' + value);
+        };
+    }
+    return foundList
+}
+
+const print = (list) => {
+    var toPrint = ''
+    for (let index = 0; index < list.length; index++) {
+        const element = list[index];
+        toPrint += element + '; '
+    }
+    return toPrint
+}
+
 parseEnv();
+
--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,4 @@
 import fs from 'node:fs';
-import path from 'node:path';
 
 const list = async () => {
     // Write your code here
