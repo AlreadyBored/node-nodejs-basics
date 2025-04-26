249918803d2b3d72fab90d5514d3188ea588fdcc
AndreevSV <andreevsv@gmail.com>
2025-04-26 15:10:12 +0300

    fix: change console.log to cut unnecessary signs -- before propName

--- src/cli/args.js
+++ src/cli/args.js
@@ -6,7 +6,7 @@
 const parseArgs = () => {
     // Write your code here
     for (let i = 2; i < process.argv.length; i = i + 2) {
-        console.log(`${process.argv[i]} is ${process.argv[i + 1]}`);
+        console.log(`${process.argv[i].slice(2)} is ${process.argv[i + 1]}`);
     }
 };
 
