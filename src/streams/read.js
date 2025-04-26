062cf66d33e70b67faf520ca02b7b55cb63e8857
UnluckyDjentleman <v.goroshchenja@gmail.com>
2025-04-26 16:33:15 +0300

    feat: start streams module

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { stdout } from 'process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(path.join(__dirname, 'files'), 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+     const rS=fs.createReadStream(filePath);
+     rS.pipe(stdout);
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(path.join(__dirname, 'files'), 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+    const fS=fs.createWriteStream(filePath);
+    process.stdin.pipe(fS);
 };
 
 await write();
