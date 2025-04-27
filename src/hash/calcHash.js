2719578bd92fe52c90f3b8d47aac9ab07a72be95
Anastasiya Zizeka <anastasiya.zizeka@LeverX.local>
2025-04-28 03:58:12 +0500

    feat: add chamges for hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { readFile } from 'fs/promises';
+import { createHash } from 'node:crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url),
+          __dirname = dirname(__filename),
+          fullPathToTargetFile = resolve(__dirname, './files/fileToCalculateHashFor.txt'),
+          fileContent = await readFile(fullPathToTargetFile, 'utf8');
+          
+    const resultHash = createHash('sha256').update(fileContent).digest('hex');
+    console.log(resultHash);
 };
 
 await calculateHash();
