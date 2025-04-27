004b37227b595f8abe4363bc0a93789f88245a04
Bogdan Ilin <starkavichi@gmail.com>
2025-04-27 14:07:04 +0300

    feat: implement file compression functionality in compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import path from "node:path";
+import { createReadStream, createWriteStream } from "node:fs";
+import { createGzip } from "node:zlib";
+import { pipeline } from "node:stream/promises";
+
+const dirname = import.meta.dirname;
+
 const compress = async () => {
-    // Write your code here 
+  const pathFile = path.join(dirname, "files", "fileToCompress.txt");
+  const pathGzip = path.join(dirname, "files", "archive.gz");
+
+  const gzip = createGzip();
+  const readStream = createReadStream(pathFile);
+  const writeStream = createWriteStream(pathGzip);
+
+  try {
+    await pipeline(readStream, gzip, writeStream);
+  } catch (error) {
+    console.error("Failed to compress file:", error);
+  }
 };
 
 await compress();
+
