b6925b3eab7d1f507fb7d51f2bb6578a4c9f818b
Egor Mishin <em@egormishin.com>
2025-04-27 15:05:14 +0300

    refactoring

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,16 +1,16 @@
 
-import {pathExists} from '../../libs/fs/pathExists.js';
-import path from 'node:path';
 import { unlink } from 'fs/promises';
-import {ERROR_MSG, FILES_PATH} from '../constants.js';
+import path from 'node:path';
+import { pathExists } from '../../libs/fs/pathExists.js';
+import { ERROR_MSG, FILES_PATH } from '../constants.js';
 
-// const COPY_FOLDER_PATH = 'files_copy';
 const FOLDER_URL = path.join(import.meta.dirname, FILES_PATH);
+const FILE_NAME = 'fileToRemove.txt';
 
 const remove = async () => {
     try {
         if(await pathExists(FOLDER_URL)) {
-            unlink(path.join(import.meta.dirname, FILES_PATH, 'fileToRemove.txt'))
+            unlink(path.join(import.meta.dirname, FILES_PATH, FILE_NAME))
         }
 
     }
