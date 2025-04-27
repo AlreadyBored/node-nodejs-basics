04b0f2915adb3ddefbedf434ec7b8c25ca01051f
davidtavartk <d.tavartkiladze5@gmail.com>
2025-04-28 01:30:15 +0400

    feat: add rename.js logic

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,19 @@
+import { rename as renamePromise} from "node:fs/promises";
+import { join, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
 const rename = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+  
+    const oldPath = join(__dirname, "files", "wrongFilename.txt");
+    const newPath = join(__dirname, "files", "properFilename.md"); 
+
+    try {
+        await renamePromise(oldPath, newPath);
+      } catch (err) {
+        throw new Error("FS operation failed");
+      }
 };
 
 await rename();
