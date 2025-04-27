d59e2c27a9624ecee9a0195fa1afea58f2a77c78
davidtavartk <d.tavartkiladze5@gmail.com>
2025-04-28 03:26:10 +0400

    feat: add streams write logic

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,26 @@
+import { createWriteStream } from 'node:fs';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const write = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const filePath = join(__dirname, 'files', 'fileToWrite.txt');
+
+    return new Promise((resolve, reject) => {
+        const writeStream = createWriteStream(filePath);
+
+        writeStream.on('error', (err) => {
+            reject(err);
+        });
+
+        process.stdin.pipe(writeStream);
+
+        process.stdin.on('end', () => {
+            resolve();
+        });
+    });
 };
 
 await write();
