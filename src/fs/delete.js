d0185252805c1dc6489ceb7709fe06e01d143e4d
Oscarraizer <boltenkovs36@gmail.com>
2025-04-27 16:23:20 +0700

    feat: implement 'delete' function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,13 @@
+import { rm } from "node:fs/promises";
+
 const remove = async () => {
-    // Write your code here 
+  const deleteTarget = "./src/fs/files/fileToRemove.txt";
+
+  try {
+    await rm(deleteTarget);
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await remove();
