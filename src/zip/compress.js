32f169a6c6786f4e8605ab5e04eb11676d0755fd
Anastasiya Stashevskaya <anastasiya@vibez.io>
2025-04-27 15:54:29 +0300

    feat: write compress/decompress functions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { createGzip } from "zlib";
+
 const compress = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, "files", "fileToCompress.txt");
+  const destinationPath = path.join(__dirname, "files", "archive.gz");
+
+  const readStream = fs.createReadStream(filePath);
+  const writeStream = fs.createWriteStream(destinationPath);
+  const gzip = createGzip();
+
+  readStream.pipe(gzip).pipe(writeStream);
+
+  return new Promise((resolve, reject) => {
+    writeStream.on("finish", resolve);
+    writeStream.on("error", reject);
+  });
 };
 
 await compress();
