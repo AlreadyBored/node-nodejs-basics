f84825ac5f053003be1c931823957eedb779d3ca
Jesper Høgsted <81517227+JesperHogstedDk@users.noreply.github.com>
2025-04-27 14:30:18 +0200

    feat: implement write in write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,9 @@
 const write = async () => {
     // Write your code here 
+    const { createWriteStream } = await import('fs')
+    const writableStream = createWriteStream('./src/streams/files/fileToWrite.txt');
+    process.stdin.pipe(writableStream);
+    console.log('Type something and press Enter. It will be saved to fileToWrite.txt.');
 };
 
 await write();
