ab522cc037c169a30f08975674fc6a30db3f072d
Bogdan Ilin <starkavichi@gmail.com>
2025-04-27 14:07:14 +0300

    feat: implement file decompression functionality in decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import path from "node:path";
+import { createReadStream, createWriteStream } from "node:fs";
+import { createGunzip } from "node:zlib";
+import { pipeline } from "node:stream/promises";
+
+const dirname = import.meta.dirname;
+
 const decompress = async () => {
-    // Write your code here 
+  const pathGzip = path.join(dirname, "files", "archive.gz");
+  const pathFile = path.join(dirname, "files", "fileToCompress.txt");
+
+  const gunzip = createGunzip();
+  const readStream = createReadStream(pathGzip);
+  const writeStream = createWriteStream(pathFile);
+
+  try {
+    await pipeline(readStream, gunzip, writeStream);
+  } catch (error) {
+    console.error("Failed to decompress file:", error);
+  }
 };
 
 await decompress();
+
