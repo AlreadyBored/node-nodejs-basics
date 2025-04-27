78bc0b026d298837991b5296e58db24fc5a1bc73
Jesper Høgsted <81517227+JesperHogstedDk@users.noreply.github.com>
2025-04-27 20:56:50 +0200

    feat: implement transform in transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,21 @@
 const transform = async () => {
-    // Write your code here 
+    // Write your code here   
+      const { Transform } = await import('stream');
+
+      class ReverseStream extends Transform {
+           _transform(chunk, encoding, callback) {
+          const reversed = chunk.toString().split('').reverse().join('');
+          this.push(reversed);
+          callback();
+        }
+      }
+      
+      const reverseStream = new ReverseStream();
+      console.log('Write some character og number in the Terminal!');
+      console.log('Press enter and see input reversed!');
+      console.log('Press CTRL+C to end the stream!');
+      process.stdin.setEncoding('utf8'); 
+      process.stdin.pipe(reverseStream).pipe(process.stdout);
 };
 
 await transform();
