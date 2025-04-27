ba368bd7ee6735b0ee20f0c7e2cc037c18d9bf14
Andrii Shopiak <andriyshopyak@gmail.com>
2025-04-27 12:18:17 +0300

    feat: add reading with Stream API

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import { createReadStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+    const stream = createReadStream(filePath);
+
+    stream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    });
+    stream.on('end', () => {
+        console.log('\nАll data has been read');
+    });
 };
 
 await read();
