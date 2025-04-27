6536a6921404e966ea84834e3783e31075c8e610
Natalia Efimova <n.efimova@systtech.ru>
2025-04-27 19:31:38 +0500

    feat: implement read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import { promises } from 'fs';
+import { join } from 'path';
+
 const read = async () => {
-    // Write your code here 
+    const filePath = join(import.meta.dirname, 'files', 'fileToRead.txt');
+
+    try {
+        await promises.access(filePath);
+        const content = await promises.readFile(filePath, 'utf-8');
+
+        console.log(content);
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
