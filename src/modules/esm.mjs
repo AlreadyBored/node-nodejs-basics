797bfe429a2d51bedab5cae6f4f8ebbbf398b2e1
Olha Popova <fc.odessa.10@gmail.com>
2025-04-26 21:42:19 -0700

    fix: change type assert to type with

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -11,9 +11,9 @@ const random = Math.random();
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = import('./files/a.json', { assert: { type: 'json' } });
+    unknownObject = import('./files/a.json', { with: { type: 'json' }});
 } else {
-    unknownObject = import('./files/b.json', { assert: { type: 'json' } });
+    unknownObject = import('./files/b.json', { with: { type: 'json' }});
 }
 
 console.log(`Release ${release()}`);
