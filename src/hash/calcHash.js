c648d954e2f5ad1e9c7963cfb2ea57a80c8b76fe
Maks Shevchenko <m.shevchenko@inftele.com>
2025-04-28 00:05:54 +0200

    hash done

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import { join } from 'node:path';
+import { createHash } from 'node:crypto';
+import { createReadStream } from 'node:fs';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = join('src', 'hash', 'files', 'fileToCalculateHashFor.txt');
+
+  const hash = createHash('sha256');
+  const fileStream = createReadStream(filePath);
+
+  fileStream.on('data', (chunk) => {
+    hash.update(chunk);
+  });
+
+  fileStream.on('end', () => {
+    const hexHash = hash.digest('hex');
+    console.log(`SHA256 Hash: ${hexHash}`);
+  });
+
+  fileStream.on('error', (err) => {
+    console.error('Error reading file:', err);
+  });
 };
 
 await calculateHash();
