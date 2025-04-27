0898654906b9feb74529c915b2ec3a9209806e9e
Natalia Efimova <n.efimova@systtech.ru>
2025-04-27 18:53:44 +0500

    feat: implement rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,22 @@
+import { promises } from 'fs';
+import { join } from 'path';
+
 const rename = async () => {
-    // Write your code here 
+    const sourcePath = join(import.meta.dirname, 'files', 'wrongFilename.txt');
+    const destPath = join(import.meta.dirname, 'files', 'properFilename.md');
+
+    try {
+        await promises.access(sourcePath);
+
+        const isNewFileExists = await promises.access(destPath).then(() => true).catch(() => false);
+        if (isNewFileExists) {
+            throw new Error('FS operation failed');
+        }
+
+        await promises.rename(sourcePath, destPath);
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await rename();
