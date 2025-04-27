4ccb01a6e805966dcf9371d5412446d55c9d1e0a
Oscarraizer <boltenkovs36@gmail.com>
2025-04-27 17:09:03 +0700

    feat: implement 'read' function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,14 @@
+import { readFile } from "node:fs/promises";
+
 const read = async () => {
-    // Write your code here 
+  const listSource = "./src/fs/files/fileToRead.txt";
+
+  try {
+    const listData = await readFile(listSource);
+    console.log(listData.toString());
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await read();
