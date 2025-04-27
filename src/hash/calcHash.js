85fa22f03a78b0473a96f9b9d66fe72348009cbe
Anastasiya Stashevskaya <anastasiya@vibez.io>
2025-04-27 14:28:15 +0300

    add hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+import crypto from 'crypto';
+import fs from 'fs';
+import path from "path";
+import { fileURLToPath } from "url";
 
+const calculateHash = async () => {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const fileToReadPath = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+    
+    const hash = crypto.createHash('sha256');
+    const stream = fs.createReadStream(fileToReadPath);
+    stream.on('data', (chunk) => {
+        hash.update(chunk);
+    });
+    stream.on('end', () => {
+        console.log(hash.digest('hex'));
+    });
+};
 await calculateHash();
