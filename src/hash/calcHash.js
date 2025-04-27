f91a51cbdd2817e7ace22a8cb5fe988beb54ce20
Olha Popova <fc.odessa.10@gmail.com>
2025-04-26 21:54:51 -0700

    feat: implement file hash calculation using Streams API (SHA256)

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,33 @@
+import { createHash } from 'node:crypto';
+import { createReadStream } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+    try {
+        const hash = createHash('sha256');
+        const stream = createReadStream(filePath);
+
+        stream.on('data', (chunk) => {
+            hash.update(chunk);
+        });
+
+        stream.on('end', () => {
+            const result = hash.digest('hex');
+            console.log(result);
+        });
+
+        stream.on('error', (error) => {
+            console.error('Error reading the file:', error.message);
+        });
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
 };
 
 await calculateHash();
