a5245e1bff6a5a5642971e612612c741716e26c3
davidtavartk <d.tavartkiladze5@gmail.com>
2025-04-28 03:12:03 +0400

    feat: add streams read logic

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,27 @@
+import { createReadStream } from 'node:fs';
+import { join, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
 const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const filePath = join(__dirname, "files", "fileToRead.txt");
+
+    return new Promise((resolve, reject) => {
+        const stream = createReadStream(filePath, { encoding: 'utf8' });
+
+        stream.on('error', (err) => {
+            reject(err);
+        });
+
+        stream.pipe(process.stdout);
+
+        stream.on('end', () => {
+            process.stdout.write('\n');
+            resolve();
+        });
+    });
 };
 
 await read();
