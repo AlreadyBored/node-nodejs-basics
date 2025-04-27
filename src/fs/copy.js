cd0966c64f3a48f985de45405296331b7ba4c03f
Jesper Høgsted <81517227+JesperHogstedDk@users.noreply.github.com>
2025-04-27 10:13:16 +0200

    refactor: move import inside function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,11 +1,10 @@
-import * as fs from 'fs/promises';
-
 const copy = async () => {
     // Write your code here 
+    const { cp } = await import('node:fs/promises');
     const pathFrom = './src/fs/files'
     const pathTo = './src/fs/files_copy'
     try {
-        await fs.cp(pathFrom, pathTo, { recursive: true, force: false, errorOnExist: true})
+        await cp(pathFrom, pathTo, { recursive: true, force: false, errorOnExist: true})
         console.log('All files copied: ', pathFrom, pathTo)
     } catch (error) {
         throw new Error('FS operation failed', { cause: error.message })
