39e1d58b5909672911906598697e4d6f9d77b419
davidtavartk <d.tavartkiladze5@gmail.com>
2025-04-28 01:22:18 +0400

    fix: fix issues in copy,js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -3,14 +3,14 @@ import { join, dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 
 const copy = async () => {
-  const filename = fileURLToPath(import.meta.url);
-  const dirname = dirname(__filename);
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
 
-  const sourceDir = join(dirname, "files");
-  const destDir = join(dirname, "files_copy");
+  const sourceDir = join(__dirname, "files");
+  const destDir = join(__dirname, "files_copy");
 
   try {
-    await cp(sourceDir, destDir, {recursive: true})
+    await cp(sourceDir, destDir, {recursive: true, errorOnExist: true, force: false})
   } catch (err) {
     console.log(err)
     throw new Error("FS operation failed");
