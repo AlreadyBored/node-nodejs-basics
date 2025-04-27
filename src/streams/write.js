a3aa2410dc9a9559428bc23a045fcc8a08a3a775
Natalia Efimova <n.efimova@systtech.ru>
2025-04-28 00:37:50 +0500

    feat: implement wtite to file function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,11 @@
+import { createWriteStream } from 'fs';
+import { join } from 'path';
+
 const write = async () => {
-    // Write your code here 
+    const filePath = join(import.meta.dirname, 'files', 'fileToWrite.txt');
+    const writeStream = createWriteStream(filePath);
+
+    process.stdin.pipe(writeStream);
 };
 
 await write();
