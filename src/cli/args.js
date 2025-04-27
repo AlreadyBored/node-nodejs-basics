882c6a225d3b6d7c8a29daa24a86bd2707efc053
Sasha Kalko <kalko.alexandr.dg@gmail.com>
2025-04-27 14:46:44 +0300

    feat(cli): implement functions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,30 @@
+import { argv } from 'node:process';
+
 const parseArgs = () => {
-    // Write your code here 
+    const arArg = [];
+    const prefixPropName = new RegExp('^--.+')
+
+    for (let i = 2; i < argv.length; i++) {
+        const element = argv[i];
+        if (element.match(prefixPropName)) {
+            const valLastObj = arArg[arArg.length - 1]?.value;
+            if (valLastObj === null) arArg[arArg.length - 1].value = '';
+            arArg.push({
+                name: element,
+                value: null,
+            });
+        }
+        else {
+            const valLastObj = arArg[arArg.length - 1]?.value;
+            if (valLastObj === null) arArg[arArg.length - 1].value = element;
+        }
+    }
+    if (arArg[arArg.length - 1]?.value === null) arArg[arArg.length - 1].value = '';
+    
+    let strOut = '';
+    arArg.forEach(obj => strOut += `${obj.name} is ${obj.value}, `);
+    strOut = strOut.substring(0, strOut.length - 2)
+    console.log(strOut)
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+    const pefixVar = new RegExp('^RSS_.+')
+    let strMatch = '';
+    for (const [key,value] of Object.entries(process.env)) {
+        if(key.match(pefixVar)) strMatch += `${key}=${value}; `  
+    }
+    console.log(strMatch.substring(0,strMatch.length - 2))
 };
 
 parseEnv();
