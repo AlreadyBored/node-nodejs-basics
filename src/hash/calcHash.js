5fd34a48d89f9287e8375dac1e36f1bc5a481d6b
Natalia Efimova <n.efimova@systtech.ru>
2025-04-28 00:24:33 +0500

    feat: implement calc hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import { join } from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = join(import.meta.dirname, 'files', 'fileToCalculateHashFor.txt');
+    const hash = createHash('sha256');
+    const readStream = createReadStream(filePath);
+
+    readStream.on('data', (chunk) => hash.update(chunk));
+    readStream.on('end', () => {
+        console.log(hash.digest('hex'));
+    });
 };
 
 await calculateHash();
