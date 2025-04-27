95a7a39472a77fbd64268ca45b8af258cb568a85
davidtavartk <d.tavartkiladze5@gmail.com>
2025-04-28 01:41:30 +0400

    feat: add delete.js logic

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import { unlink } from "node:fs/promises";
+import { join, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+
 const remove = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const filePath = join(__dirname, "files", "fileToRemove.txt");
+
+    try {
+        await unlink(filePath)
+    } catch (error) {
+        throw new Error("FS operation failed"); 
+    }
+
 };
 
 await remove();
