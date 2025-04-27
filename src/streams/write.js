8c4091d38e4fa2c73d279a3a0e58af0ca69cbcc6
Bogdan Ilin <starkavichi@gmail.com>
2025-04-27 12:27:00 +0300

    feat: implement file writing functionality in write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import path from "node:path";
+import fs from "node:fs";
+
 const write = async () => {
-    // Write your code here 
+  const pathFile = path.join(import.meta.dirname, "files", "fileToWrite.txt");
+  const writeStream = fs.createWriteStream(pathFile, { encoding: "utf-8" });
+
+  process.stdin.on("error", (error) => {
+    console.error("Failed to write file:", error);
+  });
+
+  process.stdin.pipe(writeStream);
 };
 
 await write();
+
