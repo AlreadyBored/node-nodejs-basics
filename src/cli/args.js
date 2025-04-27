e186afe547f28fc4bf346bdaba497d1ad25da65a
Anastasiya Zizeka <anastasiya.zizeka@LeverX.local>
2025-04-27 19:25:37 +0500

    feat: add code for tasks for cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,19 @@
 const parseArgs = () => {
-    // Write your code here 
+    const envArguments = process.argv,
+          resultArray = [];
+    if(envArguments) {
+        envArguments.forEach((argValue, index, envArguments) => {
+            if (argValue.startsWith('--')){
+                const resultString = `${argValue.split('--')[1]} is ${envArguments[index + 1]}`;
+                resultArray.push(resultString);
+            }
+        });
+    
+        console.log(resultArray.join(', '));
+    } else {
+        throw new Error('No environment arguments were found');
+    }
+    
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+    const envProps = Object.entries(process.env);
+    if (envProps) {
+        const resultString = envProps
+            .filter(([propName, _]) => propName.startsWith('RSS_'))
+            .map(([propName, propValue]) => `${propName}=${propValue}`)
+            .join(';');
+        console.log(resultString);
+    } else {
+        throw new Error('No environment props were found');
+    }
 };
 
 parseEnv();
