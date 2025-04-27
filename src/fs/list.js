db265c52c80fae0ad92584fd468718892d459a59
davidtavartk <d.tavartkiladze5@gmail.com>
2025-04-28 01:43:23 +0400

    feat: add list.js logic

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import { readdir } from "node:fs/promises";
+import { join, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
 const list = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const folderPath = join(__dirname, "files");
+
+  try {
+    const files = await readdir(folderPath);
+    console.log(files);
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await list();
