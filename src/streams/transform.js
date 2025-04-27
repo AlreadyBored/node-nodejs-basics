be9a3299d213d2a01096693eb2eff071d697cd4c
davidtavartk <d.tavartkiladze5@gmail.com>
2025-04-28 03:39:47 +0400

    feat: add transform stream

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,24 @@
+import { Transform } from "node:stream";
+
 const transform = async () => {
-    // Write your code here 
+  return new Promise((resolve, reject) => {
+    const reverseStream = new Transform({
+      transform(chunk, _, callback) {
+        const reversedChunk = chunk.toString().replace(/\n$/, '').split("").reverse().join("");
+        callback(null, reversedChunk + "\n".repeat(2));
+      },
+    });
+
+    reverseStream.on("error", (err) => {
+      reject(err);
+    });
+
+    process.stdin.pipe(reverseStream).pipe(process.stdout);
+
+    process.stdin.on("end", () => {
+      resolve();
+    });
+  });
 };
 
 await transform();
