288f76b113382a5590e72187ca35edb9e48f2ed3
davidtavartk <d.tavartkiladze5@gmail.com>
2025-04-28 02:50:36 +0400

    feat: add hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,31 @@
+import { createHash } from "node:crypto";
+import { createReadStream } from "node:fs";
+import { stdout } from 'node:process';
+import { join, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const filePath = join(__dirname, "files", "fileToCalculateHashFor.txt");
+
+  return new Promise((resolve, reject) => {
+    const input = createReadStream(filePath);
+    const hash = createHash("sha256");
+    
+    input.on('error', (err) => {
+      reject(err);
+    });
+    
+    input.pipe(hash).setEncoding("hex").pipe(stdout);
+    
+    input.on('end', () => {
+      stdout.write('\n');
+      resolve();
+    });
+  });
 };
+
 
 await calculateHash();
