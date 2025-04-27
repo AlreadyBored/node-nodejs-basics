88197f440340c0f2b6c494dfcbe7ca08098d8e45
davidtavartk <d.tavartkiladze5@gmail.com>
2025-04-28 01:08:31 +0400

    feat: add create functionality

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import { writeFile } from "node:fs/promises";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
 const create = async () => {
-    // Write your code here 
+  const content = "I am fresh and young";
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const filePath = join(__dirname, "files", "fresh.txt");
+
+  try {
+    await writeFile(filePath, content, {flag: 'wx'});
+
+  } catch (err) {
+
+    throw new Error("FS operation failed");
+  }
 };
 
 await create();
