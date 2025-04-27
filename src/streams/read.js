6268ae6631bf29c0e6d77a9c8a77a58fa38ea6a3
Jesper Høgsted <81517227+JesperHogstedDk@users.noreply.github.com>
2025-04-27 13:32:35 +0200

    feat: implement createReadStream in read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
 const read = async () => {
     // Write your code here 
+    const { createReadStream } = await import('node:fs');
+
+    let txt = '';
+    const readStream = createReadStream('./src/streams/files/fileToRead.txt', { encoding: 'utf-8' })
+    try {
+        for await (const chunk of readStream) {
+            txt += chunk
+        }
+    } catch (error) {
+        console.error(`Error reading file: ${error.message}`);
+    }
+
+    process.stdout.write(txt)
 };
 
 await read();
