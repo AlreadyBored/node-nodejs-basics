f2967388ada810d5b421e4f5e317412fbc7caf9a
davidtavartk <d.tavartkiladze5@gmail.com>
2025-04-28 01:13:13 +0400

    feat: add copy.js functionality

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,28 @@
+import { access } from "node:fs/promises";
+import { join, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
 const copy = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const sourceDir = join(__dirname, "files");
+  const destDir = join(__dirname, "files_copy");
+
+  try {
+    await access(sourceDir);
+
+    try {
+      await access(destDir);
+      throw new Error("FS operation failed");
+    } catch (error) {
+      if (error.code === "ENOENT") {
+        await mkdir(destDir);
+      }
+    }
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await copy();
