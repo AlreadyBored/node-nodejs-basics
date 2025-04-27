75366fedaaf8d756783a53ffb0f927cc4f8fec06
Natalia Efimova <n.efimova@systtech.ru>
2025-04-28 00:44:51 +0500

    feat: implement decompress function

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,16 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { resolve } from 'path';
+import { createGunzip } from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+    const sourcePath = resolve(import.meta.dirname, 'files', 'archive.gz');
+    const destPath = resolve(import.meta.dirname, 'files', 'fileToCompress.txt');
+
+    const readStream = createReadStream(sourcePath);
+    const writeStream = createWriteStream(destPath);
+    const gunzip = createGunzip();
+
+    readStream.pipe(gunzip).pipe(writeStream);
 };
 
 await decompress();
