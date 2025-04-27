7313316c37e3c6225756f469e4ff52df93c51ffb
Natalia Efimova <n.efimova@systtech.ru>
2025-04-27 18:50:48 +0500

    feat: implement copy files function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,37 @@
+import { promises } from 'fs';
+import { join } from 'path';
+
 const copy = async () => {
-    // Write your code here 
+    const sourceDir = join(import.meta.dirname, 'files');
+    const destDir = join(import.meta.dirname, 'files_copy');
+
+    try {
+        const isSourceExist = await promises.access(sourceDir).then(() => true).catch(() => false);
+        if (!isSourceExist) {
+            throw new Error('FS operation failed');
+        }
+
+        const isDestExists = await promises.access(destDir).then(() => true).catch(() => false);
+        if (isDestExists) {
+            throw new Error('FS operation failed');
+        }
+
+        await promises.mkdir(destDir);
+
+        const elements = await promises.readdir(sourceDir, { withFileTypes: true });
+        for (const element of elements) {
+            const sourcePath = join(sourceDir, element.name);
+            const destPath = join(destDir, element.name);
+
+            if (element.isDirectory()) {
+                await promises.mkdir(destPath);
+            } else if (element.isFile()) {
+                await promises.copyFile(sourcePath, destPath);
+            }
+        }
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await copy();
