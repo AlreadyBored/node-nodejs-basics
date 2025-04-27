c027d1f39d36c2df0989cc63e535ee81f5414343
Natalia Efimova <n.efimova@systtech.ru>
2025-04-27 15:09:21 +0500

    feat: implement create file function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,13 @@
+import { promises } from 'fs';
+import { join } from 'path';
+
 const create = async () => {
-    // Write your code here 
+    const filePath = join(import.meta.dirname, 'files', 'fresh.txt');
+    try {
+        await promises.writeFile(filePath, 'I am fresh and young', { flag: 'wx' });
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await create();
