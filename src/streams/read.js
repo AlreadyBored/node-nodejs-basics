5a8fdc0a63b444d71312da4f7d0787338da041c1
Natalia Efimova <n.efimova@systtech.ru>
2025-04-28 00:35:44 +0500

    feat: implement read file function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import { createReadStream } from 'fs';
+import { join } from 'path';
+
 const read = async () => {
-    // Write your code here 
+    const filePath = join(import.meta.dirname, 'files', 'fileToRead.txt');
+    const readStream = createReadStream(filePath);
+
+    readStream.pipe(process.stdout);
+
+    readStream.on('end', () => {
+        process.stdout.write('\n');
+    });
 };
 
 await read();
