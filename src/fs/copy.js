d6adf8663dee6905fefa8c7c4c5e1f9c4ff671e3
Yana <syanaa9@gmail.com>
2025-04-27 22:44:06 +1000

    feat: implement create and copy func

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,25 @@
+import { mkdir, cp } from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const copy = async () => {
-    // Write your code here 
+
+    try {
+        const filePath = fileURLToPath(import.meta.url);
+        const dirName = path.dirname(filePath);
+        const sourceDir = path.join(dirName, 'files');
+        const destDir = path.join(dirName, 'files_copy');
+        await cp( sourceDir, destDir, {force: false, errorOnExist: true, recursive : true});
+
+    } catch (error) {
+        if(error.code === 'ERR_FS_CP_EEXIST'){
+            throw new Error('FS operation failed');
+        } else {
+            throw error;
+        }  
+       console.log(`Error! ${error}`);
+       
+    } 
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,28 @@
+import  { open, appendFile, access } from 'node:fs/promises';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+
 const create = async () => {
-    // Write your code here 
+
+    let fileHandle;
+    try {
+        const fileURL = new URL('./files/fresh.txt', import.meta.url);
+        const filePath = fileURLToPath(fileURL);
+        
+        fileHandle = await open(filePath, 'ax');
+    
+        fileHandle.appendFile('I am fresh and young');
+
+    } catch (error) {
+       if(error.code === 'EEXIST'){
+        throw new Error('FS operation failed');
+       } else {
+        throw error;
+       }   
+    } finally {
+        await fileHandle?.close();
+    } 
 };
 
 await create();
