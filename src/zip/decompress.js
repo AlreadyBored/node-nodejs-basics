11b9bf5c53f63b9e8c35f4c67f45ed8bb711b69e
Roman Gromov <gromov.r.v.92@gmail.com>
2025-04-29 00:01:06 +0500

    feat: implement decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { join } from 'path';
+import { createGunzip } from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+    const archivePath = join('files', 'archive.gz');
+    const fileToCompressPath = join('files', 'fileToCompress.txt');
+
+    const readStream = createReadStream(archivePath);
+    const writeStream = createWriteStream(fileToCompressPath);
+    const gunzip = createGunzip();
+
+    readStream.on('error', () => {
+        throw new Error('FS operation failed');
+    });
+
+    writeStream.on('error', () => {
+        throw new Error('FS operation failed');
+    });
+
+    readStream.pipe(gunzip).pipe(writeStream);
 };
 
 await decompress();
