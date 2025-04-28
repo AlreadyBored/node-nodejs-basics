fe25b6067c5afe80f758a24d8a547dd25ca6bd47
Sviatoslav Pokhvalenko <pokhvalenkosvyat@gmail.com>
2025-04-29 00:50:56 +0200

    zip task finished

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,25 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import { createGzip } from "node:zlib";
+import { join } from "node:path";
+
+const dirname = import.meta.dirname;
+
 const compress = async () => {
-    // Write your code here 
+  const filePath = join(dirname, "files", "fileToCompress.txt");
+  const archPath = join(dirname, "files", "archive.gz");
+
+  const readable = createReadStream(filePath);
+  const writable = createWriteStream(archPath);
+  const gzip = createGzip();
+
+  readable.pipe(gzip).pipe(writable);
+
+  readable.on("error", () => {
+    throw new Error("FS operation failed");
+  });
+  writable.on("error", () => {
+    throw new Error("FS operation failed");
+  });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import { createGunzip } from "node:zlib";
+import { join } from "node:path";
+
+const dirname = import.meta.dirname;
+
 const decompress = async () => {
-    // Write your code here 
+  const filePath = join(dirname, "files", "fileToCompress.txt");
+  const archPath = join(dirname, "files", "archive.gz");
+
+  const readable = createReadStream(archPath);
+  const writable = createWriteStream(filePath);
+  const gunzip = createGunzip();
+
+  readable.pipe(gunzip).pipe(writable);
+
+  readable.on("error", () => {
+    throw new Error("FS operation failed");
+  });
+  writable.on("error", () => {
+    throw new Error("FS operation failed");
+  });
 };
 
 await decompress();
