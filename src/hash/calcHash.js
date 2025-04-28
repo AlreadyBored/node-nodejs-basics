a43b1417c6492fd07d9ada6c157a193129b8c030
Ewelai <palevo.ewelai@gmail.com>
2025-04-28 14:58:16 +0300

    feat: hash tasks

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import { createReadStream } from "node:fs";
+import { createHash } from "node:crypto";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const fileToRead = join(__dirname, "files", "fileToCalculateHashFor.txt");
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash("sha256");
+  const stream = createReadStream(fileToRead);
+
+  return await new Promise((resolve, reject) => {
+    stream.on("error", reject);
+    hash.on("error", reject);
+
+    stream.on("data", (chunk) => hash.update(chunk));
+    stream.on("end", () => {
+      console.log(hash.digest("hex"));
+      resolve();
+    });
+  });
 };
 
 await calculateHash();
