3c2027181c13bcac93fee7ee9f27f5369a8e812b
davidtavartk <d.tavartkiladze5@gmail.com>
2025-04-28 16:06:03 +0400

    feat: add compress/decompress logic

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,33 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+import {dirname, join} from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const compress = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const sourceFile = join(__dirname, "files", 'fileToCompress.txt');
+    const destinationFile = join(__dirname, 'archive.gz');
+
+    try {
+        
+    const readStream = createReadStream(sourceFile);
+    const writeStream = createWriteStream(destinationFile);
+    const gzipStream = createGzip();
+
+    await pipeline(
+        readStream,
+        gzipStream,
+        writeStream
+      );
+
+      console.log('File compressed successfully');
+    } catch (error) {
+        console.error('Compression failed:', error);
+    }
+
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,30 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGunzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
 const decompress = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const sourceFile = join(__dirname, 'archive.gz');
+    const destinationFile = join(__dirname, "files", 'fileToCompress.txt');
+
+    try {
+        const readStream = createReadStream(sourceFile);
+        const writeStream = createWriteStream(destinationFile);
+        const gunzipStream = createGunzip();
+
+        await pipeline(
+            readStream,
+            gunzipStream,
+            writeStream
+        );
+
+        console.log('File decompressed successfully');
+    } catch (error) {
+        console.error('Decompression failed:', error);
+    }
 };
 
 await decompress();
