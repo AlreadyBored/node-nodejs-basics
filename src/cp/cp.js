2a5693ed9d58c65ee6cf0c4eb49d406853b76651
Eduard Simonenko <edik.simonenko2019@gmail.com>
2025-04-28 22:17:44 +0300

    feat: add cp task solution

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,21 @@
+import { spawn } from 'child_process';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const child = spawn('node', [join(__dirname, 'files', 'script.js'), ...args], {
+    stdio: ['pipe', 'pipe', process.stderr]
+  });
+
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
+
+  child.on('exit', (code) => {
+    console.log(`Child process exited with code ${code}`);
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['arg1', 'arg2', 'arg3']);
