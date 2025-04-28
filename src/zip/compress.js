072239751676073e86eabb34fb7a81eafc856445
Maks Shevchenko <m.shevchenko@inftele.com>
2025-04-28 08:25:41 +0200

    zip done

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGzip } from 'node:zlib';
+import { join } from 'node:path';
 const compress = async () => {
-    // Write your code here 
+  const inputPath = join('src', 'zip/files', 'fileToCompress.txt');
+  const outputPath = join('src', 'zip/files', 'archive.gz');
+
+  const gzip = createGzip();
+
+  const inputStream = createReadStream(inputPath);
+  const outputStream = createWriteStream(outputPath);
+
+  inputStream.pipe(gzip).pipe(outputStream);
+
+  outputStream.on('finish', () => {
+    console.log('File compressed');
+  });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGunzip } from 'node:zlib';
+import { join } from 'node:path';
+
 const decompress = async () => {
-    // Write your code here 
+  const inputPath = join('src', 'zip/files', 'archive.gz');
+  const outputPath = join('src', 'zip/files', 'fileToCompress.txt');
+
+  const gzip = createGunzip();
+
+  const inputStream = createReadStream(inputPath);
+  const outputStream = createWriteStream(outputPath);
+
+  inputStream.pipe(gzip).pipe(outputStream);
+
+  outputStream.on('finish', () => {
+    console.log('File decompressed');
+  });
 };
 
 await decompress();
