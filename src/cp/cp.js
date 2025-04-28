fd1dd110b2c8b7db8f12d19becb628bebf8de2f3
Sasha Kalko <kalko.alexandr.dg@gmail.com>
2025-04-28 22:56:44 +0300

    fea(cp): implement function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,9 @@
+import { fork } from 'node:child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = fork('src/cp/files/script.js', args);
+
+    child.on('message', data => console.log('Received from child', data))
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2',]);
--- src/cp/files/script.js
+++ src/cp/files/script.js
@@ -10,3 +10,4 @@ const echoInput = (chunk) => {
 };
 
 process.stdin.on('data', echoInput);
+process.send('Test string');
