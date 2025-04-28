343b1893c80a339f8645c2982cbadc40c084f73c
Roman Gromov <gromov.r.v.92@gmail.com>
2025-04-28 20:11:08 +0500

    feat: implement create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import { promises as fs } from 'fs';
+import { join } from 'path';
+
 const create = async () => {
-    // Write your code here 
+    const filePath = join('files', 'fresh.txt');
+
+    try {
+        await fs.access(filePath);
+        throw new Error('FS operation failed');
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            await fs.writeFile(filePath, 'I am fresh and young');
+        } else {
+            throw new Error('FS operation failed');
+        }
+    }
 };
 
 await create();
