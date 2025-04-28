8fe88119ee2fef23b69255a084dfc9d8db1b870f
Sasha Kalko <kalko.alexandr.dg@gmail.com>
2025-04-28 16:52:18 +0300

    feat(wt): implement functions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,28 @@
+import { Worker } from 'node:worker_threads';
+import { cpus } from 'node:os';
+
+const createThread = async (argThread) => {
+    return new Promise((res, rej) => {
+        const worker = new Worker('./src/wt/worker.js', { workerData: argThread });
+        worker.on('message', res);
+        worker.on('error', rej);
+    })
+}
 const performCalculations = async () => {
-    // Write your code here
+    const threads = []
+    for (let i = 10; i < 10 + cpus().length; i++) {
+        threads.push(createThread(i));
+    }
+
+    const resConsole = [];
+    const results = await Promise.allSettled(threads);
+    for (let elem of results) {
+        resConsole.push({
+            status: elem.status === 'fulfilled' ? 'resolved' : 'error',
+            data: elem?.value ? elem.value : null,
+        });
+    }
+    console.log(resConsole);
 };
 
 await performCalculations();
