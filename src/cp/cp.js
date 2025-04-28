613449d2a627f86c7f99e9ff4470d72a81e0a9e8
Yaroslav <y.silkin@netrika.ru>
2025-04-28 10:19:55 +0300

    feat: solved cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,26 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { dirname, join } from 'node:path';
+import { spawn } from 'node:child_process';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export const spawnChildProcess = async (args) => {
+    const scriptPath = join(__dirname, 'files/script.js');
+    const child = spawn('node', [scriptPath, ...args], {
+        stdio: ['pipe', 'pipe', 'inherit', 'ipc']
+    });
+
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
+
+    return new Promise((resolve, reject) => {
+        child.on('exit', (code) => {
+            if (code === 0) resolve();
+            else reject(new Error(`Child exited with code ${code}`));
+        });
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+// spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['--input', 'test', '--mode', 'development']);
