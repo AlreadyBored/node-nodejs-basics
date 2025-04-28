e313c79ff9aed90efbaaf766f1b2c8d923a866fb
notTural <rahman.trl.mammadov@gmail.com>
2025-04-29 00:45:13 +0200

    feat: implemented fs functions

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,16 @@
+import { unlink, access } from 'fs/promises';
+import { constants } from 'fs';
+
 const remove = async () => {
-    // Write your code here 
+    const path = 'files/fileToRemove.txt';
+
+    try {
+        await access(path, constants.F_OK);
+
+        await unlink(path);
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await remove();
--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import { readdir, access } from 'fs/promises';
+import { constants } from 'fs';
+
 const list = async () => {
-    // Write your code here 
+    const path = 'files';
+
+    try {
+        await access(path, constants.F_OK);
+
+        const files = await readdir(path);
+
+        console.log(files);
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await list();
