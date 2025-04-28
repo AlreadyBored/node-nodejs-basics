d4bbb9fe5aa4faf1c1da4fea5ff5c98563a5f26b
Andrii Shopiak <andriyshopyak@gmail.com>
2025-04-28 09:50:50 +0300

    feat: add solution writeStream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,31 @@
+import { createWriteStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+
+    const writeStream = createWriteStream(filePath, { flags: 'a' });
+
+    console.log('Enter text to write to the file (press "Ctrl+C" to finish):');
+    process.stdin.on('data', (chunk) => {
+        writeStream.write(chunk);
+    });
+
+    process.on('SIGINT', () => {
+
+        writeStream.end(() => {
+            console.log('\n--- Finish ---');
+            process.exit(0);
+        });
+});
+
+    writeStream.on('error', (error) => {
+        console.error('Error:', error.message);
+    });
 };
 
 await write();
