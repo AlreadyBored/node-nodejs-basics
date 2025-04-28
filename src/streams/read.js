896e62705fd3ce397d1f1172f3e6b45491ff1e1f
Gennady Kashin <gennady.kashin94@gmail.com>
2025-04-28 20:10:28 +0300

    feat: streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,23 @@
+import path from 'path';
+import fs from 'fs'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const FILENAME_TO_READ = 'fileToRead.txt';
+
 const read = async () => {
-    // Write your code here 
+    const stream = fs.createReadStream(
+        path.join(__dirname, 'files', FILENAME_TO_READ)
+      );
+    
+      stream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+      });
+    
+      stream.on('end', () => {
+        console.log('\nFinished!');
+      });
 };
 
 await read();
