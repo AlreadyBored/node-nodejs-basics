3a6026d1e4629567184e8e6692813424366fd6a5
Fedor Kaputsky <fedor.kaputsky@sap.com>
2025-04-28 19:13:46 +0300

    feat: add solution to read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,26 @@
+import { access, constants, readFile } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const ERROR_CODE_NOT_EXIST = "ENOENT";
+
 const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const folderPath = path.join(__dirname, "files");
+    const filePath = path.join(folderPath, "fileToRead.txt");
+
+    access(filePath, constants.F_OK, (error) => {
+        if (error && error.code === ERROR_CODE_NOT_EXIST) {
+            throw new Error("FS operation failed");
+        } else {
+            readFile(filePath, "utf8", (err, data) => {
+                if (err) throw err;
+                console.log(data);
+            });
+        }
+    });
 };
 
 await read();
