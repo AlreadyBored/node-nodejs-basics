1f0312c59163428c234269fb2878b41166c08a49
Ewelai <palevo.ewelai@gmail.com>
2025-04-28 14:58:39 +0300

    feat: wt tasks

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,43 @@
+import { Worker } from "worker_threads";
+import os from "os";
+
 const performCalculations = async () => {
-    // Write your code here
+  const cpuCores = os.cpus().length;
+  const workerPromises = [];
+
+  for (let i = 0; i < cpuCores; i++) {
+    const workerData = 10 + i;
+    const worker = new Worker(new URL("./worker.js", import.meta.url), { workerData });
+
+    const workerPromise = new Promise((resolve) => {
+      worker.on("message", (result) =>
+        resolve({
+          status: "resolved",
+          data: result,
+        })
+      );
+
+      worker.on("error", () =>
+        resolve({
+          status: "error",
+          data: null,
+        })
+      );
+
+      worker.on("exit", (code) => {
+        if (code !== 0)
+          resolve({
+            status: "error",
+            data: null,
+          });
+      });
+    });
+
+    workerPromises.push(workerPromise);
+  }
+
+  const results = await Promise.all(workerPromises);
+  console.log(results);
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,16 @@
+import { workerData, parentPort } from "worker_threads";
+
 // n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+const nthFibonacci = (n) => (n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2));
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  try {
+    const result = nthFibonacci(workerData);
+    parentPort.postMessage(result);
+  } catch (error) {
+    parentPort.postMessage(null);
+    throw error;
+  }
 };
 
 sendResult();
