8a46201241d7a1d2277db23eb3c994c7e14d1cf9
Gennady Kashin <gennady.kashin94@gmail.com>
2025-04-28 20:01:30 +0300

    feat: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import path from 'path';
+import fs from 'fs'
+import { fileURLToPath } from 'url';
+import crypto from 'crypto';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const FILENAME = 'fileToCalculateHashFor.txt';
+
 const calculateHash = async () => {
-    // Write your code here 
+    try {
+        const filePath = path.join(__dirname, 'files', FILENAME);
+        const readStream = fs.createReadStream(filePath);
+        const hash = crypto.createHash('sha256');
+    
+        readStream.pipe(hash).on('finish', () => {
+            console.log(hash.digest('hex'));
+        });
+       } catch (e) {
+        console.log(e);
+       }
 };
 
 await calculateHash();
