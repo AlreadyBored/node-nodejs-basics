bd843219767f450d30b63d4e3e2e60a43eb8bf1c
Sasha Kalko <kalko.alexandr.dg@gmail.com>
2025-04-28 13:27:59 +0300

    feat(zip): implement functions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,13 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+import { createGzip } from 'node:zlib';
+
 const compress = async () => {
-    // Write your code here 
+    const source = createReadStream('src/zip/files/fileToCompress.txt');
+    const output = createWriteStream('src/zip/files/archive.gz');
+    const gzip = createGzip();
+
+    await pipeline(source, gzip, output);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,13 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+import { createUnzip } from 'node:zlib';
+
 const decompress = async () => {
-    // Write your code here 
+    const source = createReadStream('src/zip/files/archive.gz');
+    const output = createWriteStream('src/zip/files/fileToCompress.txt');
+    const gzip = createUnzip();
+
+    await pipeline(source, gzip, output);
 };
 
 await decompress();
