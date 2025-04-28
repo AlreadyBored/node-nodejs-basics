faba4c240f563e49c9ed51cdd4f99d91a8cad585
NataliiaVirt <virtnatalie@gmail.com>
2025-04-28 16:42:13 +0300

    Update_rename.js
    Add a rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,27 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+
 const rename = async () => {
-    // Write your code here 
+  const filePath = path.join('files', 'fresh.txt');
+  const newFilePath = path.join('files', 'newFresh.md');
+
+  try {
+    await fs.access(filePath);
+
+    try {
+      await fs.access(newFilePath);
+      throw new Error('FS operation failed');
+    } catch (err) {
+      if (err.code !== 'ENOENT') {
+        throw new Error('FS operation failed');
+      }
+    }
+
+    await fs.rename(filePath, newFilePath);
+
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
-await rename();
+rename();
