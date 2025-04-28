3fdc273ef5c993320d3ca8790bed8686a0023822
Andrii Shopiak <andriyshopyak@gmail.com>
2025-04-28 10:08:33 +0300

    feat: add transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,20 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+    const reverseTransform = new Transform({
+        transform(chunk, _encoding, callback) {
+          const input = chunk.toString();
+          const reversed = input.split('').reverse().join('');
+          this.push(reversed);
+          callback();
+        }
+      });
+      
+    console.log('Enter text to reverse (Ctrl+C to stop):');
+
+    process.stdin
+    .pipe(reverseTransform)
+    .pipe(process.stdout);
 };
 
 await transform();
