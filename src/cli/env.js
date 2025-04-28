e3fb90805d8b08eccf0023ad0cf95b23ce5f3481
N1kolozz <mr.osievi5@gmail.com>
2025-04-28 18:54:28 +0400

    feat: implement cli solution

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
     // Write your code here 
+    const envVars = process.env;
+ 
+    const rssVars = Object.entries(envVars)
+        .filter(([key, _]) => key.startsWith('RSS_'))
+        .map(([key, value]) => `${key}=${value}`)
+        .join('; ');
+
+    console.log(rssVars);
 };
 
 parseEnv();
