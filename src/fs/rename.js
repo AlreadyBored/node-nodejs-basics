6d4e830d4c15546056f1c67f7e15b8cb60175e34
Roman Gromov <gromov.r.v.92@gmail.com>
2025-04-28 20:16:32 +0500

    feat: implement rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,27 @@
+import { promises as fs } from 'fs';
+import { join } from 'path';
+
 const rename = async () => {
-    // Write your code here 
+    const filesPath = 'files';
+    const wrongFilenamePath = join(filesPath, 'wrongFilename.txt');
+    const properFilenamePath = join(filesPath, 'properFilename.md');
+
+    try {
+        await fs.access(wrongFilenamePath);
+
+        try {
+            await fs.access(properFilenamePath);
+            throw new Error('FS operation failed');
+        } catch (err) {
+            if (err.code !== 'ENOENT') {
+                throw new Error('FS operation failed');
+            }
+        }
+
+        await fs.rename(wrongFilenamePath, properFilenamePath);
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await rename();
