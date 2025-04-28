3361035060ae71ba63b6aa1388faa9a63f7949b0
AndreevSV <andreevsv@gmail.com>
2025-04-28 14:30:10 +0300

    feat: implement compression and decompression use zlib

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+import { createGzip } from 'node:zlib';
+
+// compress.js - implement function that compresses file
+//  fileToCompress.txt to archive.gz using zlib and Streams API
+
 const compress = async () => {
-    // Write your code here 
+    // Write your code here
+    const pathToFolder = './files';
+    const sourceFilename = 'fileToCompress.txt';
+    const targetFilename = 'archive.gz';
+
+    const pathToSourceFile = `${pathToFolder}/${sourceFilename}`;
+    const pathToTargetFile = `${pathToFolder}/${targetFilename}`;
+
+    const gzip = createGzip();
+    const readableStream = createReadStream(pathToSourceFile);
+    const writableStream = createWriteStream(pathToTargetFile);
+
+    await pipeline(readableStream, gzip, writableStream);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGunzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+
+// decompress.js - implement function that decompresses archive.gz
+// back to the fileToCompress.txt with same content as before compression
+// using zlib and Streams API
+
 const decompress = async () => {
-    // Write your code here 
+    // Write your code here
+    const pathToFolder = './files';
+    const sourceFilename = 'archive.gz';
+    const targetFilename = 'fileToCompress.txt';
+
+    const pathToSourceFile = `${pathToFolder}/${sourceFilename}`;
+    const pathToTargetFile = `${pathToFolder}/${targetFilename}`;
+
+    const gunzip = createGunzip();
+    const readableStream = createReadStream(pathToSourceFile);
+    const writableStream = createWriteStream(pathToTargetFile);
+
+    await pipeline(readableStream, gunzip, writableStream);
 };
 
 await decompress();
