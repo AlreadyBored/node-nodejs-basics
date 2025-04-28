79463e9285ae7375e955ce627bdb12efb6294dd0
Yaroslav <y.silkin@netrika.ru>
2025-04-28 09:41:45 +0300

    feat: solved worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,37 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { Worker } from 'node:worker_threads';
+import { cpus } from 'node:os';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const performCalculations = async () => {
-    // Write your code here
+    const countCpus = cpus().length;
+
+    const workerPromises = Array.from({ length: countCpus }, (_, i) => {
+        return new Promise((resolve) => {
+            const worker = new Worker(join(__dirname, 'worker.js'), {
+                workerData: 10 + i
+            });
+
+            worker.on('message', (message) => {
+                resolve(message);
+            });
+
+            worker.on('error', () => {
+                resolve({ status: 'error', data: null });
+            });
+
+            worker.on('exit', (code) => {
+                if (code !== 0) {
+                    resolve({ status: 'error', data: null });
+                }
+            });
+        });
+    });
+
+    const allWorkersResults = await Promise.all(workerPromises);
+    console.log(allWorkersResults);
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,22 @@
-// n should be received from main thread
+import { parentPort, workerData } from 'node:worker_threads';
+
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    try {
+        if (typeof workerData !== 'number') throw new Error('Invalid input');
+
+        const result = nthFibonacci(workerData);
+        parentPort.postMessage({
+            status: 'resolved',
+            data: result
+        });
+    } catch (error) {
+        parentPort.postMessage({
+            status: 'error',
+            data: null
+        });
+    }
 };
 
 sendResult();
