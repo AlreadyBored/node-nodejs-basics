5bcf2d0daf9c81f854ebb80943337e3c722547ca
Anastasiya Zizeka <anastasiya.zizeka@LeverX.local>
2025-04-28 12:03:22 +0500

    feat: add changes for zlib task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,29 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { fileURLToPath } from 'node:url';
+import { dirname, resolve } from 'node:path';
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream/promises';
+
 const compress = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url),
+          __dirname = dirname(__filename),
+          pathToSourceFile = './files/fileToCompress.txt',
+          pathToTargetFile = './files/archive.gz',
+          fullPathToSourceFile = resolve(__dirname, pathToSourceFile),
+          fullPathToTargetFile = resolve(__dirname, pathToTargetFile);
+
+    const readableStream = createReadStream(fullPathToSourceFile);
+    const writableStream = createWriteStream(fullPathToTargetFile);
+    const gzip = createGzip();
+
+    try {
+        // await pipeline(readableStream, gzip, writableStream);
+        await pipeline(createReadStream(fullPathToSourceFile), createGzip(), createWriteStream(fullPathToTargetFile));
+        console.log('File was compressed to archive.gz successfully');
+    } catch (compressionError) {
+        throw new Error ('File compression was failed');
+    }
+
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,23 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { fileURLToPath } from 'node:url';
+import { dirname, resolve } from 'node:path';
+import { createGunzip } from 'zlib';
+import { pipeline } from 'stream/promises';
+
 const decompress = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url),
+          __dirname = dirname(__filename),
+          pathToSourceFile = './files/archive.gz',
+          pathToTargetFile = './files/fileToCompress.txt',
+          fullPathToSourceFile = resolve(__dirname, pathToSourceFile),
+          fullPathToTargetFile = resolve(__dirname, pathToTargetFile);
+    try {
+        await pipeline(createReadStream(fullPathToSourceFile), createGunzip(), createWriteStream(fullPathToTargetFile));
+        console.log('File was decompressed from archive.gz successfully');
+    } catch (compressionError) {
+        throw new Error ('File decompression was failed');
+    }
+
 };
 
 await decompress();
