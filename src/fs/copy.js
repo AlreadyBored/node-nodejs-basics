4a1242aac1a607452a204f5384b587c20c7daf0d
Roman Gromov <gromov.r.v.92@gmail.com>
2025-04-28 20:11:18 +0500

    feat: implement copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,26 @@
+import { promises as fs } from 'fs';
+import { join } from 'path';
+
 const copy = async () => {
-    // Write your code here 
+    const filesPath = join('files');
+    const filesCopyPath = join('files_copy');
+
+    try {
+        await fs.access(filesPath);
+
+        try {
+            await fs.access(filesCopyPath);
+            throw new Error('FS operation failed');
+        } catch (err) {
+            if (err.code !== 'ENOENT') {
+                throw new Error('FS operation failed');
+            }
+        }
+
+        await fs.cp(filesPath, filesCopyPath, { recursive: true });
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await copy();
