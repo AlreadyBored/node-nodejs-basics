387760723f48d12cf8b3a9e3ba28ec7ab5515bf7
Roman Gromov <gromov.r.v.92@gmail.com>
2025-04-29 00:20:08 +0500

    feat: implement worker

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,17 @@
+import { parentPort } from 'worker_threads';
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    if (!parentPort) {
+        throw new Error('This file must be run as a Worker thread');
+    }
+
+    parentPort.on('message', (n) => {
+        const result = nthFibonacci(n);
+        parentPort.postMessage(result);
+    });
 };
 
 sendResult();
