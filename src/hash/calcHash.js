5be92e43bf4f481495e71ba6c25c97a17c94ea7d
Sviatoslav Pokhvalenko <pokhvalenkosvyat@gmail.com>
2025-04-29 00:05:35 +0200

    hash task finished

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import { createHash } from "node:crypto";
+import { createReadStream } from "node:fs";
+import { join } from "node:path";
+
+const dirname = import.meta.dirname;
+const filePath = join(dirname, "files", "fileToCalculateHashFor.txt");
+const hash = createHash("sha256");
+const stream = createReadStream(filePath);
+
 const calculateHash = async () => {
-    // Write your code here 
+  stream.on("data", (chunk) => hash.update(chunk));
+  stream.on("end", () => {
+    console.log(hash.digest("hex"));
+  });
+  stream.on("error", () => {
+    throw new Error("FS operation failed");
+  });
 };
 
 await calculateHash();
