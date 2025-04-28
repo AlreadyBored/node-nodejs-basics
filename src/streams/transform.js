b92b94b4c479bff8febe5cdb912d7b1cf9c8368f
NataliiaVirt <virtnatalie@gmail.com>
2025-04-28 22:32:58 +0300

    Update_transform.js
    Add a transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,35 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+  let leftover = '';
+
+  const reverseLineTransform = new Transform({
+    transform(chunk, encoding, callback) {
+      const input = leftover + chunk.toString();
+      const lines = input.split('\n');
+
+      leftover = lines.pop();
+
+      const reversedLines = lines.map(line => line.split('').reverse().join('')).join('\n') + '\n';
+      callback(null, reversedLines);
+    },
+    flush(callback) {
+      if (leftover) {
+        const reversed = leftover.split('').reverse().join('');
+        callback(null, reversed + '\n');
+      } else {
+        callback();
+      }
+    }
+  });
+
+  try {
+    process.stdin
+      .pipe(reverseLineTransform)
+      .pipe(process.stdout);
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
-await transform();
+await transform('Hello');
