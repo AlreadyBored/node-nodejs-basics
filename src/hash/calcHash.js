879ed2804e182a8d3f2f3ac6c306da83bbf07a4d
NataliiaVirt <virtnatalie@gmail.com>
2025-04-28 21:53:42 +0300

    Update_calcHash.js
    Add calculateHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,30 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import path from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = path.join('files', 'fileToCalculateHashFor.txt');
+
+  try {
+    const hash = createHash('sha256');
+    const stream = createReadStream(filePath);
+
+    stream.on('error', (err) => {
+      throw new Error('FS operation failed');
+    });
+
+    stream.on('data', (chunk) => {
+      hash.update(chunk);
+    });
+
+    stream.on('end', () => {
+      const result = hash.digest('hex');
+      console.log(result);
+    });
+
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await calculateHash();
