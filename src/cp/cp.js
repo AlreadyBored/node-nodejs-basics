954218792b3faaa6a2498d8ce62ce48743e879a7
Anastasiya Zizeka <anastasiya.zizeka@LeverX.local>
2025-04-28 13:02:50 +0500

    feat: add changes for child process task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,25 @@
+import { spawn } from 'child_process';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const __filename = fileURLToPath(import.meta.url),
+          __dirname = dirname(__filename),
+          pathToScript = './files/script.js';
+
+    const childProcess = spawn('node', [join(__dirname, pathToScript), ...args], {
+        stdio: ['pipe', 'pipe', 'inherit', 'ipc']
+    });
+
+    childProcess.stdout.pipe(process.stdout);
+    process.stdin.pipe(childProcess.stdin);
+
+    childProcess.on('error', (childprocessError) => {
+        throw childprocessError;
+    });
+
+    return childProcess;
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['balerina capuchina', 'some other meme from tik-tok']);
