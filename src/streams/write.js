1edb0b39fb62cbc03bb603a73bcf833818a6bb0d
Roman Gromov <gromov.r.v.92@gmail.com>
2025-04-28 23:52:28 +0500

    feat: implement write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import { createWriteStream } from 'fs';
+import { join } from 'path';
+
 const write = async () => {
-    // Write your code here 
+    const filesPath = join('files', 'fileToWrite.txt');
+    const stream = createWriteStream(filesPath);
+
+    stream.on('error', () => {
+        throw new Error('FS operation failed');
+    });
+
+    process.stdin.pipe(stream);
 };
 
 await write();
