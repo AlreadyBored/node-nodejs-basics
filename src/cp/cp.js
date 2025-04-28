80fbab9ec110c5b034b12aead7602f85a98f3d04
davidtavartk <d.tavartkiladze5@gmail.com>
2025-04-28 16:27:04 +0400

    feat: add cp logic

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,23 @@
+import { spawn } from 'node:child_process';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    
+    const scriptPath = join(__dirname, "files", 'script.js');
+    
+    return new Promise((resolve) => {
+        const childProcess = spawn('node', [scriptPath, ...args], {
+            stdio: ['inherit', 'inherit', 'inherit']
+        });
+        
+        childProcess.on('close', (code) => {
+            console.log(`Child process exited with code ${code}`);
+            resolve();
+        });
+    });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['arg1', 'arg2']);
