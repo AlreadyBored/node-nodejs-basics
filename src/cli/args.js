05d372759422c7f7918b26582ff9aeff9e13d0ce
Roman Gromov <gromov.r.v.92@gmail.com>
2025-04-28 22:17:33 +0500

    feat: implement args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    const result = args
+        .map((arg) => arg
+            .replace(/^--/, '')
+            .replace('=', ' is '))
+        .join(', ');
+
+    console.log(result.join(', '));
 };
 
 parseArgs();
