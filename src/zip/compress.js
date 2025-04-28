965ce228a78e403d87893b7700f3a0ab0ba33190
Jesper Høgsted <81517227+JesperHogstedDk@users.noreply.github.com>
2025-04-28 09:17:51 +0200

    feat: implement compress function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
 const compress = async () => {
     // Write your code here 
+    const { createReadStream, createWriteStream, } = await import('node:fs');
+    const { createGzip } = await import('node:zlib'); 
+    const { pipeline } = await import('node:stream');
+
+    const gzip = createGzip();
+    const source = createReadStream('src/zip/files/fileToCompress.txt');
+    const destination = createWriteStream('src/zip/files/archive.gz');
+
+    pipeline(source, gzip, destination, (err) => {
+        if (err) {
+            throw new Error('Zip operation failed', { cause: err.message })
+        } else {
+            console.log('File compressed!')
+        }
+    });
 };
 
 await compress();
