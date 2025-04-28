0c821ba2fb58386c3469c3110df545787f36784a
Sasha Kalko <kalko.alexandr.dg@gmail.com>
2025-04-28 10:47:55 +0300

    feat(streams): implement read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,12 @@
+import { createReadStream } from 'node:fs';
+import { stdout } from 'node:process';
+
 const read = async () => {
-    // Write your code here 
+    const input = createReadStream('src/streams/files/fileToRead.txt');
+    input.on('end', () => {
+        console.log('\n');
+    })
+    input.pipe(stdout);
 };
 
 await read();
