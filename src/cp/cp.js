cac0ee1fbecfc4462fbfa1cdc4da26f5a6806aa8
Maks Shevchenko <m.shevchenko@inftele.com>
2025-04-28 09:17:03 +0200

    cp done

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,25 @@
+import { spawn } from 'node:child_process';
+import { join } from 'node:path';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const scriptPath = join('src', 'cp', 'files', 'script.js');
+
+  const child = spawn('node', [scriptPath, ...args], {
+    stdio: ['pipe', 'pipe', 'inherit'],
+  });
+
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
+
+  await new Promise((resolve, reject) => {
+    child.on('exit', (code) => {
+      if (code === 0) {
+        resolve();
+      } else {
+        reject(new Error(`Child process exited with code ${code}`));
+      }
+    });
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+await spawnChildProcess(['arg1', 'arg2']);
