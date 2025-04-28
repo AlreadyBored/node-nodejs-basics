c39d9662359b434ee92913ae1f06a6fb2873b6c6
iurii-oleinik <iurii.oleinik@alpeso.io>
2025-04-28 17:51:41 +0300

    feat: update child processses

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,17 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+import { spawn } from 'node:child_process';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const spawnChildProcess = async (args) => {
+    const PATH = join(__dirname, 'files/script.js');
+
+    const childProcess = spawn('node', [PATH, ...args]);
+
+    childProcess.stdout.pipe(process.stdout);
+    process.stdin.pipe(childProcess.stdin);
+}
+
+spawnChildProcess(['arg0', 'arg1']);
