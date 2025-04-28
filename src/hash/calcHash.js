8312e761e0f5b21bb0df84104dbb1d97cc8f1b94
N1kolozz <mr.osievi5@gmail.com>
2025-04-28 18:55:58 +0400

    feat: implement hash solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import { join } from 'path';
+
+
 const calculateHash = async () => {
     // Write your code here 
+    const filePath = join('files', 'fileToCalculateHashFor.txt'); 
+
+    const fileStream = createReadStream(filePath);
+
+    const hash = createHash('sha256');
+
+    fileStream.pipe(hash);
+
+    hash.on('finish', () => {
+
+        const result = hash.digest('hex');
+        console.log(`SHA256 Hash: ${result}`);
+    });
+
 };
 
 await calculateHash();
