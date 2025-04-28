c1bcafd454b8c41b0788f1dc3acdc12b6a45534c
Ann Kedich <keda20021002@gmail.com>
2025-04-28 11:51:45 +0300

    feat: implement copy task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,33 @@
+import fs from 'node:fs';
+import path from 'node:path';
 const copy = async () => {
-    // Write your code here 
+    const sourceDir = path.join('fs', 'files');
+    const copyDir = path.join('fs', 'files_copy');
+
+    fs.access(sourceDir, fs.constants.R_OK, (err) => {
+        if (err && err.code === 'ENOENT')
+            throw new Error('FS operation failed');
+    });
+
+    fs.access(copyDir, fs.constants.R_OK, (err) => {
+        if(!err) {
+            throw new Error('FS operation failed');
+        }
+    });
+
+    fs.mkdir(copyDir, { recursive: true }, (err) => {});
+    fs.readdir(sourceDir, (err, files) => {
+        files.forEach(file => {
+            const filePath = path.join(sourceDir, file);
+            fs.readFile(filePath, {encoding: 'utf8'}, (err, data) => {
+                if (err) {
+                    console.error('Error reading:', err);
+                } else {
+                    fs.writeFile(path.join(copyDir, file), data, {flag: 'w+'}, err => {});
+                }
+            })
+        })
+    });
 };
 
 await copy();
