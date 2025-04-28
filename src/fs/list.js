d81304111b54b5a06444e9b79d1cf8c49b3d866c
Fedor Kaputsky <fedor.kaputsky@sap.com>
2025-04-28 19:11:40 +0300

    feat: add solution to list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,26 @@
+import { access, constants, readdir } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const ERROR_CODE_NOT_EXIST = "ENOENT";
+
 const list = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const folderPath = path.join(__dirname, "files");
+
+    access(folderPath, constants.F_OK, (error) => {
+        if (error && error.code === ERROR_CODE_NOT_EXIST) {
+            throw new Error("FS operation failed");
+        } else {
+            readdir(folderPath, (err, files) => {
+                if (err) throw err;
+
+                console.log(files);
+            });
+        }
+    }); 
 };
 
 await list();
