35675cb768dc56a6c1109be1ed48ed3b9c4c545e
Sasha Kalko <kalko.alexandr.dg@gmail.com>
2025-04-28 11:59:16 +0300

    feat(streams): implement transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,19 @@
+import { stdin, stdout } from 'node:process';
+import { Transform } from 'node:stream';
+
 const transform = async () => {
-    // Write your code here 
+    const transformInput = new Transform({
+        transform(chunk, encoding, callback) {
+            callback(null, chunk.toString().split('').reverse().join(''));
+        }
+    })
+    stdin.on('data', data =>
+        transformInput.write(data)
+    );
+    transformInput.on('data', data =>
+        stdout.write(data + '\n')
+    );
+    console.log('Write something, that will be reverse and print. Ctr+C - exist from print mode.');
 };
 
 await transform();
