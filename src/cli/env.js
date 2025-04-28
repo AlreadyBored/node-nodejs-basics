45e2865df5ae6ca039b6abf199294b402d3c3ee9
davidtavartk <d.tavartkiladze5@gmail.com>
2025-04-28 15:01:11 +0400

    feat: add env variables

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+  const rssArr = Object.entries(process.env)
+    .filter(([key]) => key.startsWith("RSS_"))
+    .map(([key, value]) => `${key}=${value}`)
+    .join("; ");
+
+  console.log(rssArr);
 };
 
 parseEnv();
