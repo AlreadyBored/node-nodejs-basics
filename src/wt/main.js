043d848152ac850213419451e604ac1c95ae1bb5
davidtavartk <d.tavartkiladze5@gmail.com>
2025-04-28 21:12:23 +0400

    feat: add worker threads logic

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,57 @@
+import { Worker } from "node:worker_threads";
+import { cpus } from "node:os";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
 const performCalculations = async () => {
-    // Write your code here
+  const numCores = cpus().length;
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const workerPath = join(__dirname, "worker.js");
+
+  const workerPromises = [];
+
+  for (let i = 0; i < numCores; i++) {
+    const valueToSend = 10 + i;
+
+    const workerPromise = new Promise((resolve) => {
+      const worker = new Worker(workerPath);
+
+      worker.on("message", (result) => {
+        if (result && result.error) {
+          resolve({
+            status: "error",
+            data: null,
+          });
+        } else {
+          resolve({
+            status: "resolved",
+            data: result,
+          });
+        }
+
+        worker.terminate();
+      });
+
+      worker.on("error", () => {
+        resolve({
+          status: "error",
+          data: null,
+        });
+
+        worker.terminate();
+      });
+
+      worker.postMessage(valueToSend);
+    });
+
+    workerPromises.push(workerPromise);
+  }
+
+  const results = await Promise.all(workerPromises);
+
+  console.log(results);
 };
 
 await performCalculations();
