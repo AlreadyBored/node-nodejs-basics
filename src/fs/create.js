20c81ea3439202c0aafb2c31422ebab974a327bf
Ann Kedich <keda20021002@gmail.com>
2025-04-28 11:50:18 +0300

    feat: implement create task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,16 @@
+import fs from 'node:fs';
+import path from 'node:path';
 const create = async () => {
-    // Write your code here 
+    // Write your code here
+    const filePath = path.join('fs', 'files', 'fresh.txt');
+    const content = "I am fresh and young";
+
+    fs.mkdir(path.dirname(filePath), { recursive: true }, err => {});
+    fs.writeFile(filePath, content, { flag: 'wx' }, (err) => {
+        if(err) {
+            throw new Error('FS operation failed');
+        }
+    })
 };
 
 await create();
