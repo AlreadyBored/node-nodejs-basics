1ba9892a02e2a981f5cecd68d34d584bf797e879
Ann Kedich <keda20021002@gmail.com>
2025-04-28 19:49:33 +0300

    feat: implement wt task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,46 @@
+import os from 'os';
+import  path from 'path';
+import { Worker } from 'worker_threads';
 const performCalculations = async () => {
-    // Write your code here
+    const filePath = path.join(import.meta.dirname, 'worker.js');
+    const numWorkers = os.cpus().length;
+    const results = [];
+
+    const runWorker = (n) => {
+        return new Promise((resolve, reject) => {
+            const worker = new Worker(filePath);
+            worker.postMessage(n);
+
+            worker.on('message', (result) => {
+                resolve({ status: 'resolved', data: result });
+            });
+
+            worker.on('error', (err) => {
+                reject({ status: 'error', data: null });
+            });
+
+            worker.on('exit', (code) => {
+                if (code !== 0) {
+                    reject({ status: 'error', data: null });
+                }
+            });
+        });
+    }
+
+
+    for (let i = 0; i < numWorkers; i++) {
+        const n = 10 + i;
+        try {
+            const result = await runWorker(n);
+            results.push(result);
+        } catch (err) {
+            results.push({ status: 'error', data: null });
+        }
+
+    }
+
+    console.log(results);
+    process.exit();
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,17 @@
-// n should be received from main thread
+import { parentPort } from 'worker_threads';
+
+// Функция для вычисления n-го числа Фибоначчи
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    parentPort.on('message', (n) => {
+        try {
+            const result = nthFibonacci(n);
+            parentPort.postMessage(result);
+        } catch (err) {
+            parentPort.postMessage({ status: 'error', data: null });
+        }
+    });
 };
 
 sendResult();
