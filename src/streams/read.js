6879d44848b76ad5a448812a20d75fcc5b70e0a7
NataliiaVirt <virtnatalie@gmail.com>
2025-04-28 22:01:21 +0300

    Update_read.js in streams folder
    Add read function to read.js in the streams folder

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,21 @@
+import { createReadStream } from 'fs';
+import path from 'path';
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.join('files', 'fileToRead.txt');
+
+  try {
+    const stream = createReadStream(filePath, { encoding: 'utf8' });
+
+    stream.on('error', (err) => {
+      throw new Error('FS operation failed');
+    });
+
+    stream.pipe(process.stdout);
+
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await read();
