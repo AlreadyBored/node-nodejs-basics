393de1972b678221084994d8a8ce9f7e886e50f6
Ewelai <palevo.ewelai@gmail.com>
2025-04-28 14:57:43 +0300

    feat: cp tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const result = [];
+
+  for (let i = 0; i < args.length; i += 2) {
+    const prop = args[i].replace(/^--/, "");
+    const value = args[i + 1];
+    result.push(`${prop} is ${value}`);
+  }
+
+  console.log(result.join(", "));
 };
 
 parseArgs();
