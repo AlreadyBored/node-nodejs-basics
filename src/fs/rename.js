53cc2c317afbb5f027aec57d1c183890f63bb44a
Ann Kedich <keda20021002@gmail.com>
2025-04-28 12:16:37 +0300

    feat: implement rename task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,22 @@
+import fs from 'node:fs';
+import path from 'path';
+
 const rename = async () => {
-    // Write your code here 
+    const oldPath = path.join('fs', 'files', 'wrongFilename.txt');
+    const newPath = path.join('fs', 'files', 'properFilename.md');
+
+    fs.access(newPath, fs.constants.R_OK, (err) => {
+        if (!err) {
+            throw new Error('FS operation failed');
+        } else if (err && err.code === 'ENOENT') {
+            fs.rename(oldPath, newPath, (err) => {
+                if (err) {
+                    throw new Error('FS operation failed');
+                }
+            })
+        }
+    })
+
 };
 
 await rename();
