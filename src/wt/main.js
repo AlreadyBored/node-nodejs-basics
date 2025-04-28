ed1a101ebc4b29f60cc0f0780e963baeabe3fd34
Anastasiya Zizeka <anastasiya.zizeka@LeverX.local>
2025-04-28 12:38:56 +0500

    feat: add changes for threads task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,38 @@
+import { cpus } from 'os';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { Worker, isMainThread } from 'node:worker_threads';
+
 const performCalculations = async () => {
-    // Write your code here
+    if(isMainThread) {
+        const results = [];
+        const __filename = fileURLToPath(import.meta.url),
+            __dirname = dirname(__filename);
+        const cpuCount = cpus().length;
+        const pathToWorkerFile = './worker.js';
+
+        const threadReadyPromises = Array.from({length: cpuCount}, (_, i) => {
+            return new Promise((resolve) => {
+                const thread = new Worker(join(__dirname, pathToWorkerFile));
+                const valueForThread = 10 + i;
+
+                thread.postMessage(valueForThread);
+                thread.on('message', (resultValue) => {
+                    results[i] = resultValue;
+                    resolve();
+                });
+                thread.on('error', (calculationError) => {
+                    results[i] = {status: 'error', data: null};
+                    resolve();
+                });
+            });
+        });
+
+        await Promise.all(threadReadyPromises);
+        console.log(results)
+    } else {
+        throw new Error('Calculation failed');
+    }
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,18 @@
+import { parentPort } from 'worker_threads';
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
     // This function sends result of nthFibonacci computations to main thread
+    parentPort.on('message', (messageFromMain) => {
+        try {
+            const resultForThread = nthFibonacci(messageFromMain);
+            parentPort.postMessage({status: 'resolved', data: resultForThread});
+        } catch (error) {
+            parentPort.postMessage({status: 'failed', data: null});
+        }
+    })
 };
 
 sendResult();
