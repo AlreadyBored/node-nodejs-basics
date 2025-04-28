0b0dc60905b8281912ac66810a958880db8d69a3
N1kolozz <mr.osievi5@gmail.com>
2025-04-28 18:57:01 +0400

    feat: implement zip solution

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+import { createReadStream, createWriteStream, read } from 'fs';
+import { createGzip } from 'zlib';
+import { join } from 'path';
+
+
 const compress = async () => {
     // Write your code here 
+    const sourceFile = join("files", 'fileToCompress.txt');
+    const destinationFile = join("files", 'archive.gz');
+
+    const readStream = createReadStream(sourceFile);
+    const gzipStream = createGzip();
+    const writeStream = createWriteStream(destinationFile);
+
+    readStream.pipe(gzipStream).pipe(writeStream);
+
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+import { createReadStream, createWriteStream, read } from 'fs';
+import { createGzip } from 'zlib';
+import { join } from 'path';
+
+
 const decompress = async () => {
     // Write your code here 
+    const sourceFile = join("files", 'archive.gz');
+    const destinationFile = join("files", 'fileToCompress.txt');
+
+    const readStream = createReadStream(sourceFile);
+    const gzipStream = createGzip();
+    const writeStream = createWriteStream(destinationFile);
+
+    readStream.pipe(gzipStream).pipe(writeStream);
+
 };
 
 await decompress();
