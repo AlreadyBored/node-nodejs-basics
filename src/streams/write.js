d130ffd8eb432073d3c12925b0388af016e3f409
NataliiaVirt <virtnatalie@gmail.com>
2025-04-28 22:19:06 +0300

    Update_write.js in streams folder
    Add write.js to streams folder

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,34 @@
+import { createWriteStream } from 'fs';
+import path from 'path';
+
 const write = async () => {
-    // Write your code here 
+  const filePath = path.join('files', 'fileToWrite.txt');
+
+  try {
+    const writableStream = createWriteStream(filePath, { encoding: 'utf8' });
+
+    writableStream.on('error', (err) => {
+      throw new Error('FS operation failed');
+    });
+
+    process.stdin.setEncoding('utf8');
+
+    process.stdin.on('data', (chunk) => {
+      writableStream.write(chunk);
+    });
+
+    process.stdin.on('end', () => {
+      writableStream.end();
+      console.log('Success!');
+    });
+
+    process.stdin.on('error', () => {
+      throw new Error('FS operation failed');
+    });
+
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await write();
