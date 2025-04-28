9851d7da2ceb4d7a7aee8ccd5cb5c4f616b8c93a
Jesper Høgsted <81517227+JesperHogstedDk@users.noreply.github.com>
2025-04-28 21:13:06 +0200

    feat: implement performCalculations and sendResult

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,38 @@
+import { Worker, isMainThread } from 'node:worker_threads';
+
+const createWorkerThreadPromise = (fibno, coreNo) =>
+    new Promise((resolve, reject) => {
+        const worker = new Worker('./src/wt/worker.js', { workerData: fibno });
+        worker.on('message', (result) => {
+            // console.log(`Result received from worker ${coreNo}: `, result);
+            const workerResult = { status: 'resolved', data: result, workerNo: coreNo }
+            console.log('workerResult: ', workerResult)
+            resolve(workerResult);
+        }),
+        worker.on('error', (reject) => {
+            console.log(`Error Result received from worker ${coreNo}: `, reject);
+            const errorResult = { status: 'error', data: null };
+            reject;
+        });
+    });
+
 const performCalculations = async () => {
-    // Write your code here
+    if (isMainThread) {
+        Promise.allSettled([
+            createWorkerThreadPromise(10, 1),
+            createWorkerThreadPromise(11, 2),
+            createWorkerThreadPromise(12, 3),
+            createWorkerThreadPromise(13, 4)])
+            .then(results => {
+                console.log('Promise results: ', results);
+            })
+            .catch(error => {
+                console.error('Errors: ', error);
+            })
+            .finally(() => {
+                console.log('The promise has completed'); 
+            });
+    }
 };
 
 await performCalculations();
