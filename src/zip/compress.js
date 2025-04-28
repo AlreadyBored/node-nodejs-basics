01bf1cbc68310bebcbd04a5bd473a3ab636f58c5
Roman Gromov <gromov.r.v.92@gmail.com>
2025-04-28 23:58:31 +0500

    feat: implement compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { join } from 'path';
+import { createGzip } from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+    const fileToCompressPath = join('files', 'fileToCompress.txt');
+    const archivePath = join('files', 'archive.gz');
+
+    const readStream = createReadStream(fileToCompressPath);
+    const writeStream = createWriteStream(archivePath);
+    const gzip = createGzip();
+
+    readStream.on('error', () => {
+        throw new Error('FS operation failed');
+    });
+
+    writeStream.on('error', () => {
+        throw new Error('FS operation failed');
+    });
+
+    readStream.pipe(gzip).pipe(writeStream);
 };
 
 await compress();
