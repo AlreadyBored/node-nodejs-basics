693c147da148e23bc0aa232abc51e11b2364eea2
BrustS <s3rg3yburak@ya.ru>
2025-04-28 23:15:26 +0300

    add:zip sections solution

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,15 @@
+import { pipeline } from 'stream/promises';
+import { createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from 'zlib';
+
+
+const fileForCompress = './files/fileToCompress.txt';
+const fileForArchive = './files/archive.gz';
+const pathToFile = new URL(fileForCompress, import.meta.url);
+const pathToFileArchive = new URL(fileForArchive, import.meta.url);
+
 const decompress = async () => {
-    // Write your code here 
+   await pipeline(createReadStream(pathToFileArchive), createGunzip(), createWriteStream(pathToFile));
 };
 
 await decompress();
