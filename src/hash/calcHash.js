2794b97bcde9d1c73599c4ab99ea4552b64dc209
Roman Gromov <gromov.r.v.92@gmail.com>
2025-04-28 23:49:58 +0500

    feat: implement calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import { join } from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filesPath = join('files', 'fileToCalculateHashFor.txt');
+    const hash = createHash('sha256');
+    const stream = createReadStream(filesPath);
+
+    stream.on('error', () => {
+        throw new Error('FS operation failed')
+    });
+    stream.on('data', (chunk) => hash.update(chunk));
+    stream.on('end', () => console.log(hash.digest('hex')));
 };
 
 await calculateHash();
