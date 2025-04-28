53879a119b55bb0fbb3ab1d170c45380b8ba0bac
Roman Gromov <gromov.r.v.92@gmail.com>
2025-04-28 20:19:28 +0500

    feat: implement list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
+import { promises as fs } from 'fs';
+import { join } from 'path';
+
 const list = async () => {
-    // Write your code here 
+    const filesPath = join('files');
+
+    try {
+        await fs.access(filesPath);
+        const files = await fs.readdir(filesPath);
+
+        console.log(files);
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await list();
