15040d26f16922343813a76cb2311015b19e77fb
Roman Gromov <gromov.r.v.92@gmail.com>
2025-04-28 20:20:31 +0500

    feat: implement read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import { promises as fs } from 'fs';
+import { join } from 'path';
+
 const read = async () => {
-    // Write your code here 
+    const fileToReadPath = join('files', 'fileToRead.txt');
+
+    try {
+        await fs.access(fileToReadPath);
+        const content = await fs.readFile(fileToReadPath, 'utf-8');
+
+        console.log(content);
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
