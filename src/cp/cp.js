6c09104ea5bcca09b00f46be0b18d77418a455c3
Ann Kedich <keda20021002@gmail.com>
2025-04-28 20:18:54 +0300

    feat: implement cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,12 @@
+import { spawn } from 'child_process';
+import { stdin, stdout } from 'process';
+import path from 'path';
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const parentFilePath = path.join(import.meta.dirname, 'files', 'script.js');
+    const child = spawn('node', [parentFilePath, ...args]);
+    stdin.pipe(child.stdin);
+    child.stdout.pipe(stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ['someArgument1', 'someArgument2', 'someArgument3']);
