bcfcb2dbdaf7db3f4f18affb529cd3c944bc91f7
NataliiaVirt <virtnatalie@gmail.com>
2025-04-28 14:54:33 +0300

    Update create.js
    Add a create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+
 const create = async () => {
-    // Write your code here 
-};
+  const filePath = path.join('files', 'fresh.txt');
+
+  try {
+    await fs.access(filePath);
+    throw new Error('FS operation failed');
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+        fs.writeFile(filePath, 'I am fresh and young', 'utf8') 
+    } else {
+        throw new Error('FS operation failed');
+    }
+  }};
 
 await create();
