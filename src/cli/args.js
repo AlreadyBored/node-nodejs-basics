34e37c4b569d8e7d3254d39d31fc327f3267df3f
davidtavartk <d.tavartkiladze5@gmail.com>
2025-04-28 15:01:37 +0400

    feat: add args in cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const result = [];
+
+  for (let i = 0; i < args.length; i += 2) {
+    const propName = args[i].slice(2);
+    const value = args[i + 1];
+
+    result.push(`${propName} is ${value}`);
+  }
+
+  console.log(result.join(", "));
 };
 
 parseArgs();
