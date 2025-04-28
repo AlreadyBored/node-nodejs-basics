38812b6291592c926ae80b1304c88b411634fd74
Ann Kedich <keda20021002@gmail.com>
2025-04-28 14:05:28 +0300

    feat: implement hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,10 @@
+import crypto from 'crypto';
+import fs from 'node:fs';
+import { stdout } from 'node:process';
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = crypto.createHash('sha256');
+    const input = fs.createReadStream(`${import.meta.dirname}/files/fileToCalculateHashFor.txt`);
+    input.pipe(hash).setEncoding('hex').pipe(stdout);
 };
 
 await calculateHash();
