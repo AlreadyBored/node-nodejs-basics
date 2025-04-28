1a66b54e68d0a5c4e514463dc5972b695c49e8fa
Fedor Kaputsky <fedor.kaputsky@sap.com>
2025-04-28 17:49:04 +0300

    feat: add functionality to create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,26 @@
+import { access, writeFile, constants } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+const ERROR_CODE_NOT_EXIST = "ENOENT";
+
 const create = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const folderPath = path.join(__dirname, "files");
+    const filePath = path.join(folderPath, "fresh.txt");
+    const fileContent = "I am fresh and young";
+
+    access(filePath, constants.R_OK, (error) => {
+        if (error && error.code === ERROR_CODE_NOT_EXIST) {
+            writeFile(filePath, fileContent, (err) => {
+                if (err) throw err;
+                console.log("File created!");
+            });
+        } else {
+            throw new Error("FS operation failed");
+        }
+    });
 };
 
 await create();
