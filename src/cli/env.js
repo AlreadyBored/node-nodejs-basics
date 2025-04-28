aa5dee9b6673d742e6471b244c4df4e1a3759ef3
Fedor Kaputsky <fedor.kaputsky@sap.com>
2025-04-28 19:23:13 +0300

    feat: add solution to env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
+const searchPart = "RSS_";
+
 const parseEnv = () => {
-    // Write your code here 
+    const envVariables = process.env;
+
+    const rssVariables = Object.entries(envVariables)
+        .filter(([key]) => key.startsWith(searchPart))
+        .map(([key, value]) => `${key}=${value}`);
+
+    const result = rssVariables.join("; ");
+    console.log(result);
 };
 
 parseEnv();
