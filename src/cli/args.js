5e014cafeebc8047de026f62811918baa3b7cb45
Gennady Kashin <gennady.kashin94@gmail.com>
2025-04-28 19:55:01 +0300

    feat: cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+    try {
+        const args = process.argv.slice(2);
+        const result = args
+            .filter((_, i) => i % 2 === 0)
+            .map((key, i) => `${key.slice(2)} is ${args[i * 2 + 1]}`)
+            .join(', ')
+        console.log(result);        
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 parseArgs();
