114172cf2789796ac50be01e9bd664d374ea3389
Bogdan Ilin <starkavichi@gmail.com>
2025-04-28 19:10:26 +0300

    feat: implement child process spawning functionality in cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,14 @@
+import path from "node:path";
+import { fork } from "node:child_process";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const scriptPath = path.join(import.meta.dirname, "files", "script.js");
+  const childProcess = fork(scriptPath, args, { silent: true });
+
+  process.stdin.pipe(childProcess.stdin);
+  childProcess.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess();
+
