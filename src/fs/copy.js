899f5e9abf96f07dcd6eae29ff80745e816d6b64
Ann Kedich <keda20021002@gmail.com>
2025-04-28 12:44:41 +0300

    refactor: change apply methods

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -15,19 +15,23 @@ const copy = async () => {
         }
     });
 
-    fs.mkdir(copyDir, { recursive: true }, (err) => {});
-    fs.readdir(sourceDir, (err, files) => {
-        files.forEach(file => {
-            const filePath = path.join(sourceDir, file);
-            fs.readFile(filePath, {encoding: 'utf8'}, (err, data) => {
-                if (err) {
-                    console.error('Error reading:', err);
-                } else {
-                    fs.writeFile(path.join(copyDir, file), data, {flag: 'w+'}, err => {});
-                }
-            })
-        })
+    fs.cp(sourceDir, copyDir, { recursive: true }, (err) => {
+        if(err) throw err;
     });
+
+    //
+    // fs.readdir(sourceDir, (err, files) => {
+    //     files.forEach(file => {
+    //         const filePath = path.join(sourceDir, file);
+    //         fs.readFile(filePath, {encoding: 'utf8'}, (err, data) => {
+    //             if (err) {
+    //                 console.error('Error reading:', err);
+    //             } else {
+    //                 fs.writeFile(path.join(copyDir, file), data, {flag: 'w+'}, err => {});
+    //             }
+    //         })
+    //     })
+    // });
 };
 
 await copy();
