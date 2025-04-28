3c1220a58bbded7937349b77c6db085a23b11aa7
Sviatoslav Pokhvalenko <pokhvalenkosvyat@gmail.com>
2025-04-28 23:37:34 +0200

    cli task finished

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const res = [];
+
+  for (let i = 0; i < args.length; i += 2) {
+    const key = args[i].replace(/^--/, "");
+    const val = args[i + 1];
+
+    res.push(`${key} is ${val}`);
+  }
+
+  console.log(res.join(", "));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envArr = Object.entries(process.env)
+    .filter(([key]) => key.startsWith("RSS_"))
+    .map(([key, val]) => `${key}=${val}`)
+    .join("; ");
+
+  console.log(envArr);
 };
 
 parseEnv();
