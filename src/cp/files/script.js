4a9cc25b9f1a58f6a1bdf26bd4550c7df91a85c7
Alexey Koran <alexey.koran@gmail.com>
2025-05-01 18:14:59 +0400

    refactor: `cp script.js`, use EOL instead of `\n`

--- src/cp/files/script.js
+++ src/cp/files/script.js
@@ -1,12 +1,14 @@
+import { EOL } from 'node:os';
+
 const args = process.argv.slice(2);
 
 console.log(`Total number of arguments is ${args.length}`);
-console.log(`Arguments: ${JSON.stringify(args)}`);
+console.log(`Arguments: ${JSON.stringify(args)}${EOL}`);
 
 const echoInput = (chunk) => {
   const chunkStringified = chunk.toString();
   if (chunkStringified.includes('CLOSE')) process.exit(0);
-  process.stdout.write(`Received from master process: ${chunk.toString()}\n`)
+  process.stdout.write(`Received from master process: ${chunk.toString()}${EOL}`)
 };
 
 process.stdin.on('data', echoInput);
