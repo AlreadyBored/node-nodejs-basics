4df31c1f1710628cd4aec88cc3f9b223dad29d75
Jesper Høgsted <81517227+JesperHogstedDk@users.noreply.github.com>
2025-05-01 19:12:31 +0200

    Delete src directory

--- src/streams/write.js
+++
@@ -1,9 +0,0 @@
-const write = async () => {
-    // Write your code here 
-    const { createWriteStream } = await import('fs')
-    const writableStream = createWriteStream('./src/streams/files/fileToWrite.txt');
-    process.stdin.pipe(writableStream);
-    console.log('Type something and press Enter. It will be saved to fileToWrite.txt.');
-};
-
-await write();
